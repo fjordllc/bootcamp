565a1efb5f2832bfda32dbe57db66e1fdb824fe3
Hiroshi Yoshida <hrysd22@gmail.com>
2013-03-13 16:07:35 +0900

    Move method to helper

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,16 +1,4 @@
 class Practice < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
-
-  def rendering
-    Redcarpet::Markdown.new(
-      Redcarpet::Render::HTML,
-      no_intra_emphasis:   true,
-      fenced_code_blocks:  true,
-      autolink:            true,
-      tables:              true,
-      superscript:         true,
-      space_after_headers: true
-    ).render(self.description)
-  end
 end
