aace27bd007a3ded063293a254e12d19e5e64449
Hiroshi Yoshida <hrysd22@gmail.com>
2013-03-13 15:53:25 +0900

    Change rendering method

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,17 +1,16 @@
 class Practice < ActiveRecord::Base
-  before_validation :rendering
   validates :title, presence: true
+  validates :description, presence: true
 
-  protected
-    def rendering
-      self.description = Redcarpet::Markdown.new(
-        Redcarpet::Render::HTML,
-        no_intra_emphasis:   true,
-        fenced_code_blocks:  true,
-        autolink:            true,
-        tables:              true,
-        superscript:         true,
-        space_after_headers: true
-      ).render(self.draft) if self.draft.present?
-    end
+  def rendering
+    Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML,
+      no_intra_emphasis:   true,
+      fenced_code_blocks:  true,
+      autolink:            true,
+      tables:              true,
+      superscript:         true,
+      space_after_headers: true
+    ).render(self.description)
+  end
 end
