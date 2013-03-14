b203c6f0a3ff5a4ceb19904a6fd320f2a0e34cac
Hiroshi Yoshida <hrysd22@gmail.com>
2013-03-14 15:10:47 +0900

    Add learning status.

---
+++ app/models/learning.rb
@@ -0,0 +1,10 @@
+class Learning < ActiveRecord::Base
+  as_enum :status, active: 0, complete: 1
+  belongs_to :user
+  belongs_to :practice
+
+  validates :user_id, presence: true
+  validates :practice_id,
+    presence: true,
+    uniqueness: { scope: :user_id }
+end
