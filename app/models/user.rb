b0435e0250a4678571b60684c699188f0b5dc458
Masaki Komagata <komagata@gmail.com>
2013-03-25 21:39:41 +0900

    Add completed percentage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   as_enum :job, [:programmer, :designer]
   authenticates_with_sorcery!
+
   has_many :learnings
 
   has_many :completed_learnings,
     -> { where(status_cd: 1) },
     class_name: 'Learning'
+
   has_many :completed_practices,
     through: :completed_learnings,
     source: :practice
@@ -13,6 +15,7 @@ class User < ActiveRecord::Base
   has_many :active_learnings,
     -> { where(status_cd: 0) },
     class_name: 'Learning'
+
   has_many :active_practices,
     through: :active_learnings,
     source: :practice
@@ -23,4 +26,16 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def my_practices_size
+    Practice.where(target_cd: job_cd + 1).size.to_f
+  end
+
+  def completed_my_practices_size
+    completed_practices.where(target_cd: job_cd + 1).size.to_f
+  end
+
+  def completed_percentage
+    completed_my_practices_size / my_practices_size * 100
+  end
 end
