a18be55fc02ac0b2a0c38ea2f04b1f76a0b953ad
Masaki Komagata <komagata@gmail.com>
2013-03-25 14:57:10 +0900

    Fixed

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ ruby-2.0.0-p
     $ bundle
     $ rake db:setup
     $ rails s
+
+### Heroku
+
+    $ heroku run rake db:migrate db:seed assets:precompile
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Interns::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
