0dc79490d5b6eee49d072c8015e4fbf6029258c3
Masaki Komagata <komagata@gmail.com>
2013-03-25 14:36:18 +0900

    Disabled assets comipile in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Interns::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
