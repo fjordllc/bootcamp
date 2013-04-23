c3c33facb55c0b7f38685166f89a1b36f72b50f0
Masaki Komagata <komagata@gmail.com>
2013-04-23 20:44:06 +0900

    Fix layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
       redirect_to controller: :users, action: :index
     end
 
-    render action: 'index', layout: false
+    render :action 'index', layout: false
   end
 
   def application
