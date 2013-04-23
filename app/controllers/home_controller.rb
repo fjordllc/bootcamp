e9f169031bcbb8240698c64732ffd46527a2ddf4
Masaki Komagata <komagata@gmail.com>
2013-04-23 20:47:47 +0900

    Fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
+  layout false, only: %w(index)
   def index
     if current_user.present?
       redirect_to controller: :users, action: :index
     end
-
-    render :action 'index', layout: false
   end
 
   def application
