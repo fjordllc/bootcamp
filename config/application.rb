32ffcdcef2012c3ae4109252ca919d9b5b45bc57
Hiroshi Yoshida <hrysd22@gmail.com>
2013-06-08 20:11:41 +0900

    Update rails gem.
    - rake rails:update
    - Add database_cleaner
    - Sort out Gemfile Alphabet order
    - Unfork `active_decorator`

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Assets should be precompiled for production (so we don't need the gems loaded then)
-Bundler.require(*Rails.groups(assets: %w(development test)))
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
 
 module Interns
   class Application < Rails::Application
@@ -13,12 +14,10 @@ module Interns
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
     config.i18n.default_locale = :ja
 
     config.generators do |g|
