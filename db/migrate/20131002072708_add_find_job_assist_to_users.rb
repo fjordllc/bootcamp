b32ed1dc32f47ddc43ed110b9c8f4cdd161d9c2e
Masaki Komagata <komagata@gmail.com>
2013-10-02 17:29:21 +0900

    Fix default value

--- db/migrate/20131002072708_add_find_job_assist_to_users.rb
+++ db/migrate/20131002072708_add_find_job_assist_to_users.rb
@@ -1,5 +1,5 @@
 class AddFindJobAssistToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :find_job_assist, :boolean, null: false, default: 0
+    add_column :users, :find_job_assist, :boolean, null: false, default: false
   end
 end
