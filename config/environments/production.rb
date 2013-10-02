8154e79c54b00d68e8d1259437bb5314904dd0aa
Masaki Komagata <komagata@gmail.com>
2013-10-02 18:45:51 +0900

    Add config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Interns::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.initialize_on_precompile = true
 end
