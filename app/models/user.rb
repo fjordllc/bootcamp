65e317c6c081935300056d53cd730676595e3e71
Masaki Komagata <komagata@gmail.com>
2013-10-10 18:09:11 +0900

    Add old functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,22 @@ class User < ActiveRecord::Base
   belongs_to :company
   has_many :learnings
 
+  has_many :completed_learnings,
+    -> { where(status_cd: 1) },
+    class_name: 'Learning'
+
+  has_many :completed_practices,
+    through: :completed_learnings,
+    source: :practice
+
+  has_many :active_learnings,
+    -> { where(status_cd: 0) },
+    class_name: 'Learning'
+
+  has_many :active_practices,
+    through: :active_learnings,
+    source:  :practice
+
   validates :company_id, presence: true
   validates :email,      presence: true, uniqueness: true
   validates :first_name, presence: true
