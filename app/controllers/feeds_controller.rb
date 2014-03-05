91b0e8cbb93b4a72775454053cd303087bce51b2
komagata <komagata@gmail.com>
2014-03-05 16:27:10 +0900

    Revmoe static feed list

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,7 +3,6 @@ class FeedsController < ApplicationController
   def index
     @entries = []
     urls = User.pluck(:feed_url).reject {|f| f.blank? }
-    urls = ["http://dresscording.com/blog/", "https://www.facebook.com/teiyu.ueki", "http://nomnel.net/atom.xml", "http://sasa1010.hatenablog.com/feed", "http://intern-intern.tumblr.com/rss", "http://jabropt.com/atom.xml", "http://satoko.tumblr.com/rss", "http://docs.komagata.org/index.atom", "http://shigotop.hatenablog.com/feed", "http://keenjoe.hatenadiary.com/", "http://gotagotagoat.hatenablog.com/", "http://yukihr.github.io/fjord_intern/feed.xml", "http://a-few-resources.hatenablog.com/feed", "http://diskogs.hatenablog.com/feed", "http://korosuke565.hatenablog.com/feed", "http://nnaoto0606.hatenablog.com/feed", "http://smellmemory.hatenablog.com/rss", "http://okami6.hatenablog.com/feed", "http://blog.goo.ne.jp/moonycat/index.rdf"]
     @feeds = Feedzirra::Feed.fetch_and_parse(urls)
     @feeds.each do |url, feed|
       if feed.respond_to?(:entries) && feed.entries.present?
