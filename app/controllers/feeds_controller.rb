72899b014032483bc86a8e522ccd964580e54af7
katsumata_ryo <ryo@andamp.jp>
2015-11-22 13:41:43 +0900

    resolving for feed 500 error(feedjira2.0.0)

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,13 +3,16 @@ class FeedsController < ApplicationController
   def index
     @entries = []
     urls = User.pluck(:feed_url).reject {|f| f.blank? }
-    @feeds = Feedjira::Feed.fetch_and_parse(urls)
-    @feeds.each do |url, feed|
-      if feed.respond_to?(:entries) && feed.entries.present?
-        feed.entries.each {|e| @entries << e }
+    urls.each do |url|
+      begin
+        @feed = Feedjira::Feed.fetch_and_parse(url)
+        if @feed.respond_to?(:entries) && @feed.entries.present?
+          @feed.entries.each {|e| @entries << e }
+        end
+      rescue
+        next
       end
     end
-
     @entries.sort! {|a, b| b.published <=> a.published }
   end
 end
