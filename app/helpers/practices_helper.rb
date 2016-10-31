e4a4b819cd4d635e1b8e491c6482491162e9c97b
Masaki Komagata <komagata@gmail.com>
2016-10-31 14:35:07 +0900

    Kowabanaのコードが混入していたのを削除

--- app/helpers/practices_helper.rb
+++ app/helpers/practices_helper.rb
@@ -11,7 +11,7 @@ module PracticesHelper
   end
 
   def markdown(text)
-    render = Kowabana::Render.new(hard_wrap: true)
+    render = Redcarpet::Render::HTML.new(hard_wrap: true)
     markdown_options = { autolink: true, no_intra_emphasis: true, tables: true }
     markdown = Redcarpet::Markdown.new(render, markdown_options)
     markdown.render(text).html_safe
