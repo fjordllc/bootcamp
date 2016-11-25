e23ceb6504bc97883ef3cf90b23c1e291b33de0f
teppei machida <machida@fjord.jp>
2016-11-25 16:37:16 +0900

    wip

--- app/helpers/current_link_helper.rb
+++ app/helpers/current_link_helper.rb
@@ -1,6 +1,8 @@
 module CurrentLinkHelper
   def current_link(path)
-    if path == request.fullpath
+    if !path.include?('admin') && request.fullpath.include?('admin')
+      false
+    elsif request.fullpath.include?(path)
       'is-active'
     end
   end
