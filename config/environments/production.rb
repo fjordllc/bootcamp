ccda99ad8bca1f4decbc05c79081dcf0621c96ee
Hisashi Kamezawa <hkame@outlook.jp>
2016-12-01 19:28:15 +0900

    fix smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,22 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  # config.action_mailer.smtp_settings = {
+  #   authentication: :plain,
+  #   address:        'smtp.sendgrid.net',
+  #   port:           587,
+  #   domain:         'heroku.com',
+  #   user_name:      ENV['SENDGRID_USERNAME'],
+  #   password:       ENV['SENDGRID_PASSWORD']
+  # }
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
     authentication: :plain,
-    address:        'smtp.sendgrid.net',
-    port:           587,
-    domain:         'heroku.com',
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD']
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain:  'heroku.com',
+    enable_starttls_auto: true
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: '256interns.com' }
