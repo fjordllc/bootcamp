4bb2e497a61bf9426a446e7dc9c03bdfca492d15
Hisashi Kamezawa <hkame@outlook.jp>
2016-12-02 14:23:07 +0900

    bin/rails db:migrate RAILS_ENV=test

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20161212065808) do
     t.integer  "report_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["report_id"], name: "index_comments_on_report_id", using: :btree
-    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+    t.index ["report_id"], name: "index_comments_on_report_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "companies", force: :cascade do |t|
@@ -84,24 +84,14 @@ ActiveRecord::Schema.define(version: 20161212065808) do
     t.text     "goal"
     t.integer  "category_id"
     t.integer  "position"
-    t.index ["category_id"], name: "index_practices_on_category_id", using: :btree
+    t.index ["category_id"], name: "index_practices_on_category_id"
   end
 
   create_table "practices_reports", id: false, force: :cascade do |t|
     t.integer "practice_id", null: false
     t.integer "report_id",   null: false
-    t.index ["practice_id", "report_id"], name: "index_practices_reports_on_practice_id_and_report_id", using: :btree
-    t.index ["report_id", "practice_id"], name: "index_practices_reports_on_report_id_and_practice_id", using: :btree
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "correct_answer_id"
-    t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
+    t.index ["practice_id", "report_id"], name: "index_practices_reports_on_practice_id_and_report_id"
+    t.index ["report_id", "practice_id"], name: "index_practices_reports_on_report_id_and_practice_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -138,11 +128,7 @@ ActiveRecord::Schema.define(version: 20161212065808) do
     t.boolean  "retire",                       default: false, null: false
     t.boolean  "nda",                          default: true,  null: false
     t.string   "slack_account"
-    t.string   "reset_password_token"
-    t.datetime "reset_password_token_expires_at"
-    t.datetime "reset_password_email_sent_at"
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
 
 end
