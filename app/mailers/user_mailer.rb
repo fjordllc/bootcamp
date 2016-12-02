51d60a59ccf6503e7b227ac47436918f5f4ae5cf
Hisashi Kamezawa <hkame@outlook.jp>
2016-12-02 15:25:53 +0900

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
-class UserMailer < ActionMailer::Base
-  default from: "from@256interns.com"
+class UserMailer < ApplicationMailer
+  default from: "from@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
