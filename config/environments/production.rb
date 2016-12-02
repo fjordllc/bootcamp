df87f4d5ef978cd8755e29152c807f47785b50da
Hisashi Kamezawa <hkame@outlook.jp>
2016-12-02 11:54:41 +0900

    fix mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,9 +28,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
@@ -77,18 +74,17 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   authentication: :plain,
-  #   address:        'smtp.sendgrid.net',
-  #   port:           587,
-  #   domain:         'heroku.com',
-  #   user_name:      ENV['SENDGRID_USERNAME'],
-  #   password:       ENV['SENDGRID_PASSWORD']
-  # }
+  config.action_mailer.raise_delivery_errors = :smtp
+  config.action_mailer.default_url_options = { host: '256interns.com' }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
@@ -98,6 +94,4 @@ Rails.application.configure do
     domain:  'heroku.com',
     enable_starttls_auto: true
   }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: '256interns.com' }
 end
