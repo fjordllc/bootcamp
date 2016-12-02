7f282c553f8308a108a94ce3f472ee959391807d
Hisashi Kamezawa <hkame@outlook.jp>
2016-12-02 15:50:23 +0900

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = :smtp
   config.action_mailer.default_url_options = { host: '256interns.com' }
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
