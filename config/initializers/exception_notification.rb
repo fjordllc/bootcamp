333cec16017a688c68d8c58e607b46ef446e247d
Hisashi Kamezawa <hkame@outlook.jp>
2016-12-06 15:00:47 +0900

    delete exception_notification

--- config/initializers/exception_notification.rb
+++
@@ -1,6 +0,0 @@
-Interns::Application.config.middleware.use ExceptionNotification::Rack,
-  :email => {
-    :email_prefix => "[interns] ",
-    :sender_address => %{"notifier" <notifier@fjord.jp>},
-    :exception_recipients => %w{develop@fjord.jp}
-  }
