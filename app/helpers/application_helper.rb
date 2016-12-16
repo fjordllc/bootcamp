cd47032dfdfa669a08fcce4ce012c6fc2ad9803b
Masaki Komagata <komagata@gmail.com>
2016-12-16 18:31:40 +0900

    gemのバージョンを揃えたでのコンフリクト解消

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,14 @@ module ApplicationHelper
     content_tag_for(:tr, record, prefix, options, &block)
   end
 
+  def page_slug
+    controller.class.to_s.underscore.
+      gsub(%r{/}, "-").
+      gsub(/_controller/, "_") + action_name
+  end
+
   def my_practice?(practice)
     return false if current_user.blank?
     [:everyone, current_user.job].include?(practice.target)
   end
-
 end
