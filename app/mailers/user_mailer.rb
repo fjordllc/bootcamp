59c4f7f3504d24f620417847fb2ea070e7899200
Hisashi Kamezawa <hkame6926@gmail.com>
2017-01-13 16:46:02 +0900

    fix setting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@interns.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
   def reset_password_email
     @greeting = "Hi"
 
-    mail to: "to@example.org"
+    mail to: "hkame6926@gmail.com"
   end
 
   def contact_email(contact)
