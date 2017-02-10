b5b9e50854ddd98589bd7fda06e6076288502832
Hisashi Kamezawa <hkame6926@gmail.com>
2017-02-10 12:56:10 +0900

    fix conflict

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,4 +1,8 @@
 module ReportsHelper
+  def recent_reports
+    @reports = Report.order(updated_at: :desc, id: :desc).limit(10)
+  end
+
   def report_title(report_id)
     title = Report.find(report_id).title
   end
