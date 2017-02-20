36155f7be6c51b84cf110ac0168f2090bcafdb28
Hisashi Kamezawa <hkame6926@gmail.com>
2017-02-20 15:50:13 +0900

    change = to -

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,5 @@
 module CommentsHelper
   def recent_comments
     @activities = PublicActivity::Activity.order(created_at: :desc).where(recipient_id: current_user.id, read_flag: 0).where.not(owner_id: current_user.id)
-    return
   end
 end
