e7dcd6193b94062cbbe00fe64b9e9d8427e02f86
hattori6789 <hattori.stdio@gmail.com>
2017-03-01 14:41:27 +0900

    質問のタイトルを保存前に検証する

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   belongs_to :correct_answer, foreign_key: :correct_answer_id, class_name: "Answer"
   has_many :answers
+  validates :title, presence: true, length: { maximum: 256 }
   validates :description, presence: true
   validates :user, presence: true
   scope :solved, -> { where.not(correct_answer_id: nil) }
