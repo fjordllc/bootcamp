acf9645e82d3b9f912d56f6370cc4c95b072bd3c
hattori6789 <hattori.stdio@gmail.com>
2017-06-28 11:26:01 +0900

    require_loginのテストを追加

---
+++ test/integration/require_login_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+
+class RequireLoginTest < ActionDispatch::IntegrationTest
+
+  test 'users_path' do
+    get '/users'
+    assert_redirected_to '/login'
+    assert_equal 'ログインしてください', flash[:alert]
+  end
+
+  test 'feeds_path' do
+    get '/feeds'
+    assert_redirected_to '/login'
+    assert_equal 'ログインしてください', flash[:alert]
+  end
+
+  test 'questions_path' do
+    get '/questions'
+    assert_redirected_to '/login'
+    assert_equal 'ログインしてください', flash[:alert]
+  end
+
+  test 'practices_path' do
+    get '/practices'
+    assert_redirected_to '/login'
+    assert_equal 'ログインしてください', flash[:alert]
+  end
+
+  test 'reports_path' do
+    get '/reports'
+    assert_redirected_to '/login'
+    assert_equal 'ログインしてください', flash[:alert]
+  end
+
+  test 'pages_path' do
+    get '/pages'
+    assert_redirected_to '/login'
+    assert_equal 'ログインしてください', flash[:alert]
+  end
+end
