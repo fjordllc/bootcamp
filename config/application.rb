d64770193ab6ace1d7835bfa0b4d52dc725df237
teppei machida <machida@fjord.jp>
2017-08-03 15:04:44 +0900

    redcarpet、gemojiを削除

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,5 @@ module Interns
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
-
-    #gemoji
-    config.assets.paths << Emoji.images_path
-    config.assets.precompile << "emoji/**/*.png"
   end
 end
