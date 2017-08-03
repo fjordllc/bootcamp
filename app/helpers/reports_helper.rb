1b3ba3427d1c2079138544558c570f6e2d221fb5
teppei machida <machida@fjord.jp>
2017-08-03 15:54:52 +0900

    recent-reports を共通化

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,9 +1,5 @@
 module ReportsHelper
   def recent_reports
-    @reports = Report.order(updated_at: :desc, id: :desc).limit(10)
-  end
-
-  def all_recent_reports
-    @reports = Report.order(updated_at: :desc, id: :desc).limit(16)
+    @reports = Report.order(updated_at: :desc, id: :desc).limit(15)
   end
 end
