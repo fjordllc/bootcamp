4e708d76f2a8a10aff2ac8c94226a5c331afcecd
teppei machida <machida@fjord.jp>
2017-08-03 15:40:13 +0900

    全ページに最新の日報を表示するようにした

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,4 +2,8 @@ module ReportsHelper
   def recent_reports
     @reports = Report.order(updated_at: :desc, id: :desc).limit(10)
   end
+
+  def all_recent_reports
+    @reports = Report.order(updated_at: :desc, id: :desc).limit(16)
+  end
 end
