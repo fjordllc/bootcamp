093df12ff18a8584f8f6a15f41b86b0f9bc4c3e3
hirotaka <developer.hiro.east9.hey.world@gmail.com>
2017-08-07 22:08:46 +0900

    チェックする時の条件をわかりやすい名前に変更。

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,4 +2,19 @@ module ReportsHelper
   def recent_reports
     @reports = Report.order(updated_at: :desc, id: :desc).limit(15)
   end
+
+  def admin_can_check_it?
+    current_user.admin? && (un_checked_report? and not_report_user?)
+  end
+
+  private
+
+  def un_checked_report?
+    !current_user.checks.find_by(report_id: params[:id])
+  end
+
+  def not_report_user?
+    !(current_user == @report.user)
+  end
+
 end
