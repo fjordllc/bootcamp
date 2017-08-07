d4e49ba8a29f81248510681a9ecb3f428cbe194b
Masaki Komagata <komagata@gmail.com>
2017-08-08 01:05:10 +0900

    migrationのバージョンを追加

--- db/migrate/20161127104306_sorcery_reset_password.rb
+++ db/migrate/20161127104306_sorcery_reset_password.rb
@@ -1,4 +1,4 @@
-class SorceryResetPassword < ActiveRecord::Migration
+class SorceryResetPassword < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :reset_password_token, :string, :default => nil
     add_column :users, :reset_password_token_expires_at, :datetime, :default => nil
