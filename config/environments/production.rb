697d14ea2723bc1ffb723508726d086dbb5f9031
midoriasi <midoriasi@gmail.com>
2017-08-08 12:51:57 +0900

    rubocop --auto-correct --only Layout/IndentationConsistency

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,13 +90,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: "256interns.com" }
-    ActionMailer::Base.smtp_settings = {
-      address: "smtp.sendgrid.net",
-      port: "587",
-      authentication: :plain,
-      user_name: ENV["SENDGRID_USERNAME"],
-      password: ENV["SENDGRID_PASSWORD"],
-      domain: "heroku.com",
-      enable_starttls_auto: true
-    }
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: "587",
+    authentication: :plain,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    enable_starttls_auto: true
+  }
 end
