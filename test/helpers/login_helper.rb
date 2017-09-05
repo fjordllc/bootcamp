94294029bca5bfaf6280cea2ffe387a33694c0ad
Masaki Komagata <komagata@gmail.com>
2017-09-05 17:29:58 +0900

    fix typo

--- test/helpers/login_helper.rb
+++ test/helpers/login_helper.rb
@@ -1,9 +1,9 @@
 module LoginHelper
-  def login_user(login_name, passwrod)
+  def login_user(login_name, password)
     visit "/login"
     within("#sign-in-form") do
       fill_in("user[login_name]", with: login_name)
-      fill_in("user[password]", with: passwrod)
+      fill_in("user[password]", with: password)
     end
     click_button "サインイン"
   end
