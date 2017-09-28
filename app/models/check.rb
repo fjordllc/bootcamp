62a483428d30033096eaa0e7befe478af2d991a1
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-09-28 15:14:15 +0900

    日報へのチェックの通知を作成

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,6 +1,12 @@
 class Check < ApplicationRecord
   belongs_to :user
   belongs_to :report
+  after_create CheckCallbacks.new
   validates :user_id, presence: true
   validates :report_id, presence: true
+  alias_method :sender, :user
+
+  def reciever
+    report.user
+  end
 end
