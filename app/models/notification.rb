2ef79368aa72270f88d5b8528cfc2e64ff2d2227
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-09-29 00:38:57 +0900

    通知機能のデザイン

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -11,23 +11,23 @@ class Notification < ApplicationRecord
 
   def self.came_comment(comment)
     Notification.create!(
-        kind:    0,
-        user:    comment.reciever,
-        sender:  comment.sender,
-        path:    Rails.application.routes.url_helpers.report_path(comment.report),
-        message: "#{comment.sender.login_name}さんからコメントが届きました。",
-        read:    false
+      kind:    0,
+      user:    comment.reciever,
+      sender:  comment.sender,
+      path:    Rails.application.routes.url_helpers.report_path(comment.report),
+      message: "#{comment.sender.login_name}さんからコメントが届きました。",
+      read:    false
     )
   end
 
   def self.checked(check)
     Notification.create!(
-        kind:    1,
-        user:    check.reciever,
-        sender:  check.sender,
-        path:    Rails.application.routes.url_helpers.report_path(check.report),
-        message: "#{check.sender.login_name}さんが#{check.report.title}を確認しました。",
-        read:    false
+      kind:    1,
+      user:    check.reciever,
+      sender:  check.sender,
+      path:    Rails.application.routes.url_helpers.report_path(check.report),
+      message: "#{check.sender.login_name}さんが#{check.report.title}を確認しました。",
+      read:    false
     )
   end
 end
