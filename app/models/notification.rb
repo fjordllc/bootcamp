452647ee5a17c5f98728ae550ae9ef8d0a28e098
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-09-28 15:14:15 +0900

    日報へのチェックの通知を作成

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,18 +4,30 @@ class Notification < ApplicationRecord
 
   enum kind: {
       came_comment: 0,
+      checked:      1
   }
 
-  scope :unreads, -> { where(read: false) }
+  scope :unreads, -> { where(read: false).order(created_at: :desc) }
 
   def self.came_comment(comment)
     Notification.create!(
-      kind:    0,
-      user:    comment.reciever,
-      sender:  comment.sender,
-      path:    Rails.application.routes.url_helpers.report_path(comment.report),
-      message: "#{comment.sender.login_name}さんからコメントが届きました。",
-      read:    false
+        kind:    0,
+        user:    comment.reciever,
+        sender:  comment.sender,
+        path:    Rails.application.routes.url_helpers.report_path(comment.report),
+        message: "#{comment.sender.login_name}さんからコメントが届きました。",
+        read:    false
+    )
+  end
+
+  def self.checked(check)
+    Notification.create!(
+        kind:    1,
+        user:    check.reciever,
+        sender:  check.sender,
+        path:    Rails.application.routes.url_helpers.report_path(check.report),
+        message: "#{check.sender.login_name}さんが#{check.report.title}を確認しました。",
+        read:    false
     )
   end
 end
