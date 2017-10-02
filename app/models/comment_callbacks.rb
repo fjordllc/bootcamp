6bd81805cb8cb08d6b2aa129a09b8e693b54f969
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-10-02 13:10:59 +0900

    メンション実装

--- app/models/comment_callbacks.rb
+++ app/models/comment_callbacks.rb
@@ -1,7 +1,22 @@
 class CommentCallbacks
+  def before_save(comment)
+    send_mention(comment) if comment.new_mentions?
+  end
+
   def after_create(comment)
     if comment.sender != comment.reciever
       Notification.came_comment(comment)
     end
   end
+
+  private
+
+    def send_mention(comment)
+      comment.new_mentions.each do |mention|
+        reciever = User.find_by(login_name: mention)
+        if reciever && comment.sender != reciever
+          Notification.mentioned(comment, reciever)
+        end
+      end
+    end
 end
