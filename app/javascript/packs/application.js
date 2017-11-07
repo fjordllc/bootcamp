bd135577285b6cada42c11b5273e56a155a017a9
Masaki Komagata <komagata@gmail.com>
2017-11-07 17:58:11 +0900

    markdown-itを全てmarkedに置き換えた

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,4 +6,5 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-require("./markdown");
+require('./markdown');
+require('./marked');
