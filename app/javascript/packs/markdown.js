9ee77fe1f8130ebc65dfd2e608a0342e461c3266
Masaki Komagata <komagata@gmail.com>
2017-11-07 17:38:48 +0900

    practiceのエディタをwebpackerベースに変更

--- app/javascript/packs/markdown.js
+++ app/javascript/packs/markdown.js
@@ -4,14 +4,20 @@ import MarkdownEditor from '../markdown-editor'
 document.addEventListener('DOMContentLoaded', () => {
   const textareas = document.querySelectorAll('.js-markdown');
   if (textareas) {
-   for (let i = 0; i < textareas.length; i++) {
-     new MarkdownEditor(textareas[i]);
-   }
-  }
-  const preview = document.querySelector('.js-markdown-preview');
-  if (preview) {
-    textareas[0].addEventListener('keyup', () => {
-      preview.innerHTML = marked(textareas[0].value, { breaks: true });
-    })
+    for (let i = 0; i < textareas.length; i++) {
+      const textarea = textareas[i];
+      const previewId = textarea.getAttribute('data-preview-id');
+      new MarkdownEditor(textarea);
+      if (previewId) {
+        const preview = document.getElementById(previewId);
+        if (preview) {
+          preview.innerHTML = marked(textarea.value, { breaks: true });
+
+          textarea.addEventListener('keyup', () => {
+            preview.innerHTML = marked(textarea.value, { breaks: true });
+          })
+        }
+      }
+    }
   }
 });
