2c32b387d220be7c8080385d1e81d913216e8b9f
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-11-09 00:04:40 +0900

    Artifact(成果物)モデルのバリデーション設定

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,4 +1,8 @@
 class Artifact < ApplicationRecord
   belongs_to :user
   belongs_to :practice
+
+  validates :user_id, presence: true, uniqueness: { scope: :practice_id }
+  validates :practice_id, presence: true, uniqueness: { scope: :user_id }
+  validates :content, presence: true, length: { minimum: 5, maximum: 2000 }
 end
--- test/models/artifact_test.rb
+++ test/models/artifact_test.rb
@@ -1,7 +1,47 @@
-require 'test_helper'
+require "test_helper"
 
 class ArtifactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @artifact = artifacts(:artifact_1)
+  end
+
+  test "Should be valid artifact" do
+    assert @artifact.valid?
+  end
+
+  test "Should be user and practice with unique" do
+    dup_request = @artifact.dup
+    assert @artifact.valid?
+    assert_not dup_request.valid?
+  end
+
+  test "Invalid artifacts user_id blank" do
+    @artifact.user = nil
+    assert_not @artifact.valid?
+  end
+
+  test "Invalid artifacts practice_id blank" do
+    @artifact.practice_id = nil
+    assert_not @artifact.valid?
+  end
+
+  test "Invalid artifacts content nil" do
+    @artifact.content = nil
+    assert_not @artifact.valid?
+  end
+
+  test "Invalid artifacts content blank" do
+    @artifact.content = ""
+    assert_not @artifact.valid?
+  end
+
+  test "Invalid artifacts content short( 5 character )" do
+    @artifact.content = "a" * 4
+    assert_not @artifact.valid?
+  end
+
+  test "Invalid artifacts content long( 2000 character )" do
+    @artifact.content = "a" * 2001
+    assert_not @artifact.valid?
+  end
 end
