fcc71361b5b199f374414953ddf514e76c8e59b3
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-11-08 19:47:44 +0900

    課題有りプラクティスは一般ユーザーは完了できないように実装

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -2,17 +2,17 @@ class Practice < ActiveRecord::Base
   has_many :learnings
   has_and_belongs_to_many :reports
   has_many :started_learnings,
-    -> { where(status_cd: 0) },
-    class_name: "Learning"
+           -> { where(status_cd: 0) },
+           class_name: "Learning"
   has_many :completed_learnings,
-    -> { where(status_cd: 1) },
-    class_name: "Learning"
+           -> { where(status_cd: 1) },
+           class_name: "Learning"
   has_many :started_users,
-    through: :started_learnings,
-    source: :user
+           through: :started_learnings,
+           source:  :user
   has_many :completed_users,
-    through: :completed_learnings,
-    source: :user
+           through: :completed_learnings,
+           source:  :user
   belongs_to :category
   acts_as_list scope: :category
 
@@ -22,7 +22,7 @@ class Practice < ActiveRecord::Base
 
   def status(user)
     learnings = Learning.where(
-      user_id: user.id,
+      user_id:     user.id,
       practice_id: id
     )
     if learnings.blank?
@@ -47,6 +47,10 @@ class Practice < ActiveRecord::Base
       user:        user,
       practice_id: id
     )
+  end
+
+  def not_has_task_and_not_done?(user)
+    not_completed?(user) && !task?
   end
 
   def all_text
