34c16ae3d2b4b2b0b570c2702e3901fef3dab0b7
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-11-08 22:08:58 +0900

    課題有りプラクティスは管理者しか作れない。

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -9,10 +9,10 @@ class Practice < ActiveRecord::Base
     class_name: "Learning"
   has_many :started_users,
     through: :started_learnings,
-    source: :user
+    source:  :user
   has_many :completed_users,
     through: :completed_learnings,
-    source: :user
+    source:  :user
   belongs_to :category
   acts_as_list scope: :category
 
@@ -22,7 +22,7 @@ class Practice < ActiveRecord::Base
 
   def status(user)
     learnings = Learning.where(
-      user_id: user.id,
+      user_id:     user.id,
       practice_id: id
     )
     if learnings.blank?
@@ -36,15 +36,15 @@ class Practice < ActiveRecord::Base
     status_cds = Learning.statuses.values_at("complete")
 
     !Learning.exists?(
-      user: user,
+      user:        user,
       practice_id: id,
-      status_cd: status_cds
+      status_cd:   status_cds
     )
   end
 
   def exists_learning?(user)
     Learning.exists?(
-      user: user,
+      user:        user,
       practice_id: id
     )
   end
