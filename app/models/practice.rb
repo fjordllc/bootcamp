fee0c2c06a3dcb7cf212a363c7c2af6d7e1d9c2a
Hirotaka Wagai <developer.hiro.east9.hey.world@gmail.com>
2017-11-08 21:59:08 +0900

    コードリファクタリング

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -2,17 +2,17 @@ class Practice < ActiveRecord::Base
   has_many :learnings
   has_and_belongs_to_many :reports
   has_many :started_learnings,
-           -> { where(status_cd: 0) },
-           class_name: "Learning"
+    -> { where(status_cd: 0) },
+    class_name: "Learning"
   has_many :completed_learnings,
-           -> { where(status_cd: 1) },
-           class_name: "Learning"
+    -> { where(status_cd: 1) },
+    class_name: "Learning"
   has_many :started_users,
-           through: :started_learnings,
-           source:  :user
+    through: :started_learnings,
+    source: :user
   has_many :completed_users,
-           through: :completed_learnings,
-           source:  :user
+    through: :completed_learnings,
+    source: :user
   belongs_to :category
   acts_as_list scope: :category
 
@@ -22,7 +22,7 @@ class Practice < ActiveRecord::Base
 
   def status(user)
     learnings = Learning.where(
-      user_id:     user.id,
+      user_id: user.id,
       practice_id: id
     )
     if learnings.blank?
@@ -36,15 +36,15 @@ class Practice < ActiveRecord::Base
     status_cds = Learning.statuses.values_at("complete")
 
     !Learning.exists?(
-      user:        user,
+      user: user,
       practice_id: id,
-      status_cd:   status_cds
+      status_cd: status_cds
     )
   end
 
   def exists_learning?(user)
     Learning.exists?(
-      user:        user,
+      user: user,
       practice_id: id
     )
   end
