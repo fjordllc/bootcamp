dfc8be9c6326838a69065013297802f6e6768d1a
Masaki Komagata <komagata@gmail.com>
2017-11-09 17:46:05 +0900

    typeを修正

--- app/javascript/packs/marked.js
+++ app/javascript/packs/marked.js
@@ -1,7 +1,7 @@
 import marked from "marked"
 
 document.addEventListener('DOMContentLoaded', () => {
-  Array.from(document.querySelectorAll('.js-marked'), (r) => {
+  Array.from(document.querySelectorAll('.js-marked'), (e) => {
     e.innerHTML = marked(e.innerHTML, { breaks: true });
   });
 });
