62c12b3198b51565afff489ac268a06f01931c1f
Masaki Komagata <komagata@gmail.com>
2017-11-14 14:58:51 +0900

    Array.fromは一部のブラウザでしか動かないのでreplace

--- app/javascript/packs/marked.js
+++ app/javascript/packs/marked.js
@@ -1,7 +1,7 @@
 import marked from "marked"
 
 document.addEventListener('DOMContentLoaded', () => {
-  Array.from(document.querySelectorAll('.js-marked'), (e) => {
+  [].forEach.call(document.querySelectorAll('.js-marked'), (e) => {
     e.innerHTML = marked(e.innerHTML, { breaks: true });
   });
 });
