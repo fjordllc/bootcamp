00677895f2e19bb637baf7cf27447b0b6a550648
Masaki Komagata <komagata@gmail.com>
2017-11-15 19:33:31 +0900

    Markdown表示前には表示を隠しておくようにした

--- app/javascript/packs/marked.js
+++ app/javascript/packs/marked.js
@@ -2,6 +2,7 @@ import marked from "marked"
 
 document.addEventListener('DOMContentLoaded', () => {
   [].forEach.call(document.querySelectorAll('.js-marked'), (e) => {
+    e.style.display = 'block';
     e.innerHTML = marked(e.innerHTML, { breaks: true });
   });
 });
