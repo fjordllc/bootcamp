33dbd27c15abcce6caf6f36f1b2d8951f1782fdc
Masaki Komagata <komagata@gmail.com>
2017-11-27 14:18:49 +0900

    markedをmarkdown-itに置き換え
    markedでは絵文字が使えず、PRも何年もマージされてないため。

---
+++ app/javascript/packs/markdown-it.js
@@ -0,0 +1,15 @@
+import MarkdownIt from 'markdown-it'
+
+document.addEventListener('DOMContentLoaded', () => {
+  let md = new MarkdownIt({
+    html: true,
+    breaks: true,
+    langPrefix: true,
+    linkify: true
+  });
+
+  [].forEach.call(document.querySelectorAll('.js-markdown'), (e) => {
+    e.style.display = 'block';
+    e.innerHTML =  md.render(e.innerHTML);
+  });
+});
