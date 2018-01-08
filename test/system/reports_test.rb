0396389a6da82145b472f4cebd9e2a5b9bac955c
Masaki Komagata <komagata@gmail.com>
2018-01-09 04:55:41 +0900

    動かないテストを修正

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -5,29 +5,20 @@ class ReportsTest < ApplicationSystemTestCase
     login_user "komagata", "testtest"
   end
 
+  def practices
+    visit "/practices"
+    all(".category-practices-item__value.is-title").map { |e| e.text }
+  end
+
   test "equal practices order in practices and new report" do
-    click_link "プラクティス"
-    practices = page.all(".category-practices-item__value.is-title")
-
-    click_link "日報"
-    click_link "日報の新規作成"
-    report_practices = page.all(".select-practices__label-title")
-
-    report_practices.each_with_index do |practice, i|
-      assert_equal practice.text, practices[i].find(".category-practices-item__title-link").text
-    end
+    visit "/reports/new"
+    report_practices = page.all(".select-practices__label-title").map { |e| e.text }
+    assert_equal practices, report_practices
   end
 
   test "equal practices order in practices and edit report" do
-    click_link "プラクティス"
-    practices = page.all(".category-practices-item__value.is-title")
-
-    find(".global-nav-current-user__link").click
-    page.all(".user-reports-item-actions__item-link").first.click
-    report_practices = page.all(".select-practices__label-title")
-
-    report_practices.each_with_index do |practice, i|
-      assert_equal practice.text, practices[i].find(".category-practices-item__title-link").text
-    end
+    visit "/reports/#{reports(:report_1).id}/edit"
+    report_practices = page.all(".select-practices__label-title").map { |e| e.text }
+    assert_equal practices, report_practices
   end
 end
