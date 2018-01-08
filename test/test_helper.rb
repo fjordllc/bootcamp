2ba96e92ffcead8d474ad51b0df6979ecea614d5
Masaki Komagata <komagata@gmail.com>
2018-01-09 04:02:42 +0900

    integration testをsystem testにリプレイス

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,16 +2,12 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "capybara/rails"
-require "helpers/login_helper"
 
-ActionMailer::Base.delivery_method = :test
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  class ActionDispatch::IntegrationTest
-    include Capybara::DSL
-    include LoginHelper
-  end
 end
+
+ActionMailer::Base.delivery_method = :test
