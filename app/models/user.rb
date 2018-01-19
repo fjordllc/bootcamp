c3cb1615a6b396155b13d9d18fa5d018ddfef465
Masaki Komagata <komagata@gmail.com>
2018-01-19 18:03:21 +0900

    SSLにして、ログイン時のページでのみカメラを起動するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
   validates :nda, presence: true
   validates :password, length: { minimum: 4 }, confirmation: true, if: :password_required?
 
-  has_attached_file :face, styles: { small: "32x32>", normal: "64x64#" }
+  has_attached_file :face, styles: { small: "32x32>", normal: "72x72#" }
   validates_attachment_content_type :face, content_type: /\Aimage\/.*\z/
 
   scope :in_school, -> { where(graduation: false) }
