d5ec92a62ab628be01bb7aea054ce84676c9bb10
Masaki Komagata <komagata@gmail.com>
2018-01-23 10:56:01 +0900

    無駄なことしてた部分を修正

--- app/controllers/api/face_controller.rb
+++ app/controllers/api/face_controller.rb
@@ -1,8 +1,6 @@
 class API::FaceController < API::BaseController
   def update
-    current_user.update(face: params[:face])
-
-    if current_user.save
+    if current_user.update(face: params[:face])
       render :show, status: :ok
     else
       render json: current_user.errors, status: :unprocessable_entity
