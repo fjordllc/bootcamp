e2ecb67955c6c07d7a2237126bf9887e21545405
Masaki Komagata <komagata@gmail.com>
2018-03-12 20:37:36 +0900

    学習時間をリファクタリング

--- app/models/learning_time.rb
+++ app/models/learning_time.rb
@@ -2,4 +2,8 @@ class LearningTime < ApplicationRecord
   belongs_to :report
   validates :started_at, presence: true
   validates :finished_at, presence: true
+
+  def diff
+    finished_at - started_at
+  end
 end
