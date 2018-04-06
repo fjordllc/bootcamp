ecd1cb1422ea350093dbf0188f14f43ce471e136
Masaki Komagata <komagata@gmail.com>
2018-04-06 16:46:42 +0900

    学習時間のフォーマットを変更
    1.5時間の表記から1時間30分の表記に変更

---
+++ app/decorators/report_decorator.rb
@@ -0,0 +1,17 @@
+module ReportDecorator
+  def total_learning_time
+    total_time = learning_times.inject(0) do |sum, learning_time|
+      sum + learning_time.diff
+    end
+
+    total_minute = total_time / 60
+    hour = (total_minute / 60).round
+    minute = (total_minute % 60).round
+
+    if minute == 0
+      "#{hour}時間"
+    else
+      "#{hour}時間#{minute}分"
+    end
+  end
+end
