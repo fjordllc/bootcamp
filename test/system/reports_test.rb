e97458f9d3b782b0a95f81b0820a675e34fbfb7c
nishide <nishide.h@gmail.com>
2018-05-16 15:54:45 +0900

    add "issue#360 再現テストコード"

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -21,4 +21,104 @@ class ReportsTest < ApplicationSystemTestCase
     report_practices = page.all(".select-practices__label-title").map { |e| e.text }
     assert_equal practices, report_practices
   end
+ 
+  test "issue #360 duplicate" do
+    visit "/reports/new"
+    fill_in "report_title", with: "テスト日報"
+    fill_in "report_description", with: "不具合再現の結合テストコード"
+
+    selects = all("select")
+    select "07", from: selects[0]["id"]
+    select "50", from: selects[1]["id"]
+    select "08", from: selects[2]["id"]
+    select "50", from: selects[3]["id"]
+
+    click_link "学習時間追加"
+    selects = all("select")
+    select "08", from: selects[4]["id"]
+    select "50", from: selects[5]["id"]
+    select "09", from: selects[6]["id"]
+    select "50", from: selects[7]["id"]
+
+    click_link "学習時間追加"
+    selects = all("select")
+    select "19", from: selects[8]["id"]
+    select "30", from: selects[9]["id"]
+    select "20", from: selects[10]["id"]
+    select "10", from: selects[11]["id"]
+
+    click_button "登録する"
+
+    assert_text "2時間40分"
+    assert_text "07:50 〜 08:50"
+    assert_text "08:50 〜 09:50"
+    assert_text "19:30 〜 20:10"
+  end
+
+  test "register learning_times 2h" do
+    visit "/reports/new"
+    fill_in "report_title", with: "テスト日報 成功"
+    fill_in "report_description", with: "不具合再現の結合テストコード"
+
+    selects = all("select")
+    select "07", from: selects[0]["id"]
+    select "50", from: selects[1]["id"]
+    select "08", from: selects[2]["id"]
+    select "50", from: selects[3]["id"]
+
+    click_link "学習時間追加"
+    selects = all("select")
+    select "08", from: selects[4]["id"]
+    select "50", from: selects[5]["id"]
+    select "09", from: selects[6]["id"]
+    select "50", from: selects[7]["id"]
+
+    click_button "登録する"
+
+    assert_text "2時間\n"
+    assert_text "07:50 〜 08:50"
+    assert_text "08:50 〜 09:50"
+  end
+  
+  test "register learning_times 1h40m" do
+    visit "/reports/new"
+    fill_in "report_title", with: "テスト日報 成功"
+    fill_in "report_description", with: "不具合再現の結合テストコード"
+
+    selects = all("select")
+    select "07", from: selects[0]["id"]
+    select "50", from: selects[1]["id"]
+    select "08", from: selects[2]["id"]
+    select "50", from: selects[3]["id"]
+
+    click_link "学習時間追加"
+    selects = all("select")
+    select "19", from: selects[4]["id"]
+    select "30", from: selects[5]["id"]
+    select "20", from: selects[6]["id"]
+    select "10", from: selects[7]["id"]
+
+    click_button "登録する"
+
+    assert_text "1時間40分\n"
+    assert_text "07:50 〜 08:50"
+    assert_text "19:30 〜 20:10"
+  end
+
+  test "register learning_time 40m" do
+    visit "/reports/new"
+    fill_in "report_title", with: "テスト日報 成功"
+    fill_in "report_description", with: "不具合再現の結合テストコード"
+
+    selects = all("select")
+    select "19", from: selects[0]["id"]
+    select "30", from: selects[1]["id"]
+    select "20", from: selects[2]["id"]
+    select "10", from: selects[3]["id"]
+
+    click_button "登録する"
+
+    assert_text "40分\n"
+    assert_text "19:30 〜 20:10"
+  end
 end
