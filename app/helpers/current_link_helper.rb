baf69315fa8f7897e6c04c37d9aaccacdb9db934
chamao <44nocycle@gmail.com>
2018-05-29 12:16:58 +0900

    controller名でglobal_navのactiveを判定するように修正

--- app/helpers/current_link_helper.rb
+++ app/helpers/current_link_helper.rb
@@ -2,7 +2,9 @@ module CurrentLinkHelper
   def current_link(path)
     if !path.include?("admin") && request.fullpath.include?("admin")
       false
-    elsif request.fullpath.include?(path)
+    elsif controller_name.include?(path)
+      "is-active"
+    elsif path.include?("admin") && request.fullpath.include?(path)
       "is-active"
     end
   end
