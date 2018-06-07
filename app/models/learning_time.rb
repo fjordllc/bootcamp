3115a8a56c918b73702cf9d45ecde5deb795e16b
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-06-07 15:25:00 +0900

    issue #380 への対応
    - 既存レコードで日付をまたいだ際に学習時間がおかしかった箇所を修正

--- app/models/learning_time.rb
+++ app/models/learning_time.rb
@@ -2,31 +2,9 @@ class LearningTime < ApplicationRecord
   belongs_to :report
   validates :started_at, presence: true
   validates :finished_at, presence: true
-  before_save :fix_finished_date
 
   def diff
-    finished_at - started_at
-  end
-
-  private
-
-  def fix_finished_date
-    if finished_at_is_next_day_but_current_start_and_current_finish_is_same_day?
-      self.finished_at += 1.day
-    elsif finished_at_is_same_day_but_current_finish_is_next_day?
-      self.finished_at -= 1.day
-    end
-  end
-
-  def finished_at_is_next_day_but_current_start_and_current_finish_is_same_day?
-    finished_at.hour < started_at.hour && to_date(finished_at) <= to_date(started_at)
-  end
-
-  def finished_at_is_same_day_but_current_finish_is_next_day?
-    finished_at.hour >= started_at.hour && to_date(finished_at) > to_date(started_at)
-  end
-
-  def to_date(target)
-    Date.new(target.year, target.month, target.day)
+    default = finished_at - started_at
+    (default >= 0) ? default : (finished_at + 1.day) - started_at
   end
 end
