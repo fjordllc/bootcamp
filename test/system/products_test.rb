a9f1ee794a574c74d10941035d5eb6dccb84a008
Masaki Komagata <komagata@gmail.com>
2018-06-23 18:03:01 +0900

    提出物ページがsyntax errorだったのを修正

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,9 +1,10 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit products_url
-  #
-  #   assert_selector "h1", text: "Product"
-  # end
+  setup { login_user "komagata", "testtest" }
+
+  test "show a product page" do
+    visit "/practices/#{practices(:practice_1).id}/products/#{products(:product_1).id}"
+    assert_equal "提出物 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+  end
 end
