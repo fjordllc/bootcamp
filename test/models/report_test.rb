37edd6c768c286b38d6674be825dbf1ff90148e5
keiko <keiko.cda@gmail.com>
2018-07-17 10:50:32 +0900

    reportモデルのメソッドとフィクスチャを変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,13 +3,13 @@ require "test_helper"
 class ReportTest < ActiveSupport::TestCase
   #前の日報の取得
   test "Should get previous report" do
-    report = reports(:report_2)
+    report = reports(:report_3)
     assert report.previous, reports(:report_1)
   end
   
   #次の日報の取得
   test "Should get next report" do
-    report = reports(:report_2)
-    assert report.next, reports(:report_3)
+    report = reports(:report_3)
+    assert report.next, reports(:report_2)
   end
 end
