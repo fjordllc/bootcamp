6ae2c097e5da79551cf4c65261c311769349dd0e
keiko <keiko.cda@gmail.com>
2018-07-19 10:35:12 +0900

    テストとフィクスチャを修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,13 +1,11 @@
 require "test_helper"
 
 class ReportTest < ActiveSupport::TestCase
-  #前の日報の取得
   test "Should get previous report" do
     report = reports(:report_3)
     assert report.previous, reports(:report_1)
   end
   
-  #次の日報の取得
   test "Should get next report" do
     report = reports(:report_3)
     assert report.next, reports(:report_2)
