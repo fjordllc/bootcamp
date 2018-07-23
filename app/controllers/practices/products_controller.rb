d428e6a979e2c899b02455763a0785a23e8fa7fe
chamao <44nocycle@gmail.com>
2018-07-23 17:41:59 +0900

    paramsの引数を文字列からシンボルに変更

--- app/controllers/practices/products_controller.rb
+++ app/controllers/practices/products_controller.rb
@@ -1,5 +1,5 @@
 class Practices::ProductsController < ApplicationController
   def show
-    @practice = Practice.find(params["practice_id"])
+    @practice = Practice.find(params[:practice_id])
   end
 end
