8aae2a5ab5472f3b1e4cef2fa90dee31639a0aaf
keiko <keiko.cda@gmail.com>
2018-08-01 11:50:14 +0900

    products.ymlを変更

---
+++ test/system/products_list_test.rb
@@ -0,0 +1,13 @@
+require "application_system_test_case"
+
+class ProductsListTest < ApplicationSystemTestCase
+
+  test "Show products list when user's product is checked" do
+  end
+
+  test "Don't show products list while user'product doesn't get checked yet" do
+  end
+
+  test "Don't show products list when user don't upload product" do
+  end
+end
