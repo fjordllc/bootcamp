d2a48ec41e5ddcd7d9db21fb7a3bf7f517caa700
keiko <keiko.cda@gmail.com>
2018-08-01 15:36:07 +0900

    提出物一覧に関するテストを追加

--- test/system/products_list_test.rb
+++ test/system/products_list_test.rb
@@ -1,13 +1,30 @@
 require "application_system_test_case"
 
 class ProductsListTest < ApplicationSystemTestCase
-
-  test "Show products list when user's product is checked" do
+  setup { login_user "tanaka", "testtest"}
+  
+  test "show products list when user's product is checked" do
+    visit "/practices/#{practices(:practice_2).id}/products"
+    click_link "提出物"
+    assert_text "tanaka"
+    assert_text "yamada"
+    assert_text "kimura"
   end
 
   test "Don't show products list while user'product doesn't get checked yet" do
+    visit "/practices/#{practices(:practice_3).id}/products"
+    click_link "提出物"
+    assert_text "tanaka"
+    assert_no_text "yamada"
+    assert_no_text "kimura"
+
   end
 
   test "Don't show products list when user don't upload product" do
+    visit "/practices/#{practices(:practice_4).id}/products"
+    click_link "提出物"
+    assert_no_text "tanaka"
+    assert_no_text "yamada"
+    assert_no_text "kimura"
   end
 end
