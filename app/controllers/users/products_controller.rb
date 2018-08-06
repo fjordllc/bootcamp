4d9ee8b47afb3530be5896a1c6fb022c0a4011e1
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-08-07 01:39:04 +0900

    ユーザー詳細ページの提出物部分を分離

---
+++ app/controllers/users/products_controller.rb
@@ -0,0 +1,16 @@
+class Users::ProductsController < ApplicationController
+  before_action :set_user
+  before_action :set_products
+
+  def index
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def set_products
+      @products = @user.products.eager_load(:user, :comments).order(updated_at: :desc)
+    end
+end
