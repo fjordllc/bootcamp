cae301401546d88ab37ede388be27465f16b65af
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-08-07 01:39:04 +0900

    ユーザー詳細ページの提出物部分を分離

--- app/controllers/practices/products_controller.rb
+++ app/controllers/practices/products_controller.rb
@@ -1,5 +1,6 @@
 class Practices::ProductsController < ApplicationController
   def index
     @practice = Practice.find(params[:practice_id])
+    @products = @practice.products.eager_load(:user, :comments).order(updated_at: :desc)
   end
 end
