585d8d745699a3a74a5ee3966f4016e3a9977ef3
keiko <keiko.cda@gmail.com>
2018-08-12 14:17:06 +0900

    アドバイザーが日報・提出物を確認できるかどうかのテストを追加

--- test/system/check_product_test.rb
+++ test/system/check_product_test.rb
@@ -17,4 +17,21 @@ class CheckProductTest < ApplicationSystemTestCase
     visit reports_path
     assert_text "確認済"
   end
+
+  test "Success Adviser's Product Checking" do
+    login_user "mineo", "testtest"
+    click_link "プラクティス"
+    assert_text "OS X Mountain Lionをクリーンインストールする"
+    click_link "OS X Mountain Lionをクリーンインストールする"
+
+    page.all(".page-tabs__item-link")[2].click
+    page.first(".thread-list-item__title-link").click
+
+    assert has_button? "提出物を確認"
+    click_button "提出物を確認"
+    assert_not has_button? "提出物を確認"
+    assert_text "この提出物を確認しました。"
+    visit reports_path
+    assert_text "確認済"
+  end
 end
