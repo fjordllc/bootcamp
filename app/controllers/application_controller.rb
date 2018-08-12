f5afb1bcb42e0ccd6adee645c8223b8f52122da1
keiko <keiko.cda@gmail.com>
2018-08-12 13:58:01 +0900

    アドバイザーログイン時に「確認する」ボタンを表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < ActionController::Base
       end
     end
 
+
     def allow_cross_domain_access
       response.headers["Access-Control-Allow-Origin"] = "*"
       response.headers["Access-Control-Allow-Methods"] = "*"
