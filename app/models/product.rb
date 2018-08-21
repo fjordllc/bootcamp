d16ccd42e7965e7c85e98511a51fa48dff21c7bc
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-08-21 12:30:26 +0900

    ユーザーページのタブ機能で閲覧権限が正しく設定できていなかった箇所を修正
    - users/productsにおけるproducts取得の条件・ロジックを修正
    - product_displayable?の引数・ロジックを修正
    - 対象ユーザーの確認済提出物のid一覧を取得するメソッドを追加
    - has_checked_product?の引数・ロジックを修正
    - 確認済提出物に紐づくプラクティスの一覧を取得するメソッドを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,6 @@ class Product < ApplicationRecord
   after_create ProductCallbacks.new
   validates :user, presence: true, uniqueness: { scope: :practice, message: "既に提出物があります。" }
   validates :body, presence: true
+
+  scope :checked_ids, -> (user) { where(practice: user.practices_with_checked_product).select(&:checked?).map(&:id) }
 end
