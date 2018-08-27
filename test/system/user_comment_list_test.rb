eeaf4abc2d154a19b7c862e08217f953d7786e25
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-08-27 19:27:08 +0900

    コメント一覧のテストを追加
    - ユーザーページのコメント一覧のテストを追加
    - タブ表示機能のテストを修正
    - コメントのfixtureに提出物に対するコメントを追加

---
+++ test/system/user_comment_list_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class UserCommentListTest < ApplicationSystemTestCase
+  test "show all comments for reports of the target user" do
+    login_user "komagata", "testtest"
+    visit polymorphic_path([users(:komagata), :comments])
+    assert_equal users(:komagata).comments.where(commentable_type: "Report").size, 3
+  end
+end
