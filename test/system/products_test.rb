29714103e3d82aeca1ee1a5594032bbbc58eabd0
Masaki Komagata <komagata@gmail.com>
2018-09-11 19:50:49 +0900

    提出物の変更と提出物へのコメントを管理者に通知するようにした

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,10 +1,21 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
-  setup { login_user "komagata", "testtest" }
+  setup { login_user "yamada", "testtest" }
 
   test "show a product page" do
     visit "/practices/#{practices(:practice_1).id}/products/#{products(:product_1).id}"
     assert_equal "提出物 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+  end
+
+  test "create a product" do
+    visit "/practices/#{practices(:practice_5).id}/products/new"
+
+    within("#new_product") do
+      fill_in("product[body]", with: "test")
+    end
+    click_button "提出する"
+
+    assert_text "提出物を作成しました。"
   end
 end
