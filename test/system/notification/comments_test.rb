1770ef3fbe726eca72c4d373aa3677c5cdc6a5cc
Masaki Komagata <komagata@gmail.com>
2018-09-12 15:22:27 +0900

    メンションが二回飛ぶ問題を修正
    一コメントにメンションが二箇所書かれていたら二回通知が飛んでしまう
    問題を修正。

---
+++ test/system/notification/comments_test.rb
@@ -0,0 +1,21 @@
+require "application_system_test_case"
+
+class Notification::CommentsTest < ApplicationSystemTestCase
+  test "recieve only one notificaiton if you send two mentions in one comment" do
+    login_user "komagata", "testtest"
+    visit "/reports/#{reports(:report_1).id}"
+
+    within("#new_comment") do
+      fill_in("comment[description]", with: "@machida @machida test")
+    end
+    click_button "コメントを投稿"
+    assert_text "コメントを投稿しました。"
+
+    logout
+    login_user "machida", "testtest"
+
+    first(".test-bell").click
+    assert_text "komagataさんからメンションがきました。"
+    assert_selector ".header-notification-count", text: "1"
+  end
+end
