3f7b0886359b642875613af64bc8c844d2955023
teppei machida <machida@fjord.jp>
2018-09-21 19:30:15 +0900

    編集ページに右カラムを表示しないようにした

--- app/helpers/body_class_helper.rb
+++ app/helpers/body_class_helper.rb
@@ -3,10 +3,20 @@ module BodyClassHelper
     controller.controller_path.tr("/", "-")
   end
 
+  def page_category
+    if params[:action] == "new" || params[:action] == "create" || params[:action] == "edit"
+      "edit-page"
+    elsif params[:action] == "index"
+      "index-page"
+    elsif params[:action] == "show"
+      "show-page"
+    end
+  end
+
   def body_class(options = {})
     extra_body_classes_symbol = options[:extra_body_classes_symbol] || :extra_body_classes
     qualified_controller_name = controller.controller_path.tr("/", "-")
-    basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
+    basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name} is-#{page_category}"
 
     if content_for?(extra_body_classes_symbol)
       [basic_body_class, content_for(extra_body_classes_symbol)].join(" ")
