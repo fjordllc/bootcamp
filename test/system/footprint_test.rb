5365e326d429f19eb0888ebd953b9b112a71a9f7
keiko <keiko.cda@gmail.com>
2018-09-28 17:10:14 +0900

    テストの書き方を変更

--- test/system/footprint_test.rb
+++ test/system/footprint_test.rb
@@ -4,31 +4,15 @@ require "application_system_test_case"
 
 class FootprintTest < ApplicationSystemTestCase
   test "should be create footprint" do
-    visit "/login"
-    within("#sign-in-form") do
-      fill_in("user[login_name]", with: "tanaka")
-      fill_in("user[password]", with: "testtest")
-    end
-    click_button "サインイン"
-    assert_equal "/users", current_path
-    click_link "日報"
-    assert_text "作業週2日目"
-    click_link "作業週2日目"
+    login_user "tanaka", "testtest"
+    visit "/reports/#{reports(:report_2).id}"
     assert_text "見たよ"
     assert page.has_css?(".footprints-item__checker-icon.is-tanaka")
   end
 
   test "should not footpoint with my report" do
-    visit "/login"
-    within("#sign-in-form") do
-      fill_in("user[login_name]", with: "tanaka")
-      fill_in("user[password]", with: "testtest")
-    end
-    click_button "サインイン"
-    assert_equal "/users", current_path
-    click_link "日報"
-    assert_text "学習週3日目"
-    click_link "学習週3日目"
+    login_user "tanaka", "testtest"
+    visit "/reports/#{reports(:report_7).id}"
     assert_no_text "見たよ"
     assert_not page.has_css?(".footprints-item__checker-icon.is-tanaka")
   end
