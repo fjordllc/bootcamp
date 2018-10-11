0f6119010caccdd9831d67e848b156bb2a9a63cd
nishide <nishide.h@gmail.com>
2018-10-11 17:15:54 +0900

    update refs #495 テストコード修正

--- test/system/notification/answers_test.rb
+++ test/system/notification/answers_test.rb
@@ -7,10 +7,8 @@ class Notification::AnswersTest < ApplicationSystemTestCase
   }
 
   test "recieve a notification when I got my question's answer" do
-    # 質問を作成
     login_user "yamada", "testtest"
     visit "/questions/new"
-
     within("#new_question") do
       fill_in("question[title]", with: question[:title])
       fill_in("question[description]", with: question[:description])
@@ -19,23 +17,19 @@ class Notification::AnswersTest < ApplicationSystemTestCase
     assert_text "質問を作成しました。"
     logout
 
-    # 管理者が回答する
     login_user "komagata", "testtest"
     visit "/questions"
-
     assert_text question[:title]
     click_link question[:title]
-
     within("#new_answer") do
       fill_in("answer[description]", with: "kowabanaではなく自分のGitHubアカウントにリポジトリを作ってそこでやりましょう!")
     end
     click_button "コメントを投稿"
     logout
 
-    # ユーザーがログインしたら通知があること
     login_user "yamada", "testtest"
-
     assert_selector ".header-notification-count", text: "1"
+    first(".test-bell").click
     assert_text "komagataさんから回答がありました。"
   end
 end
