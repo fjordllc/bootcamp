77988961d9c5d47c8d7559b9ed91f133b19dc88c
Masaki Komagata <komagata@gmail.com>
2018-10-12 11:24:12 +0900

    みんなのブログをブログトップへのリンクのみにした
    ユーザーが増えてきたのでまともにフィードを取ると
    フィードリーダーを作るぐらい大変なので。

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,35 +2,6 @@
 
 class FeedsController < ApplicationController
   def index
-    urls = User.student.pluck(:feed_url).reject { |f| f.blank? }
-    hydra = Typhoeus::Hydra.new
-    requests = urls.map do |url|
-      req = Typhoeus::Request.new(url, followlocation: true)
-      hydra.queue(req)
-      req
-    end
-    hydra.run
-
-    @entries = []
-    requests.each do |req|
-      begin
-        feed = Feedjira::Feed.parse(req.response.options[:response_body])
-        if feed.class == Feedjira::Parser::Atom
-          feed.entries.each do |entry|
-            @entries << entry
-          end
-        elsif feed.class == Feedjira::Parser::RSS
-          feed.entries.each do |entry|
-            @entries << entry
-          end
-        else
-          raise "Unknown feed format."
-        end
-      rescue
-        next
-      end
-    end
-    @entries.sort! { |a, b| b.published <=> a.published }
-    @entries
+    @users = User.student.where.not(blog_url: nil)
   end
 end
