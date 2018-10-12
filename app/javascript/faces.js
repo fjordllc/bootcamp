f3366446925bdc06e7feb5d3ef95387aa17b032b
Masaki Komagata <komagata@gmail.com>
2018-10-12 18:41:59 +0900

    eslintを追加

--- app/javascript/faces.js
+++ app/javascript/faces.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Faces from './faces.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const faces = document.getElementById('js-faces');
+  const faces = document.getElementById('js-faces')
   if (faces) {
     new Vue({
       render: h => h(Faces)
-    }).$mount('#js-faces');
+    }).$mount('#js-faces')
   }
-});
+})
--- app/javascript/packs/markdown-it.js
+++ app/javascript/packs/markdown-it.js
@@ -5,15 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
   let md = new MarkdownIt({
     html: true,
     breaks: true,
-    langPrefix: true,
     linkify: true,
     langPrefix: 'language-'
-  });
+  })
 
-  md.use(MarkdownItPlantUML);
-
-  [].forEach.call(document.querySelectorAll('.js-markdown-view'), (e) => {
-    e.style.display = 'block';
-    e.innerHTML =  md.render(e.textContent);
-  });
-});
+  md.use(MarkdownItPlantUML)
+  ;[].forEach.call(document.querySelectorAll('.js-markdown-view'), e => {
+    e.style.display = 'block'
+    e.innerHTML = md.render(e.textContent)
+  })
+})
