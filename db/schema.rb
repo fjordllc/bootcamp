20d1d8daf45c33df46b123f19aa0ce7f7e8ca8fd
keiko <keiko.cda@gmail.com>
2018-10-14 21:20:57 +0900

    お知らせのtitleとdescriptionにnull:falseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_051214) do
+ActiveRecord::Schema.define(version: 2018_10_14_121959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "announcements", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
+    t.string "title", null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
