bdcca7335050068241d09fd51b77943d31771bc7
nishide <nishide.h@gmail.com>
2018-10-15 22:50:35 +0900

    update テスト速度を考慮し、fixturesにあるQuestionを利用したテストに変更

--- test/system/notification/answers_test.rb
+++ test/system/notification/answers_test.rb
@@ -1,35 +1,25 @@
 require "application_system_test_case"
 
 class Notification::AnswersTest < ApplicationSystemTestCase
-  question = {
-    title: "pull requestについて質問です",
-    description: "kowabanaでPRしたら宜しいでしょうか？"
-  }
+  setup do
+    @notice_text = "komagataさんから回答がありました。"
+  end
 
   test "recieve a notification when I got my question's answer" do
-    login_user "yamada", "testtest"
-    visit "/questions/new"
-    within("#new_question") do
-      fill_in("question[title]", with: question[:title])
-      fill_in("question[description]", with: question[:description])
-    end
-    click_button "登録する"
-    assert_text "質問を作成しました。"
-    logout
-
     login_user "komagata", "testtest"
-    visit "/questions"
-    assert_text question[:title]
-    click_link question[:title]
+    visit "/questions/#{questions(:question_2).id}"
     within("#new_answer") do
-      fill_in("answer[description]", with: "kowabanaではなく自分のGitHubアカウントにリポジトリを作ってそこでやりましょう!")
+      fill_in("answer[description]", with: "reduceも使ってみては？")
     end
     click_button "コメントを投稿"
     logout
 
-    login_user "yamada", "testtest"
-    assert_selector ".header-notification-count", text: "1"
+    login_user "tanaka", "testtest"
     first(".test-bell").click
-    assert_text "komagataさんから回答がありました。"
+    assert_text @notice_text
+    logout
+
+    login_user "komagata", "testtest"
+    refute_text @notice_text
   end
 end
