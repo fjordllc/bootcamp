7e867a1cceba7d4615dd8d5928390f9ff9a47c13
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-10-16 12:10:07 +0900

    質問とプラクティスにアソシエーションを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,7 @@ class Question < ActiveRecord::Base
   belongs_to :user, touch: true
   has_many :answers
   has_one :correct_answer
+  belongs_to :practice, optional: true
 
   validates :title, presence: true, length: { maximum: 256 }
   validates :description, presence: true
