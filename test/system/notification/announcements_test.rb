2c9de3255650c3ac8f30e6df83faea8dbcf12861
nishide <nishide.h@gmail.com>
2018-10-24 17:34:52 +0900

    add refs #484 お知らせ通知機能のテストコード

---
+++ test/system/notification/announcements_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class Notification::AnnouncementsTest < ApplicationSystemTestCase
+  setup do
+    @notice_text = "komagataさんからの お知らせ です！！"
+    @notified_count = Notification.where(kind: 5).size
+    @reciever_count = User.where(retire: false).size - 1 # 送信者は除くため-1
+  end
+
+  test "all menber recieve a notification when announcement posted" do
+    login_user "komagata", "testtest"
+    count = "nav.pagination"
+    visit "/announcements"
+    click_link "お知らせの新規作成"
+
+    find("input[name='announcement[title]']").set("お知らせです")
+    find("textarea[name='announcement[description]']").set("お知らせ内容です")
+    click_button "作成"
+    logout
+
+    login_user "tanaka", "testtest"
+    first(".test-bell").click
+    assert_text @notice_text
+    logout
+
+    login_user "komagata", "testtest"
+    refute_text @notice_text
+
+    assert_equal(@notified_count + @reciever_count, Notification.where(kind: 5).size)
+  end
+end
