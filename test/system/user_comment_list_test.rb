3bb37b457bd2217b59196ddadbc9055cc776b0aa
tosite0345 <tl091264@gmail.com>
2018-10-24 17:31:09 +0900

    ページネーション機能
    コントローラ側修正
    ビュー修正
    テスト修正
    テストデータ追加
    テストデータに伴うテスト修正

--- test/system/user_comment_list_test.rb
+++ test/system/user_comment_list_test.rb
@@ -6,6 +6,6 @@ class UserCommentListTest < ApplicationSystemTestCase
   test "show all comments for reports of the target user" do
     login_user "komagata", "testtest"
     visit polymorphic_path([users(:komagata), :comments])
-    assert_equal 3, users(:komagata).comments.where(commentable_type: "Report").size
+    assert_equal 103, users(:komagata).comments.where(commentable_type: "Report").size
   end
 end
