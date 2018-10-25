befeef53ed49861c98d69b295880a7e6dbf0d232
tosite0345 <tl091264@gmail.com>
2018-10-25 11:45:13 +0900

    テスト修正

--- test/system/paginate_test.rb
+++ test/system/paginate_test.rb
@@ -5,6 +5,12 @@ require "application_system_test_case"
 class PaginateTest < ApplicationSystemTestCase
   setup { login_user "komagata", "testtest" }
 
+  test "Show pagination in /users/:id/comments" do
+    visit user_comments_path([users(:komagata)])
+    assert_selector "span.thread-comments-container__title-count", text: "（1 〜 25 件を表示）"
+    assert_selector "nav.pagination", count: 1
+  end
+
   test "GET /users/:id/comments" do
     visit user_comments_path([users(:komagata)])
     assert_equal 25, users(:komagata).comments.page(1).size
