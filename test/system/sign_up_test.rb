46f66a4f39e990272936e3618a3e1b88a7ff74ba
tosite0345 <tl091264@gmail.com>
2018-10-29 15:37:15 +0900

    マイグレーション実装他
    View側不要部分削除
    View側一部文言修正
    Controller側params修正
    テスト実装
    ついでにusers_controllerのカバレッジ上げた

--- test/system/sign_up_test.rb
+++ test/system/sign_up_test.rb
@@ -4,21 +4,36 @@ require "application_system_test_case"
 
 class SignUpTest < ApplicationSystemTestCase
   test "sign up" do
-    visit "/users/new"
+    visit new_user_path
     within "#new_user" do
+      filler
+    end
+
+    click_on "参加する"
+    assert_text "サインアップしました。"
+  end
+
+  test "faild to sign up" do
+    visit new_user_path
+    within "#new_user" do
+      filler
+      fill_in "user[login_name]", with: "komagata"
+    end
+
+    click_on "参加する"
+    assert_text "ユーザー名 はすでに存在します。"
+  end
+
+  private
+    def filler
       fill_in "user[login_name]", with: "testuser"
       fill_in "user[password]", with: "testtest"
       fill_in "user[password_confirmation]", with: "testtest"
       fill_in "user[email]", with: "testuser@example.com"
       fill_in "user[first_name]", with: "Jean"
       fill_in "user[last_name]", with: "Valjean"
+      first(".js-nda-action").click
+      execute_script "document.querySelector('.nda__action').click();"
+      execute_script "document.querySelector('.nda__close').click();"
     end
-
-    first(".js-nda-action").click
-    execute_script "document.querySelector('.nda__action').click();"
-    execute_script "document.querySelector('.nda__close').click();"
-
-    click_button "参加する"
-    assert_text "サインアップしました。"
-  end
 end
