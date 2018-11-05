2c9aa20c238ecbe1da515664ee7de4c19d3438c2
Masaki Komagata <komagata@gmail.com>
2018-11-05 17:00:00 +0900

    成果物に足跡機能を追加
    足跡系のテストを整理

--- test/system/footprint/announcements_test.rb
+++ test/system/footprint/announcements_test.rb
@@ -2,23 +2,7 @@
 
 require "application_system_test_case"
 
-class FootprintTest < ApplicationSystemTestCase
-  test "should be create footprint in /reports/:id" do
-    login_user "tanaka", "testtest"
-    report = users(:komagata).reports.first
-    visit report_path(report)
-    assert_text "見たよ"
-    assert page.has_css?(".footprints-item__checker-icon.is-tanaka")
-  end
-
-  test "should not footpoint with my own report" do
-    login_user "tanaka", "testtest"
-    report = users(:tanaka).reports.first
-    visit report_path(report)
-    assert_no_text "見たよ"
-    assert_not page.has_css?(".footprints-item__checker-icon.is-tanaka")
-  end
-
+class Footprint::AnnouncementsTest < ApplicationSystemTestCase
   test "should be create footprint in /announcements/:id" do
     login_user "tanaka", "testtest"
     announce = users(:komagata).announcements.first
