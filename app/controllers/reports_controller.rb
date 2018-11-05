0088d49d1704149c3ec5d41bdf4d263e799f2290
tosite0345 <tl091264@gmail.com>
2018-11-05 18:23:12 +0900

    日報をコピーした際に今日日付が入るようにした
    コントローラー修正
    ビュー修正
    テスト追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -39,11 +39,11 @@ class ReportsController < ApplicationController
     @report.learning_times.build()
 
     if params[:id]
-      report = current_user.reports.find(params[:id])
-      @report.title = report.title
-      @report.reported_at = report.reported_at
+      report              = current_user.reports.find(params[:id])
+      @report.title       = report.title
+      @report.reported_at = Date.current
       @report.description = report.description
-      @report.practices = report.practices
+      @report.practices   = report.practices
     end
   end
 
