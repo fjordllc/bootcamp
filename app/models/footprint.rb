fc4f523f039f8bfdb6e50186fb147d2140a47866
Masaki Komagata <komagata@gmail.com>
2018-11-05 16:11:20 +0900

    足跡をつけるだけでは学習してないことにした

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Footprint < ApplicationRecord
-  belongs_to :user, touch: true
+  belongs_to :user
   belongs_to :footprintable, polymorphic: true
   validates :user_id, presence: true
 end
