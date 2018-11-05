85b297aef54be984a98011cd973ca22dd575d213
tosite0345 <tl091264@gmail.com>
2018-11-05 15:00:48 +0900

    お知らせに足あと機能を実装
    [footprint]--belongs_to-->[footprintable]のpolymorphicを定義
    Viewファイルを修正
    テストファイルを修正
    report・announcementにFootprintを紐づけ

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Footprint < ApplicationRecord
-  belongs_to :user
-  belongs_to :report
+  belongs_to :user, touch: true
+  belongs_to :footprintable, polymorphic: true
   validates :user_id, presence: true
-  validates :report_id, presence: true
 end
