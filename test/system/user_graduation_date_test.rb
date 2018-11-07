8695f69db5d38c36fcd245f6066d321b386f472a
ycoda <tankaku@gmail.com>
2018-11-07 18:36:00 +0900

    画面構成の変更に伴い卒業日を記録するテストを修正

--- test/system/user_graduation_date_test.rb
+++ test/system/user_graduation_date_test.rb
@@ -6,7 +6,7 @@ class UserGraduationDate < ApplicationSystemTestCase
   test "graduation date is displayed" do
     login_user "komagata", "testtest"
 
-    all("a")[9].click
+    click_link "管理"
     click_link "ユーザー一覧"
     click_link "yamada（Yamada Taro）"
     switch_to_window(windows.last)
@@ -14,6 +14,7 @@ class UserGraduationDate < ApplicationSystemTestCase
     click_link "管理者として情報変更"
     find("#user_graduation", visible: false).click
     click_button "更新する"
+    click_link "卒業生"
     click_link "yamada（Yamada Taro）"
     switch_to_window(windows.last)
     assert_text "卒業日時"
