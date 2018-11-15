dfc64d9845049904a23db246879adbe95280db38
Masaki Komagata <komagata@gmail.com>
2018-11-15 16:08:50 +0900

    ダッシュボードのテスト追加

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -3,8 +3,14 @@
 require "application_system_test_case"
 
 class HomeTest < ApplicationSystemTestCase
-  test "GET /" do
-    visit "/welcome"
+  test "GET / without sign in" do
+    visit "/"
     assert_equal "FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+  end
+
+  test "GET /" do
+    login_user "komagata", "testtest"
+    visit "/"
+    assert_equal "ダッシュボード | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
   end
 end
