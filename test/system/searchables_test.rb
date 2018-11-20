742aa0cd7c0e3155e0bb7f852c10bf20955125ae
nishide <nishide.h@gmail.com>
2018-11-20 18:45:40 +0900

    update refs #569 extend search range テストコード

--- test/system/searchables_test.rb
+++ test/system/searchables_test.rb
@@ -5,11 +5,35 @@ require "application_system_test_case"
 class SearchablesTest < ApplicationSystemTestCase
   setup { login_user "hatsuno", "testtest" }
 
-  test "search with 'Bootcamp'" do
+  test "search Report" do
     within("form[name=search]") do
-      fill_in "word", with: "Bootcamp"
+      fill_in "word", with: "作業"
     end
     find("#test-search").click
-    assert_equal "'Bootcamp'の検索結果 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+    assert_text "作業週1日目"
+  end
+
+  test "search Announcement" do
+    within("form[name=search]") do
+      fill_in "word", with: "お知らせ"
+    end
+    find("#test-search").click
+    assert_text "お知らせ1"
+  end
+
+  test "search Practice" do
+    within("form[name=search]") do
+      fill_in "word", with: "Mountain"
+    end
+    find("#test-search").click
+    assert_text "OS X Mountain Lionをクリーンインストールする"
+  end
+
+  test "search Question " do
+    within("form[name=search]") do
+      fill_in "word", with: "エディター"
+    end
+    find("#test-search").click
+    assert_text "どのエディターを使うのが良いでしょうか"
   end
 end
