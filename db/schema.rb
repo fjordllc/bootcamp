e49cbe33be8fa5efc800b6ed42ae2ee55877b316
Masaki Komagata <komagata@gmail.com>
2018-11-20 14:31:42 +0900

    graduated_atをgraduated_onに変更
    datetimeからdateに変更があったので

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_091128) do
+ActiveRecord::Schema.define(version: 2018_11_20_044954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -251,7 +251,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_091128) do
     t.datetime "face_updated_at"
     t.bigint "course_id"
     t.index ["course_id"], name: "index_users_on_course_id"
-    t.date "graduated_at"
+    t.date "graduated_on"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
