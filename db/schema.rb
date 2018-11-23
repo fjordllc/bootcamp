728dd7f1963223d197033f52bc0d848e402712d0
Masaki Komagata <komagata@gmail.com>
2018-11-23 19:13:40 +0900

    admin flagを追加
    メールアドレスでの判別からflagに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_110337) do
+ActiveRecord::Schema.define(version: 2018_11_23_101305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -249,6 +249,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_110337) do
     t.date "graduated_on"
     t.bigint "course_id"
     t.date "retired_on"
+    t.boolean "admin", default: false, null: false
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
