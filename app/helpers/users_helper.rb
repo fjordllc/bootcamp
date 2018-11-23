7535a65d380a29fcf7be5e9e188300350a59df99
Masaki Komagata <komagata@gmail.com>
2018-11-23 20:26:02 +0900

    find_job_assistを削除
    希望者のみが対象になったので。

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def user_find_job_assist(user)
-    user.find_job_assist ? "フィヨルドの就職支援を希望する" : "フィヨルドの就職支援を希望しない"
-  end
-
   def user_tab_attrs(name)
     target = params.fetch("target", "all")
     if target == name
