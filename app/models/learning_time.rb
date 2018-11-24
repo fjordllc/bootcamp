9210def8ff98301474c4bdc31e4bd40d986ac0bc
shikibum <satoshikibu@gmail.com>
2018-11-24 23:59:39 +0900

    valideteを追加

--- app/models/learning_time.rb
+++ app/models/learning_time.rb
@@ -4,6 +4,13 @@ class LearningTime < ApplicationRecord
   belongs_to :report
   validates :started_at, presence: true
   validates :finished_at, presence: true
+  validate :finished_at_be_greater_than_started_at
+
+  def finished_at_be_greater_than_started_at
+    if diff <= 0
+      errors.add(:finished_at, ": 学習時間0時間は登録できません")
+    end
+  end
 
   def diff
     default = finished_at - started_at
