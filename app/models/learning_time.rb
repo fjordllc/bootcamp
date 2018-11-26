9043d8eab96577c7250c3aac142b5847c80a9507
shikibum <satoshikibu@gmail.com>
2018-11-26 22:26:13 +0900

    return if diff > 0を削除

--- app/models/learning_time.rb
+++ app/models/learning_time.rb
@@ -8,7 +8,6 @@ class LearningTime < ApplicationRecord
 
   def finished_at_be_greater_than_started_at
     return if report.wip? || diff > 0
-    return if diff > 0
     errors.add(:finished_at, ": 学習時間0時間は登録できません")
   end
 
