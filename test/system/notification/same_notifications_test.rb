e147f2d2a09238c61f8d68472ece993a3964c497
ycoda <tankaku@gmail.com>
2018-12-03 15:32:57 +0900

    同じURLの通知をまとめて既読にする処理でupdated_atが更新されていなかったので更新処理を追加

--- test/system/notification/same_notifications_test.rb
+++ test/system/notification/same_notifications_test.rb
@@ -31,12 +31,15 @@ class NotificationsTest < ApplicationSystemTestCase
     assert_text 1, first("li.has-count .header-notification-count").text
 
     # 同じURLの通知があった時、その中の1つが既読になったら同じURLの通知全てを既読にする
+    # NotificationControllerでobjects.update_allを使っているのでupdated_atが更新されている事の確認
+    @before_update_notification = Notification.find_by(id: @user.notifications.first)
     first("li .header-links__link.js-drop-down__trigger.test-bell").click # 通知をクリック
     click_link "komagataさんからコメントが届きました。"
     @notification = Notification.find_by(id: @user.notifications.first)
     @notifications = @user.notifications.where(path: @notification.path)
     @notifications.each do |notification|
       assert_equal true, notification.read
+      assert_equal true, (@before_update_notification.updated_at < notification.updated_at)
     end
 
     # ユーザーからコメントが来た時(admin)
