86f5468d02319c161060c4e7d624ceaf6e63d4b0
Masaki Komagata <komagata@gmail.com>
2018-12-03 11:43:39 +0900

    プラクティスの日報一覧が表示できない問題を修正

--- app/controllers/practices/reports_controller.rb
+++ app/controllers/practices/reports_controller.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
 class Practices::ReportsController < ApplicationController
-  before_action :set_practice
-  before_action :set_reports
-
   def index
+    @practice = Practice.find(params[:practice_id])
+    @reports = @practice.reports.eager_load(:user, :comments).order(updated_at: :desc, id: :desc).page(params[:page])
   end
-
-  private
-    def set_practice
-      @practice = Practice.find(params[:practice_id])
-    end
-
-    def set_reports
-      @reports = practice.reports.eager_load(:user, :comments).order(updated_at: :desc, id: :desc)
-    end
-
-    def practice
-      @practice ||= Practice.find(params[:practice_id])
-    end
 end
