a9bb31281fbd94229b2d230cbd3e23174356566d
shikibum <satoshikibu@gmail.com>
2018-12-05 16:55:42 +0900

    indexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_105150) do
+ActiveRecord::Schema.define(version: 2018_12_05_065904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_105150) do
   create_table "images", force: :cascade do |t|
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.bigint "user_id"
     t.text "image_meta"
@@ -163,6 +163,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_105150) do
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["updated_at"], name: "index_pages_on_updated_at"
   end
 
   create_table "practices", id: :serial, force: :cascade do |t|
@@ -243,7 +244,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_105150) do
     t.boolean "mentor", default: false, null: false
     t.string "face_file_name"
     t.string "face_content_type"
-    t.bigint "face_file_size"
+    t.integer "face_file_size"
     t.datetime "face_updated_at"
     t.date "graduated_on"
     t.bigint "course_id"
