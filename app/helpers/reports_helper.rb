0a314651603e30448b172a073803200710aa015f
shikibum <satoshikibu@gmail.com>
2018-12-09 13:22:15 +0900

    practice_optionsを追記

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -4,4 +4,12 @@ module ReportsHelper
   def recent_reports
     @recent_reports ||= Report.eager_load(:user, :checks).order(updated_at: :desc, id: :desc).limit(15)
   end
+
+  def practice_options(categories)
+    categories.flat_map do |category|
+      category.practices.map do |practice|
+        ["[#{category.name}] #{practice.title}", practice.id]
+      end
+    end
+  end
 end
