7d35960ea2015be59d9bc1d6a86f09ef323912cc
Masaki Komagata <komagata@gmail.com>
2018-12-14 15:32:49 +0900

    テストが失敗するように修正
    CircleCIのテストのスクリーンショットが取れるかどうか確認するため

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -5,7 +5,7 @@ require "application_system_test_case"
 class HomeTest < ApplicationSystemTestCase
   test "GET / without sign in" do
     visit "/"
-    assert_equal "FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+    assert_equal "aFJORD BOOT CAMP（フィヨルドブートキャンプ）", title
   end
 
   test "GET /" do
