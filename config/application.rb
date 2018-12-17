522b9ec810ad3ebbdad907163318c8ddf9c8e88c
teppei machida <machida@fjord.jp>
2018-12-17 14:39:50 +0900

    フォームのエラー表示削除設定の修正

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Bootcamp
     end
 
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
-      %Q(#{html_tag}).html_safe
+      html_tag.html_safe
     end
   end
 end
