815152648c359d6cc3a57cd3c5774817a0219264
Masaki Komagata <komagata@gmail.com>
2018-12-18 14:23:54 +0900

    メールテストの文言修正

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -11,6 +11,6 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ["info@fjord.jp"], email.from
     assert_equal ["komagata@fjord.jp"], email.to
     assert_equal "フィヨルドブートキャンプへようこそ", email.subject
-    assert_match %r{ご応募ありがとうございます}, email.body.to_s
+    assert_match %r{お申し込みありがとうございます}, email.body.to_s
   end
 end
