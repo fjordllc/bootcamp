19bcd9213b12656486c8d4d0a2a011dd1ec983a3
Masaki Komagata <komagata@gmail.com>
2018-12-28 18:12:19 +0900

    convert scriptを削除
    imageについてはURLとしてGCSのURLとして保存しているので必要ないため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_065824) do
+ActiveRecord::Schema.define(version: 2018_12_10_065255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
