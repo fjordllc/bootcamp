d21c685d749682da7cb0301ab9eb0f74a2186e7d
shikibum <satoshikibu@gmail.com>
2018-12-29 17:37:54 +0900

    リファクタリング

--- app/models/question_callbacks.rb
+++ app/models/question_callbacks.rb
@@ -2,16 +2,20 @@
 
 class QuestionCallbacks
   def after_create(question)
-    send_notification(question)
+    send_notification_to_mentors(question)
+    send_notification_to_completed_students(question)
   end
 
   private
-    def send_notification(question)
+    def send_notification_to_mentors(question)
       User.mentor.each do |user|
         if question.sender != user
           Notification.came_question(question, user)
         end
       end
+    end
+
+    def send_notification_to_completed_students(question)
       question.practice.completed_learnings.where.not(user_id: question.sender).eager_load(:user).each do |learning|
         Notification.came_question(question, learning.user)
       end
