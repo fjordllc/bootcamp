a4800fe9868f63169ab23eba285a37754cbdf97c
ycoda <tankaku@gmail.com>
2019-01-08 13:12:06 +0900

    perperclipを削除した為、マイグレーションファイルを修正した

--- db/migrate/20171107061439_add_attachment_image_to_images.rb
+++ db/migrate/20171107061439_add_attachment_image_to_images.rb
@@ -3,7 +3,10 @@
 class AddAttachmentImageToImages < ActiveRecord::Migration[5.1]
   def change
     create_table :images do |t|
-      t.attachment :image
+      t.string :image_file_name
+      t.string :image_content_type
+      t.integer :image_file_size
+      t.datetime :image_updated_at
       t.belongs_to :user, foreign_key: true
       t.text :image_meta
 
