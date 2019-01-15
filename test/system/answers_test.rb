f4114e4d8412ee5c06ab721a5ef59b54f05f9951
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2019-01-15 15:35:01 +0900

    コメントフォーム・解答フォームのタブに関するテストを追加

--- test/system/answers_test.rb
+++ test/system/answers_test.rb
@@ -5,6 +5,21 @@ require "application_system_test_case"
 class AnswersTest < ApplicationSystemTestCase
   setup { login_user "komagata", "testtest" }
 
+  test "answer form in questions/:id has comment tab and preview tab" do
+    visit "/questions/#{questions(:question_2).id}"
+    comment_form_tabs = all(".thread-comment-form__tab")
+    assert_equal "コメント", comment_form_tabs[0].text
+    assert_equal "プレビュー", comment_form_tabs[1].text
+  end
+
+  test "answer form in questions/:question_id/answers/:id/edit has comment tab and preview tab" do
+    answer = answers(:answer_3)
+    visit "/questions/#{answer.question_id}/answers/#{answer.id}/edit"
+    comment_form_tabs = all(".thread-comment-form__tab")
+    assert_equal "コメント", comment_form_tabs[0].text
+    assert_equal "プレビュー", comment_form_tabs[1].text
+  end
+
   test "admin can resolve user's question" do
     visit "/questions/#{questions(:question_2).id}"
     assert_text "解決にする"
