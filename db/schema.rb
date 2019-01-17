3218b27b539acee2838ea5d0969c7d81e7b4732d
Masaki Komagata <komagata@gmail.com>
2019-01-17 14:46:45 +0900

    無料フラグを追加
    有料化に向けて。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_064709) do
+ActiveRecord::Schema.define(version: 2019_01_17_050043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -256,6 +256,7 @@ ActiveRecord::Schema.define(version: 2019_01_04_064709) do
     t.integer "study_place"
     t.integer "experience"
     t.string "how_did_you_know"
+    t.boolean "free", default: false, null: false
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
