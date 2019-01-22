e3526a483e651677cd194d20b00d65a96d3cfb85
ryo_hasebe <wtc.r.h173@gmail.com>
2019-01-22 14:33:37 +0900

    画像をリサイズするようにした

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,5 @@
 
 class Company < ActiveRecord::Base
   validates :name, presence: true
-  validates :logo, blob: { content_type: :image, size_range: 0..10.megabytes }
   has_one_attached :logo
 end
