1fe890d1a76ec74b7ba2e6d1eb3247c767ef9361
ycoda <tankaku@gmail.com>
2019-02-01 21:43:09 +0900

    ユーザー個別ページにロールを表示した

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,4 +4,16 @@ module UserDecorator
   def twitter_url
     "https://twitter.com/#{twitter_account}"
   end
+
+  def role
+    roles = [
+      { role: :admin, value: admin },
+      { role: :mentor, value: mentor },
+      { role: :adviser, value: adviser },
+      { role: :trainee, value: trainee },
+      { role: :graduate, value: graduated_on },
+      { role: :student, value: true }
+    ]
+    roles.detect { |v| v[:value] }[:role]
+  end
 end
