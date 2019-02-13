65dd12d2fd901c1f8e1f6245f8f45f1742f65e96
teppei machida <machida@fjord.jp>
2019-02-13 11:07:03 +0900

    :cop:

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -11,7 +11,7 @@ module CommentsHelper
   end
 
   def comment_summury(comment, word_count)
-    summury = strip_tags(md2html(comment)).gsub(/[\r\n]/,"")
+    summury = strip_tags(md2html(comment)).gsub(/[\r\n]/, "")
     simple_format(truncate(summury, length: word_count))
   end
 end
