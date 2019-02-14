f8057480a4e462f53a2002710adcf64cbf56ec54
Rina Mito <satoshikibu@gmail.com>
2019-02-14 22:06:28 +0900

    FILTERSをDOCUMENT_TYPESに変更

--- test/models/searcher_test.rb
+++ test/models/searcher_test.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class SearchableTest < ActiveSupport::TestCase
-  test "returns all types when filter argument isn't specified" do
+  test "returns all types when document_type argument isn't specified" do
     result = Searcher.search("テスト").map(&:class)
     assert_includes(result, Report)
     assert_includes(result, Page)
@@ -12,8 +12,8 @@ class SearchableTest < ActiveSupport::TestCase
     assert_includes(result, Announcement)
   end
 
-  test "returns all types when filter argument is :all" do
-    result = Searcher.search("テスト", filter: :all).map(&:class)
+  test "returns all types when document_type argument is :all" do
+    result = Searcher.search("テスト", document_type: :all).map(&:class)
     assert_includes(result, Report)
     assert_includes(result, Page)
     assert_includes(result, Practice)
@@ -21,8 +21,8 @@ class SearchableTest < ActiveSupport::TestCase
     assert_includes(result, Announcement)
   end
 
-  test "returns only report type when filter argument is :reports" do
-    result = Searcher.search("テスト", filter: :reports).map(&:class)
+  test "returns only report type when document_type argument is :reports" do
+    result = Searcher.search("テスト", document_type: :reports).map(&:class)
     assert_includes(result, Report)
     assert_not_includes(result, Page)
     assert_not_includes(result, Practice)
@@ -30,8 +30,8 @@ class SearchableTest < ActiveSupport::TestCase
     assert_not_includes(result, Announcement)
   end
 
-  test "returns only page type when filter argument is :pages" do
-    result = Searcher.search("テスト", filter: :pages).map(&:class)
+  test "returns only page type when document_type argument is :pages" do
+    result = Searcher.search("テスト", document_type: :pages).map(&:class)
     assert_not_includes(result, Report)
     assert_includes(result, Page)
     assert_not_includes(result, Practice)
@@ -39,8 +39,8 @@ class SearchableTest < ActiveSupport::TestCase
     assert_not_includes(result, Announcement)
   end
 
-  test "returns only practice type when filter argument is :practices" do
-    result = Searcher.search("テスト", filter: :practices).map(&:class)
+  test "returns only practice type when document_type argument is :practices" do
+    result = Searcher.search("テスト", document_type: :practices).map(&:class)
     assert_not_includes(result, Report)
     assert_not_includes(result, Page)
     assert_includes(result, Practice)
@@ -48,8 +48,8 @@ class SearchableTest < ActiveSupport::TestCase
     assert_not_includes(result, Announcement)
   end
 
-  test "returns only question type when filter argument is :questions" do
-    result = Searcher.search("テスト", filter: :questions).map(&:class)
+  test "returns only question type when document_type argument is :questions" do
+    result = Searcher.search("テスト", document_type: :questions).map(&:class)
     assert_not_includes(result, Report)
     assert_not_includes(result, Page)
     assert_not_includes(result, Practice)
@@ -57,8 +57,8 @@ class SearchableTest < ActiveSupport::TestCase
     assert_not_includes(result, Announcement)
   end
 
-  test "returns only announcement type when filter argument is :announcements" do
-    result = Searcher.search("テスト", filter: :announcements).map(&:class)
+  test "returns only announcement type when document_type argument is :announcements" do
+    result = Searcher.search("テスト", document_type: :announcements).map(&:class)
     assert_not_includes(result, Report)
     assert_not_includes(result, Page)
     assert_not_includes(result, Practice)
