5ca4c12744398dcd43bf00e5d8fdf0ecadf45aee
Masaki Komagata <komagata@gmail.com>
2019-02-15 18:01:57 +0900

    変換済みの画像はキャッシュから表示するようにした
    画像表示が遅かったため

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -55,39 +55,6 @@ class UsersTest < ApplicationSystemTestCase
     assert_no_text "リタイア日"
   end
 
-  test "user list corresponding to selected status is displayed" do
-    login_user "komagata", "testtest"
-
-    assert_text "ユーザー"
-    click_link "ユーザー"
-
-    assert_equal 6, all(".tab-nav__item-link").length
-    assert_text "yamada"
-    assert_text "kimura"
-    assert_text "hatsuno"
-    assert_text "hajime"
-    assert_text "kensyu"
-
-
-    assert_text "卒業生"
-    click_link "卒業生"
-    assert_equal 1, all(".users-item__inner").length
-    assert_text "tanaka"
-
-    assert_text "アドバイザー"
-    click_link "アドバイザー"
-    assert_equal 1, all(".users-item__inner").length
-    assert_text "mineo"
-
-    assert_text "全員"
-    click_link "全員"
-    assert_text "yameo"
-
-    assert_text "研修生"
-    click_link "研修生"
-    assert_text "kensyu"
-  end
-
   test "nomal user can't see unchecked number table" do
     login_user "hatsuno", "testtest"
     visit "/users/#{users(:hatsuno).id}"
