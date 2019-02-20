cff5ee862e247510ad4e42272a2e54cec6947cf0
rono23 <rono23@gmail.com>
2019-02-20 14:42:03 +0900

    rack-mini-profilerの設定を削除
    https://github.com/fjordllc/bootcamp/pull/312 で `rack-mini-profiler` が削除されたので、設定ファイルの削除やREADMEの更新をしました。

--- config/initializers/rack_profiler.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-if Rails.env.development? && ENV.has_key?("RACK_PROFILER")
-  require "rack-mini-profiler"
-  Rack::MiniProfilerRails.initialize!(Rails.application)
-  Rack::MiniProfiler.config.position = "bottom-right"
-end
