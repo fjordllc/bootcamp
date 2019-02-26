ca1edcf7f46707f02700aa2b2b2ed19d7d8cb78e
Masaki Komagata <komagata@gmail.com>
2019-02-26 15:45:08 +0900

    提出物の削除時に関連する通知も削除した

--- app/models/product_callbacks.rb
+++ app/models/product_callbacks.rb
@@ -9,10 +9,18 @@ class ProductCallbacks
     send_notification(product, "#{product.user.login_name}さんが提出物を更新しました。")
   end
 
+  def after_destroy(product)
+    delete_notification(product)
+  end
+
   private
     def send_notification(product, message)
       User.admins.each do |user|
         Notification.submitted(product, user, message)
       end
+    end
+
+    def delete_notification(product)
+      Notification.where(path: "/products/#{product.id}").destroy_all
     end
 end
