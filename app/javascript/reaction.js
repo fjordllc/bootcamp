ed3809774d6cca6feff36050d418b7ccc6dafd32
Masaki Komagata <komagata@gmail.com>
2019-03-04 10:15:59 +0900

    eslintの指摘点修正

--- app/javascript/reaction.js
+++ app/javascript/reaction.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const reactions = document.querySelectorAll('.js-reactions');
+  const reactions = document.querySelectorAll('.js-reactions')
 
-  if (reactions.length === 0)
-    return;
+  if (reactions.length === 0) { return }
 
   const requestReaction = (url, method, callback) => {
     fetch(url, {
@@ -13,75 +12,72 @@ document.addEventListener('DOMContentLoaded', () => {
         'X-CSRF-Token': $.rails.csrfToken()
       }
     }).then(response => {
-      return response.json();
+      return response.json()
     }).then(json => {
-      callback(json);
+      callback(json)
     }).catch(error => {
-      console.warn(error);
-    });
-  };
+      console.warn(error)
+    })
+  }
 
   const updateReaction = (element, count) => {
-    let reactionCount = element.querySelector('.js-reaction-count');
+    let reactionCount = element.querySelector('.js-reaction-count')
 
     if (reactionCount) {
-      reactionCount.textContent = Number(reactionCount.textContent) + count;
+      reactionCount.textContent = Number(reactionCount.textContent) + count
 
-      switch(reactionCount.textContent) {
+      switch (reactionCount.textContent) {
         case '0':
-          element.hidden = true;
-          break;
+          element.hidden = true
+          break
         case '1':
-          element.hidden = false;
-          break;
+          element.hidden = false
+          break
       }
     }
-  };
+  }
 
   const createReaction = (reaction, kind, reactionableId) => {
-    const url = `/api/reactions?reactionable_id=${reactionableId}&kind=${kind}`;
+    const url = `/api/reactions?reactionable_id=${reactionableId}&kind=${kind}`
 
     requestReaction(url, 'POST', (json) => {
       Array.from(reaction.querySelectorAll(`[data-reaction-kind="${kind}"]`), element => {
-        element.classList.add('is-reacted');
-        element.dataset.reactionId = json.id;
-        updateReaction(element, 1);
-      });
-    });
-  };
+        element.classList.add('is-reacted')
+        element.dataset.reactionId = json.id
+        updateReaction(element, 1)
+      })
+    })
+  }
 
   const destroyReaction = (reaction, kind, reactionId) => {
-    const url = `/api/reactions/${reactionId}`;
+    const url = `/api/reactions/${reactionId}`
 
     requestReaction(url, 'DELETE', (json) => {
       Array.from(reaction.querySelectorAll(`[data-reaction-kind="${kind}"]`), element => {
-        element.classList.remove('is-reacted');
-        delete(element.dataset.reactionId);
-        updateReaction(element, -1);
-      });
-    });
-  };
+        element.classList.remove('is-reacted')
+        delete (element.dataset.reactionId)
+        updateReaction(element, -1)
+      })
+    })
+  }
 
   Array.from(reactions, reaction => {
-    const reactionableId = reaction.dataset.reactionReactionableId;
+    const reactionableId = reaction.dataset.reactionReactionableId
 
     Array.from(reaction.querySelectorAll('li'), element => {
       element.addEventListener('click', e => {
-        const kind = e.currentTarget.dataset.reactionKind;
-        const reactionId = e.currentTarget.dataset.reactionId;
+        const kind = e.currentTarget.dataset.reactionKind
+        const reactionId = e.currentTarget.dataset.reactionId
 
-        if (reactionId)
-          destroyReaction(reaction, kind, reactionId);
-        else
-          createReaction(reaction, kind, reactionableId);
-      });
-    });
-  });
+        if (reactionId) { destroyReaction(reaction, kind, reactionId) } else { createReaction(reaction, kind, reactionableId) }
+      })
+    })
+  })
 
   Array.from(document.querySelectorAll('.js-reaction-dropdown'), dropdown => {
     dropdown.addEventListener('click', e => {
-      const reaction = e.currentTarget.querySelector('.js-reaction');
-      reaction.hidden = !reaction.hidden;
-    });
-  });
-});
+      const reaction = e.currentTarget.querySelector('.js-reaction')
+      reaction.hidden = !reaction.hidden
+    })
+  })
+})
