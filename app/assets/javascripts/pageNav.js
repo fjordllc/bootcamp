6fa7b06b77e49c801336261042fc9ce6b68bd152
teppei machida <machida@fjord.jp>
2019-03-20 01:48:59 +0900

    スクロール設定の変更

--- app/assets/javascripts/pageNav.js
+++ app/assets/javascripts/pageNav.js
@@ -3,9 +3,8 @@ $(function() {
   if ($('.js-page-nav').length > 0) {
     offsetTop = $('.js-page-nav').offset().top;
     offsetLeft = $('.js-page-nav').offset().left;
-    $(window).scroll(function() {
+    $(".wrapper").scroll(function() {
       if ($(this).scrollTop() > offsetTop) {
-        console.log(offsetTop);
         $('.js-page-nav').css('left', offsetLeft);
         $('.js-page-nav').addClass('is-sticky');
       } else {
