a3a62d63782461049c62b4c82a9ec3d504ba0824
teppei machida <machida@fjord.jp>
2019-03-19 14:44:02 +0900

    ナビのjsを削除し、cssで実装

--- app/assets/javascripts/dropDown.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  return $('.js-drop-down__trigger').click(function() {
-    if ($(this).parents('.js-drop-down').hasClass('is-active')) {
-      return $(this).parents('.js-drop-down').removeClass('is-active');
-    } else {
-      return $(this).parents('.js-drop-down').addClass('is-active');
-    }
-  });
-});
