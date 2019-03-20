6e537ad89592af3edce028af664814ffdfff45d3
teppei machida <machida@fjord.jp>
2019-03-20 13:56:09 +0900

    スクロールをjsからcssに移行
    ページのパーツの出し分けをリファクタ
    
    フォームの並び順変更
    
    Fix Typo
    
    テストの画面外のボタンクリック対策
    
    テストの画面外のボタンクリック対策
    
    テストの画面外のボタンクリック対策
    
    テストの画面外のボタンクリック対策
    
    test時のcssを変更
    
    Fix typo
    
    スクロール設定修正
    
    テストにスクロール追加
    
    :cop:
    
    テストにスクロール追加
    
    db/schema.rb 更新
    
    テスト修正
    
    テスト修正
    
    テスト修正
    
    テスト修正
    
    testのためのsassリファクタ

--- app/helpers/body_class_helper.rb
+++ app/helpers/body_class_helper.rb
@@ -12,13 +12,29 @@ module BodyClassHelper
       "index-page"
     elsif params[:action] == "show"
       "show-page"
+    else
+      "other-page"
     end
+  end
+
+  def page_area
+    if controller.controller_path.include?("admin/")
+      "admin-page"
+    elsif qualified_controller_name.include?("welcome")
+      "welcome-page"
+    else
+      "learning-page"
+    end
+  end
+
+  def admin_page?
+    controller.controller_path.include?("admin/")
   end
 
   def body_class(options = {})
     extra_body_classes_symbol = options[:extra_body_classes_symbol] || :extra_body_classes
     qualified_controller_name = controller.controller_path.tr("/", "-")
-    basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name} is-#{page_category}"
+    basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name} is-#{page_category} is-#{page_area} is-#{Rails.env}"
 
     if content_for?(extra_body_classes_symbol)
       [basic_body_class, content_for(extra_body_classes_symbol)].join(" ")
--- test/system/current_user_test.rb
+++ test/system/current_user_test.rb
@@ -9,6 +9,7 @@ class CurrentUserTest < ApplicationSystemTestCase
     visit "/current_user/edit"
     within "form[name=user]" do
       fill_in "user[login_name]", with: "testuser"
+      page.execute_script("window.scroll(0,10000);")
       click_on "更新する"
     end
     assert_text "ユーザー情報を更新しました。"
