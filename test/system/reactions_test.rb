34e2635a11b1c9a63f9a041b2dc2672f00719304
teppei machida <machida@fjord.jp>
2019-03-28 03:07:06 +0900

    待ち時間追加

--- test/system/reactions_test.rb
+++ test/system/reactions_test.rb
@@ -9,39 +9,51 @@ class ReactionsTest < ApplicationSystemTestCase
     visit report_path(reports(:report_1))
     first(".thread-body .js-reaction-dropdown-toggle").click
     first(".thread-body .js-reaction-dropdown li[data-reaction-kind='smile']").click
-    assert_text "😄2"
+    using_wait_time 5 do
+      assert_text "😄2"
+    end
   end
 
   test "destroy reaction for report from dropdown" do
     visit report_path(reports(:report_1))
     first(".thread-body .js-reaction-dropdown-toggle").click
     first(".thread-body .js-reaction-dropdown li[data-reaction-kind='thumbsup']").click
-    refute_text "👍1"
+    using_wait_time 5 do
+      refute_text "👍1"
+    end
   end
 
   test "destroy reaction for report from footer" do
     visit report_path(reports(:report_1))
     first(".thread-body .js-reaction li.is-reacted").click
-    refute_text "👍1"
+    using_wait_time 5 do
+      refute_text "👍1"
+    end
   end
 
   test "post new reaction for comment" do
     visit report_path(reports(:report_3))
     first(".thread-comment .js-reaction-dropdown-toggle").click
     first(".thread-comment .js-reaction-dropdown li[data-reaction-kind='smile']").click
-    assert_text "😄1"
+    using_wait_time 5 do
+      assert_text "😄1"
+    end
   end
 
   test "destroy reaction for comment from dropdown" do
     visit report_path(reports(:report_3))
     first(".thread-comment .js-reaction-dropdown-toggle").click
     first(".thread-comment .js-reaction-dropdown li[data-reaction-kind='heart']").click
-    assert_text "❤️1"
+    using_wait_time 5 do
+      assert_text "❤️1"
+    end
   end
 
   test "destroy reaction for comment from footer" do
     visit report_path(reports(:report_3))
     first(".thread-comment .js-reaction li[data-reaction-kind='heart']").click
-    assert_text "❤️1"
+    using_wait_time 5 do
+      assert_text "❤️1"
+    end
   end
 end
