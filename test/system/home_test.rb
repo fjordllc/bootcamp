6360f627cdf14d7ab4f4a9fed5aa38ff2d96dbeb
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-03-27 22:10:50 +0900

    GitHubアカウント、slackアカウントが未入力の現役生に警告を出す

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -14,4 +14,38 @@ class HomeTest < ApplicationSystemTestCase
     visit "/"
     assert_equal "ダッシュボード | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
   end
+
+  test "GET / without github account " do
+    login_user "muryou", "testtest"
+    visit "/"
+    within(".alert_github_account") do
+      assert_text "GitHubアカウントが未入力です"
+    end
+  end
+
+  test "GET / with github account" do
+    user = users(:muryou)
+    user.github_account = "muryou"
+    login_user user, "testtest"
+
+    visit "/"
+    assert_no_selector ".alert_github_account"
+  end
+
+  test "GET / without slack account" do
+    login_user "muryou", "testtest"
+    visit "/"
+    within(".alert_slack_account") do
+      assert_text "Slackアカウントが未入力です"
+    end
+  end
+
+  test "GET / with slack_account" do
+    user = users(:muryou)
+    user.slack_account = "muryou"
+    login_user user, "testtest"
+
+    visit "/"
+    assert_no_selector ".alert_slack_account"
+  end
 end
