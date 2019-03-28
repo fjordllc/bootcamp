1fd45f1f57a51df0b70846aa68945455f7ace779
kasumi8pon <all.splashing@gmail.com>
2019-03-29 01:10:41 +0900

    job_seekerからjob_seekingに変更

--- db/migrate/20190328141321_add_job_seeker_to_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddJobSeekerToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :job_seeker, :boolean, default: false, null: false
-  end
-end
