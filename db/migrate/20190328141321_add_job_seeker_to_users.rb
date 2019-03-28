8a68e667f704c96d790fd9433d8a20410306127b
kasumi8pon <all.splashing@gmail.com>
2019-03-28 23:28:45 +0900

    userにjob_seekerカラムとスコープを追加

---
+++ db/migrate/20190328141321_add_job_seeker_to_users.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true.
+
+class AddJobSeekerToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :job_seeker, :boolean, default: false, null: false
+  end
+end
