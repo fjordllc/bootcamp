31bac51050246f052601bbe3ab559247191c0885
UsaMomokawa <spcmomo@gmail.com>
2019-04-12 11:40:15 +0900

    rubocopによる修正

--- db/migrate/20190411073945_add_open_product_to_practices.rb
+++ db/migrate/20190411073945_add_open_product_to_practices.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddOpenProductToPractices < ActiveRecord::Migration[5.2]
   def change
     add_column :practices, :open_product, :boolean, default: false, null: false
