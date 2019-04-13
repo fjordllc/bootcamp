80f2a47b522aa804460b9ca92b8be76da8122f5b
UsaMomokawa <spcmomo@gmail.com>
2019-04-13 13:07:48 +0900

    Reportsテーブルにfeelingカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_141321) do
+ActiveRecord::Schema.define(version: 2019_04_12_103144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -229,6 +229,7 @@ ActiveRecord::Schema.define(version: 2019_03_28_141321) do
     t.datetime "updated_at"
     t.date "reported_on"
     t.boolean "wip", default: false, null: false
+    t.integer "feeling"
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
