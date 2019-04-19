85541641ca18894948f8f78d08842405201fb4e3
Masaki Komagata <komagata@gmail.com>
2019-04-19 15:44:10 +0900

    Policyの仕組みを追加
    Product個別ページさえ見えなければタイトルは見えてみ良いように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :init_user
   before_action :allow_cross_domain_access
-  helper_method :product_displayable?
 
   protected
-    def product_displayable?(practice: nil, user: nil)
-      return true if admin_login? || mentor_login? || adviser_login?
-      if user
-        user == current_user || user.has_checked_product_of?(current_user.practices_with_checked_product)
-      else
-        current_user.has_checked_product_of?(practice)
-      end
-    end
-
     def allow_cross_domain_access
       response.headers["Access-Control-Allow-Origin"] = "*"
       response.headers["Access-Control-Allow-Methods"] = "*"
