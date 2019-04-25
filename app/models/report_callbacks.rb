8eeec73b85a8cb82e5b93f3c5eb7438d4adf87e6
lime1024 <lime1024@gmail.com>
2019-04-25 23:20:30 +0900

    はじめての日報を書いたら全員に通知が飛ぶようにした

---
+++ app/models/report_callbacks.rb
@@ -0,0 +1,17 @@
+class ReportCallbacks
+  def after_create(report)
+    send_notification(report)
+  end
+
+  private
+    def send_notification(report)
+      if report.user.reports.count == 1
+        reciever_list = User.where(retired_on: nil)
+        reciever_list.each do |receiver|
+          if report.sender != receiver
+            Notification.first_report(report, receiver)
+          end
+        end
+      end
+    end
+end
