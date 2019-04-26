24a63c7cdd94b30d3cf0497cf02105cf07308419
lime1024 <lime1024@gmail.com>
2019-04-26 15:52:11 +0900

    はじめての日報は通知が飛ぶことをテストした

---
+++ test/system/notification/reports_test.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class Notification::ReportsTest < ApplicationSystemTestCase
+  test "はじめての日報が投稿されたときに全員が通知を受け取る" do
+    login_user "muryou", "testtest"
+    visit "/reports"
+    click_link "日報作成"
+
+    within("#new_report") do
+      fill_in("report[title]", with: "test title")
+      fill_in("report[description]",   with: "test")
+    end
+
+    all(".learning-time")[0].all(".learning-time__started-at select")[0].select("07")
+    all(".learning-time")[0].all(".learning-time__started-at select")[1].select("30")
+    all(".learning-time")[0].all(".learning-time__finished-at select")[0].select("08")
+    all(".learning-time")[0].all(".learning-time__finished-at select")[1].select("30")
+
+    click_button "提出"
+    logout
+
+    login_user "komagata", "testtest"
+    first(".test-bell").click
+    assert_text "muryouさんがはじめての日報を書きました！"
+    logout
+
+    login_user "yamada", "testtest"
+    first(".test-bell").click
+    assert_text "muryouさんがはじめての日報を書きました！"
+    logout
+  end
+
+  test "複数の日報が投稿されているときは通知が飛ばない" do
+    login_user "komagata", "testtest"
+    visit "/reports"
+    click_link "日報作成"
+
+    within("#new_report") do
+      fill_in("report[title]", with: "test title")
+      fill_in("report[description]",   with: "test")
+    end
+
+    all(".learning-time")[0].all(".learning-time__started-at select")[0].select("07")
+    all(".learning-time")[0].all(".learning-time__started-at select")[1].select("30")
+    all(".learning-time")[0].all(".learning-time__finished-at select")[0].select("08")
+    all(".learning-time")[0].all(".learning-time__finished-at select")[1].select("30")
+
+    click_button "提出"
+    logout
+
+    login_user "muryou", "testtest"
+    assert page.has_css?('.has-no-count')
+    logout
+
+    login_user "yamada", "testtest"
+    assert page.has_css?('.has-no-count')
+    logout
+  end
+end
