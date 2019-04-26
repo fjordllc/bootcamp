31f34472929bf7077d5d1c4a61cd0406d667c061
UsaMomokawa <spcmomo@gmail.com>
2019-04-26 23:59:25 +0900

    Q&Aのウォッチ機能のテストを書いた

--- test/system/notification/watches_test.rb
+++ test/system/notification/watches_test.rb
@@ -6,6 +6,7 @@ class Notification::WatchesTest < ApplicationSystemTestCase
   setup do
     watches(:report1_watch_kimura)
     watches(:product1_watch_kimura)
+    watches(:question1_watch_kimura)
   end
 
   test "report watcher receive a notification" do
@@ -13,7 +14,7 @@ class Notification::WatchesTest < ApplicationSystemTestCase
     visit "/reports/#{reports(:report_1).id}"
 
     within(".thread-comment-form__form") do
-      fill_in("comment[description]", with: "test")
+      fill_in("comment[description]", with: "いい日報ですね。")
     end
     click_button "コメントする"
     assert_text "コメントを投稿しました。"
@@ -30,7 +31,7 @@ class Notification::WatchesTest < ApplicationSystemTestCase
     visit "/products/#{products(:product_1).id}"
 
     within(".thread-comment-form__form") do
-      fill_in("comment[description]", with: "test")
+      fill_in("comment[description]", with: "いい提出物ですね。")
     end
     click_button "コメントする"
     assert_text "コメントを投稿しました。"
@@ -40,5 +41,23 @@ class Notification::WatchesTest < ApplicationSystemTestCase
 
     first(".test-bell").click
     assert_text "あなたがウォッチしている【 #{products(:product_1).title} 】にコメントが投稿されました。"
+  end
+
+  test "question watcher receive a notification" do
+    login_user "sotugyou", "testtest"
+    visit "/questions/#{questions(:question_1).id}"
+
+    within(".thread-comment-form__form") do
+      fill_in("answer[description]", with: "vimチュートリアルがおすすめだよ。")
+    end
+    click_button "コメントする"
+    assert_text "回答を作成しました。"
+
+    logout
+    login_user "kimura", "testtest"
+
+    visit "/questions/#{questions(:question_1).id}"
+    first(".test-bell").click
+    assert_text "あなたがウォッチしている【 #{questions(:question_1).title} 】にコメントが投稿されました。"
   end
 end
