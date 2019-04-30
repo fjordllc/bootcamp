92009ca87c46f2e0a862b709f72c9893021c19a6
lime1024 <lime1024@gmail.com>
2019-04-30 17:44:47 +0900

    finished_at を次の日の日付に変えた

--- app/models/learning_time.rb
+++ app/models/learning_time.rb
@@ -6,6 +6,8 @@ class LearningTime < ApplicationRecord
   validates :finished_at, presence: true
   validate :learning_times_finished_at_be_greater_than_started_at
 
+  before_validation :canonicalize_finished_at
+
   def diff
     default = finished_at - started_at
     default >= 0 ? default : finished_at + 1.day - started_at
@@ -14,6 +16,14 @@ class LearningTime < ApplicationRecord
   def learning_times_finished_at_be_greater_than_started_at
     if !report.wip? && diff <= 0
       errors.add(:finished_at, ": 終了時間は開始時間より後にしてください。")
+    end
+  end
+
+  private
+
+  def canonicalize_finished_at
+    if started_at > finished_at
+      self.finished_at = finished_at + 1.day
     end
   end
 end
