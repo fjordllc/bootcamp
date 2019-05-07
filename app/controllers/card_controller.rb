699a550df651d672e1341a542a3a348544bd6a91
Masaki Komagata <komagata@gmail.com>
2019-05-07 21:07:35 +0900

    トライアル終了日を追加

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -23,9 +23,11 @@ class CardController < ApplicationController
       flash[:notice] = "カードを登録しました。"
       logger.info "[Payment] カードを登録しました。"
     rescue Stripe::CardError => e
+      current_user.update!(customer_id: nil)
       flash[:alert] = "カード情報に不備があります。：#{e.message}"
       logger.warn "[Payment] カード情報に不備があります。：#{e.message}"
     rescue => e
+      current_user.update!(customer_id: nil)
       flash[:alert] = "カード登録に失敗しました。運営会社までお問い合わせください。：#{e.message}"
       logger.warn "[Payment] カード登録に失敗しました。運営会社までお問い合わせください。：#{e.message}"
     end
@@ -44,9 +46,11 @@ class CardController < ApplicationController
       flash[:notice] = "カードを編集しました。"
       logger.info "[Payment] カードを登録しました。"
     rescue Stripe::CardError => e
+      current_user.update!(customer_id: nil)
       flash[:alert] = "カード情報に不備があります。：#{e.message}"
       logger.warn "[Payment] カード情報に不備があります。：#{e.message}"
     rescue => e
+      current_user.update!(customer_id: nil)
       flash[:alert] = "カード登録に失敗しました。運営会社までお問い合わせください。：#{e.message}"
       logger.warn "[Payment] カード登録に失敗しました。運営会社までお問い合わせください。：#{e.message}"
     end
