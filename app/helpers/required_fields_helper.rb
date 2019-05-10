d851e3f18d4e1a3b983bd612a1baacae160277c7
Masaki Komagata <komagata@gmail.com>
2019-05-10 15:30:30 +0900

    ユーザーの各種項目を非必須にした
    - プロフィール画像
    - 自己紹介
    - どこでしったか
    - 所属組織

---
+++ app/helpers/required_fields_helper.rb
@@ -0,0 +1,29 @@
+module RequiredFieldsHelper
+  def required_fields
+    return nil unless logged_in?
+
+    messages = []
+
+    if current_user.student? && !current_user.free? && !current_user.paid?
+      messages << "クレジットカードを#{link_to "登録", new_card_path}してください。".html_safe
+    end
+
+    if !current_user.avatar.attached?
+      messages << "プロフィール画像を#{link_to "登録", edit_current_user_path}してください。".html_safe
+    end
+
+    if !current_user.description?
+      messages << "自己紹介を#{link_to "入力", edit_current_user_path}してください。".html_safe
+    end
+
+    if !current_user.how_did_you_know?
+      messages << "弊社をどこで知ったかを#{link_to "入力", edit_current_user_path}してください。".html_safe
+    end
+
+    if !current_user.organization?
+      messages << "現在の所属組織を#{link_to "入力", edit_current_user_path}してください。".html_safe
+    end
+
+    messages
+  end
+end
