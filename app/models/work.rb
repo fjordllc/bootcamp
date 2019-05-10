a799fe5fe7a8a35fad689d468822ba6505469d48
kasumi8pon <all.splashing@gmail.com>
2019-05-10 17:05:59 +0900

    workモデルにvalidationを追加

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,5 +7,6 @@ class Work < ApplicationRecord
   validates :user, presence: true
   validates :title, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
   validates :description, presence: true
+  validates :repository, presence: true
   validates :thumbnail, blob: { content_type: :image, size_range: 0..10.megabytes }
 end
