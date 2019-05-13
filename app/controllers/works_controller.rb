9b493ade3952fa23e7927853cd6aaeee0471b4ef
kasumi8pon <all.splashing@gmail.com>
2019-05-13 16:14:49 +0900

    管理者もworkの編集・削除ができるように変更

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,8 +2,8 @@
 
 class WorksController < ApplicationController
   before_action :require_login
-  before_action :set_work, only: %i(show)
-  before_action :set_my_work, only: %i(edit update destroy)
+  before_action :set_work, only: %i(show edit update destroy)
+  before_action :require_admin_or_my_work, only: %i(edit update destroy)
 
   def show
   end
@@ -19,7 +19,7 @@ class WorksController < ApplicationController
     @work = Work.new(work_params)
     @work.user = current_user
     if @work.save
-      redirect_to user_portfolio_url(current_user), notice: "ポートフォリオに作品を追加しました。"
+      redirect_to @work, notice: "ポートフォリオに作品を追加しました。"
     else
       render :new
     end
@@ -27,7 +27,7 @@ class WorksController < ApplicationController
 
   def update
     if @work.update(work_params)
-      redirect_to user_portfolio_url(current_user), notice: "作品を更新しました。"
+      redirect_to @work, notice: "作品を更新しました。"
     else
       render :edit
     end
@@ -35,7 +35,7 @@ class WorksController < ApplicationController
 
   def destroy
     @work.destroy
-    redirect_to user_portfolio_url(current_user), notice: "ポートフォリオから作品を削除しました。"
+    redirect_to user_portfolio_url(@work.user), notice: "ポートフォリオから作品を削除しました。"
   end
 
   private
@@ -54,7 +54,7 @@ class WorksController < ApplicationController
       @work = Work.find(params[:id])
     end
 
-    def set_my_work
-      @work = current_user.works.find(params[:id])
+    def require_admin_or_my_work
+      redirect_to root_url unless @work.user == current_user || admin_login?
     end
 end
