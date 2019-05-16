0a34bd45b790df01fd396380280f8876fb473f43
kasumi8pon <all.splashing@gmail.com>
2019-05-17 07:33:10 +0900

    URLまたはリポジトリのどちらかを必須入力にするよう変更

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,6 +7,12 @@ class Work < ApplicationRecord
   validates :user, presence: true
   validates :title, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
   validates :description, presence: true
-  validates :repository, presence: true
+  validates :url_or_repository, presence: true
   validates :thumbnail, blob: { content_type: :image, size_range: 0..10.megabytes }
+
+  private
+
+    def url_or_repository
+      url.presence || repository.presence
+    end
 end
