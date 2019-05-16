02133242afe8d656c0a77f200298b017c3ea4377
teppei machida <machida@fjord.jp>
2019-05-16 09:59:56 +0900

    ポートフォリオのデザイン作成

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,14 +4,4 @@ module CommentsHelper
   def user_comments_page?
     controller_path == "users/comments" && action_name == "index"
   end
-
-  def md2html(text)
-    html = CommonMarker.render_html(text)
-    raw(html)
-  end
-
-  def comment_summury(comment, word_count)
-    summury = strip_tags(md2html(comment)).gsub(/[\r\n]/, "")
-    simple_format(truncate(summury, length: word_count))
-  end
 end
