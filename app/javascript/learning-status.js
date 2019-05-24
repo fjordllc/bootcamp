aaeeb341c871c3cc3f2e939c7ac896050a74d300
Masaki Komagata <komagata@gmail.com>
2019-05-24 14:51:29 +0900

    プラクティスのステータスボタンをVue.js化

---
+++ app/javascript/learning-status.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import LearningStatus from './learning-status.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const learningStatuses = document.querySelectorAll('.js-learning-status')
+  if (learningStatuses) {
+    for (let i = 0; i < learningStatuses.length; i++) {
+      let learningStatus = learningStatuses[i];
+
+      const practiceId = learningStatus.getAttribute('data-practice-id')
+      const status = learningStatus.getAttribute('data-status')
+      new Vue({
+        render: h => h(LearningStatus, { props: { practiceId: practiceId, status: status } })
+      }).$mount('.js-learning-status')
+    }
+  }
+})
