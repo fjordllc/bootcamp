a382e2a906172567415df481979517ffe33123d0
UsaMomokawa <spcmomo@gmail.com>
2019-06-08 20:29:16 +0900

    テストをDRYにした

--- test/system/notification/watches_test.rb
+++ test/system/notification/watches_test.rb
@@ -5,36 +5,25 @@ require "application_system_test_case"
 class Notification::WatchesTest < ApplicationSystemTestCase
   setup do
     watches(:report1_watch_kimura)
-  end
-
-  test "report watcher receive a notification" do
+    # コメントを投稿しても自動的にウォッチがONになる
     login_user "machida", "testtest"
     visit "/reports/#{reports(:report_1).id}"
-
     within(".thread-comment-form__form") do
       fill_in("comment[description]", with: "いい日報ですね。")
     end
     click_button "コメントする"
     assert_text "コメントを投稿しました。"
-
     logout
+  end
+
+  test "report watcher receive a notification" do
     login_user "kimura", "testtest"
 
     first(".test-bell").click
     assert_text "あなたがウォッチしている【 #{reports(:report_1).title} 】にコメントが投稿されました。"
   end
 
-  test "ウォッチが自動的についた後、日報にコメントがついてもウォッチ通知が来ないバグの再現" do
-    # ウォッチを自動的につける
-    login_user "machida", "testtest"
-    visit "/reports/#{reports(:report_1).id}"
-    within(".thread-comment-form__form") do
-      fill_in("comment[description]", with: "いい日報ですね。")
-    end
-    click_button "コメントする"
-    assert_text "コメントを投稿しました。"
-    logout
-
+  test "自動的にウォッチがONになった際、新しくコメントがついてもウォッチ通知が来ないバグの再現" do
     login_user "komagata", "testtest"
     visit "/reports/#{reports(:report_1).id}"
     within(".thread-comment-form__form") do
