014b5c7312536da76de5bd214be6e8d6a35c7d37
tararico <emipu.uq@gmail.com>
2019-06-19 13:55:50 +0900

    リタイアするとWIP中の日報を削除

--- app/models/user_callbacks.rb
+++ app/models/user_callbacks.rb
@@ -4,6 +4,7 @@ class UserCallbacks
   def after_update(user)
     if user.saved_change_to_retired_on?
       Product.where(user: user).unchecked.destroy_all
+      Report.where(user: user).wip.destroy_all
     end
 
     if user.saved_change_to_graduated_on? || user.saved_change_to_retired_on?
