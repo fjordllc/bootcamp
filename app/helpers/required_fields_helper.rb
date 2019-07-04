88c8bdacaa53bb41b0ac3ef1e4706a0e28c27235
Masaki Komagata <komagata@gmail.com>
2019-07-04 16:42:01 +0900

    cssが崩れていたところを修正

--- app/helpers/required_fields_helper.rb
+++ app/helpers/required_fields_helper.rb
@@ -15,20 +15,18 @@ module RequiredFieldsHelper
       messages << "#{link_to "自己紹介を入力してください。", edit_current_user_path, class: "card-list__item-link"}".html_safe
     end
 
-    if !current_user.how_did_you_know?
-      messages << "#{link_to "フィヨルドブートキャンプをどこで知ったかを入力してください。", edit_current_user_path, class: "card-list__item-link"}".html_safe
-    end
-
     if !current_user.organization?
       messages << "#{link_to "現在の所属組織を入力してください。", edit_current_user_path, class: "card-list__item-link"}".html_safe
     end
 
-    if current_user.student? && !current_user.github_account?
-      messages << "#{link_to "GitHubアカウントを登録してください。", new_card_path, class: "card-list__item-link is-github"}".html_safe
-    end
+    if current_user.student?
+      if !current_user.github_account?
+        messages << "#{link_to "GitHubアカウントを登録してください。", edit_current_user_path, class: "card-list__item-link is-github"}".html_safe
+      end
 
-    if current_user.student? && !current_user.slack_account?
-      messages << "#{link_to "Slackアカウントを登録してください。", new_card_path, class: "card-list__item-link is-slack"}".html_safe
+      if !current_user.slack_account?
+        messages << "#{link_to "Slackアカウントを登録してください。", edit_current_user_path, class: "card-list__item-link is-slack"}".html_safe
+      end
     end
 
     messages
