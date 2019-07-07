888a3314900883cb26d0209df607b3261d5c93cb
kasumi8pon <all.splashing@gmail.com>
2019-07-07 17:13:53 +0900

    vueの省略記法に変更

--- app/javascript/markdown-textarea.vue
+++ app/javascript/markdown-textarea.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  textarea(ref="textarea" v-bind:value="value" v-on:keyup="update($event.target.value)" v-on:change="update($event.target.value)" v-on:drop="drop" v-on:paste="paste")
+  textarea(ref="textarea" :value="value" @keyup="update($event.target.value)" @change="update($event.target.value)" @drop="drop" @paste="paste")
 </template>
 <script>
 import 'textarea-autosize/dist/jquery.textarea_autosize.min'
