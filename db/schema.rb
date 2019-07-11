1787be2b8c4520493666bb16c22de6219d34d231
Masaki Komagata <komagata@gmail.com>
2019-07-11 14:52:12 +0900

    学習内容のカテゴリーを指定した順番に表示

--- db/schema.rb
+++ db/schema.rb
@@ -324,9 +324,9 @@ ActiveRecord::Schema.define(version: 2019_06_25_092312) do
 
   create_table "works", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.string "url"
     t.string "repository"
+    t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
