793665540c20ae8fdfb8d9558a4fac42d11e28fa
wai-doi <yusuke.doi@esm.co.jp>
2019-07-25 15:43:48 +0900

    実装の変更にあわせてmodelテストも変更

--- test/models/learning_time_test.rb
+++ test/models/learning_time_test.rb
@@ -3,13 +3,19 @@
 require "test_helper"
 
 class LearningTimeTest < ActiveSupport::TestCase
-  test "学習時間が日付を跨いだ時、終了時間が次の日になる" do
-    learning_time = LearningTime.create!(report: reports(:report_8), started_at: "2018-03-11 23:00:00", finished_at: "2018-03-11 02:00:00")
-    assert learning_time.finished_at == Time.zone.parse("2018-03-12 02:00:00")
+  test "canonicalize_finished_at when started_at is latter than finished_at" do
+    learning_time = LearningTime.create!(report: reports(:report_8), started_at: "2017-01-01 23:00:00", finished_at: "2017-01-01 02:00:00")
+    assert_equal(Time.zone.parse("2017-01-02 02:00:00"), learning_time.finished_at)
   end
 
-  test "学習時間が日付を跨がなかった時、終了時間を変更しない" do
-    learning_time = LearningTime.create!(report: reports(:report_8), started_at: "2018-03-11 22:00:00", finished_at: "2018-03-11 23:00:00")
-    assert learning_time.finished_at == Time.zone.parse("2018-03-11 23:00:00")
+  test "canonicalize_finished_at when started_at isn't latter than finished_at " do
+    learning_time = LearningTime.create!(report: reports(:report_8), started_at: "2017-01-01 22:00:00", finished_at: "2017-01-01 23:00:00")
+    assert_equal(Time.zone.parse("2017-01-01 23:00:00"), learning_time.finished_at)
+  end
+
+  test "replace_date_with_reported_on" do
+    learning_time = LearningTime.create!(report: reports(:report_8), started_at: "2017-01-02 22:00:00", finished_at: "2017-01-02 23:00:00")
+    assert_equal(Time.zone.parse("2017-01-01 22:00:00"), learning_time.started_at)
+    assert_equal(Time.zone.parse("2017-01-01 23:00:00"), learning_time.finished_at)
   end
 end
