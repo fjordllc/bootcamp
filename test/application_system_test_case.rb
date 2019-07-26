43bbf28386adc023a328dc53670657a72627ca2e
wai-doi <yusuke.doi@esm.co.jp>
2019-07-26 17:53:27 +0900

    includeしなくてもよかったため削除した

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,7 +5,6 @@ require "supports/login_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include LoginHelper
-  include ActiveSupport::Testing::TimeHelpers
 
   chrome_bin = ENV.fetch("GOOGLE_CHROME_SHIM", nil)
   chrome_opts = { chromeOptions: { args: %w(--headless) } }
