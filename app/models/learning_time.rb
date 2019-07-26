a8f6340ba6069899f457e5a6c34a06ed9862d831
wai-doi <yusuke.doi@esm.co.jp>
2019-07-26 11:55:56 +0900

    学習時間の日付を変えるのはcallbackではなくcontrollerの責務として実装した

--- app/models/learning_time.rb
+++ app/models/learning_time.rb
@@ -6,9 +6,6 @@ class LearningTime < ApplicationRecord
   validates :finished_at, presence: true
   validate :learning_times_finished_at_be_greater_than_started_at
 
-  before_validation :replace_date_with_reported_on
-  before_validation :canonicalize_finished_at
-
   def diff
     finished_at - started_at
   end
@@ -18,18 +15,4 @@ class LearningTime < ApplicationRecord
       errors.add(:finished_at, ": 終了時間は開始時間より後にしてください。")
     end
   end
-
-  private
-
-    def replace_date_with_reported_on
-      report_date = report.reported_on
-      self.started_at = started_at.change(year: report_date.year, month: report_date.month, day: report_date.day)
-      self.finished_at = finished_at.change(year: report_date.year, month: report_date.month, day: report_date.day)
-    end
-
-    def canonicalize_finished_at
-      if started_at > finished_at
-        self.finished_at = finished_at + 1.day
-      end
-    end
 end
