7ad802f835cd0a0fd7ef032b58372d0403ed507f
wai-doi <yusuke.doi@esm.co.jp>
2019-07-29 17:16:20 +0900

    category_orderのテストを追加

--- test/models/practice_test.rb
+++ test/models/practice_test.rb
@@ -19,4 +19,10 @@ class PracticeTest < ActiveSupport::TestCase
     assert practices(:practice_1).exists_learning?(users(:komagata))
     assert_not practices(:practice_1).exists_learning?(users(:machida))
   end
+
+  test "category_order" do
+    practices = Practice.category_order.pluck(:position)
+    expected = %w(1 3 2 4 5 6 9 10 11 12 13 23 24 25 26 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 49 50 20 21 22 7 8 28 44 45 46 47 27 48 51 16 17 19 14 15 18 52 53 54 55 56).map(&:to_i)
+    assert_equal expected, practices
+  end
 end
