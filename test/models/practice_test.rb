7985a1aff0b2662f0006d1c83c17990a13ed9876
wai-doi <yusuke.doi@esm.co.jp>
2019-07-30 10:39:13 +0900

    テストの実装を単純にした

--- test/models/practice_test.rb
+++ test/models/practice_test.rb
@@ -21,8 +21,8 @@ class PracticeTest < ActiveSupport::TestCase
   end
 
   test "category_order" do
-    practices = Practice.category_order.pluck(:position)
-    expected = %w(1 3 2 4 5 6 9 10 11 12 13 23 24 25 26 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 49 50 20 21 22 7 8 28 44 45 46 47 27 48 51 16 17 19 14 15 18 52 53 54 55 56).map(&:to_i)
-    assert_equal expected, practices
+    positions = Practice.category_order.pluck(:position)
+    expected = [1, 3, 2, 4, 5, 6, 9, 10, 11, 12, 13, 23, 24, 25, 26, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 49, 50, 20, 21, 22, 7, 8, 28, 44, 45, 46, 47, 27, 48, 51, 16, 17, 19, 14, 15, 18, 52, 53, 54, 55, 56]
+    assert_equal expected, positions
   end
 end
