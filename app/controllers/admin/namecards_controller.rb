488e0b3c648b3d93cfe49429c69e355a6a5c425b
tararico <emipu.uq@gmail.com>
2019-07-31 12:02:15 +0900

    namecards機能を作成した

---
+++ app/controllers/admin/namecards_controller.rb
@@ -0,0 +1,17 @@
+#frozen_string_literal: true
+
+class Admin::NamecardsController < AdminController
+  def new
+    @target = params[:target] || "except_retired"
+    @users = User.users_role(@target)
+  end
+
+  def create
+    arr = []
+    params.each do |k, v|
+      arr << k if v == "1"
+    end
+    user_ids = arr.select { |i| i =~ /submission/ }.map { |i| i.gsub(/submission/, "") }
+    @users = User.where(id: user_ids)
+  end
+end
