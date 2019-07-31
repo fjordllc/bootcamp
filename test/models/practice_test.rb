2b2f69287dbbaac23bb53429be5f8ce8975dfd49
wai-doi <yusuke.doi@esm.co.jp>
2019-07-31 17:29:15 +0900

    orderのテストを厳密になりすぎないように修正した

--- test/models/practice_test.rb
+++ test/models/practice_test.rb
@@ -21,8 +21,11 @@ class PracticeTest < ActiveSupport::TestCase
   end
 
   test "category_order" do
-    positions = Practice.category_order.pluck(:position)
-    expected = [1, 3, 2, 4, 5, 6, 9, 10, 11, 12, 13, 23, 24, 25, 26, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 49, 50, 20, 21, 22, 7, 8, 28, 44, 45, 46, 47, 27, 48, 51, 16, 17, 19, 14, 15, 18, 52, 53, 54, 55, 56]
-    assert_equal expected, positions
+    ordered_practices = Practice.category_order
+    earlier = practices(:practice_23)
+    middle = practices(:practice_20)
+    later = practices(:practice_14)
+    assert ordered_practices.index(earlier) < ordered_practices.index(middle)
+    assert ordered_practices.index(middle) < ordered_practices.index(later)
   end
 end
