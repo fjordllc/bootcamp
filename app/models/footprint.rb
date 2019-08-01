33a1fa7491d8ea50c7e8b571c15e9d909adde63a
shita1112 <shibata@MBP.local>
2019-08-01 21:34:21 +0900

    N+1潰し | 日報等に付く足跡一覧

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -4,4 +4,6 @@ class Footprint < ApplicationRecord
   belongs_to :user
   belongs_to :footprintable, polymorphic: true
   validates :user_id, presence: true
+
+  scope :with_avatar, -> { preload(user: { avatar_attachment: :blob }) }
 end
