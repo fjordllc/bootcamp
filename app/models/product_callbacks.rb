4e247537c91afdddb3ff6e0e3a1862085bf13c0e
wai-doi <yusuke.doi@esm.co.jp>
2019-08-01 17:53:18 +0900

    研修生が提出物を提出したときにアドバイザーに通知する処理を追加

--- app/models/product_callbacks.rb
+++ app/models/product_callbacks.rb
@@ -2,6 +2,7 @@
 
 class ProductCallbacks
   def after_create(product)
+    send_trainee_submit_notification(product, "#{product.user.login_name}さんが提出しました。")
     send_notification(product, "#{product.user.login_name}さんが提出しました。")
     create_admin_watch(product)
   end
@@ -18,6 +19,13 @@ class ProductCallbacks
     def send_notification(product, message)
       User.admins.each do |user|
         Notification.submitted(product, user, message)
+      end
+    end
+
+    def send_trainee_submit_notification(product, message)
+      recievers = User.where(company: product.user.company).where(adviser: true)
+      recievers.each do |reciever|
+        Notification.submitted(product, reciever, message)
       end
     end
 
