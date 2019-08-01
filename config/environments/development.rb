e68de08675e9e50bd9ab98a5565b209f78751b28
shita1112 <shibata@MBP.local>
2019-08-01 18:18:55 +0900

    N+1対策にbulletを導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,4 +66,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.asset_host = "http://localhost:3000"
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.add_footer = true
+  end
 end
