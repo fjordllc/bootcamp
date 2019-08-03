48c8b339040abbc6f064c0b844ab456a8899efae
wai-doi <yusuke.doi@esm.co.jp>
2019-08-04 01:19:26 +0900

    研修生が日報を作成するとアドバイザーに通知される処理を実装した

--- app/models/report_callbacks.rb
+++ app/models/report_callbacks.rb
@@ -17,12 +17,19 @@ class ReportCallbacks
         message: "#{report.user.login_name}さんが日報【 #{report.title} 】を書きました！"
       )
     end
+    create_davisers_watch(report) if report.user.trainee?
   end
 
   private
     def send_notification(report:, recievers:, message:)
       recievers.each do |reciever|
         Notification.report_submitted(report, reciever, message)
+      end
+    end
+
+    def create_davisers_watch(report)
+      User.advisers.where(company: report.user.company).each do |adviser|
+        Watch.create(user: adviser, watchable: report)
       end
     end
 end
