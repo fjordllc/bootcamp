5e72e82fc99e148d682f01f943e7daca59658f43
wai-doi <yusuke.doi@esm.co.jp>
2019-08-06 11:09:45 +0900

    private methodへの分割を変えた

--- app/models/report_callbacks.rb
+++ app/models/report_callbacks.rb
@@ -2,22 +2,27 @@
 
 class ReportCallbacks
   def after_create(report)
-    send_first_report_notification(report) if report.user.reports.count == 1
-    send_trainee_report_notification(report) if report.user.trainee?
+    if report.user.reports.count == 1
+      send_notification(
+        report: report,
+        recievers: User.where(retired_on: nil).where.not(id: report.sender.id),
+        message: "#{report.user.login_name}さんがはじめての日報を書きました！"
+      )
+    end
+
+    if report.user.trainee?
+      send_notification(
+        report: report,
+        recievers: User.advisers(report.user.company),
+        message: "#{report.user.login_name}さんが日報【 #{report.title} 】を書きました！"
+      )
+    end
   end
 
   private
-    def send_first_report_notification(report)
-      message = "#{report.user.login_name}さんがはじめての日報を書きました！"
-      User.where(retired_on: nil).where.not(id: report.sender.id).each do |reciever|
+    def send_notification(report:, recievers:, message:)
+      recievers.each do |reciever|
         Notification.report_submitted(report, reciever, message)
-      end
-    end
-
-    def send_trainee_report_notification(report)
-      message = "#{report.user.login_name}さんが日報【 #{report.title} 】を書きました！"
-      User.advisers(report.user.company).each do |receiver|
-        Notification.report_submitted(report, receiver, message)
       end
     end
 end
