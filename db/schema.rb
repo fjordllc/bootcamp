7c31b3c4747f5ec486374edee5b0a67ec445166f
Masaki Komagata <komagata@gmail.com>
2019-08-14 18:27:39 +0900

    mail通知のON/OFFのチェックボックスをつけた

--- db/schema.rb
+++ db/schema.rb
@@ -326,6 +326,7 @@ ActiveRecord::Schema.define(version: 2019_08_16_100249) do
     t.string "customer_id"
     t.boolean "job_seeking", default: false, null: false
     t.string "subscription_id"
+    t.boolean "mail_notification", default: true, null: false
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
