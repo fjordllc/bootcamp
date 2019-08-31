2cc33255efbf5ab6104c7d7fc2ad5ea37897e36b
tararico <emipu.uq@gmail.com>
2019-08-31 17:04:32 +0900

    リファクタリング

--- app/controllers/admin/namecards_controller.rb
+++ app/controllers/admin/namecards_controller.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class Admin::NamecardsController < AdminController
+  layout "namecards"
+
   def index
-    arr = []
-    params.each do |k, v|
-      arr << k if v == "1"
-    end
-    user_ids = arr.select { |i| i =~ /submission/ }.map { |i| i.gsub(/submission/, "") }
+    user_ids = params.select { |k, v| k =~ /submission/ && v == "1" }.keys.map { |i| i.gsub(/submission/, "") }
     @users = User.where(id: user_ids)
-    render layout: "namecards"
   end
 end
