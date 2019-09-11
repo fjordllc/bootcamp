88628d5a68064362d4fed2bd87581e72227a7c7e
Masaki Komagata <komagata@gmail.com>
2019-09-12 00:16:08 +0900

    testのファイル名が間違っていたのを修正

--- test/system/notifications_test.rb
+++ test/system/notifications_test.rb
@@ -3,40 +3,17 @@
 require "application_system_test_case"
 
 class NotificationsTest < ApplicationSystemTestCase
-  test "notifications" do
-    login_user "sotugyou", "testtest"
-
-    visit "/"
-    find(".test-show-notifications").click
-    assert_text "komagataさんからコメントが届きました。"
-    assert_text "machidaさんが学習週1日目を確認しました。"
-    assert_text "komagataさんからメンションがきました。"
-    assert_no_text "machidaさんからコメントが届きました。"
-  end
-
-  test "GET /notifications" do
-    login_user "sotugyou", "testtest"
-
-    visit "/notifications"
-    assert_text "komagataさんからコメントが届きました。"
-    assert_text "machidaさんが学習週1日目を確認しました。"
-    assert_text "komagataさんからメンションがきました。"
-    assert_text "machidaさんからコメントが届きました。"
-  end
-
-  test "notifications_allmarks" do
-    login_user "komagata", "testtest"
-    visit "/announcements"
-    click_link "お知らせ作成"
-
-    find("input[name='announcement[title]']").set("お知らせです")
-    find("textarea[name='announcement[description]']").set("お知らせ内容です")
-    click_button "作成"
-    logout
-
+  test "do not send mail if user deny mail" do
     login_user "kimura", "testtest"
-    visit "/notifications"
-    click_link("全て既読にする")
-    assert_text "全て既読にしました"
+    visit "/reports/#{reports(:report_8).id}"
+    within(".thread-comment-form__form") do
+      fill_in("comment[description]", with: "test")
+    end
+    click_button "コメントする"
+
+    if ActionMailer::Base.deliveries.present?
+      last_mail = ActionMailer::Base.deliveries.last
+      assert_not_equal "[Bootcamp] kimuraさんからコメントが届きました。", last_mail.subject
+    end
   end
 end
