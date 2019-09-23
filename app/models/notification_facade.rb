198d8935e9f3db2214dea55295a9e3e590a095e8
Masaki Komagata <komagata@gmail.com>
2019-09-24 02:31:48 +0900

    リタイアした人にはメールを送らないように修正

--- app/models/notification_facade.rb
+++ app/models/notification_facade.rb
@@ -3,56 +3,80 @@
 class NotificationFacade
   def self.came_comment(comment, receiver, message)
     Notification.came_comment(comment, receiver, message)
-    NotificationMailer.came_comment(comment, receiver, message).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.came_comment(comment, receiver, message).deliver_now
+    end
   end
 
   def self.checked(check)
     Notification.checked(check)
-    NotificationMailer.checked(check).deliver_now if check.receiver.mail_notification?
+    receiver = check.receiver
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.checked(check).deliver_now
+    end
   end
 
   def self.mentioned(comment, receiver)
     Notification.mentioned(comment, receiver)
-    NotificationMailer.mentioned(comment, receiver).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.mentioned(comment, receiver).deliver_now
+    end
   end
 
   def self.submitted(subject, receiver, message)
     Notification.submitted(subject, receiver, message)
-    NotificationMailer.submitted(subject, receiver, message).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.submitted(subject, receiver, message).deliver_now
+    end
   end
 
   def self.came_answer(answer)
     Notification.came_answer(answer)
-    NotificationMailer.came_answer(answer).deliver_now if answer.receiver.mail_notification?
+    receiver = answer.receiver
+    if answer.receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.came_answer(answer).deliver_now
+    end
   end
 
   def self.post_announcement(announce, receiver)
     Notification.post_announcement(announce, receiver)
-    NotificationMailer.post_announcement(announce, receiver).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.post_announcement(announce, receiver).deliver_now
+    end
   end
 
   def self.came_question(question, receiver)
     Notification.came_question(question, receiver)
-    NotificationMailer.came_question(question, receiver).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.came_question(question, receiver).deliver_now
+    end
   end
 
   def self.first_report(report, receiver)
     Notification.first_report(report, receiver)
-    NotificationMailer.first_report(report, receiver).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.first_report(report, receiver).deliver_now
+    end
   end
 
   def self.watching_notification(watchable, receiver)
     Notification.watching_notification(watchable, receiver)
-    NotificationMailer.watching_notification(watchable, receiver).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.watching_notification(watchable, receiver).deliver_now
+    end
   end
 
   def self.retired(sender, receiver)
     Notification.retired(sender, receiver)
-    NotificationMailer.retired(sender, receiver).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.retired(sender, receiver).deliver_now
+    end
   end
 
   def self.trainee_report(report, receiver)
     Notification.trainee_report(report, receiver)
-    NotificationMailer.trainee_report(report, receiver).deliver_now if receiver.mail_notification?
+    if receiver.mail_notification? && !receiver.retired_on?
+      NotificationMailer.trainee_report(report, receiver).deliver_now
+    end
   end
 end
