3dd2926744d2f8d3d4d972afffd73e46818c1aba
s4na <appletea.umauma@gmail.com>
2019-10-30 00:18:29 +0900

    席予約｜席予約メモの追加

---
+++ app/models/memo.rb
@@ -0,0 +1,2 @@
+class Memo < ApplicationRecord
+end
---
+++ db/migrate/20191029070437_create_memos.rb
@@ -0,0 +1,10 @@
+class CreateMemos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :memos do |t|
+      t.date :date
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
