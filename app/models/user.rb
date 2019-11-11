23ed7c51635aff51bcac88625d1215c5190615db
s4na <appletea.umauma@gmail.com>
2019-11-11 18:08:54 +0900

    関連のないモデルの関連付けを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,7 +51,6 @@ class User < ActiveRecord::Base
   has_many :events,        dependent: :destroy
   has_many :participations, dependent: :destroy
   has_many :reservations, dependent: :destroy
-  has_many :memos, dependent: :destroy
 
   has_many :participate_events,
     through: :participations,
