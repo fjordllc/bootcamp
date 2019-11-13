f45405b8ea81f1f728e5eee4dca2f5b29aba24c3
吉野輝 <shoynoi.jp@gmail.com>
2019-11-13 13:58:10 +0900

    changeメソッドを使用するように修正

--- db/migrate/20191107072118_remove_feed_url_from_users.rb
+++ db/migrate/20191107072118_remove_feed_url_from_users.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class RemoveFeedUrlFromUsers < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :users, :feed_url
-  end
-
-  def down
-    add_column :users, :feed_url, :string
+  def change
+    remove_column :users, :feed_url, :string
   end
 end
