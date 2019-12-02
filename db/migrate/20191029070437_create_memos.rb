e0c246efe9ce1c3c93e28f67b3dfb02b68852be3
s4na <appletea.umauma@gmail.com>
2019-12-02 15:32:16 +0900

    モデルにuniquq条件追加

--- db/migrate/20191029070437_create_memos.rb
+++ db/migrate/20191029070437_create_memos.rb
@@ -3,7 +3,7 @@
 class CreateMemos < ActiveRecord::Migration[5.2]
   def change
     create_table :memos do |t|
-      t.date :date
+      t.date :date, unique: true
       t.string :body
 
       t.timestamps
