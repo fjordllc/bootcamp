227103d3cff3d6b3a25331b6ec072446dcf9f44f
吉野輝 <shoynoi.jp@gmail.com>
2019-12-11 07:39:38 +0900

    Eventモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_082300) do
+ActiveRecord::Schema.define(version: 2019_12_10_011230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,21 @@ ActiveRecord::Schema.define(version: 2019_11_20_082300) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.string "location", null: false
+    t.integer "capacity", null: false
+    t.datetime "start_at", null: false
+    t.datetime "end_at", null: false
+    t.datetime "open_start_at", null: false
+    t.datetime "open_end_at", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "footprints", id: :serial, force: :cascade do |t|
