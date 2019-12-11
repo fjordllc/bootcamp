8a8f62de67f4f99ad73ab419d6552590b2ced828
吉野輝 <shoynoi.jp@gmail.com>
2019-12-11 14:59:37 +0900

    マイグレーションリセットしたときに例外が発生していたのを修正

--- db/migrate/20151022011615_create_reports.rb
+++ db/migrate/20151022011615_create_reports.rb
@@ -6,8 +6,6 @@ class CreateReports < ActiveRecord::Migration[4.2]
       t.integer :user_id, null: false
       t.string :title, null: false, limit: 255
       t.text :description, null: true
-      t.datetime :created_at
-      t.datetime :updated_at
       t.timestamps
     end
   end
