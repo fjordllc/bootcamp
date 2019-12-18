1a596491c7d28f5f0f4df73cf88ccae8b97aa326
s4na <appletea.umauma@gmail.com>
2019-12-18 13:22:42 +0900

    ユーザーがadminか確認する処理をbefore_actionに変更

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -1,34 +1,29 @@
 # frozen_string_literal: true
 
 class API::MemosController < API::BaseController
+  before_action :admin_login?
   before_action :set_memo, only: [:update, :destroy]
 
   def create
-    if current_user.admin?
-      memo = Memo.new(memo_params)
-      if memo.save
-        render status: :created, json: { id: memo.id, body: memo.body }
-      else
-        render status: :unprocessable_entity, json: { message: memo.errors.full_messages }
-      end
+    memo = Memo.new(memo_params)
+    if memo.save
+      render status: :created, json: { id: memo.id, body: memo.body }
+    else
+      render status: :unprocessable_entity, json: { message: memo.errors.full_messages }
     end
   end
 
   def update
-    if current_user.admin?
-      if @memo.update(memo_params)
-        render status: :ok, json: { id: @memo.id, body: @memo.body }
-      else
-        render status: :unprocessable_entity, json: { message: memo.errors.full_messages }
-      end
+    if @memo.update(memo_params)
+      render status: :ok, json: { id: @memo.id, body: @memo.body }
+    else
+      render status: :unprocessable_entity, json: { message: memo.errors.full_messages }
     end
   end
 
   def destroy
-    if current_user.admin?
-      @memo.destroy
-      render status: :ok, json: { id: @memo.id }
-    end
+    @memo.destroy
+    render status: :ok, json: { id: @memo.id }
   end
 
   private
