d2b4a5ccb82dac7538c287744e64284e50394098
teppei machida <machida@fjord.jp>
2020-01-09 15:10:24 +0900

    使っていないファイルの削除

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( welcome.css namecards.css qrcodes.css diploma.css dev.css dev.js )
+Rails.application.config.assets.precompile += %w( welcome.css namecards.css qrcodes.css diploma.css dev.css )
