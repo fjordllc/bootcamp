8e47ba547667288937002ee224ddd6146e1c05ad
bbbbruno <bbbbruno69@gmail.com>
2020-02-09 21:47:23 +0900

    卒業ボタンを追加

---
+++ app/controllers/graduation_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class GraduationController < ApplicationController
+  before_action :set_user, only: %i(update)
+
+  def update
+    if @user.update(graduated_on: Date.current)
+      redirect_to admin_users_url, notice: "ユーザー情報を更新しました。"
+    else
+      redirect_to admin_users_url, alert: "ユーザー情報の更新に失敗しました"
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+end
