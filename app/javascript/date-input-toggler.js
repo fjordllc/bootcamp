b4a387476be8152c752af42e17b723d2dc9ec33b
hasehiro25 <hase1969.hiro@gmail.com>
2020-02-17 20:35:10 +0900

    querySelectorの範囲が広かったのでクラスを追加して絞り込む

--- app/javascript/date-input-toggler.js
+++ app/javascript/date-input-toggler.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const elements = document.getElementsByClassName('js-date-input-toggler')
   for (let element of elements) {
-    const checkbox = element.querySelector('input[type="checkbox"]')
-    const dateInput = element.querySelector('input[type="date"]')
+    const checkbox = element.querySelector('input.js-date-input-toggler-checkbox')
+    const dateInput = element.querySelector('input.js-date-input-toggler-date')
 
     if (dateInput.value === '') {
       dateInput.style.display = 'none'
     } else {
       checkbox.checked = true
     }
-
     checkbox.addEventListener('change', () => {
       if (checkbox.checked === true) {
         dateInput.style.display = 'block'
