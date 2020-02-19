cdabee921687cd2d933792144b35ca8f0e9ff5aa
NMP300 <t.NMP300@gmail.com>
2020-02-19 19:50:00 +0900

    テストを追加
    staff_rolesメソッドのテストを追加

--- test/decorators/user_decorator_test.rb
+++ test/decorators/user_decorator_test.rb
@@ -8,6 +8,11 @@ class UserDecoratorTest < ActiveSupport::TestCase
     @user2 = ActiveDecorator::Decorator.instance.decorate(users(:hajime))
   end
 
+  test "staff_roles" do
+    assert_equal "管理者、メンター", @user1.staff_roles
+    assert_equal "", @user2.staff_roles
+  end
+
   test "icon_title" do
     assert_equal "komagata: 管理者、メンター", @user1.icon_title
     assert_equal "hajime", @user2.icon_title
