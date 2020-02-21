8c742b9671010b825a93e004e2685f420c9e94f9
NMP300 <t.NMP300@gmail.com>
2020-02-21 16:31:32 +0900

    participationテーブルにenableカラムを追加
    参加者がイベントに参加できるか、補欠なのかを管理するためにenableカラムを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_061812) do
+ActiveRecord::Schema.define(version: 2020_02_21_072210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -216,6 +216,7 @@ ActiveRecord::Schema.define(version: 2019_12_11_061812) do
     t.bigint "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "enable", default: false, null: false
     t.index ["event_id"], name: "index_participations_on_event_id"
     t.index ["user_id", "event_id"], name: "index_participations_on_user_id_and_event_id", unique: true
     t.index ["user_id"], name: "index_participations_on_user_id"
