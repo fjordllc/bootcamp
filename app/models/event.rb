4e1f5810f94d5f76395e4c26dd8b6b17531cc83e
teppei machida <machida@fjord.jp>
2020-02-27 15:45:14 +0900

    イベントのデザイン修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -59,7 +59,7 @@ class Event < ApplicationRecord
     def end_at_be_greater_than_start_at
       diff = end_at - start_at
       if diff <= 0
-        errors.add(:end_at, ": 終了日時は開始日時よりも後の日時にしてください。")
+        errors.add(:end_at, ": イベント終了日時はイベント開始日時よりも後の日時にしてください。")
       end
     end
 
@@ -73,14 +73,14 @@ class Event < ApplicationRecord
     def open_start_at_be_less_than_start_at
       diff = start_at - open_start_at
       if diff <= 0
-        errors.add(:open_start_at, ": 募集開始日時は開始日時よりも前の日時にしてください。")
+        errors.add(:open_start_at, ": 募集開始日時はイベント開始日時よりも前の日時にしてください。")
       end
     end
 
     def open_end_at_be_less_than_end_at
       diff = end_at - open_end_at
       if diff < 0
-        errors.add(:open_end_at, ": 募集終了日時は終了日時よりも前の日時にしてください。")
+        errors.add(:open_end_at, ": 募集終了日時はイベント終了日時よりも前の日時にしてください。")
       end
     end
 
