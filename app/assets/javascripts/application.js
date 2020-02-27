216b05930a7b000bad3615697bffbb14288d7540
pat1543 <pat1543@gmail.com>
2020-02-27 14:26:51 +0900

    jqueryを素のjavascriptに書き換え、sprocketsから移す

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+
 //= require jquery_ujs
 //= require cocoon
 //= require select2/dist/js/select2.full.js
