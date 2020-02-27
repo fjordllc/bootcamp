20410978e9fa085c29c25d99b5d448f9170782d2
pat1543 <pat1543@gmail.com>
2020-02-27 23:44:36 +0900

    comments_testが通るようにwarning.jsを修正

--- app/javascript/learningTime.js
+++ app/javascript/learningTime.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.form-item__times-action > a.remove_fields.dynamic').style.display = 'none';
-});
+  document.querySelector('.form-item__times-action > a.remove_fields.dynamic').style.display = 'none'
+})
--- app/javascript/showHandle.js
+++ app/javascript/showHandle.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const trigger = document.querySelectorAll('.js-show-handle__trigger');
+  const trigger = document.querySelectorAll('.js-show-handle__trigger')
 
   return trigger.forEach(value => {
     value.addEventListener('click', () => {
       if (value.classList.contains('is-active')) {
         value.closest('.js-show-handle').querySelectorAll('.js-show-handle__target').forEach((target) => {
-          target.classList.remove('is-active');
-        });
-        value.classList.remove('is-active');
+          target.classList.remove('is-active')
+        })
+        value.classList.remove('is-active')
       } else {
         value.closest('.js-show-handle').querySelectorAll('.js-show-handle__target').forEach((target) => {
-          target.classList.add('is-active');
-        });
-        value.classList.add('is-active');
+          target.classList.add('is-active')
+        })
+        value.classList.add('is-active')
       }
-    });
-  });
-});
+    })
+  })
+})
