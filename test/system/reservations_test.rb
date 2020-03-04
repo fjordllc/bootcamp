1f06ac038c35251177c07280e4e5c6fba91ef08b
bbbbruno <bbbbruno69@gmail.com>
2020-03-04 13:35:21 +0900

    テストを通るように修正

--- test/system/reservations_test.rb
+++ test/system/reservations_test.rb
@@ -12,7 +12,7 @@ class ReservationsTest < ApplicationSystemTestCase
     assert_equal "席予約一覧 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
 
     within("#reservation-2019-11-02-#{seats(:seat_2).id}") do
-      click_button
+      find("#reserve-seat").click
     end
     within("#reservation-2019-11-02-#{seats(:seat_2).id}") do
       assert_text "komagata"
@@ -25,7 +25,7 @@ class ReservationsTest < ApplicationSystemTestCase
 
     accept_confirm do
       within("#reservation-#{reservations(:reservation_1).date}-#{reservations(:reservation_1).seat.id}") do
-        click_button
+        find("#cancel-reservation").click
       end
     end
     within("#reservation-#{reservations(:reservation_1).date}-#{reservations(:reservation_1).seat.id}") do
@@ -36,16 +36,16 @@ class ReservationsTest < ApplicationSystemTestCase
   test "reservations beyond one month cannot be made" do
     visit "/reservation_calenders"
     assert_equal "席予約一覧 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
-    click_on "来月"
+    click_link "next-month"
 
     reservation_date = Date.current.next_month.tomorrow
 
     if (Date.current.month + 2) <= reservation_date.month
-      click_on "来月"
+      click_link "next-month"
     end
     accept_confirm do
       within("#reservation-#{reservation_date}-#{seats(:seat_2).id}") do
-        click_button
+        find("#reserve-seat").click
       end
     end
 
