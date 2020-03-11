7ef2f6f0c6bb92e4df722b90081221f72571aaca
NMP300 <t.NMP300@gmail.com>
2020-03-11 21:09:55 +0900

    ブログ記事のタグ検索のシステムテストを修正した
    タグ検索機能をテストするために、ArticleのFixturesにtagのデータを追加した。

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -250,11 +250,10 @@ class ArticlesTest < ApplicationSystemTestCase
 
     fill_in "article[title]", with: "タイトル"
     fill_in "article[body]", with: "内容"
-    fill_in "article[tag_list]", with: "tag"
+    fill_in "article[tag_list]", with: "FJORD"
     click_on "更新する"
-    click_on "tag"
+    click_on "FJORD"
 
-    assert_equal 1, all(".articles__item").length
->>>>>>> 5145af415 (サムネイル画像のURLを返すメソッドのテストを追加・既存のテストの修正)
+    assert_equal 2, all(".articles__item").length
   end
 end
