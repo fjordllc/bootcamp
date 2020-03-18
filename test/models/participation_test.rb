a02a998e4717860d50aebc86b5957ad6e0e9effd
NMP300 <t.NMP300@gmail.com>
2020-03-18 17:34:54 +0900

    event, participationモデルのテストを追加・修正した
    繰り上がり関連の処理の実装が変更されたため、それに伴ってモデルテストを修正及び追加した。

--- test/models/participation_test.rb
+++ test/models/participation_test.rb
@@ -3,19 +3,9 @@
 require "test_helper"
 
 class ParticipationTest < ActiveSupport::TestCase
-  test "participation's enable is false when event capacity is full" do
-    event = events(:event_3)
-    user = users(:hajime)
-
-    participation = event.participations.create(user: user)
-    assert_equal false, participation.enable
-  end
-
-  test "participation's enable is true when event capacity is not full" do
-    event = events(:event_2)
-    user = users(:hajime)
-
-    participation = event.participations.create(user: user)
-    assert_equal true, participation.enable
+  test "waited?" do
+    participation = participations(:participation_2)
+    participation.update(enable: true)
+    assert participation.waited?
   end
 end
