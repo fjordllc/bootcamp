b040bd84b69de3ec808b4f965e11c5303dc05d48
chihaso <cycyccq@gmail.com>
2020-03-24 09:32:42 +0900

    テストの名前を変更

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -171,7 +171,7 @@ class UserTest < ActiveSupport::TestCase
     assert user.invalid?
   end
 
-  test "is correct completed practicies percentage" do
+  test "is correct progress" do
     user = users(:komagata)
     old_percentage = user.completed_percentage
     user.completed_practices << practices(:practice_4)
