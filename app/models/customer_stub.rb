34fabda30a326e38f389848ebc010c3a1de7e0b7
Masaki Komagata <komagata@gmail.com>
2020-03-26 23:59:13 +0900

    webmockをdevelopmentだけに限定
    productionへのdeployで読み込めなくなるため

--- app/models/customer_stub.rb
+++ app/models/customer_stub.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 module CustomerStub
-  WebMock.allow_net_connect!
-
   def retrieve(id)
-    json = File.read("#{Rails.root}/test/fixtures/files/mock_bodies/customer.json")
-    WebMock.stub_request(:get, "https://api.stripe.com/v1/customers/cus_12345678").
-      to_return(status: 200, body: json)
+    if Rails.env.development?
+      WebMock.allow_net_connect!
+
+      json = File.read("#{Rails.root}/test/fixtures/files/mock_bodies/customer.json")
+      WebMock.stub_request(:get, "https://api.stripe.com/v1/customers/cus_12345678").
+        to_return(status: 200, body: json)
+    end
 
     super
   end
