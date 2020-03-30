a8b8224762649e6589507928629f35ebb1d464b9
chihaso <cycyccq@gmail.com>
2020-03-31 00:30:00 +0900

    viewで行っている処理をヘルパーに切り出す

---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module SearchHelper
+  def matched_document(searchable)
+    if searchable.class == Comment
+      document = searchable.commentable_type.constantize.find(searchable.commentable_id)
+    else
+      document = searchable
+    end
+  end
+
+  def searchable_url(searchable)
+    if searchable.class == Comment
+      document = searchable.commentable_type.constantize.find(searchable.commentable_id)
+      "#{polymorphic_url(document)}#comment_#{searchable.id}"
+    else
+      polymorphic_url(searchable)
+    end
+  end
+
+  def filtered_message(searchable)
+    if searchable.class == Comment && searchable.commentable_type == "Product"
+      commentable = Product.find(searchable.commentable_id)
+      if policy(commentable).show? || commentable.practice.open_product?
+        searchable.description
+      else
+        "該当プラクティスを完了するまで他の人の提出物へのコメントは見れません。"
+      end
+    else
+      searchable.description
+    end
+  end
+end
