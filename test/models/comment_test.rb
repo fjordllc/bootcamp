38c7a64992f793b30d7e853fa98bdd729cdf2b13
hasehiro25 <hase1969.hiro@gmail.com>
2020-04-01 02:13:36 +0900

    コメント順にユーザーを取得するテストの修正

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,10 +3,10 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  test "return grouped users" do
+  test "return users grouped by latest date" do
     report = reports(:report_4)
     users = report.comments.grouped_users
-    sorted_users = report.comments.map(&:user).uniq
+    sorted_users = report.comments.order(created_at: :desc).map(&:user).uniq.reverse
     assert_equal users, sorted_users
   end
 end
