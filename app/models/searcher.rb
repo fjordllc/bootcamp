373513b0a4be30bd2713e2fb79a850a5d5cc4bdf
chihaso <cycyccq@gmail.com>
2020-03-31 17:11:10 +0900

    検索対象にQ&AのAを含める

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -10,13 +10,15 @@ class Searcher
     ["Docs", :pages]
   ]
 
-  AVAILABLE_TYPES = DOCUMENT_TYPES.map(&:second) - [:all] + [:comments]
+  AVAILABLE_TYPES = DOCUMENT_TYPES.map(&:second) - [:all] + [:comments] + [:answers]
 
   def self.search(word, document_type: :all)
     if document_type == :all
       AVAILABLE_TYPES.flat_map { |type| result_for(type, word) }.sort_by { |result| result.created_at }.reverse
     elsif document_type.to_s.capitalize.singularize.constantize.include?(Commentable)
       [document_type, :comments].flat_map { |type| result_for(type, word, commentable_type: document_type.to_s.capitalize.singularize) }.sort_by { |result| result.created_at }.reverse
+    elsif document_type == :questions
+      [document_type, :answers].flat_map { |type| result_for(type, word) }
     else
       result_for(document_type, word).sort_by { |result| result.created_at }.reverse
     end
@@ -32,6 +34,8 @@ class Searcher
       Practice.ransack(title_or_description_or_goal_cont_all: word).result
     when :questions
       Question.ransack(title_or_description_cont_all: word).result
+    when :answers
+      Answer.ransack(description_cont_all: word).result
     when :announcements
       Announcement.ransack(title_or_description_cont_all: word).result
     when :comments
