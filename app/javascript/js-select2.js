eb085721a885ea0352c191de5ce4eac9297c2d52
hasehiro25 <hase1969.hiro@gmail.com>
2020-04-08 01:03:30 +0900

    ブラウザバックの時にselect2がおかしくなるのを修正

--- app/javascript/js-select2.js
+++ app/javascript/js-select2.js
@@ -1,3 +1,7 @@
+document.addEventListener('turbolinks:before-cache', () => {
+  $('.js-select2').select2('destroy')
+})
+
 document.addEventListener('turbolinks:load', () => {
   $('.js-select2').select2({
     closeOnSelect: true
