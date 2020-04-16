a21ff7bb2229530810ab3d46cf03ca35ae27c052
hasehiro25 <hase1969.hiro@gmail.com>
2020-04-16 21:12:40 +0900

    日報、提出物にコメントアイコンを表示する機能にテスト追加

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,10 +3,19 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  test "return users grouped by latest date" do
+  test ".users_who_commented" do
     report = reports(:report_4)
-    users = report.comments.grouped_users
+    users = report.comments.users_who_commented
     sorted_users = report.comments.order(created_at: :desc).map(&:user).uniq.reverse
     assert_equal users, sorted_users
+  end
+
+  test ".collect_latest_for_each_user" do
+    report = reports(:report_4)
+    comments = report.comments.collect_latest_for_each_user
+    users = [users(:komagata), users(:machida), users(:sotugyou)]
+    comment_for_each_user = users.map { |user| user.comments.where(commentable_id: report.id).order(created_at: :asc).last }
+    sorted_comments = comment_for_each_user.sort_by { |comment| comment.created_at }
+    assert_equal comments, sorted_comments
   end
 end
