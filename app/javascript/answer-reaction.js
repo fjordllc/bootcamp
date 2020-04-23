f36e18b20c97da66a4faefb4baef8030893dfe53
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-04-24 01:06:56 +0900

    内容修正ボタンを正常に機能させました

---
+++ app/javascript/answer-reaction.js
@@ -0,0 +1,107 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const reactions = document.querySelectorAll('.js-answer-reactions')
+
+  if (reactions.length === 0) { return }
+
+  const requestReaction = (url, method, callback) => {
+    fetch(url, {
+      method: method,
+      credentials: 'same-origin',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': $.rails.csrfToken()
+      }
+    }).then(response => {
+      return response.json()
+    }).then(json => {
+      callback(json)
+    }).catch(error => {
+      console.warn(error)
+    })
+  }
+
+  const updateReactionCount = (element, count) => {
+    let reactionCount = element.querySelector('.js-reaction-count')
+
+    if (!reactionCount) { return }
+
+    reactionCount.textContent = Number(reactionCount.textContent) + count
+    switch (reactionCount.textContent) {
+      case '0':
+        element.hidden = true
+        break
+      case '1':
+        element.hidden = false
+        break
+    }
+  }
+
+  const updateReactionLoginNames = (element, loginName) => {
+    let reactionLoginNames = element.querySelector('.js-reaction-login-names')
+
+    if (!reactionLoginNames) { return }
+
+    let reactionLoginName = Array.from(reactionLoginNames.children).find(li => {
+      return li.textContent === loginName
+    })
+
+    if (reactionLoginName) {
+      reactionLoginNames.removeChild(reactionLoginName)
+    } else {
+      let li = document.createElement('li')
+      li.textContent = loginName
+      reactionLoginNames.appendChild(li)
+    }
+  }
+
+  const createAnswerReaction = (reaction, kind, loginName, reactionableId) => {
+    const answerUrl = `/api/reactions?reactionable_answer_id=${reactionableId}&kind=${kind}`
+
+    requestReaction(answerUrl, 'POST', (json) => {
+      Array.from(reaction.querySelectorAll(`[data-reaction-kind="${kind}"]`), element => {
+        element.classList.add('is-reacted')
+        element.dataset.reactionId = json.id
+        updateReactionCount(element, 1)
+        updateReactionLoginNames(element, loginName)
+      })
+    })
+  }
+
+  const destroyAnswerReaction = (reaction, kind, loginName, reactionId) => {
+    const answerUrl = `/api/reactions/${reactionId}`
+
+    requestReaction(answerUrl, 'DELETE', (json) => {
+      Array.from(reaction.querySelectorAll(`[data-reaction-kind="${kind}"]`), element => {
+        element.classList.remove('is-reacted')
+        delete (element.dataset.reactionId)
+        updateReactionCount(element, -1)
+        updateReactionLoginNames(element, loginName)
+      })
+    })
+  }
+
+  Array.from(reactions, reaction => {
+    const loginName = reaction.dataset.reactionLoginName
+    const reactionableId = reaction.dataset.reactionReactionableId
+
+    Array.from(reaction.querySelectorAll('li'), element => {
+      element.addEventListener('click', e => {
+        const kind = e.currentTarget.dataset.reactionKind
+        const reactionId = e.currentTarget.dataset.reactionId
+
+        if (reactionId) {
+          destroyAnswerReaction(reaction, kind, loginName, reactionId)
+        } else {
+          createAnswerReaction(reaction, kind, loginName, reactionableId)
+        }
+      })
+    })
+  })
+
+  Array.from(document.querySelectorAll('.js-reaction-dropdown'), dropdown => {
+    dropdown.addEventListener('click', e => {
+      const reaction = e.currentTarget.querySelector('.js-reaction')
+      reaction.hidden = !reaction.hidden
+    })
+  })
+})
