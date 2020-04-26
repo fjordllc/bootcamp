428c30e81e01eb484d1bbb850751843f7f61a538
bbbbruno <bbbbruno69@gmail.com>
2020-04-26 19:45:10 +0900

    Vue.jsでもシンタックスハイライトにprism.jsを使用するように変更

--- app/javascript/packs/markdown-it.js
+++ app/javascript/packs/markdown-it.js
@@ -2,23 +2,13 @@ import MarkdownIt from 'markdown-it'
 import MarkdownItEmoji from 'markdown-it-emoji'
 import MarkdownItMention from './markdown-it-mention'
 import MarkdownItTaskLists from 'markdown-it-task-lists'
-import hljs from 'highlight.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const md = new MarkdownIt({
     html: true,
     breaks: true,
     linkify: true,
-    langPrefix: 'lng-',
-    highlight: (str, lang) => {
-      if (lang && hljs.getLanguage(lang)) {
-        try {
-          return hljs.highlight(lang, str).value
-        } catch (__) {}
-      }
-
-      return ''
-    }
+    langPrefix: 'language-'
   })
 
   md.use(MarkdownItEmoji).use(MarkdownItMention).use(MarkdownItTaskLists)
