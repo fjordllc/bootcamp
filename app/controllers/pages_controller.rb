2d87b1aadfc2f6f1fe4c49b0c37c78a6c5b3b4c3
kota kanazawa <kota0818@gmail.com>
2020-06-01 11:19:15 +0900

    ドキュメントの新規作成機能を追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,6 +20,7 @@ class PagesController < ApplicationController
 
   def create
     @page = Page.new(page_params)
+    @page.user_id = current_user.id
     if @page.save
       redirect_to @page, notice: "ページを作成しました。"
     else
