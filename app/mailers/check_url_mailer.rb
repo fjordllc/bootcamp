5a630edb94d629793a7c65f9db81615d20c5c723
horikoshi <mkhth3a@gmail.com>
2020-06-12 13:57:16 +0900

    リンク切れURLの数をメールに表示するようにした

--- app/mailers/check_url_mailer.rb
+++ app/mailers/check_url_mailer.rb
@@ -4,6 +4,7 @@ class CheckUrlMailer < ApplicationMailer
   def notify_error_url(page_error_url, practice_error_url)
     @page_error_url = page_error_url
     @practice_error_url = practice_error_url
+    @number_of_error_url = page_error_url.size + practice_error_url.size
     mail to: "info@fjord.jp", subject: "[BootCamp Admin] リンク切れ報告"
   end
 end
