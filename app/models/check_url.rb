f7ce4293ac986fbf91cce66cd6c1f28bee75193f
horikoshi <mkhth3a@gmail.com>
2020-06-13 01:27:58 +0900

    modelファイルのメソッドを修正

--- app/models/check_url.rb
+++ app/models/check_url.rb
@@ -4,64 +4,65 @@ require "uri"
 require "net/http"
 
 class CheckUrl
-  def get_url(contents_array)
-    contents_array.map do |page|
-      unless URI.extract(page[:body]) == []
-        { id: page[:id], url: URI.extract(page[:body]) }
-      end
+  def notify_error_url
+    error_url_on_page = check_url(get_page_contents)
+    error_url_on_practice = check_url(get_practice_contents)
+    if error_url_on_page.size != 0 || practice_error_url.size != 0
+      CheckUrlMailer.notify_error_url(error_url_on_page, error_url_on_practice).deliver_now
     end
   end
 
-  def modify_url(url_array)
-    url_array.each do |hash|
-      hash[:url].each do |url|
-        if url[-1] == ")"
-          { id: hash[:id], url: url.chop! }
-        else
-          { id: hash[:id], url: url }
+  private
+    def get_url(contents_array)
+      contents_array.map do |hash|
+        unless URI.extract(hash[:contents]) == []
+          { id: hash[:id], url: URI.extract(hash[:contents]) }
         end
       end
     end
-  end
 
-  def access_url(url_array)
-    url_array.map do |value|
-      value[:url].map do |url|
-        begin
-          response = Net::HTTP.get_response(URI.parse(url))
-        rescue SocketError
-          next { id: value[:id], url: url }
+    def modify_url(url_array)
+      url_array.each do |hash|
+        hash[:url].each do |url|
+          if url[-1] == ")"
+            { id: hash[:id], url: url.chop! }
+          else
+            { id: hash[:id], url: url }
+          end
         end
-        { id: value[:id], url: url } unless response.code == "200" || response.code == "301"
       end
     end
-  end
 
-
-  def check_url
-    # Docs
-    page_id = Page.all.map(&:id)
-    page_array = page_id.map do |id|
-      { id: id, body: Page.find(id).body }
+    def access_url(url_array)
+      url_array.map do |hash|
+        hash[:url].map do |url|
+          begin
+            response = Net::HTTP.get_response(URI.parse(url))
+          rescue SocketError
+            next { id: hash[:id], url: url }
+          end
+          { id: hash[:id], url: url } unless response.code == "200" || response.code == "301"
+        end
+      end
     end
 
-    page_url = get_url(page_array).compact!
-    modified_page_url = modify_url(page_url)
-    page_error_url = access_url(modified_page_url).flatten.compact
-
-    # Practice
-    practice_id = Practice.all.map(&:id)
-    practice_array = practice_id.map do |id|
-      { id: id, body: Practice.find(id).description + Practice.find(id).goal }
+    def get_page_contents
+      page_id = Page.all.map(&:id)
+      page_id.map do |id|
+        { id: id, contents: Page.find(id).body }
+      end
     end
 
-    practice_url = get_url(practice_array).compact!
-    modified_practice_url = modify_url(practice_url)
-    practice_error_url = access_url(modified_practice_url).flatten.compact
-
-    # メール送信
-    if page_error_url.size != 0 || practice_error_url.size != 0
-      CheckUrlMailer.notify_error_url(page_error_url, practice_error_url).deliver_now
+    def get_practice_contents
+      practice_id = Practice.all.map(&:id)
+      practice_id.map do |id|
+        { id: id, contents: Practice.find(id).description + Practice.find(id).goal }
+      end
     end
-  end
+
+    def check_url(contents_array)
+      url_array = get_url(contents_array).compact
+      modified_url_array = modify_url(url_array)
+      access_url(modified_url_array).flatten.compact
+    end
 end
