3a5f55117e7b220c3bf6fe5f6ff09f1a83fc4322
horikoshi <mkhth3a@gmail.com>
2020-06-14 00:17:34 +0900

    APIへのリクエストが1回になるように修正

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -10,12 +10,26 @@ class API::ReservationsController < API::BaseController
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
-    @reservation.user = current_user
-    if @reservation.save
-      render :create, status: :created
+    if reservation_params[:seat_id].class == Array && reservation_params[:date].class == String
+      reservation_params[:seat_id].each_with_index do |seat_id, index|
+        @reservation = Reservation.new(seat_id: seat_id, date: reservation_params[:date])
+        @reservation.user = current_user
+        @reservation.save
+      end
+    elsif reservation_params[:seat_id].class == Integer && reservation_params[:date].class == Array
+      reservation_params[:date].each_with_index do |date, index|
+        @reservation = Reservation.new(seat_id: reservation_params[:seat_id], date: date)
+        @reservation.user = current_user
+        @reservation.save
+      end
     else
-      render status: :bad_request, json: { message: @reservation.errors.full_messages }
+      @reservation = Reservation.new(reservation_params)
+      @reservation.user = current_user
+      if @reservation.save
+        render :create, status: :created
+      else
+        render status: :bad_request, json: { message: @reservation.errors.full_messages }
+      end
     end
   end
 
@@ -27,7 +41,9 @@ class API::ReservationsController < API::BaseController
     def reservation_params
       params.require(:reservation).permit(
         :date,
-        :seat_id
+        :seat_id,
+        { seat_id: [] },
+        { date: [] },
       )
     end
 
