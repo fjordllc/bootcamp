5f7b9395788efa706d5621d45bf5a4ce47da7f16
horikoshi <mkhth3a@gmail.com>
2020-06-15 16:04:47 +0900

    共通の処理をprivateメソッドに切り出した

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -10,23 +10,8 @@ class API::ReservationsController < API::BaseController
   end
 
   def create
-    @reservations = []
-    if reservation_params[:seat_id].class == Array && reservation_params[:date].class == String
-      reservation_params[:seat_id].each do |seat_id|
-        @reservation = Reservation.new(seat_id: seat_id, date: reservation_params[:date])
-        @reservation.user = current_user
-        @reservation.save
-        @reservations << @reservation
-      end
-      render :index, status: :created
-    elsif reservation_params[:seat_id].class == Integer && reservation_params[:date].class == Array
-      reservation_params[:date].each do |date|
-        @reservation = Reservation.new(seat_id: reservation_params[:seat_id], date: date)
-        @reservation.user = current_user
-        @reservation.save
-        @reservations << @reservation
-      end
-      render :index, status: :created
+    if reservation_params[:seat_id].class == Array || reservation_params[:date].class == Array
+      create_reservations(seat_id: reservation_params[:seat_id], date: reservation_params[:date])
     else
       @reservation = Reservation.new(reservation_params)
       @reservation.user = current_user
@@ -54,5 +39,19 @@ class API::ReservationsController < API::BaseController
 
     def set_reservation
       @reservation = current_user.reservations.find(params[:id])
+    end
+
+    def create_reservations(seat_id:, date:)
+      @reservations = []
+      values = seat_id if seat_id.class == Array && date.class == String
+      values = date if date.class == Array && seat_id.class == Integer
+      values.each do |value|
+        @reservation = Reservation.new(seat_id: value, date: date) if seat_id.class == Array
+        @reservation = Reservation.new(seat_id: seat_id, date: value) if date.class == Array
+        @reservation.user = current_user
+        @reservation.save
+        @reservations << @reservation
+      end
+      render :index, status: :created
     end
 end
