5be286d93a3de1cf9333fd35ab64f84ebc606a7a
kota kanazawa <kota0818@gmail.com>
2020-06-30 13:50:00 +0900

    テストを修正

--- test/system/practices_test.rb
+++ test/system/practices_test.rb
@@ -56,8 +56,12 @@ class PracticesTest < ApplicationSystemTestCase
       fill_in "practice[description]", with: "テストの内容です"
       within "#reference_books" do
         click_link "追加"
-        fill_in "タイトル", with: "プロを目指す人のためのRuby入門"
-        fill_in "ASIN", with: "B077Q8BXHC"
+        fill_in "タイトル", with: "テストの参考書籍タイトル"
+        fill_in "ASIN", with: "テストの参考書籍ASIN"
+        click_link "削除"
+        click_link "追加"
+        fill_in "タイトル", with: "テストの参考書籍タイトル2"
+        fill_in "ASIN", with: "テストの参考書籍ASIN2"
       end
       fill_in "practice[goal]", with: "テストのゴールの内容です"
       fill_in "practice[memo]", with: "テストのメンター向けメモの内容です"
@@ -72,15 +76,33 @@ class PracticesTest < ApplicationSystemTestCase
     visit "/practices/#{practice.id}/edit"
     within "form[name=practice]" do
       fill_in "practice[title]", with: "テストプラクティス"
-      within "#reference_books" do
-        click_link "追加"
-        fill_in "タイトル", with: "プロを目指す人のためのRuby入門"
-        fill_in "ASIN", with: "B077Q8BXHC"
-      end
       click_button "更新する"
     end
     assert_text "プラクティスを更新しました"
     assert_equal "UNIX", Practice.find(practice.id).category.name
+  end
+
+  test "add a reference book" do
+    login_user "komagata", "testtest"
+    practice = practices(:practice_2)
+    visit "/practices/#{practice.id}/edit"
+    within "#reference_books" do
+      click_link "追加"
+      fill_in "タイトル", with: "テストの参考書籍タイトル", match: :prefer_exact
+      fill_in "ASIN", with: "テストの参考書籍ASIN", match: :prefer_exact
+    end
+    click_button "更新する"
+  end
+
+  test "update a reference book" do
+    login_user "komagata", "testtest"
+    practice = practices(:practice_2)
+    visit "/practices/#{practice.id}/edit"
+    within "#reference_books" do
+      fill_in "タイトル", with: "テストの参考書籍タイトル"
+      fill_in "ASIN", with: "テストの参考書籍ASIN"
+    end
+    click_button "更新する"
   end
 
   test "category button link to courses/practices#index with category fragment" do
