b156678a36e611191947c4de86bc98383d1df10d
Masaki Komagata <komagata@gmail.com>
2020-07-02 15:34:07 +0900

    受講料の引き落とし時刻が間違っていたのを修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,12 +7,6 @@ class WelcomeController < ApplicationController
   end
 
   def pricing
-    @days = [
-      Time.current.strftime("%m/%d %H:%M:%S"),
-      Time.current.since(1.day).strftime("%m/%d"),
-      Time.current.since(2.day).strftime("%m/%d %H:%M:%S"),
-      Time.current.since(2.day).since(1.second).strftime("%m/%d %H:%M:%S"),
-    ]
   end
 
   def faq
