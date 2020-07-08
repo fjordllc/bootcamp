63b983f4d370d867ee30ba2b5672ddb2db68fabb
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-07-08 12:33:59 +0900

    ユーザー別の分報機能のchannelの単体テストを追加

--- test/channels/users/timelines_channel_test.rb
+++ test/channels/users/timelines_channel_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class TimelinesChannelTest < ActionCable::Channel::TestCase
+class Users::TimelinesChannelTest < ActionCable::Channel::TestCase
   def setup
     @user = users(:hajime)
     @timeline = timelines(:timeline_1)
@@ -10,39 +10,39 @@ class TimelinesChannelTest < ActionCable::Channel::TestCase
   end
 
   test "transmit timelines when subscribed" do
-    subscribe channel: "timelines_channel"
+    subscribe channel: "user_#{@user.id}_timelines_channel"
 
     assert subscription.confirmed?
     assert_equal "subscribe", transmissions.last["event"]
   end
 
   test "#create_timeline" do
-    subscribe channel: "timelines_channel"
+    subscribe channel: "user_#{@user.id}_timelines_channel", user_id: @user.id
 
-    assert_broadcasts("timelines_channel", 1) do
+    assert_broadcasts("user_#{@user.id}_timelines_channel", 1) do
       perform :create_timeline,  { timeline: { description: "今は勉強中です" } }
     end
 
-    assert_broadcasts("user_#{@user.id}_timelines_channel", 1)
+    assert_broadcasts("timelines_channel", 1)
   end
 
   test "#update_timeline" do
-    subscribe channel: "timelines_channel"
+    subscribe channel: "user_#{@user.id}_timelines_channel", user_id: @user.id
 
-    assert_broadcasts("timelines_channel", 1) do
+    assert_broadcasts("user_#{@user.id}_timelines_channel", 1) do
       perform :update_timeline, { id: @timeline.id,  timeline: { description: "今はミーティング中です" } }
     end
 
-    assert_broadcasts("user_#{@user.id}_timelines_channel", 1)
+    assert_broadcasts("timelines_channel", 1)
   end
 
   test "#delete_timeline" do
-    subscribe channel: "timelines_channel"
+    subscribe channel: "user_#{@user.id}_timelines_channel", user_id: @user.id
 
-    assert_broadcasts("timelines_channel", 1) do
+    assert_broadcasts("user_#{@user.id}_timelines_channel", 1) do
       perform :delete_timeline,  { id: @timeline.id }
     end
 
-    assert_broadcasts("user_#{@user.id}_timelines_channel", 1)
+    assert_broadcasts("timelines_channel", 1)
   end
 end
