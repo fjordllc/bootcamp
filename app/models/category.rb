8425715994df864661df8c5a1bb06ceeaa06c936
Masaki Komagata <komagata@gmail.com>
2020-07-10 19:01:21 +0900

    modelの親クラスをApplicationRecordに変更
    Railsのバージョンアップ時に修正し忘れていたため。
    また、共通の処理が必要になったため。

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Category < ActiveRecord::Base
+class Category < ApplicationRecord
   has_many :practices, -> { order(:position) }
   has_and_belongs_to_many :courses, dependent: :destroy
   validates :name, presence: true
