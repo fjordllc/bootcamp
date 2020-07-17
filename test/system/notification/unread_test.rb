410612cd000b4862cb39e668850d971b7c205e0f
mh-mobile <mh.mobiler@gmail.com>
2020-07-17 13:08:22 +0900

    未チェックの日報と提出物を一括で開く処理を実装

--- test/system/notification/unread_test.rb
+++ test/system/notification/unread_test.rb
@@ -9,4 +9,15 @@ class Notification::UnreadTest < ApplicationSystemTestCase
     visit "/notifications/unread"
     assert_equal "未読の通知 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
   end
+
+  test "non-mentor can not see a button to open all unread notifications" do
+    visit "/notifications/unread"
+    assert_no_button "未読の通知を一括で開く"
+  end
+
+  test "mentor can see a button to open to open all unread notifications" do
+    login_user "komagata", "testtest"
+    visit "/notifications/unread"
+    assert_button "未読の通知を一括で開く"
+  end
 end
