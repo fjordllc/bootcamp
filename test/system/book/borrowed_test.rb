c7ace1854f4c98a5f9f846cf04761377b809c922
mh-mobile <mh.mobiler@gmail.com>
2020-07-24 13:33:27 +0900

    貸出中の書籍一覧のシステムテストを追加

---
+++ test/system/book/borrowed_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class Book::BorrwedTest < ApplicationSystemTestCase
+  setup { login_user "hatsuo", "testtest" }
+
+  test "show listing borrowed books" do
+    visit "/books/borrowed"
+    assert_equal "貸出中の書籍一覧 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+  end
+end
