56b941024ec664726436a57bb2064e5c35f159df
hiro_f_f <hiro.vodka06@gmail.com>
2020-07-28 16:06:56 +0900

    クエリ文字列をhashで保存する

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,6 +2,13 @@
 
 module EventsHelper
   def google_calendar_link(event)
-    "http://www.google.com/calendar/render?action=TEMPLATE&text=#{event.title}&dates=#{event.start_at.strftime("%Y%m%dT%H%M%S")}/#{event.end_at.strftime("%Y%m%dT%H%M%S")}&details=https://bootcamp.fjord.jp/events/#{event.id}"
+    event_hash =
+    {
+      :action => "TEMPLATE", 
+      :text => event.title, 
+      :dates => "#{event.start_at.strftime("%Y%m%dT%H%M%S")}/#{event.end_at.strftime("%Y%m%dT%H%M%S")}", 
+      :details => "https://bootcamp.fjord.jp/events/#{event.id}"
+    }.to_query
+    "http://www.google.com/calendar/render?#{event_hash}"
   end
 end
