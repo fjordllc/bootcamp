83bd1b9018befe883e580ca4e001377c7352aad3
hiro_f_f <hiro.vodka06@gmail.com>
2020-07-28 19:53:46 +0900

    変数名を修正

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,13 +2,13 @@
 
 module EventsHelper
   def google_calendar_link(event)
-    event_hash =
+    event_query_string=
     {
       :action => "TEMPLATE", 
       :text => event.title, 
       :dates => "#{event.start_at.strftime("%Y%m%dT%H%M%S")}/#{event.end_at.strftime("%Y%m%dT%H%M%S")}", 
       :details => "https://bootcamp.fjord.jp/events/#{event.id}"
     }.to_query
-    "http://www.google.com/calendar/render?#{event_hash}"
+    "http://www.google.com/calendar/render?#{event_query_string}"
   end
 end
