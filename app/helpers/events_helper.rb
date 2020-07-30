0e492c304625be9d2be3a2043234d94f03fdae8e
hiro_f_f <hiro.vodka06@gmail.com>
2020-07-30 14:27:28 +0900

    rubocopでコード修正

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,12 +2,12 @@
 
 module EventsHelper
   def google_calendar_url(event)
-    event_query_string=
+    event_query_string =
     {
-      :action => "TEMPLATE", 
-      :text => event.title, 
-      :dates => "#{event.start_at.strftime("%Y%m%dT%H%M%S")}/#{event.end_at.strftime("%Y%m%dT%H%M%S")}", 
-      :details => "https://bootcamp.fjord.jp/events/#{event.id}"
+      action: "TEMPLATE",
+      text: event.title,
+      dates: "#{event.start_at.strftime("%Y%m%dT%H%M%S")}/#{event.end_at.strftime("%Y%m%dT%H%M%S")}",
+      details: "https://bootcamp.fjord.jp/events/#{event.id}"
     }.to_query
     "http://www.google.com/calendar/render?#{event_query_string}"
   end
