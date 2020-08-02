6fa1f08611b196859249c9bf48bc04690aa1d7c5
kota kanazawa <kota0818@gmail.com>
2020-08-02 15:18:56 +0900

    モデルテストにvcr導入

--- test/models/amazon_test.rb
+++ test/models/amazon_test.rb
@@ -4,14 +4,20 @@ class AmazonTest < ActiveSupport::TestCase
   PARTNER_TAG = ENV['AMAZON_PARTNER_TAG']
 
   setup do
-    @amazon = Amazon.new("B07JHQ9B5T")
+    VCR.use_cassette("response") do
+      @amazon = Amazon.new("B07JHQ9B5T")
+    end
   end
 
-  test "image_url" do
-    assert_equal @amazon.image_url, "https://m.media-amazon.com/images/I/51EJg7Gnr0L._SL75_.jpg"
+  test "it should return image_url" do
+    VCR.use_cassette("response") do
+      assert_equal @amazon.image_url, "https://m.media-amazon.com/images/I/51EJg7Gnr0L._SL75_.jpg"
+    end
   end
 
-  test "page_url" do
-    assert_equal @amazon.page_url, "https://www.amazon.co.jp/dp/B07JHQ9B5T?tag=#{PARTNER_TAG}&linkCode=ogi&th=1&psc=1"
+  test "it should return page_url" do
+    VCR.use_cassette("response") do
+      assert_equal @amazon.page_url, "https://www.amazon.co.jp/dp/B07JHQ9B5T?tag=#{PARTNER_TAG}&linkCode=ogi&th=1&psc=1"
+    end
   end
 end
