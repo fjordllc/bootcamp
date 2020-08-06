0e1555879831e996adb2adc248209deb1b54d7f1
mh-mobile <mh.mobiler@gmail.com>
2020-08-06 19:00:25 +0900

    tagging UIのtagifyライブラリにタグ補完時のIMEのバグが存在するため、vue-tags-inputライブラリに差し替え

--- app/javascript/tags-input.vue
+++ app/javascript/tags-input.vue
@@ -1,41 +1,45 @@
 <template lang="pug">
   div
-    Tags.a-xs-text-input(:initialValue="tagsInitialValue" :onChange="onTagsChange" :settings="this.tagifySettings")
+    vue-tags-input(v-model="inputTag" :tags="tags" :autocomplete-items="filteredTags" @tags-changed="update" placeholder="")
     input(type="hidden" :value="tagsValue" :name="tagsParamName")
 </template>
 
 <script>
-import Tags from '@yaireo/tagify/dist/tagify.vue'
+import VueTagsInput from '@johmun/vue-tags-input'
 
 export default {
   props: ['tagsInitialValue', 'tagsParamName', 'taggableType'],
-  components: { Tags },
+  components: { VueTagsInput },
   data() {
     return {
+      inputTag: '',
+      tags: [],
       tagsValue: '',
-      tagifySettings: {
-        whitelist: [],
-        dropdown: {
-          enabled: 1,
-          fuzzySearch: true,
-          position: 'text',
-          caseSensitive: false
-        }
-      },
-      suggestions: []
+      autocompleteTags: [],
     };
   },
   methods: {
-    onTagsChange(e) {
-      const changedValue = e.target.value
-      this.tagsValue = changedValue !== '' ? this.parseTagsJSON(changedValue) : ''
+    update(newTags) {
+      this.tags = newTags
+      this.tagsValue = this.joinTags(newTags)
     },
-    parseTagsJSON(value) {
-      return JSON.parse(value).map(tag => tag.value).join(',')
+    joinTags(value) {
+      return value.map(tag => tag.text).join(',')
     },
+    parseTags(value) {
+      if (value === '') return [];
+      
+      return value.split(',').map(value => {
+        return {
+          text: value,
+          tiClasses: ["ti-valid"]
+        }
+     })
+    }
   },
   mounted() {
     this.tagsValue = this.tagsInitialValue
+    this.tags = this.parseTags(this.tagsInitialValue)
 
     fetch(`/api/tags.json?taggable_type=${this.taggableType}`, {
       method: 'GET',
@@ -49,21 +53,70 @@ export default {
         return response.json()
       })
       .then(json => {
-        this.suggestions = json
+        const suggestions = json.map(tag => { 
+          return {
+            text: tag.value 
+          }
+        })
 
-        this.tagifySettings.whitelist.length = 0;
-        this.tagifySettings.whitelist.push(...this.suggestions);
+        this.autocompleteTags.length = 0;
+        this.autocompleteTags.push(...suggestions);
       })
       .catch(error => {
         console.warn('Failed to parsing', error)
       })
   },
+  computed: {
+    filteredTags() {
+      return this.autocompleteTags.filter(tag => {
+        return tag.text.toLowerCase().indexOf(this.inputTag.toLowerCase()) !== -1;
+      });
+    },
+  },
 };
 </script>
 
-<style lang="scss">
-.tagify {
-  margin-bottom: 3em;
-  background-color: #ffffff;
-}
+<style>
+
+  .vue-tags-input {
+    max-width: 100% !important;
+  }
+
+  .vue-tags-input .ti-input {
+    padding: 4px 10px;
+    transition: border-bottom 200ms ease;
+  }
+
+  .vue-tags-input .ti-item {
+    padding: 4px 6px;
+  }
+
+  .vue-tags-input .ti-item.ti-selected-item {
+    background: #4638a0;
+    color: #ffffff;
+    padding: 4px 6px;
+  }
+
+  .vue-tags-input .ti-tag {
+    position: relative;
+    background: #dfdfdf;
+    color: #000000;
+    padding: 6px 10px;
+  }
+
+  .vue-tags-input .ti-tag:after {
+    transition: transform .2s;
+    position: absolute;
+    content: '';
+    height: 2px;
+    width: 108%;
+    left: -4%;
+    top: calc(50% - 1px);
+    background-color: #000;
+    transform: scaleX(0);
+  }
+
+  .vue-tags-input .ti-deletion-mark:after {
+    transform: scaleX(1);
+  }
 </style>
