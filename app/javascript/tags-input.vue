a63027a30bea9785620efa72a196255ec800ba28
mh-mobile <mh.mobiler@gmail.com>
2020-08-06 12:21:26 +0900

    タグの候補一覧の表示処理を追加

--- app/javascript/tags-input.vue
+++ app/javascript/tags-input.vue
@@ -1,18 +1,28 @@
 <template lang="pug">
   div
-    Tags.a-xs-text-input(:initialValue="tagsInitialValue" :onChange="onTagsChange")
+    Tags.a-xs-text-input(:initialValue="tagsInitialValue" :onChange="onTagsChange" :settings="this.tagifySettings")
     input(type="hidden" :value="tagsValue" :name="tagsParamName")
 </template>
 
 <script>
-import Tags from "@yaireo/tagify/dist/tagify.vue";
+import Tags from '@yaireo/tagify/dist/tagify.vue'
 
 export default {
-  props: ['tagsInitialValue', 'tagsParamName'],
+  props: ['tagsInitialValue', 'tagsParamName', 'taggableType'],
   components: { Tags },
   data() {
     return {
-      tagsValue: ''
+      tagsValue: '',
+      tagifySettings: {
+        whitelist: [],
+        dropdown: {
+          enabled: 1,
+          fuzzySearch: true,
+          position: 'text',
+          caseSensitive: false
+        }
+      },
+      suggestions: []
     };
   },
   methods: {
@@ -21,20 +31,39 @@ export default {
       this.tagsValue = changedValue !== '' ? this.parseTagsJSON(changedValue) : ''
     },
     parseTagsJSON(value) {
-      return JSON.parse(value).map(tag => tag.value).join(',') 
-    }
+      return JSON.parse(value).map(tag => tag.value).join(',')
+    },
   },
   mounted() {
     this.tagsValue = this.tagsInitialValue
-  }
+
+    fetch(`/api/tags.json?taggable_type=${this.taggableType}`, {
+      method: 'GET',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      credentials: 'same-origin',
+      redirect: 'manual'
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        this.suggestions = json
+
+        this.tagifySettings.whitelist.length = 0;
+        this.tagifySettings.whitelist.push(...this.suggestions);
+      })
+      .catch(error => {
+        console.warn('Failed to parsing', error)
+      })
+  },
 };
 </script>
 
 <style lang="scss">
-
 .tagify {
   margin-bottom: 3em;
   background-color: #ffffff;
 }
-
 </style>
