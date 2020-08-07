5fe9087be0ef8df1b2beee8739eeb5154a62db67
hiro_f_f <hiro.vodka06@gmail.com>
2020-08-07 13:18:14 +0900

    Docsを投稿すると通知する

--- app/models/page.rb
+++ app/models/page.rb
@@ -8,6 +8,8 @@ class Page < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   paginates_per 20
+  alias_method :sender, :user
+  after_create PageCallbacks.new
 
   columns_for_keyword_search :title, :body
 
