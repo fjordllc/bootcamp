81d847b71cca0735e06bf59997c785beb471d584
hiro_f_f <hiro.vodka06@gmail.com>
2020-08-07 22:53:21 +0900

    システムテスト追加

--- app/models/page_callbacks.rb
+++ app/models/page_callbacks.rb
@@ -12,19 +12,6 @@ class PageCallbacks
   end
 
   private
-  
-    def notify_to_slack(page)
-      link = "<#{url_for(page)}|#{page.title}>"
-      SlackNotification.notify "#{link}",
-        username: "#{page.user.login_name} (#{page.user.full_name})",
-        icon_url: page.user.avatar_url,
-        channel: "#general",
-        attachments: [{
-          fallback: "page body.",
-          text: page.body
-        }]
-    end
-
     def send_notification(page)
       receivers = User.where(retired_on: nil, graduated_on: nil, adviser: false, trainee: false)
       receivers.each do |receiver|
@@ -32,6 +19,5 @@ class PageCallbacks
           NotificationFacade.create_page(page, receiver)
         end
       end
-      notify_to_slack(page)
     end
 end
--- test/system/notification/pages_test.rb
+++ test/system/notification/pages_test.rb
@@ -21,5 +21,27 @@ class Notification::PagesTest < ApplicationSystemTestCase
     login_user "machida", "testtest"
     first(".test-bell").click
     assert_text "komagataさんがDocsにDocsTestを投稿しました。"
+
+    logout
+    login_user "yameo", "testtest"
+    visit "/notifications"
+    assert_no_text "komagataさんがDocsにDocsTestを投稿しました。"
+  end
+
+  test "don't notify when page is WIP" do
+    login_user "komagata", "testtest"
+    visit "/pages"
+    click_link "新規ページ"
+
+    within(".form") do
+      fill_in("page[title]", with: "DocsTest")
+      fill_in("page[body]", with: "DocsTestBody")
+    end
+    click_button "WIP"
+
+    logout
+    login_user "hatsuno", "testtest"
+    visit "/notifications"
+    assert_no_text "komagataさんがDocsにDocsTestを投稿しました。"
   end
 end
