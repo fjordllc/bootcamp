80df2d8b3ae957bd74bb1a9ed13873535c78f17d
Masaki Komagata <komagata@gmail.com>
2020-08-27 15:03:07 +0900

    sendgridの設定を変更
    herokuのaddonから単体のアカウントに変わるため

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
-    domain: "heroku.com",
+    domain: "bootcamp.fjord.jp",
     enable_starttls_auto: true
   }
 
