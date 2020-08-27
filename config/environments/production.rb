188e2da653a0a401e9839b8a0d5685d80636ed85
Masaki Komagata <komagata@gmail.com>
2020-08-27 17:19:53 +0900

    port編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
 
   ActionMailer::Base.smtp_settings = {
     address: "smtp.sendgrid.net",
-    port: "465",
+    port: "587",
     authentication: :plain,
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
