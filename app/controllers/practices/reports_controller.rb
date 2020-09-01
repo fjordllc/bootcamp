ad0cc404172fc853f647c9d4f3216f41dfac7456
Masaki Komagata <komagata@gmail.com>
2020-09-01 13:45:10 +0900

    プラクティスごとの日報、Q&A、提出物一覧に認証を追加
    そもそも認証を必須にする必要があるし、
    今までは認証が必要な処理でエラーを出していた。

--- app/controllers/practices/reports_controller.rb
+++ app/controllers/practices/reports_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Practices::ReportsController < ApplicationController
+  before_action :require_login
+
   def index
     @practice = Practice.find(params[:practice_id])
     @reports = @practice.reports.eager_load(:user, :comments).order(updated_at: :desc, id: :desc).page(params[:page])
