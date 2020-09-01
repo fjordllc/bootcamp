2557690d942ce3ccc743c7651f8e2a9d5910fe18
Masaki Komagata <komagata@gmail.com>
2020-09-01 20:51:52 +0900

    practiceが複数categoryを持つようにした

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :practices, -> { order(:position) }
   has_and_belongs_to_many :courses, dependent: :destroy
+  has_and_belongs_to_many :practices, dependent: :destroy
   validates :name, presence: true
   validates :slug, presence: true
   acts_as_list
