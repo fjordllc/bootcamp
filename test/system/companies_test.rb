494d3c46023390e210114edac203c8eded00c8a2
gogutan <50437473+gogutan@users.noreply.github.com>
2020-09-05 17:33:30 +0900

    企業個別ページのテストを追加

---
+++ test/system/companies_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class CompaniesTest < ApplicationSystemTestCase
+  setup { login_user "komagata", "testtest" }
+
+  test "show company information" do
+    visit "/companies/#{companies(:company_1).id}"
+    assert_equal "FJORD, LLCの会社情報 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+  end
+
+  test "show link to website if company has" do
+    visit "/companies/#{companies(:company_1).id}"
+    within ".company_website" do
+      assert_link "FJORD, LLCウェブサイト", href: "https://fjord.jp"
+    end
+  end
+end
