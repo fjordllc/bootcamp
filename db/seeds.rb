010d2ec046c562355cd3e515da251b899e6174b3
sdk-quadra <sadaka556@gmail.com>
2020-09-10 15:40:09 +0900

    学習時間中央値の表示をmodal化

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-Rake::Task["db:fixtures:load"].execute
+Rake::Task["db:fixtures:loa"].execute
+Rake::Task["bootcamp:statistics:save_learning_minute_statistics"].execute
