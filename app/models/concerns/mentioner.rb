f9e90a2f0c0fc950a04723e5acf68ab4b8094ac6
shibaaa <k.shibata9447@gmail.com>
2020-09-20 18:06:33 +0900

    メンション通知メールの件名にメンションの詳細が表示されるようにした

--- app/models/concerns/mentioner.rb
+++ app/models/concerns/mentioner.rb
@@ -19,13 +19,25 @@ module Mentioner
     when Product
       "#{self.user.login_name}さんの「#{self.practice[:title]}」の提出物"
     when Report
-      "#{self.user.login_name}さんの「#{self[:title]}」の日報"
+      "#{self.user.login_name}さんの日報「#{self[:title]}」"
     when Comment
-      "#{self.commentable.user.login_name}さんの#{self.commentable.title}のコメント"
+      select_message(self.commentable.class, self.commentable) + "へのコメント"
     end
   end
 
   def body
     self[:body] || self[:description]
   end
+
+  private
+
+    def select_message(commentable_class, commentable)
+      {
+        Report: "#{commentable.user.login_name}さんの日報「#{self.commentable.title}」",
+        Product: "#{commentable.user.login_name}さんの#{self.commentable.title}",
+        Event: "イベント「#{commentable.title}」",
+        Page: "Docs「#{commentable.title}」",
+        Announcement: "お知らせ「#{commentable.title}」"
+      }[:"#{commentable_class}"]
+    end
 end
