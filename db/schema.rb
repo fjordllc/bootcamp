2e8919b96784647a33ccc902f469830cc16a00c0
lamp7600 <newlamp7600@gmail.com>
2020-09-21 20:49:31 +0900

    announcementsテーブルのtargetカラムのデフォルト値を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_022128) do
+ActiveRecord::Schema.define(version: 2020_09_21_114131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
