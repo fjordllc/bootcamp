35184ce33a220a010add2d79211e06a0b863b5f6
lamp7600 <newlamp7600@gmail.com>
2020-09-22 15:11:25 +0900

    announcementsテーブルのtargetカラムのデフォルト値を0に戻す

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_114131) do
+ActiveRecord::Schema.define(version: 2020_08_09_022128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_114131) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "target", default: 1, null: false
+    t.integer "target", default: 0, null: false
     t.index ["user_id"], name: "index_announcements_on_user_id"
   end
 
