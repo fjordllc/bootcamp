f8270e8015bce746209ce0aefea844fa070703a8
lamp7600 <newlamp7600@gmail.com>
2020-09-22 19:07:22 +0900

    404から日報がない旨を表示するだけに変更

--- app/controllers/reports/unchecked_controller.rb
+++ app/controllers/reports/unchecked_controller.rb
@@ -3,8 +3,5 @@
 class Reports::UncheckedController < ApplicationController
   def index
     @reports = Report.unchecked.not_wip.list.page(params[:page])
-    if @reports.page(params[:page]).count == 0
-      render file: "public/404.html", status: 404, layout: false
-    end
   end
 end
