9cff7ec144b82b12d49d0d95d3e92fec34812c67
lamp7600 <newlamp7600@gmail.com>
2020-09-22 15:11:25 +0900

    announcementsテーブルのtargetカラムのデフォルト値を0に戻す

--- db/migrate/20200921114131_change_target_default_on_announcements.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeTargetDefaultOnAnnouncements < ActiveRecord::Migration[6.0]
-  def change
-    change_column_default :announcements, :target, from: 0, to: 1
-  end
-end
