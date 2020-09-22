cad73327bbd0d5ada8f1580e4e82a325c6158207
lamp7600 <newlamp7600@gmail.com>
2020-09-22 17:44:17 +0900

    未読の日報2ページ目などでデータがない場合は404にする

--- app/controllers/reports/unchecked_controller.rb
+++ app/controllers/reports/unchecked_controller.rb
@@ -3,5 +3,8 @@
 class Reports::UncheckedController < ApplicationController
   def index
     @reports = Report.unchecked.not_wip.list.page(params[:page])
+    if @reports.page(params[:page]).count == 0
+      render file: "public/404.html", status: 404, layout: false
+    end
   end
 end
