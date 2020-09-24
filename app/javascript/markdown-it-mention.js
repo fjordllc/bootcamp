f6a9f14e36fb36bfa2a947e57f9211ce8fbb301c
Masaki Komagata <komagata@gmail.com>
2020-09-24 14:46:54 +0900

    mentorへのメンション記法を追加

--- app/javascript/markdown-it-mention.js
+++ app/javascript/markdown-it-mention.js
@@ -1,7 +1,7 @@
 import MarkdownItRegexp from 'markdown-it-regexp'
 
 export default MarkdownItRegexp(
-  /@([a-zA-Z0-9_-]+)/,
+  /@(?!mentor)([a-zA-Z0-9_-]+)/,
 
   (match, _utils) => {
     return `<a href="/users/${match[1]}">${match[0]}</a>`
