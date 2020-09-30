a9525197b3b8cb8f99921be4d519cdae7c78cfa9
shibaaa <k.shibata9447@gmail.com>
2020-09-30 17:15:56 +0900

    テストの修正とディレクトリを変更

---
+++ test/system/practice/memos_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class Practice::MemoTest < ApplicationSystemTestCase
+  test "update mentor memo without page transitions" do
+    login_user "komagata", "testtest"
+    visit "/products/#{products(:product_2).id}"
+    wait_for_vuejs
+    click_button "編集"
+    fill_in("js-practice-memo", with: "メンター向けメモをページ遷移せず編集")
+    click_button "保存する"
+    wait_for_vuejs
+    assert_text "メンター向けメモをページ遷移せず編集"
+  end
+end
