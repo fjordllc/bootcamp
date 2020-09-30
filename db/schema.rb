de5568f1895961f96c0b98473826fcb6f79a7dbb
lamp7600 <newlamp7600@gmail.com>
2020-09-30 16:07:37 +0900

    eventsテーブルにpublished_atカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_015349) do
+ActiveRecord::Schema.define(version: 2020_09_30_065746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,6 +146,7 @@ ActiveRecord::Schema.define(version: 2020_09_27_015349) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "published_at"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
