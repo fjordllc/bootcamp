d79cbd793607f474dae72d85adab4def47c93033
lamp7600 <newlamp7600@gmail.com>
2020-09-30 21:21:13 +0900

    eventsテーブルにwipカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_065746) do
+ActiveRecord::Schema.define(version: 2020_09_30_121031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_065746) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "published_at"
+    t.boolean "wip", default: false, null: false
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
