e710240895b4abe0ddee5d100ba809d5a35a5f93
lamp7600 <newlamp7600@gmail.com>
2020-10-01 10:39:31 +0900

    wipを実装

--- db/migrate/20200930065746_add_published_at_to_events.rb
+++ db/migrate/20200930065746_add_published_at_to_events.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddPublishedAtToEvents < ActiveRecord::Migration[6.0]
   def change
     add_column :events, :published_at, :datetime
--- db/migrate/20200930121031_add_wip_to_events.rb
+++ db/migrate/20200930121031_add_wip_to_events.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddWipToEvents < ActiveRecord::Migration[6.0]
   def change
     add_column :events, :wip, :boolean, null: false, default: false
