ea1dfcdb0faf961c30e7c46d00a2b4246f7236e9
Masaki Komagata <komagata@gmail.com>
2020-10-01 14:30:09 +0900

    卒業時に課金を停止するようにした

--- app/controllers/graduation_controller.rb
+++ app/controllers/graduation_controller.rb
@@ -5,6 +5,8 @@ class GraduationController < ApplicationController
 
   def update
     if @user.update(graduated_on: Date.current)
+      Subscription.new.destroy(@user.subscription_id) if @user.subscription_id
+
       redirect_to admin_users_url, notice: "ユーザー情報を更新しました。"
     else
       redirect_to admin_users_url, alert: "ユーザー情報の更新に失敗しました"
