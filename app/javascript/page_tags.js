0c68d029546505ac11e9ee86ebac194fb8a73ad3
shibaaa <k.shibata9447@gmail.com>
2020-10-05 17:59:01 +0900

    タイポを修正

--- app/javascript/page_tags.js
+++ app/javascript/page_tags.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import PageTags from './page_tags.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const tagsInputElm = document.getElementById('js-page-tags')
-  if (tagsInputElm) {
-    const tagsInitialValue = tagsInputElm.getAttribute('data-tags-initial-value')
-    const tagsParamName = tagsInputElm.getAttribute('data-tags-param-name')
-    const pageId = tagsInputElm.getAttribute('data-page-id')
+  const pageTagsElm = document.getElementById('js-page-tags')
+  if (pageTagsElm) {
+    const tagsInitialValue = pageTagsElm.getAttribute('data-tags-initial-value')
+    const tagsParamName = pageTagsElm.getAttribute('data-tags-param-name')
+    const pageId = pageTagsElm.getAttribute('data-page-id')
 
     new Vue({
       render: h => h(PageTags, {
