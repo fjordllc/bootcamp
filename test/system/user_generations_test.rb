a93d2642d77e55a4902d17e0f8edbe08c038bbf2
takopom <ymc722@gmail.com>
2020-10-11 02:28:26 +0900

    リタイア以外の同期ユーザを表示する

--- test/system/user_generations_test.rb
+++ test/system/user_generations_test.rb
@@ -7,5 +7,7 @@ class UserGenerationsTest < ApplicationSystemTestCase
     login_user "kimura", "testtest"
     visit user_generation_path(users(:kimura).generation)
     assert_equal "#{users(:kimura).generation}期のユーザー一覧 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
+    assert_text users(:kimura).name
+    assert_text users(:komagata).name
   end
 end
