491b2fc064e805ccd8a50c134eb0457c8221ca80
ima1zumi <mariimaizumi5@gmail.com>
2020-10-15 16:49:53 +0900

    Followingモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_10_14_114543) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -148,6 +147,16 @@ ActiveRecord::Schema.define(version: 2020_10_14_114543) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "wip", default: false, null: false
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_followings_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_followings_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_followings_on_follower_id"
   end
 
   create_table "footprints", id: :serial, force: :cascade do |t|
