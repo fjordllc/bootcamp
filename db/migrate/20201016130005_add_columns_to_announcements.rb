1b1f6ac9aca0ade7fb3304f03b37e4065dc2e765
KishiKyousuke <carp011808pg@icloud.com>
2020-10-18 00:27:55 +0900

    announcements_testを修正

--- db/migrate/20201016130005_add_columns_to_announcements.rb
+++ db/migrate/20201016130005_add_columns_to_announcements.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddColumnsToAnnouncements < ActiveRecord::Migration[6.0]
   def change
     add_column :announcements, :wip, :boolean, default: false, null: false
