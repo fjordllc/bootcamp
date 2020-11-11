4d0dc83aafb573cfe326e0967152382129f323fd
mashoo <mashio.tech@gmail.com>
2020-11-11 22:12:21 +0900

    コントローラーにbefore_action :require_staff_loginを追加

--- app/controllers/products/not_responded_controller.rb
+++ app/controllers/products/not_responded_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Products::NotRespondedController < ApplicationController
+  before_action :require_staff_login
   def index
     @products = Product.not_responded_products.list.page(params[:page])
   end
