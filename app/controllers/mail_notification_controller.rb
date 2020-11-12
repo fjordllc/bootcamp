faad3dacb756fde26aa294b219a98d17cc42428b
hogucc <uku.h1r8@gmail.com>
2020-11-12 19:09:17 +0900

    mail_notification_controllerでユーザーが取得できなかった場合にログインページへリダイレクトする処理を追加

--- app/controllers/mail_notification_controller.rb
+++ app/controllers/mail_notification_controller.rb
@@ -3,6 +3,12 @@
 class MailNotificationController < ApplicationController
   def update
     user = User.find_by(unsubscribe_email_token: params[:token])
+
+    if user.blank?
+      not_authenticated
+      return
+    end
+
     user.mail_notification = false
     user.save!
     render :show
