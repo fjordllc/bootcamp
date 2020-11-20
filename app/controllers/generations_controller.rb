c3178ad71d01959f0bf890278f3984de6eb94f2c
hogucc <uku.h1r8@gmail.com>
2020-11-20 13:35:59 +0900

    generationsコントローラーのindexアクションを実装

--- app/controllers/generations_controller.rb
+++ app/controllers/generations_controller.rb
@@ -7,4 +7,15 @@ class GenerationsController < ApplicationController
     @generation = params[:id].to_i
     @users = Generation.new(@generation).users
   end
+
+  def index
+    @generations = (1..max_generation_number).map { |n| Generation.new(n) }
+  end
+
+  private
+
+    def max_generation_number
+      now_time = Time.now
+      (now_time.year - 2013) * 4 + (now_time.month + 2) / 3
+    end
 end
