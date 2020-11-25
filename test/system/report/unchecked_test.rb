402c9ae0d77e79e08d17f293013d1191a9835464
MashioSano <mashio.tech@gmail.com>
2020-11-25 19:50:40 +0900

    各ページのテストを追加

--- test/system/report/unchecked_test.rb
+++ test/system/report/unchecked_test.rb
@@ -6,13 +6,14 @@ class Report::UncheckedTest < ApplicationSystemTestCase
   setup { login_user "hatsuo", "testtest" }
 
   test "show listing unchecked reports" do
+    login_user "komagata", "testtest"
     visit "/reports/unchecked"
     assert_equal "未チェックの日報 | FJORD BOOT CAMP（フィヨルドブートキャンプ）", title
   end
 
-  test "non-mentor can not see a button to open all unchecked reports" do
+  test "non-staff user can not see listing unchecked reports" do
     visit "/reports/unchecked"
-    assert_no_button "未チェックの日報を一括で開く"
+    assert_text "管理者・アドバイザー・メンターとしてログインしてください"
   end
 
   test "mentor can see a button to open all unchecked reports" do
