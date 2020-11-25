e935859c18e0f56ac39d38ad9d13dbd97fa56abb
Chihiro <Chi.r.r.333@gmail.com>
2020-11-25 20:07:07 +0900

    他人のプラクティスの進捗をチェックボックスで表示/非表示に切り替えられるようにした

--- app/javascript/checkBox.js
+++ app/javascript/checkBox.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const localStorage = window.localStorage
   document.querySelectorAll('input').forEach((e) => {
-    if (e.checked === true) {
+    const categoriesItems = e.closest('.categories-items')
+    if (localStorage.getItem('displayProgress') === 'off' && categoriesItems) {
+      e.closest('.categories-items').classList.add('js-is-hidden-users')
+      document.getElementById('displayProgress').checked = false
+    } else if (e.checked === true && categoriesItems) {
+      e.closest('.categories-items').classList.add('js-is-show-users')
+    } else if (e.checked === true) {
       e.parentNode.classList.add('is-checked')
     }
   })
@@ -10,8 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
       t = c.target.type
       chk = value.checked
       if (t === 'checkbox') {
-        if (chk === true) {
+        const categoriesItems = value.closest('.categories-items')
+        if (chk === true && categoriesItems) {
+          value.closest('.categories-items').classList.remove('js-is-hidden-users')
+          value.closest('.categories-items').classList.add('js-is-show-users')
+          localStorage.removeItem('displayProgress')
+        } else if (chk === true) {
           value.parentNode.classList.add('is-checked')
+        } else if (chk === false && categoriesItems) {
+          value.closest('.categories-items').classList.remove('js-is-show-users')
+          value.closest('.categories-items').classList.add('js-is-hidden-users')
+          localStorage.setItem('displayProgress', 'off')
         } else {
           value.parentNode.classList.remove('is-checked')
         }
