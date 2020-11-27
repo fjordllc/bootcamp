2245e37e5d6c4681991c3f134089b22b3a2ff777
masuyama13 <masterxwebm13@gmail.com>
2020-11-27 16:09:48 +0900

    モデルテストのWatchの増加数検証部分をまとめた

--- test/models/watch_test.rb
+++ test/models/watch_test.rb
@@ -9,15 +9,11 @@ class WatchTest < ActiveSupport::TestCase
     report = reports(:report_19)
     product = products(:product_11)
     event = events(:event_1)
-    Watch.migrate_data
-    assert Watch.find_by(user: hatsuno, watchable: report)
-    assert Watch.find_by(user: hatsuno, watchable: product)
-    assert Watch.find_by(user: komagata, watchable: event)
-  end
-
-  test "migrate_data実行後は作成者自身がWatchしていない日報・提出物・イベントの数だけWatchが増える" do
     assert_difference("Watch.count", 46) do
       Watch.migrate_data
     end
+    assert Watch.find_by(user: hatsuno, watchable: report)
+    assert Watch.find_by(user: hatsuno, watchable: product)
+    assert Watch.find_by(user: komagata, watchable: event)
   end
 end
