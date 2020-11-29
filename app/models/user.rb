42e66f0bdc575100dcb9e3de58281e48634b64f8
rono23 <rono23@gmail.com>
2020-11-29 12:43:58 +0900

    RuboCopのLayout/HeredocIndentationの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -320,13 +320,13 @@ class User < ApplicationRecord
   end
 
   def total_learning_time
-    sql = <<-SQL
-SELECT
-  SUM(EXTRACT(epoch from learning_times.finished_at - learning_times.started_at) / 60 / 60) AS total
-FROM
-  learning_times JOIN reports ON learning_times.report_id = reports.id
-WHERE
-  reports.user_id = :user_id
+    sql = <<~SQL
+      SELECT
+        SUM(EXTRACT(epoch from learning_times.finished_at - learning_times.started_at) / 60 / 60) AS total
+      FROM
+        learning_times JOIN reports ON learning_times.report_id = reports.id
+      WHERE
+        reports.user_id = :user_id
     SQL
 
     learning_time = LearningTime.find_by_sql([sql, { user_id: id }])
