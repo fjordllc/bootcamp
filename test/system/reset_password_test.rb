be7665f3396f84b985a4bb7e6803849206ed37c5
rono23 <rono23@gmail.com>
2020-11-29 13:41:18 +0900

    RuboCopのLint/AmbiguousRegexpLiteralの修正

--- test/system/reset_password_test.rb
+++ test/system/reset_password_test.rb
@@ -22,12 +22,12 @@ class ResetPasswordTest < ApplicationSystemTestCase
     assert "[Fjord Bootcamp] #{I18n.t("your_password_has_been_reset")}"
     assert users(:komagata).email, mail.to
     assert "from@bootcamp.fjord.jp", mail.from
-    assert_match /こんにちは、#{users(:komagata).name}さん/, mail.body.to_s
-    assert_match /誰かがパスワードの再設定を希望しました/, mail.body.to_s
-    assert_match /あなたが希望したのではないのなら、このメールは無視してください/, mail.body.to_s
+    assert_match(/こんにちは、#{users(:komagata).name}さん/, mail.body.to_s)
+    assert_match(/誰かがパスワードの再設定を希望しました/, mail.body.to_s)
+    assert_match(/あなたが希望したのではないのなら、このメールは無視してください/, mail.body.to_s)
     reset_password_token = users(:komagata).reload.reset_password_token
     assert_match edit_password_reset_path(reset_password_token), mail.body.to_s
-    assert_match /上のリンクにアクセスして新しいパスワードを設定するまで、パスワードは変更されません/, mail.body.to_s
+    assert_match(/上のリンクにアクセスして新しいパスワードを設定するまで、パスワードは変更されません/, mail.body.to_s)
 
     assert_equal "/login", current_path
     assert_text "パスワードの再設定について"
