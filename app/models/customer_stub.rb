9cf1ab332338c42d7fc99a467d140c364190a94a
rono23 <rono23@gmail.com>
2020-11-29 11:57:42 +0900

    RuboCopのLayout/DotPositionの修正

--- app/models/customer_stub.rb
+++ app/models/customer_stub.rb
@@ -6,8 +6,8 @@ module CustomerStub
       WebMock.allow_net_connect!
 
       json = File.read("#{Rails.root}/test/fixtures/files/mock_bodies/customer.json")
-      WebMock.stub_request(:get, "https://api.stripe.com/v1/customers/cus_12345678").
-        to_return(status: 200, body: json)
+      WebMock.stub_request(:get, "https://api.stripe.com/v1/customers/cus_12345678")
+             .to_return(status: 200, body: json)
     end
 
     super
--- app/models/subscription_stub.rb
+++ app/models/subscription_stub.rb
@@ -6,8 +6,8 @@ module SubscriptionStub
       WebMock.allow_net_connect!
 
       json = File.read("#{Rails.root}/test/fixtures/files/mock_bodies/subscription.json")
-      WebMock.stub_request(:get, "https://api.stripe.com/v1/subscriptions/sub_12345678").
-        to_return(status: 200, body: json)
+      WebMock.stub_request(:get, "https://api.stripe.com/v1/subscriptions/sub_12345678")
+             .to_return(status: 200, body: json)
     end
 
     super
