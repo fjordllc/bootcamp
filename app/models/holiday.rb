f4aa89797f4d08503efd450c1d1890ecb5d97f60
rono23 <rono23@gmail.com>
2020-12-03 07:03:15 +0900

    RuboCopのNaming/AccessorMethodNameの修正

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Holiday
-  def self.set_one_month_holidays(beggining_of_this_month)
+  def self.one_month_holidays(beggining_of_this_month)
     holidays = {}
     days_of_this_month = beggining_of_this_month..beggining_of_this_month.end_of_month
     days_of_this_month.each do |one_day|
