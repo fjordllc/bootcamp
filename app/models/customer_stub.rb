547d97073149a2921c8537f84a0e49c2375bb43f
rono23 <rono23@gmail.com>
2020-12-03 08:26:31 +0900

    RuboCopのRails/FilePathの修正

--- app/models/customer_stub.rb
+++ app/models/customer_stub.rb
@@ -5,7 +5,7 @@ module CustomerStub
     if Rails.env.development?
       WebMock.allow_net_connect!
 
-      json = File.read("#{Rails.root}/test/fixtures/files/mock_bodies/customer.json")
+      json = File.read(Rails.root.join("test/fixtures/files/mock_bodies/customer.json"))
       WebMock.stub_request(:get, "https://api.stripe.com/v1/customers/cus_12345678")
              .to_return(status: 200, body: json)
     end
