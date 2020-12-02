da38bd120e516207bab9b3b2faf17c21ede3f0a8
KishiKyousuke <carp011808pg@icloud.com>
2020-12-02 17:16:13 +0900

    WIPでSlackに通知が飛んでしまう不具合を修正
    `announcements_controller.rb`に定義されていたSlack通知のメソッド`notify_to_slack`を、`announcement_callbacks.rb`へ移動しました。

--- app/models/announcement_callbacks.rb
+++ app/models/announcement_callbacks.rb
@@ -4,6 +4,7 @@ class AnnouncementCallbacks
   def after_create(announce)
     return if announce.wip?
 
+    notify_to_slack(announce)
     send_notification(announce)
     announce.published_at = Time.current
     announce.save
@@ -12,6 +13,7 @@ class AnnouncementCallbacks
   def after_update(announce)
     return unless !announce.wip && announce.published_at.nil?
 
+    notify_to_slack(announce)
     send_notification(announce)
     announce.published_at = Time.current
     announce.save
@@ -28,6 +30,21 @@ class AnnouncementCallbacks
     target_users.each do |target|
       NotificationFacade.post_announcement(announce, target) if announce.sender != target
     end
+  end
+
+  def notify_to_slack(announce)
+    path = Rails.application.routes.url_helpers.polymorphic_path(announce)
+    url = "https://bootcamp.fjord.jp#{path}"
+    link = "<#{url}|#{announce.title}>"
+
+    SlackNotification.notify link.to_s,
+                             username: "#{announce.user.login_name} (#{announce.user.name})",
+                             icon_url: announce.user.avatar_url,
+                             channel: '#general',
+                             attachments: [{
+                               fallback: 'announcement description.',
+                               text: announce.description
+                             }]
   end
 
   def delete_notification(announce)
