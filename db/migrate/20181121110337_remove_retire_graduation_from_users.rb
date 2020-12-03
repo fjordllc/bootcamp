12a204bbe9884d88f8efc250d95967764a5c712c
rono23 <rono23@gmail.com>
2020-12-03 11:54:16 +0900

    RuboCopのRails/ReversibleMigrationの修正

--- db/migrate/20181121110337_remove_retire_graduation_from_users.rb
+++ db/migrate/20181121110337_remove_retire_graduation_from_users.rb
@@ -2,9 +2,18 @@
 
 class RemoveRetireGraduationFromUsers < ActiveRecord::Migration[5.2]
   def change
-    change_table :users, bulk: true do |t|
-      t.remove :retire
-      t.remove :graduation
+    reversible do |dir|
+      change_table :users, bulk: true do |t|
+        dir.up do
+          t.remove :retire
+          t.remove :graduation
+        end
+
+        dir.down do
+          t.boolean :retire, null: false, default: false
+          t.boolean :graduation, null: false, default: false
+        end
+      end
     end
   end
 end
--- db/migrate/20190104064709_remove_paperclip_columns.rb
+++ db/migrate/20190104064709_remove_paperclip_columns.rb
@@ -2,18 +2,40 @@
 
 class RemovePaperclipColumns < ActiveRecord::Migration[5.2]
   def change
-    change_table :users, bulk: true do |t|
-      t.remove :face_file_name
-      t.remove :face_content_type
-      t.remove :face_file_size
-      t.remove :face_updated_at
+    reversible do |dir|
+      change_table :users, bulk: true do |t|
+        dir.up do
+          t.remove :face_file_name
+          t.remove :face_content_type
+          t.remove :face_file_size
+          t.remove :face_updated_at
+        end
+
+        dir.down do
+          t.string :face_file_name
+          t.string :face_content_type
+          t.integer :face_file_size
+          t.datetime :face_updated_at
+        end
+      end
     end
 
-    change_table :images, bulk: true do |t|
-      t.remove :image_file_name
-      t.remove :image_content_type
-      t.remove :image_file_size
-      t.remove :image_updated_at
+    reversible do |dir|
+      change_table :images, bulk: true do |t|
+        dir.up do
+          t.remove :image_file_name
+          t.remove :image_content_type
+          t.remove :image_file_size
+          t.remove :image_updated_at
+        end
+
+        dir.down do
+          t.string :image_file_name
+          t.string :image_content_type
+          t.integer :image_file_size
+          t.datetime :image_updated_at
+        end
+      end
     end
   end
 end
