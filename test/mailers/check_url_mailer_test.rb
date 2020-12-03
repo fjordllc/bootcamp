9cd98796370928e97c7228f3730826329dc5f576
rono23 <rono23@gmail.com>
2020-12-03 15:25:01 +0900

    RuboCopのStyle/NumericLiteralsの修正

--- test/mailers/check_url_mailer_test.rb
+++ test/mailers/check_url_mailer_test.rb
@@ -4,11 +4,11 @@ require "test_helper"
 
 class CheckUrlMailerTest < ActionMailer::TestCase
   test "notify_error_url" do
-    page_error_url = [{ id: 883692644, url: "http://localhost:3000/pages/5555555" }]
+    page_error_url = [{ id: 883_692_644, url: "http://localhost:3000/pages/5555555" }]
     practice_error_url = [
-      { id: 207936418, url: "http://homepage2.nifty.com/kamurai/CPU.htm" },
-      { id: 207936418, url: "http://homepage2.nifty.com/kamurai/MEMORY.htm" },
-      { id: 207936418, url: "http://homepage2.nifty.com/kamurai/HDD.htm" }
+      { id: 207_936_418, url: "http://homepage2.nifty.com/kamurai/CPU.htm" },
+      { id: 207_936_418, url: "http://homepage2.nifty.com/kamurai/MEMORY.htm" },
+      { id: 207_936_418, url: "http://homepage2.nifty.com/kamurai/HDD.htm" }
     ]
 
     mail = CheckUrlMailer.notify_error_url(page_error_url, practice_error_url).deliver_now
