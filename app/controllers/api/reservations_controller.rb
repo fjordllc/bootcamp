8d359792467589cdd02d219914fcf6b77b6be218
rono23 <rono23@gmail.com>
2020-12-03 13:38:01 +0900

    RuboCopのStyle/ClassEqualityComparisonの修正

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -10,7 +10,7 @@ class API::ReservationsController < API::BaseController
   end
 
   def create
-    if reservation_params[:seat_id].class == Array || reservation_params[:date].class == Array
+    if reservation_params[:seat_id].instance_of?(Array) || reservation_params[:date].instance_of?(Array)
       create_reservations(seat_id: reservation_params[:seat_id], date: reservation_params[:date])
     else
       @reservation = Reservation.new(reservation_params)
@@ -44,11 +44,11 @@ class API::ReservationsController < API::BaseController
 
   def create_reservations(seat_id:, date:)
     @reservations = []
-    values = seat_id if seat_id.class == Array && date.class == String
-    values = date if date.class == Array && seat_id.class == Integer
+    values = seat_id if seat_id.instance_of?(Array) && date.instance_of?(String)
+    values = date if date.instance_of?(Array) && seat_id.instance_of?(Integer)
     values.each do |value|
-      @reservation = Reservation.new(seat_id: value, date: date) if seat_id.class == Array
-      @reservation = Reservation.new(seat_id: seat_id, date: value) if date.class == Array
+      @reservation = Reservation.new(seat_id: value, date: date) if seat_id.instance_of?(Array)
+      @reservation = Reservation.new(seat_id: seat_id, date: value) if date.instance_of?(Array)
       @reservation.user = current_user
       @reservation.save
       @reservations << @reservation
