2f99259ab9829d8d2ac5967328e334a7c85231f9
rono23 <rono23@gmail.com>
2020-12-03 09:52:18 +0900

    RuboCopのRails/OutputSafetyの修正

--- app/controllers/partial/git_hub_grass_controller.rb
+++ app/controllers/partial/git_hub_grass_controller.rb
@@ -2,6 +2,6 @@
 
 class Partial::GitHubGrassController < PartialController
   def show
-    render plain: GithubGrass.new(params[:name]).fetch.html_safe
+    render plain: GithubGrass.new(params[:name]).fetch.html_safe # rubocop:disable Rails/OutputSafety
   end
 end
--- app/helpers/github_grass_helper.rb
+++ app/helpers/github_grass_helper.rb
@@ -2,6 +2,6 @@
 
 module GithubGrassHelper
   def github_grass(name)
-    GithubGrass.new(name).fetch.html_safe
+    GithubGrass.new(name).fetch.html_safe # rubocop:disable Rails/OutputSafety
   end
 end
