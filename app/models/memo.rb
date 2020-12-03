f11f9fccc68dc2929906b8d4d5ab0d68c80027bd
rono23 <rono23@gmail.com>
2020-12-03 13:17:26 +0900

    RuboCopのStyle/BlockDelimitersの修正

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -5,8 +5,8 @@ class Memo < ApplicationRecord
 
   def self.one_month_memos(beggining_of_this_month)
     days_of_this_month = beggining_of_this_month..beggining_of_this_month.end_of_month
-    where(date: days_of_this_month).index_by { |memo|
+    where(date: days_of_this_month).index_by do |memo|
       I18n.l(memo[:date], format: :ymd_hy)
-    }
+    end
   end
 end
