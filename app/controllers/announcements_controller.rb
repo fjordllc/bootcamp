8a934ce3937119dd590a92ca9c87fc9bc588218a
Masaki Komagata <komagata@gmail.com>
2020-12-08 09:42:47 +0900

    announcementの並び順を修正
    published_atが同じ時の並び順を修正。

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -8,7 +8,7 @@ class AnnouncementsController < ApplicationController
   def index
     @announcements = Announcement.with_avatar
                                  .preload(:comments)
-                                 .order(published_at: :desc)
+                                 .order(published_at: :desc, created_at: :desc)
                                  .page(params[:page])
   end
 
