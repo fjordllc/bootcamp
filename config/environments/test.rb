f0ea4d8ac85c17dc36be48e5d395497151d37cf6
KishiKyosuke <carp011808pg@icloud.com>
2020-12-16 13:37:38 +0900

    `announcements_test.rb`の`show pagination`テストを修正
    - after_createコールバック内のnotify_to_slackで呼ばれているavatar_urlメソッド内のservice_urlがnilとなりエラーが起こっていたため、としてActiveStorage::Current.hostに"http://localhost:3000"を指定する処理をテスト環境の設定に追加

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  ActiveStorage::Current.host = 'http://localhost:3000'
 end
