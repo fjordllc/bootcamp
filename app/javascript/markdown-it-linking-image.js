df8234b00696d7ac90928f604fe77235f6706ce5
rono23 <rono23@gmail.com>
2020-12-17 07:39:21 +0900

    マークダウンで不要なリンクの生成を修正

--- app/javascript/markdown-it-linking-image.js
+++ app/javascript/markdown-it-linking-image.js
@@ -1,17 +1,15 @@
 export default function (md) {
+  const defaultRender = md.renderer.rules.image
+
   md.renderer.rules.image = function (tokens, idx, options, env, self) {
+    if (tokens.some(token => token.type === 'link_open')) {
+      return defaultRender(tokens, idx, options, env, self)
+    }
+
     const imageToken = tokens[idx]
     const imageIndex = imageToken.attrIndex('src')
     const imageUrl = imageToken.attrs[imageIndex][1]
     const alt = md.utils.escapeHtml(imageToken.content)
-    const linkToken = tokens.find(token => token.type === 'link_open')
-    let linkUrl = imageUrl
-
-    if (linkToken) {
-      const linkSrcIndex = linkToken.attrIndex('href')
-      linkUrl = linkToken.attrs[linkSrcIndex][1]
-    }
-
-    return `<a href='${linkUrl}' target='_blank' rel='noopener noreferrer'><img src='${imageUrl}' alt='${alt}'></a>`
+    return `<a href='${imageUrl}' target='_blank' rel='noopener noreferrer'><img src='${imageUrl}' alt='${alt}'></a>`
   }
 }
