5344c969439c88d93cca10ba807dc56bff77a2f1
teppei machida <machida@fjord.jp>
2020-12-16 13:54:05 +0900

    みんなの進捗にデザイン

--- app/javascript/checkBox.js
+++ app/javascript/checkBox.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('input').forEach((e) => {
     const hasJsClass = e.hasClass('js-users-visibility__trigger')
     if (localStorage.getItem('hidden-users') === 'on' && hasJsClass) {
-      e.closest('.js-users-visibility').classList.add('is-hidden-users')
+      document.querySelector('.js-users-visibility').classList.add('is-hidden-users')
       e.checked = false
     } else if (e.checked === true && !hasJsClass) {
       e.parentNode.classList.add('is-checked')
@@ -17,12 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
       if (t === 'checkbox') {
         const hasJsClass = value.hasClass('js-users-visibility__trigger')
         if (chk === true && hasJsClass) {
-          value.closest('.js-users-visibility').classList.remove('is-hidden-users')
+          document.querySelector('.js-users-visibility').classList.remove('is-hidden-users')
           localStorage.removeItem('hidden-users')
         } else if (chk === true) {
           value.parentNode.classList.add('is-checked')
         } else if (chk === false && hasJsClass) {
-          value.closest('.js-users-visibility').classList.add('is-hidden-users')
+          document.querySelector('.js-users-visibility').classList.add('is-hidden-users')
           localStorage.setItem('hidden-users', 'on')
         } else {
           value.parentNode.classList.remove('is-checked')
