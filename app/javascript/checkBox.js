43eca823e4225c0c837a4f3a36eca32f9abc13d9
teppei machida <machida@fjord.jp>
2021-01-06 19:27:23 +0900

    hasClass -> classList.contains()

--- app/javascript/checkBox.js
+++ app/javascript/checkBox.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const localStorage = window.localStorage
   document.querySelectorAll('input').forEach((e) => {
-    const jsUsersVisibilityTriggerClass = e.hasClass('js-users-visibility__trigger')
+    const jsUsersVisibilityTriggerClass = e.classList.contains('js-users-visibility__trigger')
     if (localStorage.getItem('hidden-users') && jsUsersVisibilityTriggerClass) {
       document.querySelector('.js-users-visibility').classList.add('is-hidden-users')
       e.checked = false
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
       t = c.target.type
       chk = value.checked
       if (t === 'checkbox') {
-        const jsUsersVisibilityTriggerClass = value.hasClass('js-users-visibility__trigger')
+        const jsUsersVisibilityTriggerClass = value.classList.contains('js-users-visibility__trigger')
         if (chk && jsUsersVisibilityTriggerClass) {
           document.querySelector('.js-users-visibility').classList.remove('is-hidden-users')
           localStorage.removeItem('hidden-users')
