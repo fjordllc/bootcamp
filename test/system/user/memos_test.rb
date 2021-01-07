780c42840be4d2f9da991842d02ff3446290d338
Chihiro <Chi.r.r.333@gmail.com>
2021-01-07 12:05:21 +0900

    メンター以外、メモを表示しないテストを追加

--- test/system/user/memos_test.rb
+++ test/system/user/memos_test.rb
@@ -3,18 +3,19 @@
 require 'application_system_test_case'
 
 class User::MemoTest < ApplicationSystemTestCase
-  setup do
-    login_user 'komagata', 'testtest'
-    @user = users(:hatsuno)
-  end
-
   test 'update memorandom without page transition' do
-    visit user_path(@user)
-    wait_for_vuejs
+    login_user 'komagata', 'testtest'
+    visit user_path(users(:hatsuno))
     click_button '編集'
     fill_in 'js-user-mentor-memo', with: 'ユーザーメンターメモ'
     click_button '保存する'
     wait_for_vuejs
     assert_text 'ユーザーメンターメモ'
+  end
+
+  test 'No one but mentor or administrator can see memorandom' do
+    login_user 'hatsuno', 'testtest'
+    visit user_path(users(:hatsuno))
+    assert page.has_no_content? 'メンター向けメモ'
   end
 end
