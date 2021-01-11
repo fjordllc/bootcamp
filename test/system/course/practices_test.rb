e7938f7021357e1bbc69dc5656e921b6cb4ff903
Chihiro <Chi.r.r.333@gmail.com>
2021-01-11 11:21:09 +0900

    表示・非表示のテストを追加

--- test/system/course/practices_test.rb
+++ test/system/course/practices_test.rb
@@ -9,4 +9,24 @@ class Course::PracticesTest < ApplicationSystemTestCase
     visit "/courses/#{courses(:course1).id}/practices"
     assert_equal 'Rails Webプログラマーコース | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
+
+  test 'show/hide the progress of others' do
+    visit practice_path(practices(:practice1).id)
+    click_button '着手'
+    visit course_practices_path(courses(:course1).id)
+
+    assert page.find(:css, '#displayProgress', visible: false).checked?
+    assert_not has_selector?('.is-hidden-users')
+    user_path = user_path(users(:hatsuno).id)
+    within page.find('.categories-items__inner') do
+      assert page.has_link?(href: user_path)
+    end
+
+    page.find('#checkboxProgress').click
+    assert_not page.find(:css, '#displayProgress', visible: false).checked?
+    assert has_selector?('.is-hidden-users')
+    within page.find('.categories-items__inner') do
+      assert page.has_no_link?(href: user_path)
+    end
+  end
 end
