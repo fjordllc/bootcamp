4f0aa4a0ab91177590d0f5cca50a7b44904319a9
Chihiro <Chi.r.r.333@gmail.com>
2021-01-11 17:14:03 +0900

    全ロールでのテストを追加
    管理者orメンター以外に、メモは表示されないことを確認

--- test/integration/api/users_test.rb
+++ test/integration/api/users_test.rb
@@ -15,6 +15,61 @@ class API::UsersTest < ActionDispatch::IntegrationTest
     assert_response :ok
   end
 
+  test 'GET /api/users/1234.json as admin' do
+    get api_users_path(users(:kimura).id, format: :json)
+    assert_response :unauthorized
+
+    token = create_token('komagata', 'testtest')
+    get api_user_path(users(:kimura).id, format: :json),
+        headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+    assert_equal(users(:kimura).memo, JSON.parse(@response.body)['memo'])
+  end
+
+  test 'GET /api/users/1234.json as mentor' do
+    get api_users_path(users(:kimura).id, format: :json)
+    assert_response :unauthorized
+
+    token = create_token('yamada', 'testtest')
+    get api_user_path(users(:kimura).id, format: :json),
+        headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+    assert_equal(users(:kimura).memo, JSON.parse(@response.body)['memo'])
+  end
+
+  test 'GET /api/users/1234.json as adviser' do
+    get api_users_path(users(:kimura).id, format: :json)
+    assert_response :unauthorized
+
+    token = create_token('advijirou', 'testtest')
+    get api_user_path(users(:kimura).id, format: :json),
+        headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+    assert_nil(JSON.parse(@response.body)['memo'])
+  end
+
+  test 'GET /api/users/1234.json as trainee' do
+    get api_users_path(users(:kimura).id, format: :json)
+    assert_response :unauthorized
+
+    token = create_token('kensyu', 'testtest')
+    get api_user_path(users(:kimura).id, format: :json),
+        headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+    assert_nil(JSON.parse(@response.body)['memo'])
+  end
+
+  test 'GET /api/users/1234.json as graduate' do
+    get api_users_path(users(:kimura).id, format: :json)
+    assert_response :unauthorized
+
+    token = create_token('sotugyou', 'testtest')
+    get api_user_path(users(:kimura).id, format: :json),
+        headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+    assert_nil(JSON.parse(@response.body)['memo'])
+  end
+
   test 'GET /api/users/1234.json as student' do
     get api_users_path(users(:kimura).id, format: :json)
     assert_response :unauthorized
@@ -24,16 +79,5 @@ class API::UsersTest < ActionDispatch::IntegrationTest
         headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :ok
     assert_nil(JSON.parse(@response.body)['memo'])
-  end
-
-  test 'GET /api/users/1234.json as mentor' do
-    get api_users_path(users(:kimura).id, format: :json)
-    assert_response :unauthorized
-
-    token = create_token('komagata', 'testtest')
-    get api_user_path(users(:kimura).id, format: :json),
-        headers: { 'Authorization' => "Bearer #{token}" }
-    assert_response :ok
-    assert_equal(users(:kimura).memo, JSON.parse(@response.body)['memo'])
   end
 end
