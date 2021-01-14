e69e1866efa8dbca141ba74f8a19acb6e3add5d4
gallardo <aiki.hosogaya@gmail.com>
2021-01-15 00:57:57 +0900

    modelに作成したメソッドを削除してcontrollerで実装

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -6,10 +6,6 @@ class Generation
       (1..max_generation_number).map { |n| Generation.new(n) }
     end
 
-    def descending_order_generations
-      generations.reverse
-    end
-
     def max_generation_number
       now_time = Time.zone.now
       (now_time.year - 2013) * 4 + (now_time.month + 2) / 3
