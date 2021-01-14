ad1e1f6b015db3cf81a0335d11560d743cabd795
Chihiro <Chi.r.r.333@gmail.com>
2021-01-14 15:42:01 +0900

    hide-user.jsをリファクタリング

--- app/javascript/hide-user.js
+++ app/javascript/hide-user.js
@@ -1,29 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const localStorage = window.localStorage
+  const checkboxProgress = document.querySelector('.js-users-visibility__trigger')
   const jsUsersVisibilityClass = document.querySelector('.js-users-visibility')
-  document.querySelectorAll('input').forEach((e) => {
-    const jsUsersVisibilityTriggerClass = e.classList.contains('js-users-visibility__trigger')
-    if (localStorage.getItem('hidden-users') && jsUsersVisibilityTriggerClass) {
-      document.querySelector('.js-users-visibility').classList.add('is-hidden-users')
-      e.checked = false
+  if (checkboxProgress) {
+    if (localStorage.getItem('hidden-users')) {
+      jsUsersVisibilityClass.classList.add('is-hidden-users')
+      checkboxProgress.checked = false
     }
-  })
-  document.querySelectorAll('input').forEach((value) => {
-    value.addEventListener('click', (c) => {
-      const t = c.target.type
-      const chk = value.checked
-      if (t === 'checkbox') {
-        const jsUsersVisibilityTriggerClass = value.classList.contains('js-users-visibility__trigger')
-        if (jsUsersVisibilityTriggerClass) {
-          if (chk) {
-            jsUsersVisibilityClass.classList.remove('is-hidden-users')
-            localStorage.removeItem('hidden-users')
-          } else {
-            jsUsersVisibilityClass.classList.add('is-hidden-users')
-            localStorage.setItem('hidden-users', 'on')
-          }
-        }
+    checkboxProgress.addEventListener('click', () => {
+      if (checkboxProgress.checked) {
+        jsUsersVisibilityClass.classList.remove('is-hidden-users')
+        localStorage.removeItem('hidden-users')
+      } else {
+        jsUsersVisibilityClass.classList.add('is-hidden-users')
+        localStorage.setItem('hidden-users', 'on')
       }
     })
-  })
+  }
 })
