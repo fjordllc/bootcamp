057400942996edf8f5f98632dcaab3ac18f19976
Chihiro <Chi.r.r.333@gmail.com>
2021-01-16 16:45:37 +0900

    checkBox.jsからユーザー表示・非表示関連の処理を除去

--- app/javascript/checkBox.js
+++ app/javascript/checkBox.js
@@ -1,26 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('input').forEach((e) => {
-    const jsUsersVisibilityTriggerClass = e.classList.contains('js-users-visibility__trigger')
-    if (e.checked && !jsUsersVisibilityTriggerClass) {
-      e.parentNode.classList.add('is-checked')
+  document.querySelectorAll('input').forEach((input) => {
+    if (input.checked) {
+      input.parentNode.classList.add('is-checked')
     }
   })
   document.querySelectorAll('input').forEach((value) => {
-    value.addEventListener('click', (c) => {
-      var chk, t
-      t = c.target.type
-      chk = value.checked
-      if (t === 'checkbox') {
-        const jsUsersVisibilityTriggerClass = value.classList.contains('js-users-visibility__trigger')
-        if (!jsUsersVisibilityTriggerClass) {
-          if (chk) {
-            value.parentNode.classList.add('is-checked')
-          } else {
-            value.parentNode.classList.remove('is-checked')
-          }
+    value.addEventListener('click', (e) => {
+      const type = e.target.type
+      const check = value.checked
+      if (type === 'checkbox') {
+        if (check) {
+          value.parentNode.classList.add('is-checked')
+        } else {
+          value.parentNode.classList.remove('is-checked')
         }
-      } else if (t === 'radio') {
-        if (chk) {
+      } else if (type === 'radio') {
+        if (check) {
           document.querySelectorAll('ul, input').forEach(function (value) {
             if (value.type === 'radio') {
               value.parentNode.classList.remove('is-checked')
