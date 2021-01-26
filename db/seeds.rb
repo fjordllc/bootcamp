37160b2d5f7bf358c203ca61880302767e69bb91
Masaki Komagata <komagata@gmail.com>
2021-01-26 21:59:54 +0900

    seedとtest用のfixturesを分けた
    テーブルが投入される順番を制御できないとviolationに引っかかるため。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,42 @@
 # frozen_string_literal: true
 
-Rake::Task['db:fixtures:load'].execute
-Rake::Task['bootcamp:statistics:save_learning_minute_statistics'].execute
+require 'active_record/fixtures'
+
+FIXTURES = %i[
+  users
+  announcements
+  answers
+  articles
+  books
+  borrowings
+  categories
+  checks
+  comments
+  companies
+  correct_answers
+  courses
+  events
+  followings
+  reports
+  learning_times
+  learnings
+  memos
+  notifications
+  pages
+  participations
+  practices
+  products
+  questions
+  reactions
+  reservations
+  seats
+  watches
+  works
+]
+
+ActiveRecord::FixtureSet.create_fixtures 'db/fixtures', FIXTURES
+
+
+
+#rake::Task['db:fixtures:load'].execute
+#rake::Task['bootcamp:statistics:save_learning_minute_statistics'].execute
