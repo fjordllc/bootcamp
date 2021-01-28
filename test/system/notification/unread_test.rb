cb5a6de6a54e74772464f1f26557baa7ffb9eca6
MashioSano <mashio.tech@gmail.com>
2021-01-29 02:45:16 +0900

    未読の通知を一括で開くボタンが表示されることを確認するテストを修正
    未読の通知を一括で開くボタンが表示される条件は、メンターかつ未読の通知が1件以上あることです
    未読の通知を一括で開くボタンはメンターのみに表示されることをテストするため、明示的にNotification.createしています
    通知一覧を読み込んでいる間に、読み込み中を表現するコンテンツを表示させる仕様になっています
    通知が表示される前にアサーションが評価されることがあるため、wait_for_vuejsしています
    
    ref: https://github.com/fjordllc/bootcamp/pull/2260

--- test/system/notification/unread_test.rb
+++ test/system/notification/unread_test.rb
@@ -11,13 +11,21 @@ class Notification::UnreadTest < ApplicationSystemTestCase
   end
 
   test 'non-mentor can not see a button to open all unread notifications' do
+    Notification.create(message: 'machidaさんがコメントしました',
+                        kind: 'came_comment', path: '/reports/20400118',
+                        user: users(:hatsuno), sender: users(:machida))
     visit '/notifications/unread'
+    wait_for_vuejs
     assert_no_button '未読の通知を一括で開く'
   end
 
   test 'mentor can see a button to open to open all unread notifications' do
+    Notification.create(message: 'machidaさんがコメントしました',
+                        kind: 'came_comment', path: '/reports/20400118',
+                        user: users(:komagata), sender: users(:machida))
     login_user 'komagata', 'testtest'
     visit '/notifications/unread'
+    wait_for_vuejs
     assert_button '未読の通知を一括で開く'
   end
 end
