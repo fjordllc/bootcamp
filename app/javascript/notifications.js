4c628362012ca47c28b5a019df5835b27f476059
MashioSano <mashio.tech@gmail.com>
2021-01-28 22:58:13 +0900

    未読の通知を一括で開くボタンをVue化

--- app/javascript/notifications.js
+++ app/javascript/notifications.js
@@ -5,8 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-notifications'
   const notifications = document.querySelector(selector)
   if (notifications) {
+    const mentorLogin = notifications.getAttribute('data-mentor-login')
     new Vue({
-      render: h => h(Notifications)
+      render: h => h(Notifications, {
+        props: {
+          // 文字列を真偽値に変換して渡す
+          mentorLogin: mentorLogin === 'true'
+        }
+      })
     }).$mount(selector)
   }
 })
