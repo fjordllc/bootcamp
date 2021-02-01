47b96626ca3449c97489c779813ba811ccc1ad83
masuyama13 <masterxwebm13@gmail.com>
2021-02-01 09:43:02 +0900

    データ移行のコードを修正

--- db/data/20210118060304_add_position_to_categories_practices.rb
+++ db/data/20210118060304_add_position_to_categories_practices.rb
@@ -3,13 +3,13 @@
 class AddPositionToCategoriesPractices < ActiveRecord::Migration[6.0]
   def up
     CategoriesPractice.acts_as_list_no_update do
-      Practice.all.find_each do |practice|
+      Practice.find_each do |practice|
         CategoriesPractice.where(practice_id: practice.id).find_each do |categories_practice|
           categories_practice.position = practice.position
         end
       end
       ActiveRecord::Base.transaction do
-        Category.all.find_each do |category|
+        Category.find_each do |category|
           CategoriesPractice.where(category_id: category.id).order(:position).each.with_index(1) do |categories_practice, index|
             categories_practice.update!(position: index, created_at: Time.current, updated_at: Time.current)
           end
