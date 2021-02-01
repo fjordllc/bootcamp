b46ea9d731b0721405a9397af1dcb46c80b5a244
masuyama13 <masterxwebm13@gmail.com>
2021-02-01 09:54:37 +0900

    不要なpropsを削除

--- app/javascript/categories-practice.js
+++ app/javascript/categories-practice.js
@@ -6,13 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const categories = document.querySelectorAll(selector)
   if (categories) {
     for (let i = 0; i < categories.length; i++) {
-      const id = categories[i].getAttribute('data-category-id')
       const categoriesPractices = categories[i].getAttribute('data-categories-practices')
       const categoryPractices = categories[i].getAttribute('data-practices')
       new Vue({
         render: h => h(CategoriesPractice, {
           props: {
-            id: id,
             categoriesPractices: categoriesPractices,
             categoryPractices: categoryPractices
           }
