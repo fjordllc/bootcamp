dc002279672c6a67fe4e264a6419678f17ade27d
masuyama13 <masterxwebm13@gmail.com>
2021-02-02 15:24:06 +0900

    プラクティス並び替えページのルーティングを変更

--- test/system/course/practice/sort_test.rb
+++ test/system/course/practice/sort_test.rb
@@ -2,10 +2,10 @@
 
 require 'application_system_test_case'
 
-class Course::PracticesSortTest < ApplicationSystemTestCase
+class Practice::SortTest < ApplicationSystemTestCase
   test '管理者がプラクティス並び替えページにアクセスする' do
     login_user 'komagata', 'testtest'
-    visit course_practices_sort_path(courses(:course1).id)
+    visit course_sort_index_path(courses(:course1).id)
     within('.categories-items__inner') do
       assert_selector '.grab'
     end
@@ -13,7 +13,7 @@ class Course::PracticesSortTest < ApplicationSystemTestCase
 
   test '管理者以外はプラクティス並び替えページにアクセスできない' do
     login_user 'kimura', 'testtest'
-    visit course_practices_sort_path(courses(:course1).id)
+    visit course_sort_index_path(courses(:course1).id)
     assert_text '管理者としてログインしてください'
   end
 end
