4742dd8bde164f9f9bde65417c8a10a4426785b5
MashioSano <mashio.tech@gmail.com>
2021-02-06 16:02:58 +0900

    mentorLoginプロパティをisMentorプロパティに変更

--- app/javascript/notifications.js
+++ app/javascript/notifications.js
@@ -5,12 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-notifications'
   const notifications = document.querySelector(selector)
   if (notifications) {
-    const mentorLogin = notifications.getAttribute('data-mentor-login')
+    const isMentor = notifications.getAttribute('data-is-mentor')
     new Vue({
       render: h => h(Notifications, {
         props: {
           // 文字列を真偽値に変換して渡す
-          mentorLogin: mentorLogin === 'true'
+          isMentor: isMentor === 'true'
         }
       })
     }).$mount(selector)
