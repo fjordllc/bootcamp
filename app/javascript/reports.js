5bbb20d81cde2cd78a1a37c507ecbad727eff990
universato <universato@gmail.com>
2021-02-13 00:35:14 +0900

    不要なstoreを削除

--- app/javascript/reports.js
+++ app/javascript/reports.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Reports from './reports.vue'
-import store from './check-store.js'
 
 import Paginate from 'vuejs-paginate'
 Vue.component('paginate', Paginate)
@@ -10,7 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const reports = document.querySelector(selector)
   if (reports) {
     new Vue({
-      store,
       render: h => h(Reports)
     }).$mount(selector)
   }
