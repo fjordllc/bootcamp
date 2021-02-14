dda0156e93dfad0956cfd23945606e319adc8d94
gallardo <aiki.hosogaya@gmail.com>
2021-02-14 23:56:21 +0900

    rubocop指摘点の修正

--- db/migrate/20210214134753_change_emotion_to_notnull_in_reports.rb
+++ db/migrate/20210214134753_change_emotion_to_notnull_in_reports.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ChangeEmotionToNotnullInReports < ActiveRecord::Migration[6.0]
   def change
-    change_column :reports, :emotion,:integer, default: 2, null: false
+    change_column :reports, :emotion, :integer, default: 2, null: false
   end
 end
