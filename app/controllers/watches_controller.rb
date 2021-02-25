1e8a75858272ea14aa5c5acf48092cadecac5046
harukus <43bw.hana@gmail.com>
2021-02-25 13:53:30 +0900

    並び方を降順にし、ページャーを追加

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class WatchesController < ApplicationController
   before_action :require_login
   def index
+    user_watching = current_user.watches.order(created_at: :desc)
+    @watches = user_watching.page(params[:page]).per(25)
   end
 end
