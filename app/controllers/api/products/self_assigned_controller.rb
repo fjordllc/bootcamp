e502fb0015ab6dd32fa0e3d3600424ffb2e1067a
hogucc <uku.h1r8@gmail.com>
2021-02-27 22:25:04 +0900

    提出物一覧関連のAPIをApplicationControllerではなくAPI::BaseControllerを継承するように修正

--- app/controllers/api/products/self_assigned_controller.rb
+++ app/controllers/api/products/self_assigned_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class API::Products::SelfAssignedController < ApplicationController
+class API::Products::SelfAssignedController < API::BaseController
   before_action :require_staff_login
   def index
     @products = Product.self_assigned_product(current_user.id).unchecked.list.page(params[:page])
