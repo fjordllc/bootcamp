36f94913f15c305e961a917e92458c0e879151b7
tam03 <tamuramura2803@gmail.com>
2021-02-27 21:56:32 +0900

    rubocopの修正

--- test/system/mention/answers_test.rb
+++ test/system/mention/answers_test.rb
@@ -8,15 +8,13 @@ module Mention
       login_user 'komagata', 'testtest'
     end
 
-    test "recieve a notification when I got my question's answer" do
+    test 'mention from a answer' do
       visit "/questions/#{questions(:question8).id}"
       within('.thread-comment-form__form') do
         fill_in('answer[description]', with: '@hatsuno test')
       end
       click_button 'コメントする'
-      wait_for_vuejs
-      logout
-  
+
       login_user 'hatsuno', 'testtest'
       visit '/notifications'
       assert_text 'komagataさんからメンションがきました。'
