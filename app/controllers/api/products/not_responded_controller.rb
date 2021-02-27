55e860e50c477cc05645ebba66975e0c15291c90
hogucc <uku.h1r8@gmail.com>
2021-02-27 22:25:04 +0900

    提出物一覧関連のAPIをApplicationControllerではなくAPI::BaseControllerを継承するように修正

--- app/controllers/api/products/not_responded_controller.rb
+++ app/controllers/api/products/not_responded_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class API::Products::NotRespondedController < ApplicationController
+class API::Products::NotRespondedController < API::BaseController
   before_action :require_staff_login
   def index
     @products = Product.not_responded_products.list.page(params[:page])
--- app/controllers/api/products/unchecked_controller.rb
+++ app/controllers/api/products/unchecked_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class API::Products::UncheckedController < ApplicationController
+class API::Products::UncheckedController < API::BaseController
   before_action :require_staff_login
   def index
     @products = Product.unchecked.not_wip.list.page(params[:page])
