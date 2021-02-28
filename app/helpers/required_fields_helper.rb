1c1161aebfa742ddf47391ec08cb70424bc91f10
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-02-28 17:53:48 +0900

    ダッシュボードにDiscordアカウントの登録を促す表示を追加
    メソッドが長くなりrubocopに引っかかるため、required_snsメソッドを作り分割する。

--- app/helpers/required_fields_helper.rb
+++ app/helpers/required_fields_helper.rb
@@ -12,17 +12,23 @@ module RequiredFieldsHelper
 
     messages << link_to('自己紹介を入力してください。', edit_current_user_path, class: 'card-list__item-link'.to_s.html_safe) unless current_user.description?
 
-    if current_user.student?
-      unless current_user.github_account?
-        messages << link_to('GitHubアカウントを登録してください。', edit_current_user_path, class: 'card-list__item-link is-github'.to_s.html_safe)
-      end
-
-      unless current_user.slack_account?
-        messages << link_to('Slackアカウントを登録してください。', edit_current_user_path, class: 'card-list__item-link is-slack'.to_s.html_safe)
-      end
-    end
+    required_sns(messages) if current_user.student?
 
     messages
+  end
+
+  def required_sns(messages)
+    return if current_user.github_account?
+
+    messages << link_to('GitHubアカウントを登録してください。', edit_current_user_path, class: 'card-list__item-link is-github'.to_s.html_safe)
+
+    return if current_user.slack_account?
+
+    messages << link_to('Slackアカウントを登録してください。', edit_current_user_path, class: 'card-list__item-link is-slack'.to_s.html_safe)
+
+    return if current_user.discord_account?
+
+    messages << link_to('Discordアカウントを登録してください。', edit_current_user_path, class: 'card-list__item-link is-discord'.to_s.html_safe)
   end
   # rubocop:enable Rails/OutputSafety
 end
