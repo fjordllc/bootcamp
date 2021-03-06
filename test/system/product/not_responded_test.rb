bc149cc021f336e3c9ca4884befa092e1000ecaa
Junichi Ito <jit@sonicgarden.jp>
2021-03-07 08:45:10 +0900

    未返信の提出物が提出日の昇順で並ぶように修正 https://github.com/fjordllc/bootcamp/pull/2280/files#r585987542

--- test/system/product/not_responded_test.rb
+++ test/system/product/not_responded_test.rb
@@ -32,7 +32,27 @@ class ProductsTest < ApplicationSystemTestCase
     click_button '未返信の提出物を一括で開く'
 
     within_window(windows.last) do
-      assert_text 'テストの提出物1です。'
+      assert_text Product.not_responded_products.reorder_for_not_responded_products.first.body
     end
+  end
+
+  test 'products order' do
+    # id順で並べたときの最初と最後の提出物を、提出日順で見たときに最新と最古になるように入れ替える
+    Product.update_all(published_at: 1.day.ago) # rubocop:disable Rails/SkipsModelValidations
+    newest_product = Product.not_responded_products.reorder(:id).first
+    newest_product.update(published_at: Time.current)
+    oldest_product = Product.not_responded_products.reorder(:id).last
+    oldest_product.update(published_at: 2.days.ago)
+
+    login_user 'komagata', 'testtest'
+    visit '/products/not_responded'
+
+    # 提出日の昇順で並んでいることを検証する
+    titles = all('.thread-list-item__title').map { |t| t.text.gsub('★', '') }
+    authors = all('.thread-list-item-meta .thread-header__author').map(&:text)
+    assert_equal "#{newest_product.practice.title}の提出物", titles.first
+    assert_equal newest_product.user.login_name, authors.first
+    assert_equal "#{oldest_product.practice.title}の提出物", titles.last
+    assert_equal oldest_product.user.login_name, authors.last
   end
 end
