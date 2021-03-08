c0fb7234c87321baaa19c321ef88fdb2eae4d2cd
tam03 <tamuramura2803@gmail.com>
2021-03-08 15:49:55 +0900

    「お知らせ」のAPIで一般ユーザーが「公開」と「削除」が出来ない様制限

--- app/controllers/api/announcements_controller.rb
+++ app/controllers/api/announcements_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class API::AnnouncementsController < API::BaseController
-  protect_from_forgery except: %i[create]
+  before_action :require_admin_login_for_api, only: %i[destroy]
   before_action :set_announcement, only: %i[show update destroy]
+  protect_from_forgery except: %i[create update]
 
   def index
     @announcements = Announcement.with_avatar
@@ -13,16 +14,12 @@ class API::AnnouncementsController < API::BaseController
 
   def show; end
 
-  def new
-    @announcement = Announcement.new(target: 'students')
-  end
-
-  def edit
-    @announcement.user_id = current_user.id
-  end
-
   def update
-    if @announcement.update(announcement_params)
+    if !current_user.admin? \
+      && (announcement_params['wip'] == 'false' \
+      || announcement_params['wip'].nil?)
+      head :bad_request
+    elsif @announcement.update(announcement_params)
       head :ok
     else
       head :bad_request
@@ -32,6 +29,7 @@ class API::AnnouncementsController < API::BaseController
   def create
     @announcement = Announcement.new(announcement_params)
     @announcement.user_id = current_user.id
+    @announcement.wip = true unless current_user.admin?
     if @announcement.save
       render :create, status: :created
     else
@@ -46,7 +44,7 @@ class API::AnnouncementsController < API::BaseController
   private
 
   def announcement_params
-    params.permit(:title, :description, :target, :wip)
+    params.fetch(:announcement).permit(:title, :description, :target, :wip)
   end
 
   def set_announcement
