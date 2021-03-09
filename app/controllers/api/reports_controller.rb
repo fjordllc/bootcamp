1db6d6579cc77492147008413b6b3de8c1edd0a9
universato <universato@gmail.com>
2021-03-10 04:25:21 +0900

    使われてないワード検索を削除

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -2,13 +2,7 @@
 
 class API::ReportsController < API::BaseController
   def index
-    @search_words = params[:word]&.squish&.split(/[[:blank:]]/)&.uniq
     @reports = Report.list.page(params[:page])
-
     @reports = @reports.joins(:practices).where(practices: { id: params[:practice_id] }) if params[:practice_id].present?
-
-    return if @search_words.blank?
-
-    @reports = @reports.ransack(title_or_description_cont_all: @search_words).result
   end
 end
