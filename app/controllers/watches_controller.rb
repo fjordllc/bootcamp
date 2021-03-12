7caadc993a496d3ec40676b05de746127f9f56a8
harukus <43bw.hana@gmail.com>
2021-03-12 16:05:08 +0900

    １ページあたりの件数を定数に入れる

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -2,8 +2,10 @@
 
 class WatchesController < ApplicationController
   before_action :require_login
+
+  PAGINATES_PER = 25
   def index
     user_watching = current_user.watches.order(created_at: :desc)
-    @watches = user_watching.page(params[:page]).per(25)
+    @watches = user_watching.page(params[:page]).per(PAGINATES_PER)
   end
 end
