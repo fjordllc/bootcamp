b4e26b951143354e086f546af0d53fac71792c11
gallardo <aiki.hosogaya@gmail.com>
2021-03-17 15:04:22 +0900

    メソッド名を修正し、controllerの処理をAPIに移動

--- app/controllers/api/products/not_responded_controller.rb
+++ app/controllers/api/products/not_responded_controller.rb
@@ -4,5 +4,8 @@ class API::Products::NotRespondedController < API::BaseController
   before_action :require_staff_login
   def index
     @products = Product.not_responded_products.list.page(params[:page])
+    @latest_product_submitted_just_5days = @products.order(published_at: :desc).find { |product| product.submitted_just_specific_days(5) }
+    @latest_product_submitted_just_6days = @products.order(published_at: :desc).find { |product| product.submitted_just_specific_days(6) }
+    @latest_product_submitted_over_7days = @products.order(published_at: :desc).find { |product| product.submitted_over_specific_days(7) }
   end
 end
