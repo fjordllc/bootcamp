09084e65f345bb482495e41214e94257c180b715
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-21 05:25:13 +0900

    編集可能者しか質問が表示されない問題を修正

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -2,10 +2,12 @@
 
 class API::QuestionsController < API::BaseController
   include Rails.application.routes.url_helpers
-  before_action :set_question, only: %i[show update destroy]
+  before_action :set_editable_question, only: %i[update destroy]
   before_action :set_available_emojis, only: %i[show]
 
-  def show; end
+  def show
+    @question = Question.find(params[:id])
+  end
 
   def update
     if @question.update(question_params)
@@ -21,7 +23,7 @@ class API::QuestionsController < API::BaseController
 
   private
 
-  def set_question
+  def set_editable_question
     @question =
       if current_user.admin? || current_user.mentor?
         Question.find(params[:id])
