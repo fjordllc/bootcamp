42f4d816bc504077053d5910dca475b81e0fdb8f
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-21 17:26:06 +0900

    ユーザーアイコンをvue化

--- app/javascript/question.vue
+++ app/javascript/question.vue
@@ -1,135 +1,139 @@
 <template lang="pug">
-.thread__inner.a-card(
+.thread(
   v-if="question === null || practices === null || currentUser === null"
 )
   .empty
     .fas.fa-spinner.fa-pulse
     |  ロード中
-.thread__inner.a-card(v-else)
-  header.thread-header
-    .thread-header__upper-side
-      a.thread-header__author(:href="`/users/${question.user.id}`")
-        | {{ question.user.login_name }}
-      .thread-header__date
-        time.thread_header_date-value(
-          :datetime="updateAtISO8601",
-          pubdate="pubdate"
+.thread(v-else)
+  userIcon(
+    :user="question.user"
+    threadClassSuffix=""
+  )
+  .thread__inner.a-card
+    header.thread-header
+      .thread-header__upper-side
+        a.thread-header__author(:href="`/users/${question.user.id}`")
+          | {{ question.user.login_name }}
+        .thread-header__date
+          time.thread_header_date-value(
+            :datetime="updateAtISO8601",
+            pubdate="pubdate"
+          )
+            | {{ updateAt }}
+      .thread-practice(v-if="question.practice")
+        a.thread-practice__link(:href="`/practices/${question.practice.id}`")
+          | {{ question.practice.title }}
+      h1.thread-header__title
+        span.thread-header__title-icon.is-solved.is-success(
+          v-if="question.correct_answer !== null"
         )
-          | {{ updateAt }}
-    .thread-practice(v-if="question.practice")
-      a.thread-practice__link(:href="`/practices/${question.practice.id}`")
-        | {{ question.practice.title }}
-    h1.thread-header__title
-      span.thread-header__title-icon.is-solved.is-success(
-        v-if="question.correct_answer !== null"
+          | 解決済
+        span.thread-header__title-icon.is-solved.is-danger(v-else)
+          | 未解決
+        | {{ question.title }}
+      .thread-header__lower-side
+        watch(:watchableId="questionId", watchableType="Question")
+        .thread-header__raw
+          a.a-button.is-sm.is-secondary(
+            :href="`/questions/${questionId}.md`",
+            target="_blank"
+          )
+            | Raw
+    .thread__tags
+      tags(
+        :tagsInitialValue="question.tag_list"
+        :questionId="questionId"
+        tagsParamName="question[tag_list]"
+        :editAble="editAble"
       )
-        | 解決済
-      span.thread-header__title-icon.is-solved.is-danger(v-else)
-        | 未解決
-      | {{ question.title }}
-    .thread-header__lower-side
-      watch(:watchableId="questionId", watchableType="Question")
-      .thread-header__raw
-        a.a-button.is-sm.is-secondary(
-          :href="`/questions/${questionId}.md`",
-          target="_blank"
-        )
-          | Raw
-  .thread__tags
-    tags(
-      :tagsInitialValue="question.tag_list"
-      :questionId="questionId"
-      tagsParamName="question[tag_list]"
-      :editAble="editAble"
-    )
 
-  .thread__body
-    .thread-question__body.a-card(v-if="!editing")
-      .thread-question__description.js-target-blank.is-long-text(
-        v-html="markdownDescription"
-      )
-      reaction(
-        :reactionable="question",
-        :currentUser="currentUser",
-        :reactionableId="`Question_${this.questionId}`"
-      )
-      footer.card-footer(v-if="editAble")
-        .card-footer-actions
-          ul.card-footer-actions__items
-            li.card-footer-actions__item
-              button.card-footer-actions__action.a-button.is-md.is-primary.is-block(
-                @click="editQuestion"
-              )
-                i.fas.fa-pen
-                  | 内容修正
-            li.card-footer-actions__item
-              button.js-delete.card-footer-actions__action.a-button.is-md.is-danger.is-block(
-                @click="deleteQuestion"
-              )
-                i.fas.fa-trash-alt
-                | 削除
-    .thread-question-for(v-show="editing")
-      form(name="question")
-        .form__items
-          .form-item
-            .form-item
-              .row
-                .col-lg-6.col-xs-12
-                  .form-item
-                    | プラクティス
-                    .select-practices
-                      select(v-model="edited.practiceId")
-                        option(
-                          v-for="practice in practices",
-                          :value="practice.id"
-                        )
-                          div {{ practice.categoryAndPracticeName }}
-            .form-item
-              .row.js-markdown-parent
-                .col-md-6.col-xs-12
-                  .a-label
-                    | タイトル
-                  input(v-model="edited.title", name="question[title]")
-            .form-item
-              .thread-question-form__tabs.js-tabs
-                .thread-question-form__tab.js-tabs__tab(
-                  v-bind:class="{ 'is-active': isActive('question') }",
-                  @click="changeActiveTab('question')"
+    .thread__body
+      .thread-question__body.a-card(v-if="!editing")
+        .thread-question__description.js-target-blank.is-long-text(
+          v-html="markdownDescription"
+        )
+        reaction(
+          :reactionable="question",
+          :currentUser="currentUser",
+          :reactionableId="`Question_${this.questionId}`"
+        )
+        footer.card-footer(v-if="editAble")
+          .card-footer-actions
+            ul.card-footer-actions__items
+              li.card-footer-actions__item
+                button.card-footer-actions__action.a-button.is-md.is-primary.is-block(
+                  @click="editQuestion"
                 )
-                  | コメント
-                .thread-question-form__tab.js-tabs__tab(
-                  v-bind:class="{ 'is-active': isActive('preview') }",
-                  @click="changeActiveTab('preview')"
+                  i.fas.fa-pen
+                    | 内容修正
+              li.card-footer-actions__item
+                button.js-delete.card-footer-actions__action.a-button.is-md.is-danger.is-block(
+                  @click="deleteQuestion"
                 )
-                  | プレビュー
-              .thread-question-form__markdown-parent.js-markdown-parent
-                .thread-question-form__markdown.js-tabs__content(
-                  v-bind:class="{ 'is-active': isActive('question') }"
-                )
-                  // - TODO classQuestionId は必要?
-                  textarea#js-question-content.a-text-input.js-warning-form.thread-question-form__textarea(
-                    v-model="edited.description",
-                    data-preview="#js-question-preview",
-                    name="question[description]"
+                  i.fas.fa-trash-alt
+                  | 削除
+      .thread-question-for(v-show="editing")
+        form(name="question")
+          .form__items
+            .form-item
+              .form-item
+                .row
+                  .col-lg-6.col-xs-12
+                    .form-item
+                      | プラクティス
+                      .select-practices
+                        select(v-model="edited.practiceId")
+                          option(
+                            v-for="practice in practices",
+                            :value="practice.id"
+                          )
+                            div {{ practice.categoryAndPracticeName }}
+              .form-item
+                .row.js-markdown-parent
+                  .col-md-6.col-xs-12
+                    .a-label
+                      | タイトル
+                    input(v-model="edited.title", name="question[title]")
+              .form-item
+                .thread-question-form__tabs.js-tabs
+                  .thread-question-form__tab.js-tabs__tab(
+                    v-bind:class="{ 'is-active': isActive('question') }",
+                    @click="changeActiveTab('question')"
                   )
-                .thread-question-form__markdown.js-tabs__content(
-                  :class="{ 'is-active': isActive('preview') }"
-                )
-                  #js-question-preview.js-preview.is-long-text.thread-question-form__preview
-            ul.thread-question-form__actions
-              li.thread-question-form__action
-                button.a-button.is-md.is-warning.is-block(
-                  @click="updateQuestion",
-                  v-bind:disabled="!validation",
-                  type="button"
-                )
-                  | 更新する
-              li.thread-question-form__action
-                button.a-button.is-md.is-secondary.is-block(
-                  @click="cancel",
-                  type="button"
-                )
-                  | キャンセル
+                    | コメント
+                  .thread-question-form__tab.js-tabs__tab(
+                    v-bind:class="{ 'is-active': isActive('preview') }",
+                    @click="changeActiveTab('preview')"
+                  )
+                    | プレビュー
+                .thread-question-form__markdown-parent.js-markdown-parent
+                  .thread-question-form__markdown.js-tabs__content(
+                    v-bind:class="{ 'is-active': isActive('question') }"
+                  )
+                    textarea#js-question-content.a-text-input.js-warning-form.thread-question-form__textarea(
+                      v-model="edited.description",
+                      data-preview="#js-question-preview",
+                      name="question[description]"
+                    )
+                  .thread-question-form__markdown.js-tabs__content(
+                    :class="{ 'is-active': isActive('preview') }"
+                  )
+                    #js-question-preview.js-preview.is-long-text.thread-question-form__preview
+              ul.thread-question-form__actions
+                li.thread-question-form__action
+                  button.a-button.is-md.is-warning.is-block(
+                    @click="updateQuestion",
+                    v-bind:disabled="!validation",
+                    type="button"
+                  )
+                    | 更新する
+                li.thread-question-form__action
+                  button.a-button.is-md.is-secondary.is-block(
+                    @click="cancel",
+                    type="button"
+                  )
+                    | キャンセル
 </template>
 <script>
 import Reaction from './reaction.vue'
@@ -137,6 +141,7 @@ import Watch from './watch.vue'
 import MarkdownInitializer from './markdown-initializer'
 import TextareaInitializer from './textarea-initializer'
 import Tags from './question_tags.vue'
+import UserIcon from './user-icon.vue'
 import moment from 'moment'
 moment.locale('ja')
 
@@ -145,6 +150,7 @@ export default {
     watch: Watch,
     tags: Tags,
     reaction: Reaction,
+    userIcon: UserIcon
   },
   props: {
     currentUserId: { type: String, required: true },
