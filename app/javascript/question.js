f1c1460372f0ef407b36cd031238c816a81af8e3
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-21 13:13:03 +0900

    Adminでloginしているかの情報をpropsからではなくREST APIから取得

--- app/javascript/question.js
+++ app/javascript/question.js
@@ -6,16 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
   if (questionElement) {
     const currentUserId = questionElement.getAttribute('data-current-user-id')
     const questionId = questionElement.getAttribute('data-question-id')
-    var adminLogin = questionElement.getAttribute('data-admin-login')
-    adminLogin = adminLogin === 'true'
 
     new Vue({
       render: (h) =>
         h(Question, {
           props: {
             currentUserId: currentUserId,
-            questionId: questionId,
-            adminLogin: adminLogin
+            questionId: questionId
           }
         })
     }).$mount('#js-question')
