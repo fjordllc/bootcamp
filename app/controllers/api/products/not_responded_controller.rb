ec98c37d3c7d80fb2dc0cecab450458fb9c0e2be
gallardo <aiki.hosogaya@gmail.com>
2021-03-22 16:56:31 +0900

    余計な空行を削除

--- app/controllers/api/products/not_responded_controller.rb
+++ app/controllers/api/products/not_responded_controller.rb
@@ -8,7 +8,6 @@ class API::Products::NotRespondedController < API::BaseController
                 .list
                 .reorder_for_not_responded_products
                 .page(params[:page])
-    
     @latest_product_submitted_just_5days = @products.find { |product| product.submitted_just_specific_days(5) }
     @latest_product_submitted_just_6days = @products.find { |product| product.submitted_just_specific_days(6) }
     @latest_product_submitted_over_7days = @products.find { |product| product.submitted_over_specific_days(7) }
