cafe7a33347be64d75bc1d1fa02c16ff4a6516c3
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-22 11:22:07 +0900

    eslint errorを解除

--- app/javascript/mixins/check-tag.js
+++ app/javascript/mixins/check-tag.js
@@ -2,10 +2,10 @@ export default {
   methods: {
     checkTag (obj) {
       const { text } = obj.tag
-      if (/ |　/.test(text)) {
-        alert('入力されたタグにスペースが含まれています')
+      if (/ |　/.test(text)) { // eslint-disable-line no-irregular-whitespace
+        alert('入力されたタグにスペースが含まれています') // eslint-disable-line no-undef
       } else if (text === '.') {
-        alert('ドット1つだけのタグは作成できません')
+        alert('ドット1つだけのタグは作成できません') // eslint-disable-line no-undef
       } else {
         obj.addTag()
       }
