10a857a24362fd1cf0e5968e0f5f27979258190d
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-23 15:28:03 +0900

    メンション通知をリファクタリング

--- app/models/concerns/mentioner.rb
+++ app/models/concerns/mentioner.rb
@@ -1,21 +1,12 @@
 # frozen_string_literal: true
 
 module Mentioner
-  def after_save_mention(mentions)
-    new_mention_users.each do |receiver|
-      NotificationFacade.mentioned(self, receiver) if receiver && sender != receiver
-    end
-
-    return unless mentions.include? '@mentor'
-
-    User.mentor.each do |receiver|
-      NotificationFacade.mentioned(self, receiver) if sender != receiver
-    end
+  def after_save_mention(new_mentions)
+    notify_users_found_by_mentions(new_mentions)
   end
 
   def new_mention_users
-    names = new_mentions.map { |s| s.gsub(/@/, '') }
-    User.where(login_name: names)
+    find_users_from_login_names(extract_login_names_from_mentions(new_mentions))
   end
 
   def where_mention
@@ -34,6 +25,31 @@ module Mentioner
   end
 
   private
+
+  def notify_users_found_by_mentions(mentions)
+    notify_mentions(find_users_from_mentions(mentions))
+  end
+
+  def notify_mentions(receivers)
+    receivers.each do |receiver|
+      NotificationFacade.mentioned(self, receiver) if sender != receiver
+    end
+  end
+
+  def find_users_from_login_names(names)
+    User.where(login_name: names)
+  end
+
+  def find_users_from_mentions(mentions)
+    names = extract_login_names_from_mentions(mentions)
+    names.concat(User.mentor.map(&:login_name)) if mentions.include?('@mentor')
+    # find_users_from_login_names 内のwhereで重複は削除する
+    find_users_from_login_names(names)
+  end
+
+  def extract_login_names_from_mentions(mentions)
+    mentions.map { |s| s.gsub(/@/, '') }
+  end
 
   def target_of_comment(commentable_class, commentable)
     {
