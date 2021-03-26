13b3739b68d92b929012ee37577cff0b2b407fd0
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-03-26 18:40:52 +0900

    モデル側でエラーメッセージの文面を決めるように修正

--- app/models/required_field.rb
+++ app/models/required_field.rb
@@ -10,7 +10,7 @@ class RequiredField
   attribute :discord_account, :string
 
   validates :description, presence: true
-  validates :github_account, presence: { message: 'を登録してください。' }
-  validates :slack_account, presence: { message: 'を登録してください。' }
-  validates :discord_account, presence: { message: 'を登録してください。' }
+  validates :github_account, presence: { message: 'GitHubアカウントを登録してください。' }
+  validates :slack_account, presence: { message: 'Slackアカウントを登録してください。' }
+  validates :discord_account, presence: { message: 'Discordアカウントを登録してください。' }
 end
