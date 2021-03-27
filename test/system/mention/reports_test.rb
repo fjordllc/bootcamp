3be62198dfc206fc8d628c2dc5c0f217d9867062
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-27 18:39:25 +0900

    メンション通知のテストをリファクタリング

--- test/system/mention/reports_test.rb
+++ test/system/mention/reports_test.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
 require 'application_system_test_case'
+require 'supports/mention_helper'
+require 'supports/report_helper'
 
 module Mention
   class ReportsTest < ApplicationSystemTestCase
-    setup do
-      login_user 'kimura', 'testtest'
-    end
+    include MentionHelper
+    include ReportHelper
 
     test 'mention from a report' do
-      visit '/reports/new'
-      fill_in 'report_title', with: 'テスト日報'
-      fill_in 'report_description', with: '@hatsuno test'
-      select '00', from: :report_learning_times_attributes_0_started_at_4i
-      select '00', from: :report_learning_times_attributes_0_started_at_5i
-      select '01', from: :report_learning_times_attributes_0_finished_at_4i
-      select '00', from: :report_learning_times_attributes_0_finished_at_5i
-      click_button '提出'
+      post_mention = lambda { |description|
+        create_report('メンション通知が送信されるかのテスト', description, false)
+      }
 
-      login_user 'hatsuno', 'testtest'
-      visit '/notifications'
-      assert_text 'kimuraさんからメンションがきました。'
+      %w[hatsuno].each do |mention_target_login_name|
+        assert exists_unread_mention_notification_after_posting_mention?(
+          'kimura', mention_target_login_name, post_mention
+        )
+      end
     end
   end
 end
