1d3d0ad0946c963e60610c360d537ce41d2b02c0
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-03-27 18:39:25 +0900

    メンション通知のテストをリファクタリング

--- test/system/mention/products_test.rb
+++ test/system/mention/products_test.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
 require 'application_system_test_case'
+require 'supports/mention_helper'
 
 module Mention
   class ProductsTest < ApplicationSystemTestCase
-    setup do
-      login_user 'kimura', 'testtest'
-    end
+    include MentionHelper
 
     test 'mention from a production' do
-      practice = practices(:practice5)
-      visit "/products/new?practice_id=#{practice.id}"
-      within('#new_product') do
-        fill_in('product[body]', with: '@machida test')
-      end
-      click_button '提出する'
-      assert_text "提出物を提出しました。7日以内にメンターがレビューしますので、次のプラクティスにお進みください。\n7日以上待ってもレビューされない場合は、気軽にメンターにメンションを送ってください。"
+      post_mention = lambda { |body|
+        visit "#{new_product_path}?practice_id=#{practices(:practice5).id}"
+        within('#new_product') do
+          fill_in('product[body]', with: body)
+        end
+        click_button '提出する'
+        assert_text "提出物を提出しました。7日以内にメンターがレビューしますので、次のプラクティスにお進みください。\n7日以上待ってもレビューされない場合は、気軽にメンターにメンションを送ってください。"
+      }
 
-      login_user 'machida', 'testtest'
-      visit '/notifications'
-      assert_text 'kimuraさんからメンションがきました。'
+      %w[hatsuno].each do |mention_target_login_name|
+        assert exists_unread_mention_notification_after_posting_mention?(
+          'kimura', mention_target_login_name, post_mention
+        )
+      end
     end
   end
 end
