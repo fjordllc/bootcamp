61bdb61b0a70d462321e687579f824bfe552ba37
harukus <43bw.hana@gmail.com>
2021-03-29 17:19:42 +0900

    アーリーリターンを使ったリファクタリング

--- app/javascript/mentor-mode.js
+++ app/javascript/mentor-mode.js
@@ -2,19 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const localStorage = window.localStorage
   const checkboxMentorMode = document.querySelector('.js-mentor-mode__trigger')
   const jsMentorModeClass = document.querySelector('.is-memo')
-  if (checkboxMentorMode){
-    if (localStorage.getItem('mentor-mode')) {
-      checkboxMentorMode.checked = false
+  if (!checkboxMentorMode) return
+  if (localStorage.getItem('mentor-mode')) {
+    checkboxMentorMode.checked = false
+    jsMentorModeClass?.classList.add('is-only-mentor')
+  }
+  checkboxMentorMode.addEventListener('click', () => {
+    if (checkboxMentorMode.checked) {
+      localStorage.removeItem('mentor-mode')
+      jsMentorModeClass?.classList.remove('is-only-mentor')
+    } else {
+      localStorage.setItem('mentor-mode', 'off')
       jsMentorModeClass?.classList.add('is-only-mentor')
     }
-    checkboxMentorMode.addEventListener('click', () => {
-      if (checkboxMentorMode.checked) {
-        localStorage.removeItem('mentor-mode')
-        jsMentorModeClass?.classList.remove('is-only-mentor')
-      } else {
-        localStorage.setItem('mentor-mode', 'off')
-        jsMentorModeClass?.classList.add('is-only-mentor')
-      }
-    })
-  }
   })
+})
