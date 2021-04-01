fb562d0aa80dfdaca4d0a67d9ecb1244671a60b2
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-04-01 20:49:40 +0900

    apicontrollerとrouteの設定

---
+++ app/controllers/api/courses/practices_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class API::Courses::PracticesController < API::BaseController
+  before_action :require_login
+
+  def index
+    @course = Course.find(params[:course_id])
+    @categories = @course.categories
+                         .includes(practices: [{ started_students: { avatar_attachment: :blob } }, :learning_minute_statistic])
+                         .order(:position)
+    @learnings = current_user.learnings
+    # TODO: リタイアした人のセッションが切れたら外す
+    return unless current_user.retired_on?
+
+    logout
+    redirect_to retire_path
+  end
+end
---
+++ app/javascript/courses-practices.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  .fas.fa-spinner.fa-pulse
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
