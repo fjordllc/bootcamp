49efa9629246519869092c8ce196b1ca0d51b6e7
harukus <43bw.hana@gmail.com>
2021-04-02 18:11:38 +0900

    仕様に合わせた実装に修正

--- app/javascript/mentor-mode.js
+++ app/javascript/mentor-mode.js
@@ -1,24 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   const localStorage = window.localStorage
   const checkboxMentorMode = document.querySelector('.js-mentor-mode__trigger')
-  const jsMentorModeClass = document.querySelector('.is-memo')
+  const jsMentorModeClass = document.body
   if (!checkboxMentorMode) return
   if (localStorage.getItem('mentor-mode')) {
     checkboxMentorMode.checked = false
     if (jsMentorModeClass !== null) {
-      jsMentorModeClass.classList.add('is-only-mentor')
+      jsMentorModeClass.classList.remove('is-mentor-mode')
+    }
+  } else {
+    checkboxMentorMode.checked = true
+    if (jsMentorModeClass !== null) {
+      jsMentorModeClass.classList.add('is-mentor-mode')
     }
   }
   checkboxMentorMode.addEventListener('click', () => {
     if (checkboxMentorMode.checked) {
       localStorage.removeItem('mentor-mode')
       if (jsMentorModeClass !== null) {
-        jsMentorModeClass.classList.remove('is-only-mentor')
+        jsMentorModeClass.classList.add('is-mentor-mode')
       }
     } else {
       localStorage.setItem('mentor-mode', 'off')
       if (jsMentorModeClass !== null) {
-        jsMentorModeClass.classList.add('is-only-mentor')
+        jsMentorModeClass.classList.remove('is-mentor-mode')
       }
     }
   })
