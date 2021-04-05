dae5a49f164a2e71d6212ed79200b9f61f45dc00
harukus <43bw.hana@gmail.com>
2021-04-05 11:09:00 +0900

    修正前に戻す

--- app/javascript/mentor-mode.js
+++ app/javascript/mentor-mode.js
@@ -4,18 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const jsMentorModeClass = document.body
   if (!checkboxMentorMode) return
   if (localStorage.getItem('mentor-mode')) {
-    checkboxMentorMode.checked = true
-    jsMentorModeClass.classList.add('is-mentor-mode')
-  } else {
     checkboxMentorMode.checked = false
     jsMentorModeClass.classList.remove('is-mentor-mode')
+  } else {
+    checkboxMentorMode.checked = true
+    jsMentorModeClass.classList.add('is-mentor-mode')
   }
   checkboxMentorMode.addEventListener('click', () => {
     if (checkboxMentorMode.checked) {
-      localStorage.setItem('mentor-mode', 'on')
+      localStorage.removeItem('mentor-mode')
       jsMentorModeClass.classList.add('is-mentor-mode')
     } else {
-      localStorage.removeItem('mentor-mode')
+      localStorage.setItem('mentor-mode', 'off')
       jsMentorModeClass.classList.remove('is-mentor-mode')
     }
   })
