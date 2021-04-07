2cd2395d0b57f0e96514faa6f575f28d6f368709
teppei machida <machida@fjord.jp>
2021-04-08 00:39:24 +0900

    ベストアンサー取り消しのボタンを目立たなくした

--- app/javascript/answer.vue
+++ app/javascript/answer.vue
@@ -24,17 +24,17 @@
         .card-main-actions
           ul.card-main-actions__items
             li.card-main-actions__item(
-              v-if="!hasCorrectAnswer && answer.type != 'CorrectAnswer' && (currentUser.id === questionUser.id || currentUser.role === 'admin')")
-              button.card-main-actions__action.a-button.is-md.is-warning.is-block(@click="makeToBestAnswer")
-                | ベストアンサーにする
-            li.card-main-actions__item(
               v-if="answer.user.id == currentUser.id || currentUser.role == 'admin'")
               button.card-main-actions__action.a-button.is-md.is-secondary.is-block(@click="editAnswer")
                 i.fas.fa-pen
                 | 内容修正
             li.card-main-actions__item(
+              v-if="!hasCorrectAnswer && answer.type != 'CorrectAnswer' && (currentUser.id === questionUser.id || currentUser.role === 'admin')")
+              button.card-main-actions__action.a-button.is-md.is-primary.is-block(@click="makeToBestAnswer")
+                | ベストアンサーにする
+            li.card-main-actions__item(
               v-if="hasCorrectAnswer && answer.type == 'CorrectAnswer' && (currentUser.id === questionUser.id || currentUser.role === 'admin')")
-              button.card-main-actions__action.a-button.is-md.is-warning.is-block(@click="cancelBestAnswer")
+              button.card-main-actions__action.a-button.is-md.is-muted.is-block(@click="cancelBestAnswer")
                 | ベストアンサーを取り消す
             li.card-main-actions__item.is-sub(
               v-if="answer.user.id == currentUser.id || currentUser.role == 'admin'")
