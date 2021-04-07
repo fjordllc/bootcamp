ae1f4edf322132f18df7a1c0cc0a2947829733ce
hituzi_no_sippo <dev@hituzi-no-sippo.me>
2021-04-08 06:03:50 +0900

    質問更新のテストをリファクタリング

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -50,15 +50,19 @@ class QuestionsTest < ApplicationSystemTestCase
   test 'update a question' do
     question = questions(:question8)
     visit question_path(question)
+    updated_question = {
+      title: 'テストの質問（修正）',
+      description: 'テストの質問です。（修正）'
+    }
     wait_for_vuejs
     click_button '内容修正'
     within 'form[name=question]' do
-      fill_in 'question[title]', with: 'テストの質問（修正）'
-      fill_in 'question[description]', with: 'テストの質問です。（修正）'
+      fill_in 'question[title]', with: updated_question[:title]
+      fill_in 'question[description]', with: updated_question[:description]
       click_button '更新する'
     end
-    assert_text 'テストの質問（修正）'
-    assert_text 'テストの質問です。（修正）'
+
+    updated_question.each_value { |text| assert_text text }
     assert_text '質問を更新しました'
   end
 
