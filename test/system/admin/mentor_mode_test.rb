f657a3bd0046db4bf88b9fff4142457eae66cc18
harukus <43bw.hana@gmail.com>
2021-04-07 15:34:20 +0900

    assertionの書き方を変更

--- test/system/admin/mentor_mode_test.rb
+++ test/system/admin/mentor_mode_test.rb
@@ -6,13 +6,13 @@ class Admin::MentorModeTest < ApplicationSystemTestCase
   test 'admin show/hide mentor-user-memo' do
     login_user 'komagata', 'testtest'
     visit user_path(users(:hatsuno))
-    assert page.has_content? 'メンター向けユーザーメモ'
+    page.assert_text('メンター向けユーザーメモ')
     find(:css, '#checkbox-mentor-mode').set(false)
-    assert page.has_no_content? 'メンター向けユーザーメモ'
+    page.assert_no_text('メンター向けユーザーメモ')
   end
 
   test 'student don`t show mentor-user-memo' do
     login_user 'kimura', 'testtest'
-    assert page.has_no_content? 'メンター向けユーザーメモ'
+    page.assert_no_text('メンター向けユーザーメモ')
   end
 end
