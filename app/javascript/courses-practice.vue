1ff4a5a856cd103584f77d7c1cf0887af437f7f9
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-04-13 19:59:17 +0900

    vueファイルの作成　リンクなど詳細はまだ未完成

---
+++ app/javascript/courses-practice.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .category-practices-item.js-practice
+    a.category-practices-item__anchor(:id="`practice_${category.id}`")
+    header.category-practices-item__header(v-for="practices in category.practices")
+      .category-practices-item__title
+        a.category-practices-item__title-link(:href="practices.url")
+          | {{practices.title}}
+          //| {{status_by_learnings(practices.id)}}
+          //ここもリンクまだ
+      //if current_user
+        a.practice-status category-practices-item__status is-status(:practice.id)
+
+      .category-practices-item__learning-time
+        | 所要時間の目安:
+        | （平均:
+      .m-user-icons
+        .m-user-icons__items
+</template>
+
+
+<script>
+
+export default {
+  props: ['courseId','currentUserId','category','learnings',],
+  data: () => {
+    return {
+      currentPage: 1,
+      totalPages: null,
+    }
+  },
+  methods: {
+    status_by_learnings(practices){
+      let le = this.learnings.find(element => practices === element.practice_id);
+      if(!le) return 'unstarted';
+      return le.status
+    }
+  }
+}
+
+</script>
--- app/javascript/courses-practices.js
+++ app/javascript/courses-practices.js
@@ -5,8 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-courses-practices'
   const practices = document.querySelector(selector)
   if (practices) {
+    const courseId = practices.getAttribute('course-id')
+    const categories = practices.getAttribute('categories')
+    const currentUserId = practices.getAttribute('data-current-user-id')
+    const currentUser = window.currentUser
     new Vue({
-      render: h => h(CorusesPractices)
+      render: h => h(CorusesPractices,{
+        props: {
+          courseId: courseId,
+          categories: categories,
+          currentUserId: currentUserId,
+          currentUser: currentUser
+        }
+      })
     }).$mount(selector)
   }
 })
