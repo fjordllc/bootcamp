59f14a799b301de6c4fdd3160cafe0d78ac85f97
miso160 <hotmisosoup160@gmail.com>
2021-04-24 13:33:40 +0900

    ユーザータグをランダム表示にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,13 +38,14 @@ class UsersController < ApplicationController
                .users_role(@target)
     end
 
-    @popular_tags = ActsAsTaggableOn::Tag
-                    .joins(:taggings)
-                    .select('tags.id, tags.name, COUNT(taggings.id) as taggings_count')
-                    .group('tags.id, tags.name, tags.taggings_count')
-                    .where(taggings: { taggable_type: 'User' })
-                    .order('taggings_count desc')
-                    .limit(20)
+    users_tags = ActsAsTaggableOn::Tag
+                .joins(:taggings)
+                .select('tags.id, tags.name, COUNT(taggings.id) as taggings_count')
+                .group('tags.id, tags.name, tags.taggings_count')
+                .where(taggings: { taggable_type: 'User' })
+
+    @max_counts = users_tags.order('taggings_count desc').limit(3).map(&:taggings_count).uniq
+    @random_tags = users_tags.find(users_tags.pluck(:id).sample(20))
   end
   # rubocop:enable Metrics/MethodLength
 
