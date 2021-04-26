ea12e675a324b485bfd434e932b94eb46941a668
hisaotoriyama <hisao.toriyama@gmail.com>
2021-04-26 23:13:39 +0900

    questions_contのindex簡略化と不要ファイル削除

--- app/controllers/users/questions_controller.rb
+++ app/controllers/users/questions_controller.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
 class Users::QuestionsController < ApplicationController
-  before_action :set_user
-  before_action :set_questions
-
-  def index; end
-
-  private
-
-  def set_user
+  def index
     @user = User.find(params[:user_id])
-  end
-
-  def set_questions
-    @questions = user.questions.order(created_at: :desc)
-  end
-
-  def user
-    @user ||= User.find(params[:user_id])
+    @questions = @user.questions.order(created_at: :desc)
   end
 end
