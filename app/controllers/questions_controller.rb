03cff4253e8ae37f2c32e80cb811b96f54fcea0d
harukus <43bw.hana@gmail.com>
2021-04-28 08:47:40 +0900

    解決済みQAが新着コメント順で表示されるようにする

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,9 +17,15 @@ class QuestionsController < ApplicationController
         Question.all
       else
         Question.not_solved
-      end.order(updated_at: :desc, id: :desc)
+      end
     @tags = questions.all_tags
     @questions = params[:practice_id].present? ? questions.where(practice_id: params[:practice_id]) : questions
+    @questions =
+      if params[:solved].present?
+        @questions.includes(:answers).order('answers.updated_at DESC')
+      else
+        @questions.order(updated_at: :desc, id: :desc)
+      end
     @questions = @questions.preload(%i[practice answers]).with_avatar.page(params[:page])
     @questions = @questions.tagged_with(params[:tag]) if params[:tag]
     @questions_property = questions_property
