86119dc883ab612f145dfcd6d235c08073f865bb
harukus <43bw.hana@gmail.com>
2021-04-28 08:41:43 +0900

    タグの有無を見る変数を変更

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -18,6 +18,7 @@ class QuestionsController < ApplicationController
       else
         Question.not_solved
       end.order(updated_at: :desc, id: :desc)
+    @tags = questions.all_tags
     @questions = params[:practice_id].present? ? questions.where(practice_id: params[:practice_id]) : questions
     @questions = @questions.preload(%i[practice answers]).with_avatar.page(params[:page])
     @questions = @questions.tagged_with(params[:tag]) if params[:tag]
