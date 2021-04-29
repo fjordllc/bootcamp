69ef8631a245990258a07daee0176890bc8ddaa8
miso160 <hotmisosoup160@gmail.com>
2021-04-29 14:01:53 +0900

    コントローラーのヘルパーメソッドをユーザーモデルに移動

--- app/controllers/users/tags_controller.rb
+++ app/controllers/users/tags_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Users::TagsController < ApplicationController
-  helper_method :users_tagged_with
-
   def index
     @tags = User.tags.order('taggings_count desc')
     @top3_tags_counts = @tags.limit(3).map(&:taggings_count).uniq
@@ -12,14 +10,5 @@ class Users::TagsController < ApplicationController
     current_user.tag_list.add(params[:tag])
     current_user.save
     redirect_to "/users/tags/#{params[:tag]}"
-  end
-
-  private
-
-  def users_tagged_with(tag_name)
-    User.with_attached_avatar
-        .unretired
-        .order(updated_at: :desc)
-        .tagged_with(tag_name)
   end
 end
