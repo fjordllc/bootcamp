a85a612651e8c7146cb1d5886b32d1215e9c6bbc
ima1zumi <mariimaizumi5@gmail.com>
2021-05-03 20:27:24 +0900

    Userと関連を持つモデルのみlogin_nameを表示するようにした

--- app/decorators/page_decorator.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module PageDecorator
-  def hide_user?
-    true
-  end
-end
--- app/decorators/practice_decorator.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module PracticeDecorator
-  def hide_user?
-    true
-  end
-end
