f57a7e90d2de5d694066e4104103eed3a8751588
miso160 <hotmisosoup160@gmail.com>
2021-05-06 09:12:44 +0900

    user-tags.vueの作成

---
+++ app/javascript/user-tags.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.tag-links
+  ul.tag-links__items(v-if='tags')
+    li.tag-links__item(v-for='tag in tags')
+      a.tag-links__item-link(:href='`/users/tags/${tag}`')
+        | {{ tag }}
+</template>
+<script>
+export default {
+  props: {
+    user: { type: Object, required: true }
+  },
+  data() {
+    return {
+      tags: this.user.tag_list
+    }
+  }
+}
+</script>
+
