97bf007f3b9b2f1de769add82d0bb524f85b4bb6
miso160 <hotmisosoup160@gmail.com>
2021-05-08 13:09:39 +0900

    リタイアユーザーの登録したタグを含まないように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -316,9 +316,11 @@ class User < ApplicationRecord
     def tags
       ActsAsTaggableOn::Tag
         .joins(:taggings)
+        .joins('INNER JOIN users ON taggings.taggable_id = users.id')
         .select('tags.id, tags.name, COUNT(taggings.id) as taggings_count')
         .group('tags.id, tags.name, tags.taggings_count')
         .where(taggings: { taggable_type: 'User' })
+        .where(users: { retired_on: nil })
     end
   end
 
