93a15cce6ff3a7ccdf5936a8a920957809e7b5d0
komagata <komagata@gmail.com>
2021-05-15 04:31:17 +0900

    学習時間無しの処理を修正
    - checkbox用のattributeを使う方法に変更した
    - 日報変更時の学習時間の削除に対応した
    - テストを追加

--- app/decorators/report_decorator.rb
+++ app/decorators/report_decorator.rb
@@ -1,22 +1,6 @@
 # frozen_string_literal: true
 
 module ReportDecorator
-  def total_learning_time
-    total_time = learning_times.inject(0) do |sum, learning_time|
-      sum + learning_time.diff
-    end
-
-    total_minute = total_time / 60
-    hour = (total_minute / 60).to_i
-    minute = (total_minute % 60).round
-
-    if minute.zero?
-      "#{hour}時間"
-    else
-      "#{hour}時間#{minute}分"
-    end
-  end
-
   def number
     serial_number == 1 ? '初日報' : serial_number
   end
