4e58beaf45a9a8df765324b47a45fcf730c0b2b5
alto-I <blmaldiy@gmail.com>
2021-05-15 17:04:06 +0900

    searchable.vueのtemplateの修正

--- app/javascript/searchable.vue
+++ app/javascript/searchable.vue
@@ -3,25 +3,29 @@
   .thread-list-item__inner
     .thread-list-item__label
       | {{ searchable.model_name_with_i18n }}
-    header.thread-list-item__header
-      .thread-list-item__title
-        a.thread-list-item__title-link(:href='searchable.url')
-          | {{ searchable.title }}
-    .thread-list-item__body
-      .thread-list-item__summury
-        p {{ searchable.summury }}
-      .thread-list-item-meta
-        .thread-list-item-meta__user
-          a.thread-header__author(
-            v-if='!["practice", "page", "user"].includes(searchable.model_name)',
-            :href='userUrl'
-          )
-            | {{ searchable.login_name }}
-        time.thread-list-item-meta__updated-at(
-          :datetime='searchable.updated_at_date_time',
-          pubdate='pubdate'
-        )
-          | {{ searchable.updated_at }}
+    .thread-list-item__rows
+      .thread-list-item__row
+        .thread-list-item-title
+          .thread-list-item-title__title
+            a.thread-list-item-title__link(:href='searchable.url')
+              | {{ searchable.title }}
+      .thread-list-item__row
+        .thread-list-item__summury
+          p {{ searchable.summury }}
+      .thread-list-item__row
+        .thread-list-item-meta
+          .thread-list-item-meta__items
+            .thread-list-item-meta__item(
+              v-if='!["practice", "page", "user"].includes(searchable.model_name)'
+            )
+              a.a-user-name(:href='userUrl')
+                | {{ searchable.login_name }}
+            .thread-list-item-meta__item
+              time.a-date(
+                :datetime='searchable.updated_at_date_time',
+                pubdate='pubdate'
+              )
+                | {{ searchable.updated_at }}
 </template>
 <script>
 export default {
