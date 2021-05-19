7f735c93d50c68a99ce4fa2f194d63bad321f54c
alto-I <blmaldiy@gmail.com>
2021-05-19 20:46:58 +0900

    searchables_controller.rbのコンフリクトの解消

--- app/controllers/searchables_controller.rb
+++ app/controllers/searchables_controller.rb
@@ -3,18 +3,5 @@
 class SearchablesController < ApplicationController
   before_action :require_login
 
-<<<<<<< HEAD
   def index; end
-=======
-  def index
-    @result = Searcher.search(params[:word], document_type: document_type_param)
-    @searchables = Kaminari.paginate_array(@result).page(params[:page]).per(50)
-  end
-
-  private
-
-  def document_type_param
-    params[:document_type]&.to_sym || :all
-  end
->>>>>>> b9d44671 (検索結果を非同期化)
 end
