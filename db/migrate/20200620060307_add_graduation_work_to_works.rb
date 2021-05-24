a826f1e6a35a84185c56f90fcceeaad92be1b65c
teppei machida <machida@fjord.jp>
2021-05-24 20:16:54 +0900

    ダブったmigrateファイルを削除

--- db/migrate/20200620060307_add_graduation_work_to_works.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddGraduationWorkToWorks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :works, :graduation_work, :boolean, default: false, null: false
-  end
-end
