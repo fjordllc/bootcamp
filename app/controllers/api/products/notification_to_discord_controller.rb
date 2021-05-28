e43f374c3f5197a83333c25b59b4fd93047ecb1a
gallardo <aiki.hosogaya@gmail.com>
2021-05-28 15:53:33 +0900

    提出物の提出後の経過日数を取得するメソッドをリファクタリング

--- app/controllers/api/products/notification_to_discord_controller.rb
+++ app/controllers/api/products/notification_to_discord_controller.rb
@@ -5,17 +5,8 @@ class API::Products::NotificationToDiscordController < API::BaseController
     products = Product
                .not_responded_products
                .list
-    @products_submitted_just_5days_count = 0
-    @products_submitted_just_6days_count = 0
-    @products_submitted_over_7days_count = 0
-    products.each do |product|
-      if product.submitted_just_specific_days(5)
-        @products_submitted_just_5days_count += 1
-      elsif product.submitted_just_specific_days(6)
-        @products_submitted_just_6days_count += 1
-      elsif product.submitted_over_specific_days(7)
-        @products_submitted_over_7days_count += 1
-      end
-    end
+    @products_submitted_just_5days_count = products.count { |product| product.elapsed_days == 5 }
+    @products_submitted_just_6days_count = products.count { |product| product.elapsed_days == 6 }
+    @products_submitted_over_7days_count = products.count { |product| product.elapsed_days >= 7 }
   end
 end
