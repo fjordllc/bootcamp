83d6e55ae4895bb0a0960190e9d1c8d5440a1c31
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-05-30 18:18:20 +0900

    DocsのタグのVueファイルをユーザーのものと共通化

--- app/javascript/page_tags.js
+++ app/javascript/page_tags.js
@@ -1,20 +1,45 @@
 import Vue from 'vue'
-import PageTags from './page_tags.vue'
+import Tags from './tags.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const pageTagsElm = document.getElementById('js-page-tags')
   if (pageTagsElm) {
     const tagsInitialValue = pageTagsElm.getAttribute('data-tags-initial-value')
     const tagsParamName = pageTagsElm.getAttribute('data-tags-param-name')
+    const tagsInputId = pageTagsElm.getAttribute('data-tags-input-id')
     const pageId = pageTagsElm.getAttribute('data-page-id')
+    const updateCallback = function (tagsValue, token) {
+      const params = {
+        page: {
+          tag_list: tagsValue
+        }
+      }
+      return fetch(`/api/pages/${pageId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': token
+        },
+        credentials: 'same-origin',
+        redirect: 'manual',
+        body: JSON.stringify(params)
+      }).catch((error) => {
+        console.warn('Failed to parsing', error)
+      })
+    }
 
     new Vue({
       render: (h) =>
-        h(PageTags, {
+        h(Tags, {
           props: {
             tagsInitialValue: tagsInitialValue,
             tagsParamName: tagsParamName,
-            pageId: pageId
+            tagsInputId: tagsInputId,
+            tagsPath: 'tags',
+            tagsEditable: true,
+            tagsType: 'Page',
+            updateCallback: updateCallback
           }
         })
     }).$mount('#js-page-tags')
