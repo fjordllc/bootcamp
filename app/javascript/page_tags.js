76ecb17f870d88a15ac08afb8b849ebf7ec0b127
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-05-30 20:15:39 +0900

    タグの更新メソッドを共通化

--- app/javascript/page_tags.js
+++ app/javascript/page_tags.js
@@ -8,26 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const tagsParamName = pageTagsElm.getAttribute('data-tags-param-name')
     const tagsInputId = pageTagsElm.getAttribute('data-tags-input-id')
     const pageId = pageTagsElm.getAttribute('data-page-id')
-    const updateCallback = function (tagsValue, token) {
-      const params = {
-        page: {
-          tag_list: tagsValue
-        }
-      }
-      return fetch(`/api/pages/${pageId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8',
-          'X-Requested-With': 'XMLHttpRequest',
-          'X-CSRF-Token': token
-        },
-        credentials: 'same-origin',
-        redirect: 'manual',
-        body: JSON.stringify(params)
-      }).catch((error) => {
-        console.warn('Failed to parsing', error)
-      })
-    }
 
     new Vue({
       render: (h) =>
@@ -36,10 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
             tagsInitialValue: tagsInitialValue,
             tagsParamName: tagsParamName,
             tagsInputId: tagsInputId,
+            tagsTypeId: pageId,
             tagsPath: 'tags',
             tagsEditable: true,
             tagsType: 'Page',
-            updateCallback: updateCallback
+            lowerTagsType: 'page'
           }
         })
     }).$mount('#js-page-tags')
