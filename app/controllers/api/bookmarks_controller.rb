df4b8579141f17633138ecf55414e2d9315aedd5
alto-I <blmaldiy@gmail.com>
2021-06-05 19:16:40 +0900

    コントローラーの作成とルーティングの修正

---
+++ app/controllers/api/bookmarks_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class API::BookmarksController < API::BaseController
+  def index
+    @bookmarks = Bookmark.where(user_id: current_user.id).order(created_at: :desc)
+    @bookmarks = Bookmark.where(user_id: current_user.id, report_id: params[:report_id]) if params[:report_id]
+  end
+
+  def create
+    Bookmark.create(user_id: current_user.id, report_id: params[:report_id])
+  end
+
+  def destroy
+    Bookmark.find(params[:id]).destroy
+    head :no_content
+  end
+end
