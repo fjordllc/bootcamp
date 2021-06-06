31f88e050034254fb579f2dfa450a65e8ef3ee48
alto-I <blmaldiy@gmail.com>
2021-06-07 07:50:59 +0900

    一覧ページにページングを実装

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -2,9 +2,11 @@
 
 class API::BookmarksController < API::BaseController
   before_action :require_login
+  PEGER_NUMBER = 25
 
   def index
-    @bookmarks = Bookmark.where(user_id: current_user.id).order(created_at: :desc)
+    bookmarks = Bookmark.where(user_id: current_user.id).order(created_at: :desc)
+    @bookmarks = Kaminari.paginate_array(bookmarks).page(params[:page]).per(PEGER_NUMBER)
     @bookmarks = Bookmark.where(user_id: current_user.id, report_id: params[:report_id]) if params[:report_id]
   end
 
