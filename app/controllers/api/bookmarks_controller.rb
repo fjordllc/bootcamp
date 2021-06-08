d45521496cfb84c157fd56e127da36c9be792428
alto-I <blmaldiy@gmail.com>
2021-06-08 19:34:50 +0900

    ブックマークボタンを実装

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -7,15 +7,29 @@ class API::BookmarksController < API::BaseController
   def index
     bookmarks = Bookmark.where(user_id: current_user.id).order(created_at: :desc)
     @bookmarks = Kaminari.paginate_array(bookmarks).page(params[:page]).per(PEGER_NUMBER)
-    @bookmarks = Bookmark.where(user_id: current_user.id, report_id: params[:report_id]) if params[:report_id]
+    return unless params[:bookmarkable_id] && params[:bookmarkable_type]
+
+    @bookmarks = Bookmark.where(user_id: current_user.id, bookmarkable_id: params[:bookmarkable_id], bookmarkable_type: params[:bookmarkable_type])
   end
 
   def create
-    Bookmark.create(user_id: current_user.id, report_id: params[:report_id])
+    @bookmark = Bookmark.new(
+      user: current_user,
+      bookmarkable: bookmarkable
+    )
+
+    @bookmark.save!
+    render json: @bookmark
   end
 
   def destroy
     Bookmark.find(params[:id]).destroy
     head :no_content
+  end
+
+  private
+
+  def bookmarkable
+    params[:bookmarkable_type].constantize.find_by(id: params[:bookmarkable_id])
   end
 end
--- app/javascript/bookmark-button.vue
+++ app/javascript/bookmark-button.vue
@@ -1,4 +1,113 @@
 <template lang="pug">
-.test
-  | bookmarks
+.thread-header-actions__action
+  .a-button.is-xs(
+    :class='isBookmark ? "is-secondary" : "is-main"',
+    @click='push'
+  )
+    | {{ bookmarkLabel }}
 </template>
+<script>
+export default {
+  props: {
+    bookmarkableId: { type: Number, required: true },
+    bookmarkableType: { type: String, required: true }
+  },
+  data() {
+    return {
+      bookmarkId: null,
+      bookmarkLabel: 'bookmark',
+      isBookmark: false
+    }
+  },
+  created() {
+    this.getBookmark()
+  },
+  methods: {
+    token() {
+      const meta = document.querySelector('meta[name="csrf-token"]')
+      return meta ? meta.getAttribute('content') : ''
+    },
+    push() {
+      if (this.isBookmark) {
+        this.unbookmark()
+      } else {
+        this.bookmark()
+      }
+    },
+    getBookmark() {
+      fetch(
+        `/api/bookmarks.json?bookmarkable_type=${this.bookmarkableType}&bookmarkable_id=${this.bookmarkableId}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRF-Token': this.token()
+          },
+          credentials: 'same-origin',
+          redirect: 'manual'
+        }
+      )
+        .then((response) => {
+          return response.json()
+        })
+        .then((json) => {
+          if (json.bookmarks.length) {
+            this.bookmarkId = json.bookmarks[0].id
+            this.bookmarkLabel = 'unbookmark'
+            this.isBookmark = true
+          }
+        })
+        .catch((error) => {
+          console.warn('Failed to parsing', error)
+        })
+    },
+    bookmark() {
+      fetch(
+        `/api/bookmarks.json?bookmarkable_type=${this.bookmarkableType}&bookmarkable_id=${this.bookmarkableId}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRF-Token': this.token()
+          },
+          credentials: 'same-origin',
+          redirect: 'manual'
+        }
+      )
+        .then((response) => {
+          return response.json()
+        })
+        .then((json) => {
+          this.bookmarkId = json.id
+          this.bookmarkLabel = 'unbookmark'
+          this.isBookmark = true
+        })
+        .catch((error) => {
+          console.warn('Failed to parsing', error)
+        })
+    },
+    unbookmark() {
+      fetch(`/api/bookmarks/${this.bookmarkId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': this.token()
+        },
+        credentials: 'same-origin',
+        redirect: 'manual'
+      })
+        .then(() => {
+          this.bookmarkId = null
+          this.bookmarkLabel = 'bookmark'
+          this.isBookmark = false
+        })
+        .catch((error) => {
+          console.warn('Failed to parsing', error)
+        })
+    }
+  }
+}
+</script>
