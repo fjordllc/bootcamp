dfb3c003c29d0b525bb5bbebecdae65b3f7322a4
SeijiNumata <seizi.numata@gmail.com>
2021-06-10 09:18:05 +0900

    lintを実行

--- app/javascript/watch-toggle.js
+++ app/javascript/watch-toggle.js
@@ -4,8 +4,8 @@ import WatchToggle from './watch-toggle.vue'
 document.addEventListener('DOMContentLoaded', () => {
   const watchToggle = document.getElementById('js-watch-toggle')
   if (watchToggle) {
-    const watchableId = Number(watch.getAttribute('data-watchable-id'))
-    const watchableType = watch.getAttribute('data-watchable-type')
+    const watchableId = Number(watchToggle.getAttribute('data-watchable-id'))
+    const watchableType = watchToggle.getAttribute('data-watchable-type')
     new Vue({
       render: (h) =>
         h(WatchToggle, {
