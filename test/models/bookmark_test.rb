c90299201e94742c667b6f181a8c2d3446d4e038
alto-I <blmaldiy@gmail.com>
2021-06-12 10:39:13 +0900

    ブックマークボタンのidの付与とモデル、システムテストの実装

--- test/models/bookmark_test.rb
+++ test/models/bookmark_test.rb
@@ -3,4 +3,11 @@
 require 'test_helper'
 
 class BookmarkTest < ActiveSupport::TestCase
+  test 'prohibition of duplicate registration' do
+    user = users(:machida)
+    report = reports(:report1)
+
+    Bookmark.create(user: user, bookmarkable: report)
+    assert_not Bookmark.new(user: user, bookmarkable: report).valid?
+  end
 end
