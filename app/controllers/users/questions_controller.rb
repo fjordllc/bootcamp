1a7b292f0bf515d97ce4770d386b2869e9280281
komagata <komagata@gmail.com>
2021-06-15 14:36:02 +0900

    ユーザーの提出物一覧をログイン必須にした

--- app/controllers/users/questions_controller.rb
+++ app/controllers/users/questions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::QuestionsController < ApplicationController
+  before_action :require_login
+
   def index
     @user = User.find(params[:user_id])
     @questions = @user.questions.order(created_at: :desc)
