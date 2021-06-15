3fb93222c58e3bdad3b1d7bef7f2d7fc5ad02a7e
SeijiNumata <seizi.numata@gmail.com>
2021-06-15 23:25:32 +0900

    watch一覧のテストを記述

--- test/system/watchings_test.rb
+++ test/system/watchings_test.rb
@@ -15,4 +15,30 @@ class WatchingTest < ApplicationSystemTestCase
     visit watches_path
     assert_text 'テストの質問1'
   end
+
+  test 'watch index checkbox test' do
+    login_user 'kimura', 'testtest'
+    visit watches_path
+    uncheck '編集'
+    assert_no_text 'Watchを外す'
+    check '編集'
+    assert_text 'Watchを外す'
+  end
+
+  test 'watch index page watchbutton test' do
+    login_user 'kimura', 'testtest'
+    report = reports(:report1)
+    visit report_path(report)
+    assert_text 'Watch中'
+    wait_for_vuejs
+    visit watches_path
+    assert_text '作業週1日目'
+    check '編集'
+    assert_text 'Watchを外す'
+    first('#watch-button').click
+    wait_for_vuejs
+    assert_no_text '作業週1日目'
+    visit report_path(report)
+    assert_text 'Watchする'
+  end
 end
