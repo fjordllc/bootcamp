b416b01325f81c5a61c7388bb67fd38a9f33a69d
kasai441 <kasai441441@gmail.com>
2021-06-15 11:14:49 +0900

    セレクトボックスがコースに沿ったプラクティスを表示するテスト追加(QA)

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -264,4 +264,13 @@ class QuestionsTest < ApplicationSystemTestCase
       assert_no_text '削除する'
     end
   end
+
+  test 'select box shows the practices that belong to a user course' do
+    login_user 'kimura', 'testtest'
+    visit questions_path
+    find('#select2-practice_id-container').click
+    # selects_size = Practice.all.size + 1
+    selects_size = users(:kimura).course.categories.map { |category| category.practices }.flatten.size + 1
+    assert_selector '.select2-results__option', count: selects_size
+  end
 end
