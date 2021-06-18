44e62c856063f653a6989704cc7d16430a589736
kanekodaichi <knkdic.bfc@gmail.com>
2021-06-18 09:58:42 +0900

    rubocopを通し修正

--- db/data/20210616072230_add_empty_string_to_nil_description.rb
+++ db/data/20210616072230_add_empty_string_to_nil_description.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class AddEmptyStringToNilDescription < ActiveRecord::Migration[6.1]
   def up
     User.find_each do |user|
       if user.description.nil?
-        user.description = "*自己紹介文はありません*"
+        user.description = '自己紹介文はありません。'
         user.save!
       end
     end
   end
 
   def down
-    puts "down"
+    raise ActiveRecord::IrreversibleMigration
   end
 end
