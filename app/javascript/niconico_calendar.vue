0615356a1c4ed5e722dc51f7844d2d9a554875ce
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-06-20 12:16:20 +0900

    前月、翌月を移動できるようにページャーを実装

--- app/javascript/niconico_calendar.vue
+++ app/javascript/niconico_calendar.vue
@@ -1,16 +1,47 @@
 <template lang="pug">
-.a-card
-  p { user }
+  .a-card
+    header.card-header.is-sm
+      h2.card-header__title
+        | ニコニコカレンダー
+    .card-body
+      .calendar__head
+        .calendar__pager--previous(@click='previousMonth')
+          | <
+        | {{ calendarMonth }}月
+        .calendar__pager--next(@click='nextMonth')
+          | >
+      table.niconico-calendar
+        thead.niconico-calendar__header
+          tr
+            th.niconico-calendar__header-day.is-sunday
+              | 日
+            th.niconico-calendar__header-day
+              | 月
+            th.niconico-calendar__header-day
+              | 火
+            th.niconico-calendar__header-day
+              | 水
+            th.niconico-calendar__header-day
+              | 木
+            th.niconico-calendar__header-day
+              | 金
+            th.niconico-calendar__header-day.is-saturday
+              | 土
+        tbody.niconico-calendar__body
 </template>
 
 <script>
 export default {
   props: {
-    userId: { type: String, required: true }
+    userId: {type: String, required: true}
   },
   data() {
     return {
-      reports: []
+      reports: [],
+      currentYear: new Date().getFullYear(),
+      currentMonth: new Date().getMonth() + 1,
+      calendarYear: new Date().getFullYear(),
+      calendarMonth: new Date().getMonth() + 1,
     }
   },
   mounted() {
@@ -24,24 +55,48 @@ export default {
           },
           credentials: 'same-origin'
         })
-          .then((response) => {
-            return response.json()
+        .then((response) => {
+          return response.json()
+        })
+        .then((json) => {
+          json.forEach((r) => {
+            this.reports.push(r)
           })
-          .then((json) => {
-            if (json[0]) {
-              json.forEach((r) => {
-                this.reports.push(r)
-              })
-            }
-          })
+        })
         .catch((error) => {
           console.warn('Failed to parsing', error)
         })
+  },
+  computed: {
+    calendarReports() {
+      return this.reports.filter(report =>
+          report.reported_on.includes(`${this.calendarYear}-${this.formatMonth(this.calendarMonth)}`)
+      )
+    },
   },
   methods: {
     token() {
       const meta = document.querySelector('meta[name="csrf-token"]')
       return meta ? meta.getAttribute('content') : ''
+    },
+    formatMonth(month) {
+      return month.toString().padStart(2, '0')
+    },
+    previousMonth() {
+      if (this.calendarMonth === 1) {
+        this.calendarMonth = 12
+        this.calendarYear--
+      } else {
+        return this.calendarMonth--
+      }
+    },
+    nextMonth() {
+      if (this.calendarMonth === 12) {
+        this.calendarMonth = 1
+        this.calendarYear++
+      } else {
+        return this.calendarMonth++
+      }
     }
   }
 }
