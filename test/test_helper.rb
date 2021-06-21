0cae034a1caa755e75264efe10b3a83c644b9299
komagata <komagata@gmail.com>
2021-06-21 13:34:20 +0900

    CI時にもテストを並列実行するように変更

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,7 +15,7 @@ Minitest::Retry.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors) unless ENV['CI']
+  parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
