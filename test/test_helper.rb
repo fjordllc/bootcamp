f73a3edabc091acfe02af21a543c77e3b3a079b3
komagata <komagata@gmail.com>
2021-06-21 15:46:47 +0900

    rubocop-minitestの指摘点を修正

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,7 +29,7 @@ class ActionDispatch::IntegrationTest
 end
 
 VCR.configure do |c|
-  #c.allow_http_connections_when_no_cassette = true
+  # c.allow_http_connections_when_no_cassette = true
   c.ignore_localhost = true
   c.cassette_library_dir = 'test/cassettes'
   c.hook_into :webmock
