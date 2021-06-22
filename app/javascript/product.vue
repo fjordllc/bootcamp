b88f205bf337edb7b1e3f588fe23a666e3088c32
gallardo <aiki.hosogaya@gmail.com>
2021-06-22 16:55:35 +0900

    lint修正

--- app/javascript/product.vue
+++ app/javascript/product.vue
@@ -89,9 +89,16 @@
 <script>
 import ProductChecker from './product_checker'
 export default {
-  props: ['product', 'currentUserId', 'isMentor', 'latestProductSubmittedJust5days', 'latestProductSubmittedJust6days', 'latestProductSubmittedOver7days'],
   components: {
     'product-checker': ProductChecker
+  },
+  props: {
+    product: { type: Object, required: true },
+    isMentor: { type: Boolean, required: true },
+    currentUserId: { type: String, required: true },
+    latestProductSubmittedJust5days: { type: String, required: false, default: null },
+    latestProductSubmittedJust6days: { type: String, required: false, default: null },
+    latestProductSubmittedOver7days: { type: String, required: false, default: null },
   },
   computed: {
     roleClass() {
@@ -105,6 +112,8 @@ export default {
         ? `★${this.product.practice.title}の提出物`
         : `${this.product.practice.title}の提出物`
     },
+  },
+  methods: {
     isLatestProductSubmittedJust5days() {
       if (this.latestProductSubmittedJust5days !== null) {
         return this.product.id === this.latestProductSubmittedJust5days.id
