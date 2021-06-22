7d6c58f57d70bc916a8c2e1acc2239fa0725e480
kanekodaichi <knkdic.bfc@gmail.com>
2021-06-22 16:11:43 +0900

    Revert "update_allを用いて処理を書き直し"
    This reverts commit 433ad13956edbc66c13be3abd6adf890535fcfde.

--- db/data/20210616072230_add_empty_string_to_nil_description.rb
+++ db/data/20210616072230_add_empty_string_to_nil_description.rb
@@ -2,9 +2,12 @@
 
 class AddEmptyStringToNilDescription < ActiveRecord::Migration[6.1]
   def up
-    now = Time.current
-
-    User.where(description: nil).update_all(description: '自己紹介文はありません。', updated_at: now)
+    User.find_each do |user|
+      if user.description.nil?
+        user.description = '自己紹介文はありません。'
+        user.save!
+      end
+    end
   end
 
   def down
