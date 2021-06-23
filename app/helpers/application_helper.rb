da04fa46fbbd4cb1b713eeae0dbdf6de29b5ee6b
kanekodaichi <knkdic.bfc@gmail.com>
2021-06-24 01:02:17 +0900

    md2htmlメソッドをredcarpetを用いて書き換える

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,24 @@ module ApplicationHelper
   end
 
   def md2html(text)
-    html = CommonMarker.render_html(text) unless text.nil?
-    raw(html) # rubocop:disable Rails/OutputSafety
+    unless text.nil?
+      options = {
+        filter_html: true,
+        autolink: true,
+        space_after_headers: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        tables: true,
+        hard_wrap: true,
+        xhtml: true,
+        lax_html_blocks: true,
+        strikethrough: true
+      }
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer)
+    end
+
+    markdown.render(text).html_safe
   end
 
   def md_summary(comment, word_count)
