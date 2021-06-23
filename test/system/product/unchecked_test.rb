e1de7eb05ba3340e972be232ddd45c5d18d3956a
gallardo <aiki.hosogaya@gmail.com>
2021-06-23 18:30:49 +0900

    fixture/product.yml変更に伴いテストを修正

--- test/system/product/unchecked_test.rb
+++ test/system/product/unchecked_test.rb
@@ -32,7 +32,7 @@ class ProductsTest < ApplicationSystemTestCase
     click_button '未チェックの提出物を一括で開く'
 
     within_window(windows.last) do
-      assert_text 'テストの提出物1です。'
+      assert_text 'テストの提出物60です。'
     end
   end
 end
