3fa181da192cbf6f3ff07904c97c4b9c359b478c
kanekodaichi <knkdic.bfc@gmail.com>
2021-06-25 18:33:45 +0900

    既存のcommonmarkerでオプションを指定して再実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,24 +8,8 @@ module ApplicationHelper
   end
 
   def md2html(text)
-    unless text.nil?
-      options = {
-        filter_html: true,
-        autolink: true,
-        space_after_headers: true,
-        no_intra_emphasis: true,
-        fenced_code_blocks: true,
-        tables: true,
-        hard_wrap: true,
-        xhtml: true,
-        lax_html_blocks: true,
-        strikethrough: true
-      }
-      renderer = Redcarpet::Render::HTML.new(options)
-      markdown = Redcarpet::Markdown.new(renderer)
-    end
-
-    markdown.render(text).html_safe
+    html = CommonMarker.render_html(text, :HARDBREAKS) unless text.nil?
+    raw(html) # rubocop:disable Rails/OutputSafety
   end
 
   def md_summary(comment, word_count)
