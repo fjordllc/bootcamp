63081ad42cc99564fccc8b24395b76f1d5f63ba8
udaikue <ikueikue@gmail.com>
2021-06-29 23:10:36 +0900

    名前の表記を()に変更

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -32,8 +32,10 @@ module UserDecorator
   end
 
   def icon_title
-    [login_name, name, staff_roles].reject(&:blank?)
-                                   .join(': ')
+    ["#{login_name} (#{name})", staff_roles].reject(&:blank?)
+                                            .join(': ')
+    # [login_name, name, staff_roles].reject(&:blank?)
+    #                                .join(': ')
   end
 
   def url
