d77300a5d508eaabc356b7c8fad4ac21294fb3d5
komagata <komagata@gmail.com>
2021-06-29 21:12:36 +0900

    テストの細部を修正
    - admin画面ではないテストがadminの分類になっていたのを修正。
    - visitをvisit_with_authに修正。

--- test/system/mentor_mode_test.rb
+++ test/system/mentor_mode_test.rb
@@ -2,17 +2,16 @@
 
 require 'application_system_test_case'
 
-class Admin::MentorModeTest < ApplicationSystemTestCase
-  test 'admin show/hide mentor-user-memo' do
-    login_user 'komagata', 'testtest'
-    visit user_path(users(:hatsuno))
+class MentorModeTest < ApplicationSystemTestCase
+  test 'show/hide mentor-user-memo' do
+    visit_with_auth user_path(users(:hatsuno)), 'komagata'
     assert_text 'メンター向けユーザーメモ'
     find(:css, '#checkbox-mentor-mode').set(false)
     assert_no_text 'メンター向けユーザーメモ'
   end
 
   test "student don't show mentor-user-memo" do
-    login_user 'kimura', 'testtest'
+    visit_with_auth user_path(users(:hatsuno)), 'kimura'
     assert_no_text 'メンター向けユーザーメモ'
   end
 end
