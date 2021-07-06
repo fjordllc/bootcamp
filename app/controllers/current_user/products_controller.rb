f27ffc252ad902358cffca1994db6e0e4274dcdb
yana-gi <yanagisawa.hika@gmail.com>
2021-07-06 13:00:45 +0900

    コントローラーとビューを仮実装

--- app/controllers/current_user/products_controller.rb
+++ app/controllers/current_user/products_controller.rb
@@ -1,4 +1,23 @@
+# frozen_string_literal: true
+
 class CurrentUser::ProductsController < ApplicationController
-  def index
+  before_action :require_login
+  before_action :set_user
+  before_action :set_products
+
+  def index; end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_products
+    @products = user.products.list
+  end
+
+  def user
+    @user ||= current_user
   end
 end
