3fb19633b3139aaf48748734cf4437ac37a82e9d
komagata <komagata@gmail.com>
2021-07-07 14:56:13 +0900

    企業管理画面にアドバイザーサインアップURLを追加
    企業向けにアドバイザーの登録が増えてきたので、URLを渡す機会が
    増えたため。

---
+++ test/decorators/company_decorator_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CompanyDecoratorTest < ActiveSupport::TestCase
+  def setup
+    controller = ApplicationController.new
+    controller.request = ActionDispatch::TestRequest.create
+    ActiveDecorator::ViewContext.push controller.view_context
+    @company1 = ActiveDecorator::Decorator.instance.decorate(companies(:company1))
+  end
+
+  test '#adviser_sign_up_url' do
+    expected = "http://test.host/users/new?company_id=#{@company1.id}&role=adviser&token=token"
+    assert_equal expected, @company1.adviser_sign_up_url
+  end
+end
