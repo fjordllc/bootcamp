2991dc48db89c984d0f3a05a6cd9f2f7ee922f23
teppei machida <machida@fjord.jp>
2021-07-08 15:14:39 +0900

    自分の日報のページのタイトルを自分の日報にした

--- test/system/current_user/reports_test.rb
+++ test/system/current_user/reports_test.rb
@@ -5,6 +5,6 @@ require 'application_system_test_case'
 class CurrentUser::ReportsTest < ApplicationSystemTestCase
   test 'show current_user reports when current_user is student' do
     visit_with_auth '/current_user/reports', 'hatsuno'
-    assert_equal 'hatsunoの日報 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
+    assert_equal '自分の日報 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
 end
