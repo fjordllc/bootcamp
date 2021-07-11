2c37205fc36947ea2b233e36fb93011935e9e6f2
FUGA0618 <aax.chiri@gmail.com>
2021-07-11 17:08:02 +0900

    Usersテーブルからslack_account,slack_participationカラムを削除するmigrationファイルを作成

---
+++ db/migrate/20210711080417_remove_slack_account_and_slack_participation_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveSlackAccountAndSlackParticipationFromUsers < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :slack_account, :string
+    remove_column :users, :slack_participation, :boolean, default: true, null: false
+  end
+end
