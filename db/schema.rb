abc0cc914938ec8446c1807f2096426028f51744
gozasoro <62997922+gozasoro@users.noreply.github.com>
2021-07-12 00:51:17 +0900

    schema.rbを元に戻した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_082712) do
+ActiveRecord::Schema.define(version: 2021_06_07_171344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
