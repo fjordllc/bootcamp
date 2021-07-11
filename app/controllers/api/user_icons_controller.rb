944c4d48d687f9e15303f2a7a6427ac555d6ab0c
gozasoro <62997922+gozasoro@users.noreply.github.com>
2021-07-11 21:48:19 +0900

    controllerとroutes.rbを修正

--- app/controllers/api/user_icons_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class API::UserIconsController < API::BaseController
-  def show
-    # user_icon_url = view_context.url_for(User.find_by(login_name: params[:login_name]).avatar)
-    user_icon_url = rails_storage_proxy_url(User.find_by(login_name: params[:login_name]).avatar)
-    render json: user_icon_url.to_json
-  end
-end
