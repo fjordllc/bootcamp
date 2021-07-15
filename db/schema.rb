7b4e273675878ce5cbcfaadaf58b73a916bad2ca
harukus <43bw.hana@gmail.com>
2021-07-15 10:29:12 +0900

    参考書籍をAmazonECSを使わない方式に変更
    AmazonECSのAPIがほぼ使えなくなったので。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_05_28_112148) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
