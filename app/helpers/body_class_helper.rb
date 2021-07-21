34c930e038bc2330582bbdcb8727f6d379cb3210
teppei machida <machida@fjord.jp>
2021-07-21 15:26:42 +0900

    :cop:

--- app/helpers/body_class_helper.rb
+++ app/helpers/body_class_helper.rb
@@ -19,9 +19,7 @@ module BodyClassHelper
   end
 
   def adviser_mode
-    if adviser_login?
-      'is-adviser-mode'
-    end
+    'is-adviser-mode' if adviser_login?
   end
 
   def page_area
@@ -41,7 +39,8 @@ module BodyClassHelper
   def body_class(options = {})
     extra_body_classes_symbol = options[:extra_body_classes_symbol] || :extra_body_classes
     qualified_controller_name = controller.controller_path.tr('/', '-')
-    basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name} is-#{page_category} is-#{page_area} is-#{Rails.env} #{adviser_mode}"
+    controller_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
+    basic_body_class = "#{controller_class} is-#{page_category} is-#{page_area} is-#{Rails.env} #{adviser_mode}"
 
     if content_for?(extra_body_classes_symbol)
       [basic_body_class, content_for(extra_body_classes_symbol)].join(' ')
