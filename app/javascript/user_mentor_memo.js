4d33ad2717f6a0f86e7f0b6df644762352d6260d
FUGA0618 <aax.chiri@gmail.com>
2021-07-24 20:01:35 +0900

    提出物個別ページのメンター用ブロックに「ユーザーメモ」のタブを追加

--- app/javascript/user_mentor_memo.js
+++ app/javascript/user_mentor_memo.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const userMentorMemo = document.querySelector(selector)
   if (userMentorMemo) {
     const userId = userMentorMemo.getAttribute('data-user-id')
+    const productsMode = Boolean(userMentorMemo.getAttribute('data-products-mode'))
     new Vue({
       render: (h) =>
         h(UserMentorMemo, {
           props: {
-            userId: userId
+            userId: userId,
+            productsMode: productsMode
           }
         })
     }).$mount(selector)
