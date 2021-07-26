854911261d4c45e2d9427e1fb0eb651a1c524c77
kanekodaichi <knkdic.bfc@gmail.com>
2021-07-26 11:28:00 +0900

    未読の物を全て別タブで開くボタンを実装

--- app/javascript/notifications_bell.vue
+++ app/javascript/notifications_bell.vue
@@ -20,7 +20,7 @@ li.header-links__item(v-bind:class='hasCountClass')
     .header-dropdown__inner.is-notification
       ul.header-dropdown__items
         li.header-dropdown__item(v-for='notification in notifications')
-          a.header-dropdown__item-link(:href='notification.path')
+          a.header-dropdown__item-link.unconfirmed_link(:href='notification.path')
             .header-notifications-item__body
               img.header-notifications-item__user-icon.a-user-icon(
                 :src='notification.sender.avatar_url'
@@ -36,6 +36,7 @@ li.header-links__item(v-bind:class='hasCountClass')
           ref='nofollow',
           data-method='post'
         ) 全て既読にする
+        button.header-dropdown__footer-link(@click="openUnconfirmedItems()") 全て別タブで開く
 </template>
 <script>
 import dayjs from 'dayjs'
@@ -89,6 +90,15 @@ export default {
     },
     createdAtFromNow(createdAt) {
       return dayjs(createdAt).fromNow()
+    },
+    openUnconfirmedItems() {
+      const links = document.querySelectorAll(
+        '.header-dropdown__item-link.unconfirmed_link'
+      )
+      console.log(links)
+      links.forEach((link) => {
+        window.open(link.href, '_target', 'noopener')
+      })
     }
   }
 }
