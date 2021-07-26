9bd61766e7625b05927916c63a7735a4cddc6618
Rikuba <rikuba@live.jp>
2021-07-26 16:30:09 +0900

    ユーザー一覧に分報へのリンクを追加

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -218,4 +218,17 @@ class UsersTest < ApplicationSystemTestCase
     visit current_path
     assert has_link?(href: 'https://discord.gg/kimura-times')
   end
+
+  test 'show times link on user list page' do
+    visit_with_auth '/users', 'hatsuno'
+    wait_for_vuejs
+    has_no_link?(href: 'https://discord.gg/kimura-times')
+
+    kimura = users(:kimura)
+    kimura.update!(times_url: 'https://discord.gg/kimura-times')
+
+    visit current_path
+    wait_for_vuejs
+    has_link?(href: 'https://discord.gg/kimura-times')
+  end
 end
