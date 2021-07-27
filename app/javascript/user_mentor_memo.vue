9bba4820f8b22d1b68ca0ebb2367ea67007597a7
FUGA0618 <aax.chiri@gmail.com>
2021-07-27 21:44:43 +0900

    クォーテーションの漏れを修正

--- app/javascript/user_mentor_memo.vue
+++ app/javascript/user_mentor_memo.vue
@@ -17,12 +17,12 @@ section.a-card.is-memo.is-only-mentor
   div(:class='{ "a-card": productsMode, "thread-comment-form__form": !productsMode }', v-show='editing')
     div(:class='[{ "form-tabs": productsMode, "thread-comment-form__tabs": !productsMode }, "js-tabs"]')
       div(
-        :class='[{ "is-active": isActive("memo"), "form-tabs__tab": productsMode, "thread-comment-form__tab": !productsMode }, js-tabs__tab]',
+        :class='[{ "is-active": isActive("memo"), "form-tabs__tab": productsMode, "thread-comment-form__tab": !productsMode }, "js-tabs__tab"]',
         @click='changeActiveTab("memo")'
       )
         | メモ
       div(
-        :class='[{ "is-active": isActive("preview"), "form-tabs__tab": productsMode, "thread-comment-form__tab": !productsMode }, js-tabs__tab]',
+        :class='[{ "is-active": isActive("preview"), "form-tabs__tab": productsMode, "thread-comment-form__tab": !productsMode }, "js-tabs__tab"]',
         @click='changeActiveTab("preview")'
       )
         | プレビュー
