39bd37cc5561adc41d4c483d58ada5f5d9c0edce
makiichikawa <punkfolkfolk@gmail.com>
2021-08-14 17:37:26 +0900

    編集・更新処理前のログインチェックがメンターのみとなっていたため修正

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -57,6 +57,12 @@ module Authentication
     redirect_to root_path, alert: '管理者としてログインしてください'
   end
 
+  def require_admin_or_mentor_login
+    return if admin_or_mentor_login?
+
+    redirect_to root_path, alert: '管理者・メンターとしてログインしてください'
+  end
+
   def require_staff_login
     return if staff_login?
 
