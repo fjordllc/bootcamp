4db3b496e72b19c1ae1069c98ab730e51be466d5
komagata <komagata@gmail.com>
2021-08-18 02:10:35 +0900

    ブログに投稿者を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,6 +63,7 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :answers,      dependent: :destroy
   has_many :watches, dependent: :destroy
+  has_many :articles, dependent: :destroy
 
   has_many :participate_events,
            through: :participations,
