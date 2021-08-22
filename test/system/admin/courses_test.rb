617fba7ae516c0983b5dc114e6b1ac54363c9c96
rjtt17 <rjtt17@gmail.com>
2021-08-22 10:10:04 +0900

    メンター管理画面のコース一覧のテスト修正

--- test/system/admin/courses_test.rb
+++ test/system/admin/courses_test.rb
@@ -4,7 +4,7 @@ require 'application_system_test_case'
 
 class Admin::CoursesTest < ApplicationSystemTestCase
   test 'show listing courses' do
-    visit_with_auth '/courses', 'komagata'
+    visit_with_auth 'admin/courses', 'komagata'
     assert_equal 'コース一覧 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
 end
