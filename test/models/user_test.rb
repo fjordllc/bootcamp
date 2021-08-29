128574a12d012531e6a50112882fc5f800492cb3
rjtt17 <rjtt17@gmail.com>
2021-08-29 11:36:38 +0900

    completed_fractionメソッドのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -91,6 +91,32 @@ class UserTest < ActiveSupport::TestCase
     assert_equal old_percentage, user.completed_percentage
   end
 
+  test '#completed_fraction don\'t calculate practice that include_progress: false' do
+    user = users(:komagata)
+    old_fraction = user.completed_fraction
+    user.completed_practices << practices(:practice5)
+
+    assert_not_equal old_fraction, user.completed_fraction
+
+    old_fraction = user.completed_fraction
+    user.completed_practices << practices(:practice53)
+
+    assert_equal old_fraction, user.completed_fraction
+  end
+
+  test '#completed_fraction don\'t calculate practice unrelated cource' do
+    user = users(:komagata)
+    old_fraction = user.completed_fraction
+    user.completed_practices << practices(:practice5)
+
+    assert_not_equal old_fraction, user.completed_fraction
+
+    old_fraction = user.completed_fraction
+    user.completed_practices << practices(:practice52)
+
+    assert_equal old_fraction, user.completed_fraction
+  end
+
   test '#depressed?' do
     user = users(:kimura)
     2.times do |i|
