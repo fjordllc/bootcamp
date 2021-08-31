37a6370f6ebfa42aa51f6d1bc2aad5d917c0c889
makiichikawa <punkfolkfolk@gmail.com>
2021-08-31 21:55:02 +0900

    ‘ボタンを設置し、コンローラがtargetというパラメータの値を取得するように変更’

--- app/controllers/products/self_assigned_controller.rb
+++ app/controllers/products/self_assigned_controller.rb
@@ -2,5 +2,14 @@
 
 class Products::SelfAssignedController < ApplicationController
   before_action :require_staff_login
-  def index; end
+  def index
+    @target = params[:target]
+    @target = 'no_replied' unless target_allowlist.include?(@target)
+  end
+
+  private
+
+  def target_allowlist
+    %w[no_replied self_assigned_all]
+  end
 end
