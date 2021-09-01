f3d1caa5dbb56433521bae4e562870c86e95646d
harukus <43bw.hana@gmail.com>
2021-09-01 17:00:50 +0900

    提出物と同じような見た目にするために修正

--- app/controllers/api/users/recent_reports_controller.rb
+++ app/controllers/api/users/recent_reports_controller.rb
@@ -3,6 +3,6 @@
 class API::Users::RecentReportsController < API::BaseController
   def index
     @user = User.find(params[:user_id])
-    @reports = @user.reports.limit(5).offset(1).order(reported_on: :DESC)
+    @reports = @user.reports.limit(10).order(reported_on: :DESC)
   end
 end
