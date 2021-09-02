bcd84284998b245f33cb1b909b91ec74c16eeb3d
masamune17 <2396niisankyumu@gmail.com>
2021-09-03 02:26:56 +0900

    current_user内のbookmarks_controllerを修正

--- app/controllers/current_user/bookmarks_controller.rb
+++ app/controllers/current_user/bookmarks_controller.rb
@@ -2,6 +2,22 @@
 
 class CurrentUser::BookmarksController < ApplicationController
   before_action :require_login
+  before_action :set_user
+  # before_action :set_bookmarks
 
   def index; end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_bookmarks
+    @bookmarks = user.bookmarks.list.page(params[:page])
+  end
+
+  def user
+    @user ||= current_user
+  end
 end
