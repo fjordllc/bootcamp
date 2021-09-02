213c0f5a08375f893befd3b0f9e35f692e486d4c
masamune17 <2396niisankyumu@gmail.com>
2021-09-03 03:34:55 +0900

    userモデルにbookmarksモデルを関連付け、current_user内のbookmarks_controllerを修正

--- app/controllers/current_user/bookmarks_controller.rb
+++ app/controllers/current_user/bookmarks_controller.rb
@@ -3,7 +3,6 @@
 class CurrentUser::BookmarksController < ApplicationController
   before_action :require_login
   before_action :set_user
-  # before_action :set_bookmarks
 
   def index; end
 
@@ -11,10 +10,6 @@ class CurrentUser::BookmarksController < ApplicationController
 
   def set_user
     @user = current_user
-  end
-
-  def set_bookmarks
-    @bookmarks = user.bookmarks.list.page(params[:page])
   end
 
   def user
