cad0a4e8b2e46c63f1582ba925f29525cd18f819
Gen Tamura <gen.tamura84@gmail.com>
2021-09-08 18:12:17 +0900

    Prettierのwarningsを修正

--- app/javascript/agreements.js
+++ app/javascript/agreements.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const checkboxes =  Array.from(document.querySelectorAll('.js-agreements-checkbox'))
+  const checkboxes = Array.from(
+    document.querySelectorAll('.js-agreements-checkbox')
+  )
   const submit = document.querySelector('.js-agreements-submit')
 
   checkboxes.forEach((element) => {
     element.addEventListener('click', () => {
-      const isSubmit = checkboxes.every(element => element.checked)
+      const isSubmit = checkboxes.every((element) => element.checked)
 
       if (isSubmit) {
         submit.classList.remove('is-disabled')
