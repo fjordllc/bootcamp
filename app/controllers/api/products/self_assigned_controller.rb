18d88b83ff3c0a2e91a60aca6ed4f2e8266748c8
makiichikawa <punkfolkfolk@gmail.com>
2021-09-09 10:12:45 +0900

    メソッドチェーンが長かったため、読みやすくするために改行

--- app/controllers/api/products/self_assigned_controller.rb
+++ app/controllers/api/products/self_assigned_controller.rb
@@ -7,9 +7,15 @@ class API::Products::SelfAssignedController < API::BaseController
     @target = 'self_assigned_no_replied' unless target_allowlist.include?(@target)
     @products = case @target
                 when 'self_assigned_all'
-                  Product.self_assigned_product(current_user.id).unchecked.list.page(params[:page])
+                  Product.self_assigned_product(current_user.id)
+                         .unchecked
+                         .list
+                         .page(params[:page])
                 when 'self_assigned_no_replied'
-                  Product.self_assigned_no_replied_products(current_user.id).unchecked.list.page(params[:page])
+                  Product.self_assigned_no_replied_products(current_user.id)
+                         .unchecked
+                         .list
+                         .page(params[:page])
                 end
   end
 
