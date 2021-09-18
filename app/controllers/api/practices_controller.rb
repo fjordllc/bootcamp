d2b2987e9e0b31b6b252460931ded850c5035d84
konaga-k <priests2020@gmail.com>
2021-09-18 15:29:19 +0900

    具体的なuser_idをAPIに送らずに済むように修正

--- app/controllers/api/practices_controller.rb
+++ app/controllers/api/practices_controller.rb
@@ -8,7 +8,7 @@ class API::PracticesController < API::BaseController
   def show; end
 
   def index
-    @categories = params[:user_id].present? ? User.find(params[:user_id]).course.categories : Category
+    @categories = params[:scoped_by_user] ? current_user.course.categories : Category
     @categories = @categories
                   .eager_load(:practices)
                   .where.not(practices: { id: nil })
