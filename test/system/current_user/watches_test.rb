4f7151e2cc63a0e7854f947697a2c94a75a31f49
yuma <ikeoku.y@gmail.com>
2021-09-24 10:39:56 +0900

    テストの自分のWatch中をWatch中に変更

--- test/system/current_user/watches_test.rb
+++ test/system/current_user/watches_test.rb
@@ -5,6 +5,6 @@ require 'application_system_test_case'
 class CurrentUser::WatchesTest < ApplicationSystemTestCase
   test 'show current_user watches when current_user is student' do
     visit_with_auth '/current_user/watches', 'kimura'
-    assert_equal '自分のWatch中 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
+    assert_equal 'Watch中 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
 end
