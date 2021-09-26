f1e19bc1db6a4196bd8c0dc0a559ce4da306afe9
Gen Tamura <gen.tamura84@gmail.com>
2021-09-26 15:27:24 +0900

    bulletで指摘されているN+1を修正

--- app/controllers/api/reports/recents_controller.rb
+++ app/controllers/api/reports/recents_controller.rb
@@ -3,7 +3,7 @@
 class API::Reports::RecentsController < API::BaseController
   def index
     @reports = Report
-               .includes([{ user: [{ avatar_attachment: :blob }, :company] }, :checks])
+               .includes(:checks, user: [{ avatar_attachment: :blob }, :company])
                .not_wip
                .order(reported_on: :desc, id: :desc)
                .limit(20)
