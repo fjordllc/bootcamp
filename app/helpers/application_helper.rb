e0c45826b367cae2e23c64c499562ff80484505c
yamadaab <s.yamada.1125@gmail.com>
2021-09-30 15:03:50 +0900

    正規表現のエスケープ処理に対応して修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,35 +21,16 @@ module ApplicationHelper
 
   def searchable_summary(comment, word_count, word = '')
     summary = strip_tags(md2html(comment)).gsub(/[\r\n]/, '')
-    return truncate(summary, length: word_count) if word.blank?
+    words = word.split(/[[:space:]]+/).compact.reject(&:empty?) unless word.nil?
+    return truncate(summary, length: word_count) if words.blank?
 
-    words = word.split(/[[:space:]]/).compact.reject(&:empty?)
-    word_indexes = create_indexes(summary, words)
-    first_match_word_index = word_indexes.compact.min
-    return truncate(summary, length: word_count) if first_match_word_index.nil?
+    words_pattern = words.map { |keyword| Regexp.escape(keyword) }.join('|')
+    words_regexp = Regexp.new(words_pattern, Regexp::IGNORECASE)
+    match = words_regexp.match(comment)
+    return truncate(summary, length: word_count) if match.nil?
 
-    start_index = first_match_word_index - EXTRACTING_CHARACTERS
-
-    first_match_word = words[word_indexes.index(first_match_word_index)]
-    matched_characters_before_and_after_word(summary, first_match_word, start_index)
-  end
-
-  def create_indexes(summary, words)
-    words.map do |keyword|
-      summary.index(/#{keyword}/i)
-    end
-  end
-
-  def matched_characters_before_and_after_word(summary, first_match_word, start_index)
-    summary =~ /#{first_match_word}/i
-    match_before = if start_index >= 0
-                     Regexp.last_match.pre_match.slice(start_index, EXTRACTING_CHARACTERS)
-                   else
-                     Regexp.last_match.pre_match.slice(0, EXTRACTING_CHARACTERS)
-                   end
-    match_word = Regexp.last_match[0]
-    match_after = Regexp.last_match.post_match.slice(0, EXTRACTING_CHARACTERS)
-
-    match_before + match_word + match_after
+    begin_offset = (match.begin(0) - EXTRACTING_CHARACTERS).clamp(0, Float::INFINITY)
+    end_offset = match.end(0) + EXTRACTING_CHARACTERS
+    comment[begin_offset...end_offset]
   end
 end
