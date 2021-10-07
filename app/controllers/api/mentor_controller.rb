d9f942dd6d43117187b43d3535f35da01429b277
kaiyu-tech <kaiyu326@gmail.com>
2021-10-07 19:10:47 +0900

    インデントの修正

--- app/controllers/api/mentor_controller.rb
+++ app/controllers/api/mentor_controller.rb
@@ -6,9 +6,9 @@ class API::MentorController < API::BaseController
 
   def index
     @worried_users = User.joins('JOIN (SELECT user_id, MAX(published_at) as published_at FROM products GROUP BY user_id) products ON users.id = user_id')
-                          .where("published_at <= CURRENT_DATE - interval '2 week'")
-                          .order('published_at DESC')
-                          .page(params[:page])
-                          .per(PAGER_NUMBER)
+                         .where("published_at <= CURRENT_DATE - interval '2 week'")
+                         .order('published_at DESC')
+                         .page(params[:page])
+                         .per(PAGER_NUMBER)
   end
 end
