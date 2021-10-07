311ae16b836a4b8c3ac42495ffcf91224f7e27a6
udaikue <ikueikue@gmail.com>
2021-10-07 15:31:40 +0900

    テストを修正

--- test/integration/api/users/companies_test.rb
+++ test/integration/api/users/companies_test.rb
@@ -3,12 +3,14 @@
 require 'test_helper'
 
 class API::Users::CompaniesTest < ActionDispatch::IntegrationTest
-  test 'GET /api/users/companies.json' do
-    get api_users_companies_path(format: :json)
-    assert_response :unauthorized
-
+  test 'should get companies' do
     token = create_token('kimura', 'testtest')
     get api_users_companies_path(format: :json), headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :ok
+  end
+
+  test 'should not get companies' do
+    get api_users_companies_path(format: :json)
+    assert_response :unauthorized
   end
 end
