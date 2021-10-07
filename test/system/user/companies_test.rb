8050e6233aa8a692de510c86a257e73aaf7a2081
udaikue <ikueikue@gmail.com>
2021-10-07 15:31:40 +0900

    テストを修正

--- test/system/user/companies_test.rb
+++ test/system/user/companies_test.rb
@@ -3,14 +3,10 @@
 require 'application_system_test_case'
 
 class User::CompaniesTest < ApplicationSystemTestCase
-  test 'show companies' do
+  test 'show companies with users' do
     visit_with_auth '/users/companies', 'komagata'
     assert_equal '企業別 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
     assert_text companies(:company1).name
-  end
-
-  test 'not show no users companies' do
-    visit_with_auth '/users/companies', 'komagata'
     assert_no_text companies(:company3).name
   end
 end
