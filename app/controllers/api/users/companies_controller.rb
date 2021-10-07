9bde8116035d77ec21ea32ad0058ab9fac8bff65
udaikue <ikueikue@gmail.com>
2021-10-07 12:40:48 +0900

    不要なbefore_actionを削除

--- app/controllers/api/users/companies_controller.rb
+++ app/controllers/api/users/companies_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class API::Users::CompaniesController < API::BaseController
-  before_action :require_login_for_api
-
   def index
     @companies = Company.with_attached_logo.order(:id)
   end
