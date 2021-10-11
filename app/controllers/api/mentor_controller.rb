9011d47845a175e304c74480af3f2fc10377d7d2
kaiyu-tech <kaiyu326@gmail.com>
2021-10-11 21:01:49 +0900

    ページャーが不要だったため除去

--- app/controllers/api/mentor_controller.rb
+++ app/controllers/api/mentor_controller.rb
@@ -2,13 +2,11 @@
 
 class API::MentorController < API::BaseController
   before_action :require_login
-  PAGER_NUMBER = 5
 
   def index
-    @worried_users = User.joins('JOIN (SELECT user_id, MAX(published_at) as published_at FROM products GROUP BY user_id) products ON users.id = user_id')
-                         .where("published_at <= CURRENT_DATE - interval '2 week'")
-                         .order('published_at DESC')
-                         .page(params[:page])
-                         .per(PAGER_NUMBER)
+    @worried_users = User.joins(:products)
+                         .select('users.*', :published_at)
+                         .where('published_at <= ?', 2.weeks.ago.to_date)
+                         .order(published_at: :desc)
   end
 end
