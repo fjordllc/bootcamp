74388a53ed5150d102b6dd9118253de501beb95b
udaikue <ikueikue@gmail.com>
2021-10-14 17:39:29 +0900

    コンフリクトの解消

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -55,8 +55,5 @@ require('../bookmarks.js')
 require('../events.js')
 require('../agreements.js')
 require('../incoming-events.js')
-<<<<<<< HEAD
 require('../practice-select.js')
-=======
 require('../companies.js')
->>>>>>> e07ec959a (JSファイルを作成)
