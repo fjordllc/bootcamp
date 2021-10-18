83b7797daa3c29ab8806bbd21f13adc1a9b4042d
konaga-k <priests2020@gmail.com>
2021-10-19 00:04:13 +0900

    ローディングのプレースホルダを再利用可能なコンポーネントに修正

---
+++ app/javascript/loading-list.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.thread-list.a-card.is-loading
+  loadingListItem(v-for='num in itemCount', :key='num')
+</template>
+
+<script>
+import LoadingListItem from './loading-list-item.vue'
+
+export default {
+  components: {
+    loadingListItem: LoadingListItem
+  },
+  data() {
+    return {
+      itemCount: 8
+    }
+  }
+}
+</script>
