93b8bba5e7c0f5f17ffc12bb9ab83dcfd2ac0d68
kaiyu-tech <kaiyu326@gmail.com>
2021-10-20 20:46:22 +0900

    対象ユーザの絞り込み部分をscope化

--- app/controllers/api/users/worried_controller.rb
+++ app/controllers/api/users/worried_controller.rb
@@ -4,14 +4,6 @@ class API::Users::WorriedController < API::BaseController
   before_action :require_login
 
   def index
-    sql = Learning.select(:user_id, 'MAX(updated_at) AS completed_at')
-                  .where(status: 3)
-                  .group(:user_id).to_sql
-
-    @worried_users = User.students_and_trainees
-                         .joins("JOIN (#{sql}) learnings ON users.id = user_id")
-                         .select('users.*', :completed_at)
-                         .where('completed_at <= ?', 2.weeks.ago.end_of_day)
-                         .order(completed_at: :asc)
+    @worried_users = User.worried.order(completed_at: :asc)
   end
 end
