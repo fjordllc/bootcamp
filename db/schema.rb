7a02a4ef1076aca77acaee1ec56b7cc685ea2e1c
yoichi <itiyou14@gmail.com>
2021-10-22 07:57:20 +0900

    blog_urlカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_140518) do
+ActiveRecord::Schema.define(version: 2021_10_21_225338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 2021_10_17_140518) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "tos"
+    t.string "blog_url"
   end
 
   create_table "courses", force: :cascade do |t|
