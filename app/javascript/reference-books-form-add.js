015392c1246523cc982da8ce3592f5d71a6ceaaf
rjtt17 <rjtt17@gmail.com>
2021-10-30 23:01:29 +0900

    Lintの指摘事項を修正

--- app/javascript/reference-books-form-add.js
+++ app/javascript/reference-books-form-add.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  $('.reference-books-form__add')
-  .on('cocoon:after-insert', () => {
+  $('.reference-books-form__add').on('cocoon:after-insert', () => {
     $('.js-select2').select2({
       closeOnSelect: true
     })
