53dce78544db388348d9984f4cdf08efaf003aff
yoichi <itiyou14@gmail.com>
2021-11-03 10:45:29 +0900

    学習日が未来日では保存できないようにした/testの修正

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -546,11 +546,11 @@ class ReportsTest < ApplicationSystemTestCase
   test 'cannot post a new report with future date' do
     visit_with_auth '/reports/new', 'komagata'
     within('#new_report') do
-      fill_in('report[title]', with: '未来日では日報を作成できない')
+      fill_in('report[title]', with: '学習日が未来日では日報を作成できない')
       fill_in('report[description]', with: 'エラーになる')
-      fill_in('report[reported_on]', with: Time.current.next_day)
+      fill_in('report[reported_on]', with: Date.current.next_day)
     end
     click_button '提出'
-    assert_text '未来の日付では日報を保存できません。'
+    assert_text '学習日は今日以前の日付にしてください'
   end
 end
