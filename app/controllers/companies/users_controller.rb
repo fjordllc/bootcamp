fec8f34eb5a5a030d34e5c51ee58a815d87e3de2
yoichi <itiyou14@gmail.com>
2021-11-05 23:16:47 +0900

    企業別のユーザー一覧で「研修生」「全て」の区分け機能を作成

--- app/controllers/companies/users_controller.rb
+++ app/controllers/companies/users_controller.rb
@@ -4,7 +4,23 @@ class Companies::UsersController < ApplicationController
   before_action :require_login
 
   def index
+    @target = params[:target]
+    @target = 'trainee' unless target_allowlist.include?(@target)
     @company = Company.find(params[:company_id])
-    @users = User.with_attached_avatar.where(company: @company).order(updated_at: :desc)
+
+    target_users =
+      if params[:tag]
+        User.tagged_with(params[:tag])
+      else
+        User.users_role(@target)
+      end
+
+    @users = target_users.with_attached_avatar.where(company: @company).order(updated_at: :desc)
+  end
+
+  private
+
+  def target_allowlist
+    %w[trainee all]
   end
 end
