128e32cacec2187b07129e1bbf9ca43aba23f326
yoichi <itiyou14@gmail.com>
2021-11-05 23:55:50 +0900

    テストの追加

--- test/system/company/users_test.rb
+++ test/system/company/users_test.rb
@@ -7,4 +7,17 @@ class Company::UsersTest < ApplicationSystemTestCase
     visit_with_auth "/companies/#{companies(:company1).id}/users", 'kimura'
     assert_equal 'Fjord Inc.所属ユーザー | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
+
+  test 'show users by user category' do
+    visit_with_auth "/companies/#{companies(:company2).id}/users", 'kimura'
+    # デフォルトは研修生のユーザーを表示
+    # Senpai Ichiro != 研修生
+    assert_no_text 'Senpai Ichiro'
+
+    click_link '全員'
+    assert_text 'Senpai Ichiro'
+
+    click_link '研修生'
+    assert_no_text 'Senpai Ichiro'
+  end
 end
