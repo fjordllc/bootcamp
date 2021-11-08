43bb4b9f917c4903a76b79569dd786635bb18d0a
IkumaTadokoro <tadokorodev@gmail.com>
2021-11-08 15:26:19 +0900

    カテゴリ別既読処理用のControllerを作成

---
+++ app/controllers/notifications/categorymarks_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Notifications::CategorymarksController < ApplicationController
+  def create
+    target = params[:target].presence&.to_sym
+    @notifications = current_user.notifications.by_target(target)
+    @notifications.update(read: true, updated_at: Time.current)
+    redirect_to notifications_path(target: target), notice: '既読にしました'
+  end
+end
