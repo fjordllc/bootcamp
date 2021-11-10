cacae215511589ebd1c74ecf76d51e99bb36985f
IkumaTadokoro <tadokorodev@gmail.com>
2021-11-11 08:12:35 +0900

    不要な更新日時指定を削除
    Rails側で自動設定されるため

--- app/controllers/notifications/categorymarks_controller.rb
+++ app/controllers/notifications/categorymarks_controller.rb
@@ -4,7 +4,7 @@ class Notifications::CategorymarksController < ApplicationController
   def create
     target = params[:target].presence&.to_sym
     notifications = current_user.notifications.by_target(target)
-    notifications.update(read: true, updated_at: Time.current)
+    notifications.update(read: true)
     redirect_to notifications_path(target: target), notice: '既読にしました'
   end
 end
