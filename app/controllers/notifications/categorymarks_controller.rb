04f3589ec4743c1eef5a750dd1fcd84e2e7d9428
IkumaTadokoro <tadokorodev@gmail.com>
2021-11-11 08:47:24 +0900

    UPDATE対象の絞り込み条件に"未読"を追加

--- app/controllers/notifications/categorymarks_controller.rb
+++ app/controllers/notifications/categorymarks_controller.rb
@@ -3,7 +3,7 @@
 class Notifications::CategorymarksController < ApplicationController
   def create
     target = params[:target].presence&.to_sym
-    notifications = current_user.notifications.by_target(target)
+    notifications = current_user.notifications.by_target(target).unreads
     notifications.update(read: true)
     redirect_to notifications_path(target: target), notice: '既読にしました'
   end
