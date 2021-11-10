b55d4d419baf6a455e6f666de8cf0964b1280ef4
teppei machida <machida@fjord.jp>
2021-11-10 15:17:42 +0900

    ユーザー一覧にユーザー秘密情報を表示しないようにした

--- app/javascript/user-sns.vue
+++ app/javascript/user-sns.vue
@@ -2,37 +2,45 @@
 .sns-links
   ul.sns-links__items.is-button-group
     li.sns-links__item
-      a.sns-links__item-link.a-button.is-xs.is-secondary.is-icon(
+      a.sns-links__item-link.a-button.is-sm.is-secondary.is-icon(
         v-if='user.github_account',
         :href='githubUrl'
       )
         i.fab.fa-github-alt
-      .sns-links__item-link.a-button.is-xs.is-disabled.is-icon(v-else)
+      .sns-links__item-link.a-button.is-sm.is-disabled.is-icon(v-else)
         i.fab.fa-github-alt
     li.sns-links__item
-      a.sns-links__item-link.a-button.is-xs.is-secondary.is-icon(
+      a.sns-links__item-link.a-button.is-sm.is-secondary.is-icon(
         v-if='user.twitter_account',
         :href='twitterUrl'
       )
         i.fab.fa-twitter
-      .sns-links__item-link.a-button.is-xs.is-disabled.is-icon(v-else)
+      .sns-links__item-link.a-button.is-sm.is-disabled.is-icon(v-else)
         i.fab.fa-twitter
     li.sns-links__item
-      a.sns-links__item-link.a-button.is-xs.is-secondary.is-icon(
+      a.sns-links__item-link.a-button.is-sm.is-secondary.is-icon(
         v-if='user.facebook_url',
         :href='user.facebook_url'
       )
         i.fab.fa-facebook
-      .sns-links__item-link.a-button.is-xs.is-disabled.is-icon(v-else)
+      .sns-links__item-link.a-button.is-sm.is-disabled.is-icon(v-else)
         i.fab.fa-facebook
     li.sns-links__item
-      a.sns-links__item-link.a-button.is-xs.is-secondary.is-icon(
+      a.sns-links__item-link.a-button.is-sm.is-secondary.is-icon(
         v-if='user.blog_url',
         :href='user.blog_url'
       )
-        i.fas.fa-file
-      .sns-links__item-link.a-button.is-xs.is-disabled.is-icon(v-else)
-        i.fas.fa-file
+        i.fas.fa-blog
+      .sns-links__item-link.a-button.is-sm.is-disabled.is-icon(v-else)
+        i.fas.fa-blog
+    li.sns-links__item
+      a.sns-links__item-link.a-button.is-sm.is-secondary.is-icon(
+        v-if='user.times_url',
+        :href='user.times_url'
+      )
+        i.far.fa-clock
+      .sns-links__item-link.a-button.is-sm.is-disabled.is-icon(v-else)
+        i.far.fa-clock
 </template>
 <script>
 export default {
