7f3f3edc2193e62cea7aebb0283d9ba853ba8786
rjtt17 <rjtt17@gmail.com>
2021-11-15 10:00:53 +0900

    選択肢のプルダウンをselect2をvue-multiselectに変更

--- app/javascript/reference-books-form-add.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  $('.reference-books-form__add').on('cocoon:after-insert', () => {
-    $('.js-select2').select2({
-      closeOnSelect: true
-    })
-  })
-})
