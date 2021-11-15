0e65371e6c7ead443dd1e4a4bc315304dba06616
yuma <ikeoku.y@gmail.com>
2021-11-15 19:26:52 +0900

    api/featured_entriesコントローラーを作成

---
+++ app/controllers/api/featured_entries_controller.rb
@@ -0,0 +1,17 @@
+class API::FeaturedEntriesController < ApplicationController
+  def create
+    @featured_entry = FeaturedEntry.create!(featureable: featureable)
+    render status: :created, json: @featured_entry
+  end
+
+  def destroy
+    FeaturedEntry.find(params[:id]).destroy
+    head :no_content
+  end
+
+  private
+
+  def featureable
+    params[:featureable_type].constantize.find_by(id: params[:featureable_id])
+  end
+end
---
+++ test/controllers/api/featured_entries_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class API::FeaturedEntriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
