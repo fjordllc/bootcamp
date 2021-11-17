5cb96f7f9a2a92c2cff5fa5e4e0c0e1375e5d503
mami-inuzuka <inuzuka.mami@gmail.com>
2021-11-17 18:48:52 +0900

    rubocopエラー解消

--- app/controllers/users/talk_controller.rb
+++ app/controllers/users/talk_controller.rb
@@ -4,8 +4,8 @@ class Users::TalkController < ApplicationController
   def show
     @user = current_user.admin? ? User.find(params[:user_id]) : current_user
 
-    if !current_user.admin? && params[:user_id].to_i != current_user.id
-      redirect_to user_talk_path(current_user)
-    end
+    return unless !current_user.admin? && params[:user_id].to_i != current_user.id
+
+    redirect_to user_talk_path(current_user)
   end
 end
