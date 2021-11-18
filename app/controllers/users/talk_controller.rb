46e780825eb814e8f8d998cb9ae6b3a8f8882eb8
mami-inuzuka <inuzuka.mami@gmail.com>
2021-11-18 18:28:27 +0900

    複雑になっている条件式を簡略化

--- app/controllers/users/talk_controller.rb
+++ app/controllers/users/talk_controller.rb
@@ -4,7 +4,7 @@ class Users::TalkController < ApplicationController
   def show
     @user = current_user.admin? ? User.find(params[:user_id]) : current_user
 
-    return unless !current_user.admin? && params[:user_id].to_i != current_user.id
+    return if current_user.admin? || params[:user_id].to_i == current_user.id
 
     redirect_to user_talk_path(current_user)
   end
