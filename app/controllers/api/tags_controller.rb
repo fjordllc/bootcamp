e8602a10b97ce805864e5d48e208a0fcd9191cc3
ikuma-t <tadokorodev@gmail.com>
2021-11-27 01:15:04 +0900

    API::TagsController#updateを作成

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -7,7 +7,31 @@ class API::TagsController < API::BaseController
     @tags = taggable_type.all_tags
   end
 
+  def update
+    tag = ActsAsTaggableOn::Tag.find(params[:id])
+    same_name_tag = ActsAsTaggableOn::Tag.find_by(name: tag_params[:name])
+
+    if same_name_tag
+      taggings = ActsAsTaggableOn::Tagging.where(tag_id: params[:id])
+      taggable_ids = ActsAsTaggableOn::Tagging.where(tag_id: same_name_tag.id).select(:taggable_id)
+      update_target = taggings.where.not(taggable_id: taggable_ids)
+      destroy_target = taggings.where(taggable_id: taggable_ids)
+
+      if update_target.update(tag_id: same_name_tag.id) && destroy_target.destroy_all
+        head :ok
+      end
+    elsif tag.update(tag_params)
+      head :ok
+    else
+      head :bad_request
+    end
+  end
+
   private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 
   def taggable_type
     params[:taggable_type].constantize
