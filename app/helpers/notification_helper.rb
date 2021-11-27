fa0de09a8fcc1102b351c9be5d3ecf0874de49a9
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-11-27 09:26:17 +0900

    rubocopを適用

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 module NotificationHelper
-
-    def read_batche(target)
-        if target == 'announcement' 
-            return Cache.read_announcement_count(current_user.id)
-        end
-      end
-
-      def unread_batche(target)
-        if target == 'announcement' 
-            return Cache.unread_announcement_count(current_user.id)
-        end
-      end
+  def read_batche(target)
+    return Cache.read_announcement_count(current_user.id) if target == 'announcement'
   end
+
+  def unread_batche(target)
+    return Cache.unread_announcement_count(current_user.id) if target == 'announcement'
+  end
+end
