370cb550a56e4cfcaf31cf53bcc99aef3835a300
kawase-k <kottan00@gmail.com>
2021-11-28 11:57:28 +0900

    エクスポート機能の実装

--- app/controllers/current_user/reports_controller.rb
+++ app/controllers/current_user/reports_controller.rb
@@ -6,6 +6,9 @@ class CurrentUser::ReportsController < ApplicationController
   before_action :set_reports
   before_action :set_export, only: %i[index]
 
+  require 'zip'
+  require 'fileutils'
+
   def index
     respond_to do |format|
       format.html
@@ -31,5 +34,32 @@ class CurrentUser::ReportsController < ApplicationController
 
   def set_export
     @reports_for_export = user.reports.not_wip
+  end
+
+  def create_reports_md
+    @reports_for_export.each do |report|
+      File.open("tmp/exports/#{report.reported_on}.md", 'w') do |file|
+        file.puts("# #{report.title}")
+        file.puts
+        file.puts(report.description)
+      end
+    end
+  end
+
+  def send_reports_md
+    create_reports_md
+
+    folderpath = 'tmp/exports'
+    zip_filename = 'tmp/exports/reports.zip'
+    filenames = Dir.open(folderpath, &:children)
+
+    Zip::File.open(zip_filename, Zip::File::CREATE) do |zipfile|
+      filenames.each do |filename|
+        zipfile.add(filename, File.join(folderpath, filename))
+      end
+    end
+
+    send_data(File.read(zip_filename), filename: '日報一覧.zip')
+    FileUtils.rm(Dir.glob("#{folderpath}/*"))
   end
 end
