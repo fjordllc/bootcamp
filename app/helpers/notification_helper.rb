e38db70fc28c052dbd8599af3cd405ae7be1b5cc
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-11-28 23:05:30 +0900

    使いやすくするためcurrent_userを外側から指定するように変更する

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module NotificationHelper
-  def read_batche(target)
+  def read_batche(current_user, target)
     return Cache.read_announcement_count(current_user.id) if target == 'announcement'
   end
 
-  def unread_batche(target)
+  def unread_batche(current_user, target)
     return Cache.unread_announcement_count(current_user.id) if target == 'announcement'
   end
 end
