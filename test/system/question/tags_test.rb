7680720b29d0df80feb5b5c5e6d06498d684a543
ikuma-t <tadokorodev@gmail.com>
2021-11-30 01:14:07 +0900

    タグ更新機能に対するシステムテストを追加

--- test/system/question/tags_test.rb
+++ test/system/question/tags_test.rb
@@ -41,4 +41,77 @@ class Question::TagsTest < ApplicationSystemTestCase
     wait_for_vuejs
     assert_text '追加タグ'
   end
+
+  test 'admin can edit tag' do
+    tag = acts_as_taggable_on_tags('game')
+    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'komagata'
+    assert_text('このタグを編集する')
+  end
+
+  test 'users except admin cannot edit tag' do
+    tag = acts_as_taggable_on_tags('game')
+    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'kimura'
+    assert_no_text('このタグを編集する')
+  end
+
+  test 'update tag with not existing tag' do
+    tag = acts_as_taggable_on_tags('beginner')
+    update_tag_text = '上級者'
+
+    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'komagata'
+    click_button 'このタグを編集する'
+    fill_in('tag[name]', with: update_tag_text)
+    click_button '更新'
+    wait_for_vuejs
+
+    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'komagata'
+    assert_text '質問はありません。'
+    visit_with_auth questions_tag_path(update_tag_text, all: 'true'), 'komagata'
+    assert_text "タグ「#{update_tag_text}」のQ&A（1）"
+
+    visit_with_auth users_tag_path(tag.name), 'komagata'
+    assert_text "#{tag.name}のユーザーはいません"
+    visit_with_auth users_tag_path(update_tag_text), 'komagata'
+    assert_text "タグ「#{update_tag_text}」のユーザー（1）"
+
+    visit_with_auth pages_tag_path(tag.name), 'komagata'
+    has_no_selector?('thread-list-item')
+    visit_with_auth pages_tag_path(update_tag_text), 'komagata'
+    has_selector?('thread-list-item')
+  end
+
+  test 'update tag with existing tag' do
+    tag = acts_as_taggable_on_tags('beginner')
+    update_tag = acts_as_taggable_on_tags('intermediate')
+
+    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'komagata'
+    click_button 'このタグを編集する'
+    fill_in('tag[name]', with: update_tag.name)
+    click_button '更新'
+    wait_for_vuejs
+
+    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'komagata'
+    assert_text '質問はありません。'
+    visit_with_auth questions_tag_path(update_tag.name, all: 'true'), 'komagata'
+    assert_text "タグ「#{update_tag.name}」のQ&A（2）"
+
+    visit_with_auth users_tag_path(tag.name), 'komagata'
+    assert_text "#{tag.name}のユーザーはいません"
+    visit_with_auth users_tag_path(update_tag.name), 'komagata'
+    assert_text "タグ「#{update_tag.name}」のユーザー（2）"
+
+    visit_with_auth pages_tag_path(tag.name), 'komagata'
+    has_no_selector?('thread-list-item')
+    visit_with_auth pages_tag_path(update_tag.name), 'komagata'
+    has_selector?('thread-list-item')
+  end
+
+  test 'update tag with same value' do
+    tag = acts_as_taggable_on_tags('beginner')
+
+    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'komagata'
+    click_button 'このタグを編集する'
+    fill_in('tag[name]', with: tag.name)
+    has_field?('更新', disabled: true)
+  end
 end
