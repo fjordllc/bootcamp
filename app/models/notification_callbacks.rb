e4f571b308512b156fe85c8d91435d646fef3f57
AudioStakes <daisuke.sato@enishi-tech.com>
2021-11-30 16:26:52 +0900

    通知の作成・更新・削除時にメンションありの通知の全件数と未読件数のキャッシュを削除するようにした

---
+++ app/models/notification_callbacks.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class NotificationCallbacks
+  def after_create(notification)
+    return unless notification.kind == 'mentioned'
+
+    Cache.delete_mentioned_notification_count(notification.user.id)
+    Cache.delete_mentioned_and_unread_notification_count(notification.user.id)
+  end
+
+  def after_update(notification)
+    return unless notification.kind == 'mentioned' && notification.saved_change_to_attribute?('read')
+
+    Cache.delete_mentioned_and_unread_notification_count(notification.user.id)
+  end
+
+  def after_destroy(notification)
+    return unless notification.kind == 'mentioned'
+
+    Cache.delete_mentioned_notification_count(notification.user.id)
+    Cache.delete_mentioned_and_unread_notification_count(notification.user.id) if notification.unread?
+  end
+end
