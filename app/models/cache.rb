86019a7dfefecd3b7612c7df98ba00e5535133cc
AudioStakes <daisuke.sato@enishi-tech.com>
2021-11-30 16:25:52 +0900

    通知一覧画面の「メンション」タブに表示する全件数と未読件数をユーザー単位でキャッシュするようにした

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -51,5 +51,17 @@ class Cache
     def delete_not_solved_question_count
       Rails.cache.delete 'not_solved_question_count'
     end
+
+    def mentioned_notification_count(user)
+      Rails.cache.fetch "#{user.id}-mentioned_notification_count" do
+        user.notifications.where(kind: :mentioned).count
+      end
+    end
+
+    def mentioned_and_unread_notification_count(user)
+      Rails.cache.fetch "#{user.id}-mentioned_and_unread_notification_count" do
+        user.notifications.where(kind: :mentioned, read: false).count
+      end
+    end
   end
 end
