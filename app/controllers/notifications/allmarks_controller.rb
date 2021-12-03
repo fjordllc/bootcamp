84200c81289cd520022236fb32a50bc7acd73b15
AudioStakes <daisuke.sato@enishi-tech.com>
2021-12-03 15:31:39 +0900

    ログインユーザーに紐づく全ての通知を既読へ一括更新する時に未読件数のキャッシュを削除するようにした

--- app/controllers/notifications/allmarks_controller.rb
+++ app/controllers/notifications/allmarks_controller.rb
@@ -4,6 +4,7 @@ class Notifications::AllmarksController < ApplicationController
   def create
     @notifications = current_user.notifications
     @notifications.update_all(read: true, updated_at: Time.current) # rubocop:disable Rails/SkipsModelValidations
+    Cache.delete_mentioned_and_unread_notification_count(current_user.id) # 全ての通知件数分のコールバックが実行されるとサーバーに負荷をかけすぎることもありそうなため、コールバックを使わずにキャッシュを削除する
     redirect_to notifications_path, notice: '全て既読にしました'
   end
 end
