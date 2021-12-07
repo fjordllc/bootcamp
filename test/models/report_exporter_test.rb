0961988b3413f9ec0a6d11577a55a968d06f00d1
kawase-k <kottan00@gmail.com>
2021-12-07 10:59:04 +0900

    モデルテストの追加

---
+++ test/models/report_exporter_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ReportExporterTest < ActiveSupport::TestCase
+  test '#create' do
+    reports = users(:kimura).reports
+    filenames = Dir.mktmpdir('exports') do |folder_path|
+      ReportExporter.new(reports, folder_path).create
+    end
+
+    assert_equal reports.map {|report| "#{report.reported_on}.md"}.sort, filenames
+  end
+end
