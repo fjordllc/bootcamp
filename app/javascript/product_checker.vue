67f65d4d7733c554d7ef0c26f4caed116bbca88f
Nga <38340962+nga20167304@users.noreply.github.com>
2021-12-11 15:56:37 +0700

    条件を否定にせず修正する

--- app/javascript/product_checker.vue
+++ app/javascript/product_checker.vue
@@ -76,9 +76,9 @@ export default {
           } else {
             this.id = json.checker_id
             this.name = json.checker_name
-            this.buttonLabel !== '担当する'
-              ? this.toast('担当になりました。')
-              : this.toast('担当から外れました。')
+            this.buttonLabel === '担当する'
+              ? this.toast('担当から外れました。')
+              : this.toast('担当になりました。')
           }
         })
     }
