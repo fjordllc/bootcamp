912a5dbc9b579605477d2507c8ec67bff94095c3
monoue <like_a_newborn_baby@icloud.com>
2021-12-14 07:20:39 +0900

    DB中のReportモデルのemotionカラムのnullデータの置換方法を、saveからupdateに変更

--- db/data/20211208002045_convert_report_emotion_null_to_soso.rb
+++ db/data/20211208002045_convert_report_emotion_null_to_soso.rb
@@ -3,8 +3,7 @@
 class ConvertReportEmotionNullToSoso < ActiveRecord::Migration[6.1]
   def up
     Report.where(emotion: nil).find_each do |report|
-      report.emotion = Report.emotions[:soso]
-      report.save!(validate: false)
+      report.update!(emotion: Report.emotions[:soso])
     end
   end
 
