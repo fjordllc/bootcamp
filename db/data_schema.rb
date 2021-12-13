9891758e83e4d8489c395b28bdfbebc23adf797d
mami-inuzuka <inuzuka.mami@gmail.com>
2021-12-13 19:14:03 +0900

    rubocopのエラー解消

--- db/data_schema.rb
+++ db/data_schema.rb
@@ -1,2 +1,3 @@
-# encoding: UTF-8
-DataMigrate::Data.define(version: 20211213092226)
+# frozen_string_literal: true
+
+DataMigrate::Data.define(version: 20211213092226) # rubocop:disable Style/NumericLiterals
