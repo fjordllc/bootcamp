b7ca662d90ea9a8dde9d10a56c734772ad4c8e59
monoue <like_a_newborn_baby@icloud.com>
2021-12-14 12:26:18 +0900

    イベントそのものとイベント募集開始時刻を設定してフォーカスが外れた際、終了時刻が同時刻に設定される機能を実装

--- app/javascript/events.js
+++ app/javascript/events.js
@@ -9,4 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
       render: (h) => h(Events)
     }).$mount(selector)
   }
+
+  const eventSets = [
+    ['event_start_at', 'event_end_at'],
+    ['event_open_start_at', 'event_open_end_at']
+  ]
+  eventSets.forEach((eventSet) => {
+    document.getElementById(eventSet[0]).addEventListener('blur', (event) => {
+      document.getElementById(eventSet[1]).value = event.target.value
+    })
+  })
 })
