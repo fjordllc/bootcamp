7a7ecba5e026b71ead8cd8b3de935599e9d00114
Nga <38340962+nga20167304@users.noreply.github.com>
2021-12-14 16:59:16 +0700

    未返信タブに関する部分を削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -47,40 +47,7 @@ class Product < ApplicationRecord
   scope :order_for_list, -> { order(created_at: :desc, id: :desc) }
   scope :order_for_not_wip_list, -> { order(published_at: :desc, id: :desc) }
 
-  # rubocop:disable Metrics/MethodLength
-  def self.not_responded_product_ids
-    sql = <<~SQL
-      WITH last_comments AS (
-        SELECT *
-        FROM comments AS parent
-        WHERE commentable_type = 'Product' AND id = (
-          SELECT id
-          FROM comments AS child
-          WHERE parent.commentable_id = child.commentable_id
-            AND commentable_type = 'Product'
-          ORDER BY created_at DESC LIMIT 1
-        )
-      ),
-      unchecked_products AS (
-        SELECT products.*
-        FROM products
-        LEFT JOIN checks ON products.id = checks.checkable_id AND checks.checkable_type = 'Product'
-        WHERE checks.id IS NULL AND wip = false
-      )
-      SELECT unchecked_products.id
-      FROM unchecked_products
-      LEFT JOIN last_comments ON unchecked_products.id = last_comments.commentable_id
-      WHERE last_comments.id IS NULL
-      OR unchecked_products.user_id = last_comments.user_id
-      ORDER BY unchecked_products.created_at DESC
-    SQL
-    Product.find_by_sql(sql).map(&:id)
-  end
   # rubocop:enable Metrics/MethodLength
-
-  def self.not_responded_products
-    Product.where(id: not_responded_product_ids)
-  end
 
   def self.add_latest_commented_at
     Product.all.includes(:comments).find_each do |product|
