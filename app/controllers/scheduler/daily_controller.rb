f0fd1e2fa1c2f03281f24d69e45fdb035e62f61d
taka61 <taka.tennisloveup61@gmail.com>
2021-12-16 11:31:32 +0900

    通知済みかを確認するためにカラムを追加

--- app/controllers/scheduler/daily_controller.rb
+++ app/controllers/scheduler/daily_controller.rb
@@ -4,7 +4,7 @@ class Scheduler::DailyController < SchedulerController
   def show
     User.notify_to_discord
     User.retired.find_each do |retired_user|
-      if retired_user.retired_on <= Date.current - 3.months
+      if retired_user.retired_on <= Date.current - 3.months && retired_user.retired_notification == false
         User.admins.each do |admin_user|
           Notification.retired_after_three_months(retired_user, admin_user)
           NotificationFacade.retired_after_three_months(retired_user, admin_user)
