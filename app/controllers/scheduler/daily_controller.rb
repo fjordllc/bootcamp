17fc06682ecaa1941df59a5d44b2f010eb00c1a9
taka61 <taka.tennisloveup61@gmail.com>
2021-12-16 10:52:36 +0900

    discord・メール・サイト内通知を実装

---
+++ app/controllers/scheduler/daily_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Scheduler::DailyController < SchedulerController
+  def show
+    User.notify_to_discord
+    User.retired.find_each do |retired_user|
+      if retired_user.retired_on <= Date.current - 3.months
+        User.admins.each do |admin_user|
+          Notification.retired_after_three_months(retired_user, admin_user)
+          NotificationFacade.retired_after_three_months(retired_user, admin_user)
+          retired_user.update!(retired_notification: true)
+        end
+      end
+    end
+    head :ok
+  end
+end
