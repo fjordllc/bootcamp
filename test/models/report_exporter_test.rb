6abc2126f302de9c2024212fd00e95f0666af766
kotakawase <kottan00@gmail.com>
2021-12-20 23:15:55 +0900

    モデルテストの修正

--- test/models/report_exporter_test.rb
+++ test/models/report_exporter_test.rb
@@ -3,10 +3,10 @@
 require 'test_helper'
 
 class ReportExporterTest < ActiveSupport::TestCase
-  test '#create' do
+  test '#export' do
     reports = users(:hajime).reports
-    filenames = Dir.mktmpdir('exports') do |folder_path|
-      ReportExporter.new(reports, folder_path).create
+    filenames = Dir.mktmpdir do |folder_path|
+      ReportExporter.export(reports, folder_path)
     end
 
     assert_equal reports.map { |report| "#{report.reported_on}.md" }.sort, filenames
