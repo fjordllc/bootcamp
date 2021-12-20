fb35bc29e5e97177adc47207910e9353ff5162c5
kotakawase <kottan00@gmail.com>
2021-12-20 23:13:30 +0900

    #save_as_fileの実装

--- app/models/report_exporter.rb
+++ app/models/report_exporter.rb
@@ -3,13 +3,22 @@
 class ReportExporter
   def self.export(reports, folder_path)
     Report.save_as_markdown!(reports, folder_path)
+    ZipFile.new(folder_path).save_as_file!
   end
 
   class ZipFile
-    def initialize
+    def initialize(folder_path)
+      @path = folder_path
+      @zip_filename = "#{folder_path}/reports.zip"
+      @filenames = Dir.open(folder_path, &:children).sort
     end
 
     def save_as_file!
+      Zip::File.open(@zip_filename, Zip::File::CREATE) do |zipfile|
+        @filenames.each do |filename|
+          zipfile.add(filename, File.join(@path, filename))
+        end
+      end
     end
   end
 end
