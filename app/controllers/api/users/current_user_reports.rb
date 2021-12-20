355e50458371152269f103e1e9d6aff14f148e01
monoue <like_a_newborn_baby@icloud.com>
2021-12-20 18:36:56 +0900

    reports取得の方式を変更

--- app/controllers/api/users/current_user_reports.rb
+++ app/controllers/api/users/current_user_reports.rb
@@ -2,6 +2,6 @@
 
 class API::Users::CurrentUserReportsController < API::BaseController
   def index
-    @reports = Report.where(user_id: current_user.id).order(reported_on: :desc)
+    @reports = current_user.reports.order(reported_on: :desc)
   end
 end
