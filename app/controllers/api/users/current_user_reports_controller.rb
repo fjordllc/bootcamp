28d22513f56e9578f522dde4153a180ad72e5b36
monoue <like_a_newborn_baby@icloud.com>
2021-12-21 11:21:02 +0900

    自分の日報ページでの表示をVue.js化

--- app/controllers/api/users/current_user_reports_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class API::Users::CurrentUserReportsController < API::BaseController
-  def index
-    @reports = current_user.reports.order(reported_on: :desc)
-  end
-end
