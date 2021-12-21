f42493d02023c378f39bd2fc43666c5144000f17
unstoppa61e <like_a_newborn_baby@icloud.com>
2021-12-21 16:55:42 +0900

    回答タブのN+1問題を解消

--- app/controllers/users/answers_controller.rb
+++ app/controllers/users/answers_controller.rb
@@ -5,6 +5,16 @@ class Users::AnswersController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @answers = @user.answers.order(created_at: :desc)
+    @answers = @user.answers.includes(
+      {
+        question: [
+          :correct_answer,
+          { user: [:company, { avatar_attachment: :blob }] },
+          :practice,
+          :tag_taggings,
+          :tags
+        ]
+      }
+    ).order(created_at: :desc)
   end
 end
