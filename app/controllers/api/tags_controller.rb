04140ca94c0a6d1cfa0f492e862cb99fb627fea4
ikuma-t <tadokorodev@gmail.com>
2021-12-21 19:19:22 +0900

    同名タグが存在する場合に、Taggingのタグを同名タグに付け替える処理をメソッド化
    - 処理の内容がわかりにくかったため
    - same_name_tagの存在チェックをガード節としてメソッドに取り込むこともできるが、API::TagsController#updateの条件分岐の可読性を考慮し、そのままにする

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -11,13 +11,8 @@ class API::TagsController < API::BaseController
     tag = ActsAsTaggableOn::Tag.find(params[:id])
     same_name_tag = ActsAsTaggableOn::Tag.find_by(name: tag_params[:name])
 
-    if same_name_tag
-      taggings = ActsAsTaggableOn::Tagging.where(tag_id: params[:id])
-      taggable_ids = ActsAsTaggableOn::Tagging.where(tag_id: same_name_tag.id).select(:taggable_id)
-      update_target = taggings.where.not(taggable_id: taggable_ids)
-      destroy_target = taggings.where(taggable_id: taggable_ids)
-
-      head :ok if update_target.update(tag_id: same_name_tag.id) && destroy_target.destroy_all
+    if same_name_tag && replace_tagging_tags_with(same_name_tag)
+      head :ok
     elsif tag.update(tag_params)
       head :ok
     else
@@ -26,6 +21,13 @@ class API::TagsController < API::BaseController
   end
 
   private
+
+  def replace_tagging_tags_with(same_name_tag)
+    taggings = ActsAsTaggableOn::Tagging.where(tag_id: params[:id])
+    taggable_ids = ActsAsTaggableOn::Tagging.where(tag_id: same_name_tag.id).select(:taggable_id)
+    taggings.where.not(taggable_id: taggable_ids).update(tag_id: same_name_tag.id)
+    taggings.where(taggable_id: taggable_ids).destroy_all
+  end
 
   def tag_params
     params.require(:tag).permit(:name)
