06f10c45cee1da51277628c7248124ca42218532
Nga <38340962+nga20167304@users.noreply.github.com>
2021-12-22 21:21:00 +0700

    bin/rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_034500) do
+ActiveRecord::Schema.define(version: 2021_12_20_015005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
