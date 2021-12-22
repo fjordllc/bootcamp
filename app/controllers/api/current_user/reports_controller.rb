c74b87778fe92025ee4a3d41207f8457d09e539c
unstoppa61e <like_a_newborn_baby@icloud.com>
2021-12-22 12:06:21 +0900

    `api/current_user/reports`アクセス時のrender対象のjbuilderファイルを既存のものに変更

--- app/controllers/api/current_user/reports_controller.rb
+++ app/controllers/api/current_user/reports_controller.rb
@@ -3,5 +3,6 @@
 class API::CurrentUser::ReportsController < API::BaseController
   def index
     @reports = current_user.reports.order(reported_on: :desc).list.page(params[:page])
+    render 'api/reports/index', handlers: 'jbuilder'
   end
 end
