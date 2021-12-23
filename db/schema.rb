0a8da197b053ffa9a2dea5e8e24f7d17aaeea49c
taka61 <taka.tennisloveup61@gmail.com>
2021-12-23 15:58:37 +0900

    コンフリクトを解消

--- db/schema.rb
+++ db/schema.rb
@@ -457,6 +457,7 @@ ActiveRecord::Schema.define(version: 2021_12_20_015005) do
     t.text "mentor_memo"
     t.string "discord_account"
     t.string "times_url"
+    t.boolean "retired_notification", default: false, null: false
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["github_id"], name: "index_users_on_github_id", unique: true
