a8be422e43d9345f67ea4e27e66bd704e195e244
unstoppa61e <like_a_newborn_baby@icloud.com>
2021-12-25 14:40:31 +0900

    APIを共通化

--- app/controllers/api/current_user/reports_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class API::CurrentUser::ReportsController < API::BaseController
-  def index
-    @reports = current_user.reports.order(reported_on: :desc).list.page(params[:page])
-    render 'api/reports/index', handlers: 'jbuilder'
-  end
-end
