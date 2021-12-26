c841a46b763948f60f0776ffa6a08e407826b3fc
taka61 <taka.tennisloveup61@gmail.com>
2021-12-26 16:49:58 +0900

    db/scheme.rbのコンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_015005) do
+ActiveRecord::Schema.define(version: 2021_12_23_071408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
