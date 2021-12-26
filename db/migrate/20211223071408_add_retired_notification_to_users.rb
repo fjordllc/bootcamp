b9a7f923b21e3ecdb007c9fa0142bbe8fde044cf
taka61 <taka.tennisloveup61@gmail.com>
2021-12-26 16:49:58 +0900

    db/scheme.rbのコンフリクト解消

--- db/migrate/20211223071408_add_retired_notification_to_users.rb
+++ db/migrate/20211223071408_add_retired_notification_to_users.rb
@@ -1,8 +1,5 @@
 class AddRetiredNotificationToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :retired_notification, :boolean, default: false, null: false
-    User.retired.find_each do |retired_user|
-      retired_user.update!(retired_notification: true) if retired_user.retired_on <= Date.current.prev_month(n = 3)
-    end
   end
 end
