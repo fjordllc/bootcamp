3ed7f6a6425fe22c8c6e737569ec15dba01cab21
vinhactindi <nguyen.the.vinh@actindi.net>
2021-12-30 10:22:59 +0700

    answerのマークダウンにチェクリストを導入

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,7 @@ class Answer < ApplicationRecord
   include Reactionable
   include Searchable
   include Mentioner
+  include Taskable
 
   belongs_to :user, touch: true
   belongs_to :question, touch: false
