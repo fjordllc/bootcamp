719a0c2afbb2036705251c516a29661b4734f910
taka61 <taka.tennisloveup61@gmail.com>
2022-01-03 11:11:13 +0900

    after_retirement.rbのテストを修正

--- test/system/notification/after_retirement.rb
+++ test/system/notification/after_retirement.rb
@@ -6,8 +6,7 @@ class Notification::AfterRetirementTest < ApplicationSystemTestCase
   test 'three months have passed since user retired' do
     visit_with_auth '/scheduler/daily', 'komagata'
 
-    login_user 'komagata', 'testtest'
-
+    visit '/'
     open_notification
     assert_equal 'taikai3さんが退会してから3カ月が経過しました。Discord ID: taikai#3333, ユーザーページ: https://bootcamp.fjord.jp/users/1008501353',
                  notification_message
