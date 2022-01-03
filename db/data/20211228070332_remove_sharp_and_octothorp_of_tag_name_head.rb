a5deccd68066f60dc92fd687f93cf4a8a2facb52
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-01-03 09:35:21 +0900

    置換後のタグ名と同名のタグ名が既に存在する場合には、そちらのタグのidにて、taggingsテーブル中の、ターゲットのタグのtag_idを置換するように変更

--- db/data/20211228070332_remove_sharp_and_octothorp_of_tag_name_head.rb
+++ db/data/20211228070332_remove_sharp_and_octothorp_of_tag_name_head.rb
@@ -7,6 +7,11 @@ class RemoveSharpAndOctothorpOfTagNameHead < ActiveRecord::Migration[6.1]
     ActsAsTaggableOn::Tag.where('name ~* ?', BEGINNING_WITH_SHARP_OR_OCTOTHORP).find_each do |tag|
       head_removed_name = tag.name.sub(/^(#|＃|♯)( |　)*/, '')
       if ActsAsTaggableOn::Tag.exists?(name: head_removed_name)
+        tag_id = ActsAsTaggableOn::Tag.find_by(name: head_removed_name).id
+        ActsAsTaggableOn::Tagging.where(tag_id: tag.id).find_each do |tagging_to_replace|
+          tagging_to_replace.tag_id = tag_id
+          tagging_to_replace.save!(validate: false)
+        end
       else
         tag.name = head_removed_name
         tag.save!(validate: false)
