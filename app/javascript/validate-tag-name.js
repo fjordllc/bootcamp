09278e93b90cdf405b990893d74fac22d4f6549a
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-01-03 17:14:17 +0900

    ハッシュ類から始まるタグを登録しようとすると、アラートを出し、ハッシュ部分を取り除いた名前で登録される機能を追加

--- app/javascript/validate-tag-name.js
+++ app/javascript/validate-tag-name.js
@@ -4,10 +4,17 @@ export default {
       const { text } = obj.tag
       // eslint-disable-next-line no-irregular-whitespace
       if (/ |　/.test(text)) {
-        alert('入力されたタグにスペースが含まれています') // eslint-disable-line no-undef
+        alert('スペースを含むタグは作成できません') // eslint-disable-line no-undef
       } else if (text === '.') {
         alert('ドット1つだけのタグは作成できません') // eslint-disable-line no-undef
       } else {
+        if (/^(#|＃|♯)/.test(text)) {
+          alert('先頭の記号は無視されます') // eslint-disable-line no-undef
+          if (text.length === 1) {
+            return
+          }
+          obj.tag.text = text.substr(1)
+        }
         obj.addTag()
       }
     }
