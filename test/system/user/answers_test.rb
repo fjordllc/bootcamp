55d5df7f2363464308f662b72ea9285af1ad8251
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-01-04 17:13:40 +0900

    テストにて確認する回答ページを、タグ付きの質問に回答を行っているユーザーのものに変更

--- test/system/user/answers_test.rb
+++ test/system/user/answers_test.rb
@@ -4,7 +4,7 @@ require 'application_system_test_case'
 
 class User::AnswersTest < ApplicationSystemTestCase
   test 'show listing answers' do
-    visit_with_auth "/users/#{users(:sotugyou).id}/answers", 'sotugyou'
-    assert_equal 'sotugyouの回答 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
+    visit_with_auth "/users/#{users(:komagata).id}/answers", 'komagata'
+    assert_equal 'komagataの回答 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
 end
