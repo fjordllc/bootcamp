7c8399020a567a6b611c53672a58deaa6b1c8f6a
vinhactindi <nguyen.the.vinh@actindi.net>
2022-01-04 14:52:56 +0700

    サポートされていないtaskable_typeの時BadRequestを発生させる

--- app/controllers/api/markdown_tasks_controller.rb
+++ app/controllers/api/markdown_tasks_controller.rb
@@ -2,10 +2,12 @@
 
 class API::MarkdownTasksController < API::BaseController
   def create
+    taskable_type = params[:taskable_type]
+    raise ActionController::BadRequest.new, 'Unsupported taskable type' unless %w[Announcement Answer Comment Page Report].include?(taskable_type)
+
     checked = params[:checked].to_s.casecmp('true').zero?
     nth = params[:nth].to_i
     taskable_id = params[:taskable_id]
-    taskable_type = params[:taskable_type]
     taskable = taskable_type.camelcase.constantize.find(taskable_id)
 
     taskable.toggle_task(nth, checked)
