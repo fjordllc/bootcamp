9d09f51f39c1ce5ed90c97a92072a0c2237157da
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-01-04 16:25:33 +0900

    不要なルーティングの削除

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -29,9 +29,6 @@ Rails.application.routes.draw do
       resources :companies, only: %i(index)
       resources :worried, only: %i(index)
     end
-    namespace :current_user do
-      resources :reports, only: %i(index), controller: "reports"
-    end
     resources :users, only: %i(index show update) do
       resources :recent_reports, only: %i(index), controller: "users/recent_reports"
     end
