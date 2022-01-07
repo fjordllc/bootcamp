4626016042e01ab0764143e5af8a39602bbf5c52
naomichi-h <naomichi.programming@gmail.com>
2022-01-07 13:10:49 +0900

    世代別ユーザー一覧ページをvue化した

---
+++ app/javascript/generation-users.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import GenerationUsers from './generation-users.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const selector = '#js-generation-users'
+  const users = document.querySelector(selector)
+  if (users) {
+    const generationID = users.getAttribute('generation-id')
+    new Vue({
+      render: (h) => h(GenerationUsers, {
+          props: {
+              generationID: generationID
+          }
+      })
+    }).$mount(selector)
+  }
+})
