907342c45f21042e1a21fff8b94cce86e718b3e2
AudioStakes <daisuke.sato@enishi-tech.com>
2022-01-07 16:50:37 +0900

    テストケースの名前が英語として正しい表現になるようにした

--- test/models/caches/unchecked_product_count_test.rb
+++ test/models/caches/unchecked_product_count_test.rb
@@ -3,13 +3,13 @@
 require 'test_helper'
 
 class UncheckedProductCountTest < ActiveSupport::TestCase
-  test 'increase the cached value of unchecked product count by 1 after create a product' do
+  test 'cached count of unchecked products increases by 1 after creating a product' do
     assert_difference 'Cache.unchecked_product_count', 1 do
       Product.create!(practice: practices(:practice5), user: users(:kimura), body: '未確認の提出物')
     end
   end
 
-  test 'decreace the cached value of unchecked product count by 1 after destroy an unchecked product' do
+  test 'cached count of unchecked products decreases by 1 after destroying an unchecked product' do
     unchecked_product = Product.not_wip.unchecked.first
 
     assert_difference 'Cache.unchecked_product_count', -1 do
@@ -17,7 +17,7 @@ class UncheckedProductCountTest < ActiveSupport::TestCase
     end
   end
 
-  test 'increase the cached value of unchecked product count by 1 after uncheck a checked product' do
+  test 'cached count of unchecked products increases by 1 after unchecking a checked product' do
     check_of_checked_product = Product.checked.first.checks.first
 
     assert_difference 'Cache.unchecked_product_count', 1 do
@@ -25,7 +25,7 @@ class UncheckedProductCountTest < ActiveSupport::TestCase
     end
   end
 
-  test 'decrease the cached value of unchecked product count by 1 after check an unchecked product' do
+  test 'cached count of unchecked products decreases by 1 after checking an unchecked product' do
     unchecked_product = Product.not_wip.unchecked.first
 
     assert_difference 'Cache.unchecked_product_count', -1 do
