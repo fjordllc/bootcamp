64d0686307c6b7f5fd25f58c4c0b35c6f9908968
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-01-13 08:34:01 +0900

    「直近の日報」の取得経路をuser-recent-reports.vueではなくreports.vueに一本化

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -5,5 +5,6 @@ class API::ReportsController < API::BaseController
     @reports = Report.list.page(params[:page])
     @reports = @reports.joins(:practices).where(practices: { id: params[:practice_id] }) if params[:practice_id].present?
     @reports = @reports.where(user_id: params[:user_id]) if params[:user_id].present?
+    @reports = @reports.limit(params[:limit].to_i) if params[:limit].present?
   end
 end
