c6a93e773fcdd88e0934ca6bd279ee5d8c3f7bfb
kaisumi <kaisumi@langue-lab.com>
2022-01-12 20:51:58 +0900

    page_testとproducts_testの該当箇所を修正

--- test/system/bookmark/page_test.rb
+++ test/system/bookmark/page_test.rb
@@ -9,7 +9,7 @@ class Bookmark::PageTest < ApplicationSystemTestCase
 
   test 'show page bookmark on lists' do
     visit_with_auth '/current_user/bookmarks', 'kimura'
-    assert_text 'ブックマーク一覧'
+    assert_text 'ダッシュボード'
     assert_text @page.title
   end
 
--- test/system/bookmark/products_test.rb
+++ test/system/bookmark/products_test.rb
@@ -9,7 +9,7 @@ class Bookmark::ProductTest < ApplicationSystemTestCase
 
   test 'show product bookmark on lists' do
     visit_with_auth '/current_user/bookmarks', 'kimura'
-    assert_text 'ブックマーク一覧'
+    assert_text 'ダッシュボード'
     assert_text @product.title
   end
 
