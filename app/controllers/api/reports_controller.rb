b0000136bbab13a97f58b19cad4f3fb3e0fbb64c
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-01-12 18:10:23 +0900

    クエリのパラメーターをcurrent_userからuser_idに変更

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -4,6 +4,6 @@ class API::ReportsController < API::BaseController
   def index
     @reports = Report.list.page(params[:page])
     @reports = @reports.joins(:practices).where(practices: { id: params[:practice_id] }) if params[:practice_id].present?
-    @reports = @reports.where(user_id: current_user.id) if params[:current_user] == true.to_s
+    @reports = @reports.where(user_id: params[:user_id]) if params[:user_id].present?
   end
 end
