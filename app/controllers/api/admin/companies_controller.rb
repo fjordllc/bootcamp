c68ae9db62f6ec076feca8cd55f81357a989365e
napple29 <snwxxx29@gmail.com>
2022-01-13 09:42:29 +0900

    ルーティング、コントローラーを変更

--- app/controllers/api/admin/companies_controller.rb
+++ app/controllers/api/admin/companies_controller.rb
@@ -7,6 +7,15 @@ class API::Admin::CompaniesController < API::BaseController
     @companies = Company.with_attached_logo
                         .order(:id)
                         .page(params[:page])
+  end
 
+  def destroy
+    @company = Company.find(params[:id])
+
+    if @company.destroy
+      head :no_content
+    else
+      head :bad_request
+    end
   end
 end
