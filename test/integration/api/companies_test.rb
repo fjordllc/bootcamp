c2cbf06f9eb3cd1027a4353ab54641eb35fd5ac5
napple29 <snwxxx29@gmail.com>
2022-01-13 13:08:55 +0900

    テストの追加、修正

---
+++ test/integration/api/companies_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class API::Admin::CompaniesTest < ActionDispatch::IntegrationTest
+  fixtures :users
+
+  test 'GET /api/admin/companies.json' do
+    get api_admin_companies_path(format: :json)
+    assert_response :unauthorized
+
+    token = create_token('komagata', 'testtest')
+    get api_users_path(format: :json),
+      headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+  end
+
+  test 'DELETE /api/admin/companies/1234.json' do
+    token = create_token('komagata', 'testtest')
+    get api_admin_companies_path
+
+    delete api_admin_company_path(companies(:company2).id, format: :json),
+      headers: { 'Authorization' => "Bearer #{token}" }
+
+    assert_response :success
+  end
+end
