8f6b953a9f784c1bb05fd2e5245694d85c4e62c1
mami-inuzuka <inuzuka.mami@gmail.com>
2022-01-14 23:10:13 +0900

    新しく追加されたプライベートメソッドを分割先のファイルに記載

--- app/models/comment/after_destroy_callback.rb
+++ app/models/comment/after_destroy_callback.rb
@@ -44,4 +44,12 @@ class Comment::AfterDestroyCallback
   def delete_product_cache(product_id)
     Rails.cache.delete "/model/product/#{product_id}/last_commented_user"
   end
+
+  def delete_assigned_and_unreplied_product_count_cache(comment)
+    product = comment.commentable
+
+    return unless product.checker_id.present? && product.replied_status_changed?(comment.previous&.user_id, comment.user_id)
+
+    Cache.delete_self_assigned_no_replied_product_count(product.checker_id)
+  end
 end
