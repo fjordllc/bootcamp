d28bfe03eb326e147c030095d8a21ae46c17e544
haruna kanno <82350582+napple29@users.noreply.github.com>
2022-01-17 17:20:27 +0900

    レビューを受けて継承するコントローラーを修正
    Co-authored-by: AudioStakes <daisuke.sato@enishi-tech.com>

--- app/controllers/api/admin/companies_controller.rb
+++ app/controllers/api/admin/companies_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class API::Admin::CompaniesController < API::BaseController
+class API::Admin::CompaniesController < API::Admin::BaseController
   before_action :require_login
 
   def index
