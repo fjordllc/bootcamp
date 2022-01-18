57d9107d801e3d0693e6a3f83e6a66ddb53cb776
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2022-01-18 21:58:51 +0900

    質問を投稿したとき、通知とWatch状態にする

--- app/models/question_callbacks.rb
+++ app/models/question_callbacks.rb
@@ -3,6 +3,7 @@
 class QuestionCallbacks
   def after_create(question)
     send_notification_to_mentors(question)
+    notify_followers(question)
     Cache.delete_not_solved_question_count
   end
 
@@ -22,4 +23,27 @@ class QuestionCallbacks
   def delete_notification(question)
     Notification.where(link: "/questions/#{question.id}").destroy_all
   end
+
+  def send_notification(product:, receivers:, message:)
+    receivers.each do |receiver|
+      NotificationFacade.submitted(product, receiver, message)
+    end
+  end
+
+  def notify_followers(question)
+    followers = question.user.followers
+    send_notification(
+      product: question,
+      receivers: followers,
+      message: "#{question.user.login_name}さんから質問がありました。"
+    )
+    question.user.followers.each do |follower|
+      create_following_watch(question, follower) if follower.watching?(question.user)
+    end
+  end
+
+  def create_following_watch(question, follower)
+    Watch.create!(user: follower, watchable: question)
+  end
+
 end
