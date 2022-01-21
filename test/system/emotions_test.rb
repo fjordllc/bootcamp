2de3b745bfea0f6c1a1d6bb46ce98e359cd1d731
kaisumi <kaisumi@langue-lab.com>
2022-01-21 15:12:21 +0900

    テストを作成（失敗するはずのテスト）

--- test/system/emotions_test.rb
+++ test/system/emotions_test.rb
@@ -21,4 +21,23 @@ class EmotionsTest < ApplicationSystemTestCase
     assert_text '日報を保存しました。'
     assert_selector 'img#happy'
   end
+  test 'create a report with the sad emotion' do
+    visit_with_auth '/reports/new', 'komagata'
+    within('#new_report') do
+      fill_in('report[title]', with: 'test title')
+      fill_in('report[description]', with: 'test')
+    end
+
+    all('.learning-time')[0].all('.learning-time__started-at select')[0].select('07')
+    all('.learning-time')[0].all('.learning-time__started-at select')[1].select('30')
+    all('.learning-time')[0].all('.learning-time__finished-at select')[0].select('08')
+    all('.learning-time')[0].all('.learning-time__finished-at select')[1].select('30')
+
+    find('#happy').click
+
+    click_button '提出'
+    assert_text '日報を保存しました。'
+    assert_selector 'img#happy'
+    assert_text '困った時は'
+  end
 end
