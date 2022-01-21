0501af7ad982c96c5ff6b5a8b18a7fcee2ec424f
napple29 <snwxxx29@gmail.com>
2022-01-21 16:19:32 +0900

    管理画面にお試し延長ページを作成

---
+++ app/controllers/admin/extended_trials_controller.rb
@@ -0,0 +1,40 @@
+class Admin::ExtendedTrialsController < AdminController
+  before_action :set_extended_trial, only: %i[edit update]
+  def new
+    @extended_trial = ExtendedTrial.new
+  end
+
+  def create
+    @extended_trial = ExtendedTrial.new(extended_trial_params)
+    if @extended_trial.save
+      redirect_to admin_extended_trials_path, notice: 'お試し延長を作成しました。'
+    else
+      render :new
+    end
+  end
+
+  def index
+    @extended_trials = ExtendedTrial.all
+  end
+
+  def edit;
+  end
+
+  def update
+    if @extended_trial.update(extended_trial_params)
+      redirect_to admin_extended_trials_path, notice: 'お試し延長を更新しました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_extended_trial
+    @extended_trial = ExtendedTrial.find(params[:id])
+  end
+
+  def extended_trial_params
+    params.require(:extended_trial).permit(:start_at, :end_at, :term)
+  end
+end
