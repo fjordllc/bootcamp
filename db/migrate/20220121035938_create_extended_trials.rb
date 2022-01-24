1b07c211ae0a90dcf0584164d09c3a9c7de108af
napple29 <snwxxx29@gmail.com>
2022-01-24 18:04:53 +0900

    ExtendedTrialテーブルにタイトルを追加し、実装

--- db/migrate/20220121035938_create_extended_trials.rb
+++ db/migrate/20220121035938_create_extended_trials.rb
@@ -3,6 +3,7 @@ class CreateExtendedTrials < ActiveRecord::Migration[6.1]
     create_table :extended_trials do |t|
       t.datetime :start_at, null: false
       t.datetime :end_at, null: false
+      t.string :title, null: false
       t.timestamps
     end
   end
