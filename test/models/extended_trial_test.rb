dd35603c6df0615c5210b0ca08cc5625cbb16797
napple29 <snwxxx29@gmail.com>
2022-01-25 14:02:34 +0900

    テストを修正

--- test/models/extended_trial_test.rb
+++ test/models/extended_trial_test.rb
@@ -8,7 +8,7 @@ class ExtendedTrialTest < ActiveSupport::TestCase
     earlier_extended_trial = extended_trials(:extended_trial1)
 
     assert_equal ExtendedTrial.recently_extended_trial, earlier_extended_trial.start_at..earlier_extended_trial.end_at
-    assert_not ExtendedTrial.recently_extended_trial, later_extended_trial.start_at..later_extended_trial.end_at
+    assert_not_equal ExtendedTrial.recently_extended_trial, later_extended_trial.start_at..later_extended_trial.end_at
   end
 
   test 'today is extended_trial?' do
