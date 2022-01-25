06ca23fa22080c44f7957529ecaf1065d4bb7414
napple29 <snwxxx29@gmail.com>
2022-01-25 13:56:22 +0900

    テストを作成

--- test/models/extended_trial_test.rb
+++ test/models/extended_trial_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class ExtendedTrialTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'recently extended_trial' do
+    later_extended_trial = extended_trials(:extended_trial2)
+    earlier_extended_trial = extended_trials(:extended_trial1)
+
+    assert_equal ExtendedTrial.recently_extended_trial, earlier_extended_trial.start_at..earlier_extended_trial.end_at
+    assert_not ExtendedTrial.recently_extended_trial, later_extended_trial.start_at..later_extended_trial.end_at
+  end
+
+  test 'today is extended_trial?' do
+    extended_trial = extended_trials(:extended_trial1)
+    assert_equal ExtendedTrial.today_is_extended_trial?, (extended_trial.start_at..extended_trial.end_at).cover?(Time.zone.today)
+  end
 end
