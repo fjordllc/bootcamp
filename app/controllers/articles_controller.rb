0d3d13ae5cd8806b6409186aae64b9748b294c5e
kaisumi <kaisumi@langue-lab.com>
2022-01-28 16:32:29 +0900

    WIP機能を実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,12 +5,18 @@ class ArticlesController < ApplicationController
   before_action :require_admin_login, except: %i[index show]
 
   def index
-    @articles = Article.all.order(created_at: :desc)
+    @articles =
+      if admin_or_mentor_login?
+        Article.all.order(created_at: :desc)
+      else
+        Article.all.where(wip: false).order(created_at: :desc)
+      end
     @articles = @articles.tagged_with(params[:tag]) if params[:tag]
     render layout: 'article'
   end
 
   def show
+    require_admin_or_mentor_login if @article.wip?
     render layout: 'article'
   end
 
@@ -23,17 +29,18 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user = current_user
-
+    set_wip_or_published_time
     if @article.save
-      redirect_to @article, notice: '記事を作成しました'
+      redirect_to @article, notice: notice_message(@article)
     else
       render :new
     end
   end
 
   def update
+    set_wip_or_published_time
     if @article.update(article_params)
-      redirect_to @article, notice: '記事を更新しました'
+      redirect_to @article, notice: notice_message(@article)
     else
       render :edit
     end
@@ -52,5 +59,22 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body, :tag_list)
+  end
+
+  def set_wip_or_published_time
+    if params[:commit] == 'WIP'
+      @article.wip = true
+    else
+      @article.published_at = Time.current
+    end
+  end
+
+  def notice_message(article)
+    case params[:action]
+    when 'create'
+      article.wip? ? '記事をWIPとして保存しました' : '記事を作成しました'
+    when 'update'
+      article.wip? ? '記事をWIPとして保存しました' : '記事を更新しました'
+    end
   end
 end
