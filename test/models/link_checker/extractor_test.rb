9617aad82590b335842003507ef720227d4cc5f6
AudioStakes <daisuke.sato@enishi-tech.com>
2022-01-29 16:18:31 +0900

    extractor_test.rb で作成している Page のテストデータをフィクスチャへ移した

--- test/models/link_checker/extractor_test.rb
+++ test/models/link_checker/extractor_test.rb
@@ -5,42 +5,27 @@ require 'test_helper'
 module LinkChecker
   class ExtractorTest < ActiveSupport::TestCase
     test '#extract_links' do
-      extractor = LinkChecker::Extractor.new(<<~TEXT, 'apt', 'https://bootcamp.fjord.jp/1234')
-                        aptとはdebianでソフトウェアをネットワークからインストールするコマンドです。
-        #{'        '}
-                        [TEST](/test)(/test2)
-        #{'        '}
-                        [missing](test)
-                #{'        '}
-                        - 参考
-                            - [APT - Wikipedia](http://ja.wikipedia.org/wiki/APT)
-                #{'        '}
-                        ## Q&A
-                #{'        '}
-                        - Q. `$ apt-cache search vim` の検索結果が多すぎる
-                        - A. [正規表現](https://ja.wikipedia.org/wiki/%E6%AD%A3%E8%A6%8F%E8%A1%A8%E7%8F%BE) を使う。
-                          - 完全一致: `$ apt-cache search ^vim$`
-                          - 前方一致: `$ apt-cache search ^vim`
-      TEXT
+      page = pages(:page8)
+      extractor = LinkChecker::Extractor.new(page.body, page.title, "https://bootcamp.fjord.jp/#{page.path}")
 
       expected = [
         LinkChecker::Link.new(
           'TEST',
           'https://bootcamp.fjord.jp/test',
           'apt',
-          'https://bootcamp.fjord.jp/1234'
+          "https://bootcamp.fjord.jp/#{page.path}"
         ),
         LinkChecker::Link.new(
           'APT - Wikipedia',
           'http://ja.wikipedia.org/wiki/APT',
           'apt',
-          'https://bootcamp.fjord.jp/1234'
+          "https://bootcamp.fjord.jp/#{page.path}"
         ),
         LinkChecker::Link.new(
           '正規表現',
           'https://ja.wikipedia.org/wiki/%E6%AD%A3%E8%A6%8F%E8%A1%A8%E7%8F%BE',
           'apt',
-          'https://bootcamp.fjord.jp/1234'
+          "https://bootcamp.fjord.jp/#{page.path}"
         )
       ]
 
