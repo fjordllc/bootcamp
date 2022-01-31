ff988c9d57d6c16689112c87a1cc71f6dee104fa
teppei machida <machida@fjord.jp>
2022-01-31 13:39:15 +0900

    相談部屋リンクにデザインを入れた

--- app/javascript/user.vue
+++ app/javascript/user.vue
@@ -57,10 +57,9 @@
                 :userId='user.id',
                 :isWatching='user.isWatching'
               )
-          a.users-item__linkto_talk.is-only-admin(:href='user.talkurl')(
-            v-if='currentUser.admin'
-          )
-            | 相談部屋
+            li.card-main-actions__item.is-only-admin(v-if='currentUser.admin')
+              a.a-button.is-secondary.is-md.is-block(:href='user.talkurl')
+                | 相談部屋
 </template>
 <script>
 import Following from './following.vue'
