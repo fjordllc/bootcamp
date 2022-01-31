7d9879f39c8286675c72678bc6dbee2f612c450a
kaisumi <kaisumi@langue-lab.com>
2022-01-31 09:13:58 +0900

    データマイグレーションを修正、実行

--- db/data/20220130224201_copy_timestamp_to_published_at_for_blog.rb
+++ db/data/20220130224201_copy_timestamp_to_published_at_for_blog.rb
@@ -2,7 +2,7 @@
 
 class CopyTimestampToPublishedAtForBlog < ActiveRecord::Migration[6.1]
   def up
-    Articles.where(wip: false).where(published_at: nil).find_each do |article|
+    Article.where(wip: false).where(published_at: nil).find_each do |article|
       article.published_at = article.created_at
       article.save!(validate: false)
     end
