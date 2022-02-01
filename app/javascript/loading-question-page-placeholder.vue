7caaaf1e368eae558220ee6da024616618c1cef7
R-Tsukada <r.tsukada14@gmail.com>
2022-02-01 14:21:14 +0900

    ローディングのデザインのためにファイル追加と、それに伴う修正

---
+++ app/javascript/loading-question-page-placeholder.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.thread-list.a-card.is-loading
+  loadingListQuestionPagePlaceholder(v-for='num in itemCount', :key='num')
+</template>
+
+<script>
+import LoadingListQuestionPageItemPlaceholder from './loading-question-page-itme-placeholder.vue'
+
+export default {
+  components: {
+    loadingListQuestionPageItemPlaceholder: LoadingListQuestionPageItemPlaceholder
+  },
+  data() {
+    return {
+      itemCount: 8
+    }
+  }
+}
+</script>
