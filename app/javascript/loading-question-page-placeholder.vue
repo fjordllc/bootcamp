fbfc302469d43245d5f71833128ee61c2eaa2426
R-Tsukada <r.tsukada14@gmail.com>
2022-02-02 22:50:35 +0900

    メソッド名を変更

--- app/javascript/loading-question-page-placeholder.vue
+++ app/javascript/loading-question-page-placeholder.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .thread-list.a-card.is-loading
-  loadingListQuestionPagePlaceholder(v-for='num in itemCount', :key='num')
+  loadingQuestionPageItemPlaceholder(v-for='num in itemCount', :key='num')
 </template>
 
 <script>
