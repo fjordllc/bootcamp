02e96855b97865ade1a078e0b0b36dafc0e69bc7
Aseiide <eiseiide@gmail.com>
2022-02-02 21:02:57 +0900

    通知を既読にする処理を入れてテストを書いた

--- test/system/notification/tabs_badges_test.rb
+++ test/system/notification/tabs_badges_test.rb
@@ -3,22 +3,61 @@
 require 'application_system_test_case'
 
 class Notification::TabsBadgesTest < ApplicationSystemTestCase
-  test 'unread badges are displayed' do
+  setup do
+    Notification.create!(
+      message: 'お知らせ',
+      kind: 5,
+      link: '/notifications/1',
+      user: users(:sotugyou),
+      sender: users(:komagata),
+      read: false
+    )
+
+    Notification.create!(
+      message: 'メンション',
+      kind: 2,
+      link: '/notifications/2',
+      user: users(:sotugyou),
+      sender: users(:komagata),
+      read: false
+    )
+
+    Notification.create!(
+      message: 'コメント',
+      kind: 0,
+      link: '/notifications/3',
+      user: users(:sotugyou),
+      sender: users(:komagata),
+      read: false
+    )
+
+    Notification.create!(
+      message: '提出物の確認',
+      kind: 1,
+      link: '/notifications/4',
+      user: users(:sotugyou),
+      sender: users(:komagata),
+      read: false
+    )
+
     Notification.create!(
       message: 'watch中',
       kind: 8,
-      link: '/notifications/1',
+      link: '/notifications/5',
       user: users(:sotugyou),
-      sender: users(:komagata)
+      sender: users(:komagata),
+      read: false
     )
     Notification.create!(
       message: 'フォロー中',
       kind: 13,
-      link: '/notifications/1',
+      link: '/notifications/6',
       user: users(:sotugyou),
-      sender: users(:komagata)
+      sender: users(:komagata),
+      read: false
     )
-
+  end
+  test 'unread badges are displayed' do
     visit_with_auth '/notifications', 'sotugyou'
 
     assert_selector '#body > div.wrapper > main > div > div > ul > li:nth-child(1) > a > div' do
@@ -45,7 +84,10 @@ class Notification::TabsBadgesTest < ApplicationSystemTestCase
   end
 
   test 'unread badges are not displayed' do
-    visit_with_auth '/notifications', 'machida'
+    user = users(:sotugyou)
+    user.notifications.unreads.update_all(read: true) # rubocop:disable Rails/SkipsModelValidations
+
+    visit_with_auth '/notifications', 'sotugyou'
 
     assert_no_selector '#body > div.wrapper > main > div > div > ul > li:nth-child(1) > a > div' do
       assert_no_selector 'div.page-tabs__item-count.a-notification-count'
