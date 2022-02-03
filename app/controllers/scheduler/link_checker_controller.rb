19e6bb8b4eff002e0faa75fad6bf88121c30e48d
AudioStakes <daisuke.sato@enishi-tech.com>
2022-02-03 14:00:45 +0900

    LinkChecker::Checker クラスをモジュールにした

--- app/controllers/scheduler/link_checker_controller.rb
+++ app/controllers/scheduler/link_checker_controller.rb
@@ -4,10 +4,10 @@ class Scheduler::LinkCheckerController < SchedulerController
   def show
     documents = Page.all + Practice.all
     links = LinkChecker::Extractor.extract_links_from_multi(documents)
-    checker = LinkChecker::Checker.new(links)
+    summary_of_broken_links = LinkChecker::Checker.check_broken_links(links)
 
-    ChatNotifier.message(checker.summary_of_broken_links, username: 'リンクチェッカー', webhook_url: ENV['DISCORD_BUG_WEBHOOK_URL'])
+    ChatNotifier.message(summary_of_broken_links, username: 'リンクチェッカー', webhook_url: ENV['DISCORD_BUG_WEBHOOK_URL'])
 
-    render plain: checker.summary_of_broken_links
+    render plain: summary_of_broken_links
   end
 end
