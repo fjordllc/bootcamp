580e53b5c077729cc3ed8f01a9d91d54c5704500
maeda-seina <ayrton19601027@gmail.com>
2022-02-05 02:01:33 +0900

    プレースフォルダの繰り返し部分をコンポーネントに分けた

---
+++ app/javascript/loading-cources-practices-page-user-icon-item-placeholder.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+.m-user-icons__item
+  .m-user-icons__item-link
+    .m-user-icons__item-icon.a-user-icon.a-placeholder
+</template>
---
+++ app/javascript/loading-courses-practices-page-items-placeholder.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.categories-item.practices
+  .categories-item__header
+    .categories-item__title.a-placeholder
+  .categories-item__description
+    .is-long-text.a-placeholder
+      p
+  .categories-item__body
+    .category-practices
+      loadingCoursesPracticesPageItemPlaceholder(
+        v-for='num in itemCount',
+        :key='num'
+      )
+</template>
+
+<script>
+import LoadingCoursesPracticesPageItemPlaceholder from './loading-courses-practices-page-item-placeholder.vue'
+
+export default {
+  components: {
+    loadingCoursesPracticesPageItemPlaceholder:
+      LoadingCoursesPracticesPageItemPlaceholder
+  },
+  data() {
+    return {
+      itemCount: 3
+    }
+  }
+}
+</script>
