781e25cdf0c774db0c0c60d7054d402906dcead1
tksmasaki <tk.masaki70@gmail.com>
2022-02-05 17:39:45 +0900

    テスト名の btn を button に修正

--- test/system/current_user/reports_test.rb
+++ test/system/current_user/reports_test.rb
@@ -8,13 +8,13 @@ class CurrentUser::ReportsTest < ApplicationSystemTestCase
     assert_equal '自分の日報 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
 
-  test 'show reports download btn when reports exist' do
+  test 'show reports download button when reports exist' do
     # 日報があるユーザーでログイン
     visit_with_auth '/current_user/reports', 'hatsuno'
     assert_text '日報一括ダウンロード'
   end
 
-  test 'not show reports download btn when no reports' do
+  test 'not show reports download button when no reports' do
     # 日報がないユーザーでログイン
     visit_with_auth '/current_user/reports', 'nippounashi'
     assert_no_text '日報一括ダウンロード'
