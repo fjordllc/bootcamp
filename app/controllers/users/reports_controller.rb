b9fe1a08571f3acb79c501ed4926ef2bd30071cb
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2022-02-07 08:56:49 +0900

    users/reports_controllerでエクスポート処理を共通化

--- app/controllers/users/reports_controller.rb
+++ app/controllers/users/reports_controller.rb
@@ -4,8 +4,16 @@ class Users::ReportsController < ApplicationController
   before_action :require_login
   before_action :set_user
   before_action :set_reports
+  before_action :set_export
 
-  def index; end
+  def index
+    respond_to do |format|
+        format.html
+        format.md do
+          send_reports_markdown(@reports_for_export)
+        end
+    end
+  end
 
   private
 
@@ -20,4 +28,16 @@ class Users::ReportsController < ApplicationController
   def user
     @user ||= User.find(params[:user_id])
   end
+
+  def set_export
+    @reports_for_export = @user.reports.not_wip
+  end
+
+  def send_reports_markdown(reports)
+    Dir.mktmpdir do |folder_path|
+      ReportExporter.export(reports, folder_path)
+      send_data(File.read("#{folder_path}/reports.zip"), filename: '日報一覧.zip')
+    end
+  end
+
 end
