25a2ca6720151fca81cb0da616a0a51275c5d044
kimura <kaisumi@langue-lab.com>
2022-02-09 14:54:11 +0900

    タイポ修正、不要な条件削除、5日以前の条件追加

--- app/controllers/api/products/unassigned_text_controller.rb
+++ app/controllers/api/products/unassigned_text_controller.rb
@@ -2,14 +2,13 @@
 
 class API::Products::UnassignedTextController < API::BaseController
   def show
-    @products = Product
-                .unassigned
-                .unchecked
-                .not_wip
-                .list
-                .order_for_not_wip_list
-    @passed5 = @products.count { |product| product.elapsed_days == 5 }
-    @passed6 = @products.count { |product| product.elapsed_days == 6 }
-    @over7 = @products.count { |product| product.elapsed_days >= 7 }
+    products = Product
+               .unassigned
+               .unchecked
+               .not_wip
+               .five_days_and_earlier
+    @passed5 = products.count { |product| product.elapsed_days == 5 }
+    @passed6 = products.count { |product| product.elapsed_days == 6 }
+    @over7 = products.count { |product| product.elapsed_days >= 7 }
   end
 end
