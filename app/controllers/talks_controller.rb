fafb5979abd40e10f2e371d555470b0ea5858988
maeda-seina <ayrton19601027@gmail.com>
2022-02-14 14:39:32 +0900

    ユーザーのステータスでの絞り込み機能を実装（Railsで表示）

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 class TalksController < ApplicationController
+  TARGETS = %w[student_and_trainee graduate adviser mentor trainee retired all].freeze
   before_action :set_talk, only: %i[show]
   before_action :set_user, only: %i[show]
   before_action :require_admin_login, only: %i[index]
   before_action :allow_show_talk_page_only_admin, only: %i[show]
 
-  def index; end
+  def index
+    @target = params[:target]
+    @target = 'student_and_trainee' unless TARGETS.include?(@target)
+    @users_talk = Talk.joins(:user).merge(User.users_role(@target))
+  end
 
-  def show;  end
+  def show; end
 
   private
 
