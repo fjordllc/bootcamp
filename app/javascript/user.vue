cd7288e7a5825ea4be153a1faaac501ef8d46ee9
Paru871 <82434093+Paru871@users.noreply.github.com>
2022-02-18 22:25:36 +0900

    talks.ymlとusers.ymlの差分を解消とprettier適用

--- app/javascript/user.vue
+++ app/javascript/user.vue
@@ -57,7 +57,9 @@
                 :userId='user.id',
                 :isWatching='user.isWatching'
               )
-            li.card-main-actions__item.is-only-admin(v-if='currentUser.admin && user.talkPresence')
+            li.card-main-actions__item.is-only-admin(
+              v-if='currentUser.admin && user.talkPresence'
+            )
               a.a-button.is-secondary.is-md.is-block(:href='user.talkUrl')
                 | 相談部屋
 </template>
