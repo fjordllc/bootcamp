91b8e19603c0949862dc8e56ae1489e16cb5eaae
Aseiide <eiseiide@gmail.com>
2022-02-20 18:51:05 +0900

    htmlのクラスを修正

--- app/javascript/notifications_bell.vue
+++ app/javascript/notifications_bell.vue
@@ -9,7 +9,7 @@ li.header-links__item(v-bind:class='hasCountClass')
         .header-notification-count.a-notification-count.test-notification-count(
           v-show='notificationExist'
         ) {{ this.notificationCount }}
-        .loadding-notifications-bell(v-if='loading')
+        .header-loadding-notifications-bell(v-if='loading')
           | loading
         i.fas.fa-bell
         .header-links__link-label 通知
