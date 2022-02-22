33b922be95dc503469ad837a3389c9298db81177
ot0m1 <coding_addiction@yahoo.co.jp>
2022-02-22 20:12:43 +0900

    ヘルパーのテストを追加

---
+++ test/helpers/search_hepler.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class  SearchHelperTest < ActionView::TestCase
+  test 'does it return correct boolean value in talk' do
+    user = users(:kimura)
+    assert_equal true, talk?(user)
+  end
+
+  test 'can get the correct user id' do
+    user = users(:kimura)
+    assert_equal 915604563, find_talk_id_from_user_id(user)
+  end
+end
