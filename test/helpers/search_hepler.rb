634086fbe1780eb9d44fed98caf4261dd48ffd35
ot0m1 <coding_addiction@yahoo.co.jp>
2022-02-25 19:30:26 +0900

    テストのパターンを追加

--- test/helpers/search_hepler.rb
+++ test/helpers/search_hepler.rb
@@ -6,5 +6,8 @@ class  SearchHelperTest < ActionView::TestCase
   test 'does it return correct boolean value in talk' do
     user = users(:kimura)
     assert_equal true, talk?(user)
+
+    user = users(:taikai3)
+    assert_equal false, talk?(user)
   end
 end
