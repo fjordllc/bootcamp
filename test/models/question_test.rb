d869e5ff740ed930afa5935875f26daf1cd41deb
Paru871 <82434093+Paru871@users.noreply.github.com>
2022-02-27 10:47:58 +0900

    データマイグレーションファイルを作成

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -12,10 +12,10 @@ class QuestionTest < ActiveSupport::TestCase
     question2 = kimura.questions.new(title: '質問タイトル', description: '質問本文', wip: true)
     assert_not question2.will_be_published?
 
-    question3 = kimura.questions.new(title: '質問タイトル', description: '質問本文', wip: false, published_at: "2022-01-01")
+    question3 = kimura.questions.new(title: '質問タイトル', description: '質問本文', wip: false, published_at: '2022-01-01')
     assert_not question3.will_be_published?
 
-    question4 = kimura.questions.new(title: '質問タイトル', description: '質問本文', wip: true, published_at: "2022-01-01")
+    question4 = kimura.questions.new(title: '質問タイトル', description: '質問本文', wip: true, published_at: '2022-01-01')
     assert_not question4.will_be_published?
   end
 end
