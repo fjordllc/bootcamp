7f133ba3e0e3a5c3db7630bff84d890738b4e85f
ot0m1 <coding_addiction@yahoo.co.jp>
2022-03-02 23:35:20 +0900

    テスト名を変更

--- test/helpers/search_hepler.rb
+++ test/helpers/search_hepler.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class SearchHelperTest < ActionView::TestCase
-  test 'does it return correct boolean value in talk' do
+  test 'return whether talk' do
     user = users(:kimura)
     assert_equal true, talk?(user)
 
