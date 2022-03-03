99ed02e8c16a39423cf023f156842064a270b34f
Junichi Ito <jit@sonicgarden.jp>
2022-03-03 17:01:19 +0900

    不安定なテストを安定化させた（つもり。あまり自信はない）

--- test/system/current_user/watches_test.rb
+++ test/system/current_user/watches_test.rb
@@ -8,6 +8,12 @@ class CurrentUser::WatchesTest < ApplicationSystemTestCase
     assert_equal 'Watch中 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
 
+  # 画面上では更新の完了がわからないため、やむを得ずsleepする
+  # 注意）安易に使用しないこと!! https://bootcamp.fjord.jp/pages/use-assert-text-instead-of-wait-for-vuejs
+  def wait_for_watch_change
+    sleep 1
+  end
+
   # 下記3つのtestは/watches削除に伴い、WatchingTestより転記し修正した
   test 'show my watch list' do
     visit_with_auth '/current_user/watches', 'hajime'
@@ -15,6 +21,7 @@ class CurrentUser::WatchesTest < ApplicationSystemTestCase
     question = questions(:question3)
     visit question_path(question)
     find('#watch-button').click
+    wait_for_watch_change
     visit '/current_user/watches'
     assert_text 'テストの質問1'
   end
@@ -37,6 +44,7 @@ class CurrentUser::WatchesTest < ApplicationSystemTestCase
     find(:css, '#spec-edit-mode').set(true)
     assert_selector '.thread-list-item__option'
     first('#watch-button').click
+    wait_for_watch_change
     assert_no_text '作業週1日目'
     visit report_path(report)
     assert_text 'Watch'
