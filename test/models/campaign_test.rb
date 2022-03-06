24ca77d134d40e5305ba049e0f51e06b3e9472cb
aim2bpg <honotaka2016@gmail.com>
2022-03-07 08:20:55 +0900

    終了日時関連とキャンペーンテストのリファクタリング

--- test/models/campaign_test.rb
+++ test/models/campaign_test.rb
@@ -10,9 +10,4 @@ class CampaignTest < ActiveSupport::TestCase
     assert_equal Campaign.recently_campaign, current_campaign.start_at..current_campaign.end_at
     assert_not_equal Campaign.recently_campaign, past_campaign.start_at..past_campaign.end_at
   end
-
-  test 'today is campaign?' do
-    campaign = campaigns(:campaign1)
-    assert_equal Campaign.today_campaign?, (campaign.start_at..campaign.end_at).cover?(Time.current)
-  end
 end
--- test/system/admin/campaigns_test.rb
+++ test/system/admin/campaigns_test.rb
@@ -3,58 +3,54 @@
 require 'application_system_test_case'
 
 class CampaignsTest < ApplicationSystemTestCase
-  WEEK_DAY = %w[日 月 火 水 木 金 土].freeze
+  TODAY = Time.current
+  PERIOD = 6
 
-  test 'show link new campaign' do
-    visit_with_auth admin_campaigns_path, 'komagata'
-    assert_link 'お試し延長作成'
-  end
-
-  test 'not visit except admin' do
+  test 'non-admin cannot be visited campaign page' do
     visit_with_auth admin_campaigns_path, 'kimura'
     assert_text '管理者としてログインしてください'
   end
 
-  test 'create a new campaign' do
-    today = Time.current
-    seven_days_later = Time.current + 7.days
+  test 'admin can be visited campaign page' do
+    visit_with_auth admin_campaigns_path, 'komagata'
+    assert_link 'お試し延長作成'
+  end
+
+  test 'new campaign cannot be created' do
     visit_with_auth new_admin_campaign_path, 'komagata'
     within 'form[name=campaign]' do
-      fill_in 'campaign[start_at]', with: Time.zone.parse(today.to_s)
-      fill_in 'campaign[end_at]', with: Time.zone.parse(seven_days_later.to_s)
+      fill_in 'campaign[start_at]', with: Time.zone.parse('2021-12-10 00:00')
+      fill_in 'campaign[end_at]', with: Time.zone.parse('2021-12-15 23:58')
+      fill_in 'campaign[title]', with: '終了日時の入力値がお試し日数に満たないケース'
+      fill_in 'campaign[trial_period]', with: PERIOD
+      click_button '内容を保存'
+    end
+    assert_text '終了日時は2021/12/15 23:59以降を入力してください。'
+  end
+
+  test 'new campaign can be created' do
+    visit_with_auth new_admin_campaign_path, 'komagata'
+    within 'form[name=campaign]' do
+      fill_in 'campaign[start_at]', with: Time.zone.parse(TODAY.to_s)
+      fill_in 'campaign[end_at]', with: Time.zone.parse((TODAY + PERIOD.days).to_s)
       fill_in 'campaign[title]', with: 'お正月キャンペーン'
-      fill_in 'campaign[trial_period]', with: 7
+      fill_in 'campaign[trial_period]', with: PERIOD
       click_button '内容を保存'
     end
     assert_text 'お試し延長を作成しました。'
     assert_text 'お正月キャンペーン'
-    assert_text today.strftime("%Y年%m月%d日(#{WEEK_DAY[today.wday]}) %H:%M")
-    assert_text seven_days_later.strftime("%Y年%m月%d日(#{WEEK_DAY[seven_days_later.wday]}) %H:%M")
+    assert_text I18n.l(TODAY)
+    assert_text I18n.l(TODAY + PERIOD.days)
   end
 
-  test 'update a campaign' do
+  test 'created campaign can be updated' do
     visit_with_auth edit_admin_campaign_path(campaigns(:campaign1)), 'komagata'
     within 'form[name=campaign]' do
-      fill_in 'campaign[start_at]', with: '2022-03-01'
-      fill_in 'campaign[end_at]', with: '2022-03-08'
       fill_in 'campaign[title]', with: '春のお試し祭り'
-      fill_in 'campaign[trial_period]', with: 7
       click_button '内容を保存'
     end
     assert_text 'お試し延長を更新しました。'
     assert_text '春のお試し祭り'
-  end
-
-  test 'cannot create a new campaign when start_at > end_at' do
-    visit_with_auth new_admin_campaign_path, 'komagata'
-    within 'form[name=campaign]' do
-      fill_in 'campaign[title]', with: 'お試し延長開始日時 > お試し延長終了日時のテスト'
-      fill_in 'campaign[start_at]', with: Time.zone.parse('2021-12-10 00:00')
-      fill_in 'campaign[end_at]', with: Time.zone.parse('2021-12-5 23:59')
-      fill_in 'campaign[trial_period]', with: 7
-      click_button '内容を保存'
-    end
-    assert_text '終了日時は開始日時よりも後の日時にしてください。'
   end
 
   test 'welcome trial extension campaign start to end' do
@@ -69,24 +65,16 @@ class CampaignsTest < ApplicationSystemTestCase
   end
 
   test 'welcome trial extension campaign period outside' do
-    visit_with_auth edit_admin_campaign_path(campaigns(:campaign1)), 'komagata'
-    within 'form[name=campaign]' do
-      fill_in 'campaign[start_at]', with: '2021-03-01'
-      fill_in 'campaign[end_at]', with: '2021-03-08'
-      fill_in 'campaign[title]', with: 'キャンペーン期間外でデフォルトのお試し期間3日間の表示テスト'
-      fill_in 'campaign[trial_period]', with: 7
-      click_button '内容を保存'
-    end
+    assert_equal Campaign.today_is_campaign?, Campaign.recently_campaign.cover?(TODAY)
 
     visit welcome_path
     assert_no_text '通常 3日間 のお試し期間が'
 
-    today = Time.current
-    example_start_at = today.strftime('%-m月%-d日10時10分10秒')
-    example_end_at = (today + 3.days - 1).strftime('%-m月%-d日10時10分9秒')
-    example_pay_at = (today + 3.days).strftime('%-m月%-d日10時10分10秒')
-
     visit pricing_path
+    example_start_at = TODAY.strftime('%-m月%-d日10時10分10秒')
+    example_end_at = (TODAY + 3.days - 1).strftime('%-m月%-d日10時10分9秒')
+    example_pay_at = (TODAY + 3.days).strftime('%-m月%-d日10時10分10秒')
+
     assert_text "3日間のお試し期間\n月額29,800円は決して安い金額ではありません。"
     assert_text 'フィヨルドブートキャンプを使うべきかを判断するために3日間のお試し期間を用意'
     assert_text 'その3日間、がっつりフィヨルドブートキャンプを見たり使ったりして判断してください。'
@@ -106,45 +94,58 @@ class CampaignsTest < ApplicationSystemTestCase
     assert_text '入会から3日間（72時間）は機能制限なくフルでフィヨルドブートキャンプを活用いただけます'
   end
 
-  test 'welcome trial extension campaign period inside' do
-    today = Time.current
-    trial_period = 7
-    seven_days_later = Time.current + trial_period.days
+  test 'welcome 5days trial extension campaign period inside' do
     visit_with_auth new_admin_campaign_path, 'komagata'
     within 'form[name=campaign]' do
-      fill_in 'campaign[start_at]', with: Time.zone.parse(today.to_s)
-      fill_in 'campaign[end_at]', with: Time.zone.parse(seven_days_later.to_s)
-      fill_in 'campaign[title]', with: 'キャンペーン期間内のお試し延長期間7日間の表示テスト'
-      fill_in 'campaign[trial_period]', with: trial_period
+      fill_in 'campaign[start_at]', with: Time.zone.parse(TODAY.to_s)
+      fill_in 'campaign[end_at]', with: Time.zone.parse((TODAY + PERIOD.days).to_s)
+      fill_in 'campaign[title]', with: 'お正月キャンペーン'
+      fill_in 'campaign[trial_period]', with: 5
+      click_button '内容を保存'
+    end
+    visit welcome_path
+    assert_text 'お試し期間が延長！！'
+  end
+
+  test 'welcome trial extension campaign period inside' do
+    visit_with_auth new_admin_campaign_path, 'komagata'
+    within 'form[name=campaign]' do
+      fill_in 'campaign[start_at]', with: Time.zone.parse(TODAY.to_s)
+      fill_in 'campaign[end_at]', with: Time.zone.parse((TODAY + PERIOD.days).to_s)
+      fill_in 'campaign[title]', with: 'お正月キャンペーン'
+      fill_in 'campaign[trial_period]', with: PERIOD
       click_button '内容を保存'
     end
 
-    visit welcome_path
-    assert_text '通常 3日間 のお試し期間が'
-    assert_text "#{trial_period}日間 となります。"
-    assert_text "#{trial_period}日以内に退会すれば受講料はかかりません。"
+    assert_equal Campaign.today_is_campaign?, Campaign.recently_campaign.cover?(TODAY)
 
-    example_start_at = today.strftime('%-m月%-d日10時10分10秒')
-    example_end_at = (today + trial_period.days - 1).strftime('%-m月%-d日10時10分9秒')
-    example_pay_at = (today + trial_period.days).strftime('%-m月%-d日10時10分10秒')
+    visit welcome_path
+    assert_text 'お試し期間が倍以上に延長！！'
+    assert_text '通常 3日間 のお試し期間が'
+    assert_text "#{PERIOD}日間 となります。"
+    assert_text "#{PERIOD}日以内に退会すれば受講料はかかりません。"
+
+    example_start_at = TODAY.strftime('%-m月%-d日10時10分10秒')
+    example_end_at = (TODAY + PERIOD.days - 1).strftime('%-m月%-d日10時10分9秒')
+    example_pay_at = (TODAY + PERIOD.days).strftime('%-m月%-d日10時10分10秒')
 
     visit pricing_path
-    assert_text "キャンペーン中につき、#{trial_period}日間のお試し期間"
-    assert_text "フィヨルドブートキャンプを使うべきかを判断するために#{trial_period}日間のお試し期間を用意"
-    assert_text "その#{trial_period}日間、がっつりフィヨルドブートキャンプを見たり使ったりして判断してください。"
+    assert_text "キャンペーン中につき、#{PERIOD}日間のお試し期間"
+    assert_text "フィヨルドブートキャンプを使うべきかを判断するために#{PERIOD}日間のお試し期間を用意"
+    assert_text "その#{PERIOD}日間、がっつりフィヨルドブートキャンプを見たり使ったりして判断してください。"
 
-    assert_text "厳密にはお試し期間は#{trial_period * 24}時間（#{trial_period}日間）になります。"
+    assert_text "厳密にはお試し期間は#{PERIOD * 24}時間（#{PERIOD}日間）になります。"
     assert_text "例えば#{example_start_at}にフィヨルドブートキャンプを利用開始したとすると、"
-    assert_text "そこから#{trial_period}日後の#{example_end_at}がお試し期間終了のタイミングになります。"
+    assert_text "そこから#{PERIOD}日後の#{example_end_at}がお試し期間終了のタイミングになります。"
     assert_text "その後#{example_pay_at}に最初の1ヶ月分の料金が引き落とされます。"
 
     assert_text "#{example_start_at} #{example_end_at} #{example_pay_at}"
 
     visit new_user_path
     assert_text 'お試し期間の延長が適用されます。'
-    assert_text "クレジットカード登録日を含む#{trial_period}日間はお試し期間です。"
+    assert_text "クレジットカード登録日を含む#{PERIOD}日間はお試し期間です。"
 
     visit_with_auth '/', 'hatsuno'
-    assert_text "入会から#{trial_period}日間（#{trial_period * 24}時間）は機能制限なくフルでフィヨルドブートキャンプを活用いただけます"
+    assert_text "入会から#{PERIOD}日間（#{PERIOD * 24}時間）は機能制限なくフルでフィヨルドブートキャンプを活用いただけます"
   end
 end
