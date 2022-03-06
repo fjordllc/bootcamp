cdb3bfec5fcba3a3edda9f60a314bb067a1e9030
aim2bpg <honotaka2016@gmail.com>
2022-03-07 08:20:55 +0900

    終了日時関連とキャンペーンテストのリファクタリング

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,15 +3,16 @@
 class Campaign < ApplicationRecord
   validates :start_at, presence: true
   validates :end_at, presence: true
-  validates :title, presence: true
-  validates :trial_period, presence: true, numericality: { greater_than_or_equal_to: 4 }
 
   # TODO: Rails 7に更新後、 `ComparisonValidator` を使うように直す。
   # refs: https://github.com/rails/rails/pull/40095
   # validates :end_at, greater_than: :start_at
-  with_options if: -> { start_at && end_at } do
-    validate :end_at_be_greater_than_start_at
+  with_options if: -> { start_at && end_at && trial_period } do
+    validate :end_at_be_greater_than_trial_period
   end
+
+  validates :title, presence: true
+  validates :trial_period, presence: true, numericality: { greater_than_or_equal_to: 4 }
 
   def self.recently_campaign
     campaign = Campaign.order(end_at: :desc).first
@@ -35,10 +36,12 @@ class Campaign < ApplicationRecord
 
   private
 
-  def end_at_be_greater_than_start_at
+  def end_at_be_greater_than_trial_period
     diff = end_at - start_at
-    return unless diff <= 0
+    period = trial_period.days - 1.minute
+    return if diff >= period
 
-    errors.add(:end_at, ': 終了日時は開始日時よりも後の日時にしてください。')
+    shortest_end_at = start_at + period
+    errors.add(:end_at, "は#{I18n.l(shortest_end_at, format: :short)}以降を入力してください。")
   end
 end
