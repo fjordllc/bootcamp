b4c2a96b0bd64504769c594d4a1c25ba1da905d7
Aseiide <eiseiide@gmail.com>
2022-03-14 15:46:29 +0900

    ascと対応させるためにdescとわかるようにメソッド名を変更

--- app/controllers/api/products/passed_controller.rb
+++ app/controllers/api/products/passed_controller.rb
@@ -4,7 +4,7 @@ class API::Products::PassedController < API::BaseController
   def show
     products = Product
                .list
-               .order_for_not_wip_list
+               .order_for_not_wip_list_descinding
     @passed5 = products.count { |product| product.elapsed_days == 5 }
     @passed6 = products.count { |product| product.elapsed_days == 6 }
     @over7 = products.count { |product| product.elapsed_days >= 7 }
