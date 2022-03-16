73d8db2e39232fb0a3667b73be89dc144e879e2b
honda <clio209@gmail.com>
2022-03-16 10:38:57 +0900

    変数talk_linkを削除

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module TalksHelper
-  def unreplied_index_path(resource)
-    admin_login? ? talks_unreplied_index_path : talk_path(resource)
+  def unreplied_index_path(talk)
+    admin_login? ? talks_unreplied_index_path : talk_path(talk)
   end
 end
