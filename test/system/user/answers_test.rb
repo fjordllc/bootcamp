7e1a0eef1d7723aebc53f784041759ec10bf4a21
eatplaynap <eatplaynap329@gmail.com>
2022-03-18 17:37:49 +0900

    落ちていたテストの修正

--- test/system/user/answers_test.rb
+++ test/system/user/answers_test.rb
@@ -5,6 +5,6 @@ require 'application_system_test_case'
 class User::AnswersTest < ApplicationSystemTestCase
   test 'show listing answers' do
     visit_with_auth "/users/#{users(:komagata).id}/answers", 'komagata'
-    assert_equal 'komagataの回答 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
+    assert_equal 'komagata | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
   end
 end
