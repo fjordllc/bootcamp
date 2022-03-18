b9917ba4c18631172dfbbef3853cdb7fb0dba327
chung3011 <chung.hitc97@gmail.com>
2022-03-18 22:08:53 +0700

    fix eslint errors

--- app/javascript/markdown-it-mention.js
+++ app/javascript/markdown-it-mention.js
@@ -1,13 +1,16 @@
 import MarkdownItRegexp from 'markdown-it-regexp'
 
-export default MarkdownItRegexp(
-  /@([a-zA-Z0-9_-]+)/,
-  (match) => {
-    if(match.input.includes("(http")) {
-      const output = ("["+match.input).match(/^\[([\w\s\d+&@]+)\]\((https?:\/\/[\w\d./?=#]+)\)$/)
-      return output ? output[1].match(/@([a-zA-Z0-9_-]+)/)[0] : ("["+match.input).match(/@([a-zA-Z0-9_-]+)/)[0]
-    } else {
-      return `<a href="${match[0] === '@mentor' ? `/users?target=mentor` : `/users/${match[1]}`}">${match[0]}</a>`
-    }
+export default MarkdownItRegexp(/@([a-zA-Z0-9_-]+)/, (match) => {
+  if (match.input.includes('(http')) {
+    const output = ('[' + match.input).match(
+      /^\[([\w\s\d+&@]+)\]\((https?:\/\/[\w\d./?=#]+)\)$/
+    )
+    return output
+      ? output[1].match(/@([a-zA-Z0-9_-]+)/)[0]
+      : ('[' + match.input).match(/@([a-zA-Z0-9_-]+)/)[0]
+  } else {
+    return `<a href="${
+      match[0] === '@mentor' ? `/users?target=mentor` : `/users/${match[1]}`
+    }">${match[0]}</a>`
   }
-)
+})
