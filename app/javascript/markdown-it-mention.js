c3b555e1dac20dddb6e8b855a570416cdb1d8a4d
chung3011 <chung.hitc97@gmail.com>
2022-03-22 14:50:25 +0700

    mentionRegexp変数に入れる

--- app/javascript/markdown-it-mention.js
+++ app/javascript/markdown-it-mention.js
@@ -1,13 +1,15 @@
 import MarkdownItRegexp from 'markdown-it-regexp'
 
-export default MarkdownItRegexp(/@([a-zA-Z0-9_-]+)/, (match) => {
+const mentionRegexp = /@([a-zA-Z0-9_-]+)/
+
+export default MarkdownItRegexp(mentionRegexp, (match) => {
   if (match.input.includes('(http')) {
     const output = ('[' + match.input).match(
       /^\[([\w\s\d+&@]+)\]\((https?:\/\/[\w\d./?=#]+)\)$/
     )
     return output
-      ? output[1].match(/@([a-zA-Z0-9_-]+)/)[0]
-      : ('[' + match.input).match(/@([a-zA-Z0-9_-]+)/)[0]
+      ? output[1].match(mentionRegexp)[0]
+      : ('[' + match.input).match(mentionRegexp)[0]
   } else {
     return `<a href="${
       match[0] === '@mentor' ? `/users?target=mentor` : `/users/${match[1]}`
