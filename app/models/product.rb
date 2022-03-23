f01b98d0e34198128db0df41990f117f49973882
Aseiide <eiseiide@gmail.com>
2022-03-23 14:45:43 +0900

    未アサインタブと未完了タブの並び順を昇順にした

--- app/models/product.rb
+++ app/models/product.rb
@@ -48,9 +48,8 @@ class Product < ApplicationRecord
                { user: :company },
                { checks: { user: { avatar_attachment: :blob } } })
   }
-  scope :order_for_list, -> { order(created_at: :desc, id: :desc) }
-  scope :order_for_not_wip_list_descinding, -> { order(published_at: :desc, id: :desc) }
-  scope :order_for_not_wip_list_ascending, -> { order(published_at: :asc, id: :asc) }
+  scope :order_for_list, -> { order(created_at: :asc, id: :asc) }
+  scope :order_for_not_wip_list, -> { order(published_at: :asc, id: :asc) }
   scope :order_for_self_assigned_list, -> { order(commented_at: :desc, published_at: :desc) }
 
   def self.add_latest_commented_at
