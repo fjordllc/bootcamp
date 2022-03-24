5e18799e3b342d787cfab7cddad05a358f784154
garammasala29 <kmasa1569@gmail.com>
2022-03-25 00:00:21 +0900

    セレクトボックスのIDが存在すればnew Choicesする形に変更
    コンソールにエラーが表示されてしまうため

--- app/javascript/company-select.js
+++ app/javascript/company-select.js
@@ -1,11 +1,15 @@
 import Choices from 'choices.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  return new Choices('#js-company-select', {
-    removeItemButton: true,
-    searchResultLimit: 10,
-    searchPlaceholderValue: '検索ワード',
-    noResultsText: '一致する情報は見つかりません',
-    itemSelectText: '選択'
-  })
+  const element = document.getElementById('js-company-select')
+  if (element) {
+    return new Choices(element, {
+      removeItemButton: true,
+      allowHTML: true,
+      searchResultLimit: 10,
+      searchPlaceholderValue: '検索ワード',
+      noResultsText: '一致する情報は見つかりません',
+      itemSelectText: '選択'
+    })
+  }
 })
