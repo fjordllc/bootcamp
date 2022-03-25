6e72ae06f5182425fa9bc22c6cf90c3d7f160ac6
eatplaynap <eatplaynap329@gmail.com>
2022-03-25 11:03:46 +0900

    prettier通過

--- app/javascript/markdown-it-container-message.js
+++ app/javascript/markdown-it-container-message.js
@@ -3,7 +3,9 @@ import MarkdownItContainer from 'markdown-it-container'
 export default (md) => {
   md.use(MarkdownItContainer, 'message', {
     render: (tokens, idx) => {
-      const messageInfo = tokens[idx].info.trim().match(/^message\s+(alert|danger|warning|info|primary|success)$/)
+      const messageInfo = tokens[idx].info
+        .trim()
+        .match(/^message\s+(alert|danger|warning|info|primary|success)$/)
       const messageName = messageInfo ? ` ${messageInfo[1]}` : ''
       if (tokens[idx].nesting === 1) {
         return `<div class="message${messageName}">\n`
