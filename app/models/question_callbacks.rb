1c491a58f42f02665a38d7a2e465c9d79645bcc2
Paru871 <82434093+Paru871@users.noreply.github.com>
2022-03-29 21:38:18 +0900

    質問本公開時のみメンターのWatchを自動的にonにする修正とテスト追加

--- app/models/question_callbacks.rb
+++ app/models/question_callbacks.rb
@@ -5,6 +5,7 @@ class QuestionCallbacks
     return unless question.saved_change_to_attribute?(:published_at, from: nil)
 
     send_notification_to_mentors(question)
+    create_mentors_watch(question)
     notify_to_chat(question)
     Cache.delete_not_solved_question_count
   end
@@ -31,5 +32,22 @@ class QuestionCallbacks
 
   def delete_notification(question)
     Notification.where(link: "/questions/#{question.id}").destroy_all
+  end
+
+  def create_mentors_watch(question)
+    watch_question_records = watch_records(question)
+    Watch.insert_all(watch_question_records) # rubocop:disable Rails/SkipsModelValidations
+  end
+
+  def watch_records(question)
+    User.mentor.map do |mentor|
+      {
+        watchable_type: 'Question',
+        watchable_id: question.id,
+        created_at: Time.current,
+        updated_at: Time.current,
+        user_id: mentor.id
+      }
+    end
   end
 end
