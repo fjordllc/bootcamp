3f9b7c8cbf0ebe72c0554c9c3021b7bf8cb95df9
niikz <60736158+niikz@users.noreply.github.com>
2022-03-29 16:06:00 +0900

    研修生のユーザー登録情報変更ページで研修生本人・管理者に研修情報のブロックを表示するようにした

---
+++ app/javascript/training-info-toggler.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.getElementsByClassName('form')
+  for (const element of elements) {
+    const checkbox = element.querySelector('input.training-info-toggler-checkbox')
+    const dateInput = element.querySelector('input.training-info-toggler-date')
+    const trainingInfoArea = element.querySelector('div.training-info-toggler')
+
+    if (checkbox.checked === true) {
+      trainingInfoArea.style.display = 'block'
+    } else {
+      dateInput.value = ''
+      trainingInfoArea.style.display = 'none'
+    }
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked === true) {
+        trainingInfoArea.style.display = 'block'
+      } else {
+        dateInput.value = ''
+        trainingInfoArea.style.display = 'none'
+      }
+    })
+  }
+})
