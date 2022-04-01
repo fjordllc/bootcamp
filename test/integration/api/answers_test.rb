8a150a944f893504507af7aa742c222d5b8ebfa9
eatplaynap <eatplaynap329@gmail.com>
2022-04-01 16:52:06 +0900

    テスト修正

--- test/integration/api/answers_test.rb
+++ test/integration/api/answers_test.rb
@@ -2,14 +2,14 @@
 
 require 'test_helper'
 
-class API::Users::AnswersTest < ActionDispatch::IntegrationTest
-  test 'GET /api/users/253826460/answers.json' do
+class API::AnswersTest < ActionDispatch::IntegrationTest
+  test 'GET /api/answers.json?user_id=253826460' do
     user = users(:hajime)
-    get api_user_answers_path(user_id: user.id, format: :json)
+    get api_answers_path(user_id: user.id, format: :json)
     assert_response :unauthorized
 
     token = create_token('hajime', 'testtest')
-    get api_user_answers_path(user_id: user.id, format: :json),
+    get api_answers_path(user_id: user.id, format: :json),
         headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :ok
   end
