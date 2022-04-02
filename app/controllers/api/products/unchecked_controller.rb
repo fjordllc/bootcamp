23845aa9794b788b98dd63a2f7d5f3c1d2b42c04
Aseiide <eiseiide@gmail.com>
2022-04-02 23:07:24 +0900

    未完了の未返信の並びを提出日の昇順にするため

--- app/controllers/api/products/unchecked_controller.rb
+++ app/controllers/api/products/unchecked_controller.rb
@@ -17,7 +17,6 @@ class API::Products::UncheckedController < API::BaseController
                          .unchecked
                          .not_wip
                          .list
-                         .ascending_by_date_of_publishing_and_id
                          .page(params[:page])
                 end
     @latest_product_submitted_just_5days = @products.find { |product| product.elapsed_days == 5 }
