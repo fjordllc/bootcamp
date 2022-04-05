055e34bbefb6b246f2405992fb3f032a2a2a9d45
eatplaynap <eatplaynap329@gmail.com>
2022-04-05 18:28:08 +0900

    Vuexを使って実装

--- app/javascript/checkable.js
+++ app/javascript/checkable.js
@@ -26,6 +26,32 @@ export default {
         .catch((error) => {
           console.warn(error)
         })
+    },
+    checker(productId, currentUserId, url, method, token) {
+      const params = {
+        product_id: productId,
+        current_user_id: currentUserId
+      }
+
+      fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': token
+        },
+        credentials: 'same-origin',
+        redirect: 'manual',
+        body: JSON.stringify(params)
+      })
+        .then(() => {
+          this.$store.dispatch('setProduct', {
+            productId: productId
+          })
+        })
+        .catch((error) => {
+          console.warn(error)
+        })
     }
   }
 }
