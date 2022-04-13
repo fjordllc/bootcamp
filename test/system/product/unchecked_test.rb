e1e751447676104a423c5c2a1ffee282c0092630
Aseiide <eiseiide@gmail.com>
2022-04-13 14:53:12 +0900

    #4345 で対応するためテストを削除

--- test/system/product/unchecked_test.rb
+++ test/system/product/unchecked_test.rb
@@ -133,16 +133,4 @@ class Product::UncheckedTest < ApplicationSystemTestCase
     visit_with_auth "/products/#{products(:product1).id}", 'komagata'
     assert_link '未完了一覧'
   end
-
-  test 'display elapsed days label' do
-    visit_with_auth '/products/unchecked', 'komagata'
-    assert_text '今日提出'
-
-    login_user 'komagata', 'testtest'
-    visit '/products/unchecked?page=2'
-    assert_text '今日提出'
-    assert_text '5日経過'
-    assert_text '6日経過'
-    assert_text '7日以上経過'
-  end
 end
