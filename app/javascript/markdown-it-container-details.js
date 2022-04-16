1a73af118abe27859e2d19277362729feae6b992
honda <clio209@gmail.com>
2022-04-16 20:09:01 +0900

    prettier 修正

--- app/javascript/markdown-it-container-details.js
+++ app/javascript/markdown-it-container-details.js
@@ -1,19 +1,17 @@
 import MarkdownItContainer from 'markdown-it-container'
 
 export default (md) => {
-    md.use(MarkdownItContainer, 'details', {
-        render: (tokens, idx) => {
-            const detailsInfo = tokens[idx].info
-                .trim()
-                .match(/^details\s+(.*)$/)
-            // const  detailsTitle = detailsInfo ? `${detailsInfo[1]}` : ''
-            if (tokens[idx].nesting === 1) {
-                // opening tag
-                return '<details><summary>' + `${detailsInfo[1]}` + '</summary>\n'
-            } else {
-                // closing tag
-                return '</details>\n'
-            }
-        }
-    })
+  md.use(MarkdownItContainer, 'details', {
+    render: (tokens, idx) => {
+      const detailsInfo = tokens[idx].info.trim().match(/^details\s+(.*)$/)
+      // const  detailsTitle = detailsInfo ? `${detailsInfo[1]}` : ''
+      if (tokens[idx].nesting === 1) {
+        // opening tag
+        return '<details><summary>' + `${detailsInfo[1]}` + '</summary>\n'
+      } else {
+        // closing tag
+        return '</details>\n'
+      }
+    }
+  })
 }
