e66e2be99493db16ccbe873fc2fbbac1390b81e4
honda <clio209@gmail.com>
2022-04-19 16:45:44 +0900

    summaryがなくなってもマークダウンが機能するようにdetailsTitleを作成

--- app/javascript/markdown-it-container-details.js
+++ app/javascript/markdown-it-container-details.js
@@ -4,10 +4,10 @@ export default (md) => {
   md.use(MarkdownItContainer, 'details', {
     render: (tokens, idx) => {
       const detailsInfo = tokens[idx].info.trim().match(/^details\s+(.*)$/)
-      // const  detailsTitle = detailsInfo ? `${detailsInfo[1]}` : ''
+      const  detailsTitle = detailsInfo ? `${detailsInfo[1]}` : ''
       if (tokens[idx].nesting === 1) {
         // opening tag
-        return '<details><summary>' + `${detailsInfo[1]}` + '</summary>\n'
+        return '<details><summary>' + detailsTitle + '</summary>\n'
       } else {
         // closing tag
         return '</details>\n'
