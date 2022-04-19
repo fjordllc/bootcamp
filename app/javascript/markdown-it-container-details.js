ff28878dff9e51ea726f3ccffc3b3889984555b2
honda <clio209@gmail.com>
2022-04-19 20:25:35 +0900

    prettier 修正

--- app/javascript/markdown-it-container-details.js
+++ app/javascript/markdown-it-container-details.js
@@ -4,7 +4,7 @@ export default (md) => {
   md.use(MarkdownItContainer, 'details', {
     render: (tokens, idx) => {
       const detailsInfo = tokens[idx].info.trim().match(/^details\s+(.*)$/)
-      const  detailsTitle = detailsInfo ? `${detailsInfo[1]}` : ''
+      const detailsTitle = detailsInfo ? `${detailsInfo[1]}` : ''
       if (tokens[idx].nesting === 1) {
         // opening tag
         return '<details><summary>' + escapeHTML(detailsTitle) + '</summary>\n'
@@ -16,10 +16,11 @@ export default (md) => {
   })
 }
 
-function escapeHTML(string){
-  return string.replace(/&/g, '&lt;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, "&#x27;");
+function escapeHTML(string) {
+  return string
+    .replace(/&/g, '&lt;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#x27;')
 }
