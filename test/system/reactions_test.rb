f3e176367f62b954039ca2b1e64fc43bece48fbb
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-04-20 19:29:38 +0900

    日報で全てのリアクションボタンを押せるかのテストを追加

--- test/system/reactions_test.rb
+++ test/system/reactions_test.rb
@@ -3,7 +3,7 @@
 require 'application_system_test_case'
 
 class ReactionsTest < ApplicationSystemTestCase
-  test 'post new reaction for report' do
+  test 'post new reaction smile for report' do
     visit_with_auth report_path(reports(:report1)), 'komagata'
     first('.card-body .js-reaction-dropdown-toggle').click
     first(".card-body .js-reaction-dropdown li[data-reaction-kind='smile']").click
@@ -52,6 +52,105 @@ class ReactionsTest < ApplicationSystemTestCase
     first(".thread-comment .js-reaction li[data-reaction-kind='heart']").click
     using_wait_time 5 do
       assert_text '❤️1'
+    end
+  end
+
+  test 'post new reaction thumbsup for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='thumbsup']").click
+    using_wait_time 5 do
+      assert_text '👍1'
+    end
+  end
+
+  test 'post new reaction tada for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='tada']").click
+    using_wait_time 5 do
+      assert_text '🎉1'
+    end
+  end
+
+  test 'post new reaction heart for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='heart']").click
+    using_wait_time 5 do
+      assert_text '❤️1'
+    end
+  end
+
+  test 'post new reaction rocket for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='rocket']").click
+    using_wait_time 5 do
+      assert_text '🚀1'
+    end
+  end
+
+  test 'post new reaction eyes for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='eyes']").click
+    using_wait_time 5 do
+      assert_text '👀1'
+    end
+  end
+
+  test 'post new reaction hundred for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='hundred']").click
+    using_wait_time 5 do
+      assert_text '💯1'
+    end
+  end
+
+  test 'post new reaction flexed for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='flexed']").click
+    using_wait_time 5 do
+      assert_text '💪1'
+    end
+  end
+
+  test 'post new reaction okwoman for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='okwoman']").click
+    using_wait_time 5 do
+      assert_text '🙆‍♀️1'
+    end
+  end
+
+  test 'post new reaction loudlycrying for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='loudlycrying']").click
+    using_wait_time 5 do
+      assert_text '😭1'
+    end
+  end
+
+  test 'post new reaction raised_hands for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='raised_hands']").click
+    using_wait_time 5 do
+      assert_text '🙌1'
+    end
+  end
+
+  test 'post new reaction pray for report' do
+    visit_with_auth report_path(reports(:report1)), 'komagata'
+    first('.thread__body .js-reaction-dropdown-toggle').click
+    first(".thread__body .js-reaction-dropdown li[data-reaction-kind='pray']").click
+    using_wait_time 5 do
+      assert_text '🙏1'
     end
   end
 end
