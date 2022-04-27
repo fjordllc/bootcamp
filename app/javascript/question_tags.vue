96eeff07bbef889626d14b9a5c153a40597bbcff
teppei machida <machida@fjord.jp>
2022-04-28 00:45:29 +0900

    ボタンサイズ更新

--- app/javascript/question_tags.vue
+++ app/javascript/question_tags.vue
@@ -24,10 +24,10 @@
     .form-actions
       ul.form-actions__items
         li.form-actions__item.is-main
-          button.a-button.is-primary.is-block.is-md(@click='updateTag')
+          button.a-button.is-primary.is-block.is-sm(@click='updateTag')
             | 保存する
         li.form-actions__item
-          button.a-button.is-secondary.is-block.is-sm(@click='cancel')
+          button.a-button.is-sm.is-text(@click='cancel')
             | キャンセル
 </template>
 
