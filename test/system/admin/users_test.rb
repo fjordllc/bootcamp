58b1b940682c3a06b6eb424c9f15b94fbcc5f145
rosh-1228 <sho.watanabe1228@gmail.com>
2022-04-28 02:26:07 +0900

    研修終了日をプロフィールページに表示する時と表示しない時のテストを追加

--- test/system/admin/users_test.rb
+++ test/system/admin/users_test.rb
@@ -197,6 +197,23 @@ class Admin::UsersTest < ApplicationSystemTestCase
     assert has_field?('user_training_ends_on')
   end
 
+  test 'show training end date if user is a trainee and has a training end date' do
+    user = users(:kensyu)
+    visit_with_auth user_path(user.id), 'kensyu'
+    assert has_text?('研修終了日')
+    assert has_text?('2022年04月01日')
+  end
+
+  test 'not show training end date if user is a trainee and not has a training end date' do
+    user = users(:kensyu)
+    training_ends_on = nil
+    visit_with_auth edit_admin_user_path(user.id), 'komagata'
+    fill_in 'user_training_ends_on', with: training_ends_on
+    click_on '更新する'
+    visit_with_auth user_path(user.id), 'kensyu'
+    assert has_no_field?('user_training_ends_on')
+  end  
+
   test 'update value of training end date' do
     user = users(:kensyu)
     training_ends_on = Date.current.next_year
