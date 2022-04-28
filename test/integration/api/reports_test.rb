8529a391fa092ab623e300fa52892c058716f979
saeyama <camper_green_yellow_21@yahoo.co.jp>
2022-04-28 20:48:01 +0900

    企業を指定したAPIのテストを追加・不要な行を削除

--- test/integration/api/reports_test.rb
+++ test/integration/api/reports_test.rb
@@ -3,8 +3,6 @@
 require 'test_helper'
 
 class API::ReportsTest < ActionDispatch::IntegrationTest
-  fixtures :reports
-
   test 'GET /api/reports.json' do
     get api_reports_path(format: :json)
     assert_response :unauthorized
@@ -32,6 +30,17 @@ class API::ReportsTest < ActionDispatch::IntegrationTest
 
     token = create_token('kimura', 'testtest')
     get api_reports_path(user_id: user.id, format: :json),
+        headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+  end
+
+  test 'GET /api/products.json?company_id=362477616' do
+    company = companies(:company4)
+    get api_reports_path(company_id: company.id, format: :json)
+    assert_response :unauthorized
+
+    token = create_token('kimura', 'testtest')
+    get api_reports_path(company_id: company.id, format: :json),
         headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :ok
   end
