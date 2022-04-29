60034d0172378a57efc71097e85e05c9effcaecd
rosh-1228 <sho.watanabe1228@gmail.com>
2022-04-29 22:58:24 +0900

    研修終了日表示のテストを別ファイルに移動

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -298,21 +298,4 @@ class UsersTest < ApplicationSystemTestCase
     visit_with_auth "/users/#{users(:sotugyou).id}", 'sotugyou'
     assert_not has_button? '非表示'
   end
-
-  test 'show training end date if user is a trainee and has a training end date' do
-    user = users(:kensyu)
-    visit_with_auth user_path(user.id), 'kensyu'
-    assert has_text?('研修終了日')
-    assert has_text?('2022年04月01日')
-  end
-
-  test 'does not display training end date if user is a trainee and not has a training end date' do
-    user = users(:kensyu)
-    training_ends_on = nil
-    visit_with_auth edit_admin_user_path(user.id), 'komagata'
-    fill_in 'user_training_ends_on', with: training_ends_on
-    click_on '更新する'
-    visit_with_auth user_path(user.id), 'kensyu'
-    assert has_no_field?('user_training_ends_on')
-  end
 end
