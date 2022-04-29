adfcfe8c9f4c85ce7cd3e9bb84ea06870cdb1e99
rosh-1228 <sho.watanabe1228@gmail.com>
2022-04-29 12:14:11 +0900

    showからdisplayに文言を変更

--- test/system/admin/users_test.rb
+++ test/system/admin/users_test.rb
@@ -204,7 +204,7 @@ class Admin::UsersTest < ApplicationSystemTestCase
     assert has_text?('2022年04月01日')
   end
 
-  test 'does not show training end date if user is a trainee and not has a training end date' do
+  test 'does not display training end date if user is a trainee and not has a training end date' do
     user = users(:kensyu)
     training_ends_on = nil
     visit_with_auth edit_admin_user_path(user.id), 'komagata'
