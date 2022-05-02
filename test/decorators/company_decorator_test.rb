f35cc75765107909182dcd910f205eb022df4adc
siso <siso25@zoho.com>
2022-05-02 22:26:59 +0900

    企業一覧に研修生の招待リンクを追加

--- test/decorators/company_decorator_test.rb
+++ test/decorators/company_decorator_test.rb
@@ -14,4 +14,9 @@ class CompanyDecoratorTest < ActiveSupport::TestCase
     expected = "http://test.host/users/new?company_id=#{@company1.id}&role=adviser&token=token"
     assert_equal expected, @company1.adviser_sign_up_url
   end
+
+  test '#trainee_sign_up_url' do
+    expected = "http://test.host/users/new?company_id=#{@company1.id}&role=trainee&token=token"
+    assert_equal expected, @company1.trainee_sign_up_url
+  end
 end
