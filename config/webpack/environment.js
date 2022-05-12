8489f5058d7bd09fce7376e2d80fa19d6a635dcc
komagata <komagata@gmail.com>
2022-05-12 10:05:39 +0900

    vue3のvue2互換モードにアップグレード
    vue3への移行のための準備

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,17 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const { environment } = require('@rails/webpacker');
+const { VueLoaderPlugin } = require('vue-loader');
+const vue = require('./loaders/vue');
 const pug = require('./loaders/pug')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
+environment.config.merge({
+  resolve: {
+    alias: {
+      vue: '@vue/compat',
+    },
+  },
+});
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.loaders.prepend('vue', vue);
 environment.loaders.prepend('pug', pug)
-module.exports = environment
+module.exports = environment;
