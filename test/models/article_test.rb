00fd69d016daa49392be07ce7127b0af5e6b97bf
teppei machida <machida@fjord.jp>
2022-05-16 16:52:42 +0900

    :cop:
    :cop:
    
    :cop:
    
    :cop:
    
    :cop:
    
    :cop:
    
    :cop:

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test "thumbnail_url" do
-    article = articles(:article_1)
-    assert_equal "/images/articles/thumbnails/default.png", article.thumbnail_url
+  test 'thumbnail_url' do
+    article = articles(:article1)
+    assert_equal '/images/articles/thumbnails/default.png', article.thumbnail_url
   end
 end
