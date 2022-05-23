d6267b4148493f4cf950bb14da5831b0b25b0de4
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-05-23 19:14:51 +0900

    相談部屋で卒業ボタンを押すテストを追加

--- test/system/talks_test.rb
+++ test/system/talks_test.rb
@@ -354,4 +354,13 @@ class TalksTest < ApplicationSystemTestCase
     fill_in 'js-talk-search-input', with: 'hoge'
     assert_text '一致する相談部屋はありません'
   end
+
+  test 'push guraduation button in talk room when admin logined' do
+    user = users(:kimura)
+    visit_with_auth "/talks/#{user.talk.id}", 'komagata'
+    accept_confirm do
+      click_link '卒業'
+    end
+    assert_text '卒業済'
+  end
 end
