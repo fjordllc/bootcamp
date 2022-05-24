e84a1402c777e305ffbd1730cee202f4d7270cdd
Norifumi Ogawa <n@ogaworks.com>
2022-05-24 14:19:31 +0900

    テストのabstract_notifier対応

--- test/system/notification/assigned_as_checker_test.rb
+++ test/system/notification/assigned_as_checker_test.rb
@@ -13,14 +13,28 @@ class Notification::AssignedAsCheckerTest < ApplicationSystemTestCase
   end
 
   test 'notify mentor when assigned as checker' do
-    visit_with_auth "/products/#{products(:product11).id}/edit", 'komagata'
-    select 'mentormentaro', from: 'product_checker_id'
+    visit_with_auth "/products/#{products(:product1).id}/edit", 'komagata'
+    select 'machida', from: 'product_checker_id'
     click_button '提出する'
     logout
 
-    visit_with_auth '/notifications', 'mentormentaro'
+    visit_with_auth '/notifications', 'machida'
     within first('.card-list-item.is-unread') do
-      assert_text "hatsunoさんの提出物「#{products(:product11).practice.title}」の提出物の担当になりました。"
+      assert_text "mentormentaroさんの提出物「#{products(:product1).practice.title}」の提出物の担当になりました。"
     end
+
+    last_mail = ActionMailer::Base.deliveries.last
+    assert_equal "[bootcamp] mentormentaroさんの提出物#{products(:product1).title}の担当になりました。", last_mail.subject
+  end
+
+  test 'not notice self assigned as checker' do
+    visit_with_auth "/products/#{products(:product1).id}", 'komagata'
+    click_link '内容修正'
+    select 'komagata', from: 'product_checker_id'
+    click_button '提出する'
+    assert_text '担当から外れる'
+
+    visit_with_auth '/notifications?status=unread', 'komagata'
+    assert_no_text "mentormentaroさんの提出物#{products(:product1).title}の担当になりました。"
   end
 end
