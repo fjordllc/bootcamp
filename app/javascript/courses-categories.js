f5e829a3de10d4c67a66c52c30b0f9add5dc381b
komagata <komagata@gmail.com>
2022-05-25 13:13:13 +0900

    プラクティス並び替えページのvuedraggableを外した
    vueに依存しないsortable.jsに変更。
    vue3のvue2互換モードでvuedraggableが動かないため。

--- app/javascript/courses-categories.js
+++ app/javascript/courses-categories.js
@@ -3,18 +3,20 @@ import Bootcamp from 'bootcamp'
 
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.querySelector('#js-category-sortable')
-  if (!element) { return null }
+  if (!element) {
+    return null
+  }
 
   Sortable.create(element, {
     handle: '.js-grab',
     onEnd(event) {
       const id = event.item.dataset.courses_category_id
       const params = { insert_at: event.newIndex + 1 }
+      const url = `/api/courses_categories/${id}/position.json`
 
-      Bootcamp.patch(`/api/courses_categories/${id}/position.json`, params)
-        .catch((error) => {
-          console.warn(error)
-        })
+      Bootcamp.patch(url, params).catch((error) => {
+        console.warn(error)
+      })
     }
   })
 })
