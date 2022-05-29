4936c614a4963c6e7a06ccefcc73c378e82f12ae
masanarih0ri <masanarihoridevelop@gmail.com>
2022-05-29 11:51:31 +0900

    decoratorのテストを追加する

--- test/decorators/user_decorator_test.rb
+++ test/decorators/user_decorator_test.rb
@@ -4,8 +4,10 @@ require 'test_helper'
 
 class UserDecoratorTest < ActiveSupport::TestCase
   def setup
+    ActiveDecorator::ViewContext.push(controller.view_context)
     @user1 = ActiveDecorator::Decorator.instance.decorate(users(:komagata))
     @user2 = ActiveDecorator::Decorator.instance.decorate(users(:hajime))
+    @user3 =  ActiveDecorator::Decorator.instance.decorate(users(:sotugyou))
   end
 
   test 'staff_roles' do
@@ -20,5 +22,10 @@ class UserDecoratorTest < ActiveSupport::TestCase
 
   test 'long_name' do
     assert_equal 'hajime (Hajime Tayo)', @user2.long_name
+  end
+
+  test 'enrollment_period' do
+    assert_equal '<span> 3070日目 </span><a href="/generations/5">5期生</a>', @user2.enrollment_period
+    assert_equal '<span> (2015年01月01日卒業 365日) </span><a href="/generations/5">5期生</a>', @user3.enrollment_period
   end
 end
