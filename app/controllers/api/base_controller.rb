bdea5680332e8715243506ed45cbce4fc9f8c20a
rosh-1228 <sho.watanabe1228@gmail.com>
2022-05-31 22:03:27 +0900

    ログイン時は全ユーザ情報を取得し、未ログイン時はアイコン表示に必要なユーザーだけを取得するように変更

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class API::BaseController < ApplicationController
-  before_action :require_login_for_api
+  before_action :require_login_for_api, if: :article_before_action?
 end
--- app/controllers/api/user_icon_urls_controller.rb
+++ app/controllers/api/user_icon_urls_controller.rb
@@ -2,6 +2,17 @@
 
 class API::UserIconUrlsController < API::BaseController
   def index
-    @users = User.with_attached_avatar
+    @users = set_user_icon
+  end
+
+  private
+
+  def article_before_action?
+    false
+  end
+
+  def set_user_icon
+    users = User.with_attached_avatar
+    logged_in? ? users : users.select {|test| !(test.avatar.blank?) }
   end
 end
