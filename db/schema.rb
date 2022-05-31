fed3718c7b87a439f130d0b454001ad808fbf378
emiko <31835314+siroemk@users.noreply.github.com>
2022-05-31 13:25:22 +0900

    コンフリクトを解消した

--- db/schema.rb
+++ db/schema.rb
@@ -391,18 +391,6 @@ ActiveRecord::Schema.define(version: 2022_05_31_063021) do
     t.index ["user_id"], name: "index_reactions_on_user_id"
   end
 
-  create_table "reference_books", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "price", null: false
-    t.string "page_url", null: false
-    t.bigint "practice_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "must_read", default: false, null: false
-    t.text "description"
-    t.index ["practice_id"], name: "index_reference_books_on_practice_id"
-  end
-
   create_table "regular_events", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
