2a952efb812846103721460e7dfe1b48e8fd9b8f
Norifumi Ogawa <n@ogaworks.com>
2022-06-01 15:06:44 +0900

    ブログ記事にサマリーを登録できるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_054838) do
+ActiveRecord::Schema.define(version: 2022_05_31_063021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2022_04_18_054838) do
     t.bigint "user_id"
     t.boolean "wip", default: false, null: false
     t.datetime "published_at"
+    t.text "summary"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
