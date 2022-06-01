4ff6145899bdd57a91b384a6f074284618f0c938
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-01 10:55:24 +0900

    base_controllerを参照しているuser_icon_urls_controller以外でエラーが出るため、base_controllerにてrefererがarticleの場合require_login_for_apiを実行しないように変更

--- app/controllers/api/user_icon_urls_controller.rb
+++ app/controllers/api/user_icon_urls_controller.rb
@@ -7,10 +7,6 @@ class API::UserIconUrlsController < API::BaseController
 
   private
 
-  def article_before_action?
-    true
-  end
-
   def set_user_icon
     users = User.with_attached_avatar
     logged_in? ? users : users.select {|user| !(user.avatar.blank?) }
