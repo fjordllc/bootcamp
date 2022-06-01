77be8f9359da6a2e7e845d431b7329b3d0782417
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-01 13:19:09 +0900

    URIからarticlesを探すのではなく、前コントローラ情報を取得するメソッドrequire_login_for_apiを実行するかしないかを判断するように変更

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class API::BaseController < ApplicationController
-  before_action :require_login_for_api, unless: proc {request.headers[:referer].include?('articles')}
+  before_action :require_login_for_api, unless: :is_before_articles_contoroller?
+
+  def is_before_articles_contoroller?
+    Rails.application.routes.recognize_path(request.referer)[:controller] == 'articles'
+  end
 end
