0311b29549837dab8e6c84e5284da33514c5b289
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-01 10:55:24 +0900

    base_controllerを参照しているuser_icon_urls_controller以外でエラーが出るため、base_controllerにてrefererがarticleの場合require_login_for_apiを実行しないように変更

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class API::BaseController < ApplicationController
-  before_action :require_login_for_api, unless: :article_before_action?
+  before_action :require_login_for_api, unless: proc {request.headers[:referer].include?('articles')}
 end
