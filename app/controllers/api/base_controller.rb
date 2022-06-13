0fa7df10d9be242a3f3fe2fb1346264d8bd041c8
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-13 23:32:18 +0900

    skip_before_actionを使い、user_icon_urls_controllerの時だけrequire_login_for_apiをメソッドを使わないように修正

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 class API::BaseController < ApplicationController
-  before_action :require_login_for_api, if: :not_before_user_icon_urls_controller?
-
-  def not_before_user_icon_urls_controller?
-    true
-  end
+  before_action :require_login_for_api
 end
--- app/controllers/api/user_icon_urls_controller.rb
+++ app/controllers/api/user_icon_urls_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class API::UserIconUrlsController < API::BaseController
+  skip_before_action :require_login_for_api
+
   def index
     @users = set_user_icon
   end
@@ -10,9 +12,5 @@ class API::UserIconUrlsController < API::BaseController
   def set_user_icon
     users = User.with_attached_avatar
     logged_in? ? users : users.select { |user| user.avatar.present? }
-  end
-
-  def not_before_user_icon_urls_controller?
-    false
   end
 end
