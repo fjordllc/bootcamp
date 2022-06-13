3c894c9e53dffd97f0369cb07b83bebcdb7b0d5e
Satoshi Haramura <satoshi.haramura@gmail.com>
2022-06-13 22:14:05 +0900

    参考書籍に紐付くプラクティスへのリンクのテストを追加

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -6,6 +6,7 @@ class BooksTest < ApplicationSystemTestCase
   test 'show listing books' do
     visit_with_auth '/books', 'komagata'
     assert_equal '参考書籍 | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
+    assert has_link?(practices(:practice1).title, href: practice_path(practices(:practice1)))
   end
 
   test 'create book' do
