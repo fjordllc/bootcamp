476d75098917569b3a345db908da5981917f32cd
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-14 17:27:05 +0900

    ステータスコードが200を確認する代わりに、user_iconを取得できるかできないかのテストに変更

--- test/integration/api/user_icon_urls_test.rb
+++ test/integration/api/user_icon_urls_test.rb
@@ -7,11 +7,14 @@ class API::UserIconUrlsTest < ActionDispatch::IntegrationTest
 
   test 'GET /api/user_icon_urls.json' do
     get api_user_icon_urls_path(format: :json)
-    assert_response :unauthorized
+    user_icons = @controller.index
+    assert user_icons.empty?
 
     token = create_token('kimura', 'testtest')
     get api_user_icon_urls_path(format: :json),
         headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :ok
+    user_icons = @controller.index
+    assert_not user_icons.empty?
   end
 end
