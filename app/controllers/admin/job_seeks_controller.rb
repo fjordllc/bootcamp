b5db4148056b4119257992a8e67c7202b686a57d
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-16 13:47:32 +0900

    admin/users_controllerからjob_seekのupdateメソッドを削除し、admin/job_seeks_controllerを作成、routes/adminにjob_seeksを追加

---
+++ app/controllers/admin/job_seeks_controller.rb
@@ -0,0 +1,21 @@
+class Admin::JobSeeksController < ApplicationController
+  before_action :set_user, only: %i[update]
+
+  def update
+    if @user.update(user_params)
+      redirect_to talk_url, notice: "#{@user.login_name}の就職活動中の情報を更新しました。"
+    else
+      redirect_to talk_url, alert: "#{@user.login_name}の就職活動中の情報を更新できませんでした。"
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(Talk.find(params[:id]).user_id)
+  end
+
+  def user_params
+    params.require(:user).permit(:job_seeking)
+  end
+end
