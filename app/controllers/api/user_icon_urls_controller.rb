96af209af300bfae527ae514d5b16cce14feb183
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-17 13:05:59 +0900

    set_user_iconを削除

--- app/controllers/api/user_icon_urls_controller.rb
+++ app/controllers/api/user_icon_urls_controller.rb
@@ -4,13 +4,6 @@ class API::UserIconUrlsController < API::BaseController
   skip_before_action :require_login_for_api
 
   def index
-    @users = set_user_icon
-  end
-
-  private
-
-  def set_user_icon
-    users = User.with_attached_avatar
-    logged_in? ? users : users.select { |user| user.avatar.present? }
+    @users = User.with_attached_avatar.select { |user| user.avatar.present? }
   end
 end
