860d6c75b6203facb9aa5aa9665628773a0b13aa
masanarih0ri <masanarihoridevelop@gmail.com>
2022-06-18 17:39:24 +0900

    定期イベントの子モデルにデータを登録できるようにする

--- app/models/regular_event.rb
+++ app/models/regular_event.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
 class RegularEvent < ApplicationRecord
+  FREQUENCY_LIST = [
+    ["毎週", 0],
+    ["第1", 1],
+    ["第2", 2],
+    ["第3", 3],
+    ["第4", 4],
+    ["第5", 5]
+  ].freeze
+
+  DAY_OF_THE_WEEK_LIST = [
+    ["日曜日", 0],
+    ["月曜日", 1],
+    ["火曜日", 2],
+    ["水曜日", 3],
+    ["木曜日", 4],
+    ["金曜日", 5],
+    ["土曜日", 6]
+  ].freeze
+
   include WithAvatar
   include Commentable
   include Footprintable
@@ -22,6 +41,7 @@ class RegularEvent < ApplicationRecord
   has_many :organizers, dependent: :destroy
   has_many :users, through: :organizers
   has_many :regular_event_repeat_rules, dependent: :destroy
+  accepts_nested_attributes_for :regular_event_repeat_rules, allow_destroy: true
 
   def organizers
     users.with_attached_avatar.order('organizers.created_at')
