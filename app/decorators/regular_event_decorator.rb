23cae42f15ab66c519bd97d770ce6b3f84ee9523
masanarih0ri <masanarihoridevelop@gmail.com>
2022-06-20 09:04:50 +0900

    次回の開催日と開催周期を表示する

--- app/decorators/regular_event_decorator.rb
+++ app/decorators/regular_event_decorator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+MAX_DAYS_OF_NEXT_EVENT_COUNT = 35
+
 module RegularEventDecorator
   def holding_cycles
     repeat_rules = regular_event_repeat_rules.pluck(:frequency, :day_of_the_week)
@@ -8,5 +10,38 @@ module RegularEventDecorator
       holding_day_of_the_week = RegularEvent::DAY_OF_THE_WEEK_LIST.find { |day_of_the_week| day_of_the_week[1] == repeat_rule[1] }[0]
       holding_frequency + holding_day_of_the_week
     end.join(',')
+  end
+
+  def next_event_date
+    "次回の開催日は #{l filtered_canditates_of_next_event_date.compact.min} です"
+  end
+
+  def filtered_canditates_of_next_event_date
+    repeat_rules = regular_event_repeat_rules.pluck(:frequency, :day_of_the_week)
+    today = Time.zone.today
+    nth_week_of_month = calc_week_of_month(today)
+    canditates_of_next_event_date = (1..MAX_DAYS_OF_NEXT_EVENT_COUNT).map { |n| today + n }
+
+    repeat_rules.map do |repeat_rule|
+      if (repeat_rule[0]).zero?
+        canditates_of_next_event_date.find { |date| date.wday == repeat_rule[1] }
+      else
+        canditates_of_next_event_date.find { |date| date.wday == repeat_rule[1] && calc_week_of_month(date) == nth_week_of_month }
+      end
+    end
+  end
+
+  def calc_week_of_month(date)
+    first_week_of_this_month = (date - (date.day - 1)).cweek
+    this_week = date.cweek
+
+    # 年末年始の対応
+    if this_week < first_week_of_this_month
+      return calc_week_of_month(date - 7) + 1 if date.month == 12
+
+      return this_week
+    end
+
+    this_week - first_week_of_this_month + 1
   end
 end
--- test/decorators/regular_event_decorator_test.rb
+++ test/decorators/regular_event_decorator_test.rb
@@ -3,11 +3,18 @@
 require 'test_helper'
 
 class RegularEventDecoratorTest < ActiveSupport::TestCase
+  include ActionView::TestCase::Behavior
+
   def setup
+    ActiveDecorator::ViewContext.push(controller.view_context)
     @regular_event = ActiveDecorator::Decorator.instance.decorate(regular_events(:regular_event1))
   end
 
   test 'holding_cycles' do
     assert_equal '毎週日曜日', @regular_event.holding_cycles
+  end
+
+  test 'next_event_date' do
+    assert_equal "次回の開催日は #{l @regular_event.filtered_canditates_of_next_event_date.compact.min} です" , @regular_event.next_event_date
   end
 end
