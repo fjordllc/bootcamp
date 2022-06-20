ae367f4ec64721662e6e1ac4e69ea9a18fa91377
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-06-20 19:57:18 +0900

    vueファイルを作成

---
+++ app/javascript/book.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+.col-xxl-3.col-xl-4.col-lg-4.col-md-6.col-xs-12
+  .card-books-item
+    .a-card
+      .card-books-item__inner
+        .card-books-item__start
+          a.card-books-item__cover-container(:href='book.page_url')
+            img.card-books-item__image(
+              :title='book.title',
+              :alt='book.title',
+              :src='book.cover_url'
+            )
+        .card-books-item__end
+          h2.card-books-item__title
+            a.card-books-item__title-link(
+              :href='book.page_url',
+              target='_blank',
+              rel='noopener'
+              )
+              span.card__title-label
+                | {{ book.title }}
+            p.card-books-item__price
+              | {{ book.price }}円（税込）
+              .card-books-item__description(v-if='book.description')
+                .a-short-text
+                  | {{ book.description }}
+      footer.card-footer.is-only-mentor(v-if='currentUser.admin || currentUser.mentor')
+        .card-main-actions
+          ul.card-main-actions__items
+            li.card-main-actions__item
+              a(
+                class='card-main-actions__action a-button is-sm is-secondary is-block',
+                href='#'
+              )
+                | 編集
+</template>
+<script>
+export default {
+  props: {
+    book: { type: Object, required: true },
+    currentUser: { type: Object, required: true }
+  }
+}
+</script>
---
+++ app/javascript/books.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+.books
+  .books__items
+    .row(v-if='books === null')
+      .empty
+        .fa-solid.fa-spinner.fa-pulse
+        |
+        | ロード中
+    .row(v-else-if='books.length !== 0')
+      book(
+        v-for='book in books',
+        :key='book.id',
+        :book='book',
+        :currentUser='currentUser'
+      )
+    .row(v-else)
+      .o-empty-message
+        .o-empty-message__icon
+          i.fa-regular.fa-sad-tear
+        p.o-empty-message__text
+          | 登録されている本はありません
+</template>
+<script>
+import Book from 'book.vue'
+
+export default {
+  components: {
+    book: Book
+  },
+  data() {
+    return {
+      books: null,
+      currentUser: null,
+    }
+  },
+  created() {
+    window.onpopstate = function () {
+      location.replace(location.href)
+    }
+    this.getBooks()
+  },
+  methods: {
+    token() {
+      const meta = document.querySelector('meta[name="csrf-token"]')
+      return meta ? meta.getAttribute('content') : ''
+    },
+    getBooks() {
+      fetch('/api/books', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': this.token()
+        },
+        credentials: 'same-origin',
+        redirect: 'manual'
+      })
+          .then((response) => {
+            return response.json()
+          })
+          .then((json) => {
+            this.books = json.books
+            this.currentUser = json.currentUser
+          })
+          .catch((error) => {
+            console.warn(error)
+          })
+    },
+  }
+}
+</script>
