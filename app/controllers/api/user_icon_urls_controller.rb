adb69b1ad2afe3e8c796cce63997e236400f92cf
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-23 13:30:43 +0900

    sqlでユーザー情報を取得するように修正

--- app/controllers/api/user_icon_urls_controller.rb
+++ app/controllers/api/user_icon_urls_controller.rb
@@ -4,6 +4,6 @@ class API::UserIconUrlsController < API::BaseController
   skip_before_action :require_login_for_api
 
   def index
-    @users = User.with_attached_avatar.select { |user| user.avatar.present? }
+    @users = User.with_attached_avatar.where(id: ActiveStorage::Attachment.select(:record_id).where(record_type: 'User'))
   end
 end
