ebfa029a96221f66c83fe9711f48f345764d492f
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-23 13:56:45 +0900

    不要なbinding.pryを削除

--- app/controllers/api/user_icon_urls_controller.rb
+++ app/controllers/api/user_icon_urls_controller.rb
@@ -4,7 +4,6 @@ class API::UserIconUrlsController < API::BaseController
   skip_before_action :require_login_for_api
 
   def index
-    #binding.pry
     @users = User.with_attached_avatar.select(:id, :login_name).where(id: ActiveStorage::Attachment.select(:record_id).where(record_type: 'User'))
   end
 end
