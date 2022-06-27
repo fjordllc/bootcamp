94e47b95078890a982361193056a2e9c40db0b19
komagata <komagata@gmail.com>
2022-06-27 11:13:55 +0900

    ダッシュボードの悲しいマークの人一覧の処理速度を改善

--- config/initializers/newspaer.rb
+++ config/initializers/newspaer.rb
@@ -1,3 +1,8 @@
 Rails.configuration.to_prepare do
   Newspaper.subscribe(:event_create, EventOrganizerWatcher.new)
+
+  sad_streak_updater = SadStreakUpdater.new
+  Newspaper.subscribe(:report_create, sad_streak_updater)
+  Newspaper.subscribe(:report_update, sad_streak_updater)
+  Newspaper.subscribe(:report_destroy, sad_streak_updater)
 end
