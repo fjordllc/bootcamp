8b7740ad766708e82c7401a4d4b99f7d96b3a491
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-28 17:51:36 +0900

    Choices-jsに置き換えた

--- app/javascript/book-select.js
+++ app/javascript/book-select.js
@@ -1,16 +1,35 @@
 import Choices from 'choices.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const element = document.getElementById('js-book-select')
-  if (element) {
-    return new Choices('#js-book-select', {
+  const bookSelectCount = document.querySelectorAll('#js-book-select').length
+  const elements = document.querySelectorAll('#js-book-select')
+  for (let i = 0; i < bookSelectCount; i++) {
+    const choicesElements = new Choices(elements[i], {
       removeItemButton: true,
       allowHTML: true,
-      searchResultLimit: 10,
+      searchResultLimit: 20,
       searchPlaceholderValue: '検索ワード',
       noResultsText: '一致する情報は見つかりません',
-      itemSelectText: '本を選んでください',
+      itemSelectText: '選択',
       shouldSort: false
     })
+    if (bookSelectCount === i) {
+      return choicesElements
+    }
   }
+  $('.reference-books-form__add').on('cocoon:after-insert', () => {
+    const elements = document.querySelectorAll('#js-book-select')
+    const element = elements[elements.length- 1]
+    if (element) {
+      return new Choices(element, {
+        removeItemButton: true,
+        allowHTML: true,
+        searchResultLimit: 20,
+        searchPlaceholderValue: '検索ワード',
+        noResultsText: '一致する情報は見つかりません',
+        itemSelectText: '選択',
+        shouldSort: false
+      })
+    }
+  })
 })
