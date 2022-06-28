af38dec209b81b2f85f5558b8fe466074a9d5530
rosh-1228 <sho.watanabe1228@gmail.com>
2022-06-28 17:50:33 +0900

    コンフリクト解消、vueファイルは不要なので削除

--- app/javascript/book-select.js
+++ app/javascript/book-select.js
@@ -1,50 +1,16 @@
-import Vue from 'vue'
-import BookSelect from './book-select.vue'
+import Choices from 'choices.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const selector = '.js-book-select'
-  const hiddenSelector = '.reference-books-form-item input[name$="[book_id]"]'
-  const hiddenSelectorAll = document.querySelectorAll(hiddenSelector)
-  const bookSelectAll = document.querySelectorAll(selector)
-  if (bookSelectAll) {
-    for (let i = 0; i < bookSelectAll.length; i++) {
-      const bookSelected = bookSelectAll[i].getAttribute('data-selected-book')
-      const books = bookSelectAll[i].getAttribute('data-books')
-      const hiddenNameParam = hiddenSelectorAll[i].getAttribute('name')
-      new Vue({
-        render: (h) =>
-          h(BookSelect, {
-            props: {
-              bookSelected: bookSelected,
-              books: books,
-              hiddenNameParam: hiddenNameParam
-            }
-          })
-      }).$mount(selector)
-    }
+  const element = document.getElementById('js-book-select')
+  if (element) {
+    return new Choices('#js-book-select', {
+      removeItemButton: true,
+      allowHTML: true,
+      searchResultLimit: 10,
+      searchPlaceholderValue: '検索ワード',
+      noResultsText: '一致する情報は見つかりません',
+      itemSelectText: '本を選んでください',
+      shouldSort: false
+    })
   }
-  $('.reference-books-form__add').on('cocoon:after-insert', () => {
-    const selector = '.js-book-select'
-    const hiddenSelector = '.reference-books-form-item input[name$="[book_id]"]'
-    const hiddenSelectorAll = document.querySelectorAll(hiddenSelector)
-    const bookSelectAll = document.querySelectorAll(selector)
-    const hiddenSelectLast = hiddenSelectorAll[hiddenSelectorAll.length - 1]
-    const bookSelectLast = bookSelectAll[bookSelectAll.length - 1]
-    if (bookSelectLast) {
-      const bookSelected = bookSelectLast.getAttribute('data-selected-book')
-      const books = bookSelectLast.getAttribute('data-books')
-      const hiddenNameParam = hiddenSelectLast.getAttribute('name')
-      console.log(hiddenNameParam)
-      new Vue({
-        render: (h) =>
-          h(BookSelect, {
-            props: {
-              bookSelected: bookSelected,
-              books: books,
-              hiddenNameParam: hiddenNameParam
-            }
-          })
-      }).$mount(selector)
-    }
-  })
 })
