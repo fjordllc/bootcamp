51935c5b7e258d7b7a5b7e9fa14f21c609aa34fd
kazumi <kazumi121589@yahoo.co.jp>
2022-06-30 12:32:06 +0900

    既存のテスト'products order'を新しいテスト'products order on all tab'で上書きした

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -244,19 +244,18 @@ class ProductsTest < ApplicationSystemTestCase
     assert_no_text "kensyuさんが「#{practices(:practice3).title}」の提出物を提出しました。"
   end
 
-  test 'products order' do
-    # id順で並べたときの最初と最後の提出物を、作成日順で見たときに最新と最古になるように入れ替える
+  test 'products order on all tab' do
     Product.update_all(created_at: 1.day.ago, published_at: 1.day.ago) # rubocop:disable Rails/SkipsModelValidations
-    # 最古の提出物を画面上で判定するため、提出物を1ページ内に収める
+    # 最新と最古の提出物を画面上で判定するため、提出物を1ページ内に収める
     Product.limit(Product.count - Product.default_per_page).delete_all
     newest_product = Product.reorder(:id).first
-    newest_product.update(created_at: Time.current)
+    newest_product.update(published_at: Time.current)
     oldest_product = Product.reorder(:id).last
-    oldest_product.update(created_at: 2.days.ago)
+    oldest_product.update(published_at: 2.days.ago)
 
     visit_with_auth '/products', 'komagata'
 
-    # 作成日の降順で並んでいることを検証する
+    # 提出日の新しい順で並んでいることを検証する
     titles = all('.card-list-item-title__title').map { |t| t.text.gsub('★', '') }
     names = all('.card-list-item-meta .a-user-name').map(&:text)
     assert_equal "#{newest_product.practice.title}の提出物", titles.first
@@ -517,25 +516,5 @@ class ProductsTest < ApplicationSystemTestCase
     click_button '提出する'
 
     assert product.reload.published_at = product_published_at
-  end
-
-  test 'products order on all tab' do
-    Product.update_all(created_at: 1.day.ago, published_at: 1.day.ago) # rubocop:disable Rails/SkipsModelValidations
-    # 最新の提出物を画面上で判定するため、提出物を1ページ内に収める
-    Product.limit(Product.count - Product.default_per_page).delete_all
-    newest_product = Product.reorder(:id).first
-    newest_product.update(published_at: Time.current)
-    oldest_product = Product.reorder(:id).last
-    oldest_product.update(published_at: 2.days.ago)
-
-    visit_with_auth '/products', 'komagata'
-
-    # 提出日の新しい順で並んでいることを検証する
-    titles = all('.card-list-item-title__title').map { |t| t.text.gsub('★', '') }
-    names = all('.card-list-item-meta .a-user-name').map(&:text)
-    assert_equal "#{newest_product.practice.title}の提出物", titles.first
-    assert_equal newest_product.user.login_name, names.first
-    assert_equal "#{oldest_product.practice.title}の提出物", titles.last
-    assert_equal oldest_product.user.login_name, names.last
   end
 end
