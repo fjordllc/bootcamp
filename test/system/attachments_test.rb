4519b604ee9777bef2494eeb10e5343c6698b594
tomonari <zcsxcacg@i.softbank.jp>
2022-07-06 12:42:12 +0900

    コメントの文言の修正

--- test/system/attachments_test.rb
+++ test/system/attachments_test.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-# Pull Request #4182(https://github.com/fjordllc/bootcamp/pull/4182) で Rails 7 への移行完了後、
-# ActiveStorage::FixtureSet.blob を使うように変更する
-
+# Pull Request #4182(https://github.com/fjordllc/bootcamp/pull/4182) で Rails 7 への移行完了後に削除する
 require 'application_system_test_case'
 
 class AttachmentsTest < ApplicationSystemTestCase
