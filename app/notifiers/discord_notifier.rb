12d7c28d75378b18ee969a51e7b18bcae79f46de
komagata <komagata@gmail.com>
2022-07-08 00:58:16 +0900

    休会機能を追加
    管理者が本番で動作確認するためにまだリンクは用意しない。

--- app/notifiers/discord_notifier.rb
+++ app/notifiers/discord_notifier.rb
@@ -14,4 +14,15 @@ class DiscordNotifier < ApplicationNotifier
       webhook_url: webhook_url
     )
   end
+
+  def hibernated(params = {})
+    params.merge!(@params)
+    webhook_url = params[:webhook_url] || Rails.application.secrets[:webhook][:admin]
+
+    notification(
+      body: "#{params[:sender].login_name}さんが休会しました。",
+      name: 'ピヨルド',
+      webhook_url: webhook_url
+    )
+  end
 end
