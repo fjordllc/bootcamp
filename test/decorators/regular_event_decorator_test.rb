4914471d2643dd477adc02d2dd55f08a04f03bf0
masanarih0ri <masanarihoridevelop@gmail.com>
2022-07-11 22:58:19 +0900

    テストを修正

--- test/decorators/regular_event_decorator_test.rb
+++ test/decorators/regular_event_decorator_test.rb
@@ -16,21 +16,27 @@ class RegularEventDecoratorTest < ActiveSupport::TestCase
     assert_equal '毎週日曜日', @regular_event.holding_cycles
   end
 
-  test 'next_event_date' do
+  test 'next_holding_date' do
     travel_to Time.zone.local(2022, 6, 1, 0, 0, 0) do
-      assert_equal '次回の開催日は 2022年06月05日 です', @regular_event.next_event_date
+      assert_equal '次回の開催日は 2022年06月05日 です', @regular_event.next_holding_date
     end
 
     travel_to Time.zone.local(2022, 6, 5, 0, 0, 0) do
-      assert_equal '本日開催', @regular_event.next_event_date
+      assert_equal '本日開催', @regular_event.next_holding_date
     end
 
     travel_to Time.zone.local(2022, 6, 5, 15, 30, 0) do
-      assert_equal '次回の開催日は 2022年06月12日 です', @regular_event.next_event_date
+      assert_equal '次回の開催日は 2022年06月12日 です', @regular_event.next_holding_date
     end
 
     travel_to Time.zone.local(2022, 6, 1, 0, 0, 0) do
-      assert_equal '開催終了', @finished_regular_event.next_event_date
+      assert_equal '開催終了', @finished_regular_event.next_holding_date
+    end
+  end
+
+  test 'next_event_date' do
+    travel_to Time.zone.local(2022, 6, 1, 0, 0, 0) do
+      assert_equal Date.new(2022, 6, 5), @finished_regular_event.next_event_date
     end
   end
 
