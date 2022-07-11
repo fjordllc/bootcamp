203d6b9e43442c711bde582b5c0e8882a261627a
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-07-11 16:08:23 +0900

    book.vueにlintを通す

--- app/javascript/components/book.vue
+++ app/javascript/components/book.vue
@@ -57,14 +57,14 @@ export default {
   },
   methods: {
     simpleFormat: function (str) {
-      str = str.replace(/\r\n?/, "\n");
-      str = $.trim(str);
+      str = str.replace(/\r\n?/, '\n')
+      str = $.trim(str)
       if (str.length > 0) {
-        str = str.replace(/\n\n+/g, '</p><p>');
-        str = str.replace(/\n/g, '<br />');
-        str = '<p>' + str + '</p>';
+        str = str.replace(/\n\n+/g, '</p><p>')
+        str = str.replace(/\n/g, '<br />')
+        str = '<p>' + str + '</p>'
       }
-      return str;
+      return str
     }
   }
 }
