c556f1fa120f57f30505dd2b9321fa385682a8d7
ai-24 <ai.morikawa2410sj@gmail.com>
2022-07-12 16:12:16 +0900

    提出物一覧（未アサイン）にページ内リンクを設置した

---
+++ app/javascript/elapsed_days.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+ol
+  .elapsed-days__list(v-for='product_n_days_passed in productsGroupedByElapsedDays')
+    li(
+      v-if='product_n_days_passed.elapsed_days === 7',
+      @click='transition(product_n_days_passed.elapsed_days)'
+    )
+      | {{ product_n_days_passed.elapsed_days }}日以上経過
+      | ({{ countProductsGroupedBy(product_n_days_passed) }})
+    li(
+      v-else-if='product_n_days_passed.elapsed_days >= 1',
+      @click='transition(product_n_days_passed.elapsed_days)'
+    )
+      | {{ product_n_days_passed.elapsed_days }}日経過
+      | ({{ countProductsGroupedBy(product_n_days_passed) }})
+
+</template>
+
+<script>
+export default {
+  props: {
+    productsGroupedByElapsedDays: { type: Array, required: true },
+    countProductsGroupedBy: { type: Function, required: true }
+  },
+  methods: {
+    transition(elapsedDays) {
+      this.$emit('transition', elapsedDays)
+    }
+  }
+}
+</script>
