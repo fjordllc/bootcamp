ab7543440f40921226de9944a1ac287f2951da24
tomonari <zcsxcacg@i.softbank.jp>
2022-07-14 14:52:11 +0900

    logo_urlに例外処理を追加

--- test/system/attachments_test.rb
+++ test/system/attachments_test.rb
@@ -7,11 +7,5 @@ class AttachmentsTest < ApplicationSystemTestCase
   test 'attachment user avatar' do
     visit_with_auth "/users/#{users(:komagata).id}", 'komagata'
     assert find('img.user-profile__user-icon-image')['src'].include?('komagata.png')
-    assert find('img.user-profile__company-logo')['src'].include?('1.png')
-  end
-
-  test 'attachment company logo' do
-    visit_with_auth "/companies/#{companies(:company2).id}", 'kimura'
-    assert find('img.company-profile__logo-image')['src'].include?('2.png')
   end
 end
