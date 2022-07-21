88c596f70a29823e673e3cf90e01e21d9418535a
ai-24 <ai.morikawa2410sj@gmail.com>
2022-07-21 23:16:40 +0900

    定期イベントのカテゴリーの実装方法を変更したことにより定期イベントの新規作成ができなくなった問題を修正

--- app/models/regular_event.rb
+++ app/models/regular_event.rb
@@ -32,7 +32,7 @@ class RegularEvent < ApplicationRecord # rubocop:disable Metrics/ClassLength
     question: 2,
     meeting: 3,
     others: 4
-  }
+  }, _prefix: true
 
   validates :title, presence: true
   validates :user_ids, presence: true
