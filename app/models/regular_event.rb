36f9bf3d732ec862210faaf5592452cbf2332fa9
masanarih0ri <masanarihoridevelop@gmail.com>
2022-07-26 21:29:43 +0900

    rubocop修正

--- app/models/regular_event.rb
+++ app/models/regular_event.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RegularEvent < ApplicationRecord # rubocop:disable Metrics/ClassLength
+class RegularEvent < ApplicationRecord
   DAYS_OF_THE_WEEK_COUNT = 7
 
   FREQUENCY_LIST = [
