e76018e60998dfe69abd8a2ccbae73853b1c9eb3
daiki0381 <daiki03810218@gmail.com>
2022-08-05 15:26:17 +0900

    競合の解消

---
+++ app/javascript/loading-users-page-company-placeholder.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+.user-group
+  header.user-group__header
+    h2.group-company-name
+      .group-company-name__link
+        span.group-company-name__icon
+          .group-company-name__icon-image.a-placeholder
+        span.group-company-name__name
+          span.group-company-name__label.a-placeholder
+          span.group-company-name__label-option.a-placeholder
+  .a-user-icons
+    .a-user-icons__items
+      loadingUsersPageCompanyUserIconPlaceholder(v-for='num in userIconCount', :key='num')
+</template>
+
+<script>
+import LoadingUsersPageCompanyUserIconPlaceholder from 'loading-users-page-company-user-icon-placeholder.vue'
+
+export default {
+  components: {
+    loadingUsersPageCompanyUserIconPlaceholder: LoadingUsersPageCompanyUserIconPlaceholder
+  },
+  data() {
+    return {
+      userIconCount: 16
+    }
+  }
+}
+</script>
---
+++ app/javascript/loading-users-page-company-user-icon-placeholder.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+.a-user-icons__item
+  .a-user-icons__item-link
+    .a-user-icons__item-icon.a-user-icon.a-placeholder
+</template>
