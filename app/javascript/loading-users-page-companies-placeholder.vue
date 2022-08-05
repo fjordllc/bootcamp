fe2a93f7e837b7b224c8ba48011b57a45d72a2a5
daiki0381 <daiki03810218@gmail.com>
2022-08-05 15:26:17 +0900

    競合の解消

---
+++ app/javascript/loading-users-page-companies-placeholder.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+div
+  LoadingUsersPageCompanyPlaceholder(v-for='num in companyCount', :key='num')
+</template>
+
+<script>
+import LoadingUsersPageCompanyPlaceholder from 'loading-users-page-company-placeholder.vue'
+
+export default {
+  components: {
+    LoadingUsersPageCompanyPlaceholder: LoadingUsersPageCompanyPlaceholder
+  },
+  data() {
+    return {
+      companyCount: 9
+    }
+  }
+}
+</script>
