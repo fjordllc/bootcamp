8f2e8e4fbff42ed221514e696e6b134a3354437b
daiki0381 <daiki03810218@gmail.com>
2022-08-06 20:57:05 +0900

    指摘事項の修正

--- test/system/courses_test.rb
+++ test/system/courses_test.rb
@@ -30,30 +30,23 @@ class CoursesTest < ApplicationSystemTestCase
   end
 
   test 'show published courses' do
+    visit_with_auth '/courses', 'hajime'
+    assert_no_text courses(:course1).title
     visit_with_auth "/admin/courses/#{courses(:course1).id}/edit", 'komagata'
     within 'form[name=course]' do
       find(:css, '#checkbox-published-course').set(true)
       click_button '内容を保存'
     end
-    visit_with_auth '/courses', 'mentormentaro'
+    visit_with_auth '/courses', 'hajime'
     assert_text courses(:course1).title
   end
 
   test 'mentors can see closed courses' do
     visit_with_auth "/admin/courses/#{courses(:course1).id}/edit", 'komagata'
     within 'form[name=course]' do
-      assert_equal false, find(:css, '#checkbox-published-course').checked?
+      assert_not find(:css, '#checkbox-published-course').checked?
     end
     visit_with_auth '/courses', 'mentormentaro'
     assert_text courses(:course1).title
-  end
-
-  test 'general users cannot see closed courses' do
-    visit_with_auth "/admin/courses/#{courses(:course1).id}/edit", 'komagata'
-    within 'form[name=course]' do
-      assert_equal false, find(:css, '#checkbox-published-course').checked?
-    end
-    visit_with_auth '/courses', 'hajime'
-    assert_no_text courses(:course1).title
   end
 end
