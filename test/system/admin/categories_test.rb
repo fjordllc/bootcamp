866b742c67f604c01c2567d8ca03bf844fa4f91b
yocajii <yocajii@stone.sakura.ne.jp>
2022-08-09 10:26:27 +0900

    カテゴリー詳細ページのテストを追加した

--- test/system/admin/categories_test.rb
+++ test/system/admin/categories_test.rb
@@ -8,6 +8,12 @@ class Admin::CategoriesTest < ApplicationSystemTestCase
     assert_equal '管理ページ | FBC', title
   end
 
+  test 'show category page' do
+    visit_with_auth admin_category_path(categories(:category2)), 'komagata'
+    assert_equal 'Mac OS X | FBC', title
+    first('.admin-table__item-value').assert_text 'OS X Mountain Lionをクリーンインストールする'
+  end
+
   test 'create category' do
     visit_with_auth '/admin/categories/new', 'komagata'
     within 'form[name=category]' do
