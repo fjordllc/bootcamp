2084dde2af18543c21c424dcf1fa5a3ae7e9c9c4
Nabegon <watami03@gmail.com>
2022-08-10 17:44:11 +0200

    テストに追記をした

--- test/system/notification/answers_test.rb
+++ test/system/notification/answers_test.rb
@@ -5,23 +5,11 @@ require 'application_system_test_case'
 class Notification::AnswersTest < ApplicationSystemTestCase
   setup do
     @notice_text = 'komagataさんから回答がありました。'
+    @delivery_mode = AbstractNotifier.delivery_mode
+    AbstractNotifier.delivery_mode = :normal
   end
 
-  test "receive a notification when I got my question's answer" do
-    visit_with_auth "/questions/#{questions(:question2).id}", 'komagata'
-    within('.thread-comment-form__form') do
-      fill_in('answer[description]', with: 'reduceも使ってみては？')
-    end
-    click_button 'コメントする'
-    assert_text '回答を投稿しました！'
-
-    visit_with_auth '/notifications', 'sotugyou'
-
-    within first('.card-list-item.is-unread') do
-      assert_text @notice_text
-    end
-
-    visit_with_auth '/', 'komagata'
-    refute_text @notice_text
+  teardown do
+    AbstractNotifier.delivery_mode = @delivery_mode
   end
 end
