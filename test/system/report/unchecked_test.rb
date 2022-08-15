f952521dc2c42309762658c65d6653776dc3b0c7
keiz1213 <fukuoka00tsc@gmail.com>
2022-08-15 20:36:03 +0900

    テスト追加

--- test/system/report/unchecked_test.rb
+++ test/system/report/unchecked_test.rb
@@ -8,6 +8,13 @@ class Report::UncheckedTest < ApplicationSystemTestCase
     assert_equal '未チェックの日報 | FBC', title
   end
 
+  test 'show listing unchecked reports when there is no unchecked reports' do
+    Report.unchecked.destroy_all
+
+    visit_with_auth '/reports/unchecked', 'komagata'
+    assert_text '未チェックの日報はありません'
+  end
+
   test 'non-staff user can not see listing unchecked reports' do
     visit_with_auth '/reports/unchecked', 'hatsuno'
     assert_text '管理者・アドバイザー・メンターとしてログインしてください'
--- test/system/user/reports_test.rb
+++ test/system/user/reports_test.rb
@@ -8,6 +8,11 @@ class User::ReportsTest < ApplicationSystemTestCase
     assert_equal 'hatsuno 日報 | FBC', title
   end
 
+  test 'show listing reports when there is no report' do
+    visit_with_auth "/users/#{users(:nippounashi).id}/reports", 'hatsuno'
+    assert_text "日報はまだありません。"
+  end
+
   test 'cannot access other users download reports' do
     visit_with_auth "/users/#{users(:hatsuno).id}/reports.md", 'kimura'
     assert_text '自分以外の日報はダウンロードすることができません'
