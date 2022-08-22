2c5b945138fa39f79cf60b713c29d631bfa91226
Nabegon <watami03@gmail.com>
2022-08-22 23:26:55 +0200

    lintの修正

--- app/controllers/api/footprints_controller.rb
+++ app/controllers/api/footprints_controller.rb
@@ -6,11 +6,11 @@ class API::FootprintsController < API::BaseController
       footprintable.footprints.create_or_find_by(user: current_user) if footprintable.user != current_user
       @footprints = footprintable.footprints.with_avatar.order(created_at: :desc)
       @footprint_total_count = @footprints.size
-   else
+    else
       head :bad_request
     end
   end
-  
+
   private
 
   def footprintable
--- app/controllers/regular_events_controller.rb
+++ app/controllers/regular_events_controller.rb
@@ -6,8 +6,7 @@ class RegularEventsController < ApplicationController
 
   def index; end
 
-  def show;
-  end
+  def show; end
 
   def new
     @regular_event = RegularEvent.new
