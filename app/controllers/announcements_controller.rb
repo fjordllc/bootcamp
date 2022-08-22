6b86761507bff5eb301f1c6c9d6b6fdfdf970663
Nabegon <watami03@gmail.com>
2022-08-22 22:59:12 +0200

    コンフリクト解消

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -3,22 +3,11 @@
 class AnnouncementsController < ApplicationController
   before_action :require_login
   before_action :set_announcement, only: %i[show edit update destroy]
-<<<<<<< HEAD
-<<<<<<< HEAD
   before_action :rewrite_announcement, only: %i[update]
-  before_action :set_footprints, only: %i[show]
-=======
->>>>>>> 足あとのVueコンポーネントとAPIを作成
 
-=======
-  before_action :rewrite_announcement, only: %i[update]
-  
->>>>>>> コンフリクト解消
   def index; end
 
-  def show
-    footprint!
-  end
+  def show; end
 
   def new
     @announcement = Announcement.new(target: 'students')
