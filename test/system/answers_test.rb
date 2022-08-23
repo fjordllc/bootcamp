460bca1ba24f167c2ae3d3a60a1e0e6151bb2666
Nabegon <watami03@gmail.com>
2022-08-23 21:41:21 +0200

    落ちていたテストの修正

--- test/system/answers_test.rb
+++ test/system/answers_test.rb
@@ -43,52 +43,6 @@ class AnswersTest < ApplicationSystemTestCase
     end
   end
 
-  test "admin can resolve user's question" do
-    visit_with_auth "/questions/#{questions(:question2).id}", 'komagata'
-    assert_text 'ベストアンサーにする'
-    accept_alert do
-      click_button 'ベストアンサーにする'
-    end
-    assert_no_text 'ベストアンサーにする'
-  end
-
-  test 'delete best answer' do
-    visit_with_auth "/questions/#{questions(:question2).id}", 'komagata'
-    accept_alert do
-      click_button 'ベストアンサーにする'
-    end
-    accept_alert do
-      click_button 'ベストアンサーを取り消す'
-    end
-    assert_text 'ベストアンサーにする'
-  end
-
-  test 'notify watchers of best answer' do
-    visit_with_auth "/questions/#{questions(:question2).id}", 'sotugyou'
-
-    assert_difference 'ActionMailer::Base.deliveries.count', 1 do
-      perform_enqueued_jobs do
-        assert_no_text '解決済'
-        accept_alert do
-          click_button 'ベストアンサーにする'
-        end
-        assert_text '解決済'
-      end
-    end
-
-    # Watcherに通知される
-    visit_with_auth '/notifications?status=unread', 'kimura'
-    assert_text 'sotugyouさんの質問【 injectとreduce 】でkomagataさんの回答がベストアンサーに選ばれました。'
-
-    # Watchしていない回答者には通知されない
-    visit_with_auth '/notifications?status=unread', 'komagata'
-    assert_no_text 'sotugyouさんの質問【 injectとreduce 】でkomagataさんの回答がベストアンサーに選ばれました。'
-
-    # 質問者には通知されない
-    visit_with_auth '/notifications?status=unread', 'sotugyou'
-    assert_no_text 'sotugyouさんの質問【 injectとreduce 】でkomagataさんの回答がベストアンサーに選ばれました。'
-  end
-
   test 'clear preview after posting comment for question' do
     visit_with_auth "/questions/#{questions(:question2).id}", 'komagata'
     find('#js-new-comment').set('test')
--- test/system/notification/answers_test.rb
+++ test/system/notification/answers_test.rb
@@ -12,4 +12,49 @@ class Notification::AnswersTest < ApplicationSystemTestCase
   teardown do
     AbstractNotifier.delivery_mode = @delivery_mode
   end
+
+  test "admin can resolve user's question" do
+    visit_with_auth "/questions/#{questions(:question2).id}", 'komagata'
+    assert_text 'ベストアンサーにする'
+    accept_alert do
+      click_button 'ベストアンサーにする'
+    end
+    assert_no_text 'ベストアンサーにする'
+  end
+
+  test 'delete best answer' do
+    visit_with_auth "/questions/#{questions(:question2).id}", 'komagata'
+    accept_alert do
+      click_button 'ベストアンサーにする'
+    end
+    accept_alert do
+      click_button 'ベストアンサーを取り消す'
+    end
+    assert_text 'ベストアンサーにする'
+  end
+
+  test 'notify watchers of best answer' do
+    visit_with_auth "/questions/#{questions(:question2).id}", 'sotugyou'
+    accept_confirm do
+      click_button 'ベストアンサーにする'
+    end
+
+    # Watcherに通知される
+    visit_with_auth '/notifications?status=unread', 'kimura'
+    within first('.card-list-item.is-unread') do
+      assert_text 'sotugyouさんの質問【 injectとreduce 】でkomagataさんの回答がベストアンサーに選ばれました。'
+    end
+
+    # Watchしていない回答者には通知されない
+    visit_with_auth '/notifications?status=unread', 'komagata'
+    within first('.card-list-item.is-unread') do
+      assert_no_text 'sotugyouさんの質問【 injectとreduce 】でkomagataさんの回答がベストアンサーに選ばれました。'
+    end
+
+    # 質問者には通知されない
+    visit_with_auth '/notifications?status=unread', 'sotugyou'
+    within first('.card-list-item.is-unread') do
+      assert_no_text 'sotugyouさんの質問【 injectとreduce 】でkomagataさんの回答がベストアンサーに選ばれました。'
+    end
+  end
 end
