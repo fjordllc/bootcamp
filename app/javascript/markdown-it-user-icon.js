3d614bc2e00479d697580b8fc2c77a80b889b2d0
lan <le.hoang.lan@actindi.net>
2022-08-25 14:45:22 +0900

    アイコン付きメンションで、アイコンが付かないユーザーがいる

--- app/javascript/markdown-it-user-icon.js
+++ app/javascript/markdown-it-user-icon.js
@@ -1,7 +1,7 @@
 import MarkdownItRegexp from 'markdown-it-regexp'
 
 export default MarkdownItRegexp(
-  /:@(?!mentor)([a-zA-Z0-9_-]+):/,
+  /:@(?!mentor:)([a-zA-Z0-9_-]+):/,
 
   (match) => {
     return `<a href="/users/${match[1]}" class="a-user-emoji-link"><img class="js-user-icon a-user-emoji" data-user="${match[1]}" width="40" height="40"></a>`
