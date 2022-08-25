e22a9533fe3ccd6ffe980150e41f503fce5ee02d
yuma-matsui <oshino.iiyo@gmail.com>
2022-08-25 19:26:28 +0900

    テストの変更#文言を変更してテストが通るようにした

--- test/system/practice/pages_test.rb
+++ test/system/practice/pages_test.rb
@@ -5,7 +5,7 @@ require 'application_system_test_case'
 class Practice::PagesTest < ApplicationSystemTestCase
   test 'show listing pages' do
     visit_with_auth "/practices/#{practices(:practice1).id}/pages", 'hatsuno'
-    assert_equal 'OS X Mountain Lionをクリーンインストールする | FBC', title
+    assert_equal 'OS X Mountain Lionをクリーンインストールするに関するDocs | FBC', title
   end
 
   test 'show last updated user icon and role' do
