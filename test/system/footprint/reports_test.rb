8e8e4ffa1fd2e961417740e932216eb9c633467e
Nabegon <watami03@gmail.com>
2022-08-27 02:38:57 +0200

    足あとのリンクの有無のテストを追加

--- test/system/footprint/reports_test.rb
+++ test/system/footprint/reports_test.rb
@@ -16,4 +16,35 @@ class Footprint::ReportsTest < ApplicationSystemTestCase
     assert_no_text '見たよ'
     assert_not page.has_css?('.a-user-icon.is-sotugyou')
   end
+
+  test 'show link if there are more than ten footprints' do
+    report = users(:komagata).reports.first
+    visit_with_auth report_path(report), 'kimura'
+    visit_with_auth report_path(report), 'machida'
+    visit_with_auth report_path(report), 'osnashi'
+    visit_with_auth report_path(report), 'marumarushain2'
+    visit_with_auth report_path(report), 'kananasi'
+    visit_with_auth report_path(report), 'nippounashi'
+    visit_with_auth report_path(report), 'hatsuno'
+    visit_with_auth report_path(report), 'jobseeker'
+    visit_with_auth report_path(report), 'advijirou'
+    visit_with_auth report_path(report), 'hajime'
+    visit_with_auth report_path(report), 'muryou'
+    assert page.has_css?('.page-content-prev-next__item-link')
+  end
+
+  test 'has no link if there are less than ten footprints' do
+    report = users(:komagata).reports.first
+    visit_with_auth report_path(report), 'kimura'
+    visit_with_auth report_path(report), 'machida'
+    visit_with_auth report_path(report), 'osnashi'
+    visit_with_auth report_path(report), 'marumarushain2'
+    visit_with_auth report_path(report), 'kananasi'
+    visit_with_auth report_path(report), 'nippounashi'
+    visit_with_auth report_path(report), 'hatsuno'
+    visit_with_auth report_path(report), 'jobseeker'
+    visit_with_auth report_path(report), 'advijirou'
+    visit_with_auth report_path(report), 'hajime'
+    assert_not page.has_css?('.a-user-icon.is-komagata')
+  end
 end
