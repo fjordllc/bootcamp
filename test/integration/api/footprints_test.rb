1bc59f7075aa46a04ff0665cf9a5d4ca90d8b115
Nabegon <watami03@gmail.com>
2022-08-31 15:14:51 +0200

    footprintable_idをfixtureから取得するようにした

--- test/integration/api/footprints_test.rb
+++ test/integration/api/footprints_test.rb
@@ -3,12 +3,18 @@
 require 'test_helper'
 
 class API::FootprintsTest < ActionDispatch::IntegrationTest
-  test 'GET /api/footprints.json?footprintable_id=12168338' do
+  fixtures :announcements
+
+  setup do
+    @announcement = announcements(:announcement1)
+  end
+
+  test 'GET /api/footprints.json?footprintable_id=@announcement.id' do
     get api_footprints_path(format: :json)
     assert_response :unauthorized
 
     token = create_token('machida', 'testtest')
-    get api_footprints_path(format: :json, footprintable_id: 12_168_338),
+    get api_footprints_path(format: :json, footprintable_id: @announcement.id),
         headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :bad_request
   end
