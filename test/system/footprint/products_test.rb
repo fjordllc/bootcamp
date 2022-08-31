afa8fbcbaf44649b245b82f1b93ad3924f1e7b24
Nabegon <watami03@gmail.com>
2022-08-31 14:19:58 +0200

    テストデータを直接作成する方法に変更

--- test/system/footprint/products_test.rb
+++ test/system/footprint/products_test.rb
@@ -3,50 +3,61 @@
 require 'application_system_test_case'
 
 class Footprint::ProductsTest < ApplicationSystemTestCase
+  setup do
+    @product = products(:product3)
+  end
+
   test 'should be create footprint in product page' do
-    product = users(:mentormentaro).products.first
-    visit_with_auth product_path(product), 'komagata'
-    assert_text '見たよ'
-    assert page.has_css?('.a-user-icon.is-komagata')
+    visit_with_auth product_path(@product), 'komagata'
+    assert_css '.a-user-icon.is-komagata'
   end
 
   test 'should not footpoint with my own product' do
-    product = users(:mentormentaro).products.first
-    visit_with_auth product_path(product), 'mentormentaro'
-    assert_no_text '見たよ'
-    assert_not page.has_css?('.a-user-icon.is-mentormentaro')
+    visit_with_auth product_path(@product), 'mentormentaro'
+    assert_no_css '.a-user-icon.is-mentormentaro'
   end
 
   test 'show link if there are more than ten footprints' do
-    users(:komagata).products.first
-    visit_with_auth "/products/#{products(:product3).id}", 'hatsuno'
-    visit_with_auth "/products/#{products(:product3).id}", 'kimura'
-    visit_with_auth "/products/#{products(:product3).id}", 'machida'
-    visit_with_auth "/products/#{products(:product3).id}", 'osnashi'
-    visit_with_auth "/products/#{products(:product3).id}", 'marumarushain2'
-    visit_with_auth "/products/#{products(:product3).id}", 'kananasi'
-    visit_with_auth "/products/#{products(:product3).id}", 'nippounashi'
-    visit_with_auth "/products/#{products(:product3).id}", 'hatsuno'
-    visit_with_auth "/products/#{products(:product3).id}", 'jobseeker'
-    visit_with_auth "/products/#{products(:product3).id}", 'advijirou'
-    visit_with_auth "/products/#{products(:product3).id}", 'hajime'
-    visit_with_auth "/products/#{products(:product3).id}", 'muryou'
-    assert page.has_css?('.page-content-prev-next__item-link')
+    user_data = User.unhibernated.unretired.last(11)
+    user_data.map do |user|
+      Footprint.create(
+        user_id: user.id,
+        footprintable_id: @product.id,
+        footprintable_type: "Product"
+      )
+    end
+
+    visit_with_auth product_path(@product), 'komagata'
+    assert_text 'その他1人'
   end
 
   test 'has no link if there are less than ten footprints' do
-    product = users(:komagata).products.first
-    visit_with_auth "/products/#{products(:product3).id}", 'kimura'
-    visit_with_auth "/products/#{products(:product3).id}", 'machida'
-    visit_with_auth "/products/#{products(:product3).id}", 'osnashi'
-    visit_with_auth "/products/#{products(:product3).id}", 'marumarushain2'
-    visit_with_auth "/products/#{products(:product3).id}", 'kananasi'
-    visit_with_auth "/products/#{products(:product3).id}", 'nippounashi'
-    visit_with_auth "/products/#{products(:product3).id}", 'hatsuno'
-    visit_with_auth "/products/#{products(:product3).id}", 'jobseeker'
-    visit_with_auth "/products/#{products(:product3).id}", 'advijirou'
-    visit_with_auth "/products/#{products(:product3).id}", 'hajime'
-    visit_with_auth "/products/#{products(:product3).id}", 'muryou'
-    assert_not page.has_css?('.a-user-icon.is-komagata')
+    user_data = User.unhibernated.unretired.last(10)
+    user_data.map do |user|
+      Footprint.create(
+        user_id: user.id,
+        footprintable_id: @product.id,
+        footprintable_type: "Product"
+      )
+    end
+
+    visit_with_auth product_path(@product), 'komagata'
+    assert_no_text 'その他1人'
+  end
+
+  test 'click on the link to view the rest of footprints' do
+    user_data = User.unhibernated.unretired.last(11)
+    user_data.map do |user|
+      Footprint.create(
+        user_id: user.id,
+        footprintable_id: @product.id,
+        footprintable_type: "Product"
+      )
+    end
+
+    visit_with_auth product_path(@product), 'komagata'
+
+    find('.page-content-prev-next__item-link', text: 'その他1人').click
+    assert_no_text 'その他1人'
   end
 end
--- test/system/footprint/reports_test.rb
+++ test/system/footprint/reports_test.rb
@@ -3,48 +3,62 @@
 require 'application_system_test_case'
 
 class Footprint::ReportsTest < ApplicationSystemTestCase
+  setup do
+    @report = reports(:report1)
+  end
+  
   test 'should be create footprint in /reports/:id' do
-    report = users(:komagata).reports.first
-    visit_with_auth report_path(report), 'sotugyou'
-    assert_text '見たよ'
-    assert page.has_css?('.a-user-icon.is-sotugyou')
+    visit_with_auth report_path(@report), 'sotugyou'
+    assert_css '.a-user-icon.is-sotugyou'
   end
 
   test 'should not footpoint with my own report' do
-    report = users(:sotugyou).reports.first
-    visit_with_auth report_path(report), 'sotugyou'
-    assert_no_text '見たよ'
-    assert_not page.has_css?('.a-user-icon.is-sotugyou')
+    visit_with_auth report_path(@report), 'komagata'
+    assert_no_css '.a-user-icon.is-komagata'
   end
 
   test 'show link if there are more than ten footprints' do
-    report = users(:komagata).reports.first
-    visit_with_auth report_path(report), 'kimura'
-    visit_with_auth report_path(report), 'machida'
-    visit_with_auth report_path(report), 'osnashi'
-    visit_with_auth report_path(report), 'marumarushain2'
-    visit_with_auth report_path(report), 'kananasi'
-    visit_with_auth report_path(report), 'nippounashi'
-    visit_with_auth report_path(report), 'hatsuno'
-    visit_with_auth report_path(report), 'jobseeker'
-    visit_with_auth report_path(report), 'advijirou'
-    visit_with_auth report_path(report), 'hajime'
-    visit_with_auth report_path(report), 'muryou'
-    assert page.has_css?('.page-content-prev-next__item-link')
+    user_data = User.unhibernated.unretired.last(11)
+    user_data.map do |user|
+      Footprint.create(
+        user_id: user.id,
+        footprintable_id: @report.id,
+        footprintable_type: "Report"
+      )
+    end
+
+    visit_with_auth report_path(@report), 'komagata'
+    assert_text 'その他1人'
   end
 
-  test 'has no link if there are less than ten footprints' do
-    report = users(:komagata).reports.first
-    visit_with_auth report_path(report), 'kimura'
-    visit_with_auth report_path(report), 'machida'
-    visit_with_auth report_path(report), 'osnashi'
-    visit_with_auth report_path(report), 'marumarushain2'
-    visit_with_auth report_path(report), 'kananasi'
-    visit_with_auth report_path(report), 'nippounashi'
-    visit_with_auth report_path(report), 'hatsuno'
-    visit_with_auth report_path(report), 'jobseeker'
-    visit_with_auth report_path(report), 'advijirou'
-    visit_with_auth report_path(report), 'hajime'
-    assert_not page.has_css?('.a-user-icon.is-komagata')
+  test 'has no link if there are ten or less footprints' do
+    user_data = User.unhibernated.unretired.last(10)
+    user_data.map do |user|
+      Footprint.create(
+        user_id: user.id,
+        footprintable_id: @report.id,
+        footprintable_type: "Report"
+      )
+    end
+
+    visit_with_auth report_path(@report), 'komagata'
+    assert_no_text 'その他'
+  end
+
+  test 'click on the link to view the rest of footprints' do
+    user_data = User.unhibernated.unretired.last(11)
+    user_data.map do |user|
+      Footprint.create(
+        user_id: user.id,
+        footprintable_id: @report.id,
+        footprintable_type: "Report"
+      )
+    end
+
+    visit_with_auth report_path(@report), 'komagata'
+    assert_text 'その他1人'
+
+    find('.page-content-prev-next__item-link', text: 'その他1人').click
+    assert_no_text 'その他1人'
   end
 end
