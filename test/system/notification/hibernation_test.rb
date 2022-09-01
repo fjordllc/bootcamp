47c66553f14abfb72983b51ee9f17854105e0c53
keiz1213 <fukuoka00tsc@gmail.com>
2022-09-02 08:00:47 +0900

    テスト追加

---
+++ test/system/notification/hibernation_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class Notification::HibernationTest < ApplicationSystemTestCase
+  setup do
+    @delivery_mode = AbstractNotifier.delivery_mode
+    AbstractNotifier.delivery_mode = :normal
+  end
+
+  teardown do
+    AbstractNotifier.delivery_mode = @delivery_mode
+  end
+
+  test 'notify admins and mentors when a user hibernate' do
+    visit_with_auth notifications_path, 'komagata'
+    find('#notifications.loaded', wait: 10)
+    within first('.card-list-item') do
+      assert_no_selector '.card-list-item-title__link-label', text: ' kimuraさんが休会しました。'
+    end
+
+    visit_with_auth new_hibernation_path, 'kimura'
+    fill_in 'hibernation[scheduled_return_on]', with: Time.current.next_month
+    fill_in 'hibernation[reason]', with: 'テストのため'
+    click_button '休会する'
+    page.driver.browser.switch_to.alert.accept
+    assert_text '休会処理が完了しました'
+
+    visit_with_auth notifications_path, 'komagata'
+    find('#notifications.loaded', wait: 10)
+    within first('.card-list-item.is-unread') do
+      assert_selector '.card-list-item-title__link-label', text: 'kimuraさんが休会しました。'
+    end
+  end
+end
