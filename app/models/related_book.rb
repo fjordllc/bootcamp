06f0e5a17974bbc6db65908eaddf427625ba3141
daiki0381 <daiki03810218@gmail.com>
2022-09-03 15:54:37 +0900

    表紙画像の形式にバリデーションをかける

--- app/models/related_book.rb
+++ app/models/related_book.rb
@@ -3,4 +3,10 @@
 class RelatedBook < ApplicationRecord
   belongs_to :user
   has_one_attached :cover
+
+  validates :cover, attached: false,
+                    content_type: {
+                      in: %w[image/png image/jpg image/jpeg image/gif],
+                      message: 'はPNG, JPG, GIF形式にしてください'
+                    }
 end
