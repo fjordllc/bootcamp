0b484323a111f14585447c2bcec668edd0db5980
komagata <komagata@gmail.com>
2022-09-14 04:25:39 +0900

    管理画面のコース一覧をReact化

---
+++ app/javascript/components/AdminCourses.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import useSWR from 'swr'
+import AdminCourse from 'components/AdminCourse'
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function AdminCourses() {
+  const { data, error } = useSWR('/api/courses.json', fetcher)
+  if (error) return "An error has occurred.";
+  if (!data) return "Loading...";
+  const courses = data.courses
+
+  return (
+    <div className="page-body">
+      <div className="container is-lg">
+        <div className="admin-table">
+          <table className="admin-table__table">
+            <thead className="admin-table__header">
+              <tr className="admin-table__labels">
+                <td className="admin-table__label">コース名</td>
+                <td className="admin-table__label">説明文</td>
+                <td className="admin-table__label">操作</td>
+              </tr>
+            </thead>
+            <tbody className="admin-table__items">
+              {courses.map((course) => {
+                return <AdminCourse key={course.id} course={course} />
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
