c5b293897070d16bdcf97ffe87333345631c6fb4
komagata <komagata@gmail.com>
2022-09-14 05:29:15 +0900

    lintの指摘を修正

--- app/javascript/components/AdminCourse.js
+++ app/javascript/components/AdminCourse.js
@@ -9,20 +9,24 @@ export default function AdminCourse(props) {
   return (
     <tr className="admin-table__item">
       <td className="admin-table__item-value">
-        <a href={url}>{course.title}</a> 
+        <a href={url}>{course.title}</a>
       </td>
-      <td className="admin-table__item-value">
-        {course.description}
-      </td>
+      <td className="admin-table__item-value">{course.description}</td>
       <td className="admin-table__item-value admin-table__item-value is-text-align-center">
         <ul className="is-inline-buttons">
           <li>
-            <a href={editUrl} className="a-button is-sm is-secondary is-icon is-block">
+            <a
+              href={editUrl}
+              className="a-button is-sm is-secondary is-icon is-block"
+            >
               <i className="fa fa-solid fa-pen" />
             </a>
           </li>
           <li>
-            <a href={sortUrl} className="a-button is-sm is-secondary is-icon is-block">
+            <a
+              href={sortUrl}
+              className="a-button is-sm is-secondary is-icon is-block"
+            >
               <i className="fa-solid fa-align-justify" />
             </a>
           </li>
