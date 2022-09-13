a88cccb0dabf21adbe96544d0f3e48910fef1c82
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-09-13 10:21:49 +0900

    テストの用意

---
+++ test/system/api/page/pages_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class Check::PagesTest < ApplicationSystemTestCase
+  test 'accessing a existent page returns 200' do
+  end
+  test 'accessing a non-existent page returns 404' do
+  end
+end
