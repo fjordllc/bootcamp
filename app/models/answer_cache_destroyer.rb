ecddc4be702f3274072257278cbd9d0c3e3448ec
choco <superbeaver.180809@gmail.com>
2022-09-16 23:40:11 +0900

    notify_correct_answerをAnswerCacheDestroyer内に定義した

--- app/models/answer_cache_destroyer.rb
+++ app/models/answer_cache_destroyer.rb
@@ -5,4 +5,14 @@ class AnswerCacheDestroyer
     notify_correct_answer(answer) if answer.saved_change_to_attribute?('type', to: 'CorrectAnswer')
     Cache.delete_not_solved_question_count
   end
+
+  def notify_correct_answer(answer)
+    question = answer.question
+    watcher_ids = question.watches.pluck(:user_id)
+    receiver_ids = watcher_ids - [question.user_id]
+    receiver_ids.each do |receiver_id|
+      receiver = User.find(receiver_id)
+      NotificationFacade.chose_correct_answer(answer, receiver)
+    end
+  end
 end
