c836e67bb339a3db1702caaf44b6a47835047657
TakashimaAyaka <takashimaayaka@MacBook-Pro.local>
2022-09-19 19:44:56 +0900

    Q＆AのWatchしてる人への通知処理をnewspaperに置き換えた

--- app/models/answer_callbacks.rb
+++ app/models/answer_callbacks.rb
@@ -3,7 +3,6 @@
 class AnswerCallbacks
   def after_create(answer)
     create_watch(answer)
-    notify_to_watching_user(answer)
   end
 
   def after_save(answer)
@@ -17,21 +16,6 @@ class AnswerCallbacks
   end
 
   private
-
-  def notify_to_watching_user(answer)
-    question = Question.find(answer.question_id)
-    mention_user_ids = answer.new_mention_users.ids
-
-    return unless question.try(:watched?)
-
-    watcher_ids = Watch.where(watchable_id: question.id).pluck(:user_id)
-    watcher_ids.each do |watcher_id|
-      if watcher_id != answer.sender.id && !mention_user_ids.include?(watcher_id)
-        watcher = User.find_by(id: watcher_id)
-        NotificationFacade.watching_notification(question, watcher, answer)
-      end
-    end
-  end
 
   def notify_correct_answer(answer)
     question = answer.question
