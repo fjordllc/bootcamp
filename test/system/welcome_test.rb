9156f89bc0bc8a79965154a06c673d723ddaafe5
daiki0381 <daiki03810218@gmail.com>
2022-09-22 15:44:07 +0900

    amazonのURLの修正

--- test/system/welcome_test.rb
+++ test/system/welcome_test.rb
@@ -61,7 +61,7 @@ class WelcomeTest < ApplicationSystemTestCase
     fill_in 'user[profile_text]', with: '[株式会社フィヨルド](https://fjord.jp)の代表兼プログラマー。Rubyが大好きで[怖話](https://kowabana.jp)、[フィヨルドブートキャンプ](https://bootcamp.fjord.jp)などを開発している。'
     click_on '書籍を追加'
     find("input[name*='[title]']").set('プロを目指す人のためのRuby入門 言語仕様からテスト駆動開発・デバッグ技法まで')
-    find("input[name*='[url]']").set('https://www.amazon.co.jp/%E3%83%97%E3%83%AD%E3%82%92%E7%9B%AE%E6%8C%87%E3%81%99%E4%BA%BA%E3%81%AE%E3%81%9F%E3%82%81%E3%81%AERuby%E5%85%A5%E9%96%80-%E8%A8%80%E8%AA%9E%E4%BB%95%E6%A7%98%E3%81%8B%E3%82%89%E3%83%86%E3%82%B9%E3%83%88%E9%A7%86%E5%8B%95%E9%96%8B%E7%99%BA%E3%83%BB%E3%83%87%E3%83%90%E3%83%83%E3%82%B0%E6%8A%80%E6%B3%95%E3%81%BE%E3%81%A7-Software-Design-plus%E3%82%B7%E3%83%AA%E3%83%BC%E3%82%BA/dp/4774193976/ref=tmm_other_meta_binding_swatch_0?_encoding=UTF8&qid=1662112087&sr=8-1')
+    find("input[name*='[url]']").set('https://www.amazon.co.jp/dp/B09MPX7SMY')
     attach_file '画像を選択', Rails.root.join('test/fixtures/files/users/books/cherry-book.jpg'), make_visible: true
     click_button '更新する'
     logout
@@ -82,7 +82,7 @@ class WelcomeTest < ApplicationSystemTestCase
     fill_in 'user[profile_text]', with: '[株式会社フィヨルド](https://fjord.jp)の代表兼プログラマー。Rubyが大好きで[怖話](https://kowabana.jp)、[フィヨルドブートキャンプ](https://bootcamp.fjord.jp)などを開発している。'
     click_on '書籍を追加'
     find("input[name*='[title]']").set('プロを目指す人のためのRuby入門 言語仕様からテスト駆動開発・デバッグ技法まで')
-    find("input[name*='[url]']").set('https://www.amazon.co.jp/%E3%83%97%E3%83%AD%E3%82%92%E7%9B%AE%E6%8C%87%E3%81%99%E4%BA%BA%E3%81%AE%E3%81%9F%E3%82%81%E3%81%AERuby%E5%85%A5%E9%96%80-%E8%A8%80%E8%AA%9E%E4%BB%95%E6%A7%98%E3%81%8B%E3%82%89%E3%83%86%E3%82%B9%E3%83%88%E9%A7%86%E5%8B%95%E9%96%8B%E7%99%BA%E3%83%BB%E3%83%87%E3%83%90%E3%83%83%E3%82%B0%E6%8A%80%E6%B3%95%E3%81%BE%E3%81%A7-Software-Design-plus%E3%82%B7%E3%83%AA%E3%83%BC%E3%82%BA/dp/4774193976/ref=tmm_other_meta_binding_swatch_0?_encoding=UTF8&qid=1662112087&sr=8-1')
+    find("input[name*='[url]']").set('https://www.amazon.co.jp/dp/B09MPX7SMY')
     attach_file '画像を選択', Rails.root.join('test/fixtures/files/users/books/cherry-book.jpg'), make_visible: true
     click_button '更新する'
     logout
