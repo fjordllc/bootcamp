23ede0983966e1f82a2aaa55e3c6a8e29ff5d255
shirotamaki <shiro.tamaki63@gmail.com>
2022-09-26 23:07:06 +0900

    コンフリクトの修正

--- config/initializers/newspaer.rb
+++ config/initializers/newspaer.rb
@@ -1,12 +1,8 @@
 Rails.configuration.to_prepare do
   Newspaper.subscribe(:event_create, EventOrganizerWatcher.new)
   Newspaper.subscribe(:answer_create, AnswerNotifier.new)
-<<<<<<< HEAD
   Newspaper.subscribe(:answer_create, NotifierToWatchingUser.new)
-  Newspaper.subscribe(:announce_destroy, AnnouncementNotificationDestroyer.new)
-=======
   Newspaper.subscribe(:announcement_destroy, AnnouncementNotificationDestroyer.new)
->>>>>>> 引数名をモデル名に合わせるため変更
 
   sad_streak_updater = SadStreakUpdater.new
   Newspaper.subscribe(:report_create, sad_streak_updater)
