e795c1039d8a11c4098839562c2f23357e6486bd
keiz1213 <fukuoka00tsc@gmail.com>
2022-09-28 07:47:48 +0900

    システムテスト追加

--- test/system/notification/answers_test.rb
+++ test/system/notification/answers_test.rb
@@ -30,4 +30,13 @@ class Notification::AnswersTest < ApplicationSystemTestCase
     visit_with_auth '/', 'komagata'
     refute_text @notice_text
   end
+
+  test 'a week have passed since last answer' do
+    visit_with_auth '/scheduler/daily', 'kimura'
+    visit '/notifications'
+
+    within first('.card-list-item.is-unread') do
+      assert_text 'Q&A「テストの質問」のベストaアンサーがまだ選ばれていません。'
+    end
+  end
 end
