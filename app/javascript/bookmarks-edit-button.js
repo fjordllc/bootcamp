abb255164aece8cc7e021594b265d06a2617fe98
choco <superbeaver.180809@gmail.com>
2022-10-01 17:04:11 +0900

    visibilityプロパティは描写速度が遅いため、displayに変更した

--- app/javascript/bookmarks-edit-button.js
+++ app/javascript/bookmarks-edit-button.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const bookmarkDeleteButton = document.getElementsByClassName('bookmark-delete-button')
   if (bookMarksEditButton && bookmarkDeleteButton) {
     for (let i = 0; i < bookmarkDeleteButton.length; i++) {
-      bookmarkDeleteButton[i].style.visibility = 'hidden'
+      bookmarkDeleteButton[i].style.display = 'none'
     }
   }
 
   bookMarksEditButton.addEventListener('click', () => {
     if (bookMarksEditButton.checked) {
       for (let i = 0; i < bookmarkDeleteButton.length; i++) {
-        bookmarkDeleteButton[i].style.visibility = 'visible'
+        bookmarkDeleteButton[i].style.display = 'block'
       }
     } else {
       for (let i = 0; i < bookmarkDeleteButton.length; i++) {
-        bookmarkDeleteButton[i].style.visibility = 'hidden'
+        bookmarkDeleteButton[i].style.display = 'none'
       }
     }
   })
