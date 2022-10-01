4d167347c00b7750ce5c16c3b7d99a5dcecbec93
choco <superbeaver.180809@gmail.com>
2022-10-01 17:07:52 +0900

    prettierの警告を修正した

--- app/javascript/bookmarks-edit-button.js
+++ app/javascript/bookmarks-edit-button.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const bookMarksEditButton = document.getElementById('bookmark_edit')
-  const bookmarkDeleteButton = document.getElementsByClassName('bookmark-delete-button')
+  const bookmarkDeleteButton = document.getElementsByClassName(
+    'bookmark-delete-button'
+  )
   if (bookMarksEditButton && bookmarkDeleteButton) {
     for (let i = 0; i < bookmarkDeleteButton.length; i++) {
       bookmarkDeleteButton[i].style.display = 'none'
