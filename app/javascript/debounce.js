aa7b7e87d5e6bbc952b456c0d7b5b1f2a7ce97d1
ai-24 <ai.morikawa2410sj@gmail.com>
2022-10-11 15:53:55 +0900

    debounce関数を他ファイルで再利用可能にした

---
+++ app/javascript/debounce.js
@@ -0,0 +1,15 @@
+const debounce = (func, wait) => {
+  let timerId
+  return function (...args) {
+    if (timerId) {
+      clearTimeout(timerId)
+    }
+    timerId = setTimeout(() => {
+      func.apply(this, args)
+    }, wait)
+  }
+}
+
+export default {
+  debounce
+}
