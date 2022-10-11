854283560ff0bdd9eec2ef37f058ad744a17ea3a
hikarook94 <walrus4l145@icloud.com>
2022-10-11 11:17:29 +0900

    DiscordNotifierを使った処理に変更

--- app/models/announcement_chat_notifier.rb
+++ app/models/announcement_chat_notifier.rb
@@ -2,12 +2,6 @@
 
 class AnnouncementChatNotifier
   def call(announce)
-    path = Rails.application.routes.url_helpers.polymorphic_path(announce)
-    url = "https://bootcamp.fjord.jp#{path}"
-
-    ChatNotifier.message(
-      "お知らせ：「#{announce.title}」\r#{url}",
-      webhook_url: ENV['DISCORD_ALL_WEBHOOK_URL']
-    )
+    DiscordNotifier.with(announce: announce).announced.notify_now
   end
 end
