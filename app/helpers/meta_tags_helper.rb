5f87933fa32628aa8af71f8209782f42445b3a38
lan <le.hoang.lan@actindi.net>
2022-10-13 05:43:57 +0900

    welcome_meta_tags追加

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -27,4 +27,33 @@ module MetaTagsHelper
     }
   end
   # rubocop:enable Metrics/MethodLength
+
+  # rubocop:disable Metrics/MethodLength
+  def welcome_meta_tags
+    {
+      title: title || nil,
+      site: 'FBC',
+      reverse: true,
+      charset: 'utf-8',
+      description: '現場の即戦力になれるプログラミングスクール。',
+      viewport: 'width=device-width, initial-scale=1.0',
+      og: {
+        title: title || 'FJORD BOOT CAMP（フィヨルドブートキャンプ）',
+        type: 'website',
+        site_name: 'fjord bootcamp',
+        description: :description,
+        image: 'https://bootcamp.fjord.jp/ogp/ogp.png',
+        url: 'https://bootcamp.fjord.jp'
+      },
+      twitter: {
+        title: title || 'FJORD BOOT CAMP（フィヨルドブートキャンプ）',
+        card: 'summary',
+        site: '@fjordbootcamp',
+        description: :description,
+        image: 'https://bootcamp.fjord.jp/ogp/ogp.png',
+        domain: 'https://bootcamp.fjord.jp'
+      }
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
 end
