04171b8f5a3994bef48acd535e7995416e28b40e
choco <superbeaver.180809@gmail.com>
2022-10-12 20:49:30 +0900

    tailwindcssを使用できる箇所を定義

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*.html.slim',
+    './app/javascript/**/*.{js,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
