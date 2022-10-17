791db90a8a3c3f596b548f23fc9825d42c4b1b4d
lan <le.hoang.lan@actindi.net>
2022-10-17 17:29:34 +0900

    共有コードにする

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -30,30 +30,11 @@ module MetaTagsHelper
 
   # rubocop:disable Metrics/MethodLength
   def welcome_meta_tags
-    {
-      title: title || nil,
-      site: 'FBC',
-      reverse: true,
-      charset: 'utf-8',
-      description: '現場の即戦力になれるプログラミングスクール。',
-      viewport: 'width=device-width, initial-scale=1.0',
-      og: {
-        title: title || 'FJORD BOOT CAMP（フィヨルドブートキャンプ）',
-        type: 'website',
-        site_name: 'fjord bootcamp',
-        description: :description,
-        image: 'https://bootcamp.fjord.jp/ogp/ogp.png',
-        url: 'https://bootcamp.fjord.jp'
-      },
-      twitter: {
-        title: title || 'FJORD BOOT CAMP（フィヨルドブートキャンプ）',
-        card: 'summary',
-        site: '@fjordbootcamp',
-        description: :description,
-        image: 'https://bootcamp.fjord.jp/ogp/ogp.png',
-        domain: 'https://bootcamp.fjord.jp'
-      }
-    }
+    welcome_meta_tags = default_meta_tags.dup
+    welcome_meta_tags[:title] = title || nil
+    welcome_meta_tags[:og][:title] = title || 'FJORD BOOT CAMP（フィヨルドブートキャンプ）'
+    welcome_meta_tags[:twitter][:title] = title || 'FJORD BOOT CAMP（フィヨルドブートキャンプ）'
+    welcome_meta_tags
   end
   # rubocop:enable Metrics/MethodLength
 end
