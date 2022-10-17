8b9049f157bf46e13bcb614347a07366a4af4335
fuwa <fuwafuwa41.glr@gmail.com>
2022-10-17 15:37:13 +0900

    プラクティスごとにcategory_idsをjsonで取得できるようにした

--- app/controllers/admin/practices_controller.rb
+++ app/controllers/admin/practices_controller.rb
@@ -7,34 +7,6 @@ class Admin::PracticesController < AdminController
     @practices = Practice.all
   end
 
-  def show; end
-
-  def new
-    @practice = Practice.new
-  end
-
-  def create
-    @practice = Practice.new(practice_params)
-    if @practice.save
-      redirect_to admin_practices_url, notice: 'プラクティスを作成しました。'
-    else
-      render action: 'new'
-    end
-  end
-
-  def update
-    if @practice.update(practice_params)
-      redirect_to return_to, notice: 'プラクティスを更新しました。'
-    else
-      render action: 'edit'
-    end
-  end
-
-  def destroy
-    @practice.destroy
-    redirect_to admin_practices_url, notice: 'プラクティスを削除しました。'
-  end
-
   private
 
   def set_practice
@@ -43,13 +15,16 @@ class Admin::PracticesController < AdminController
 
   def practice_params
     params.require(:practice).permit(
-      :name,
       :title,
       :description,
       :goal,
+      :submission,
       :open_product,
       :include_progress,
-      :memo
+      :ogp_image,
+      :memo,
+      category_ids: [],
+      practices_books_attributes: %i[id book_id must_read _destroy]
     )
   end
 
