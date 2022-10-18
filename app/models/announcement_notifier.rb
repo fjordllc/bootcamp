fd6b3ef8cd27ae74ad7b89433cc703f912f575f5
akingo55 <am1157154@gmail.com>
2022-10-19 00:05:38 +0900

    rubocop修正

--- app/models/announcement_notifier.rb
+++ app/models/announcement_notifier.rb
@@ -3,6 +3,7 @@
 class AnnouncementNotifier
   def call(announce)
     return if announce.wip?
+
     target_users = User.announcement_receiver(announce.target)
     target_users.each do |target|
       NotificationFacade.post_announcement(announce, target) if announce.sender != target
