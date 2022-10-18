c28a3201612138794b6a4cad55464d75b131a075
akingo55 <am1157154@gmail.com>
2022-10-18 23:53:00 +0900

    不要なメソッド削除

--- app/models/announcement_callbacks.rb
+++ app/models/announcement_callbacks.rb
@@ -4,14 +4,7 @@ class AnnouncementCallbacks
   def after_create(announce)
     return if announce.wip?
 
-    after_first_publish(announce)
     create_author_watch(announce)
-  end
-
-  def after_update(announce)
-    return unless !announce.wip && announce.published_at.nil?
-
-    after_first_publish(announce)
   end
 
   private
