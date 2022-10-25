51dab54eaff564c0b73b74d0e732140bdda706ea
akingo55 <am1157154@gmail.com>
2022-10-26 00:27:40 +0900

    お知らせのnewpaper処理をまとめる

--- app/models/announcement_notifier.rb
+++ app/models/announcement_notifier.rb
@@ -2,11 +2,15 @@
 
 class AnnouncementNotifier
   def call(announce)
-    return if announce.wip?
+    return if announce.wip? || !announce.published_at.nil?
+
+    announce.update(published_at: Time.current)
+    DiscordNotifier.with(announce: announce).announced.notify_now
 
     target_users = User.announcement_receiver(announce.target)
     target_users.each do |target|
       NotificationFacade.post_announcement(announce, target) if announce.sender != target
     end
+
   end
 end
