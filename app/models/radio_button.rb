13c0c2a83e1a00775ffe207605358e42b761ef9f
daiki0381 <daiki03810218@gmail.com>
2022-10-25 14:07:51 +0900

    normalize_blankメソッドの共通化

--- app/models/radio_button.rb
+++ app/models/radio_button.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
 class RadioButton < ApplicationRecord
+  include SurveyQuestionFormat
   belongs_to :survey_question
   has_many :radio_button_choices, dependent: :destroy
   accepts_nested_attributes_for :radio_button_choices, allow_destroy: true
   validates_associated :radio_button_choices
-  before_save :normalize_blank_values
+  before_save do
+    columns = %i[title_of_reason description_of_reason]
+    normalize_blank!(columns)
+  end
 
   with_options if: -> { survey_question.format == 'radio_button' }, presence: true do
     validates :radio_button_choices
     validates :title_of_reason
-  end
-
-  def normalize_blank_values
-    columns = %i[title_of_reason description_of_reason]
-    columns.each do |column|
-      self[column] = nil if self[column].blank?
-    end
   end
 end
--- app/models/radio_button_choice.rb
+++ app/models/radio_button_choice.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 class RadioButtonChoice < ApplicationRecord
+  include SurveyQuestionFormat
   belongs_to :radio_button
-  before_save :normalize_blank_values
+  before_save do
+    columns = %i[choices]
+    normalize_blank!(columns)
+  end
 
   validates :choices, presence: true, if: -> { radio_button.survey_question.format == 'radio_button' }
-
-  def normalize_blank_values
-    columns = %i[choices]
-    columns.each do |column|
-      self[column] = nil if self[column].blank?
-    end
-  end
 end
