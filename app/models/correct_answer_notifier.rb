41dc08cd6bade79e80cbe02d1d8413441444e4ac
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-10-30 07:27:19 +0900

    NewsPaperの回答通知を実装

---
+++ app/models/correct_answer_notifier.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CorrectAnswerNotifier
+  def call(answer)
+    notify_correct_answer(answer) if answer.saved_change_to_attribute?('type', to: 'CorrectAnswer')
+  end
+
+  private
+
+  def notify_correct_answer(answer)
+    question = answer.question
+    watcher_ids = question.watches.pluck(:user_id)
+    receiver_ids = watcher_ids - [question.user_id]
+    receiver_ids.each do |receiver_id|
+      receiver = User.find(receiver_id)
+      NotificationFacade.chose_correct_answer(answer, receiver)
+    end
+  end
+end
