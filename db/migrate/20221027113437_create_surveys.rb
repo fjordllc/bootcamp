265fd4a4d72efd373bd082dc8bc018f561d5bee2
TakashimaAyaka <takashimaayaka@MacBook-Pro.local>
2022-10-30 12:29:05 +0900

    surveyテーブルの作成完了&userテーブルとのリレーション構築

--- db/migrate/20221027113437_create_surveys.rb
+++ db/migrate/20221027113437_create_surveys.rb
@@ -2,9 +2,11 @@ class CreateSurveys < ActiveRecord::Migration[6.1]
   def change
     create_table :surveys do |t|
       t.references :user, null: false, foreign_key: true
-      t.text :title
-      t.datetime :expires_at
-      t.boolean :wip
+      t.string :title, null: false, limit: 255
+      t.text :description
+      t.datetime :published_at, null: false
+      t.datetime :expires_at, null: false
+      t.boolean :wip, null: false, default: false
 
       t.timestamps
     end
