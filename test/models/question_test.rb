6e69104bde161bdecc6138e20d3de0be8ae15f17
keiz1213 <fukuoka00tsc@gmail.com>
2022-10-31 20:56:28 +0900

    last_answerメソッドのテスト追加

---
+++ test/models/question_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class QuestionTest < ActiveSupport::TestCase
+  test '#last_answer' do
+    questioner = users(:kimura)
+    answerer = users(:komagata)
+    question = Question.create!(
+      title: '一週間前の質問',
+      description: 'テスト',
+      user: questioner,
+      created_at: Time.current - 1.week,
+      updated_at: Time.current - 1.week,
+      published_at: Time.current - 1.week
+    )
+    first_answer = Answer.create!(
+      description: '回答1',
+      user: answerer,
+      question: question,
+      created_at: Time.current - 1.week,
+      updated_at: Time.current - 1.week
+    )
+
+    last_answer = Answer.create!(
+      description: '回答2',
+      user: answerer,
+      question: question,
+      created_at: Time.current - 6.day,
+      updated_at: Time.current - 6.day
+    )
+
+    first_answer.update!(updated_at: Time.current - 5.day)
+    assert_equal question.last_answer, last_answer
+  end
+end
