6bbb4d7698e948ebbb1c9d0e4c47438e1fd084c1
hikarook94 <walrus4l145@icloud.com>
2022-10-31 10:53:04 +0900

    メソッド名を修正

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module BooksHelper
-  def must_read_for_any_practice?(book)
+  def must_read_for_any_practices?(book)
     book.practices_books.any?(&:must_read)
   end
 end
