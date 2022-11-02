71251c5262475f7c75c62e53daa198cc3cc26def
teppei machida <machida@fjord.jp>
2022-11-02 23:35:29 +0900

    管理ページプラクティス一覧のモーダルのデザイン調整

--- app/javascript/components/admin-practice-modal.vue
+++ app/javascript/components/admin-practice-modal.vue
@@ -4,17 +4,22 @@
     header.card-header.is-sm
       h2.card-header__title
         | 所属カテゴリー
-    .card-body.new-line(
+    .card-body(
       v-for='category_id_name in postPractice.category_ids_names'
     )
-      td.admin-table__item-value(
-        @click.prevent='linkCategory(category_id_name.category_id)'
-      )
-        | {{ category_id_name.category_name }}
-    ul.card-main-actions__items
-      li.card-main-actions__item.is-main
-        button.a-button.is-primary.is-sm.is-block(@click.prevent='closeModal')
-          | 閉じる
+      .card__description
+        table.admin-table__table
+          tbody.admin-table__items
+            tr.admin-table__item
+              td.admin-table__item-value.is-text-align-left(
+                @click.prevent='linkCategory(category_id_name.category_id)'
+              )
+                | {{ category_id_name.category_name }}
+    footer.card-footer
+      ul.card-main-actions__items
+        li.card-main-actions__item.is-main
+          button.a-button.is-primary.is-sm.is-block(@click.prevent='closeModal')
+            | 閉じる
 </template>
 <script>
 export default {
