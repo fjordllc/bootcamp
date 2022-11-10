cccfd9886b63a62b14a1eecdc03d5b646fcee6d6
daiki0381 <daiki03810218@gmail.com>
2022-11-10 21:14:42 +0900

    update時にvalidationが走らない問題の解消

--- app/models/check_box_choice.rb
+++ app/models/check_box_choice.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class CheckBoxChoice < ApplicationRecord
-  include SurveyQuestionFormat
   belongs_to :check_box
-  before_save :normalize_blank_check_box_choice_and_radio_button_choice!
 
   validates :choices, presence: true, if: -> { check_box.survey_question.format == 'check_box' }
 end
--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -4,10 +4,13 @@ class SurveyQuestion < ApplicationRecord
   belongs_to :user
   has_one :linear_scale, dependent: :destroy
   accepts_nested_attributes_for :linear_scale, allow_destroy: true
+  validates_associated :linear_scale
   has_one :radio_button, dependent: :destroy
   accepts_nested_attributes_for :radio_button, allow_destroy: true
+  validates_associated :radio_button
   has_one :check_box, dependent: :destroy
   accepts_nested_attributes_for :check_box, allow_destroy: true
+  validates_associated :check_box
 
   enum format: {
     text_area: 0,
