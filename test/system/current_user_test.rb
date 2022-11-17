391d2e9b0af685840bce8046bdbf3be64fb2d678
ksmxxxxxx <kankichi69@gmail.com>
2022-11-17 14:18:14 +0900

    アドバイザーも非表示のテストをしていたので、メンターも非表示のテストを追加

--- test/system/current_user_test.rb
+++ test/system/current_user_test.rb
@@ -56,10 +56,12 @@ class CurrentUserTest < ApplicationSystemTestCase
     assert_text '分報URLはDiscordのチャンネルURLを入力してください'
   end
 
-  test 'Do not show after graduation hope when advisor' do
+  test 'Do not show after graduation hope when advisor or mentor' do
     visit_with_auth '/current_user/edit', 'hajime'
     assert_text 'フィヨルドブートキャンプを卒業した自分はどうなっていたいかを教えてください'
     visit_with_auth '/current_user/edit', 'senpai'
+    assert_no_text 'フィヨルドブートキャンプを卒業した自分はどうなっていたいかを教えてください'
+    visit_with_auth '/current_user/edit', 'mentormentaro'
     assert_no_text 'フィヨルドブートキャンプを卒業した自分はどうなっていたいかを教えてください'
   end
 
