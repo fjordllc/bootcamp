f29b58bba4207e63119555b4dea2adce08ec3b93
TakashimaAyaka <takashimaayaka@MacBook-Pro.local>
2022-11-19 21:34:01 +0900

    index、show、new、editを作成した

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,10 +2,11 @@
 
 class SurveysController < ApplicationController
   before_action :set_survey, only: %i[ show edit update destroy ]
+  before_action :require_admin_or_mentor_login
 
   # GET /surveys or /surveys.json
   def index
-    @surveys = Survey.all
+    @surveys = Survey.all.order(end_at: :desc)
   end
 
   # GET /surveys/1 or /surveys/1.json
@@ -14,8 +15,8 @@ class SurveysController < ApplicationController
 
   # GET /surveys/new
   def new
-    @survey = Survey.new
-    @survey_question = @survey.survey_questions.build
+    @survey = Survey.new(start_at: Time.current.beginning_of_day, end_at: Time.current.end_of_day.strftime('%Y-%m-%dT-%H:%M'))
+    @survey.survey_question_listings.build
   end
 
   # GET /surveys/1/edit
@@ -25,49 +26,54 @@ class SurveysController < ApplicationController
   # POST /surveys or /surveys.json
   def create
     @survey = Survey.new(survey_params)
-
-    respond_to do |format|
-      if @survey.save
-        format.html { redirect_to survey_url(@survey), notice: "Survey was successfully created." }
-        format.json { render :show, status: :created, location: @survey }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
+    @survey.user_id = current_user.id
+    if @survey.save
+      redirect_to surveys_path, notice: notice_message(@survey)
+    else
+      render action: :new
     end
   end
 
   # PATCH/PUT /surveys/1 or /surveys/1.json
   def update
-    respond_to do |format|
-      if @survey.update(survey_params)
-        format.html { redirect_to survey_url(@survey), notice: "Survey was successfully updated." }
-        format.json { render :show, status: :ok, location: @survey }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @survey.errors, status: :unprocessable_entity }
-      end
+    if @survey.update(survey_params)
+      redirect_to surveys_path, notice: notice_message(@survey)
+    else
+      render :edit
     end
   end
 
   # DELETE /surveys/1 or /surveys/1.json
   def destroy
     @survey.destroy
-
-    respond_to do |format|
-      format.html { redirect_to surveys_url, notice: "Survey was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to surveys_path, notice: 'アンケートを削除しました。'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_survey
-      @survey = Survey.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def survey_params
-      params.require(:survey).permit(:user_id, :title, :expires_at, :wip)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_survey
+    @survey = Survey.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def survey_params
+    params.require(:survey).permit(
+      :title,
+      :start_at,
+      :end_at,
+      :description,
+      survey_question_ids: [],
+      survey_question_listings_attributes: %i[id survey_question_id _destroy]
+    )
+  end
+
+  def notice_message(survey)
+    case params[:action]
+    when 'create'
+      survey.before_start?(survey.id) ? 'アンケートを受付前として保存しました。' : 'アンケートを作成しました。'
+    when 'update'
+      survey.before_start?(survey.id) ? 'アンケートを受付前として保存しました。' : 'アンケートを更新しました。'
     end
+  end  
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,4 +4,16 @@ class Survey < ApplicationRecord
   belongs_to :user
   has_many :survey_question_listings, dependent: :destroy
   has_many :survey_questions, through: :survey_question_listings
+  accepts_nested_attributes_for :survey_question_listings, allow_destroy: true
+
+  def before_start?(survey_id)
+    survey = Survey.where('start_at >= ?', Date.current)
+                   .exists?(survey_id)
+  end
+
+  def answer_accepting?(survey_id)
+    survey = Survey.where('start_at <= ?', Date.current)
+                   .where('end_at >= ?', Date.current)
+                   .exists?(survey_id)
+  end
 end
