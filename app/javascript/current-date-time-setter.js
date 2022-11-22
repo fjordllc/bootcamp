e53229849fd71c7b635b00c9512d5a0130d695d1
pachikuriii <stakahashi0728@gmail.com>
2022-11-22 09:54:09 +0900

    currentDateTimeButtonがない場合、returnする形へ変更

--- app/javascript/current-date-time-setter.js
+++ app/javascript/current-date-time-setter.js
@@ -4,12 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const currentDateTimeButton = document.getElementById(
     'js-current-date-time-input-button'
   )
+  if (!currentDateTimeButton) return
   const dateTimeField = document.getElementById('date_time_input_field')
   function reflectDateTime() {
     const currentDateTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
     dateTimeField.value = currentDateTime
   }
-  if (currentDateTimeButton) {
-    currentDateTimeButton.addEventListener('click', reflectDateTime)
-  }
+  currentDateTimeButton.addEventListener('click', reflectDateTime)
 })
