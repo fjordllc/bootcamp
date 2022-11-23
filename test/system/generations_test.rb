7541826e764c6f5bee801160052e64e376216855
fuwa <fuwafuwa41.glr@gmail.com>
2022-11-23 11:56:34 +0900

    「全員」フィルターのテストを追加

--- test/system/generations_test.rb
+++ test/system/generations_test.rb
@@ -21,6 +21,28 @@ class GenerationsTest < ApplicationSystemTestCase
     assert_link "#{users(:kimura).generation}期生"
     assert_text '2014年01月01日 ~ 2014年03月31日'
     assert_equal '期生別ユーザー一覧 | FBC', title
-    assert_selector 'h1', text: '期一覧'
+  end
+
+  test 'no retired fileter when login whitout admin' do
+    visit_with_auth '/generations', 'kimura'
+    assert_no_text '退会'
+  end
+
+  test 'show all users each generation' do
+    visit_with_auth '/generations', 'komagata'
+
+    assert_selector('a.tab-nav__item-link.is-active', text: '全員')
+    assert_text '期生別（全員）'
+    within all('.a-user-icons__items')[-1]do
+      assert_equal first('.a-user-icons__item-icon.a-user-icon.is-admin')['title'], 'komagata (Komagata Masaki): 管理者、メンター'
+      assert_equal first('.a-user-icons__item-icon.a-user-icon.is-student')['title'], 'kimura (Kimura Tadasi)'
+      assert_equal first('.a-user-icons__item-icon.a-user-icon.is-trainee')['title'], 'kensyu (Kensyu Seiko)'
+      assert_equal first('.a-user-icons__item-icon.a-user-icon.is-adviser')['title'], 'advijirou (アドバイ 次郎): アドバイザー'
+      assert_equal first('.a-user-icons__item-icon.a-user-icon.is-graduate')['title'], 'sotugyou-with-job (卒業 就職済美)'
+      assert_equal all('.a-user-icons__item-icon.a-user-icon.is-mentor').last['title'], 'mentormentaro (メンタ 麺太郎): メンター'
+      all('.a-user-icons__item-icon.a-user-icon.is-student').each do |selector|
+        assert_not_equal selector['title'], 'yameo (辞目 辞目夫)'
+      end
+    end
   end
 end
