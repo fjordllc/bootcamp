988233477da87f84bcec17eb4f2fa753a3063053
OdenTakashi <odentakashidesu@icloud.com>
2022-12-06 15:45:47 +0900

    プラクティス一覧: プラクティス着手ユーザアイコンのHTML構造を変更

--- app/javascript/practice-user-icon.vue
+++ app/javascript/practice-user-icon.vue
@@ -1,10 +1,11 @@
 <template lang="pug">
 .a-user-icons__item
   a.a-user-icons__item-link(:href='startedStudent.user_link')
-    img(
-      :class='`a-user-icons__item-icon a-user-icon ${activeOrInactive} is-${startedStudent.primary_role}`',
-      :src='startedStudent.avatar_url',
-      :title='`${startedStudent.icon_title}`')
+    span(:class='`a-user-role is-${startedStudent.primary_role}`')
+      img(
+        :class='`a-user-icons__item-icon a-user-icon ${activeOrInactive}`',
+        :src='startedStudent.avatar_url',
+        :title='`${startedStudent.icon_title}`')
 </template>
 
 <script>
