9d04199f90355422d4a18f420ed7aa213461061f
teppei machida <machida@fjord.jp>
2022-12-07 14:48:50 +0900

    質問追加にChoices.jsが適用されるようにした

--- app/javascript/choices-ui.js
+++ app/javascript/choices-ui.js
@@ -31,20 +31,33 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  const elementAddedSelect = document.querySelector(
-    'js-choices-added-select'
-  )
-  if (elementAddedSelect) {
-    return new Choices(elementAddedSelect, {
-      removeItemButton: true,
+  const elements = document.querySelectorAll('.js-choices-added-select')
+  const elementsCount = elements.length
+  if (!elements) return
+  for (let i = 0; i < elementsCount; i++) {
+    // eslint-disable-next-line no-new
+    new Choices(elements[i], {
       allowHTML: true,
       searchResultLimit: 20,
       searchPlaceholderValue: '検索ワード',
       noResultsText: '一致する情報は見つかりません',
       itemSelectText: '選択',
-      shouldSort: false,
-      resetScrollPosition: false,
-      renderSelectedChoices: 'always'
+      shouldSort: false
     })
   }
+  $('.js-add-choice').on('cocoon:after-insert', () => {
+    const elements = document.querySelectorAll('.js-choices-added-select')
+    const element = elements[elements.length - 1]
+    if (element) {
+      return new Choices(element, {
+        allowHTML: true,
+        searchResultLimit: 20,
+        searchPlaceholderValue: '検索ワード',
+        noResultsText: '一致する情報は見つかりません',
+        itemSelectText: '選択',
+        shouldSort: false
+      })
+    }
+  })
+
 })
