6a065f442f4a44c47a7444c97df958490f6a6df1
OdenTakashi <odentakashidesu@icloud.com>
2022-12-08 16:00:02 +0900

    '見たよ'アイコンのHTML構造を変更

--- app/javascript/components/footprint.vue
+++ app/javascript/components/footprint.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 li.user-icons__item
   a.user-icons__item-link(:href='`${footprint.user.url}`')
-    img.a-user-icon.is-sm(
-      :title='footprint.user.icon_title',
-      :alt='footprint.user.icon_title',
-      :src='footprint.user.avatar_url',
-      :class='[loginNameClass, primaryRoleClass]')
+    span(:class='["a-user-role", primaryRoleClass]')
+      img.a-user-icon.is-sm(
+        :title='footprint.user.icon_title',
+        :alt='footprint.user.icon_title',
+        :src='footprint.user.avatar_url',
+        :class='loginNameClass')
 </template>
 <script>
 export default {
