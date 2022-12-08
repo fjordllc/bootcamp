bdeb9913d2713bc3600476d74aaba9c93d05528d
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-12-08 14:37:46 +0900

    react版のLoadingListPlaceholderの追加

---
+++ app/javascript/components/LoadingListPlaceholder.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function LoadingListPlaceholder() {
+  const itemCount = 8
+  return (
+    <div className="card-list a-card is-loading">
+      <ul>
+        {[...Array(itemCount)].map((_, index) => {
+          return (
+            <li key={index}>
+              <LoadingListItemPlaceholder />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+function LoadingListItemPlaceholder() {
+  return (
+    <div className="card-list-item">
+      <div className="card-list-item__inner">
+        <div className="card-list-item__user">
+          <div className="card-list-item__user-icon a-user-icon a-placeholder"></div>
+        </div>
+        <div className="card-list-item__rows">
+          <div className="card-list-item__row">
+            <div className="card-list-item-title a-placeholder"></div>
+          </div>
+          <div className="card-list-item__row">
+            <div className="card-list-item-meta__items a-placeholder">
+              <div className="card-list-item-meta__item"></div>
+              <div className="card-list-item-meta__item"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
