8baa435fca1667914061f87bb53185ec00270133
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-12-09 22:44:13 +0900

    管理者に対しても通知を送る

--- app/models/comment/after_create_callback.rb
+++ app/models/comment/after_create_callback.rb
@@ -87,12 +87,12 @@ class Comment::AfterCreateCallback
       next if comment.sender == admin_user
 
       commentable_path = Rails.application.routes.url_helpers.polymorphic_path(comment.commentable)
-      NotificationFacade.came_comment(
-        comment,
-        admin_user,
-        "#{comment.commentable.user.login_name}さんの相談部屋で#{comment.sender.login_name}さんからコメントが届きました。",
-        "#{commentable_path}#latest-comment"
-      )
+      ActivityDelivery.with(
+        comment: comment,
+        receiver: admin_user,
+        message: "#{comment.commentable.user.login_name}さんの相談部屋で#{comment.sender.login_name}さんからコメントが届きました。",
+        link: "#{commentable_path}#latest-comment"
+      ).notify(:came_comment)
     end
   end
 
