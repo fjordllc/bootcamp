b28b4bf24aa7cb2d011ace2e2a8f6a23176ff2db
peno022 <40317050+peno022@users.noreply.github.com>
2022-12-10 13:03:38 +0900

    articles_controllerにログイン認証のskip漏れがあったので追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@
 
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
+  skip_before_action :require_login, raise: false, only: %i[index show]
   before_action :require_admin_or_mentor_login, except: %i[index show]
 
   def index
