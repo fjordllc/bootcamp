ae6ae2340cc44152568a2ebd4a951ecc1bfaf350
TakashimaAyaka <takashimaayaka@MacBook-Pro-2.local>
2022-12-18 17:32:15 +0900

    lintに対応

--- app/javascript/survey.js
+++ app/javascript/survey.js
@@ -1,16 +1,15 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-  const Choices = Array.from(
-    document.querySelectorAll('.a-toggle-checkbox')
-  )
+  const Choices = Array.from(document.querySelectorAll('.a-toggle-checkbox'))
 
   Choices.forEach((choice) => {
     choice.addEventListener('click', () => {
-      const additionalQuestionField = document.getElementsByName(`js-additional_question_${choice.name}`)
-      if(
+      const additionalQuestionField = document.getElementsByName(
+        `js-additional_question_${choice.name}`
+      )
+      if (
         !choice.classList.contains('js-answer_required_choice') &&
         choice.type !== 'checkbox'
-        ) {
+      ) {
         additionalQuestionField[0].classList.add('is-hidden')
       }
     })
@@ -22,20 +21,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
   answerRequiredChoices.forEach((answerRequiredChoice) => {
     answerRequiredChoice.addEventListener('click', () => {
-      const additionalQuestionField = document.getElementsByName(`js-additional_question_${answerRequiredChoice.name}`)
-      if(answerRequiredChoice.checked) {
+      const additionalQuestionField = document.getElementsByName(
+        `js-additional_question_${answerRequiredChoice.name}`
+      )
+      if (answerRequiredChoice.checked) {
         additionalQuestionField[0].classList.remove('is-hidden')
-      } else if(!answerRequiredChoice.checked && answerRequiredChoice.type === 'checkbox') {
-        const choicesInSameQuestion = document.getElementsByName(answerRequiredChoice.name)
+      } else if (
+        !answerRequiredChoice.checked &&
+        answerRequiredChoice.type === 'checkbox'
+      ) {
+        const choicesInSameQuestion = document.getElementsByName(
+          answerRequiredChoice.name
+        )
         const checkBoxStatuses = []
         choicesInSameQuestion.forEach((ChoiceInSameQuestion) => {
-          if (ChoiceInSameQuestion.checked && ChoiceInSameQuestion.classList.contains('js-answer_required_choice')) {
+          if (
+            ChoiceInSameQuestion.checked &&
+            ChoiceInSameQuestion.classList.contains('js-answer_required_choice')
+          ) {
             checkBoxStatuses.push(true)
           } else {
             checkBoxStatuses.push(false)
           }
         })
-        if(checkBoxStatuses.includes(true)) {
+        if (checkBoxStatuses.includes(true)) {
           additionalQuestionField[0].classList.remove('is-hidden')
         } else {
           additionalQuestionField[0].classList.add('is-hidden')
@@ -44,4 +53,3 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
 })
-
