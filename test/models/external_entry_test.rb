c612684539ed925f297f23a1de006f093e1631a4
siso <siso25@zoho.com>
2022-12-20 20:56:18 +0900

    デフォルト画像が変更になったため、モデルテストを修正

--- test/models/external_entry_test.rb
+++ test/models/external_entry_test.rb
@@ -5,6 +5,6 @@ require 'test_helper'
 class ExternalEntryTest < ActiveSupport::TestCase
   test 'thumbnail_url' do
     external_entry = external_entries(:external_entry1)
-    assert_equal '/images/external_entries/thumbnails/default.png', external_entry.thumbnail_url
+    assert_equal '/images/external_entries/thumbnails/blank.svg', external_entry.thumbnail_url
   end
 end
