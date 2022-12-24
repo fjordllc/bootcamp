07ef6a919a70ca66a429ad65b463d8b2e7365b29
siso <siso25@zoho.com>
2022-12-24 19:20:18 +0900

    ブログのサムネイル画像を表示時に外部から取得するよう変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_19_111746) do
+ActiveRecord::Schema.define(version: 2022_12_24_091715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -230,6 +230,7 @@ ActiveRecord::Schema.define(version: 2022_12_19_111746) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "thumbnail_image_url"
     t.index ["user_id"], name: "index_external_entries_on_user_id"
   end
 
