aa27231dcb5e8d47dfa5080e4a459d2d397310d5
siso <siso25@zoho.com>
2022-12-24 19:20:18 +0900

    ブログのサムネイル画像を表示時に外部から取得するよう変更

--- app/controllers/api/external_entries_controller.rb
+++ app/controllers/api/external_entries_controller.rb
@@ -2,6 +2,6 @@
 
 class API::ExternalEntriesController < API::BaseController
   def index
-    @external_entries = ExternalEntry.with_attached_thumbnail.order(published_at: :desc).page(params[:page])
+    @external_entries = ExternalEntry.all.order(published_at: :desc).page(params[:page])
   end
 end
--- app/models/external_entry.rb
+++ app/models/external_entry.rb
@@ -3,8 +3,6 @@
 class ExternalEntry < ApplicationRecord
   include ActionView::Helpers::AssetUrlHelper
 
-  THUMBNAIL_SIZE = '88x88>'
-  has_one_attached :thumbnail
   belongs_to :user
 
   validates :title, presence: true
@@ -12,10 +10,6 @@ class ExternalEntry < ApplicationRecord
   validates :published_at, presence: true
 
   def thumbnail_url
-    if thumbnail.attached?
-      thumbnail.variant(resize: THUMBNAIL_SIZE).processed.url
-    else
-      image_url('/images/external_entries/thumbnails/blank.svg')
-    end
+    thumbnail_image_url.presence || image_url('/images/external_entries/thumbnails/blank.svg')
   end
 end
