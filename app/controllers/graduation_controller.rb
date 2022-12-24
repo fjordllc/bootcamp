a2ff252ed5a6fb6d51474d03565d3b90cb5e4f2c
komagata <komagata@gmail.com>
2022-12-24 19:29:09 +0900

    Discordの卒業通知がたくさん飛ぶのを修正した

--- app/controllers/graduation_controller.rb
+++ app/controllers/graduation_controller.rb
@@ -7,8 +7,7 @@ class GraduationController < ApplicationController
   def update
     if @user.update(graduated_on: Date.current)
       Subscription.new.destroy(@user.subscription_id) if @user.subscription_id
-
-      notify_to_mentors(@user)
+      Newspaper.publish(:graduation_update, @user)
       redirect_to @redirect_url, notice: 'ユーザー情報を更新しました。'
     else
       redirect_to @redirect_url, alert: 'ユーザー情報の更新に失敗しました'
@@ -23,11 +22,5 @@ class GraduationController < ApplicationController
 
   def set_redirect_url
     @redirect_url = params[:redirect_url].presence || admin_users_url
-  end
-
-  def notify_to_mentors(user)
-    User.mentor.each do |mentor|
-      ActivityDelivery.with(sender: user, receiver: mentor).notify(:graduated)
-    end
   end
 end
