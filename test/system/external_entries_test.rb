bb22155cc60a7799437e2208bf6a5d84068ff6bc
siso <siso25@zoho.com>
2022-12-26 21:55:17 +0900

    みんなのブログでブログ記事が重複して登録されないことのテストを追加

--- test/system/external_entries_test.rb
+++ test/system/external_entries_test.rb
@@ -11,9 +11,15 @@ class ExternalEntriesTest < ApplicationSystemTestCase
 
   test 'fetch and save rss feeds' do
     external_entry_count_before_daily = ExternalEntry.count
+
     VCR.use_cassette 'external_entry/fetch', vcr_options do
       visit_with_auth '/scheduler/daily', 'komagata'
+      assert_equal external_entry_count_before_daily + 1, ExternalEntry.count
     end
-    assert_equal external_entry_count_before_daily + 1, ExternalEntry.count
+
+    VCR.use_cassette 'external_entry/fetch', vcr_options do
+      visit_with_auth '/scheduler/daily', 'komagata'
+      assert_equal external_entry_count_before_daily + 1, ExternalEntry.count
+    end
   end
 end
