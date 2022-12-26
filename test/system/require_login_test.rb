c667e9a4f07f17b326f228d7992d9e0d79e4e6d2
peno022 <40317050+peno022@users.noreply.github.com>
2022-12-26 14:26:20 +0900

    ログイン必須ページのテストを追加

--- test/system/require_login_test.rb
+++ test/system/require_login_test.rb
@@ -45,4 +45,73 @@ class RequireLoginTest < ApplicationSystemTestCase
     visit '/pages'
     assert_text 'ログインしてください'
   end
+
+  test 'announcements_path' do
+    visit '/announcements'
+    assert_text 'ログインしてください'
+  end
+
+  test 'books_path' do
+    visit '/books'
+    assert_text 'ログインしてください'
+  end
+
+  test 'company_products_path' do
+    company = companies(:company1)
+    visit "/companies/#{company.id}/products"
+    assert_text 'ログインしてください'
+  end
+
+  test 'company_reports_path' do
+    company = companies(:company1)
+    visit "/companies/#{company.id}/reports"
+    assert_text 'ログインしてください'
+  end
+
+  test 'company_users_path' do
+    company = companies(:company1)
+    visit "/companies/#{company.id}/users"
+    assert_text 'ログインしてください'
+  end
+
+  test 'companies_path' do
+    visit '/companies'
+    assert_text 'ログインしてください'
+  end
+
+  test 'course_practices_path' do
+    course = courses(:course1)
+    visit "/courses/#{course.id}/practices"
+    assert_text 'ログインしてください'
+  end
+
+  test 'courses_path' do
+    visit '/courses'
+    assert_text 'ログインしてください'
+  end
+
+  test 'current_user_bookmarks_path' do
+    visit '/current_user/bookmarks'
+    assert_text 'ログインしてください'
+  end
+
+  test 'edit_current_user_password_path' do
+    visit '/current_user/password/edit'
+    assert_text 'ログインしてください'
+  end
+
+  test 'current_user_products_path' do
+    visit '/current_user/products'
+    assert_text 'ログインしてください'
+  end
+
+  test 'current_user_reports_path' do
+    visit '/current_user/reports'
+    assert_text 'ログインしてください'
+  end
+
+  test 'edit_current_user_path' do
+    visit '/current_user/edit'
+    assert_text 'ログインしてください'
+  end
 end
