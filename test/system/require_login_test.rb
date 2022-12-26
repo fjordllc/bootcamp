5248f1a8b9f5b9e26cf40e30f67bf9abbb7fd4fd
peno022 <40317050+peno022@users.noreply.github.com>
2022-12-26 14:25:52 +0900

    test/system/require_login_test.rb と test/system/login_requirements_test.rb をまとめる

--- test/system/require_login_test.rb
+++ test/system/require_login_test.rb
@@ -3,6 +3,23 @@
 require 'application_system_test_case'
 
 class RequireLoginTest < ApplicationSystemTestCase
+  setup { @user_id = users(:hatsuno).id }
+
+  test 'show listing user\'s comments' do
+    visit "/users/#{@user_id}/comments"
+    assert_text 'ログインしてください'
+  end
+
+  test 'show listing user\'s products' do
+    visit "/users/#{@user_id}/products"
+    assert_text 'ログインしてください'
+  end
+
+  test 'show listing user\'s reports' do
+    visit "/users/#{@user_id}/reports"
+    assert_text 'ログインしてください'
+  end
+
   test 'users_path' do
     visit '/users'
     assert_text 'ログインしてください'
