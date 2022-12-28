80ab870bf0c329831ccc0be7547e692618ba52cb
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-12-28 22:37:19 +0900

    コンフリクトの解消

--- test/mailers/previews/activity_mailer_preview.rb
+++ test/mailers/previews/activity_mailer_preview.rb
@@ -3,6 +3,16 @@
 require 'active_record/fixtures'
 
 class ActivityMailerPreview < ActionMailer::Preview
+  def came_comment
+    comment = Comment.find(ActiveRecord::FixtureSet.identify(:commentOfTalk))
+
+    ActivityMailer.with(
+      comment: comment,
+      message: "相談部屋で#{comment.sender.login_name}さんからコメントがありました。",
+      receiver: comment.receiver
+    ).came_comment
+  end
+
   def graduated
     sender = User.find(ActiveRecord::FixtureSet.identify(:sotugyou))
     receiver = User.find(ActiveRecord::FixtureSet.identify(:mentormentaro))
