51c4b486c19ab313d4e8db661f2b653a751a8b6b
komagata <komagata@gmail.com>
2023-01-05 13:51:11 +0900

    先に卒業通知メールが送られた場合にエラーがになる問題を修正
    サイト内通知より先にメール通知が送られるとnotification_pathが決まらない
    のでメール文がエラーになる問題を修正。

--- app/mailers/activity_mailer.rb
+++ app/mailers/activity_mailer.rb
@@ -2,6 +2,7 @@
 
 class ActivityMailer < ApplicationMailer
   helper ApplicationHelper
+  include Rails.application.routes.url_helpers
 
   before_action do
     @sender = params[:sender] if params&.key?(:sender)
@@ -14,7 +15,10 @@ class ActivityMailer < ApplicationMailer
     @receiver ||= args[:receiver]
 
     @user = @receiver
-    @notification = @user.notifications.find_by(link: "/users/#{@sender.id}", kind: Notification.kinds[:graduated])
+    @link_url = notification_redirector_path(
+      link: "/users/#{@sender.id}",
+      kind: Notification.kinds[:graduated]
+    )
     subject = "[FBC] #{@sender.login_name}さんが卒業しました。"
     mail to: @user.email, subject: subject
   end
