b60639f4f3ac87082957a789faa2ad5b2e855b83
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-01-12 15:52:09 +0900

    unconfirmed_link.vue の react 版コンポーネントを追加した

---
+++ app/javascript/components/UnconfirmedLink.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default class UnconfirmedLink extends React.Component {
+  openUnconfirmedItems() {
+    const links = document.querySelectorAll(
+      '.card-list-item .js-unconfirmed-link'
+    )
+    links.forEach((link) => {
+      window.open(link.href, '_target', 'noopener')
+    })
+  }
+
+  render() {
+    return (
+      <div className="card-footer">
+        <div className="card-main-actions">
+          <ul className="card-main-actions__items">
+            <li className="card-main-actions__item">
+              <button
+                className="thread-unconfirmed-links-form__action a-button is-block is-sm is-secondary"
+                onClick={() => this.openUnconfirmedItems()}>
+                {this.props.label}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
