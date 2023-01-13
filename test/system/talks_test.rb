fe81bdf1abb85e3514cccfcdb7cb596bf286212c
lef237 <lef237k5@gmail.com>
2023-01-13 16:51:33 +0900

    テストが通らないところを修正した

--- test/system/talks_test.rb
+++ test/system/talks_test.rb
@@ -64,7 +64,7 @@ class TalksTest < ApplicationSystemTestCase
   test 'non-admin user can access their own talk page' do
     user = users(:kimura)
     visit_with_auth "/talks/#{user.talk.id}", 'kimura'
-    assert_selector '.page-header__title', text: '相談部屋'
+    assert_selector '.page-header__title', text: 'kimura'
   end
 
   test 'a talk room is removed from unreplied tab when admin comments there' do
