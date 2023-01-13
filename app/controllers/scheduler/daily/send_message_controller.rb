e27888a6fd6d2373782bb6e40acd958aa0e9f038
ksmxxxxxx <kankichi69@gmail.com>
2023-01-13 21:27:02 +0900

    送信済みを保存する時バリデーションにかかり例外が発生していた

--- app/controllers/scheduler/daily/send_message_controller.rb
+++ app/controllers/scheduler/daily/send_message_controller.rb
@@ -19,7 +19,8 @@ class Scheduler::Daily::SendMessageController < SchedulerController
         commentable_id: Talk.find_by(user_id: student.id).id,
         commentable_type: 'Talk'
       )
-      student.update(sent_message_after_thirty_days: true)
+      student.assign_attributes(sent_message_after_thirty_days: true)
+      student.save(validate: false)
     end
   end
 end
