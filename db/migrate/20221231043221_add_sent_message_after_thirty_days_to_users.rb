5ced5276c2041a33a2cca1716ed82c0afc5f2793
ksmxxxxxx <kankichi69@gmail.com>
2023-01-13 16:47:10 +0900

    カラム名のtypoを修正

--- db/migrate/20221231043221_add_sent_message_after_thirty_days_to_users.rb
+++ db/migrate/20221231043221_add_sent_message_after_thirty_days_to_users.rb
@@ -1,5 +1,5 @@
 class AddSentMessageAfterThirtyDaysToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :sent_message_after_thrity_days, :boolean, default: false
+    add_column :users, :sent_message_after_thirty_days, :boolean, default: false
   end
 end
