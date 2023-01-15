08201b7bb98d18949d7f9853351ff86ac94cb66b
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-01-15 20:56:55 +0900

    unconfirmed_link.vue の react 版を推奨される関数コンポーネントに変更した

--- app/javascript/components/UnconfirmedLink.jsx
+++ app/javascript/components/UnconfirmedLink.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
-export default class UnconfirmedLink extends React.Component {
-  openUnconfirmedItems() {
+export default function UnconfirmedLink({ label }) {
+  const openUnconfirmedItems = useCallback(() => {
     const links = document.querySelectorAll(
       '.card-list-item .js-unconfirmed-link'
     )
     links.forEach((link) => {
       window.open(link.href, '_target', 'noopener')
     })
-  }
+  }, [])
 
-  render() {
-    return (
-      <div className="card-footer">
-        <div className="card-main-actions">
-          <ul className="card-main-actions__items">
-            <li className="card-main-actions__item">
-              <button
-                className="thread-unconfirmed-links-form__action a-button is-block is-sm is-secondary"
-                onClick={() => this.openUnconfirmedItems()}>
-                {this.props.label}
-              </button>
-            </li>
-          </ul>
-        </div>
+  return (
+    <div className="card-footer">
+      <div className="card-main-actions">
+        <ul className="card-main-actions__items">
+          <li className="card-main-actions__item">
+            <button
+              className="thread-unconfirmed-links-form__action a-button is-sm is-block is-secondary"
+              onClick={openUnconfirmedItems}>
+              {label}
+            </button>
+          </li>
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
