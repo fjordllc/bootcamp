8f05ae47b2623c6a7a1c32414a82e22d817c6733
sadanora <sadanora.pi@gmail.com>
2023-01-19 11:13:30 +0900

    フィルタのアクションの指定漏れを修正

--- app/controllers/hibernation_controller.rb
+++ app/controllers/hibernation_controller.rb
@@ -2,8 +2,8 @@
 
 class HibernationController < ApplicationController
   skip_before_action :require_login, raise: false, only: %i[show]
-  skip_before_action :refuse_retired_login, raise: false
-  skip_before_action :refuse_hibernated_login, raise: false
+  skip_before_action :refuse_retired_login, raise: false, only: %i[show]
+  skip_before_action :refuse_hibernated_login, raise: false, only: %i[show]
 
   def show; end
 
