d3bd43fbfb37848995c2ca7f733a51093ef3e3e3
ksmxxxxxx <kankichi69@gmail.com>
2023-01-21 15:44:18 +0900

    メソッド名とカラム名を汎用的な命名に変更

--- db/data/20230114032018_add_sent_student_followup_message_to_users.rb
+++ db/data/20230114032018_add_sent_student_followup_message_to_users.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class AddSentMessageAfterThirtyDaysToUser < ActiveRecord::Migration[6.1]
+class AddSentStudentFollowupMessageToUsers < ActiveRecord::Migration[6.1]
   def up
     User.where('created_at <= ?', Time.current.ago(31.days)).or(User.where.not(hibernated_at: nil)).find_each do |user|
-      user.assign_attributes(sent_message_after_thirty_days: true)
+      user.assign_attributes(sent_student_followup_message: true)
       user.save(validate: false)
     end
   end
--- db/migrate/20221231043221_add_sent_message_after_thirty_days_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSentMessageAfterThirtyDaysToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :sent_message_after_thirty_days, :boolean, default: false
-  end
-end
