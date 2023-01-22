a84a2facda692581830c320578231de1e99b9b1b
akingo55 <am1157154@gmail.com>
2023-01-22 23:39:53 +0900

    管理者とメンターに2回DiscordNotifier.graduatedを呼ぶように修正

--- app/models/discord_driver.rb
+++ app/models/discord_driver.rb
@@ -2,21 +2,10 @@
 
 class DiscordDriver
   def call(params)
-    if params[:webhook_url]
-      Discord::Notifier.message(
-        params[:body],
-        username: params[:name],
-        url: params[:webhook_url]
-      )
-    elsif params[:admin_webhook_url] && params[:mentor_webhook_url]
-      webhook_urls = [params[:admin_webhook_url], params[:mentor_webhook_url]]
-      webhook_urls.each do |url|
-        Discord::Notifier.message(
-          params[:body],
-          username: params[:name],
-          url: url
-        )
-      end
-    end
+    Discord::Notifier.message(
+      params[:body],
+      username: params[:name],
+      url: params[:webhook_url]
+    )
   end
 end
