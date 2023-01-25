a6ee4ccbac5e318553e52407fd4bbbb39dc00e7d
akingo55 <am1157154@gmail.com>
2023-01-25 21:21:53 +0900

    graduation_notifierのリファクタリング

--- app/models/graduation_notifier.rb
+++ app/models/graduation_notifier.rb
@@ -6,10 +6,14 @@ class GraduationNotifier
       ActivityDelivery.with(sender: user, receiver: mentor).notify(:graduated)
     end
 
-    admin_webhook_url = Rails.application.secrets[:webhook][:admin]
-    DiscordNotifier.graduated(sender: user, webhook_url: admin_webhook_url).notify_now
+    DiscordNotifier.graduated(
+      sender: user,
+      webhook_url: Rails.application.secrets[:webhook][:admin]
+    ).notify_now
 
-    mentor_webhook_url = Rails.application.secrets[:webhook][:mentor]
-    DiscordNotifier.graduated(sender: user, webhook_url: mentor_webhook_url).notify_now
+    DiscordNotifier.graduated(
+      sender: user,
+      webhook_url: Rails.application.secrets[:webhook][:mentor]
+    ).notify_now
   end
 end
