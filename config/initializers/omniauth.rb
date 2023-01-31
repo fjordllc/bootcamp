4d6113f0b66bcdb30a652a407c9263840ebc2d80
komagata <komagata@gmail.com>
2023-01-31 17:06:39 +0900

    githubサインアップができない問題を修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
+  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: 'user:email'
 end
