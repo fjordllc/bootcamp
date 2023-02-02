c438ac0e72ccf5c2099ccb1e108ece7c3f747c52
ksmxxxxxx <kankichi69@gmail.com>
2023-02-02 21:54:24 +0900

    一時対応として一度でも休会した人にはメッセージは送信しない

--- app/controllers/scheduler/daily/send_message_controller.rb
+++ app/controllers/scheduler/daily/send_message_controller.rb
@@ -2,11 +2,21 @@
 
 class Scheduler::Daily::SendMessageController < SchedulerController
   def show
+    mark_message_as_sent_for_hibernated_student
     sent_student_followup_message
     head :ok
   end
 
   private
+
+  # FIXME: 一次対応として一回でも休会している受講生にはメッセージ送信済みとする
+  #        別Issueで入会n日目、休会開けn日目目の受講生にメッセージを送信する方針へ改修してほしい
+  #        改修後、このメソッドは不要になると思われるので削除すること
+  def mark_message_as_sent_for_hibernated_student
+    User.find_each do |user|
+      user.update!(sent_student_followup_message: true) if user.hibernated?
+    end
+  end
 
   def sent_student_followup_message
     @komagata = User.find_by(login_name: 'komagata')
