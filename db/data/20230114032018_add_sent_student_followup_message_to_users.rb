144c9177fdcadd1d5d90cfcd6998ea91941224fc
ksmxxxxxx <kankichi69@gmail.com>
2023-02-03 14:43:30 +0900

    特定のコンテキストでvalidateをスキップしてDBに値を保存する

--- db/data/20230114032018_add_sent_student_followup_message_to_users.rb
+++ db/data/20230114032018_add_sent_student_followup_message_to_users.rb
@@ -3,7 +3,8 @@
 class AddSentStudentFollowupMessageToUsers < ActiveRecord::Migration[6.1]
   def up
     User.where('created_at <= ?', Time.current.ago(31.days)).or(User.where.not(hibernated_at: nil)).find_each do |user|
-      user.update!(sent_student_followup_message: true)
+      user.assign_attributes(sent_student_followup_message: true)
+      user.save(context: :followup_message)
     end
   end
 
