1f9143f505a7d781fe8105f80b534ca954ab4eb6
tominaga <aurora25sh.50nonono@gmail.com>
2023-02-03 18:31:17 +0900

    cookieに保存するキーをイベントの種類ごとに指定するよう変更した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -63,7 +63,7 @@ class HomeController < ApplicationController
   end
 
   def display_regular_events_on_dashboard
-    cookies_ids = JSON.parse(cookies[:confirmed_event_ids]) if cookies[:confirmed_event_ids]
+    cookies_ids = JSON.parse(cookies[:confirmed_regular_event_ids]) if cookies[:confirmed_regular_event_ids]
     regular_events_comming_soon = RegularEvent.today_events + RegularEvent.tomorrow_events
     @regular_events_comming_soon = regular_events_comming_soon.select { |event| event.participated_by?(current_user) }
 
