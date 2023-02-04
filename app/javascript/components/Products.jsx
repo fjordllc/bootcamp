e8778e86c34acc20885a5bc2425d2763431cdc82
lef237 <lef237k5@gmail.com>
2023-02-04 23:16:01 +0900

    ProductsとProductに分割して拡張性を高めた

--- app/javascript/components/Products.jsx
+++ app/javascript/components/Products.jsx
@@ -3,7 +3,7 @@ import queryString from 'query-string'
 import useSWR from 'swr'
 import Pagination from './Pagination'
 import LoadingListPlaceholder from './LoadingListPlaceholder'
-import UserIcon from './UserIcon'
+import Product from './Product'
 import fetcher from '../fetcher'
 
 export default function Products() {
@@ -66,59 +66,5 @@ export default function Products() {
         )}
       </div>
     </div>
-  )
-}
-
-function Product({ product }) {
-  return (
-    <li className="card-list-item">
-      <div className="card-list-item__inner">
-        <div className="card-list-item__user">
-          <UserIcon
-            user={product.user}
-            blockClassSuffix='card-list-item'
-          />
-        </div>
-      <div className="card-list-item__rows">
-        <div className="card-list-item__row">
-          <div className="card-list-item-title">
-            {product.wip ? (
-              <div className="a-list-item-badge is-wip">
-                <span>WIP</span>
-              </div>
-            ) : product.ended && (
-              <div className="a-list-item-badge is-ended">
-                <span>終了</span>
-              </div>
-            )}
-              <h2 className="card-list-item-title__title" itemProp="name">
-                <a className="card-list-item-title__link a-text-link" href={product.url} itemProp="url">
-                  { product.practice.title }の提出物
-                </a>
-              </h2>
-            </div>
-          </div>
-        <div className="card-list-item__row">
-          <a className="a-user-name" href={product.user.url}>
-            { product.user.long_name }
-          </a>
-        </div>
-          <div className="card-list-item__row">
-            <div className="card-list-item-meta">
-              <div className="card-list-item-meta__items">
-                <div className="card-list-item-meta__item"><time className="a-meta" dateTime={product.created_at}>
-                  <span className="a-meta__label">提出</span><span className="a-meta__value">{ product.created_at }</span>
-                  </time>
-                </div>
-                <div className="card-list-item-meta__item"><time className="a-meta" dateTime={product.updated_at}>
-                  <span className="a-meta__label">更新</span><span className="a-meta__value">{ product.updated_at }</span>
-                  </time>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
   )
 }
