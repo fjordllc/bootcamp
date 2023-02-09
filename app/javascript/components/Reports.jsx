460d06de558bc639ed97fd538fa81bf823f8cbd6
peno022 <40317050+peno022@users.noreply.github.com>
2023-02-09 10:19:49 +0900

    ドロップダウンを別ファイルのコンポーネントに切り出す

--- app/javascript/components/Reports.jsx
+++ app/javascript/components/Reports.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import useSWR from 'swr'
 import queryString from 'query-string'
 import fetcher from '../fetcher'
 import LoadingListPlaceholder from './LoadingListPlaceholder'
 import Report from './Report'
 import Pagination from './Pagination'
-import Choices from "choices.js";
+import PracticeFilterDropdown from './PracticeFilterDropdown'
 
 export default function Reports({user, currentUser, practices}) {
   const per = 20
@@ -37,7 +37,7 @@ export default function Reports({user, currentUser, practices}) {
     <>
       {(data.totalPages === 0) && (
         <div className="container is-md">
-          <DropDown
+          <PracticeFilterDropdown
             practices={practices}
             setPracticeId={setPracticeId}
             practiceId={practiceId}
@@ -47,47 +47,46 @@ export default function Reports({user, currentUser, practices}) {
       )}
       {(data.totalPages > 0) && (
         <div className="container is-md">
-        <DropDown
-          practices={practices}
-          setPracticeId={setPracticeId}
-          practiceId={practiceId}
-        />
-        <div className="page-content reports">
-          {data.totalPages > 1 && (
-            <Pagination
-              sum={data.totalPages * per}
-              per={per}
-              neighbours={neighbours}
-              page={page}
-              onChange={e => setPage(e.page)}
-            />
-          )}
-          <ul className="card-list a-card">
-            <div className="card-list__items">
-              {data.reports.map((report) => {
-                return (
-                  <Report
-                    key={report.id}
-                    report={report}
-                    currentUser={currentUser}
-                  />
-                )
-              })}
-            </div>
-          </ul>
-          {data.totalPages > 1 && (
-            <Pagination
-              sum={data.totalPages * per}
-              per={per}
-              neighbours={neighbours}
-              page={page}
-              onChange={e => setPage(e.page)}
-            />
-          )}
+          <PracticeFilterDropdown
+            practices={practices}
+            setPracticeId={setPracticeId}
+            practiceId={practiceId}
+          />
+          <div className="page-content reports">
+            {data.totalPages > 1 && (
+              <Pagination
+                sum={data.totalPages * per}
+                per={per}
+                neighbours={neighbours}
+                page={page}
+                onChange={(e) => setPage(e.page)}
+              />
+            )}
+            <ul className="card-list a-card">
+              <div className="card-list__items">
+                {data.reports.map((report) => {
+                  return (
+                    <Report
+                      key={report.id}
+                      report={report}
+                      currentUser={currentUser}
+                    />
+                  )
+                })}
+              </div>
+            </ul>
+            {data.totalPages > 1 && (
+              <Pagination
+                sum={data.totalPages * per}
+                per={per}
+                neighbours={neighbours}
+                page={page}
+                onChange={(e) => setPage(e.page)}
+              />
+            )}
+          </div>
         </div>
-      </div>
-      )
-      }
+      )}
     </>
   )
 }
@@ -100,62 +99,5 @@ const NoReports = () => {
         <p className="o-empty-message__text">日報はまだありません。</p>
       </div>
     </div>
-  )
-}
-
-const DropDown = ({practices, setPracticeId, practiceId}) => {
-  const [selectedId, setSelectedId] = useState(practiceId)
-
-  const onChange = (event) => {
-    const value = event.target.value
-    setSelectedId(value)
-    setPracticeId(value)
-  };
-
-  const selectRef = useRef(null);
-
-  useEffect(() => {
-    const selectElement = selectRef.current;
-    const choicesInstance = new Choices(selectElement, {
-      searchEnabled: true,
-      allowHTML: true,
-      searchResultLimit: 20,
-      searchPlaceholderValue: '検索ワード',
-      noResultsText: '一致する情報は見つかりません',
-      itemSelectText: '選択',
-      shouldSort: false
-    });
-
-    return () => {
-      choicesInstance.destroy();
-    };
-  }, []);
-
-  return (
-    <>
-      <nav className="page-filter form">
-        <div className="container is-md">
-          <div className="form-item is-inline-md-up">
-            <label className="a-form-label" htmlFor='js-choices-single-select'>プラクティスで絞り込む</label>
-            <select
-              className="a-form-select"
-              onChange={onChange}
-              ref={selectRef}
-              value={selectedId}
-              id='js-choices-single-select'
-            >
-              <option key="" value="">全ての日報を表示</option>
-              {practices.map((practice) => {
-                return (
-                  <option key={practice.id} value={practice.id}>
-                    {practice.title}
-                  </option>
-                )
-              })}
-            </select>
-          </div>
-        </div>
-      </nav>
-    </>
   )
 }
