ac88ae08233ee43641f2c6af7f097c5d79d70d6c
peno022 <40317050+peno022@users.noreply.github.com>
2023-02-09 10:17:13 +0900

    lint

--- app/javascript/components/Report.jsx
+++ app/javascript/components/Report.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ListComment from './ListComment'
 
-export default function Report({report, currentUser}){
+export default function Report({ report, currentUser }) {
   return (
-    <div className={`card-list-item ${report.wip ? "is-wip" : ""}`}>
+    <div className={`card-list-item ${report.wip ? 'is-wip' : ''}`}>
       <div className="card-list-item__inner">
         <div className="card-list-item__user">
           <a href={report.user.url} className="card-list-item__user-link">
-            <span className="[&quot;a-user-role&quot;, roleClass]">
-              <img className="card-list-item__user-icon a-user-icon" src={report.user.avatar_url} title={report.user.login_name} alt={report.user.login_name} />
+            <span className='["a-user-role", roleClass]'>
+              <img
+                className="card-list-item__user-icon a-user-icon"
+                src={report.user.avatar_url}
+                title={report.user.login_name}
+                alt={report.user.login_name}
+              />
             </span>
           </a>
         </div>
@@ -22,12 +27,18 @@ export default function Report({report, currentUser}){
                   </div>
                 )}
                 <h2 className="card-list-item-title__title">
-                  <a className="card-list-item-title__link a-text-link js-unconfirmed-link" href={report.url}>
-                    <img className="card-list-item-title__emotion-image" src={`/images/emotion/${report.emotion}.svg`} alt={report.emotion} />
+                  <a
+                    className="card-list-item-title__link a-text-link js-unconfirmed-link"
+                    href={report.url}>
+                    <img
+                      className="card-list-item-title__emotion-image"
+                      src={`/images/emotion/${report.emotion}.svg`}
+                      alt={report.emotion}
+                    />
                     {report.title}
                   </a>
                 </h2>
-                {(currentUser.id === report.user.id) && (
+                {currentUser.id === report.user.id && (
                   <ReportListItemActions report={report} />
                 )}
               </div>
@@ -38,11 +49,11 @@ export default function Report({report, currentUser}){
               <div className="card-list-item-meta__items">
                 <div className="card-list-item-meta__item">
                   <a className="a-user-name" href={report.user.url}>
-                    { report.user.long_name }
+                    {report.user.long_name}
                   </a>
                 </div>
                 <div className="card-list-item-meta__item">
-                  <time className="a-meta">{ `${report.reportedOn}の日報` }</time>
+                  <time className="a-meta">{`${report.reportedOn}の日報`}</time>
                 </div>
               </div>
             </div>
@@ -52,9 +63,11 @@ export default function Report({report, currentUser}){
         {report.hascheck && (
           <div className="stamp stamp-approve">
             <h2 className="stamp__content is-title">確認済</h2>
-            <time className="stamp__content is-created-at">{ report.checkDate }</time>
+            <time className="stamp__content is-created-at">
+              {report.checkDate}
+            </time>
             <div className="stamp__content is-user-name">
-              <div className="stamp__content-inner">{ report.checkUserName }</div>
+              <div className="stamp__content-inner">{report.checkUserName}</div>
             </div>
           </div>
         )}
@@ -66,18 +79,23 @@ export default function Report({report, currentUser}){
 const ReportListItemActions = ({ report }) => {
   return (
     <div className="card-list-item-title__end">
-      <label className="card-list-item-actions__trigger" htmlFor={report.id}></label>
+      <label className="card-list-item-actions__trigger" htmlFor={report.id}>
+      </label>
       <div className="card-list-item-actions">
         <input className="a-toggle-checkbox" type="checkbox" id={report.id} />
         <div className="card-list-item-actions__inner">
           <ul>
             <li className="card-list-item-actions__item">
-              <a className="card-list-item-actions__action" href={report.editURL}>
+              <a
+                className="card-list-item-actions__action"
+                href={report.editURL}>
                 <i className="fa-solid fa-pen">内容変更</i>
               </a>
             </li>
             <li className="card-list-item-actions__item">
-              <a className="card-list-item-actions__action" href={report.newURL}>
+              <a
+                className="card-list-item-actions__action"
+                href={report.newURL}>
                 <i className="fa-solid fa-copy">コピー</i>
               </a>
             </li>
