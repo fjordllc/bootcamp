40c4567c447068974074e18ba1194eba14a32f5b
lef237 <lef237k5@gmail.com>
2023-02-11 19:56:45 +0900

    Linterの指摘を受けた箇所を修正した。テストの位置を移動させた。

--- test/system/admin/campaigns_test.rb
+++ test/system/admin/campaigns_test.rb
@@ -181,4 +181,11 @@ class CampaignsTest < ApplicationSystemTestCase
     visit_with_auth '/', 'enchoowata'
     assert_text '入会から3日間（72時間）は機能制限なくフルでフィヨルドブートキャンプを活用いただけます'
   end
+
+  test 'check the texts of campaigns' do
+    visit_with_auth admin_campaigns_path, 'komagata'
+    assert_text '入会者'
+    assert_text '現役生'
+    assert_text '退会者'
+  end
 end
