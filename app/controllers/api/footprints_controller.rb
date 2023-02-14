0b5024bff897b0139aec0f1e605305f4fe3e44ce
akingo55 <am1157154@gmail.com>
2023-02-14 23:17:19 +0900

    お知らせテストの安定性向上

--- app/controllers/api/footprints_controller.rb
+++ app/controllers/api/footprints_controller.rb
@@ -2,9 +2,10 @@
 
 class API::FootprintsController < API::BaseController
   def index
-    if params[:footprintable_type].present?
-      footprintable.footprints.create_or_find_by(user: current_user) if footprintable.user != current_user
-      @footprints = footprintable.footprints.with_avatar.order(created_at: :desc)
+    footprintable_data = footprintable
+    if params[:footprintable_type].present? && footprintable_data.present?
+      footprintable_data.footprints.create_or_find_by(user: current_user) if footprintable_data.user != current_user
+      @footprints = footprintable_data.footprints.with_avatar.order(created_at: :desc)
       @footprint_total_count = @footprints.size
     else
       head :bad_request
@@ -14,6 +15,6 @@ class API::FootprintsController < API::BaseController
   private
 
   def footprintable
-    params[:footprintable_type].constantize.find(params[:footprintable_id])
+    params[:footprintable_type].constantize.find_by(id: params[:footprintable_id])
   end
 end
