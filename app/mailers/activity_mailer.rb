df6c216ccd87f7b297a0e742a3806f869898e8da
akingo55 <am1157154@gmail.com>
2023-02-15 11:56:01 +0900

    コンフリクト修正漏れを直す

--- app/mailers/activity_mailer.rb
+++ app/mailers/activity_mailer.rb
@@ -38,7 +38,7 @@ class ActivityMailer < ApplicationMailer
 
     return false unless @receiver.mail_notification? # cancel sending email
 
-     @user = @receiver
+    @user = @receiver
     @link_url = notification_redirector_url(
       link: "/users/#{@sender.id}",
       kind: Notification.kinds[:graduated]
