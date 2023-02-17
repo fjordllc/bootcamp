074d78b4fef0ae4ca1440b8adde7f1ba24568f19
uchihiro <tohchi4122@gmail.com>
2023-02-17 22:21:57 +0900

    不要な条件分岐（ガード節）を削除
    以前は条件分岐（ガード節）の下にslackの通知に関するメソッドがありましたが、現在メソッドは削除されていて、条件分岐（ガード節）を記載する必要がなくなったため削除しました。

--- app/models/user_retirement.rb
+++ app/models/user_retirement.rb
@@ -7,7 +7,5 @@ class UserRetirement
       Report.where(user: user).wip.destroy_all
       user.update(job_seeking: false)
     end
-
-    return unless user.saved_change_to_graduated_on? && user.graduated?
   end
 end
