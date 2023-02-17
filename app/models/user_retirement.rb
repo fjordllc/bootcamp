e6ae65a5204717245035e581f16f2a9faec6e210
uchihiro <tohchi4122@gmail.com>
2023-02-17 22:46:45 +0900

    rubocopからの指摘点を修正
    ガード節を利用するようにrubocopから指摘を受けたため、ガード節を利用した形にコードを修正しました。

--- app/models/user_retirement.rb
+++ app/models/user_retirement.rb
@@ -2,10 +2,10 @@
 
 class UserRetirement
   def call(user)
-    if user.saved_change_to_retired_on?
-      Product.where(user: user).unchecked.destroy_all
-      Report.where(user: user).wip.destroy_all
-      user.update(job_seeking: false)
-    end
+    return unless user.saved_change_to_retired_on?
+
+    Product.where(user: user).unchecked.destroy_all
+    Report.where(user: user).wip.destroy_all
+    user.update(job_seeking: false)
   end
 end
