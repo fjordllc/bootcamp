3a4b4686190d1239937fee3ff37ea9d6eb6e16cc
lef237 <lef237k5@gmail.com>
2023-02-25 20:21:08 +0900

    入会者のスコープを修正した

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -23,6 +23,7 @@ class Admin::CampaignsController < AdminController
 
   def index
     @campaigns = Campaign.order(end_at: :desc)
+    @normal_students = User.where(admin: false, mentor: false, adviser: false, trainee: false)
     @retired_students = User.retired_students
   end
 
