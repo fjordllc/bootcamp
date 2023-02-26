c7663fc0ededb17091437cddfdf287c0c7141775
maimux2x <m0smile10often@gmail.com>
2023-02-26 10:31:23 +0900

    コンフリクトの解消間違いを修正

--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -39,21 +39,11 @@ Rails.configuration.to_prepare do
   Newspaper.subscribe(:page_create, page_notifier)
   Newspaper.subscribe(:page_update, page_notifier)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   product_notifier = ProductNotifier.new
   Newspaper.subscribe(:product_create, product_notifier)
   Newspaper.subscribe(:product_update, product_notifier)
 
-  Newspaper.subscribe(:question_create, CreatingMentorsWatchForQuestion.new)
-=======
-  creating_mentors_watch_for_question = CreatingMentorsWatchForQuestion.new
-  Newspaper.subscribe(:question_create, creating_mentors_watch_for_question)
-  Newspaper.subscribe(:question_update, creating_mentors_watch_for_question)
->>>>>>> 7792e7778 (createとupdateでnewspaperの呼び出しを分けた)
-=======
   mentors_watch_for_question_creator = MentorsWatchForQuestionCreator.new
   Newspaper.subscribe(:question_create, mentors_watch_for_question_creator)
   Newspaper.subscribe(:question_update, mentors_watch_for_question_creator)
->>>>>>> dd2eaab4c (ファイルとクラス名をCreatingMentorsWatchForQueestionからMentorsWatchForQuestionCreatorに修正した)
 end
