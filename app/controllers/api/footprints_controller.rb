5bc403f83877e5cbcd9f73095fdba9eeef7c3cab
akingo55 <am1157154@gmail.com>
2023-03-01 22:57:30 +0900

    呼び出すメソッドを制限する

--- app/controllers/api/footprints_controller.rb
+++ app/controllers/api/footprints_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class API::FootprintsController < API::BaseController
-  before_action :footprintable
+  before_action :footprintable, only: %i[index]
 
   def index
     if @footprintable.present?
