8d64446d58014a4230eea08a11b2e1bf76891593
sadanora <sadanora.pi@gmail.com>
2023-03-05 13:40:50 +0900

    アンケートの質問の並び替え画面を追加

---
+++ app/javascript/survey-question-listings.js
@@ -0,0 +1,24 @@
+import Sortable from 'sortablejs'
+import Bootcamp from 'bootcamp'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector(
+    '#js-survey-question-listings-sortable'
+  )
+  if (!element) {
+    return null
+  }
+
+  Sortable.create(element, {
+    handle: '.js-grab',
+    onEnd(event) {
+      const id = event.item.dataset.survey_question_listings_id
+      const params = { insert_at: event.newIndex + 1 }
+      // const url = `/api/survey_question_listings/${id}/position.json`
+
+      Bootcamp.patch(url, params).catch((error) => {
+        console.warn(error)
+      })
+    }
+  })
+})
