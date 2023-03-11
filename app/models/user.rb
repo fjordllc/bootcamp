40c3d4afd8e0c46cc23eead3aa35f97c8de1a3a8
ksmxxxxxx <kankichi69@gmail.com>
2023-03-11 15:31:46 +0900

    メッセージ送信済み付与メソッドをクラスメソッドに変更 && テストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -429,6 +429,18 @@ class User < ApplicationRecord
       ).pluck(:last_sad_report_id)
       Report.joins(:user).where(id: ids).order(reported_on: :desc)
     end
+
+    # FIXME: 一次対応として一回でも休会している受講生にはメッセージ送信済みとする
+    #        別Issueで入会n日目、休会開けn日目目の受講生にメッセージを送信する方針へ改修してほしい
+    #        改修後、このメソッドは不要になると思われるので削除すること
+    def mark_message_as_sent_for_hibernated_student
+      User.find_each do |user|
+        if user.hibernated?
+          user.assign_attributes(sent_student_followup_message: true)
+          user.save(context: :followup_message)
+        end
+      end
+    end
   end
 
   def retired_three_months_ago_and_notification_not_sent?
