985c63954b9720fc49940bbb1aa71e67271062ea
hikarook94 <walrus4l145@icloud.com>
2023-03-19 21:27:16 +0900

    クラス名変更

--- test/models/regular_event/participants_watcher_test.rb
+++ test/models/regular_event/participants_watcher_test.rb
@@ -2,11 +2,11 @@
 
 require 'test_helper'
 
-class RegularEventParticipantsWatcherTest < ActiveSupport::TestCase
+class ParticipantsWatcherTest < ActiveSupport::TestCase
   test '.call' do
     regular_event = regular_events(:regular_event3)
     target = User.students_and_trainees.ids - regular_event.watches.pluck(:user_id)
-    RegularEventParticipantsWatcher.call(regular_event: regular_event, target: target)
+    RegularEvent::ParticipantsWatcher.call(regular_event: regular_event, target: target)
 
     assert_equal regular_event.watches.count, target.count
   end
