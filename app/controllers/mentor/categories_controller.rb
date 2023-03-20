3abca09dbbf3cc5547ac331c05161c2432b9d9e9
mono-nobe <ryoya.goemono@gmail.com>
2023-03-20 13:59:04 +0900

    mentor用のcontroller(courses, categories)を追加

---
+++ app/controllers/mentor/categories_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class Mentor::CategoriesController < MentorController
+  before_action :set_category, only: %i[show edit update]
+
+  def index; end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to mentor_categories_url, notice: 'カテゴリーを作成しました。'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to return_to, notice: 'カテゴリーを更新しました。'
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :slug,
+      :description
+    )
+  end
+
+  def return_to
+    course_id = params[:category][:course_id]
+    course_id.present? ? course_practices_url(course_id) : mentor_categories_url
+  end
+end
