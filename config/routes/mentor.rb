eeff6be825422a0747da6561346483f8f00e9ddf
mono-nobe <ryoya.goemono@gmail.com>
2023-03-20 13:42:18 +0900

    mentorのroutesにresourcesを追加

--- config/routes/mentor.rb
+++ config/routes/mentor.rb
@@ -3,5 +3,8 @@
 Rails.application.routes.draw do
   namespace :mentor do
     root to: "home#index", as: :root
+    resources :categories, except: %i(destroy)
+    resources :practices, only: %i(index)
+    resources :courses, except: %i(show destroy)
   end
 end
