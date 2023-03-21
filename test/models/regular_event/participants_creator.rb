806e3ccb7604937659fd5609d4a2965b452fa1d4
hikarook94 <walrus4l145@icloud.com>
2023-03-21 19:11:40 +0900

    時刻を外から渡すように修正

--- test/models/regular_event/participants_creator.rb
+++ test/models/regular_event/participants_creator.rb
@@ -10,4 +10,14 @@ class ParticipantsCreatorTest < ActiveSupport::TestCase
 
     assert_equal regular_event.regular_event_participations.count, target.count
   end
+
+  test '.call with date_time' do
+    regular_event = regular_events(:regular_event3)
+    target = User.students_and_trainees.ids
+    date_time = Time.current.end_of_day.floor
+    RegularEvent::ParticipantsCreator.call(regular_event: regular_event, target: target, date_time: date_time)
+
+    assert_equal regular_event.regular_event_participations.count, target.count
+    assert_equal regular_event.regular_event_participations.first.created_at, date_time
+  end
 end
