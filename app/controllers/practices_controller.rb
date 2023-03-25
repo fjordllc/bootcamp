13de35327c66bef4e4748daedbd070dc7de8fcd0
mono-nobe <ryoya.goemono@gmail.com>
2023-03-25 21:51:22 +0900

    プラクティスの未認証時のアクション名を変更

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -7,12 +7,16 @@ class PracticesController < ApplicationController
   skip_before_action :require_active_user_login, only: %i[show]
 
   def show
-    render template: 'practices/index_not_auth/index', locals: { title: @practice.title } unless logged_in?
-
     @categories = @practice.categories
     @tweet_url = @practice.tweet_url(practice_completion_url(@practice.id))
     @common_page = Page.find_by(slug: 'practice_common_description')
     @common_page = nil if @common_page&.wip?
+
+    if logged_in?
+      render :show
+    else
+      render :unauthorized_show
+    end
   end
 
   def new
