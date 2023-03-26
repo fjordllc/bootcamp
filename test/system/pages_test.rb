4833e22001e48105311c7051a8bbd404ac782114
YukiWatanabe824 <gyggg950@gmail.com>
2023-03-26 14:25:27 +0900

    test:フラッシュメッセージ修正に伴うテストの修正

--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -50,8 +50,8 @@ class PagesTest < ApplicationSystemTestCase
     assert_equal new_page_path, current_path
     fill_in 'page[title]', with: '新規Docを作成する'
     fill_in 'page[body]', with: '新規Docを作成する本文です'
-    click_button 'Docを公開'
-    assert_text 'ページを作成しました'
+    click_button '内容を保存'
+    assert_text 'ドキュメントを作成しました'
     assert_text 'Watch中'
   end
 
@@ -224,7 +224,7 @@ class PagesTest < ApplicationSystemTestCase
       click_button 'Docを公開'
     end
 
-    assert_text 'ページを作成しました'
+    assert_text 'ドキュメントを作成しました'
     assert_text 'Watch中'
     assert_match 'Message to Discord.', mock_log.to_s
   end
