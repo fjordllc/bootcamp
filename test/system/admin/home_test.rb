cd54eb36d372533faa2e25262e1c1ecdae08df5a
mono-nobe <ryoya.goemono@gmail.com>
2023-03-26 17:52:12 +0900

    管理者・メンター用ページに表示されるタブのテストを追加

--- test/system/admin/home_test.rb
+++ test/system/admin/home_test.rb
@@ -6,5 +6,12 @@ class Admin::HomeTest < ApplicationSystemTestCase
   test 'GET /admin' do
     visit_with_auth '/admin', 'komagata'
     assert_equal '管理ページ | FBC', title
+    assert_selector '.page-tabs__item-link', text: '管理ページ', visible: true
+    assert_selector '.page-tabs__item-link', text: 'ユーザー', visible: true
+    assert_selector '.page-tabs__item-link', text: '企業', visible: true
+    assert_selector '.page-tabs__item-link', text: 'お試し延長', visible: true
+    assert_no_selector '.page-tabs__item-link', text: 'プラクティス', visible: true
+    assert_no_selector '.page-tabs__item-link', text: 'カテゴリー', visible: true
+    assert_no_selector '.page-tabs__item-link', text: 'コース', visible: true
   end
 end
--- test/system/mentor/home_test.rb
+++ test/system/mentor/home_test.rb
@@ -8,6 +8,13 @@ class Mentor::HomeTest < ApplicationSystemTestCase
     assert_equal 'メンターページ | FBC', title
     assert_no_text 'jobseeker (シュウカツ ノゾム)'
     assert_text 'muryou (ムリョウ ノスケ)'
+    assert_selector '.page-tabs__item-link', text: 'メンターページ', visible: true
+    assert_selector '.page-tabs__item-link', text: 'プラクティス', visible: true
+    assert_selector '.page-tabs__item-link', text: 'カテゴリー', visible: true
+    assert_selector '.page-tabs__item-link', text: 'コース', visible: true
+    assert_no_selector '.page-tabs__item-link', text: 'ユーザー', visible: true
+    assert_no_selector '.page-tabs__item-link', text: '企業', visible: true
+    assert_no_selector '.page-tabs__item-link', text: 'お試し延長', visible: true
   end
 
   test 'accessed by non-mentor users' do
