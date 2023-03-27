09a22c5192800cc7b20970ad6c30b5e00c50660a
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-03-28 06:29:36 +0900

    追加したチェックボックスの動作を確認するテストの追加

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -397,4 +397,74 @@ class EventsTest < ApplicationSystemTestCase
     visit_with_auth '/current_user/watches', 'komagata'
     assert_selector '.card-list-item', count: 1
   end
+
+  test 'when the create announcements checkbox is enabled, the user is redirected to the new announcement page after create a new event' do
+    event = {
+      title: 'チェックボックスを有効にするとお知らせ作成ページにリダイレクトする',
+      description: 'お知らせ作成ページには、リダイレクト元のイベント情報が自動入力される'
+    }
+
+    visit_with_auth new_event_path, 'komagata'
+    within 'form[name=event]' do
+      fill_in 'event[title]', with: event[:title]
+      fill_in 'event[description]', with: event[:description]
+      fill_in 'event[capacity]', with: 10
+      fill_in 'event[location]', with: 'FJORDオフィス'
+      fill_in 'event[start_at]', with: Time.current.next_day
+      fill_in 'event[end_at]', with: Time.current.next_day + 2.hours
+      fill_in 'event[open_start_at]', with: Time.current
+      fill_in 'event[open_end_at]', with: Time.current + 2.hours
+      check 'イベント公開のお知らせを書く', allow_label_click: true
+      click_button 'イベントを公開'
+    end
+
+    assert_text 'イベントを作成しました'
+    within 'form[name=announcement]' do
+      assert has_field? 'announcement[title]', with: /#{event[:title]}/
+      assert has_field? 'announcement[description]', with: /#{event[:desription]}/
+    end
+  end
+
+  test 'when the create announcements checkbox is enabled, the user is redirected to the new announcement page after edit a wip event' do
+    event = {
+      title: 'WIP中のイベントを公開する際、チェックボックスを有効にするとお知らせ作成ページにリダイレクトする',
+      description: '但し、WIPとして保存する場合はチェックを有効にしてもリダイレクトしない'
+    }
+
+    visit_with_auth new_event_path, 'komagata'
+    within 'form[name=event]' do
+      fill_in 'event[title]', with: event[:title]
+      fill_in 'event[description]', with: event[:description]
+      fill_in 'event[capacity]', with: 10
+      fill_in 'event[location]', with: 'FJORDオフィス'
+      fill_in 'event[start_at]', with: Time.current.next_day
+      fill_in 'event[end_at]', with: Time.current.next_day + 2.hours
+      fill_in 'event[open_start_at]', with: Time.current
+      fill_in 'event[open_end_at]', with: Time.current + 2.hours
+      check 'イベント公開のお知らせを書く', allow_label_click: true
+      click_button 'WIP'
+    end
+    click_link '内容修正'
+
+    check 'イベント公開のお知らせを書く', allow_label_click: true
+    click_button '内容を更新'
+
+    assert_text 'イベントを更新しました'
+    within 'form[name=announcement]' do
+      assert has_field? 'announcement[title]', with: /#{event[:title]}/
+      assert has_field? 'announcement[description]', with: /#{event[:desription]}/
+    end
+  end
+
+  test 'checkboxes for creating an announcement are display the edit page of wip event' do
+    wip_event = events(:event32)
+    visit_with_auth edit_event_path(wip_event), wip_event.user.login_name
+    assert_selector 'label', text: 'イベント公開のお知らせを書く'
+  end
+
+  test 'checkboxes for creating an announcement are not display the edit page of published event' do
+    published_event = events(:event1)
+    visit_with_auth edit_event_path(published_event), published_event.user.login_name
+    assert_no_selector 'label', text: 'イベント公開のお知らせを書く'
+  end
 end
