7f3d3e8a98c6dd7e36770c76106d1a4dbf7bf7fc
ksmxxxxxx <kankichi69@gmail.com>
2023-04-01 15:57:27 +0900

    余分な引数だった

--- app/controllers/scheduler/daily/send_message_controller.rb
+++ app/controllers/scheduler/daily/send_message_controller.rb
@@ -3,21 +3,17 @@
 class Scheduler::Daily::SendMessageController < SchedulerController
   def show
     User.mark_message_as_sent_for_hibernated_student
-    sent_student_followup_message(from_user)
+    sent_student_followup_message
     head :ok
   end
 
   private
 
-  def from_user
-    User.find_by(login_name: 'komagata')
-  end
-
-  def sent_student_followup_message(from_user)
+  def sent_student_followup_message
     User.students.find_each do |student|
       next unless student.followup_message_target?
 
-      User.create_followup_comment(from_user, student)
+      User.create_followup_comment(student)
     end
   end
 end
