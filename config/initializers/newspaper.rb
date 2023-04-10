42c6871201a41c63ac87a5890bbb2f53237f8a5e
wata00913 <175d8639@gmail.com>
2023-04-10 13:18:01 +0900

    共通のインスタンス変数を使うように修正

--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -35,10 +35,12 @@ Rails.configuration.to_prepare do
 
   Newspaper.subscribe(:comeback_update, ComebackNotifier.new)
 
-  Newspaper.subscribe(:check_create, LearningStatusUpdater.new)
   Newspaper.subscribe(:product_create, ProductAuthorWatcher.new)
-  Newspaper.subscribe(:product_create, LearningStatusUpdater.new)
-  Newspaper.subscribe(:product_update, LearningStatusUpdater.new)
+
+  learning_status_updater = LearningStatusUpdater.new
+  Newspaper.subscribe(:check_create, learning_status_updater)
+  Newspaper.subscribe(:product_create, learning_status_updater)
+  Newspaper.subscribe(:product_update, learning_status_updater)
 
   page_notifier = PageNotifier.new
   Newspaper.subscribe(:page_create, page_notifier)
