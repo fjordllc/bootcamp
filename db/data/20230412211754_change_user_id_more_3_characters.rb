315d627e9bb37a96dff0970ec15beedc345a993e
YukiWatanabe824 <gyggg950@gmail.com>
2023-04-13 23:33:43 +0900

    feat: アカウント名にバリデーション追加と文字数修正用のdata-migrateファイルを追加

---
+++ db/data/20230412211754_change_user_id_more_3_characters.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class ChangeUserIdMore3Characters < ActiveRecord::Migration[6.1]
+  def up
+    User.where('LENGTH(login_name) < ?', 3).find_each do |user|
+      user.login_name = user.login_name.ljust(3, '_')
+      user.save!(validate: false)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
