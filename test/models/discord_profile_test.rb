605c71cc044832bddfe786a46ea8343783dc3d73
mono-nobe <ryoya.goemono@gmail.com>
2023-04-15 22:44:07 +0900

    discord_profileのmodelテストを追加

--- test/models/discord_profile_test.rb
+++ test/models/discord_profile_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class DiscordProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'account_name' do
+    discord_profile = discord_profiles(:discord_profile1)
+    discord_profile.account_name = ''
+    assert discord_profile.valid?
+    discord_profile.account_name = 'komagata#1234'
+    assert discord_profile.valid?
+    discord_profile.account_name = 'komagata'
+    assert discord_profile.invalid?
+    discord_profile.account_name = '#1234'
+    assert discord_profile.invalid?
+    discord_profile.account_name = ' komagata　#1234'
+    assert discord_profile.invalid?
+    discord_profile.account_name = 'komagata1234'
+    assert discord_profile.invalid?
+  end
+
+  test 'times_url' do
+    discord_profile = discord_profiles(:discord_profile1)
+    discord_profile.times_url = ''
+    assert discord_profile.valid?
+    discord_profile.times_url = 'https://discord.com/channels/715806612824260640/123456789000000001'
+    assert discord_profile.valid?
+    discord_profile.times_url = "https://discord.com/channels/715806612824260640/12345678900000000\n"
+    assert discord_profile.invalid?
+    discord_profile.times_url = 'https://discord.com/channels/715806612824260640/123456789000000001/123456789000000001'
+    assert discord_profile.invalid?
+    discord_profile.times_url = 'https://discord.gg/jc9fnWk4'
+    assert discord_profile.invalid?
+    discord_profile.times_url = 'https://example.com/channels/715806612824260640/123456789000000001'
+    assert discord_profile.invalid?
+  end
 end
