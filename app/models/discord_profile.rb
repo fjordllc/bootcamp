cf682b32fd87fb06f4866aee3d5bd259d5c4d3a6
mono-nobe <ryoya.goemono@gmail.com>
2023-04-15 13:44:11 +0900

    バリデーションの追加

--- app/models/discord_profile.rb
+++ app/models/discord_profile.rb
@@ -2,4 +2,19 @@
 
 class DiscordProfile < ApplicationRecord
   belongs_to :user
+
+  validates :times_url,
+            format: {
+              allow_blank: true,
+              with: %r{\Ahttps://discord\.com/channels/\d+/\d+\z},
+              message: 'はDiscordのチャンネルURLを入力してください'
+            }
+  with_options if: -> { validation_context != :retirement } do
+    validates :account_name,
+              format: {
+              allow_blank: true,
+              with: /\A[^\s\p{blank}].*[^\s\p{blank}]#\d{4}\z/,
+              message: 'は「ユーザー名#４桁の数字」で入力してください'
+              }
+  end
 end
