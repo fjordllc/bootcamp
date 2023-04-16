a40460811be8c6611ef1781f23aa4476d5f6ad54
tomo <o_tomo_1990@yahoo.co.jp>
2023-04-16 17:55:44 +0900

    退会してから3ヶ月経過の通知に関する処理を削除（プロダクトコード側）

--- config/routes/scheduler.rb
+++ config/routes/scheduler.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     resource :validator, only: %i(show), controller: "validator"
     resource :daily, only: %i(show), controller: "daily"
     namespace :daily do
-      resource :after_retirement, only: %i(show), controller: "after_retirement"
       resource :notify_tomorrow_regular_event, only: %i(show), controller: "notify_tomorrow_regular_event"
       resource :notify_certain_period_passed_after_last_answer, only: %i(show), controller: "notify_certain_period_passed_after_last_answer"
       resource :notify_product_review_not_completed, only: %i(show), controller: "notify_product_review_not_completed"
