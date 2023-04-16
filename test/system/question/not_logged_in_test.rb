820d0fd799e8bc9b62a5fc277d18a6232d72686b
tomo <o_tomo_1990@yahoo.co.jp>
2023-04-16 11:11:44 +0900

    system testでヘルパーメソッドを用いる

--- test/system/question/not_logged_in_test.rb
+++ test/system/question/not_logged_in_test.rb
@@ -5,7 +5,7 @@ require 'application_system_test_case'
 class Question::NotLoggedInTest < ApplicationSystemTestCase
   test 'meta discription is set when not logged-in user access show' do
     question = questions(:question1)
-    visit "/questions/#{question.id}"
+    visit question_path(question)
     assert_text "「#{question.title}」"
     assert_text 'このページの閲覧にはフィヨルドブートキャンプの入会が必要です'
     assert_selector "meta[name='description'][content='オンラインプログラミングスクール「フィヨルドブートキャンプ」のQ&A「#{question.title}」のページです。']", visible: false
