56db721896a7ed4a2ae6a460b65868857b83f4c3
tomo <o_tomo_1990@yahoo.co.jp>
2023-04-16 20:15:33 +0900

    退会してから3ヶ月経過の通知に関する処理を削除（テストコード側）

--- test/system/notification/after_retirement_test.rb
+++ test/system/notification/after_retirement_test.rb
@@ -12,13 +12,4 @@ class Notification::AfterRetirementTest < ApplicationSystemTestCase
   teardown do
     AbstractNotifier.delivery_mode = @delivery_mode
   end
-
-  test 'three months have passed since user retired' do
-    visit_with_auth '/scheduler/daily/after_retirement', 'komagata'
-    visit '/notifications'
-
-    within first('.card-list-item.is-unread') do
-      assert_text 'taikai3さんが退会してから3カ月が経過しました。Discord ID: taikai#3333, ユーザーページ: https://bootcamp.fjord.jp/users/1008501353'
-    end
-  end
 end
