fd4da0cce5478a349ec04427767f8e74aade3644
uchihiro <tohchi4122@gmail.com>
2023-04-20 16:28:01 +0900

    テストを作成

--- test/integration/api/watches_test.rb
+++ test/integration/api/watches_test.rb
@@ -12,4 +12,158 @@ class API::WatchesTest < ActionDispatch::IntegrationTest
         headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :ok
   end
+
+  test 'duplicate POST announce' do
+    announcement = announcements(:announcement1)
+    token = create_token('komagata', 'testtest')
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: announcement.user_id,
+           watchable_id: announcement.id,
+           watchable_type: 'Announcement'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: announcement.user_id,
+           watchable_id: announcement.id,
+           watchable_type: 'Announcement'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :unprocessable_entity
+  end
+
+  test 'duplicate POST product' do
+    product = products(:product1)
+    token = create_token('komagata', 'testtest')
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: product.user_id,
+           watchable_id: product.id,
+           watchable_type: 'Product'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: product.user_id,
+           watchable_id: product.id,
+           watchable_type: 'Product'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :unprocessable_entity
+  end
+
+  test 'duplicate POST report' do
+    report = reports(:report1)
+    token = create_token('komagata', 'testtest')
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: report.user_id,
+           watchable_id: report.id,
+           watchable_type: 'Report'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: report.user_id,
+           watchable_id: report.id,
+           watchable_type: 'Report'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :unprocessable_entity
+  end
+
+  test 'duplicate POST question' do
+    question = questions(:question1)
+    token = create_token('komagata', 'testtest')
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: question.user_id,
+           watchable_id: question.id,
+           watchable_type: 'Question'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: question.user_id,
+           watchable_id: question.id,
+           watchable_type: 'Question'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :unprocessable_entity
+  end
+
+  test 'duplicate POST page' do
+    page = pages(:page1)
+    token = create_token('komagata', 'testtest')
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: page.user_id,
+           watchable_id: page.id,
+           watchable_type: 'Page'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: page.user_id,
+           watchable_id: page.id,
+           watchable_type: 'Page'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :unprocessable_entity
+  end
+
+  test 'duplicate POST event' do
+    event = events(:event1)
+    token = create_token('komagata', 'testtest')
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: event.user_id,
+           watchable_id: event.id,
+           watchable_type: 'Event'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: event.user_id,
+           watchable_id: event.id,
+           watchable_type: 'Event'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :unprocessable_entity
+  end
+
+  test 'duplicate POST regular event' do
+    regular_event = regular_events(:regular_event1)
+    token = create_token('komagata', 'testtest')
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: regular_event.user_id,
+           watchable_id: regular_event.id,
+           watchable_type: 'RegularEvent'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :ok
+
+    post api_watches_toggle_index_path(format: :json),
+         params: {
+           user_id: regular_event.user_id,
+           watchable_id: regular_event.id,
+           watchable_type: 'RegularEvent'
+         },
+         headers: { 'Authorization' => "Bearer #{token}" }
+    assert_response :unprocessable_entity
+  end
 end
