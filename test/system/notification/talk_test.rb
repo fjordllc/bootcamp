f652b473fa4b0e8e6241c635d8010496d85231b2
OdenTakashi <odentakashidesu@icloud.com>
2023-04-29 14:14:08 +0900

    実装により落ちたテストの修正

--- test/system/notification/talk_test.rb
+++ test/system/notification/talk_test.rb
@@ -88,7 +88,7 @@ class Notification::TalkTest < ApplicationSystemTestCase
 
     talk_id = users(:with_hyphen).talk.id
     visit_with_auth "/talks/#{talk_id}", 'komagata'
-    check 'action-check'
+    check 'action-completed-checkbox'
     assert_text '対応済みにしました'
 
     visit '/talks/action_uncompleted'
