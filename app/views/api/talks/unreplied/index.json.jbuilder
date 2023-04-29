2a2d2f87a590cba5d2ea604c55e848494a4b35f4
OdenTakashi <odentakashidesu@icloud.com>
2023-04-29 15:27:46 +0900

    不要な箇所を削除

--- app/views/api/talks/unreplied/index.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.partial! 'api/talks/talks', talks: @talks
-json.totalPages @talks.total_pages if @talks.respond_to? :total_pages
--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,6 @@ Rails.application.routes.draw do
   end
   resources :works, except: %i(index)
   namespace :talks do
-    resources :unreplied, only: %i(index)
     resources :action_uncompleted, only: %i(index)
   end
   resources :talks, only: %i(index show)
