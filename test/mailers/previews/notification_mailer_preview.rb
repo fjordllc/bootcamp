f584b41a2b92b5d390f7d191172273f1bf982ffe
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-04-30 11:21:38 +0900

    イベント参加への繰り上がり通知メールを、プレビューに追加し、見れるようにした

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -50,16 +50,6 @@ class NotificationMailerPreview < ActionMailer::Preview
     NotificationMailer.with(report: report, receiver: receiver).trainee_report
   end
 
-  def moved_up_event_waiting_user
-    event = Event.find(ActiveRecord::FixtureSet.identify(:event3))
-    receiver = User.find(ActiveRecord::FixtureSet.identify(:hajime))
-
-    NotificationMailer.with(
-      event: event,
-      receiver: receiver
-    ).moved_up_event_waiting_user
-  end
-
   def chose_correct_answer
     answer = Answer.find(ActiveRecord::FixtureSet.identify(:correct_answer2))
     receiver = answer.sender
