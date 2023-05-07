d26aaae1ba68675e8e554c91c51542e792c81f8d
tomo <o_tomo_1990@yahoo.co.jp>
2023-05-07 20:59:55 +0900

    動くようにミスを修正

--- app/controllers/scheduler/daily/update_retired_after_long_hibernation_controller.rb
+++ app/controllers/scheduler/daily/update_retired_after_long_hibernation_controller.rb
@@ -9,7 +9,7 @@ class Scheduler::Daily::UpdateRetiredAfterLongHibernationController < SchedulerC
   private
 
   def update_retired_after_long_hibernation
-    User.unretired.hibernated_for(3.months).auto_retire_after_long_hibernation each do |user|
+    User.unretired.hibernated_for(3.months).auto_retire_after_long_hibernation.each do |user|
       user.retire_reason = '（休会後三ヶ月経過したため自動退会）'
       user.retired_on = Date.current
       user.save!(validate: false)
@@ -21,25 +21,25 @@ class Scheduler::Daily::UpdateRetiredAfterLongHibernationController < SchedulerC
         logger.warn "[Postmark] 受信者由来のエラーのためメールを送信できませんでした。：#{e.message}"
       end
 
-      destroy_subscription
-      notify_to_admins
-      notify_to_mentors
+      destroy_subscription(user)
+      notify_to_admins(user)
+      notify_to_mentors(user)
     end
   end
 
-  def destroy_subscription
-    Subscription.new.destroy(current_user.subscription_id) if current_user.subscription_id
+  def destroy_subscription(user)
+    Subscription.new.destroy(user.subscription_id) if user.subscription_id
   end
 
-  def notify_to_admins
+  def notify_to_admins(user)
     User.admins.each do |admin_user|
-      ActivityDelivery.with(sender: current_user, receiver: admin_user).notify(:retired)
+      ActivityDelivery.with(sender: user, receiver: admin_user).notify(:retired)
     end
   end
 
-  def notify_to_mentors
+  def notify_to_mentors(user)
     User.mentor.each do |mentor_user|
-      ActivityDelivery.with(sender: current_user, receiver: mentor_user).notify(:retired)
+      ActivityDelivery.with(sender: user, receiver: mentor_user).notify(:retired)
     end
   end
 end
