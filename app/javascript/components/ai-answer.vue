3974c0750c69cb946243e2f31c84da35f7fc5bd8
komagata <komagata@gmail.com>
2023-05-08 05:42:43 +0900

    Q&AにAIが回答する機能を追加

---
+++ app/javascript/components/ai-answer.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div
+  h2 AIの解答
+  div(v-html='nl2brText')
+</template>
+<script>
+export default {
+  name: 'AIAnswer',
+  props: {
+    text: { type: String, required: true }
+  },
+  computed: {
+    nl2brText() {
+      return this.text.replaceAll('\n', '<br>')
+    }
+  }
+}
+</script>
---
+++ app/models/ai/answer_generator.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module AI
+  class AnswerGenerator
+    PRECONDITION = <<~COND
+      私はプログラミングスクールの生徒でプログラミングの学習中です。
+      あなたはプログラミングスクールの先生です。
+      質問に対して優しく、わかりやすく答えてください。
+      質問に必要な情報が足りない場合はその点についても指摘してください。
+    COND
+
+    TEMPERATURE = 0.7
+
+    DEFAULT_TEXT = '仮の解答です。'
+
+    def initialize(open_ai_access_token:)
+      @open_ai_access_token = open_ai_access_token
+    end
+
+    def call(question_text)
+      return DEFAULT_TEXT if Rails.env.development?
+
+      client = OpenAI::Client.new(access_token: @open_ai_access_token)
+      response = client.chat(
+        parameters: {
+          model: 'gpt-4',
+          messages: [
+            { role: 'system', content: PRECONDITION },
+            { role: 'user', content: question_text }
+          ],
+          temperature: TEMPERATURE
+        }
+      )
+      response.dig('choices', 0, 'message', 'content')
+    end
+  end
+end
