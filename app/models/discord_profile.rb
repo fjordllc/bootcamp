9110f2eab82607f38d61f0c930c1d07c6e971199
mono-nobe <ryoya.goemono@gmail.com>
2023-05-11 01:19:40 +0900

    lintによる整形

--- app/models/discord_profile.rb
+++ app/models/discord_profile.rb
@@ -12,9 +12,9 @@ class DiscordProfile < ApplicationRecord
   with_options if: -> { validation_context != :retirement } do
     validates :account_name,
               format: {
-              allow_blank: true,
-              with: /\A[^\s\p{blank}].*[^\s\p{blank}]#\d{4}\z/,
-              message: 'は「ユーザー名#４桁の数字」で入力してください'
+                allow_blank: true,
+                with: /\A[^\s\p{blank}].*[^\s\p{blank}]#\d{4}\z/,
+                message: 'は「ユーザー名#４桁の数字」で入力してください'
               }
   end
 end
--- test/models/discord_profile_test.rb
+++ test/models/discord_profile_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class DiscordProfileTest < ActiveSupport::TestCase
   test 'account_name' do
