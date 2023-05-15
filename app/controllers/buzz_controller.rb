bbb44a8a2e00f371fe3443c4a79368e21df2a034
djkazunoko <djkazunoko@gmail.com>
2023-05-16 08:35:06 +0900

    showページをログインなしでアクセスできるようにした

--- app/controllers/buzz_controller.rb
+++ app/controllers/buzz_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class BuzzController < ApplicationController
+  skip_before_action :require_active_user_login, raise: false, only: %i[show]
+
   def show
     render layout: 'welcome'
   end
