c44febc889ef554244b3e8d4fc6baa28164b7e03
mono-nobe <ryoya.goemono@gmail.com>
2023-05-19 13:14:22 +0900

    userテーブルからdiscord関連のカラムを削除するmigrationを削除

--- db/migrate/20230415074554_remove_discord_account_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDiscordAccountFromUsers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :discord_account, :string
-    remove_column :users, :times_url, :string
-  end
-end
