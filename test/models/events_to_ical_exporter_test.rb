d440153a76fb0f21b9c602af9e0776e394e544f8
maimux2x <m0smile10often@gmail.com>
2023-05-21 15:16:46 +0900

    テストの修正

--- test/models/events_to_ical_exporter_test.rb
+++ test/models/events_to_ical_exporter_test.rb
@@ -2,11 +2,11 @@
 
 require 'test_helper'
 
-class EventsInIcalFormatExporterTest < ActiveSupport::TestCase
+class EventsToIcalExporterTest < ActiveSupport::TestCase
   test 'check to export only feature events' do
     travel_to Time.zone.local(2023, 5, 19, 10, 0, 0) do
       events = Event.where('start_at > ?', Time.zone.today)
-      calendar = EventsInIcalFormatExporter.export_events(events)
+      calendar = EventsToIcalExporter.export_events(events)
 
       calendar.publish
       assert_match(/明日開催のイベント/, calendar.to_ical)
