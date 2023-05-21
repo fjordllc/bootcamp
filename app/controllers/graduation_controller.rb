4979cb7fef7f029a944078404f031a52b817bb49
YukiWatanabe824 <gyggg950@gmail.com>
2023-05-21 17:16:48 +0900

    卒業させようとした際のリクエスト元のログイン&管理者チェックを追加

--- app/controllers/graduation_controller.rb
+++ app/controllers/graduation_controller.rb
@@ -4,6 +4,7 @@ class GraduationController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
   before_action :set_user, only: %i[update]
   before_action :set_redirect_url, only: %i[update]
+  before_action :check_admin_permission, only: %i[update]
 
   def update
     if @user.update(graduated_on: Date.current)
@@ -19,6 +20,10 @@ class GraduationController < ApplicationController
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def check_admin_permission
+    redirect_to root_path, alert: '管理者としてログインしてください' unless current_user.admin?
   end
 
   def set_redirect_url
