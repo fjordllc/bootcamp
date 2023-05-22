617a96940460fed6175aa0f1c25a1624df7bdb33
djkazunoko <djkazunoko@gmail.com>
2023-05-22 09:33:39 +0900

    buzzの表示と更新処理を追加

--- app/controllers/buzz_controller.rb
+++ app/controllers/buzz_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class BuzzController < ApplicationController
+  before_action :set_buzz
   skip_before_action :require_active_user_login, raise: false, only: %i[show]
   before_action :require_admin_or_mentor_login, except: %i[show]
 
@@ -8,9 +9,23 @@ class BuzzController < ApplicationController
     render layout: 'welcome'
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @buzz.update(buzz_params)
+      redirect_to buzz_path, notice: 'Buzzを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_buzz
+    @buzz = Buzz.first
+  end
+
+  def buzz_params
+    params.require(:buzz).permit(:body)
   end
 end
