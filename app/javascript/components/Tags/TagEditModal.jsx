a618f5976db6051a48dc2c72f973d835f4058d22
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-05-22 14:24:15 +0900

    不要なonClick属性を削除

--- app/javascript/components/Tags/TagEditModal.jsx
+++ app/javascript/components/Tags/TagEditModal.jsx
@@ -59,9 +59,7 @@ export default function TagEditModal({ tagId, propTagName, setShowModal }) {
   }
 
   return (
-    <div
-      className="a-overlay is-vue"
-      onClick={(e) => e.target === e.currentTarget && closeModal()}>
+    <div className="a-overlay is-vue">
       <div className="a-card is-modal">
         <header className="card-header is-sm">
           <h2 className="card-header__title">タグ名変更</h2>
