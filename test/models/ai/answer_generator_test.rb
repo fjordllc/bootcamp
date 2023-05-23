5e4485cb86b683179d063702229423401d866785
komagata <komagata@gmail.com>
2023-05-24 05:27:19 +0900

    落ちているAI関連のテストを修正
    access_token周りが間違っていたのを修正。

--- test/models/ai/answer_generator_test.rb
+++ test/models/ai/answer_generator_test.rb
@@ -4,8 +4,9 @@ require 'test_helper'
 
 module AI
   class AnswerGeneratorTest < ActiveSupport::TestCase
-    test 'call' do
-      generator = AnswerGenerator.new(open_ai_access_token: ENV['OPEN_AI_ACCESS_TOKEN'])
+    test '#call' do
+      token = Rails.application.secrets[:open_ai][:access_token]
+      generator = AnswerGenerator.new(open_ai_access_token: token)
 
       VCR.use_cassette 'ai/answer_generator' do
         assert_equal 'テストの解答です。', generator.call('テストの質問です。')
