5ebfe7d6f833009f265fb56dda44bba5411974dd
OdenTakashi <odentakashidesu@icloud.com>
2023-05-26 09:59:57 +0900

    rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_014916) do
+ActiveRecord::Schema.define(version: 2023_05_04_083810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
