1ed92feae1e16a0f9ee296c4cbe00b5e7e180090
djkazunoko <djkazunoko@gmail.com>
2023-05-29 08:19:23 +0900

    ChatNotifier.messageをQuestionNotifier#call内に移動

--- app/models/question_notifier.rb
+++ app/models/question_notifier.rb
@@ -4,12 +4,6 @@ class QuestionNotifier
   def call(question)
     return if question.wip?
 
-    notify_to_chat(question)
-  end
-
-  private
-
-  def notify_to_chat(question)
     ChatNotifier.message(<<~TEXT)
       質問：「#{question.title}」を#{question.user.login_name}さんが作成しました。
       #{Rails.application.routes.url_helpers.question_url(question)}
