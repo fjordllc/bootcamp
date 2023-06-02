82aefb1e2e9a4544b84067019fa66034955f8793
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-06-02 14:54:59 +0900

    Prettierの指摘を反映

--- app/javascript/components/Companies.jsx
+++ app/javascript/components/Companies.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import CSRF from "../csrf";
+import CSRF from '../csrf'
 import Company from './Company'
 import LoadingUsersPageCompaniesPlaceholder from './LoadingUsersPageCompaniesPlaceholder'
 
