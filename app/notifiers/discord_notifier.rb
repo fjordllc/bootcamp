d3c2bcfd9a0c7cfaface623ca2d2f5978f50da13
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-06-04 23:33:13 +0900

    一部処理を三項演算子に置き換える

--- app/notifiers/discord_notifier.rb
+++ app/notifiers/discord_notifier.rb
@@ -67,8 +67,7 @@ class DiscordNotifier < ApplicationNotifier # rubocop:disable Metrics/ClassLengt
 
   def add_event_info(events, date_message, date)
     day_of_the_week = %w[日 月 火 水 木 金 土]
-    event_info = ''
-    event_info = "< #{date_message} (#{date.strftime('%m/%d')} #{day_of_the_week[date.wday]} 開催 >\n\n" if events.present?
+    event_info = events.present? ? "< #{date_message} (#{date.strftime('%m/%d')} #{day_of_the_week[date.wday]} 開催 >\n\n" : ''
     not_held_events, held_events = events.partition { |event| !event.hold_national_holiday && HolidayJp.holiday?(date) }
     held_events.each do |event|
       event_info += "#{event.title}\n"
