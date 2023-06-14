bc102eaf778d579399bde4d5c01dbbf9f9eeb60a
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-06-14 10:50:06 +0900

    fetchでapiからデータを取得するのを、useSWRに変更する

--- app/javascript/components/Companies.jsx
+++ app/javascript/components/Companies.jsx
@@ -1,53 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import CSRF from '../csrf'
+import React from 'react'
 import Company from './Company'
 import LoadingUsersPageCompaniesPlaceholder from './LoadingUsersPageCompaniesPlaceholder'
+import useSWR from "swr";
+import fetcher from "../fetcher";
 
 export default function Companies({ target }) {
-  const [companies, setCompanies] = useState([])
-  const [loaded, setLoaded] = useState(false)
+  const { data, error } = useSWR(`/api/users/companies?target=${target}`, fetcher)
 
-  useEffect(() => {
-    getCompaniesPage()
-  }, [])
-
-  const getCompaniesPage = () => {
-    const url = `/api/users/companies?target=${target}`
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-Token': CSRF.getToken()
-      },
-      credentials: 'same-origin',
-      redirect: 'manual'
-    })
-      .then((response) => response.json())
-      .then((companies) => {
-        setCompanies(companies)
-        setLoaded(true)
-      })
-      .catch((error) => {
-        console.warn(error)
-      })
+  if (error) return console.warn(error)
+  if (!data) {
+    return (
+      <div className="page-body">
+        <div className="container is-lg">
+            <div className="card-list a-card is-loading">
+              <LoadingUsersPageCompaniesPlaceholder />
+            </div>
+        </div>
+      </div>
+    )
   }
 
   return (
     <div className="page-body">
       <div className="container is-lg">
-        {!loaded ? (
-          <div className="card-list a-card is-loading">
-            <LoadingUsersPageCompaniesPlaceholder />
-          </div>
-        ) : (
-          <div className="card-list a-card">
-            {companies.map((company) => (
-              <Company key={company.id} company={company} />
-            ))}
-          </div>
-        )}
+        <div className="card-list a-card">
+          {data.map((company) => (
+            <Company key={company.id} company={company} />
+          ))}
+        </div>
       </div>
     </div>
   )
