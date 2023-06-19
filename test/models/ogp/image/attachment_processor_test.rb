4adc37462b133ff9ee6cc69b903cb07f08816a1b
wata00913 <175d8639@gmail.com>
2023-06-20 08:09:18 +0900

    テストコードを修正

--- test/models/ogp/image/attachment_processor_test.rb
+++ test/models/ogp/image/attachment_processor_test.rb
@@ -8,10 +8,10 @@ module Ogp
       test '.fit_to_size' do
         article = articles(:article4)
 
-        Ogp::Image::AttachmentProcessor.fit_to_size(article.thumbnail)
+        AttachmentProcessor.fit_to_size(article.thumbnail)
 
         article.thumbnail.blob.analyze unless article.thumbnail.blob.analyzed?
-        assert_equal({ width: 1200, height: 630 },
+        assert_equal({ width: WIDTH, height: HEIGHT },
                      article.thumbnail.blob.metadata.slice(:width, :height).symbolize_keys)
       end
     end
