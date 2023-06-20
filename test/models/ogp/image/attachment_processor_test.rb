ff320ada4cf4a6b35aac51d711addf20f12174b8
wata00913 <175d8639@gmail.com>
2023-06-20 12:51:24 +0900

    テストコード内で定義した定数か、モジュールで定義した定数か判断しづらいのでハードコーディングに戻した

--- test/models/ogp/image/attachment_processor_test.rb
+++ test/models/ogp/image/attachment_processor_test.rb
@@ -11,7 +11,7 @@ module Ogp
         AttachmentProcessor.fit_to_size(article.thumbnail)
 
         article.thumbnail.blob.analyze unless article.thumbnail.blob.analyzed?
-        assert_equal({ width: WIDTH, height: HEIGHT },
+        assert_equal({ width: 1200, height: 630 },
                      article.thumbnail.blob.metadata.slice(:width, :height).symbolize_keys)
       end
     end
