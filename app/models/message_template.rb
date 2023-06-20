3147b66c805a7aada8edbcb49446fe616d5ad856
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-06-21 00:50:16 +0900

    MessageTemplate をオブジェクト指向に従った設計に変更

--- app/models/message_template.rb
+++ app/models/message_template.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 
-module MessageTemplate
+class MessageTemplate
   DEFAULT_DIRECTORY = 'config/message_templates/'
 
-  class << self
-    def load(file, hash: {})
-      path = DEFAULT_DIRECTORY + file
+  def initialize(path)
+    @path = path
+  end
 
-      erb_str = File.read(path)
-      yaml_str = ERB.new(erb_str).result_with_hash(hash)
-      yaml = adjust_indentation(yaml_str)
-      YAML.load(yaml)
-    end
+  def load(params = {})
+    full_path = DEFAULT_DIRECTORY + @path
 
-    private
+    erb_str = File.read(full_path)
+    yaml_str = ERB.new(erb_str).result_with_hash(params)
+    yaml = adjust_indentation(yaml_str)
+    YAML.load(yaml)
+  end
 
-    def adjust_indentation(str)
-      str.gsub(/\r\n/, "\n  ")
-    end
+  def self.load(path, params: {})
+    new(path).load(params)
+  end
+
+  private
+
+  def adjust_indentation(str)
+    str.gsub(/\r\n/, "\n  ")
   end
 end
