6820389a0960e8522c31ebbe644c58b84468d293
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-06-22 16:18:57 +0900

    テストの改善
    テストに必要なリソースを fixture から削除し、
    直接テストコードで明示する事で分かりやすくした

--- test/system/practice/not_logged_in_test.rb
+++ test/system/practice/not_logged_in_test.rb
@@ -4,24 +4,28 @@ require 'application_system_test_case'
 
 class Practice::NotLoggedInTest < ApplicationSystemTestCase
   test 'set ogp image to meta-tag when exists ogp image' do
-    practice = practices(:practice60)
-    upload_ogp_image = practice.ogp_image.blob.filename.sanitized
+    practice = practices(:practice1)
+    visit_with_auth edit_practice_path(practice), 'komagata'
+    within 'form[name=practice]' do
+      attach_file 'practice[ogp_image]', 'test/fixtures/files/practices/ogp_images/1.jpg', make_visible: true
+    end
+    click_button '更新する'
+    logout
 
     visit practice_path(practice)
     ogp_image = File.basename find('meta[property="og:image"]', visible: false)['content']
     twitter_card_image = File.basename find('meta[name="twitter:image"]', visible: false)['content']
-    assert_equal upload_ogp_image, ogp_image
-    assert_equal upload_ogp_image, twitter_card_image
+    assert_equal '1.jpg', ogp_image
+    assert_equal '1.jpg', twitter_card_image
   end
 
   test 'set default ogp image to meta-tag when do not exists ogp image' do
-    practice = practices(:practice58)
-    default_ogp_image = 'ogp.png'
+    practice = practices(:practice1)
 
     visit practice_path(practice)
     ogp_image = File.basename find('meta[property="og:image"]', visible: false)['content']
     twitter_card_image = File.basename find('meta[name="twitter:image"]', visible: false)['content']
-    assert_equal default_ogp_image, ogp_image
-    assert_equal default_ogp_image, twitter_card_image
+    assert_equal 'ogp.png', ogp_image
+    assert_equal 'ogp.png', twitter_card_image
   end
 end
