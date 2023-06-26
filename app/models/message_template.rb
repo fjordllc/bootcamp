355383a22384adf356d29c98904358c11bca6b60
ymmtd0x0b <ymmtd0x0b@gmail.com>
2023-06-27 03:36:38 +0900

    不要な変数を減らし可読性を上げる

--- app/models/message_template.rb
+++ app/models/message_template.rb
@@ -3,22 +3,24 @@
 class MessageTemplate
   DEFAULT_DIRECTORY = 'config/message_templates/'
 
+  def self.load(path, params: {})
+    new(path).load(params)
+  end
+
   def initialize(path)
     @path = path
   end
 
   def load(params = {})
-    erb_str = File.read(DEFAULT_DIRECTORY + @path)
-    yaml_str = ERB.new(erb_str).result_with_hash(params)
-    yaml = adjust_indentation(yaml_str)
-    YAML.load(yaml)
-  end
-
-  def self.load(path, params: {})
-    new(path).load(params)
+    yaml = ERB.new(read_yaml_with_embedded_ruby).result_with_hash(params)
+    YAML.load(adjust_indentation(yaml))
   end
 
   private
+
+  def read_yaml_with_embedded_ruby
+    File.read(DEFAULT_DIRECTORY + @path)
+  end
 
   def adjust_indentation(str)
     str.gsub(/\r\n/, "\n  ")
