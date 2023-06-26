a570cdc023c2cc72503a8abdf31750a9e8266457
wata00913 <175d8639@gmail.com>
2023-06-26 15:51:55 +0900

    モジュールからクラスへの変更とモジュールを削除しクラスにまとめた

--- app/models/ogp/image/attachment_processor.rb
+++ app/models/ogp/image/attachment_processor.rb
@@ -2,24 +2,54 @@
 
 module Ogp
   module Image
-    module AttachmentProcessor
-      extend Ogp::Image
+    class AttachmentProcessor
+      WIDTH = 1200
+      HEIGHT = 630
 
-      def self.fit_to_size(attached_one)
-        blob = attached_one.blob
-        blob.analyze unless blob.analyzed?
-        width = blob.metadata[:width]
-        height = blob.metadata[:height]
+      class << self
+        def fit_to_size(attached_one)
+          blob = attached_one.blob
+          blob.analyze unless blob.analyzed?
+          width = blob.metadata[:width]
+          height = blob.metadata[:height]
 
-        return nil if just_fit_to_size?(width: width, height: height)
+          return nil if just_fit_to_size?(width: width, height: height)
 
-        opts = fit_to_size_option(width: width, height: height)
+          opts = fit_to_size_option(width: width, height: height)
 
-        fitted = attached_one.variant(**opts).processed.image.blob
+          fitted = attached_one.variant(**opts).processed.image.blob
 
-        # 変形前の画像関連データの削除にpurge、purge_laterは不要。
-        # データの削除はattachで担える
-        attached_one.attach(fitted)
+          # 変形前の画像関連データの削除にpurge、purge_laterは不要。
+          # データの削除はattachで担える
+          attached_one.attach(fitted)
+        end
+
+        def just_fit_to_size?(width:, height:)
+          width == WIDTH && height == HEIGHT
+        end
+
+        def fit_to_size_option(width:, height:)
+          # ハッシュを返すが処理順(リサイズ->切り抜き)にオプションを記述
+          {}.merge(resize_option(width: width, height: height), clip_option)
+        end
+
+        def resize_option(width:, height:)
+          ratio_w = WIDTH.to_f / width
+          ratio_h = HEIGHT.to_f / height
+
+          if ratio_w > ratio_h
+            { resize: "#{WIDTH}x" }
+          else
+            { resize: "x#{HEIGHT}" }
+          end
+        end
+
+        def clip_option
+          { gravity: :center,
+            # 仮想キャンバスのメタデータをリセットするために!を末尾につける
+            # +repageは指定できないため、+repage相当の動作をする!を採用
+            crop: "#{WIDTH}x#{HEIGHT}+0+0!" }
+        end
       end
     end
   end
--- test/models/ogp/image/attachment_processor_test.rb
+++ test/models/ogp/image/attachment_processor_test.rb
@@ -14,6 +14,24 @@ module Ogp
         assert_equal({ width: 1200, height: 630 },
                      article.thumbnail.blob.metadata.slice(:width, :height).symbolize_keys)
       end
+
+      test '.just_fit_to_size?' do
+        assert AttachmentProcessor.just_fit_to_size?(width: 1200, height: 630)
+        assert_not AttachmentProcessor.just_fit_to_size?(width: 1201, height: 630)
+        assert_not AttachmentProcessor.just_fit_to_size?(width: 1200, height: 631)
+      end
+
+      test '.resize_option return 1200x when width magnification is grater than height magnification' do
+        assert_equal({ resize: '1200x' }, AttachmentProcessor.resize_option(width: 500, height: 500))
+        assert_equal({ resize: '1200x' }, AttachmentProcessor.resize_option(width: 500, height: 630))
+        assert_equal({ resize: '1200x' }, AttachmentProcessor.resize_option(width: 1500, height: 1500))
+      end
+
+      test '.resize_option return x630 when height magnification is grater than width magnification' do
+        assert_equal({ resize: 'x630' }, AttachmentProcessor.resize_option(width: 1000, height: 500))
+        assert_equal({ resize: 'x630' }, AttachmentProcessor.resize_option(width: 1200, height: 500))
+        assert_equal({ resize: 'x630' }, AttachmentProcessor.resize_option(width: 1500, height: 700))
+      end
     end
   end
 end
