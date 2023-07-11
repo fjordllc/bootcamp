a8a1a8abae68bb5825a7bfccbaa586510ee19a2a
monyatto <atsuuu.0925@gmail.com>
2023-07-11 13:24:46 +0900

    テストを修正

--- test/system/categories/practices_test.rb
+++ test/system/categories/practices_test.rb
@@ -3,6 +3,18 @@
 require 'application_system_test_case'
 
 class Categories::PracticesTest < ApplicationSystemTestCase
+  test 'admin user can access practices sort page' do
+    visit_with_auth category_practices_path(categories(:category2)), 'komagata'
+    within first('.admin-table__item-value.is-text-align-center.is-grab') do
+      assert_selector '.js-grab'
+    end
+  end
+
+  test 'non-admin user cannot access practices sort page' do
+    visit_with_auth category_practices_path(categories(:category2)), 'kimura'
+    assert_text '管理者としてログインしてください'
+  end
+
   test 'sorting practices of a category' do
     visit_with_auth course_practices_path(courses(:course1)), 'kimura'
     assert_equal all('span.category-practices-item__title-link-label')[0].text, practices(:practice1).title
@@ -31,10 +43,5 @@ class Categories::PracticesTest < ApplicationSystemTestCase
     visit_with_auth course_practices_path(courses(:course1)), 'kimura'
     assert_equal all('span.category-practices-item__title-link-label')[0].text, practices(:practice1).title
     assert_equal all('span.category-practices-item__title-link-label')[1].text, practices(:practice3).title
-  end
-
-  test 'non-admin user cannot access practices sort page' do
-    visit_with_auth category_practices_path(categories(:category2)), 'kimura'
-    assert_text '管理者としてログインしてください'
   end
 end
