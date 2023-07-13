4b362753c7e936e5b12e1fb693bc6e0615b21a89
yuma-matsui <oshino.iiyo@gmail.com>
2023-07-13 18:25:52 +0900

    FAQの編集、更新機能を追加した

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
     resources :campaigns, only: %i(new create index edit update)
     resources :inquiries, only: %i(index show)
     resources :invitation_url, only: %i(index)
-    resources :faqs, only: %i(index new create), controller: 'faq'
+    resources :faqs, controller: 'faq'
   end
 end
