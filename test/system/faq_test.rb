1fec33ddac2dbba9e6a3b2369c9b576e40b90449
yuma-matsui <oshino.iiyo@gmail.com>
2023-07-14 16:23:38 +0900

    FAQ一覧ページのシステムテストを修正した

--- test/system/faq_test.rb
+++ test/system/faq_test.rb
@@ -5,6 +5,6 @@ require 'application_system_test_case'
 class FAQTest < ApplicationSystemTestCase
   test 'show listing FAQs' do
     visit faq_path
-    FAQ.all.each { |faq| assert_text "#{faq.question}？" }
+    assert_selector '.faqs-item', count: FAQ.all.size
   end
 end
