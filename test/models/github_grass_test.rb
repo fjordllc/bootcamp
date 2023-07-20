3d6d5447891edfc28d398f053fcb44431246a62d
Masaki Komagata <komagata@gmail.com>
2023-07-21 01:33:49 +0900

    rubocopの指摘点を修正

--- test/models/github_grass_test.rb
+++ test/models/github_grass_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class GithubGrassTest < ActiveSupport::TestCase
   test '#fetch' do
     skip 'skip until you can keep up with github spec changes'
-    
+
     VCR.use_cassette 'github_grass/fetch' do
       assert_match(/svg/, GithubGrass.new('komagata').fetch)
       assert_no_match(/svg/, GithubGrass.new('komagata1234').fetch)
