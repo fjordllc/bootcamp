0637c88e17f17d2b90b3f69560dc67bc0566e3a0
yuma-matsui <oshino.iiyo@gmail.com>
2023-07-21 13:06:04 +0900

    FAQテーブルにpositionカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -271,6 +271,7 @@ ActiveRecord::Schema.define(version: 2024_06_17_061351) do
     t.string "question", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "position"
     t.index ["answer", "question"], name: "index_faqs_on_answer_and_question", unique: true
     t.index ["question"], name: "index_faqs_on_question", unique: true
   end
