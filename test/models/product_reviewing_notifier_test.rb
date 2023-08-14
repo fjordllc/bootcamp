7cddedbee2d65b61abfdbafca9f8038239256c97
rira <101010hayakawa@gmail.com>
2023-08-14 21:53:28 +0900

    setupとteardownが不要な書き方に変更

--- test/models/product_reviewing_notifier_test.rb
+++ test/models/product_reviewing_notifier_test.rb
@@ -5,32 +5,18 @@ require 'test_helper'
 class ProductReviewingNotifierTest < ActiveSupport::TestCase
   include ActiveJob::TestHelper
 
-  setup do
-    @delivery_mode = AbstractNotifier.delivery_mode
-    AbstractNotifier.delivery_mode = :normal
-  end
-
-  teardown do
-    AbstractNotifier.delivery_mode = @delivery_mode
-  end
-
   test '#call' do
     product = products(:product72)
-
-    assert_difference -> { Notification.count }, 1 do
-      perform_enqueued_jobs do
-        ProductReviewingNotifier.new.call(product)
-      end
+    assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 1 do
+      ProductReviewingNotifier.new.call(product)
     end
   end
 
   test 'does not notify when checker_id is nil' do
     product = products(:product73)
 
-    assert_difference -> { Notification.count }, 0 do
-      perform_enqueued_jobs do
-        ProductReviewingNotifier.new.call(product)
-      end
+    assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 0 do
+      ProductReviewingNotifier.new.call(product)
     end
   end
 end
