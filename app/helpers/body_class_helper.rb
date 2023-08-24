9e93bb7e5219099d707772781912946e1eb6cd07
OdenTakashi <odentakashidesu@icloud.com>
2023-08-24 09:47:57 +0900

    controller_classメソッドとして切り出した

--- app/helpers/body_class_helper.rb
+++ app/helpers/body_class_helper.rb
@@ -38,9 +38,12 @@ module BodyClassHelper
     controller_path.include?('admin/')
   end
 
+  def controller_class
+    "#{qualified_controller_name} #{qualified_page_name}"
+  end
+
   def body_class(options = {})
     extra_body_classes_symbol = options[:extra_body_classes_symbol] || :extra_body_classes
-    controller_class = "#{qualified_controller_name} #{qualified_controller_name}-#{action_name}"
     basic_body_class = "#{controller_class} is-#{page_category} is-#{page_area} is-#{Rails.env} #{adviser_mode}"
 
     if content_for?(extra_body_classes_symbol)
