e53a80321a985099fbe30940f5760806121476a4
Kassy0220 <shinbokutaro0220@gmail.com>
2023-08-24 14:49:51 +0900

    リダイレクトの処理をConcernに移動するように修正

---
+++ app/controllers/concerns/redirection.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Redirection
+  extend ActiveSupport::Concern
+
+  def redirect_url(resource)
+    resource.wip? ? polymorphic_url(resource, action: :edit) : polymorphic_url(resource)
+  end
+end
