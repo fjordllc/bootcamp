c8a9cfafebb40455d5f987d1dcbaa33337960123
tomo <o_tomo_1990@yahoo.co.jp>
2023-08-24 20:38:46 +0900

    not_auto_retireをauto_retireに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -235,7 +235,7 @@ class User < ApplicationRecord
   scope :retired, -> { where.not(retired_on: nil) }
   scope :unretired, -> { where(retired_on: nil) }
   scope :hibernated_for, ->(period) { where(hibernated_at: nil..period.ago) }
-  scope :auto_retire, -> { where.not(not_auto_retire: true) }
+  scope :auto_retire, -> { where(auto_retire: true) }
   scope :advisers, -> { where(adviser: true) }
   scope :not_advisers, -> { where(adviser: false) }
   scope :students_and_trainees, lambda {
--- db/migrate/20230506121936_add_not_auto_retire_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNotAutoRetireToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :not_auto_retire, :boolean, default: false
-  end
-end
