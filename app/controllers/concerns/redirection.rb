7047c7b51d893a6234a8718d29ca3bb9f415ef25
Kassy0220 <shinbokutaro0220@gmail.com>
2023-08-30 08:23:42 +0900

    tracerouteの設定を削除する様に修正

--- app/controllers/concerns/redirection.rb
+++ app/controllers/concerns/redirection.rb
@@ -3,6 +3,8 @@
 module Redirection
   extend ActiveSupport::Concern
 
+  private
+
   def redirect_url(resource)
     resource.wip? ? polymorphic_url(resource, action: :edit) : polymorphic_url(resource)
   end
