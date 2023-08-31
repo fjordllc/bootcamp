8fbcaaa6da26bdf46de392770bb7746d3444193d
machida <machida@fjord.jp>
2023-08-31 17:14:45 +0900

    :cop:

--- app/models/discord_profile.rb
+++ app/models/discord_profile.rb
@@ -12,17 +12,25 @@ class DiscordProfile < ApplicationRecord
   with_options if: -> { validation_context != :retirement } do
     validates :account_name,
               allow_blank: true,
-              format: {
-                with: /\A[\w\.]+\z/,
-                message: 'はアンダースコアとピリオド以外の特殊文字を使用できません。'
-              },
-              format: {
-                without: /\.\./,
-                message: 'にピリオドを2つ連続して使用することはできません。'
-              },
               length: {
                 in: 2..32,
                 message: 'は2文字以上32文字以内で入力してください。'
               }
+    validate :validate_only_underscore_and_period
+    validate :no_consecutive_periods
+  end
+
+  private
+
+  def validate_only_underscore_and_period
+    return if account_name.match?(/\A[\w.]+\z/)
+
+    errors.add(:account_name, 'はアンダースコアとピリオド以外の特殊文字を使用できません。')
+  end
+
+  def no_consecutive_periods
+    if account_name&.include?('..')
+      errors.add(:account_name, 'にピリオドを2つ連続して使用することはできません。')
+    end
   end
 end
