7db66c0d0632bb4dcc8bfef2ecf01286b39ec491
OdenTakashi <odentakashidesu@icloud.com>
2023-08-31 11:28:12 +0900

    qualified_controller_nameメソッドのテストを追加
    以下の3つのテストケースを追加
    コントローラーにスラッシュが一つのみの場合
    コントローラーにスラッシュがない場合
    コントローラーにスラッシュが複数ある場合

--- test/helpers/body_class_helper.rb
+++ test/helpers/body_class_helper.rb
@@ -3,5 +3,25 @@
 require 'test_helper'
 
 class BodyClassHelperTest < ActionView::TestCase
+  include TagHelper
 
+  test 'qualified_controller_name' do
+    def controller_path
+      'practices/products'
+    end
+
+    assert_equal 'practices-products', qualified_controller_name
+
+    def controller_path
+      'practices'
+    end
+
+    assert_equal 'practices', qualified_controller_name
+
+    def controller_path
+      'admin/users/password'
+    end
+
+    assert_equal 'admin-users-password', qualified_controller_name
+  end
 end
