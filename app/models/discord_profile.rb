7eff814f94fbc3b60c2cb28f841476dd74d6226e
machida <machida@fjord.jp>
2023-08-31 16:40:53 +0900

    discord_account を使わず DiscordProfile account_name に統一

--- app/models/discord_profile.rb
+++ app/models/discord_profile.rb
@@ -7,14 +7,22 @@ class DiscordProfile < ApplicationRecord
             format: {
               allow_blank: true,
               with: %r{\Ahttps://discord\.com/channels/\d+/\d+\z},
-              message: 'はDiscordのチャンネルURLを入力してください'
+              message: 'は https://discord.com/channels/ で始まる Discord のチャンネル URL を入力してください。'
             }
   with_options if: -> { validation_context != :retirement } do
     validates :account_name,
+              allow_blank: true,
               format: {
-                allow_blank: true,
-                with: /\A[^\s\p{blank}].*[^\s\p{blank}]#\d{4}\z/,
-                message: 'は「ユーザー名#４桁の数字」で入力してください'
+                with: /\A[\w\.]+\z/,
+                message: 'はアンダースコアとピリオド以外の特殊文字を使用できません。'
+              },
+              format: {
+                without: /\.\./,
+                message: 'にピリオドを2つ連続して使用することはできません。'
+              },
+              length: {
+                in: 2..32,
+                message: 'は2文字以上32文字以内で入力してください。'
               }
   end
 end
