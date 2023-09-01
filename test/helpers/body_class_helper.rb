bddf05eb4d91b8ec80b349885d83f5bd1294f87b
OdenTakashi <odentakashidesu@icloud.com>
2023-09-01 10:43:04 +0900

    controller_classのテストを追加
    include TagHelperが別moduleとぶつかり入らないためqualified_page_nameをテスト内に追加

--- test/helpers/body_class_helper.rb
+++ test/helpers/body_class_helper.rb
@@ -3,8 +3,6 @@
 require 'test_helper'
 
 class BodyClassHelperTest < ActionView::TestCase
-  include TagHelper
-
   test 'qualified_controller_name' do
     def controller_path
       'practices/products'
@@ -23,5 +21,33 @@ class BodyClassHelperTest < ActionView::TestCase
     end
 
     assert_equal 'admin-users-password', qualified_controller_name
+  end
+
+  test 'controller_class' do
+    def qualified_page_name
+      "#{qualified_controller_name}-#{action_name}"
+    end
+
+    def controller_path
+      'practices/products'
+    end
+
+    def action_name
+      'index'
+    end
+
+    assert_equal 'practices-products practices-products-index', controller_class
+
+    def controller_path
+      'practices'
+    end
+
+    assert_equal 'practices practices-index', controller_class
+
+    def controller_path
+      'admin/users/password'
+    end
+
+    assert_equal 'admin-users-password admin-users-password-index', controller_class
   end
 end
