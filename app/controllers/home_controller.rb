8dd04e2487e889a40ddfbc9f28228ffbb574e914
fuwa <fuwafuwa41.glr@gmail.com>
2023-09-01 21:52:25 +0900

    全ての定期イベントを表示&参加登録済のイベントの表記を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -49,14 +49,15 @@ class HomeController < ApplicationController
 
   def display_events_on_dashboard
     @today_events = (Event.today_events.related_to(current_user) \
-                     + RegularEvent.today_events.participated_by(current_user))
+                     + RegularEvent.today_events)
                     .sort_by { |e| e.start_at.strftime('%H:%M') }
     @tomorrow_events = (Event.tomorrow_events.related_to(current_user) \
-                        + RegularEvent.tomorrow_events.participated_by(current_user))
+                        + RegularEvent.tomorrow_events)
                        .sort_by { |e| e.start_at.strftime('%H:%M') }
     @day_after_tomorrow_events = (Event.day_after_tomorrow_events.related_to(current_user) \
-                                  + RegularEvent.day_after_tomorrow_events.participated_by(current_user))
+                                  + RegularEvent.day_after_tomorrow_events)
                                  .sort_by { |e| e.start_at.strftime('%H:%M') }
+                          .sort_by { |e| e.start_at.strftime('%H:%M') }
   end
 
   def display_welcome_message_for_adviser
