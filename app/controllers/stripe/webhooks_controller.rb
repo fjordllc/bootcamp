04f643ff07e420ef59cf6648850f6731f62814f3
komagata <komagata@gmail.com>
2023-09-06 08:51:04 +0900

    webhookでendpoint_secretが正しく指定できていなかったのを修正

--- app/controllers/stripe/webhooks_controller.rb
+++ app/controllers/stripe/webhooks_controller.rb
@@ -1,18 +1,27 @@
 # frozen_string_literal: true
 
 class Stripe::WebhooksController < ApplicationController
-  protect_from_forgery
+  SECRET = Rails.application.secrets['stripe'][:endpoint_secret]
+  ERROR_EVENTS = %w[
+    payment_intent.payment_failed
+    payment_intent.requires_payment_method
+    payment_intent.canceled
+    invoice.payment_failed
+  ].freeze
+
+  protect_from_forgery except: :create
   skip_before_action :require_active_user_login, raise: false
 
   def create
     event = Webhook.construct_event(
       payload: request.body.read,
-      signature: request.env['HTTP_STRIPE_SIGNATURE']
+      signature: request.env['HTTP_STRIPE_SIGNATURE'],
+      endpoint_secret: SECRET
     )
 
     if event
       case event['type']
-      when 'payment_intent.payment_failed'
+      when *ERROR_EVENTS
         intent = event['data']['object']
         message = <<~MESSAGE
           支払いが失敗しました。詳細は下記を参照してください。
