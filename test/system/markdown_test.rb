cc0ad9979ceaf51f376338474dc7f97d0ee46133
djkazunoko <djkazunoko@gmail.com>
2023-09-06 10:19:57 +0900

    念の為fill_in直後にassert_fieldで確認しとく

--- test/system/markdown_test.rb
+++ test/system/markdown_test.rb
@@ -31,9 +31,11 @@ class MarkdownTest < ApplicationSystemTestCase
   test 'should automatically create Markdown link by pasting URL into selected text' do
     visit_with_auth new_report_path, 'komagata'
     fill_in('report[description]', with: 'https://bootcamp.fjord.jp/')
+    assert_field('report[description]', with: 'https://bootcamp.fjord.jp/')
     cmd_ctrl = page.driver.browser.capabilities.platform_name.include?('mac') ? :command : :control
     find('.js-report-content').native.send_keys([cmd_ctrl, 'a'], [cmd_ctrl, 'x'])
     fill_in('report[description]', with: 'FBC')
+    assert_field('report[description]', with: 'FBC')
     # クリップボードを読み取る権限を付与
     cdp_permission = {
       origin: page.server_url,
