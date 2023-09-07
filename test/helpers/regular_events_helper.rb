bdb422214007998c148377f88452014b6e3d7584
Kassy0220 <shinbokutaro0220@gmail.com>
2023-09-07 21:21:44 +0900

    ヘルパーメソッド修正に伴いテストを修正

--- test/helpers/regular_events_helper.rb
+++ test/helpers/regular_events_helper.rb
@@ -3,16 +3,16 @@
 require 'test_helper'
 
 class RegularEventsHelperTest < ActionView::TestCase
-  test '#no_holding return true when event is not held' do
+  test '#holding return true when event is held' do
+    weekdays = Time.zone.parse('2023-9-17')
     holidays = Time.zone.parse('2023-9-18')
-    weekdays = Time.zone.parse('2023-9-19')
-
-    regular_event_not_held_on_holidays = regular_events(:regular_event1)
-    assert no_holding?(regular_event_not_held_on_holidays, holidays)
-    assert_not no_holding?(regular_event_not_held_on_holidays, weekdays)
 
     regular_event_held_on_holidays = regular_events(:regular_event4)
-    assert_not no_holding?(regular_event_held_on_holidays, weekdays)
-    assert_not no_holding?(regular_event_held_on_holidays, holidays)
+    assert holding?(weekdays, regular_event_held_on_holidays)
+    assert holding?(holidays, regular_event_held_on_holidays)
+
+    regular_event_not_held_on_holidays = regular_events(:regular_event1)
+    assert holding?(weekdays, regular_event_not_held_on_holidays)
+    assert_not holding?(holidays, regular_event_not_held_on_holidays)
   end
 end
