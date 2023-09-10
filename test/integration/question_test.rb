ca35dae690c3594ba366b389695fead0c1f497bc
sochi419 <equipment2241@gmail.com>
2023-09-11 00:51:23 +0900

    testを作成 controllerファイルのフラッシュメッセージとルーティングを修正

---
+++ test/integration/question_test.rb
@@ -0,0 +1,57 @@
+require "test_helper"
+
+class QuestionTest < ActionDispatch::IntegrationTest
+  test "regular user cannot delete a question" do
+    token = create_token('kimura', 'testtest')
+    user = users(:kimura)
+
+    post user_sessions_path, params: {
+      authenticity_token: token, user: {
+        login: user.login_name, password: 'testtest'
+      }
+    }
+    follow_redirect!
+
+    question = questions(:question8)
+    delete question_path(question)
+
+    assert_redirected_to questions_path
+    assert_equal 'メンター/管理者以外は質問を削除できません。', flash[:notice]
+  end
+
+  test "admin can delete a question" do
+    token = create_token('adminonly', 'testtest')
+    user = users(:adminonly)
+
+    post user_sessions_path, params: {
+      authenticity_token: token, user: {
+        login: user.login_name, password: 'testtest'
+      }
+    }
+    follow_redirect!
+
+    question = questions(:question8)
+    delete question_path(question)
+
+    assert_redirected_to questions_path
+    assert_equal '質問を削除しました。', flash[:notice]
+  end
+
+  test "mentor can delete a question" do
+    token = create_token('mentormentaro', 'testtest')
+    user = users(:mentormentaro)
+
+    post user_sessions_path, params: {
+      authenticity_token: token, user: {
+        login: user.login_name, password: 'testtest'
+      }
+    }
+    follow_redirect!
+
+    question = questions(:question8)
+    delete question_path(question)
+
+    assert_redirected_to questions_path
+    assert_equal '質問を削除しました。', flash[:notice]
+  end
+end
