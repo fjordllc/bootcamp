04d7a57da3d9c4bc03595e206badbc7e6ad0f9d2
djkazunoko <djkazunoko@gmail.com>
2023-09-12 13:50:05 +0900

    ctrl + Aに変えてみる(文字列の選択とペーストを分けて)

--- test/system/markdown_test.rb
+++ test/system/markdown_test.rb
@@ -36,7 +36,7 @@ class MarkdownTest < ApplicationSystemTestCase
     find('#report_title').native.send_keys([cmd_ctrl, 'a'], [cmd_ctrl, 'c'])
     fill_in('report[description]', with: 'test')
     assert_field('report[description]', with: 'test')
-    find('#report_description').native.send_keys([:shift, :arrow_up])
+    find('#report_description').native.send_keys([cmd_ctrl, 'a'])
     find('#report_description').native.send_keys([cmd_ctrl, 'v'])
     assert_field('report[description]', with: 'FBC')
   end
