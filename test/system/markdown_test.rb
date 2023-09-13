64d454b1822157565b46eb04dc86d58fd462bd50
djkazunoko <djkazunoko@gmail.com>
2023-09-13 11:57:53 +0900

    元のテストに戻す

--- test/system/markdown_test.rb
+++ test/system/markdown_test.rb
@@ -28,16 +28,25 @@ class MarkdownTest < ApplicationSystemTestCase
     assert find('.js-user-icon.a-user-emoji')['data-user'].include?('mentormentaro')
   end
 
-  test 'copy and paste' do
+  test 'should automatically create Markdown link by pasting URL into selected text' do
     visit_with_auth new_report_path, 'komagata'
-    fill_in('report[title]', with: 'FBC')
-    assert_field('report[title]', with: 'FBC')
+    fill_in('report[description]', with: 'https://bootcamp.fjord.jp/')
+    assert_field('report[description]', with: 'https://bootcamp.fjord.jp/')
     cmd_ctrl = page.driver.browser.capabilities.platform_name.include?('mac') ? :command : :control
-    find('#report_title').native.send_keys([cmd_ctrl, 'a'], [cmd_ctrl, 'c'])
-    fill_in('report[description]', with: 'test')
-    assert_field('report[description]', with: 'test')
-    page.execute_script("document.querySelector('#report_description').select();")
-    find('#report_description').native.send_keys([cmd_ctrl, 'v'])
+    find('.js-report-content').native.send_keys([cmd_ctrl, 'a'], [cmd_ctrl, 'x'])
+    fill_in('report[description]', with: 'FBC')
     assert_field('report[description]', with: 'FBC')
+    # クリップボードを読み取る権限を付与
+    cdp_permission = {
+      origin: page.server_url,
+      permission: { name: 'clipboard-read' },
+      setting: 'granted'
+    }
+    page.driver.browser.execute_cdp('Browser.setPermission', **cdp_permission)
+    clip_text = page.evaluate_async_script('navigator.clipboard.readText().then(arguments[0])')
+    assert_equal 'https://bootcamp.fjord.jp/', clip_text
+    page.execute_script("document.querySelector('#report_description').select();")
+    find('.js-report-content').native.send_keys([cmd_ctrl, 'v'])
+    assert_field('report[description]', with: '[FBC](https://bootcamp.fjord.jp/)')
   end
 end
