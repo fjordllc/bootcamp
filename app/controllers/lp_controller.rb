c34e794cc66f7a5ef58ff854b1a3be9585420cd9
machida <machida@fjord.jp>
2023-09-15 15:36:11 +0900

    新しいLP用のデモの入れ物を作った

---
+++ app/controllers/lp_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class LpController < ApplicationController
+  skip_before_action :require_active_user_login, raise: false
+  layout 'lp'
+  DEFAULT_COURSE = 'Railsプログラマー'
+
+  def index
+    @mentors = User.mentors_sorted_by_created_at
+  end
+
+  def pricing; end
+
+  def faq; end
+
+  def training; end
+
+  def practices
+    @categories = Course.find_by(title: DEFAULT_COURSE).categories.preload(:practices).order(:position)
+  end
+
+  def tos; end
+
+  def pp; end
+
+  def law; end
+
+  def coc; end
+end
