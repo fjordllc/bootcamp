e66e483353bafc996f6becb398e180f4a28aacc2
Kassy0220 <shinbokutaro0220@gmail.com>
2023-09-15 10:05:00 +0900

    不要になったヘルパーを削除

--- app/helpers/regular_events_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RegularEventsHelper
-  def holding?(date, event)
-    return true unless HolidayJp.holiday?(date)
-
-    event.hold_national_holiday
-  end
-end
