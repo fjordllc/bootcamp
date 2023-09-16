9a81f89f23b87c861ef5d9d21b473c27f4a83d7d
rira <101010hayakawa@gmail.com>
2023-09-16 19:03:30 +0900

    相談部屋のコメント通知をNewspaper化した

---
+++ app/models/comment_notifier.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CommentNotifier
+  def call(payload)
+    comment = payload[:comment]
+    current_user = payload[:current_user]
+    return if current_user.nil?
+
+    commentable_path = Rails.application.routes.url_helpers.polymorphic_path(comment.commentable)
+    ActivityDelivery.with(
+      comment:,
+      receiver: comment.receiver,
+      message: "相談部屋で#{comment.sender.login_name}さんからコメントがありました。",
+      link: "#{commentable_path}#latest-comment"
+    ).notify(:came_comment)
+  end
+end
