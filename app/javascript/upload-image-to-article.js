a3c584c22b99aa1f7bda076d9a8a0b2d0aa35ce7
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-09-24 21:26:57 +0900

    prettierの整形漏れを修正する

--- app/javascript/upload-image-to-article.js
+++ app/javascript/upload-image-to-article.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const radioButtons = document.getElementsByName(
-    'article[thumbnail_type]'
-  )
+  const radioButtons = document.getElementsByName('article[thumbnail_type]')
   const fileField = document.getElementById('upload-thumbnail')
   for (const radioButton of radioButtons) {
     radioButton.addEventListener('change', () => {
