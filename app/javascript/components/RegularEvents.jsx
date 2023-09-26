aa903a0f39bcc881497e14582940ef1e040eeebe
Kassy0220 <shinbokutaro0220@gmail.com>
2023-09-27 08:52:55 +0900

    ページネーションを追加

--- app/javascript/components/RegularEvents.jsx
+++ app/javascript/components/RegularEvents.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import useSWR from 'swr'
 import fetcher from '../fetcher'
+import queryString from 'query-string'
+import Pagination from './Pagination'
 import RegularEvent from './RegularEvent'
 
 const RegularEvents = () => {
+  const per = 20
+  const neighbours = 4
+  const defaultPage = parseInt(queryString.parse(location.search).page) || 1
+  const [page, setPage] = useState(defaultPage)
+
+  useEffect(() => {
+    setPage(page)
+  }, [page])
+
   const params = new URL(location.href).searchParams
+  params.set('page', page)
   const { data, error } = useSWR(`/api/regular_events?${params}`, fetcher)
+
+  const handlePaginate = (p) => {
+    setPage(p)
+    window.history.pushState(null, null, `/regular_events?${params}`)
+  }
+
   if (error) console.warn(error)
   if (!data) return <div className="page-content loaing">ロード中…</div>
 
   return (
     <div className="page-content loaded">
+      {data.total_pages > 1 && (
+        <Pagination
+          sum={data.total_pages * per}
+          per={per}
+          neighbours={neighbours}
+          page={page}
+          onChange={(e) => handlePaginate(e.page)}
+        />
+      )}
       <div className="card-list a-card">
         {data.regular_events.map(regularEvent =>
           <RegularEvent
@@ -19,6 +46,15 @@ const RegularEvents = () => {
           />
         )}
       </div>
+      {data.total_pages > 1 && (
+        <Pagination
+          sum={data.total_pages * per}
+          per={per}
+          neighbours={neighbours}
+          page={page}
+          onChange={(e) => handlePaginate(e.page)}
+        />
+      )}
     </div>
   )
 }
