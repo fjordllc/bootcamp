df7b0715a7300e469d00d327d2dcc9faaa593021
Kassy0220 <shinbokutaro0220@gmail.com>
2023-09-26 14:10:45 +0900

    イベント表示を「開催中」または「全て」で切り替えられるようにした

--- app/javascript/components/RegularEvents.jsx
+++ app/javascript/components/RegularEvents.jsx
@@ -4,7 +4,8 @@ import fetcher from '../fetcher'
 import RegularEvent from './RegularEvent'
 
 const RegularEvents = () => {
-  const { data, error } = useSWR('/api/regular_events', fetcher)
+  const params = new URL(location.href).searchParams
+  const { data, error } = useSWR(`/api/regular_events?${params}`, fetcher)
   if (error) console.warn(error)
   if (!data) return <div className="page-content loaing">ロード中…</div>
 
