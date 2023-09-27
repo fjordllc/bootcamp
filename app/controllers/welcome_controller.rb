18db46a883c8ccbb37ad43b0aa361205c1797d07
machida <machida@fjord.jp>
2023-09-27 14:47:57 +0900

    新しいデザイン用のテンプレートを静的ページに適用した

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,11 +8,17 @@ class WelcomeController < ApplicationController
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
   end
 
-  def pricing; end
+  def pricing
+    render layout: 'lp'
+  end
 
-  def faq; end
+  def faq
+    render layout: 'lp'
+  end
 
-  def training; end
+  def training
+    render layout: 'lp'
+  end
 
   def practices
     render layout: 'lp'
