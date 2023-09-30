7fb86463fc88ca55a6c2c89d2760aae56969ebdb
Kassy0220 <shinbokutaro0220@gmail.com>
2023-09-30 18:12:19 +0900

    イベント表示の切り替え(開催中/全て)をReactで行うように修正

--- app/javascript/components/RegularEvents.jsx
+++ app/javascript/components/RegularEvents.jsx
@@ -5,23 +5,41 @@ import queryString from 'query-string'
 import Pagination from './Pagination'
 import RegularEvent from './RegularEvent'
 
+const buildParams = (targetParam, page) => {
+  const target = targetParam === 'not_finished' ? 'target=not_finished&' : ''
+  const pageNumber = `page=${page}`
+  return `${target}${pageNumber}`
+}
+
 const RegularEvents = () => {
   const per = 20
   const neighbours = 4
   const defaultPage = parseInt(queryString.parse(location.search).page) || 1
+  const [targetParam, setTargetParam] = useState('not_finished')
   const [page, setPage] = useState(defaultPage)
 
   useEffect(() => {
+    setTargetParam(targetParam)
     setPage(page)
-  }, [page])
+  }, [targetParam, page])
 
-  const params = new URL(location.href).searchParams
-  params.set('page', page)
-  const { data, error } = useSWR(`/api/regular_events?${params}`, fetcher)
+  const { data, error } = useSWR(`/api/regular_events?${buildParams(targetParam, page)}`, fetcher)
+
+  const handleNotFinishedClick = () => {
+    setTargetParam('not_finished')
+    setPage(1)
+    window.history.pushState(null, null, '/regular_events?target=not_finished')
+  }
+
+  const handleAllClick = () => {
+    setTargetParam('all')
+    setPage(1)
+    window.history.pushState(null, null, '/regular_events')
+  }
 
   const handlePaginate = (p) => {
     setPage(p)
-    window.history.pushState(null, null, `/regular_events?${params}`)
+    window.history.pushState(null, null, `/regular_events?${buildParams(targetParam, p)}`)
   }
 
   if (error) console.warn(error)
@@ -29,6 +47,11 @@ const RegularEvents = () => {
 
   return (
     <div className="page-content loaded">
+      <Navigation
+        targetParam={targetParam}
+        handleNotFinishedClick={handleNotFinishedClick}
+        handleAllClick={handleAllClick}
+      />
       {data.total_pages > 1 && (
         <Pagination
           sum={data.total_pages * per}
@@ -56,6 +79,31 @@ const RegularEvents = () => {
         />
       )}
     </div>
+  )
+}
+
+const Navigation = ({ targetParam, handleNotFinishedClick, handleAllClick }) => {
+  return (
+    <nav className="pill-nav">
+      <ul className="pill-nav__items">
+        <li className="pill-nav__item">
+          <button
+            className={`pill-nav__item-link ${targetParam === 'not_finished' ? 'is-active' : ''}`}
+            onClick={handleNotFinishedClick}
+          >
+            開催中
+          </button>
+        </li>
+        <li className="pill-nav__item">
+          <button
+            className={`pill-nav__item-link ${targetParam === 'all' ? 'is-active' : ''}`}
+            onClick={handleAllClick}
+          >
+            全て
+          </button>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
