8458846f42c453a2e45ecc5fb6b12ee39b2ed4f1
rira <101010hayakawa@gmail.com>
2023-10-04 13:58:06 +0900

    テストデータの意図をコメントに残す

--- test/models/product_update_notifier_for_watcher_test.rb
+++ test/models/product_update_notifier_for_watcher_test.rb
@@ -6,6 +6,7 @@ class ProductUpdateNotifierForWatcherTest < ActiveSupport::TestCase
   include ActiveJob::TestHelper
 
   test '#call' do
+    # watchされていて、担当のいない提出物
     product = products(:product6)
 
     assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 1 do
@@ -14,6 +15,7 @@ class ProductUpdateNotifierForWatcherTest < ActiveSupport::TestCase
   end
 
   test 'does not notify when product is wip' do
+    # watchされていて、wipの提出物
     product = products(:product5)
 
     assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 0 do
@@ -22,6 +24,7 @@ class ProductUpdateNotifierForWatcherTest < ActiveSupport::TestCase
   end
 
   test 'does not notify when watches and checker are nil' do
+    # 担当者がいなくてwatchされていない提出物
     product = products(:product73)
 
     assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 0 do
@@ -30,6 +33,7 @@ class ProductUpdateNotifierForWatcherTest < ActiveSupport::TestCase
   end
 
   test 'does not notify when current_user is admin or mentor' do
+    # watchされていて、担当のいない提出物
     product = products(:product6)
 
     assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 0 do
