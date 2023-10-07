b2755bed167c93ea4d5a343bbd7b324733023439
Koji Ishimoto <koji.ishimoto@gmail.com>
2023-10-08 00:30:26 +0900

    run prettier

--- app/javascript/components/Areas/Areas.jsx
+++ app/javascript/components/Areas/Areas.jsx
@@ -11,10 +11,10 @@ function Region({ region, areas, handleClick }) {
     <li key={region}>
       <h2>{region}</h2>
       <ul>
-        {Object.keys(areas).map(area => (
+        {Object.keys(areas).map((area) => (
           <li key={area}>
             <button onClick={() => handleClick(region, area)}>
-              { `${area}（${areas[area]})` }
+              {`${area}（${areas[area]})`}
             </button>
           </li>
         ))}
@@ -30,20 +30,20 @@ export default function Areas({ userCounts }) {
 
   const handleClick = async (region, area) => {
     const searchParams = new URLSearchParams({ region, area })
-    const newUsers = await fetcher(`${apiUrl}${searchParams}`)
-      .catch((error) => {
+    const newUsers = await fetcher(apiUrl + searchParams).catch(
+      (error) => {
         console.error(error)
-      })
+      }
+    )
     mutate(newUsers)
     setSearchParams({ region, area })
   }
 
   const onPopstate = async () => {
     const search = new URL(location).searchParams
-    const newUsers = await fetcher(`${apiUrl}${search}`)
-      .catch((error) => {
-        console.error(error)
-      })
+    const newUsers = await fetcher(apiUrl + search).catch((error) => {
+      console.error(error)
+    })
     mutate(newUsers)
   }
 
@@ -67,7 +67,7 @@ export default function Areas({ userCounts }) {
     <div className="page-body">
       <div className="container flex is-lg">
         <ul>
-          {Object.keys(userCounts).map(region => (
+          {Object.keys(userCounts).map((region) => (
             <Region
               key={region}
               region={region}
@@ -77,13 +77,14 @@ export default function Areas({ userCounts }) {
           ))}
         </ul>
         <div className="a-card">
-          {users.length > 0
-            ? <UserGroup>
-                <UserGroupHeader>{ searchParams.get('area') }</UserGroupHeader>
-                <UserGroupIcons users={users} />
-              </UserGroup>
-            : <EmptyMessage>都道府県別ユーザー一覧はありません</EmptyMessage>
-          }
+          {users.length > 0 ? (
+            <UserGroup>
+              <UserGroupHeader>{searchParams.get('area')}</UserGroupHeader>
+              <UserGroupIcons users={users} />
+            </UserGroup>
+          ) : (
+            <EmptyMessage>都道府県別ユーザー一覧はありません</EmptyMessage>
+          )}
         </div>
       </div>
     </div>
