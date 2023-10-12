d6bf5ec5dfdfa8ee785d236066af688c2a3fd097
sochi419 <equipment2241@gmail.com>
2023-10-12 12:09:15 +0900

    インデントのずれを修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -73,28 +73,28 @@ module UsersHelper
 
     ((user.automatic_retire_datetime - Time.zone.now) / 86_400).floor
   end
-end
 
-def remaining_time_until_automatic_retire(user)
-  if remaining_hours_until_automatic_retire(user) < 1
-    "#{remaining_minutes_until_automatic_retire(user)}分"
-  elsif remaining_hours_until_automatic_retire(user) < 24
-    "#{remaining_hours_until_automatic_retire(user)}時間"
-  else
-    "#{remaining_days_until_automatic_retire(user)}日"
+  def remaining_time_until_automatic_retire(user)
+    if remaining_hours_until_automatic_retire(user) < 1
+      "#{remaining_minutes_until_automatic_retire(user)}分"
+    elsif remaining_hours_until_automatic_retire(user) < 24
+      "#{remaining_hours_until_automatic_retire(user)}時間"
+    else
+      "#{remaining_days_until_automatic_retire(user)}日"
+    end
   end
-end
 
-private
+  private
 
-def remaining_hours_until_automatic_retire(user)
-  return unless user.hibernated_at
+  def remaining_hours_until_automatic_retire(user)
+    return unless user.hibernated_at
 
-  ((user.automatic_retire_datetime - Time.zone.now) / 3600).floor
-end
+    ((user.automatic_retire_datetime - Time.zone.now) / 3600).floor
+  end
 
-def remaining_minutes_until_automatic_retire(user)
-  return unless user.hibernated_at
+  def remaining_minutes_until_automatic_retire(user)
+    return unless user.hibernated_at
 
-  ((user.automatic_retire_datetime - Time.zone.now) / 60).floor
+    ((user.automatic_retire_datetime - Time.zone.now) / 60).floor
+  end
 end
