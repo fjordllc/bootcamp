69a8105d2a8ce22d86d43eabd6075610d015b009
Koji Ishimoto <koji.ishimoto@gmail.com>
2023-10-12 12:21:48 +0900

    簡易なテストの追加

---
+++ test/models/area_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AreaTest < ActiveSupport::TestCase
+  test '#users' do
+    assert_includes Area.users('米国', '海外'), users(:tom)
+    assert_includes Area.users('東京都', '関東地方'), users(:kimura)
+  end
+
+  test '#number_of_users' do
+    assert_equal Area.number_of_users, {
+      '関東地方' => { '東京都' => 1, '栃木県' => 1 },
+      '九州・沖縄地方' => { '長崎県' => 1 },
+      '海外' => { '米国' => 2, 'カナダ' => 1 }
+    }
+  end
+end
