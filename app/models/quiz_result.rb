e6b808bc4d1d9ca4858ee0d36ec643bacd09b2f0
unstoppa61e <like_a_newborn_baby@icloud.com>
2023-10-17 10:55:04 +0900

    回答時にスコアが保存されるようにした

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -2,4 +2,13 @@ class QuizResult < ApplicationRecord
   belongs_to :quiz
   belongs_to :user
   has_many :responses, dependent: :destroy
+
+  def calculate_score
+    correct_responses = responses.select do |response|
+      response.answer == response.statement.is_correct
+    end
+
+    self.score = correct_responses.count
+    save
+  end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,12 @@
 class Response < ApplicationRecord
   belongs_to :quiz_result
   belongs_to :statement
+
+  after_save :update_quiz_score
+
+  private
+
+  def update_quiz_score
+    quiz_result.calculate_score
+  end
 end
