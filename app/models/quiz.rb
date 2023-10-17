24206b9bfe5a5b24f0a5d75648c17475f4c3d983
eatplaynap <eatplaynap329@gmail.com>
2023-10-17 17:35:25 +0900

    不足していたquiz_resultsとのリレーション追加

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,6 @@
 class Quiz < ApplicationRecord
   has_many :statements, dependent: :destroy
+  has_many :quiz_results, dependent: :destroy
 
   accepts_nested_attributes_for :statements, allow_destroy: true
 end
