c5e1578e8e20d92b53e71f4db5ffe15482dd4796
rira <101010hayakawa@gmail.com>
2023-10-17 17:24:07 +0900

    ページネーション使用時の戻るボタンの不具合修正

--- app/javascript/components/Notifications.jsx
+++ app/javascript/components/Notifications.jsx
@@ -4,61 +4,40 @@ import LoadingListPlaceholder from './LoadingListPlaceholder'
 import Pagination from './Pagination'
 import queryString from 'query-string'
 import UnconfirmedLink from './UnconfirmedLink'
+import useSWR from 'swr'
+import fetcher from '../fetcher'
 
 export default function Notifications(props) {
-  const [notifications, setNotifications] = useState([])
-  const [totalPages, setTotalPages] = useState(0)
-  const per = 20
-  const neighbours = 4
-
-  const defaultPage = parseInt(queryString.parse(location.search).page) || 1
-  const [page, setPage] = useState(defaultPage)
-  const [loaded, setLoaded] = useState(false)
-
-  const getPageValueFromParameter = () => {
-    const url = new URL(location)
-    return Number(url.searchParams.get('page')) || 1
+  const target = () => {
+    return props.target ? `&target=${props.target}` : ''
   }
+  const getStatusQueryParam = () => {
+    return queryString.parse(location.search).status || ''
+  }
+  const getPageQueryParam = () => {
+    return parseInt(queryString.parse(location.search).page) || 1
+  }
+  const [statusParam, setStatusParam] = useState(getStatusQueryParam())
+  const [page, setPage] = useState(getPageQueryParam())
 
   const isUnreadPage = () => {
     const params = new URLSearchParams(location.search)
     return params.get('status') !== null && params.get('status') === 'unread'
   }
 
-  const target = () => {
-    return props.target ? `&target=${props.target}` : ''
-  }
-
   const url = () => {
     if (isUnreadPage()) {
+      console.log(page)
       return `/api/notifications.json?page=${page}&status=unread${target()}`
     } else {
+      console.log(page)
       return `/api/notifications.json?page=${page}${target()}`
     }
   }
 
-  const getNotificationsPerPage = () => {
-    fetch(url(), {
-      method: 'GET',
-      headers: { 'X-Requested-With': 'XMLHttpRequest' },
-      credentials: 'same-origin',
-      redirect: 'manual'
-    })
-      .then((response) => {
-        return response.json()
-      })
-      .then((json) => {
-        setTotalPages(json.total_pages)
-        setNotifications(json.notifications)
-        console.log('json.notifications')
-        console.log(json.notifications)
-        setLoaded(true)
-      })
-      .catch((error) => {
-        console.warn(error)
-      })
-  }
-  const paginateClickCallback = (pageNumber) => {
+  const { data, error } = useSWR(url, fetcher)
+
+  const handlePaginate = (pageNumber) => {
     setPage(pageNumber)
     const url = new URL(location)
     if (pageNumber > 1) {
@@ -66,33 +45,60 @@ export default function Notifications(props) {
     } else {
       url.searchParams.delete('page')
     }
-    history.pushState(null, null, url)
+    console.log('save history ' + url.href)
+    window.history.pushState(null, null, url)
     window.scrollTo(0, 0)
   }
 
+  const handlePopstate = () => {
+    setStatusParam(getStatusQueryParam())
+    setPage(getPageQueryParam())
+  }
+
+  window.addEventListener('popstate', handlePopstate)
+
   useEffect(() => {
-    const handlePopstate = () => {
-      console.log('page=' + page)
-      setPage(getPageValueFromParameter())
-    }
+    setStatusParam(statusParam)
+    setPage(page)
+  }, [statusParam, page])
 
-    window.addEventListener('popstate', handlePopstate)
-    getNotificationsPerPage()
-    return () => {
-      window.removeEventListener('popstate', handlePopstate)
-    }
-  }, [page])
+  return (
+    <>
+      <NotificationList
+        isUnreadPage={isUnreadPage}
+        error={error}
+        data={data}
+        page={page}
+        setPage={setPage}
+        handlePaginate={handlePaginate}
+      />
+    </>
+  )
+}
+const NotificationList = ({
+  isUnreadPage,
+  error,
+  data,
+  page,
+  handlePaginate
+}) => {
+  const per = 20
+  const neighbours = 4
+  console.log('render')
+  console.log('page=' + page)
+  if (error) {
+    console.warn(error)
+    return <div>failed to load</div>
+  }
 
-  let content
-
-  if (!loaded) {
-    content = (
+  if (!data) {
+    return (
       <div id="notifications" className="page-content loading">
         <LoadingListPlaceholder />
       </div>
     )
-  } else if (notifications.length === 0) {
-    content = (
+  } else if (data.notifications.length === 0) {
+    return (
       <div className="o-empty-message">
         <div className="o-empty-message__icon">
           <i className="fa-regular fa-smile" />
@@ -103,43 +109,39 @@ export default function Notifications(props) {
       </div>
     )
   } else {
-    content = (
+    return (
       <div id="notifications" className="page-content loaded">
-        {totalPages > 1 && (
+        {data.total_pages > 1 && (
           <nav className="pagination">
             <Pagination
-              sum={totalPages * per}
+              sum={data.total_pages * per}
               per={per}
               neighbours={neighbours}
               page={page}
-              setPage={setPage}
-              onChange={(e) => paginateClickCallback(e.page)}
+              handlePaginate={handlePaginate}
             />
           </nav>
         )}
         <div className="card-list a-card">
-          {notifications.map((notification) => {
+          {data.notifications.map((notification) => {
             return (
               <Notification key={notification.id} notification={notification} />
             )
           })}
         </div>
         <UnconfirmedLink label="未読の通知を一括で開く" />
-        {totalPages > 1 && (
+        {data.total_pages > 1 && (
           <nav className="pagination">
             <Pagination
-              sum={totalPages * per}
+              sum={data.total_pages * per}
               per={per}
               neighbours={neighbours}
               page={page}
-              setPage={setPage}
-              onChange={(e) => paginateClickCallback(e.page)}
+              handlePaginate={handlePaginate}
             />
           </nav>
         )}
       </div>
     )
   }
-
-  return <>{content}</>
 }
