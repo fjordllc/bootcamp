9ebb04d9e9e977d09ea84898edfa4793bf1ff2ee
eatplaynap <eatplaynap329@gmail.com>
2023-10-17 11:42:36 +0900

    exec: bin/rails g controller quiz_results create

---
+++ app/controllers/quiz_results_controller.rb
@@ -0,0 +1,4 @@
+class QuizResultsController < ApplicationController
+  def create
+  end
+end
---
+++ test/controllers/quiz_results_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class QuizResultsControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get quiz_results_create_url
+    assert_response :success
+  end
+end
