b1880f963e2eac3fbe2b2b69875b2cf2b20c6a0c
eatplaynap <eatplaynap329@gmail.com>
2023-10-21 14:06:55 +0900

    新規クイズ作成後クイズ一覧画面に遷移させる

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -29,7 +29,7 @@ class QuizzesController < ApplicationController
 
     respond_to do |format|
       if @quiz.save
-        format.html { redirect_to quiz_url(@quiz), notice: 'Quiz was successfully created.' }
+        format.html { redirect_to quizzes_path, notice: 'Quiz was successfully created.' }
         format.json { render :show, status: :created, location: @quiz }
       else
         format.html { render :new, status: :unprocessable_entity }
