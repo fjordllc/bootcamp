756990172c6a73b1c92808617b751e7776c943d4
rira <101010hayakawa@gmail.com>
2023-10-23 10:08:47 +0900

    一括で開くボタンの修正

--- app/javascript/components/UnconfirmedLink.jsx
+++ app/javascript/components/UnconfirmedLink.jsx
@@ -2,9 +2,8 @@ import React, { useCallback } from 'react'
 
 export default function UnconfirmedLink({ label }) {
   const openUnconfirmedItems = useCallback(() => {
-    const links = document.querySelectorAll(
-      '.card-list-item .js-unconfirmed-link'
-    )
+    const links = document.querySelectorAll('.card-list-item-title__link')
+    console.log(links)
     links.forEach((link) => {
       window.open(link.href, '_target', 'noopener')
     })
