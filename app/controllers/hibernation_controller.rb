e6b480c8a350c3350eea8b6ea5e42a4545401c23
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-11-04 10:39:53 +0900

    休会したら定期イベントの主催者がkomagataになるように修正

--- app/controllers/hibernation_controller.rb
+++ app/controllers/hibernation_controller.rb
@@ -18,6 +18,7 @@ class HibernationController < ApplicationController
       destroy_subscription!
       notify_to_chat
       notify_to_mentors_and_admins
+      current_user.delete_organizer
       logout
       redirect_to hibernation_path
     else
