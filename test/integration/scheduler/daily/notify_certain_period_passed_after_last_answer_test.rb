01c8b866bea1915057120bb52a509ff1ab6249ed
tomo <o_tomo_1990@yahoo.co.jp>
2023-11-07 20:22:30 +0900

    コメントを削除

--- test/integration/scheduler/daily/notify_certain_period_passed_after_last_answer_test.rb
+++ test/integration/scheduler/daily/notify_certain_period_passed_after_last_answer_test.rb
@@ -7,18 +7,15 @@ class Scheduler::Daily::NotifyCertainPeriodPassedAfterLastAnswerTest < ActionDis
   include MockEnvHelper
 
   test 'token evaluation' do
-    # サーバー側のTOKENが未設定
     mock_env('TOKEN' => '') do
       get scheduler_daily_notify_certain_period_passed_after_last_answer_path(token: '')
       assert_response 401
     end
 
     mock_env('TOKEN' => 'token') do
-      # リクエストで指定したtokenが不正
       get scheduler_daily_notify_certain_period_passed_after_last_answer_path(token: 'invalid')
       assert_response 401
 
-      # tokenが正しい
       Question.stub(:notify_certain_period_passed_after_last_answer, nil) do
         get scheduler_daily_notify_certain_period_passed_after_last_answer_path(token: 'token')
         assert_response 200
