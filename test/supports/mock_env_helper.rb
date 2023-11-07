dcb3f103277cc08f2aa96f5af75a1d9d552bf6be
tomo <o_tomo_1990@yahoo.co.jp>
2023-11-07 20:42:56 +0900

    環境変数をモックする処理をstubを使って書き換える

--- test/supports/mock_env_helper.rb
+++ test/supports/mock_env_helper.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 module MockEnvHelper
-  def mock_env(partial_env_hash)
-    old = ENV.to_hash
-    ENV.update partial_env_hash
-    begin
-      yield
-    ensure
-      ENV.replace old
-    end
+  def mock_env(hash_for_mock, &block)
+    env_hash = ENV.to_hash.merge(hash_for_mock)
+    ENV.stub(:[], ->(key) { env_hash[key] }, &block)
   end
 end
