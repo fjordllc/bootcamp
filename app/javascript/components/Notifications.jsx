fb95b982570cf640180a3434cfb347b0597a6d5b
rira <101010hayakawa@gmail.com>
2023-11-08 14:41:41 +0900

    未返信、全てボタンが表示されない不具合修正

--- app/javascript/components/Notifications.jsx
+++ app/javascript/components/Notifications.jsx
@@ -35,24 +35,6 @@ export default function Notifications({ isMentor }) {
     return url.toString()
   }
 
-  const filterButtonUrl = (status) => {
-    const searchParams = new URLSearchParams()
-    const params = new URLSearchParams(location.search)
-    const target = params.get('target')
-    if (target) {
-      searchParams.set('target', target)
-    }
-
-    if (status) {
-      searchParams.set('status', status)
-    }
-
-    const url = new URL('/notifications', location.origin)
-    url.search = searchParams
-
-    return url.toString()
-  }
-
   const { data, error } = useSWR(apiUrl, fetcher)
   if (error) {
     console.warn(error)
@@ -68,43 +50,22 @@ export default function Notifications({ isMentor }) {
     )
   } else if (data.notifications.length === 0) {
     return (
-      <div className="o-empty-message">
-        <div className="o-empty-message__icon">
-          <i className="fa-regular fa-smile" />
+      <>
+        <FilterButton />
+        <div className="o-empty-message">
+          <div className="o-empty-message__icon">
+            <i className="fa-regular fa-smile" />
+          </div>
+          <p className="o-empty-message__text">
+            {isUnreadPage() ? '未読の通知はありません' : '通知はありません'}
+          </p>
         </div>
-        <p className="o-empty-message__text">
-          {isUnreadPage() ? '未読の通知はありません' : '通知はありません'}
-        </p>
-      </div>
+      </>
     )
   } else {
-    const params = new URLSearchParams(location.search)
     return (
       <>
-        <nav className="pill-nav">
-          <div className="container">
-            <ul className="pill-nav__items">
-              <li className="pill-nav__item">
-                <a
-                  className={`pill-nav__item-link ${
-                    params.get('status') === 'unread' ? 'is-active' : ''
-                  }`}
-                  href={filterButtonUrl('unread')}>
-                  未読
-                </a>
-              </li>
-              <li className="pill-nav__item">
-                <a
-                  className={`pill-nav__item-link ${
-                    params.get('status') === 'unread' ? '' : 'is-active'
-                  }`}
-                  href={filterButtonUrl()}>
-                  全て
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <FilterButton />
         <div id="notifications" className="page-content loaded">
           {data.total_pages > 1 && (
             <nav className="pagination">
@@ -143,4 +104,52 @@ export default function Notifications({ isMentor }) {
       </>
     )
   }
+}
+
+const FilterButton = () => {
+  const filterButtonUrl = (status) => {
+    const searchParams = new URLSearchParams()
+    const params = new URLSearchParams(location.search)
+    const target = params.get('target')
+    if (target) {
+      searchParams.set('target', target)
+    }
+
+    if (status) {
+      searchParams.set('status', status)
+    }
+
+    const url = new URL('/notifications', location.origin)
+    url.search = searchParams
+
+    return url.toString()
+  }
+
+  const params = new URLSearchParams(location.search)
+  return (
+    <nav className="pill-nav">
+      <div className="container">
+        <ul className="pill-nav__items">
+          <li className="pill-nav__item">
+            <a
+              className={`pill-nav__item-link ${
+                params.get('status') === 'unread' ? 'is-active' : ''
+              }`}
+              href={filterButtonUrl('unread')}>
+              未読
+            </a>
+          </li>
+          <li className="pill-nav__item">
+            <a
+              className={`pill-nav__item-link ${
+                params.get('status') === 'unread' ? '' : 'is-active'
+              }`}
+              href={filterButtonUrl()}>
+              全て
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
