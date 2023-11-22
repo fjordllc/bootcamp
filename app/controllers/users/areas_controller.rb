e6f081974162df80abdcbba366438d6f12768526
Koji Ishimoto <koji.ishimoto@gmail.com>
2023-11-22 18:47:28 +0900

    areaからregionへと名前を変更

--- app/controllers/users/areas_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Users::AreasController < ApplicationController
-  def index
-    @number_of_users_by_region = Area.number_of_users_by_region
-  end
-end
--- test/system/user/regions_test.rb
+++ test/system/user/regions_test.rb
@@ -2,11 +2,11 @@
 
 require 'application_system_test_case'
 
-class User::AreasTest < ApplicationSystemTestCase
-  test 'show users devided by areas' do
+class User::RegionsTest < ApplicationSystemTestCase
+  test 'show users devided by regions' do
     visit_with_auth '/users/areas', 'komagata'
     assert_equal '都道府県別ユーザー一覧 | FBC', title
-    within "[data-testid='areas']" do
+    within "[data-testid='regions']" do
       assert_text '関東地方'
       assert_selector "[data-login-name='kimura']"
     end
