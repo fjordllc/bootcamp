bb4791f95546794b9e43462dea94560b0243ab54
Koji Ishimoto <koji.ishimoto@gmail.com>
2023-11-22 19:48:02 +0900

    MultiColumnsを追加

--- app/javascript/components/UserList/FilterByRegion.jsx
+++ app/javascript/components/UserList/FilterByRegion.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+// components
 import LoadingListPlaceholder from '../LoadingListPlaceholder'
 import EmptyMessage from '../ui/EmptyMessage'
 import { UserGroup } from '../ui/UserGroup'
-import useSWR from 'swr'
-import fetcher from '../../fetcher'
+import { MultiColumns } from '../layout/MultiColumns'
+// hooks
 import { useSearchParams, usePopstate } from '../../hooks/useSearchParams'
+import useSWR from 'swr'
+// helper
+import fetcher from '../../fetcher'
 
-function Region({ region, numberOfUsersByRegion, handleClick }) {
+function RegionCard({ region, numberOfUsersByRegion, updateSelectedSubdivisionOrCountry }) {
   return (
-    <nav key={region} className="page-nav a-card">
+    <nav className="page-nav a-card">
       <header className="page-nav__header">
         <h2 className="page-nav__title">
           <span className="page-nav__title-inner">{region}</span>
@@ -19,7 +23,7 @@ function Region({ region, numberOfUsersByRegion, handleClick }) {
         {Object.keys(numberOfUsersByRegion).map((subdivisionOrCountry) => (
           <li key={subdivisionOrCountry} className="page-nav__item">
             <button
-              onClick={() => handleClick(region, subdivisionOrCountry)}
+              onClick={() => updateSelectedSubdivisionOrCountry(region, subdivisionOrCountry)}
               className="page-nav__item-link a-text-link">
               {`${subdivisionOrCountry}（${numberOfUsersByRegion[subdivisionOrCountry]})`}
             </button>
@@ -38,7 +42,7 @@ export default function FilterByRegion({ numberOfUsersByRegion }) {
   const apiUrl = '/api/users/regions?'
   const { data: users, error, mutate } = useSWR(apiUrl + searchParams, fetcher)
 
-  const handleClick = async (region, subdivisionOrCountry) => {
+  const updateSelectedSubdivisionOrCountry = async (region, subdivisionOrCountry) => {
     const search = new URLSearchParams({ region, subdivision_or_country: subdivisionOrCountry })
     const newUsers = await fetcher(apiUrl + search).catch((error) => {
       console.error(error)
@@ -67,35 +71,31 @@ export default function FilterByRegion({ numberOfUsersByRegion }) {
   }
 
   return (
-    <div data-testid="regions" className="page-body">
-      <div className="container is-lg">
-        <div className="page-body__columns is-reverse">
-          <div className="page-body__column is-sub is-sm">
-            {Object.keys(numberOfUsersByRegion).map((region) => (
-              <Region
-                key={region}
-                region={region}
-                numberOfUsersByRegion={numberOfUsersByRegion[region]}
-                handleClick={handleClick}
-              />
-            ))}
-          </div>
-          <div className="page-body__column is-main">
-            <section className="a-card">
-              {users.length > 0 ? (
-                <UserGroup>
-                  <UserGroup.Header>
-                    {searchParams.get('subdivision_or_country') || '東京都'}
-                  </UserGroup.Header>
-                  <UserGroup.Icons users={users} />
-                </UserGroup>
-              ) : (
-                <EmptyMessage>都道府県別ユーザー一覧はありません</EmptyMessage>
-              )}
-            </section>
-          </div>
-        </div>
-      </div>
-    </div>
+    <MultiColumns data-testid="regions" isReverse>
+      <MultiColumns.Sub className='is-sm'>
+        {Object.keys(numberOfUsersByRegion).map((region) => (
+          <RegionCard
+            key={region}
+            region={region}
+            numberOfUsersByRegion={numberOfUsersByRegion[region]}
+            updateSelectedSubdivisionOrCountry={updateSelectedSubdivisionOrCountry}
+          />
+        ))}
+      </MultiColumns.Sub>
+      <MultiColumns.Main>
+        <section className="a-card">
+          {users.length > 0 ? (
+            <UserGroup>
+              <UserGroup.Header>
+                {searchParams.get('subdivision_or_country') || '東京都'}
+              </UserGroup.Header>
+              <UserGroup.Icons users={users} />
+            </UserGroup>
+          ) : (
+            <EmptyMessage>都道府県別ユーザー一覧はありません</EmptyMessage>
+          )}
+        </section>
+      </MultiColumns.Main>
+    </MultiColumns>
   )
 }
---
+++ app/javascript/components/layout/MultiColumns.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import clsx from 'clsx'
+
+export const MultiColumns = ({ className, children, isReverse = false, ...props}) => {
+  return (
+    <div className={clsx('page-body', className)} {...props}>
+      <div className="container is-lg">
+        <div className={clsx('page-body__columns', isReverse && 'is-reverse' )}>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const MultiColumnsMain = ({ className, children, ...props }) => {
+  return (
+    <div className={clsx('page-body__column is-main', className)} {...props}>
+      {children}
+    </div>
+  )
+}
+
+const MultiColumnsSub = ({ className, children, ...props }) => {
+  return (
+    <div className={clsx('page-body__column is-sub', className)} {...props}>
+      {children}
+    </div>
+  )
+}
+
+MultiColumns.Main = MultiColumnsMain
+MultiColumns.Sub = MultiColumnsSub
