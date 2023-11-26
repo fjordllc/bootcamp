5fd4246748c1d7c2c4628bcec2a12f1bb8a575f7
Kassy0220 <shinbokutaro0220@gmail.com>
2023-11-26 18:26:31 +0900

    コントローラー内のTARGETSをより具体的な定数名に変更

--- app/controllers/companies/users_controller.rb
+++ app/controllers/companies/users_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Companies::UsersController < ApplicationController
-  TARGETS = %w[all student_and_trainee graduate adviser mentor].freeze
+  ALLOWED_TARGETS = %w[all student_and_trainee graduate adviser mentor].freeze
 
   def index
     @target = params[:target]
-    @target = 'student_and_trainee' unless TARGETS.include?(@target)
+    @target = 'student_and_trainee' unless ALLOWED_TARGETS.include?(@target)
     @company = Company.find(params[:company_id])
 
-    target_users = User.users_role(@target, allowed_targets: TARGETS, default_target: 'student_and_trainee')
+    target_users = User.users_role(@target, allowed_targets: ALLOWED_TARGETS, default_target: 'student_and_trainee')
 
     @users = target_users.with_attached_avatar.where(company: @company).order(updated_at: :desc)
   end
--- app/views/api/users/companies/_company.json.jbuilder
+++ app/views/api/users/companies/_company.json.jbuilder
@@ -6,5 +6,5 @@ json.description company.description
 json.logo_url company.logo_url
 json.users_url company_users_url(company)
 
-targets = %w[all trainee adviser graduate mentor]
-json.users company.users.users_role(@target, allowed_targets: targets, default_target: 'all').order(:id), partial: "api/users/user", as: :user
+allowed_targets = %w[all trainee adviser graduate mentor]
+json.users company.users.users_role(@target, allowed_targets: allowed_targets, default_target: 'all').order(:id), partial: "api/users/user", as: :user
