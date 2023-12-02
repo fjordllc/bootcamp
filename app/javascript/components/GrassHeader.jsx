88d55e0517bc2aa27569c503d254840262c75fd8
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-12-02 17:01:25 +0900

    コンポーネントを分割

---
+++ app/javascript/components/GrassHeader.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function GrassHeader ({ currentUser, hideGrass, isDashboard }) {
+  return (
+    <header className="card-header is-sm">
+      <h2 className="card-header__title">学習時間</h2>
+      {currentUser &&
+        currentUser.primary_role === 'graduate' &&
+        isDashboard && (
+          <button
+            onClick={hideGrass}
+            className="a-button is-xs is-muted-bordered">
+            非表示
+          </button>
+        )}
+    </header>
+  )
+}
