a1e145635ad49ff9855617c6a762ddd392bf88a7
sochi419 <equipment2241@gmail.com>
2023-12-04 18:37:14 +0900

    休会日数計算メソッドをHibernationクラスに定義

--- app/models/hibernation.rb
+++ app/models/hibernation.rb
@@ -4,4 +4,10 @@ class Hibernation < ApplicationRecord
   belongs_to :user
   validates :reason, presence: true
   validates :scheduled_return_on, presence: true
+
+  def calculate_absence_days(user)
+    return unless user.hibernated_at
+
+    ((Time.zone.now - user.hibernated_at) / 86_400).floor
+  end
 end
--- app/models/user_hibernation.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class UserHibernation
-  def calculate_absence_days(user)
-    return unless user.hibernated_at
-
-    ((Time.zone.now - user.hibernated_at) / 86_400).floor
-  end
-end
