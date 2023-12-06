105f34976a7bedbc91713abde254a6ade812731d
Kassy0220 <shinbokutaro0220@gmail.com>
2023-12-06 09:08:52 +0900

    users_roleが簡潔になる様修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -397,12 +397,8 @@ class User < ApplicationRecord
     # allowed_targets:　呼び出したいscope名に対応するtargetを過不足なく指定した配列。
     # default_target: targetに不正な値が渡された際、users_roleが返すスコープ名に対応するtargetを指定する。デフォルトでは:noneを指定しているため何も返さない。
     def users_role(target, allowed_targets: [], default_target: :none)
-      scope_name =
-        if (ALL_ALLOWED_TARGETS & allowed_targets).include?(target)
-          TARGET_TO_SCOPE.fetch(target, target)
-        else
-          TARGET_TO_SCOPE.fetch(default_target, default_target)
-        end
+      key = (ALL_ALLOWED_TARGETS & allowed_targets).include?(target) ? target : default_target
+      scope_name = TARGET_TO_SCOPE.fetch(key, key)
       send(scope_name)
     end
 
