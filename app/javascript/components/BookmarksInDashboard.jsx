8dd37b0e4e87443f3fc02a3a938ef691db20421b
Toshiyasu Nonohara <hh0648075@gmail.com>
2023-12-07 22:55:37 +0900

    BookmarksInDashboardとBookmarkを関数コンポーネント化

--- app/javascript/components/BookmarksInDashboard.jsx
+++ app/javascript/components/BookmarksInDashboard.jsx
@@ -2,42 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Bookmark from './Bookmark.jsx'
 
-class BookmarksInDashboard extends React.Component {
-  render() {
-    return (
-      <div className="a-card">
-        <header className="card-header is-sm">
-          <h2 className="card-header__title">最新のブックマーク</h2>
-          <div className="card-header__action">
-            <label className="a-form-label is-sm is-inline">編集</label>
-            <label className="a-on-off-checkbox is-sm spec-bookmark-edit">
-              <input id="bookmark_edit" type="checkbox" />
-              <span></span>
-            </label>
-          </div>
-        </header>
-        <hr className="a-border-tint" />
-        <div className="card-list">
-          {this.props.bookmarks.map((bookmark) => (
-            <Bookmark key={bookmark.id} {...bookmark} />
-          ))}
+function BookmarksInDashboard(props) {
+  return (
+    <div className="a-card">
+      <header className="card-header is-sm">
+        <h2 className="card-header__title">最新のブックマーク</h2>
+        <div className="card-header__action">
+          <label className="a-form-label is-sm is-inline">編集</label>
+          <label className="a-on-off-checkbox is-sm spec-bookmark-edit">
+            <input id="bookmark_edit" type="checkbox" />
+            <span></span>
+          </label>
         </div>
-        <footer className="card-footer">
-          <div className="card-footer__footer-link">
-            <a
-              href="current_user/bookmarks"
-              className="card-footer__footer-text-link">
-              全てのブックマーク（{this.props.bookmarks.length}）
-            </a>
-          </div>
-        </footer>
+      </header>
+      <hr className="a-border-tint" />
+      <div className="card-list">
+        {props.bookmarks.map((bookmark) => (
+          <Bookmark key={bookmark.id} {...bookmark} />
+        ))}
       </div>
-    )
-  }
+      <footer className="card-footer">
+        <div className="card-footer__footer-link">
+          <a
+            href="current_user/bookmarks"
+            className="card-footer__footer-text-link">
+            全てのブックマーク（{props.bookmarks.length}）
+          </a>
+        </div>
+      </footer>
+    </div>
+  )
 }
 
 BookmarksInDashboard.propTypes = {
   user: PropTypes.object,
   bookmarks: PropTypes.array
 }
+
 export default BookmarksInDashboard
