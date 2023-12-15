cdeb3e5c9817e5efe9ccd56a910ba3bed2f64821
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-12-15 13:15:03 +0900

    RegularEventモデルのテストケースを修正

--- test/models/regular_event_test.rb
+++ test/models/regular_event_test.rb
@@ -157,8 +157,19 @@ class RegularEventTest < ActiveSupport::TestCase
   end
 
   test '#assign_admin_as_organizer_if_none' do
-    regular_event = regular_events(:regular_event33)
+    regular_event = RegularEvent.new(
+      title: '主催者のいないイベント',
+      description: '主催者のいないイベント',
+      finished: false,
+      hold_national_holiday: false,
+      start_at: Time.zone.local(2020, 1, 1, 21, 0, 0),
+      end_at: Time.zone.local(2020, 1, 1, 22, 0, 0),
+      user: users(:kimura),
+      category: 0,
+      published_at: '2023-08-01 00:00:00'
+    )
+    regular_event.save(validate: false)
     regular_event.assign_admin_as_organizer_if_none
-    assert_equal User.find_by(login_name: 'komagata'), regular_event.organizers[0]
+    assert_equal User.find_by(login_name: User::DEFAULT_ADMIN_LOGIN_NAME), regular_event.organizers.first
   end
 end
