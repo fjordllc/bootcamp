f7a61241b2b71f0ae0b58b08e876ad516a596c73
komagata <komagata@gmail.com>
2023-12-15 16:52:49 +0800

    mentor_last_commented_atがあるのにcommentがない場合のエラーを抑制
    comment削除時にmentor_last_commented_atを空にしてないのが原因。

--- app/models/product.rb
+++ app/models/product.rb
@@ -99,7 +99,7 @@ class Product < ApplicationRecord
 
   def self.unchecked_no_replied_products
     self_last_commented_products = Product.where.not(commented_at: nil).filter do |product|
-      product.comments.last.user_id == product.user.id
+      product.comments&.last&.user_id == product.user.id
     end
     no_comments_products = Product.where(commented_at: nil)
     no_replied_products_ids = (self_last_commented_products + no_comments_products).map(&:id)
