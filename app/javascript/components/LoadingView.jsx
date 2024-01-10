555ff0a200127483dab2c78c08bf2dc534395905
machida <machida@fjord.jp>
2024-01-10 22:16:25 +0900

    loadingのデザイン適用

--- app/javascript/components/LoadingView.jsx
+++ app/javascript/components/LoadingView.jsx
@@ -1,6 +1,148 @@
 import React from 'react'
 
 const LoadingView = () => {
-  return <>読み込み中</>
+  return (
+    <div className="admin-table is-loading">
+    <table className="admin-table__table">
+      <thead className="admin-table__header">
+        <tr className="admin-table__labels">
+          <th className="admin-table__label">
+            <div className="a-placeholder"></div>
+          </th>
+          <th className="admin-table__label">
+            <div className="a-placeholder"></div>
+          </th>
+          <th className="admin-table__label">
+            <div className="a-placeholder"></div>
+          </th>
+        </tr>
+      </thead>
+      <tbody className="admin-table__items">
+      <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+        <tr class="admin-table__item">
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+          <td class="admin-table__item-value">
+            <div className="a-placeholder"></div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  )
 }
 export default LoadingView
