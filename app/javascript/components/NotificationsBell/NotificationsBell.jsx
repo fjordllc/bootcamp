3d1ca9c5f2e8eca3affdd9d10a5008150ad3e92a
junohm410 <junohm410@gmail.com>
2024-01-18 18:00:34 +0900

    Vue版と同じ仕様で、通知ベルをReact化

---
+++ app/javascript/components/NotificationsBell/NotificationsBell.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from 'react'
+import useSWR from 'swr'
+import fetcher from '../../fetcher'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
+
+export default function NotificationsBell() {
+  const [showNotifications, setShowNotifications] = useState(false)
+
+  const url = '/api/notifications.json?status=unread'
+  const { data, error } = useSWR(url, fetcher)
+
+  const notificationExist = data?.notifications.length > 0
+  const hasCountClass = notificationExist ? 'has-count' : 'has-no-count'
+
+  const notificationCount = () => {
+    if (!data) return
+
+    const count = data.notifications.length
+    return count > 99 ? '99+' : String(count)
+  }
+
+  const openNotifications = () => {
+    setShowNotifications(true)
+  }
+
+  const clickOutsideNotifications = (e) => {
+    if (e.target !== e.currentTarget) return
+
+    setShowNotifications(false)
+  }
+
+  const openUnconfirmedItems = () => {
+    const links = document.querySelectorAll(
+      '.header-dropdown__item-link.unconfirmed_link'
+    )
+    links.forEach((link) => {
+      window.open(link.href, '_target', 'noopener')
+    })
+  }
+
+  if (error) {
+    console.warn(error)
+    return <div>failed to load</div>
+  }
+
+  return (
+    <div className={hasCountClass}>
+      <label
+        onClick={openNotifications}
+        className="header-links__link test-show-notifications">
+        <div className="header-links__link test-bell">
+          <div className="header-notification-icon">
+            {notificationExist && (
+              <div className="header-notification-count a-notification-count test-notification-count">
+                {notificationCount()}
+              </div>
+            )}
+            {!data && (
+              <div className="header-notification-count a-notification-count is-loading"></div>
+            )}
+            <i className="fa-solid fa-bell"></i>
+            <div className="header-links__link-label">通知</div>
+          </div>
+        </div>
+      </label>
+      {data && showNotifications && (
+        <div>
+          <label
+            className="header-dropdown__background"
+            onClick={clickOutsideNotifications}></label>
+          <div className="header-dropdown__inner is-notification">
+            <ul className="header-dropdown__items">
+              {data.notifications.map((notification) => {
+                return (
+                  <Notification
+                    key={notification.id}
+                    notification={notification}
+                  />
+                )
+              })}
+            </ul>
+            <footer className="header-dropdown__footer">
+              <a
+                href="/notifications?status=unread"
+                className="header-dropdown__footer-link">
+                全ての未読通知
+              </a>
+              <a href="/notifications" className="header-dropdown__footer-link">
+                全ての通知
+              </a>
+              <a
+                href="/notifications/allmarks"
+                className="header-dropdown__footer-link"
+                data-method="post">
+                全て既読にする
+              </a>
+              <button
+                className="header-dropdown__footer-link"
+                onClick={openUnconfirmedItems}>
+                全て別タブで開く
+              </button>
+            </footer>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function Notification({ notification }) {
+  const createdAtFromNow = (createdAt) => {
+    return dayjs(createdAt).fromNow()
+  }
+
+  return (
+    <li className="header-dropdown__item">
+      <a
+        href={notification.path}
+        className="header-dropdown__item-link unconfirmed_link">
+        <div className="header-notifications-item__body">
+          <span
+            className={`a-user-role header-notifications-item__user-icon is-${notification.sender.primary_role}`}>
+            <img
+              src={notification.sender.avatar_url}
+              className="a-user-icon"
+              alt="User Icon"
+            />
+          </span>
+          <div className="header-notifications-item__message">
+            <p className="test-notification-message">{notification.message}</p>
+          </div>
+          <time className="header-notifications-item__created-at">
+            {createdAtFromNow(notification.created_at)}
+          </time>
+        </div>
+      </a>
+    </li>
+  )
+}
