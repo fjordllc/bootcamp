cc3ef6259e9699e29727387e090894c1184fbd79
sochi419 <equipment2241@gmail.com>
2024-01-24 19:00:58 +0900

    休会からの経過日数を示すメソッドの名称,  定義場所, テストの配置場所を変更

--- app/models/hibernation.rb
+++ app/models/hibernation.rb
@@ -4,10 +4,4 @@ class Hibernation < ApplicationRecord
   belongs_to :user
   validates :reason, presence: true
   validates :scheduled_return_on, presence: true
-
-  def calculate_absence_days(user)
-    return unless user.hibernated_at
-
-    ((Time.zone.now - user.hibernated_at) / 86_400).floor
-  end
 end
