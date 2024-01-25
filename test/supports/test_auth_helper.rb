f0be2c28153436b3333892d31e502d0dc1640e90
Kassy0220 <shinbokutaro0220@gmail.com>
2024-01-25 18:43:04 +0900

    テスト時のログアウト処理を、visit_with_authからController側へ移行

--- test/supports/test_auth_helper.rb
+++ test/supports/test_auth_helper.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 module TestAuthHelper
-  # include Sorcery::TestHelpers::Rails
-
   def visit_with_auth(url, login_name)
-    logout if page.has_selector?('.test-show-menu')
-
     uri = URI.parse(url)
     queries = Rack::Utils.parse_nested_query(uri.query)
     queries['_login_name'] = login_name
