3bb160128e889583a42b1ad35590359b383669cb
junohm410 <junohm410@gmail.com>
2024-01-26 10:32:28 +0900

    フッターのコンポーネントを切り出し、未読タブ上にのみフッターのリンクを表示するように実装

---
+++ app/javascript/components/NotificationsBell/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Footer({ notificationsCount }) {
+  const openUnconfirmedItems = () => {
+    const links = document.querySelectorAll(
+      '.header-dropdown__item-link.unconfirmed_link'
+    )
+    links.forEach((link) => {
+      window.open(link.href, '_target', 'noopener')
+    })
+  }
+
+  if (notificationsCount === 0) return null
+
+  return (
+    <footer className="header-dropdown__footer">
+      <a
+        href="/notifications/allmarks"
+        className="header-dropdown__footer-link"
+        data-method="post">
+        全て既読にする
+      </a>
+      <button
+        className="header-dropdown__footer-link"
+        onClick={openUnconfirmedItems}>
+        全て別タブで開く
+      </button>
+    </footer>
+  )
+}
