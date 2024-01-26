151f16e241a66c40c143883965a74cb99ad68c92
nicole2525 <nicole2525nomail@gmail.com>
2024-01-26 20:04:37 +0900

    LearningCacheDestroyerにloggerを追記した

--- app/models/learning_cache_destroyer.rb
+++ app/models/learning_cache_destroyer.rb
@@ -4,5 +4,6 @@ class LearningCacheDestroyer
   def call(payload)
     user = payload[:user]
     Rails.cache.delete "/model/user/#{user.id}/completed_percentage"
+    Rails.logger.info "[LearningCacheDestroyer] Cache destroyed for user #{user.id}"
   end
 end
