390618db1ee08cd8882150b0dd18e65ea4d9ad07
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-01-26 22:36:36 +0900

    User.jsxの不足部分を追記

--- app/javascript/components/User.jsx
+++ app/javascript/components/User.jsx
@@ -29,11 +29,28 @@ export default function User({ user, currentUser }) {
     <div className="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-xs-12">
       <div className="users-item">
         <div className={`users-item__inner a-card ${roleClass()}`}>
-          {currentUser?.mentor && user.student_or_trainee && !user.active && (
-            <div className="users-item__inactive-message is-only-mentor">
-              1ヶ月以上ログインがありません
-            </div>
-          )}
+          {currentUser &&
+            (currentUser.mentor || currentUser.admin) &&
+            user.student_or_trainee && (
+              <div className="users-item__inactive-message-container is-only-mentor">
+                {user.roles.includes('retired') && (
+                  <div className="users-item__inactive-message">
+                    退会しました
+                  </div>
+                )}
+                {user.roles.includes('hibernationed') && (
+                  <div className="users-item__inactive-message">
+                    休会中: {user.hibernated_at}〜(
+                    {user.hibernation_elapsed_days}日経過)
+                  </div>
+                )}
+                {!user.active && (
+                  <div className="users-item__inactive-message">
+                    1ヶ月以上ログインがありません
+                  </div>
+                )}
+              </div>
+            )}
           <header className="users-item__header">
             <div className="users-item__header-inner">
               <div className="users-item__header-start">
@@ -59,6 +76,38 @@ export default function User({ user, currentUser }) {
                         href={user.url}>
                         {user.login_name}
                       </a>
+                      {user.company && user.company.logo_url && (
+                        <a href={user.company.url}>
+                          <img
+                            className="user-item__company-logo"
+                            src={user.company.logo_url}
+                          />
+                        </a>
+                      )}
+                    </div>
+                  </div>
+                  <div className="card-list-item__row">
+                    <div className="card-list-item-meta">
+                      <div className="card-list-item-meta__items">
+                        <div className="card-list-item-meta__item">
+                          <div className="a-meta">{user.name}</div>
+                        </div>
+                        <div className="card-list-item-meta__item">
+                          {user.discord_profile.times_url ? (
+                            <a
+                              className="a-meta"
+                              href={user.discord_profile.times_url}>
+                              <i className="fa-brands fa-discord"></i>
+                              {user.discord_profile.account_name}
+                            </a>
+                          ) : (
+                            <div className="a-meta">
+                              <i className="fa-brands fa-discord"></i>
+                              {user.discord_profile.account_name}
+                            </div>
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -78,11 +127,42 @@ export default function User({ user, currentUser }) {
             </div>
           </div>
           <UserPracticeProgress user={user} />
-          <Following
-            isFollowing={user.isFollowing}
-            userId={user.id}
-            isWatching={user.isWatching}
-          />
+          <hr className="a-border-tint" />
+          <footer className="card-footer">
+            <div className="card-main-actions">
+              <ul className="card-main-actions__items">
+                {currentUser.id !== user.id &&
+                  currentUser.adviser &&
+                  user.company &&
+                  currentUser.company_id === user.company.id && (
+                    <li className="card-main-actions__item">
+                      <div className="a-button is-disabled is-sm is-block">
+                        <i className="fa-solid fa-check"></i>
+                        <span>自社研修生</span>
+                      </div>
+                    </li>
+                  )}
+                {currentUser.id !== user.id && (
+                  <li className="card-main-actions__item">
+                    <Following
+                      isFollowing={user.isFollowing}
+                      userId={user.id}
+                      isWatching={user.isWatching}
+                    />
+                  </li>
+                )}
+                {currentUser.admin && user.talkUrl && (
+                  <li className="card-main-actions__item is-only-mentor">
+                    <a
+                      className="a-button is-secondary is-sm is-block"
+                      href={user.talkUrl}>
+                      相談部屋
+                    </a>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </footer>
         </div>
       </div>
     </div>
