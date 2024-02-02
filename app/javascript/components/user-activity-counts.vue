627b2fec0cf5b4392214a1de7853ee711942a9cb
naokinaokiboo <naokinaokiboo@gmail.com>
2024-02-02 22:57:45 +0900

    ユーザー一覧ページの各アクション数をリンクに変更

--- app/javascript/components/user-activity-counts.vue
+++ app/javascript/components/user-activity-counts.vue
@@ -7,35 +7,50 @@
           | 日報
         dd.card-counts__item-value(
           :class='user.report_count == 0 ? "is-empty" : ""')
-          | {{ user.report_count }}
+          span(v-if='user.report_count == 0')
+            | {{ user.report_count }}
+          a.a-text-link(v-else)(:href='`${user.url}/reports`')
+            | {{ user.report_count }}
     .card-counts__item
       .card-counts__item-inner
         dt.card-counts__item-label
           | 提出物
         dd.card-counts__item-value(
           :class='user.product_count == 0 ? "is-empty" : ""')
-          | {{ user.product_count }}
+          span(v-if='user.product_count == 0')
+            | {{ user.product_count }}
+          a.a-text-link(v-else)(:href='`${user.url}/products`')
+            | {{ user.product_count }}
     .card-counts__item
       .card-counts__item-inner
         dt.card-counts__item-label
           | コメント
         dd.card-counts__item-value(
           :class='user.comment_count == 0 ? "is-empty" : ""')
-          | {{ user.comment_count }}
+          span(v-if='user.comment_count == 0')
+            | {{ user.comment_count }}
+          a.a-text-link(v-else)(:href='`${user.url}/comments`')
+            | {{ user.comment_count }}
     .card-counts__item
       .card-counts__item-inner
         dt.card-counts__item-label
           | 質問
         dd.card-counts__item-value(
           :class='user.question_count == 0 ? "is-empty" : ""')
-          | {{ user.question_count }}
+          span(v-if='user.question_count == 0')
+            | {{ user.question_count }}
+          a.a-text-link(v-else)(:href='`${user.url}/questions`')
+            | {{ user.question_count }}
     .card-counts__item
       .card-counts__item-inner
         dt.card-counts__item-label
           | 回答
         dd.card-counts__item-value(
           :class='user.answer_count == 0 ? "is-empty" : ""')
-          | {{ user.answer_count }}
+          span(v-if='user.answer_count == 0')
+            | {{ user.answer_count }}
+          a.a-text-link(v-else)(:href='`${user.url}/answers`')
+            | {{ user.answer_count }}
 </template>
 <script>
 export default {
