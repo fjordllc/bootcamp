0c79665288d634444f1a86dbd6b4389de54805e8
ko <kyokucho1989@gmail.com>
2024-02-02 19:11:39 +0900

    カラムの指定を配列から修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_28_205255) do
+ActiveRecord::Schema.define(version: 2024_02_02_100641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
