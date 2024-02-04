b19a940670d6ef0efe59b9c920fdae29baa7abcb
ko <kyokucho1989@gmail.com>
2024-02-04 20:21:52 +0900

    イベントの日時を秒単位で変更してもエラーが出ないように修正

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -83,6 +83,28 @@ class EventsTest < ApplicationSystemTestCase
     assert_text '特別イベントを更新しました。'
   end
 
+  test 'update event in seconds' do
+    visit_with_auth edit_event_path(events(:event1)), 'kimura'
+    within 'form[name=event]' do
+      fill_in 'event[title]', with: 'ミートアップ(修正2)'
+      fill_in 'event[description]', with: 'ミートアップを開催します(修正2)'
+      fill_in 'event[capacity]', with: 30
+      fill_in 'event[location]', with: 'FJORDオフィス'
+      fill_in 'event[start_at]', with: '002019-12-21T19:00:01'
+      fill_in 'event[end_at]', with: '002019-12-21T22:30:01'
+      fill_in 'event[open_start_at]', with: '002019-12-11T09:00:01'
+      fill_in 'event[open_end_at]', with: '002019-12-20T23:59:01'
+      click_button '内容を更新'
+    end
+    assert_text '特別イベントを更新しました。'
+    assert_equal 1, Event.where(\
+      start_at: Time.zone.parse('2019-12-21 19:00:01'),\
+      end_at: Time.zone.parse('2019-12-21 22:30:01'),\
+      open_start_at: Time.zone.parse('2019-12-11 09:00:01'),\
+      open_end_at: Time.zone.parse('2019-12-20 23:59:01')
+    ).count
+  end
+
   test 'destroy event' do
     visit_with_auth event_path(events(:event1)), 'kimura'
     find 'h2', text: 'コメント'
