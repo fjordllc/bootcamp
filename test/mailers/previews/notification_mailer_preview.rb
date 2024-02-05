472d977afa3eba7e13acba1497ee0eebdfe94474
masyuko0222 <masyuko0222@gmail.com>
2024-02-05 21:00:29 +0900

    rubocopのルールから除外されていたファイルを、適用するように

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -8,48 +8,48 @@ class NotificationMailerPreview < ActionMailer::Preview
     mentionable = Comment.find(ActiveRecord::FixtureSet.identify(:comment9))
     receiver = report.user
 
-    NotificationMailer.with(mentionable: mentionable, receiver: receiver).mentioned
+    NotificationMailer.with(mentionable:, receiver:).mentioned
   end
 
   def checked
     report = Report.find(ActiveRecord::FixtureSet.identify(:report5))
     check = report.checks.first
 
-    NotificationMailer.with(check: check).checked
+    NotificationMailer.with(check:).checked
   end
 
   def retired
     sender = User.find(ActiveRecord::FixtureSet.identify(:yameo))
     receiver = User.find(ActiveRecord::FixtureSet.identify(:komagata))
 
-    NotificationMailer.with(sender: sender, receiver: receiver).retired
+    NotificationMailer.with(sender:, receiver:).retired
   end
 
   def trainee_report
     report = Report.find(ActiveRecord::FixtureSet.identify(:report11))
     receiver = User.find(ActiveRecord::FixtureSet.identify(:senpai))
 
-    NotificationMailer.with(report: report, receiver: receiver).trainee_report
+    NotificationMailer.with(report:, receiver:).trainee_report
   end
 
   def chose_correct_answer
     answer = Answer.find(ActiveRecord::FixtureSet.identify(:correct_answer2))
     receiver = answer.sender
 
-    NotificationMailer.with(answer: answer, receiver: receiver).chose_correct_answer
+    NotificationMailer.with(answer:, receiver:).chose_correct_answer
   end
 
   def graduated
     sender = User.find(ActiveRecord::FixtureSet.identify(:sotugyou))
     receiver = User.find(ActiveRecord::FixtureSet.identify(:mentormentaro))
 
-    NotificationMailer.with(sender: sender, receiver: receiver).graduated
+    NotificationMailer.with(sender:, receiver:).graduated
   end
 
   def product_update
     product = Product.find(ActiveRecord::FixtureSet.identify(:product1))
     receiver = User.find(ActiveRecord::FixtureSet.identify(:komagata))
 
-    NotificationMailer.with(product: product, receiver: receiver).product_update
+    NotificationMailer.with(product:, receiver:).product_update
   end
 end
