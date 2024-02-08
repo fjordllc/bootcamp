caa4bb3feaa0bc51753d1cb033b34d7e8ff500e3
goruchan <ppcfr481@yahoo.co.jp>
2024-02-08 22:45:26 +0900

    ブログ一覧をパーシャル化、詳細ページにプレビューに関するメッセージ追加、テストケース名修正

--- app/controllers/articles/wips_controller.rb
+++ app/controllers/articles/wips_controller.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class Articles::WipsController < ApplicationController
+  before_action :require_admin_or_mentor_login, only: %i[index]
+
   def index
-    @articles = Article.where(wip: true)
+    @articles = list_articles
+    @articles = @articles.tagged_with(params[:tag]) if params[:tag]
     render layout: 'welcome'
+  end
+
+  private
+
+  def list_articles
+    articles = Article.with_attached_thumbnail.includes(user: { avatar_attachment: :blob }).order(created_at: :desc).page(params[:page])
+    articles.where(wip: true)
   end
 end
