0ddcbfc5d3d6e997a42a0d93e3d4696964837a76
goruchan <ppcfr481@yahoo.co.jp>
2024-02-08 22:45:26 +0900

    ブログ一覧をパーシャル化、詳細ページにプレビューに関するメッセージ追加、テストケース名修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,8 @@ class ArticlesController < ApplicationController
     if @article.published? || @article.token == params[:token] || admin_or_mentor_login?
       render layout: 'welcome'
     else
-      redirect_to root_path, alert: '管理者・メンターとしてログインしてください'
+      message = params[:token].nil? ? '管理者・メンターとしてログインしてください' : 'token が一致しませんでした'
+      redirect_to root_path, alert: message
     end
   end
 
@@ -68,7 +69,7 @@ class ArticlesController < ApplicationController
 
   def list_articles
     articles = Article.with_attached_thumbnail.includes(user: { avatar_attachment: :blob }).order(created_at: :desc).page(params[:page])
-    admin_or_mentor_login? ? articles : articles.where(wip: false)
+    articles.where(wip: false)
   end
 
   def list_recent_articles(number)
@@ -98,7 +99,9 @@ class ArticlesController < ApplicationController
 
   def set_wip
     @article.wip = params[:commit] == 'WIP'
-    @article.token = @article.token.nil? ? SecureRandom.urlsafe_base64 : @article.token if params[:commit] == 'WIP'
+
+    token = @article.wip ? (@article.token.presence || SecureRandom.urlsafe_base64) : nil
+    @article.token = token
   end
 
   def notice_message(article)
--- test/system/article/wips_test.rb
+++ test/system/article/wips_test.rb
@@ -5,6 +5,6 @@ require 'application_system_test_case'
 class Article::WipsTest < ApplicationSystemTestCase
   test 'show listing wip articles' do
     visit_with_auth articles_wips_path, 'komagata'
-    assert_selector '.articles-item__link', count: 1
+    assert_selector '.thumbnail-card__inner', count: 1
   end
 end
