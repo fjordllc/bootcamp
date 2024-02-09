0c00bfca8c4e0347da3994cd57792cbed58287e8
naokinaokiboo <naokinaokiboo@gmail.com>
2024-02-09 18:48:11 +0900

    静的な値であるpropsの書き方を修正

--- app/javascript/components/user-activity-counts.vue
+++ app/javascript/components/user-activity-counts.vue
@@ -2,23 +2,23 @@
 .card-counts.mt-3(v-if='user.student_or_trainee')
   dl.card-counts__items
     user-activity-count(
-      :activity-name='`日報`',
+      activity-name='日報',
       :activity-count='user.report_count',
       :activity-url='`${user.url}/reports`')
     user-activity-count(
-      :activity-name='`提出物`',
+      activity-name='提出物',
       :activity-count='user.product_count',
       :activity-url='`${user.url}/products`')
     user-activity-count(
-      :activity-name='`コメント`',
+      activity-name='コメント',
       :activity-count='user.comment_count',
       :activity-url='`${user.url}/comments`')
     user-activity-count(
-      :activity-name='`質問`',
+      activity-name='質問',
       :activity-count='user.question_count',
       :activity-url='`${user.url}/questions`')
     user-activity-count(
-      :activity-name='`回答`',
+      activity-name='回答',
       :activity-count='user.answer_count',
       :activity-url='`${user.url}/answers`')
 </template>
