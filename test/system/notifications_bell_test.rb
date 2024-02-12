6f3e7e247596ca6557af5d53973876e3f8f487ff
junohm410 <junohm410@gmail.com>
2024-02-12 18:04:14 +0900

    通知ベルの全てタブには最大で10件の通知が表示されることのテストを追加

--- test/system/notifications_bell_test.rb
+++ test/system/notifications_bell_test.rb
@@ -44,4 +44,26 @@ class NotificationsBellTest < ApplicationSystemTestCase
     find('.pill-nav__item-link.w-full', text: '全て').click
     assert_selector('.o-empty-message.mb-8 .o-empty-message__text', text: '通知はありません')
   end
+
+  test 'Up to 10 notifications are displayed in an All tab' do
+    10.times do |n|
+      Notification.create(message: "machidaさんからメンションが届きました#{n}",
+                          kind: 'mentioned',
+                          link: "/reports/#{n}",
+                          user: users(:komagata),
+                          sender: users(:machida))
+    end
+    Notification.create(message: '1番新しい通知',
+                        created_at: '2040-01-18 06:06:42',
+                        kind: 'mentioned',
+                        link: '/reports/20400118',
+                        user: users(:komagata),
+                        sender: users(:machida))
+
+    visit_with_auth '/', 'komagata'
+    find('.header-links__link.test-show-notifications').click
+    find('.pill-nav__item-link.w-full', text: '全て').click
+    assert_selector('.test-notification-message', text: '1番新しい通知')
+    assert_equal 10, all('.test-notification-message').size
+  end
 end
