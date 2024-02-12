dcd95f01b345d8eb2a15d384a5185b1665309b26
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-02-12 22:08:10 +0900

    テストをテーブルタグ用に修正

--- test/models/github_grass_test.rb
+++ test/models/github_grass_test.rb
@@ -5,8 +5,8 @@ require 'test_helper'
 class GithubGrassTest < ActiveSupport::TestCase
   test '#fetch' do
     VCR.use_cassette 'github_grass/fetch' do
-      assert_match(/svg/, GithubGrass.new('komagata').fetch)
-      assert_no_match(/svg/, GithubGrass.new('komagata1234').fetch)
+      assert_match(/table/, GithubGrass.new('komagata').fetch)
+      assert_no_match(/table/, GithubGrass.new('komagata1234').fetch)
     end
   end
 end
