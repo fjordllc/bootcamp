d2ebbdcf1ffd67e64407c462fc431f36be25e0aa
reckyy <srreki98@gmail.com>
2024-02-14 09:53:50 +0900

    正しくテストできていなかったため修正

--- test/system/learning_status_test.rb
+++ test/system/learning_status_test.rb
@@ -4,23 +4,23 @@ require 'application_system_test_case'
 
 class LearningStatusTest < ApplicationSystemTestCase
   test 'learning status changes to submitted after the mentor cancels the confirmation' do
-    visit_with_auth "/products/#{products(:product8).id}", 'machida'
+    visit_with_auth "/products/#{products(:product74).id}", 'machida'
     click_button '担当する'
     click_button '提出物を確認'
     click_button '提出物の確認を取り消す'
-    visit_with_auth "/products/#{products(:product8).id}", 'kimura'
+    visit_with_auth "/products/#{products(:product74).id}", 'kimuramitai'
     assert_no_selector 'h2', text: 'このプラクティスは修了しました🎉'
   end
 
   test 'learning status changes to submitted after the mentor cancels the confirmation with comment' do
-    visit_with_auth "/products/#{products(:product8).id}", 'machida'
+    visit_with_auth "/products/#{products(:product74).id}", 'machida'
     click_button '担当する'
     fill_in('new_comment[description]', with: 'LGTMです。')
     accept_alert do
       click_button '確認OKにする'
     end
     click_button '提出物の確認を取り消す'
-    visit_with_auth "/products/#{products(:product8).id}", 'kimura'
+    visit_with_auth "/products/#{products(:product74).id}", 'kimuramitai'
     assert_no_selector 'h2', text: 'このプラクティスは修了しました🎉'
   end
 end
