22649868bcce6ac3b59a5a5b8f0f7b69dd561449
yocchan-git <yocchan.business@gmail.com>
2024-02-15 16:14:23 +0900

    プラクティス詳細の折りたたみ機能を作成する

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -213,4 +213,22 @@ class ProductTest < ActiveSupport::TestCase
     assert_includes Product.all, hibernated_user_product
     assert_not_includes Product.unhibernated_user_products, hibernated_user_product
   end
+
+  test '#practice_content_for_toggle' do
+    product =
+      Product.create!(
+        body: 'test',
+        user: users(:kimura),
+        practice: practices(:practice5),
+        checker_id: nil
+      )
+
+    practice_content = product.practice_content_for_toggle(true)
+    assert_equal 'toggle_description_body', practice_content[:id_name]
+    assert_equal product.practice.description, practice_content[:description]
+
+    practice_content = product.practice_content_for_toggle(false)
+    assert_equal 'toggle_goal_body', practice_content[:id_name]
+    assert_equal product.practice.goal, practice_content[:description]
+  end
 end
