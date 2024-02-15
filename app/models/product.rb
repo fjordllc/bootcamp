4496153319649f954ddec6eae26f6b5176235b3f
yocchan-git <yocchan.business@gmail.com>
2024-02-15 16:35:09 +0900

    シンボルを使って条件分岐に変更する

--- app/models/product.rb
+++ app/models/product.rb
@@ -206,14 +206,15 @@ class Product < ApplicationRecord
     created_at != updated_at
   end
 
-  def practice_content_for_toggle(is_description)
-    if is_description
+  def practice_content_for_toggle(content_type)
+    case content_type
+    when :practice
       {
         id_name: 'toggle_description_body',
         title: 'プラクティスを確認',
         description: practice.description
       }
-    else
+    when :goal
       {
         id_name: 'toggle_goal_body',
         title: '終了条件を確認',
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -223,11 +223,11 @@ class ProductTest < ActiveSupport::TestCase
         checker_id: nil
       )
 
-    practice_content = product.practice_content_for_toggle(true)
+    practice_content = product.practice_content_for_toggle(:practice)
     assert_equal 'toggle_description_body', practice_content[:id_name]
     assert_equal product.practice.description, practice_content[:description]
 
-    practice_content = product.practice_content_for_toggle(false)
+    practice_content = product.practice_content_for_toggle(:goal)
     assert_equal 'toggle_goal_body', practice_content[:id_name]
     assert_equal product.practice.goal, practice_content[:description]
   end
