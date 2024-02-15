cc4572cba6a7a9841d7b8dd51f70942a14131d57
machida <machida@fjord.jp>
2024-02-15 16:29:02 +0900

    テストの順番変更

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -259,6 +259,11 @@ class ArticlesTest < ApplicationSystemTestCase
     assert_no_text meta_description
   end
 
+  test 'if there is no featured image, the default image is set as the OGP image' do
+    visit_with_auth "/articles/#{@article.id}", 'komagata'
+    verify_default_ogp_image_used
+  end
+
   test 'can set it as an OGP image by uploading an eye-catching image' do
     visit_with_auth edit_article_path(@article), 'komagata'
     find('label[for=article_thumbnail_type_prepared_thumbnail]').click
@@ -269,11 +274,6 @@ class ArticlesTest < ApplicationSystemTestCase
     meta = find('meta[name="twitter:image"]', visible: false)
     content = meta.native['content']
     assert_match(/test\.jpg$/, content)
-  end
-
-  test 'if there is no featured image, the default image is set as the OGP image' do
-    visit_with_auth "/articles/#{@article.id}", 'komagata'
-    verify_default_ogp_image_used
   end
 
   test 'can set up prepared images for eye-catching image, the default OGP image will be used' do
