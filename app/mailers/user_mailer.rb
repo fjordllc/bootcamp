22792d814b2311cdd2fd1d88ce5cf0218dda6e53
taco <nnnantai-doubutsu@yahoo.co.jp>
2024-02-15 13:03:31 +0900

    自動退会メールのBCCにinfo@lokka.jpを追加

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -19,6 +19,6 @@ class UserMailer < ApplicationMailer
 
   def auto_retire(user)
     @user = user
-    mail to: user.email, subject: '[FBC] 重要なお知らせ：受講ステータスの変更について'
+    mail to: user.email, bcc: 'info@lokka.jp', subject: '[FBC] 重要なお知らせ：受講ステータスの変更について'
   end
 end
