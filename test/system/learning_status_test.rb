0b48a04a529ab421e535933b98a9c235665376ed
reckyy <srreki98@gmail.com>
2024-02-16 12:03:58 +0900

    テストケースの中でデータを用意する形に修正

--- test/system/learning_status_test.rb
+++ test/system/learning_status_test.rb
@@ -4,23 +4,33 @@ require 'application_system_test_case'
 
 class LearningStatusTest < ApplicationSystemTestCase
   test 'learning status changes to submitted after the mentor cancels the confirmation' do
-    visit_with_auth "/products/#{products(:product74).id}", 'machida'
+    product = Product.create!(
+      body: '直接確認する提出物',
+      user: users(:kimuramitai),
+      practice: practices(:practice1)
+    )
+    visit_with_auth "/products/#{product.id}", 'machida'
     click_button '担当する'
     click_button '提出物を確認'
     click_button '提出物の確認を取り消す'
-    visit_with_auth "/products/#{products(:product74).id}", 'kimuramitai'
+    visit_with_auth "/products/#{product.id}", 'kimuramitai'
     assert_no_selector 'h2', text: 'このプラクティスは修了しました🎉'
   end
 
   test 'learning status changes to submitted after the mentor cancels the confirmation with comment' do
-    visit_with_auth "/products/#{products(:product74).id}", 'machida'
+    product = Product.create!(
+      body: 'コメントで確認する提出物',
+      user: users(:kimuramitai),
+      practice: practices(:practice1)
+    )
+    visit_with_auth "/products/#{product.id}", 'machida'
     click_button '担当する'
     fill_in('new_comment[description]', with: 'LGTMです。')
     accept_alert do
       click_button '確認OKにする'
     end
     click_button '提出物の確認を取り消す'
-    visit_with_auth "/products/#{products(:product74).id}", 'kimuramitai'
+    visit_with_auth "/products/#{product.id}", 'kimuramitai'
     assert_no_selector 'h2', text: 'このプラクティスは修了しました🎉'
   end
 end
