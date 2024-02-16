c91b8d186248f56a739e8ce53d4afa920e4f88bc
88-99 <xz835.525o39@gmail.com>
2024-02-17 01:07:17 +0900

    CSSのidをothersからothers_editorに変更

--- app/javascript/editor-selection-form.js
+++ app/javascript/editor-selection-form.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const editors = document.getElementsByName('user[editor]')
-  const othersRadio = document.getElementById('others')
+  const othersEditor = document.getElementById('others_editor')
   editors.forEach((editor) => {
     editor.addEventListener('change', () => {
       const othersForm = document.getElementById('others_form')
-      othersForm.classList.toggle('is-hidden', !othersRadio.checked)
+      othersForm.classList.toggle('is-hidden', !othersEditor.checked)
     })
   })
 
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function handleFormSubmit() {
     const othersInput = document.getElementById('others_input')
-    if (othersRadio.checked) {
+    if (othersEditor.checked) {
       const othersValue = othersInput.value
       const hiddenInput = document.createElement('input')
       Object.assign(hiddenInput, {
