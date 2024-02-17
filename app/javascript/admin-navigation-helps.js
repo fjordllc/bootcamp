0fafa8fc7ea3fb6bc0b40a66c5782829fe41d4b0
taco <nnnantai-doubutsu@yahoo.co.jp>
2024-02-18 06:09:35 +0900

    セミコロンを削除

--- app/javascript/admin-navigation-helps.js
+++ app/javascript/admin-navigation-helps.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dropdownMenu = document.querySelector('.admin-nav-helps');
-  const dropdownList = dropdownMenu.querySelector('ul');
+  const dropdownMenu = document.querySelector('.admin-nav-helps')
+  const dropdownList = dropdownMenu.querySelector('ul')
 
-  dropdownList.style.display = 'none';
-  
-  dropdownList.style.position = 'absolute';
-  dropdownList.style.top = '0';
-  dropdownList.style.left = '100%';
+  dropdownList.style.display = 'none'
+
+  dropdownList.style.position = 'absolute'
+  dropdownList.style.top = '0'
+  dropdownList.style.left = '100%'
 
   document.addEventListener('click', (event) => {
     if (!dropdownMenu.contains(event.target)) {
-      dropdownList.style.display = 'none';
+      dropdownList.style.display = 'none'
     }
-  });
+  })
 
   dropdownMenu.addEventListener('click', () => {
     if (dropdownList.style.display === 'none') {
-      dropdownList.style.display = 'block';
+      dropdownList.style.display = 'block'
     } else {
-      dropdownList.style.display = 'none';
+      dropdownList.style.display = 'none'
     }
-  });
-});
+  })
+})
