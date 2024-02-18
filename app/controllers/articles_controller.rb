edc2e762c500ee6b73f59ef8e8d9fe660ec3afde
goruchan <ppcfr481@yahoo.co.jp>
2024-02-18 09:27:45 +0900

    WIPブログのコントローラができた、公開用コントローラからWIPを削除

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,7 +60,7 @@ class ArticlesController < ApplicationController
 
   def list_articles
     articles = Article.with_attached_thumbnail.includes(user: { avatar_attachment: :blob }).order(created_at: :desc).page(params[:page])
-    admin_or_mentor_login? ? articles : articles.where(wip: false)
+    articles.where(wip: false)
   end
 
   def list_recent_articles
