c37a50c80d40d9c0c42cdb7fc4fe98f954ca9eef
88-99 <xz835.525o39@gmail.com>
2024-02-18 18:30:45 +0900

    登録した管理者向けメモを、管理者以外のアカウントでは表示されないことをチェックするためのテストを実装

--- test/system/admin/companies_test.rb
+++ test/system/admin/companies_test.rb
@@ -80,4 +80,22 @@ class Admin::CompaniesTest < ApplicationSystemTestCase
       assert_selector 'td.company-name', text: '【created_at降順確認】最古株式会社'
     end
   end
+
+  test 'don\'t show adomin memo on company show' do
+    visit_with_auth "/admin/companies/#{companies(:company1).id}/edit", 'komagata'
+    fill_in 'company[memo]', with: '管理者のみが閲覧できるメモです。'
+    click_button '更新する'
+    visit_with_auth company_path(companies(:company1)), 'mentormentaro'
+
+    assert_no_text '管理者のみが閲覧できるメモです。。'
+  end
+
+  test 'don\'t show adomin memo on company index' do
+    visit_with_auth "/admin/companies/#{companies(:company1).id}/edit", 'komagata'
+    fill_in 'company[memo]', with: '管理者のみが閲覧できるメモです。'
+    click_button '更新する'
+    visit_with_auth users_companies_path, 'kimura'
+
+    assert_no_text '管理者のみが閲覧できるメモです。'
+  end
 end
