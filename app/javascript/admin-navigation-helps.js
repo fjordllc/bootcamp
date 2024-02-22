54e6f1fc35ce310aff84f4bdaad17535b1afa577
machida <machida@fjord.jp>
2024-02-22 15:40:49 +0900

    ヘルプリンクの階層調整

--- app/javascript/admin-navigation-helps.js
+++ app/javascript/admin-navigation-helps.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dropdownMenu = document.querySelector('.admin-nav-helps')
-  const dropdownList = dropdownMenu.querySelector('ul')
+  const dropdownMenu = document.querySelector('.js-admin-helps')
+  const dropdownList = document.querySelector('.js-admin-helps__target')
+  const dropdownListParent = document.querySelector('.global-nav__inner')
 
   dropdownList.style.display = 'none'
 
@@ -11,14 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('click', (event) => {
     if (!dropdownMenu.contains(event.target)) {
       dropdownList.style.display = 'none'
+      dropdownListParent.style.overflowY = 'auto'
     }
   })
 
   dropdownMenu.addEventListener('click', () => {
     if (dropdownList.style.display === 'none') {
       dropdownList.style.display = 'block'
+      dropdownListParent.style.overflowY = 'visible'
     } else {
       dropdownList.style.display = 'none'
+      dropdownListParent.style.overflowY = 'auto'
     }
   })
 })
