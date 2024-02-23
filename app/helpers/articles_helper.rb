1701bb26397db1927316bcec6e01ca337f161986
masyuko0222 <masyuko0222@gmail.com>
2024-02-23 23:17:53 +0900

    mainブランチとコンフリクトを起こさないように。

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,12 +5,12 @@ module ArticlesHelper
     User.admins.or(User.mentor).pluck(:login_name, :id).sort
   end
 
+  def thumbnail_blank?(article)
+    !article.thumbnail.attached? && article.thumbnail_type == 'prepared_thumbnail'
+  end
+
   def ogp_image_url(image_url)
     base_url = "#{request.protocol}#{request.host_with_port}"
     base_url + image_url
-  end
-
-  def thumbnail_blank?(article)
-    !article.thumbnail.attached? && article.thumbnail_type == 'prepared_thumbnail'
   end
 end
