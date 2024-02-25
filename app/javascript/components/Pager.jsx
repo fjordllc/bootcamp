0a7335bccf6ce2af960ba3ff20130a9e3c7a2be2
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2024-02-25 13:16:37 +0900

    5期生のユーザを追加しページネーションを確認

--- app/javascript/components/Pager.jsx
+++ app/javascript/components/Pager.jsx
@@ -7,30 +7,38 @@ export default function Pager({
   pageRange,
   clickHandle
 }) {
-  const makeITag = (iconName) => {
-    return <i className={`fas fa-angle-${iconName}`}></i>
-  }
+  const makeITag = (iconName) => <i className={`fas fa-angle-${iconName}`}></i>
 
   return (
-    <ReactPaginate
-      forcePage={initialPageNumber}
-      pageCount={pageCount}
-      pageRangeDisplayed={pageRange}
-      onPageChange={(event) => clickHandle(event.selected + 1)}
-      previousLabel={makeITag('left')}
-      nextLabel={makeITag('right')}
-      breakLabel={'...'}
-      breakClassName={'break-me'}
-      marginPagesDisplayed={0}
-      containerClassName={'pagination__items'}
-      pageClassName={'pagination__item'}
-      pageLinkClassName={'pagination__item-link'}
-      previousClassName={'pagination__item is-prev'}
-      previousLinkClassName={'pagination__item-link is-prev'}
-      nextClassName={'pagination__item is-next'}
-      nextLinkClassName={'pagination__item-link is-next'}
-      activeClassName={'is-active'}
-      disabledClassName={'is-disabled'}
-    />
+    <div className="pagination-container">
+      <button onClick={() => clickHandle(1)} className="pagination__item-link">
+        {makeITag('double-left')}
+      </button>
+
+      <ReactPaginate
+        forcePage={initialPageNumber - 1}
+        pageCount={pageCount}
+        pageRangeDisplayed={pageRange}
+        marginPagesDisplayed={0}
+        previousLabel={makeITag('left')}
+        nextLabel={makeITag('right')}
+        onPageChange={(e) => clickHandle(e.selected + 1)}
+        containerClassName="pagination__items"
+        pageClassName="pagination__item"
+        pageLinkClassName="pagination__item-link"
+        previousClassName="pagination__item is-prev"
+        previousLinkClassName="pagination__item-link is-prev"
+        nextClassName="pagination__item is-next"
+        nextLinkClassName="pagination__item-link is-next"
+        activeClassName="is-active"
+        disabledClassName="is-disabled"
+      />
+
+      <button
+        onClick={() => clickHandle(pageCount)}
+        className="pagination__item-link">
+        {makeITag('double-right')}
+      </button>
+    </div>
   )
 }
