0562b7b1adf423484033b9993bb1bae3958e01fe
yocchan-git <yocchan.business@gmail.com>
2024-02-25 14:40:16 +0900

    decoratorにロジックを移動させる

--- test/decorators/product_decorator_test.rb
+++ test/decorators/product_decorator_test.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'active_decorator_test_case'
 
-class ProductHelperTest < ActionView::TestCase
+class ProductDecoratorTest < ActiveDecoratorTestCase
   test 'practice_content_for_toggle' do
-    product = products(:product8)
+    product = ActiveDecorator::Decorator.instance.decorate(products(:product8))
 
-    practice_content = practice_content_for_toggle(product, :practice)
+    practice_content = product.practice_content_for_toggle(:practice)
     assert_equal 'toggle_description_body', practice_content[:id_name]
     assert_equal product.practice.description, practice_content[:description]
 
-    practice_content = practice_content_for_toggle(product, :goal)
+    practice_content = product.practice_content_for_toggle(:goal)
     assert_equal 'toggle_goal_body', practice_content[:id_name]
     assert_equal product.practice.goal, practice_content[:description]
   end
