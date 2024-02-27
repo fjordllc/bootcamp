e0db0c3e6751336f7335f433b99c78eb05e2e2e1
Koji Ishimoto <koji.ishimoto@gmail.com>
2024-02-28 04:26:31 +0900

    Markdownコンポーネントの追加

---
+++ app/javascript/components/Markdown/index.jsx
@@ -0,0 +1,99 @@
+import React, { forwardRef } from 'react'
+import clsx from 'clsx'
+
+const Markdown = ({
+  children,
+}) => {
+  return (
+    <div className='a-markdown-input js-markdown-parent'>
+      {children}
+    </div>
+  )
+}
+
+Markdown.displayName = 'Markdown'
+
+const MarkdownItem = ({
+  isActive,
+  children,
+}) => {
+  return (
+    <div className={clsx('a-markdown-input__inner js-tabs__content', { 'is-active': isActive })}>
+      {children}
+    </div>
+  )
+}
+
+MarkdownItem.displayName = 'MarkdownItem'
+
+const MarkdownForm = ({ children }) => {
+  return (
+    <div className="form-textarea">
+      {children}
+    </div>
+  )
+}
+
+MarkdownForm.displayName = 'MarkdownForm'
+
+const MarkdownTextarea = forwardRef(({
+  variant = 'primary',
+  className,
+  description,
+  setDescription,
+  ...props
+}, ref) => {
+  return (
+    <div className="form-textarea__body">
+      <textarea
+        className={clsx('a-text-input a-markdown-input__textarea', variant === 'warning' ? 'js-warning-form' : 'primary', className)}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        ref={ref}
+        {...props}
+      />
+    </div>
+  )
+})
+
+MarkdownTextarea.displayName = 'MarkdownTextarea'
+
+const MarkdownFile = ({ ...props }) => {
+  return (
+    <div className="form-textarea__footer">
+      <div className="form-textarea__insert">
+        <label className="a-file-insert a-button is-xs is-text-reversal is-block">
+          ファイルを挿入
+          <input
+            type="file"
+            multiple
+            {...props}
+          />
+        </label>
+      </div>
+    </div>
+  )
+}
+
+MarkdownFile.displayName = 'MarkdownFile'
+
+const MarkdownPreview = forwardRef(({ ...props }, ref) => {
+  return (
+    <div
+      className="a-long-text is-md a-markdown-input__preview"
+      ref={ref}
+      {...props}
+    />
+  )
+})
+
+MarkdownPreview.displayName = 'MarkdownPreview'
+
+const Root = Markdown
+const Item = MarkdownItem
+const Form = MarkdownForm
+const Textarea = MarkdownTextarea
+const File = MarkdownFile
+const Preview = MarkdownPreview
+
+export { Root, Item, Form, Textarea, File, Preview }
