4e62cef053045f55648e46b8db084e807a263f6e
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-02-27 21:03:42 +0900

    検索内容のバリデーションを設定

--- app/controllers/talks/action_uncompleted_controller.rb
+++ app/controllers/talks/action_uncompleted_controller.rb
@@ -8,6 +8,8 @@ class Talks::ActionUncompletedController < ApplicationController
                  .includes(user: [{ avatar_attachment: :blob }, :discord_profile])
                  .action_uncompleted
                  .order(updated_at: :desc, id: :asc)
+    params[:search_word] = validate_search_word(params[:search_word]) if params[:search_word]
+
     if params[:search_word]
       @searched_talks = @talks.merge(
         User.search_by_keywords({ word: params[:search_word] })
@@ -15,6 +17,14 @@ class Talks::ActionUncompletedController < ApplicationController
       ).page(params[:page])
     else
       @talks = @talks.page(params[:page])
+    end
+  end
+
+  def validate_search_word(search_word)
+    if search_word.match?(/^[\w-]+$/)
+      search_word.strip if search_word.strip.length >= 3
+    elsif search_word.strip.length >= 2
+      search_word.strip
     end
   end
 end
