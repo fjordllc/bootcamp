1bafc5899cd6de337a4cee5cad806b8e84d3d6e1
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-02-27 12:49:33 +0900

    相談部屋一覧のVueファイルの内容をslim化する
    コントローラも修正し、app/controllers/talks_controller.rbに移動

--- app/controllers/talks/action_uncompleted_controller.rb
+++ app/controllers/talks/action_uncompleted_controller.rb
@@ -2,5 +2,20 @@
 
 class Talks::ActionUncompletedController < ApplicationController
   before_action :require_admin_login
-  def index; end
+
+  def index
+    @talks = Talk.joins(:user)
+                 .includes(user: [{ avatar_attachment: :blob }, :discord_profile])
+                 .action_uncompleted
+                 .order(updated_at: :desc, id: :asc)
+    if params[:search_word]
+      @searched_talks = @talks.merge(
+        User.search_by_keywords({ word: params[:search_word] })
+            .unscope(where: :retired_on)
+      )
+    else
+      # 後ほどページネーションを実装する
+      @talks = @talks
+    end
+  end
 end
--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -7,8 +7,24 @@ class TalksController < ApplicationController
   before_action :set_members, only: %i[show]
   before_action :allow_show_talk_page_only_admin, only: %i[show]
 
+  ALLOWED_TARGETS = %w[all student_and_trainee mentor graduate adviser trainee retired].freeze
+
   def index
     @target = params[:target]
+    @talks = Talk.joins(:user)
+                 .includes(user: [{ avatar_attachment: :blob }, :discord_profile])
+                 .order(updated_at: :desc, id: :asc)
+    users = User.users_role(@target, allowed_targets: ALLOWED_TARGETS, default_target: 'all')
+    if params[:search_word]
+      # search_by_keywords内では { unretired } というスコープが設定されている
+      # 退会したユーザーも検索対象に含めたいので、unscope(where: :retired_on) で上記のスコープを削除
+      searched_users = users.search_by_keywords(word: params[:search_word]).unscope(where: :retired_on)
+      # もし検索対象が退会したユーザーである場合、searched_usersには退会していないユーザーも含まれているため、retired スコープを設定する
+      searched_users = searched_users.retired if @target == 'retired'
+      @searched_talks = @talks.merge(searched_users)
+    else
+      @talks = @talks.merge(users)
+    end
   end
 
   def show; end
