69ad327bdfc45a71147417619a8353f773c1900f
machida <machida@fjord.jp>
2024-02-28 13:28:47 +0900

    ヘルプの位置変更とデザインの調整

--- app/javascript/admin-navigation-helps.js
+++ app/javascript/admin-navigation-helps.js
@@ -1,28 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dropdownMenu = document.querySelector('.js-admin-helps')
   const dropdownList = document.querySelector('.js-admin-helps__target')
-  const dropdownListParent = document.querySelector('.global-nav__inner')
+  const dropdownListParent = document.querySelector('.header-links__link')
 
   dropdownList.style.display = 'none'
 
   dropdownList.style.position = 'absolute'
-  dropdownList.style.top = '0'
-  dropdownList.style.left = '100%'
+  dropdownList.style.top = '100%'
+  dropdownList.style.right = '0%'
 
   document.addEventListener('click', (event) => {
     if (!dropdownMenu.contains(event.target)) {
       dropdownList.style.display = 'none'
-      dropdownListParent.style.overflowY = 'auto'
     }
   })
 
   dropdownMenu.addEventListener('click', () => {
     if (dropdownList.style.display === 'none') {
       dropdownList.style.display = 'block'
-      dropdownListParent.style.overflowY = 'visible'
     } else {
       dropdownList.style.display = 'none'
-      dropdownListParent.style.overflowY = 'auto'
     }
   })
 })
