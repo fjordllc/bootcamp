c62bc2f7ce6e02d7c1bcbb2a0bf0ae6919cacae8
machida <machida@fjord.jp>
2024-02-28 17:12:10 +0900

    ナビゲーションの開閉を共通化

--- app/javascript/header-dropdown.js
+++ app/javascript/header-dropdown.js
@@ -1,22 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dropdownMenu = document.querySelector('.js-header-dropdown');
-
-  if (!dropdownMenu) {
-    console.warn('.js-header-dropdown not found');
-    return;
-  }
+  const dropdownMenus = document.querySelectorAll('.js-header-dropdown')
 
   document.addEventListener('click', (event) => {
-    if (!dropdownMenu.contains(event.target)) {
-      dropdownMenu.classList.remove('is-opened-dropdown');
-    }
-  });
+    dropdownMenus.forEach((dropdownMenu) => {
+      if (!dropdownMenu.contains(event.target)) {
+        dropdownMenu.classList.remove('is-opened-dropdown')
+      }
+    })
+  })
 
-  dropdownMenu.addEventListener('click', () => {
-    if (dropdownMenu.classList.contains('is-opened-dropdown')) {
-      dropdownMenu.classList.remove('is-opened-dropdown');
-    } else {
-      dropdownMenu.classList.add('is-opened-dropdown');
-    }
-  });
-});
+  dropdownMenus.forEach((dropdownMenu) => {
+    dropdownMenu.addEventListener('click', () => {
+      if (dropdownMenu.classList.contains('is-opened-dropdown')) {
+        dropdownMenu.classList.remove('is-opened-dropdown')
+      } else {
+        dropdownMenu.classList.add('is-opened-dropdown')
+      }
+    })
+  })
+})
