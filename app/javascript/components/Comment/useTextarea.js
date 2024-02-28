8ebd6f1877e5ed3a65ad0b6b92109a348eb46e54
Koji Ishimoto <koji.ishimoto@gmail.com>
2024-02-28 14:53:20 +0900

    textareaをuseRefで管理するための変更

--- app/javascript/components/Comment/useTextarea.js
+++ app/javascript/components/Comment/useTextarea.js
@@ -1,18 +1,13 @@
 import { useEffect } from 'react'
-import TextareaInitializer from '../../textarea-initializer-copy'
+import TextareaInitializer from '../../textarea-initializer-element'
 
-/**
- * propTypes.node
-*/
 export const useTextarea = (elementRef) => {
-  // const defaultTextareaSize = element.current.scrollHeight
-
   useEffect(() => {
     TextareaInitializer.initialize(elementRef.current)
   }, [])
 
-  const resizeTextarea = () => {
-    // element.current.style.height = `${defaultTextareaSize}px`
-  }
-  return { resizeTextarea }
+  // const resizeTextarea = () => {
+  //   // element.current.style.height = `${defaultTextareaSize}px`
+  // }
+  // return { resizeTextarea }
 }
