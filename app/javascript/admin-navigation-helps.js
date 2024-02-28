735a772c04e877aeb127fbf2bf30f4bece1ea0ab
machida <machida@fjord.jp>
2024-02-28 14:45:14 +0900

    不要なスタイル設定を削除

--- app/javascript/admin-navigation-helps.js
+++ app/javascript/admin-navigation-helps.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dropdownMenu = document.querySelector('.js-admin-helps')
   const dropdownList = document.querySelector('.js-admin-helps__target')
-  const dropdownListParent = document.querySelector('.header-links__link')
 
   dropdownList.style.display = 'none'
-
-  dropdownList.style.position = 'absolute'
-  dropdownList.style.top = '100%'
-  dropdownList.style.right = '0%'
 
   document.addEventListener('click', (event) => {
     if (!dropdownMenu.contains(event.target)) {
