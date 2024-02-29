1ce56d6e87eddf953666e587198c28c6d185d03a
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-02-29 19:41:12 +0900

    ユーザー登録のemailにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -153,7 +153,7 @@ class User < ApplicationRecord
 
   after_create UserCallbacks.new
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :name, presence: true
   validates :description, presence: true
   validates :nda, presence: true
