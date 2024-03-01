fc0ce53fbfb126ec8bb3d0ecedc5ff39f28d17d2
appsu-mi <natubate870@gmail.com>
2024-03-01 21:24:32 +0900

    企業一覧ページから各企業のアドバイザー・現役生のページへ飛べるようにした

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -5,4 +5,8 @@ module CompaniesHelper
     desc_ordered_companies = Company.order(created_at: :desc).to_a
     desc_ordered_companies.unshift(Company.new(name: '所属なし'))
   end
+
+  def link_to_company_users_path(company, target)
+    link_to t("target.#{target}"), company_users_path(target:, company_id: company.id), class: 'tab-nav__item-link'
+  end
 end
