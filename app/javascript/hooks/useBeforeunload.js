36aeabd9a285468b095a38491a2f7c5e397be3d2
Koji Ishimoto <koji.ishimoto@gmail.com>
2024-03-04 23:32:23 +0900

    MarkdownでuseBeforeunloadを使うよう変更

--- app/javascript/hooks/useBeforeunload.js
+++ app/javascript/hooks/useBeforeunload.js
@@ -1,30 +1,32 @@
+import { useCallback } from 'react'
+
+/**
+ * HTMLの仕様ではpreventDefaultによって確認ダイアログを出力させて、Chromeに対応するために必要な古い仕様ではreturnValueに空文字を設定する
+ * @see https://html.spec.whatwg.org/multipage/nav-history-apis.html#the-beforeunloadevent-interface
+ */
+const handleBeforeUnload = (event) => {
+  event.preventDefault()
+  event.returnValue = ''
+}
+
 /**
  * 編集中の未保存のテキストがある状態でページ遷移しようとした時に警告の確認ダイアログを表示させるreact hookです
- * @see https://developer.chrome.com/articles/page-lifecycle-api/#the-beforeunload-event
+ * @see https://developer.chrome.com/docs/web-platform/page-lifecycle-api?hl=ja#the_beforeunload_event
  */
 export const useBeforeunload = () => {
   /**
-   * HTMLの仕様ではpreventDefaultによって確認ダイアログを出力させて、Chromeに対応するために必要な古い仕様ではreturnValueに空文字を設定する
-   * @see https://html.spec.whatwg.org/multipage/nav-history-apis.html#the-beforeunloadevent-interface
-   */
-  const handleBeforeUnload = (event) => {
-    event.preventDefault()
-    event.returnValue = ''
-  }
-
-  /**
    * @returns {() => void} テキストの編集を始めたときに実行する関数
    */
-  const onPageHasUnsavedChanges = () => {
+  const onPageHasUnsavedChanges = useCallback(() => {
     addEventListener('beforeunload', handleBeforeUnload)
-  }
+  }, [])
 
   /**
    * @returns {() => void} テキストの保存が完了したときに実行する関数
    */
-  const onAllChangesSaved = () => {
+  const onAllChangesSaved = useCallback(() => {
     removeEventListener('beforeunload', handleBeforeUnload)
-  }
+  }, [])
 
   return { onPageHasUnsavedChanges, onAllChangesSaved }
 }
