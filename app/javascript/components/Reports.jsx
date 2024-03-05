1026ed38cf3e53de882f170b4e6532eb40ebc281
hirano-vm4 <hirano.vm4@gmail.com>
2024-03-05 23:00:33 +0900

    管理者権限からみた相談部屋内の「ユーザー日報」タブの表示バグを修正

--- app/javascript/components/Reports.jsx
+++ app/javascript/components/Reports.jsx
@@ -136,10 +136,12 @@ const NoReports = ({ unchecked }) => {
             </p>
           </>
         ) : (
-          <>
-            <i className="fa-regular fa-sad-tear" />
-            <p className="o-empty-message__text">日報はまだありません。</p>
-          </>
+          <div className="card-list a-card">
+            <div className="card-list__items">
+              <i className="fa-regular fa-sad-tear" />
+              <p className="o-empty-message__text">日報はまだありません。</p>
+            </div>
+          </div>
         )}
       </div>
     </div>
