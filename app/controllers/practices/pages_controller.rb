68c8cfb21022fe1794c5ae24fe4d23acdef295a2
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-06 11:15:21 +0900

    プラクティス別Docsページをslimに置き換えた

--- app/controllers/practices/pages_controller.rb
+++ app/controllers/practices/pages_controller.rb
@@ -3,5 +3,9 @@
 class Practices::PagesController < ApplicationController
   def index
     @practice = Practice.find(params[:practice_id])
+    @pages = Page.with_avatar
+                 .includes(:comments, :practice, { last_updated_user: { avatar_attachment: :blob } })
+                 .where(practice_id: params[:practice_id])
+                 .order(updated_at: :desc)
   end
 end
