cbd443302798b5ebc8fa4593fa6a2a61c393ce45
reckyy <srreki98@gmail.com>
2024-03-08 12:03:24 +0900

    ヘルパーメソッドのテスト追加

--- test/helpers/users_helper.rb
+++ test/helpers/users_helper.rb
@@ -8,4 +8,33 @@ class UsersHelperTest < ActionView::TestCase
     assert_includes countries['JP'], %w[北海道 01]
     assert_includes countries['US'], %w[アラスカ州 AK]
   end
+
+  test 'roles_for_select returns array for select_tag containing role info' do
+    user_roles = [
+      %w[全員 all],
+      %w[現役生 student_and_trainee],
+      %w[非アクティブ inactive],
+      %w[休会 hibernated],
+      %w[退会 retired],
+      %w[卒業 graduate],
+      %w[アドバイザー adviser],
+      %w[メンター mentor],
+      %w[研修生 trainee],
+      %w[忘年会 year_end_party],
+      %w[お試し延長 campaign]
+    ]
+    assert roles_for_select, user_roles
+  end
+
+  test 'jobs_for_select returns array for select_tag containing job info' do
+    user_jobs = [
+      %w[全員 all],
+      %w[学生 student],
+      %w[会社員 office_worker],
+      %w[フリーター part_time_worker],
+      %w[休職中 vacation],
+      %w[働いていない unemployed]
+    ]
+    assert jobs_for_select, user_jobs
+  end
 end
