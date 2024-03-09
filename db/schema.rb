b514f912c2bf019aff57bae08d871b528ea9bb12
88-99 <xz835.525o39@gmail.com>
2024-03-09 18:49:20 +0900

    usersテーブルにother_editorカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_09_091258) do
+ActiveRecord::Schema.define(version: 2024_03_09_094130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -734,6 +734,7 @@ ActiveRecord::Schema.define(version: 2024_03_09_091258) do
     t.boolean "auto_retire", default: true
     t.boolean "invoice_payment", default: false, null: false
     t.integer "editor"
+    t.string "other_editor"
     t.index ["course_id"], name: "index_users_on_course_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["github_id"], name: "index_users_on_github_id", unique: true
