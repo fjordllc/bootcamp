c5d409ec63f9266d09518afa5c685343b70a2bb4
SuzukaHori <horikoshi.suzuka@gmail.com>
2024-03-11 11:07:47 +0900

    プラクティスのDocsページにもページネーションを追加

--- app/controllers/practices/pages_controller.rb
+++ app/controllers/practices/pages_controller.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class Practices::PagesController < ApplicationController
+  PAGER_NUMBER = 20
+
   def index
     @practice = Practice.find(params[:practice_id])
     @pages = Page.with_avatar
                  .includes(:comments, :practice, { last_updated_user: { avatar_attachment: :blob } })
                  .where(practice_id: params[:practice_id])
                  .order(updated_at: :desc, id: :desc)
+                 .page(params[:page])
+                 .per(PAGER_NUMBER)
   end
 end
