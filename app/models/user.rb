299e00b3197aaf583758c574db55bd73c6e362c2
hirano-vm4 <hirano.vm4@gmail.com>
2024-03-11 13:55:09 +0900

    Userモデルのhide_mentor_profileに真偽値バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -167,6 +167,7 @@ class User < ApplicationRecord
   validates :nda, presence: true
   validates :password, length: { minimum: 4 }, confirmation: true, if: :password_required?
   validates :mail_notification, inclusion: { in: [true, false] }
+  validates :hide_mentor_profile, inclusion: { in: [true, false] }
   validates :github_id, uniqueness: true, allow_nil: true
   validates :other_editor, presence: true, if: -> { editor == 'other_editor' }
 
@@ -324,6 +325,13 @@ class User < ApplicationRecord
       .mentor
       .includes(authored_books: { cover_attachment: :blob })
       .order(:created_at)
+  }
+  scope :visible_sorted_mentors, lambda {
+    with_attached_profile_image
+      .mentor
+      .includes(authored_books: { cover_attachment: :blob })
+      .order(:created_at)
+      .where(hide_mentor_profile: false)
   }
   scope :working, lambda {
     active.where(
