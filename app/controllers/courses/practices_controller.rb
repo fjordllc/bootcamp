d24873ffddabb647ac92e2cc7d3b40ef9365f8d0
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-03-12 17:59:08 +0900

    htmlとjsでの実装に変更

--- app/controllers/courses/practices_controller.rb
+++ app/controllers/courses/practices_controller.rb
@@ -3,5 +3,7 @@
 class Courses::PracticesController < ApplicationController
   def index
     @course = Course.find(params[:course_id])
+    @categories = Course.find(@course.id).categories.order(:created_at)
+    @learnings = current_user.learnings
   end
 end
