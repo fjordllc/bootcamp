e16ac9d78a3ad350f6a78e579f4f068736aac0a9
naokinaokiboo <naokinaokiboo@gmail.com>
2024-03-12 12:30:28 +0900

    ドロップダウンの選択項目を元に招待URLを作成する処理を追加

---
+++ app/javascript/invitation-url-updater.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  const invitationElements = Array.from(
+    document.querySelectorAll('.invitation-elements select')
+  )
+  const invitationUrl = document.querySelector('.js-invitation-url')
+
+  if (!invitationElements || !invitationUrl) {
+    return
+  }
+
+  const updateInvitationURL = async () => {
+    const invitationCompany = document.querySelector('.js-invitation-company')
+    const invitationRole = document.querySelector('.js-invitation-role')
+    const invitationCourse = document.querySelector('.js-invitation-course')
+
+    const params = new URLSearchParams()
+    params.append(
+      'company_id',
+      invitationCompany.options[invitationCompany.selectedIndex].value
+    )
+    params.append(
+      'role',
+      invitationRole.options[invitationRole.selectedIndex].value
+    )
+    params.append(
+      'course_id',
+      invitationCourse.options[invitationCourse.selectedIndex].value
+    )
+
+    const response = await fetch(
+      `/api/admin/invitation_url?${params.toString()}`
+    ).catch((error) => console.warn(error))
+    const json = await response.json().catch((error) => console.warn(error))
+
+    invitationUrl.href = json.url
+    invitationUrl.textContent = json.url
+  }
+
+  invitationElements.forEach((invitationElement) => {
+    invitationElement.addEventListener('change', updateInvitationURL)
+  })
+
+  await updateInvitationURL()
+})
