8ade8e7abdf42e7a9375a0812961a029d2a33724
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-03-13 16:54:19 +0900

    動画をダイレクトアップロードで追加できるように

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
+  validates :title, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
+  validates :description, presence: true
 end
