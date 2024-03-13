381e16ca2bffb547253553c9f0962c5840ce2a6a
unikounio <bubbbble428@gmail.com>
2024-03-13 15:37:12 +0900

    schema.rbのversionを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_28_105218) do
+ActiveRecord::Schema.define(version: 2024_03_12_150915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
