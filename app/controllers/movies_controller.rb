5cbf26556be74b6a7ea11953ee711a6a1f75c76f
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-03-13 16:54:19 +0900

    動画をダイレクトアップロードで追加できるように

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,32 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.order(created_at: :desc)
   end
 
   def new
-    
+    @movie = Movie.new
   end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.user = current_user
+    if @movie.save
+      redirect_to @movie, notice: '動画を追加しました。'
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def movie_params
+    params.require(:movie).permit(
+      :title,
+      :description,
+      :tags,
+      :public_scope,
+      :movie_data
+    )
+  end
+
 end
