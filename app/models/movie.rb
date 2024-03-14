b68406a8c6ed672195457f2f3b0628e154f961dc
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-03-14 15:09:42 +0900

    動画アップロード機能追加とアップロードページ実装

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,8 @@
 class Movie < ApplicationRecord
+  include Taggable
+
   belongs_to :user
+  belongs_to :practice, optional: true
 
   validates :user, presence: true
   validates :title, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
