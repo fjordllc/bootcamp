6493775aed16571d0ba338c2c04fc331385105ed
naokinaokiboo <naokinaokiboo@gmail.com>
2024-03-14 13:03:20 +0900

    通知用Jobに例外処理を追加

--- app/jobs/post_announcement_job.rb
+++ app/jobs/post_announcement_job.rb
@@ -5,7 +5,24 @@ class PostAnnouncementJob < ApplicationJob
 
   def perform(announcement, receivers)
     receivers.each do |receiver|
-      ActivityDelivery.with(announcement:, receiver:).notify!(:post_announcement)
+      send_mail_notification(announcement, receiver)
+      send_on_site_notification(announcement, receiver)
     end
+  end
+
+  private
+
+  def send_mail_notification(announcement, receiver)
+    ActivityMailer.post_announcement(announcement:, receiver:).deliver_now
+  rescue Postmark::InactiveRecipientError => e
+    Rails.logger.warn "[Postmark] 受信者由来のエラーのためメールを送信できませんでした。：#{e.message}"
+  rescue StandardError => e
+    Rails.logger.warn(e)
+  end
+
+  def send_on_site_notification(announcement, receiver)
+    ActivityNotifier.post_announcement(announcement:, receiver:).notify_now
+  rescue StandardError => e
+    Rails.logger.warn(e)
   end
 end
