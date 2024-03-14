1f1549e36d758865e95df5aecdd5465ba95785ca
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-03-14 15:09:42 +0900

    動画アップロード機能追加とアップロードページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -109,7 +109,6 @@ class User < ApplicationRecord
   has_one :report_template, dependent: :destroy
   has_one :talk, dependent: :destroy
   has_one :discord_profile, dependent: :destroy
-  has_one :movies, dependent: :destroy
   accepts_nested_attributes_for :discord_profile, allow_destroy: true
   has_many :request_retirements, dependent: :destroy
   has_one :targeted_request_retirement, class_name: 'RequestRetirement', foreign_key: 'target_user_id', dependent: :destroy, inverse_of: :target_user
