af22e56f8a87a842a707e7f3803035ec48698568
reckyy <srreki98@gmail.com>
2024-03-16 10:39:03 +0900

    [refactor]不要なステップを削除 また、以下の修正も追加で行った  calendar_with_reportsメソッドの第二引数をcurrent_dateに変更（後にdateという変数を登場させたかったため）

--- app/controllers/concerns/calendar_methods.rb
+++ app/controllers/concerns/calendar_methods.rb
@@ -10,19 +10,18 @@ module CalendarMethods
     params_date.future? ? today : params_date
   end
 
-  def calendar_with_reports(user, date)
-    first_date_of_month = date.beginning_of_month
-    last_date_of_month = date.end_of_month
+  def calendar_with_reports(user, current_date)
+    first_date_of_month = current_date.beginning_of_month
+    last_date_of_month = current_date.end_of_month
     dates_of_month = (first_date_of_month..last_date_of_month)
 
     reports = user.reports.where(reported_on: dates_of_month, wip: false)
-    emotions = reports.index_by(&:reported_on)
+    reports_by_date = reports.index_by(&:reported_on)
 
     dates = dates_of_month.index_with { |_day| nil }
 
-    dates.merge(emotions)
-         .to_a
-         .map { |set| [report: set[1], date: set[0], emotion: set[1]&.emotion] }
-         .flatten
+    dates.merge(reports_by_date).map do |date, report|
+      { report:, date:, emotion: report&.emotion }
+    end
   end
 end
