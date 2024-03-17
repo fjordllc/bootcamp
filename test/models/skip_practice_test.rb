c7c8248912dc0b7154b48241e7d82e33f5b417f7
ko <kyokucho1989@gmail.com>
2024-03-18 05:47:26 +0900

    skip_practiceのテスト追加

--- test/models/skip_practice_test.rb
+++ test/models/skip_practice_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class SkipPracticeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :practices, :users
+
+  test '# practice_belongs_to_user' do
+    user1 = users(:kensyu)
+    skip_practice = user1.skip_practices.new(practice: practices(:practice56))
+    assert_not skip_practice.save
+  end
 end
