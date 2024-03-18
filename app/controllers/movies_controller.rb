b035ef73bafcb9c9f2e074ab5c70db0ee95781e1
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-03-18 15:14:53 +0900

    サムネイルを自動生成するように

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'streamio-ffmpeg'
+
 class MoviesController < ApplicationController
   before_action :set_categories, only: %i[new create]
 
@@ -17,13 +21,40 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
     @movie.user = current_user
-    if @movie.save
-      redirect_to @movie, notice: '動画を追加しました。'
-    else
-      render :new
+
+    ActiveRecord::Base.transaction do
+      if @movie.save
+        redirect_to @movie, notice: '動画を追加しました。'
+  
+        movie_blob = params[:movie][:movie_data].tempfile.path
+        thumbnail_blob = generate_thumbnail_from_blob(movie_blob)
+
+        if thumbnail_blob
+          @movie.thumbnail.attach(thumbnail_blob)
+        else
+          raise ActiveRecord::Rollback, "サムネイルの添付に失敗しました。"
+        end
+      else
+        render :new, notice: '動画の追加に失敗しました。'
+      end
     end
+  rescue ActiveRecord::Rollback => e
+    redirect_to new_movie_path, alert: e.message
   end
 
+  def generate_thumbnail_from_blob(movie_blob)
+    movie_path = params[:movie][:movie_data].tempfile.path
+    thumbnail_path = Tempfile.new(['thumbnail', '.png']).path
+
+    movie = FFMPEG::Movie.new(movie_path)
+    movie.screenshot(thumbnail_path, { seek_time: 5, vframes: 1 })
+
+    thumbnail_blob = ActiveStorage::Blob.create_after_upload!(
+      io: File.open(thumbnail_path),
+      filename: 'thumbnail.jpg',
+      content_type: 'image/jpeg'
+    )
+  end
 
   private
 
@@ -40,8 +71,8 @@ class MoviesController < ApplicationController
       :title,
       :description,
       :tag_list,
-      :movie_data
+      :movie_data,
+      :practice_id
     )
   end
-
 end
