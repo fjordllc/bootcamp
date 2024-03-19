aa2d274dcf2de5983590fa2a274c040963ce164e
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-03-19 13:25:06 +0900

    重い処理を減らすために、リンクのテストを削除

--- test/system/admin/inquiries_test.rb
+++ test/system/admin/inquiries_test.rb
@@ -12,19 +12,10 @@ class Admin::InquiriesTest < ApplicationSystemTestCase
   end
 
   test 'show inquiry details' do
-    visit_with_auth '/admin/inquiries', 'komagata'
-    find('.card-list-item-title', text: 'inquiry1 様 （inquiry1@example.com）').click
-
+    visit_with_auth "/admin/inquiries/#{inquiries(:inquiry1).id}", 'komagata'
     assert_text inquiries(:inquiry1).name
     assert_text inquiries(:inquiry1).email
     assert_text I18n.l inquiries(:inquiry1).created_at.to_date
     assert_text inquiries(:inquiry1).body
-  end
-
-  test 'click on the link of back to inquiries index' do
-    visit_with_auth "/admin/inquiries/#{inquiries(:inquiry1).id}", 'komagata'
-    click_link 'お問い合わせ一覧へ'
-
-    assert_selector 'h1.page-main-header__title', text: 'お問い合わせ一覧'
   end
 end
