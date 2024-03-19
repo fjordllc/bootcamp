58dbaeb53e7e2591867e7cd6ffc733c4a6a35b15
rira <101010hayakawa@gmail.com>
2024-03-19 15:13:55 +0900

    アバター取得にログインネームを使用

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -560,7 +560,7 @@ class UsersTest < ApplicationSystemTestCase
     assert_text 'ユーザー情報を更新しました。'
     img = find('img.user-profile__user-icon-image', visible: false)
     user = users(:hajime)
-    assert_match(/#{user.id}\.png$/, img.native['src'])
+    assert_match(/#{user.login_name}\.png$/, img.native['src'])
   end
 
   test 'mentor can see retired and hibernated tabs' do
