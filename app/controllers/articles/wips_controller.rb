9e62e74bc37939d69f194d00ea8e63257ba9d8cc
goruchan <ppcfr481@yahoo.co.jp>
2024-03-19 22:24:05 +0900

    一文が長かったため長さを調整

--- app/controllers/articles/wips_controller.rb
+++ app/controllers/articles/wips_controller.rb
@@ -4,15 +4,15 @@ class Articles::WipsController < ApplicationController
   before_action :require_admin_or_mentor_login, only: %i[index]
 
   def index
-    @articles = list_articles
+    @articles = wip_articles
     @articles = @articles.tagged_with(params[:tag]) if params[:tag]
     render layout: 'welcome'
   end
 
   private
 
-  def list_articles
-    articles = Article.with_attached_thumbnail.includes(user: { avatar_attachment: :blob }).order(created_at: :desc).page(params[:page])
-    articles.where(wip: true)
+  def wip_articles
+    articles = Article.with_attached_thumbnail.includes(user: { avatar_attachment: :blob })
+    articles.where(wip: true).order(created_at: :desc).page(params[:page])
   end
 end
