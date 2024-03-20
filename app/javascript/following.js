eb037bb5868853a043de280a2d16d1269c7a069f
yocchan-git <yocchan.business@gmail.com>
2024-03-20 17:13:43 +0900

    フロント側でボタンの内容を出力するよう修正

--- app/javascript/following.js
+++ app/javascript/following.js
@@ -29,15 +29,40 @@ const usersIndex = {
       redirect: 'manual',
       body: JSON.stringify(params)
     })
-      .then(function (response) {
+      .then(response => {
         if (response.ok) {
           return response.json()
         } else {
           alert('フォロー処理に失敗しました')
         }
       })
-      .then(function (data) {
-        document.getElementById(`follow_${userId}`).innerHTML = data.html
+      .then(() => {
+        const button = document.getElementById(`follow_${userId}`);
+        const summary = button.querySelector('.following__summary span');
+        const options = Array.from(button.querySelectorAll('.following__dropdown-item button'));
+
+        if (isWatch) {
+          summary.className = 'a-button is-warning is-sm is-block';
+          summary.innerHTML = '<i class="fa-solid fa-check"></i><span>コメントあり</span>';
+
+          options[0].className = 'following-option a-dropdown__item-inner is-active';
+          options[0].onclick = null;
+          options[1].className = 'following-option a-dropdown__item-inner';
+          options[1].onclick = function() { usersIndex.followOrChangeFollow(userId, true, false); };
+          options[2].className = 'following-option a-dropdown__item-inner';
+          options[2].onclick = function() { usersIndex.unfollow(userId, true); };
+        } else {
+          summary.className = 'a-button is-warning is-sm is-block';
+          summary.innerHTML = '<i class="fa-solid fa-check"></i><span>コメントなし</span>';
+
+          options[0].className = 'following-option a-dropdown__item-inner';
+          options[0].onclick = function() { usersIndex.followOrChangeFollow(userId, true, true); };
+          options[1].className = 'following-option a-dropdown__item-inner is-active';
+          options[1].onclick = null;
+          options[2].className = 'following-option a-dropdown__item-inner';
+          options[2].onclick = function() { usersIndex.unfollow(userId, true); };
+        }
+        this.closeFollowDetails(userId);
       })
       .catch(function (error) {
         console.warn(error)
@@ -59,19 +84,37 @@ const usersIndex = {
       redirect: 'manual',
       body: JSON.stringify(params)
     })
-      .then(function (response) {
+      .then(response => {
         if (response.ok) {
           return response.json()
         } else {
           alert('フォロー処理に失敗しました')
         }
       })
-      .then(function (data) {
-        document.getElementById(`follow_${userId}`).innerHTML = data.html
+      .then(() => {
+        const button = document.getElementById(`follow_${userId}`);
+
+        const summary = button.querySelector('.following__summary span');
+        summary.className = 'a-button is-secondary is-sm is-block';
+        summary.innerHTML = "フォローする";
+
+        const options = Array.from(button.querySelectorAll('.following__dropdown-item button'));
+        options[0].className = 'following-option a-dropdown__item-inner';
+        options[0].onclick = function() { usersIndex.followOrChangeFollow(userId, false, true); };
+        options[1].className = 'following-option a-dropdown__item-inner';
+        options[1].onclick = function() { usersIndex.followOrChangeFollow(userId, false, false); };
+        options[2].className = 'following-option a-dropdown__item-inner is-active';
+        options[2].onclick = null;
+
+        this.closeFollowDetails(userId);
       })
       .catch(function (error) {
         console.warn(error)
       })
+  },
+  closeFollowDetails(userId) {
+    const details = document.getElementById(`follow_details${userId}`);
+    details.open = false
   }
 }
 
