a5826cddb27849c86df57ac7226ac4d5060073e4
appsu-mi <natubate870@gmail.com>
2024-03-20 12:51:03 +0900

    ヘルパーメソッドを再導入し企業の各usersへのリンクの呼び出しをDRYに

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -5,4 +5,9 @@ module CompaniesHelper
     desc_ordered_companies = Company.order(created_at: :desc).to_a
     desc_ordered_companies.unshift(Company.new(name: '所属なし'))
   end
+
+  def link_to_company_users_path(company, target:)
+    link_to company.users.unretired.send(target).size, company_users_path(target: target.singularize, company_id: company.id),
+            class: 'card-counts__item-value-link'
+  end
 end
