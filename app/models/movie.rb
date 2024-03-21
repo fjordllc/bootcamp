79e4acf9d144f67db35f0298572dceecd83319a0
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-03-21 11:37:17 +0900

    動画を表示できるようにして動画詳細ページを簡易実装

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,7 @@ class Movie < ApplicationRecord
   belongs_to :user
   belongs_to :practice, optional: true
   has_one_attached :thumbnail
+  has_one_attached :movie_data
 
   validates :user, presence: true
   validates :title, presence: true, uniqueness: { scope: :user_id }, length: { maximum: 255 }
