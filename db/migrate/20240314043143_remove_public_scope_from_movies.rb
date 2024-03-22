e83708f662190497aeeb26fe99dde6c63788f908
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-03-22 20:04:58 +0900

    マイグレーションファイルの整理

--- db/migrate/20240314043143_remove_public_scope_from_movies.rb
+++
@@ -1,5 +0,0 @@
-class RemovePublicScopeFromMovies < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :movies, :public_scope, :boolean
-  end
-end
--- db/migrate/20240315013154_rename_practices_id_to_practice_id_in_movies.rb
+++
@@ -1,5 +0,0 @@
-class RenamePracticesIdToPracticeIdInMovies < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :movies, :practices_id, :practice_id
-  end
-end
