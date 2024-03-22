b37b151f3016ae5d7b74f90ac86671c39e79d60c
yocchan-git <yocchan.business@gmail.com>
2024-03-22 19:38:21 +0900

    テストする項目を追加する

--- test/models/image_resizer_test.rb
+++ test/models/image_resizer_test.rb
@@ -3,9 +3,30 @@
 require 'test_helper'
 
 class ImageResizerTest < ActiveSupport::TestCase
+  setup do
+    user = users(:komagata)
+    @image = user.avatar
+  end
+
   test '正常時ActiveStorage::VariantWithRecordインスタンスが返ること' do
-    @user = users(:komagata)
-    resizer = ImageResizer.new(@user.avatar)
+    resizer = ImageResizer.new(@image)
     assert_instance_of ActiveStorage::VariantWithRecord, resizer.resize
+  end
+
+  test 'resize_to_fitとcropで切り抜くこと' do
+    image_resizer = ImageResizer.new(@image, resize_side: { width: 120, height: 120 })
+    format_option_keys = image_resizer.resize.variation.transformations.keys
+
+    assert_includes format_option_keys, :resize_to_fit
+    assert_includes format_option_keys, :crop
+  end
+
+  test 'optionsを使って引数を渡せること' do
+    options = { autorot: true, saver: { strip: true, quality: 60 } }
+    image_resizer = ImageResizer.new(@image, options:)
+
+    format_option_keys = image_resizer.resize.variation.transformations.keys
+    assert_includes format_option_keys, :autorot
+    assert_includes format_option_keys, :saver
   end
 end
