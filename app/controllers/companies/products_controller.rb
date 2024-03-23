4679682b75ce29101047bd1fb35e490d9d961025
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-03-23 17:51:44 +0900

    _company-products.html.slimを作成し企業所属ユーザーの提出物がなかった場合の表示を仮で実装

--- app/controllers/companies/products_controller.rb
+++ app/controllers/companies/products_controller.rb
@@ -3,5 +3,11 @@
 class Companies::ProductsController < ApplicationController
   def index
     @company = Company.find(params[:company_id])
+    @products = Kaminari.paginate_array([]).page(params[:page]).per(5)
+    @company.users.each do |user|
+      user.products.each do |product|
+        @products << product
+      end
+    end
   end
 end
