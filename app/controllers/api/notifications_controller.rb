b7933a6c10888c69f7d7c12301affb2c1139ecae
masyuko0222 <masyuko0222@gmail.com>
2024-03-24 00:57:19 +0900

    Fatになり始めていたので、一旦index内だけslimにした。命名まだ適当。

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -2,23 +2,24 @@
 
 class API::NotificationsController < API::BaseController
   def index
+    @notifications = fetch_notifications
+    paginate_notifications if params[:page]
+  end
+
+  private
+
+  def fetch_notifications
     target = params[:target].presence&.to_sym
     status = params[:status]
-    latest_notifications = current_user.notifications
-                                       .by_target(target)
-                                       .by_read_status(status)
-                                       .latest_of_each_link
 
-    # latest_notifications のクエリで指定している ORDER BY の順序を他と混ぜないようにするため、
-    # from を使ってサブクエリとした
-    @notifications = Notification.with_avatar
-                                 .from(latest_notifications, :notifications)
-                                 .order(created_at: :desc)
+    latest_notifications = current_user.notifications.by_target(target).by_read_status(status).latest_of_each_link
 
-    page = params[:page]
-    return @notifications unless page
+    # latest_notifications のクエリで指定している ORDER BY の順序を他と混ぜないようにするため、from を使ってサブクエリとした
+    Notification.with_avatar.from(latest_notifications, :notifications).order(created_at: :desc)
+  end
 
-    per = params[:per]
-    @notifications = per ? @notifications.page(page).per(per) : @notifications.page(page)
+  def paginate_notifications
+    per_page = params[:per]
+    @notifications = per_page ? @notifications.page(params[:page]).per(per_page) : @notifications.page(params[:page])
   end
 end
