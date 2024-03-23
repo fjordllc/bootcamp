e7f0b7ec0526961ce0afe5137975ba7350713666
ko <kyokucho1989@gmail.com>
2024-03-24 05:52:12 +0900

    ユーザー登録情報変更画面にスキップするプラクティスの項目追加
    userモデルに関連付けのメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -128,12 +128,6 @@ class User < ApplicationRecord
            source: :practice,
            dependent: :destroy
 
-  has_many :practices,
-           through: :skip_practices
-
-  has_many :skip_practices,
-           dependent: :destroy
-
   has_many :active_learnings,
            -> { where(status: 'started') },
            class_name: 'Learning',
@@ -150,6 +144,12 @@ class User < ApplicationRecord
            foreign_key: 'follower_id',
            inverse_of: 'follower',
            dependent: :destroy
+
+  has_many :skip_practices,
+           dependent: :destroy
+
+  has_many :practices,
+           through: :skip_practices
 
   has_many :followees,
            through: :active_relationships,
@@ -600,6 +600,10 @@ class User < ApplicationRecord
     Subscription.new.retrieve(subscription_id)
   end
 
+  def skip_practice_ids
+    skip_practices.pluck(:practice_id)
+  end
+
   def student?
     !admin? && !adviser? && !mentor? && !trainee?
   end
@@ -740,6 +744,14 @@ class User < ApplicationRecord
 
   def practices
     course.practices.order('courses_categories.position', 'categories_practices.position')
+  end
+
+  def category_ids
+    course.categories.pluck(:id)
+  end
+
+  def practice_ids_in_category(category)
+    category.practices
   end
 
   def update_mentor_memo(new_memo)
