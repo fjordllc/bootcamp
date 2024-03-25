9ff858d28213838ec2e62df6ece6d18b06871193
appsu-mi <natubate870@gmail.com>
2024-03-25 16:01:03 +0900

    ローカル変数target_allowlistを導入、sendメソッドを実行する前にチェックを入れるようにした

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -7,6 +7,9 @@ module CompaniesHelper
   end
 
   def link_to_company_users_path(company, target:)
+    target_allowlist = %w[advisers students_and_trainees admins mentor]
+    return unless target_allowlist.include?(target)
+
     link_to company.users.unretired.send(target).size, company_users_path(target: target.singularize, company_id: company.id),
             class: 'card-counts__item-value-link'
   end
