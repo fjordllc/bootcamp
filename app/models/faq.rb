8b9f2a594e868de37931d7858a9a086f5c091f0c
sochi419 <equipment2241@gmail.com>
2024-03-25 09:49:13 +0900

    faqs_categoryテーブルを作成, faqテーブルにあったcategoryのcalumnを削除

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -3,8 +3,8 @@
 class FAQ < ApplicationRecord
   validates :answer, presence: true, uniqueness: { scope: :question }
   validates :question, presence: true, uniqueness: true
-  validates :category, presence: true
 
   default_scope -> { order(:position) }
+  belongs_to :faqs_categories
   acts_as_list
 end
