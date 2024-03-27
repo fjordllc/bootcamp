6bb88ee5381f5249e314096a2f0aa7d3c68ab407
rira <101010hayakawa@gmail.com>
2024-03-27 12:09:14 +0900

    open_avatar_uriをプライベートメソッドに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -607,11 +607,6 @@ class User < ApplicationRecord
     avatar.attach(io: icon, filename: login_name, key: "icon/#{login_name}") unless icon.nil?
   end
 
-  def open_avatar_uri
-    url = avatar_url
-    url != '/images/users/avatars/default.png' ? URI.parse(url).open : nil
-  end
-
   def generation
     (created_at.year - 2013) * 4 + (created_at.month + 2) / 3
   end
@@ -806,5 +801,10 @@ class User < ApplicationRecord
 
   def category_having_unstarted_practice
     unstarted_practices&.first&.categories&.first
+  end
+
+  def open_avatar_uri
+    url = avatar_url
+    url != '/images/users/avatars/default.png' ? URI.parse(url).open : nil
   end
 end
