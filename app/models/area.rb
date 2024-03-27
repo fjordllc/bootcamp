226da7297adf9a129f9380556d8c8ce87e11fd2d
rira <101010hayakawa@gmail.com>
2024-03-27 16:51:47 +0900

    Revert "Merge branch 'main' of github.com:fjordllc/bootcamp"
    This reverts commit bde83c7bc329eda72ebe432ce20d5504daf17f69, reversing
    changes made to 033db0ed12ea2354cd2456213f8a3c0716b8ba8e.

--- app/models/area.rb
+++ app/models/area.rb
@@ -37,7 +37,6 @@ class Area
     end
 
     # regionとareaによって分類されたユーザー数をハッシュで取得して返す関数
-    # country_codeかsubdivision_codeのどちらかがnullのユーザーのデータは無視されます
     #
     # 返されるハッシュの例
     # {
@@ -67,7 +66,6 @@ class Area
     def country_subdivision_pairs
       User
         .select('country_code, subdivision_code')
-        .where.not(country_code: nil)
         .where.not(subdivision_code: nil)
         .pluck(:country_code, :subdivision_code)
     end
