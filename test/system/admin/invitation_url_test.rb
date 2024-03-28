f08f9e83f6d401f97f2b2e237bafb75a23668176
naokinaokiboo <naokinaokiboo@gmail.com>
2024-03-28 12:03:10 +0900

    1つのテストの粒度を小さくし、複数のテストに分割

--- test/system/admin/invitation_url_test.rb
+++ test/system/admin/invitation_url_test.rb
@@ -3,51 +3,72 @@
 require 'application_system_test_case'
 
 class Admin::InvitationUrlTest < ApplicationSystemTestCase
+  setup do
+    @new_user_url = "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/users/new"
+  end
+
   test 'non-admin cannot be visited invitation-url page' do
     visit_with_auth '/admin/invitation_url', 'kimura'
     assert_text '管理者としてログインしてください'
   end
 
-  test 'show invitation-url page' do
+  test 'admin can be visited invitation-url page' do
     visit_with_auth '/admin/invitation_url', 'komagata'
     assert_equal '管理ページ | FBC', title
-    company = Company.order(created_at: :desc).first
-    course = Course.order(:created_at).first
-    assert_text "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/users/new?company_id=#{company.id}&course_id=#{course.id}&role=adviser&token=token"
   end
 
-  test 'change selected dropdown item' do
+  test 'show invitation-url page' do
     visit_with_auth '/admin/invitation_url', 'komagata'
-    assert_equal '招待URL | FBC', title
+    company = Company.order(created_at: :desc).first
+    role = User::INVITATION_ROLES.first[1]
+    course = Course.order(:created_at).first
+    expected = "#{@new_user_url}?company_id=#{company.id}&course_id=#{course.id}&role=#{role}&token=token"
 
+    Timeout.timeout(Capybara.default_max_wait_time, StandardError) do
+      loop until find('.js-invitation-url-text').value == expected && find('.js-invitation-url')[:href] == expected
+    end
+  end
+
+  test 'change selected company' do
+    visit_with_auth '/admin/invitation_url', 'komagata'
     company = companies(:company3)
-    course = courses(:course3)
+    role = User::INVITATION_ROLES.first[1]
+    course = Course.order(:created_at).first
+    expected = "#{@new_user_url}?company_id=#{company.id}&course_id=#{course.id}&role=#{role}&token=token"
+
     find('.js-invitation-company').click
     select(company.name)
-    find('.js-invitation-role').click
-    select(I18n.t('invitation_role.mentor'))
-    find('.js-invitation-course')
-    select(course.title)
-    assert_text "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/users/new?company_id=#{company.id}&course_id=#{course.id}&role=mentor&token=token"
+    Timeout.timeout(Capybara.default_max_wait_time, StandardError) do
+      loop until find('.js-invitation-url-text').value == expected && find('.js-invitation-url')[:href] == expected
+    end
+  end
 
-    company = companies(:company2)
+  test 'change selected role' do
+    visit_with_auth '/admin/invitation_url', 'komagata'
+    company = Company.order(created_at: :desc).first
+    role_text = User::INVITATION_ROLES.second[0]
+    role = User::INVITATION_ROLES.second[1]
+    course = Course.order(:created_at).first
+    expected = "#{@new_user_url}?company_id=#{company.id}&course_id=#{course.id}&role=#{role}&token=token"
+
+    find('.js-invitation-role').click
+    select(role_text)
+    Timeout.timeout(Capybara.default_max_wait_time, StandardError) do
+      loop until find('.js-invitation-url-text').value == expected && find('.js-invitation-url')[:href] == expected
+    end
+  end
+
+  test 'change selected course' do
+    visit_with_auth '/admin/invitation_url', 'komagata'
+    company = Company.order(created_at: :desc).first
+    role = User::INVITATION_ROLES.first[1]
     course = courses(:course2)
-    find('.js-invitation-company').click
-    select(company.name)
-    find('.js-invitation-role').click
-    select(I18n.t('invitation_role.trainee'))
-    find('.js-invitation-course')
-    select(course.title)
-    assert_text "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/users/new?company_id=#{company.id}&course_id=#{course.id}&role=trainee&token=token"
+    expected = "#{@new_user_url}?company_id=#{company.id}&course_id=#{course.id}&role=#{role}&token=token"
 
-    company = companies(:company1)
-    course = courses(:course1)
-    find('.js-invitation-company').click
-    select(company.name)
-    find('.js-invitation-role').click
-    select(I18n.t('invitation_role.adviser'))
     find('.js-invitation-course')
     select(course.title)
-    assert_text "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}/users/new?company_id=#{company.id}&course_id=#{course.id}&role=adviser&token=token"
+    Timeout.timeout(Capybara.default_max_wait_time, StandardError) do
+      loop until find('.js-invitation-url-text').value == expected && find('.js-invitation-url')[:href] == expected
+    end
   end
 end
