30b2aa6de99f9f6c7c5c263a33f36f6492f055fd
yocchan-git <yocchan.business@gmail.com>
2024-03-29 11:10:43 +0900

    モデルテストを追加する

--- test/models/search_user_test.rb
+++ test/models/search_user_test.rb
@@ -3,25 +3,52 @@
 require 'test_helper'
 
 class SearchUserTest < ActiveSupport::TestCase
-  include SearchUser
-
-  test '検索文字が半角英文字2文字以下の時' do
-    assert_nil validate_search_word('ki')
+  test 'ユーザーを指定しない場合' do
+    kimura = users(:kimura)
+    komagata = users(:komagata)
+    search_user = SearchUser.new(search_word: 'kimu')
+    
+    searched_users = search_user.search
+    assert_includes searched_users, kimura
+    assert_not_includes searched_users, komagata
   end
 
-  test '検索文字が半角英文字3文字以上の時' do
-    assert_equal 'kim', validate_search_word('kim')
+  test '退会ユーザーを必要としない場合' do
+    yameo = users(:yameo)
+    search_user = SearchUser.new(search_word: 'yame', require_retire_user: false)
+
+    assert_not_includes search_user.search, yameo
   end
 
-  test '検索文字が全角英文字1文字以下の時' do
-    assert_nil validate_search_word('き')
+  test '退会ユーザーを必要とした場合' do
+    yameo = users(:yameo)
+    search_user = SearchUser.new(search_word: 'yame', require_retire_user: true)
+
+    assert_includes search_user.search, yameo
   end
 
-  test '検索文字が全角英文字2文字以下の時' do
-    assert_equal 'きむ', validate_search_word('きむ')
+  test 'targetがretiredの場合' do
+    yameo = users(:yameo)
+    kimura = users(:kimura)
+
+    search_user = SearchUser.new(search_word: 'yame', target: 'retired')
+    assert_includes search_user.search, yameo
+
+    search_user = SearchUser.new(search_word: 'キム', target: 'retired')
+    assert_not_includes search_user.search, kimura
   end
 
-  test '前後の空白は削除されること' do
-    assert_equal 'kim', validate_search_word('  kim ')
+  test 'ユーザーを指定した時、そのユーザーの中から検索される' do
+    kimura = users(:kimura)
+    mentor = users(:mentormentaro)
+
+    allowed_targets = %w[student_and_trainee followings mentor graduate adviser trainee year_end_party]
+    users = User.users_role('mentor', allowed_targets:)
+
+    search_user = SearchUser.new(users: , search_word: 'kimu')
+    assert_not_includes search_user.search, kimura
+
+    search_user = SearchUser.new(users: , search_word: 'メンター')
+    assert_includes search_user.search, mentor
   end
 end
