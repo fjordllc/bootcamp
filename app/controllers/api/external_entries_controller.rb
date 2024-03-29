6c1b0c11810eaaa4b1053c9f5ff5d44739532f50
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2024-03-29 10:14:23 +0900

    VueからHTMLに移行（メモあり）
    途中保存。テスト未確認、Vueコンポーネント削除前。
    
    途中保存（書き方修正）
    
    途中保存（vueファイル削除。他コメント）
    
    VueからHTMLに移行（メモあり）

--- app/controllers/api/external_entries_controller.rb
+++ app/controllers/api/external_entries_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class API::ExternalEntriesController < API::BaseController
-  def index
-    @external_entries = ExternalEntry.all.order(published_at: :desc).page(params[:page])
-  end
+  def index; end
 end
