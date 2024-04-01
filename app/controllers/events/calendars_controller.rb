1f1f428eeb79899edb1a8b74bf1ccc9bfb5b82af
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-01 13:42:15 +0900

    参加済みの特別イベントに「参加登録済」の表示が出るように修正

--- app/controllers/events/calendars_controller.rb
+++ app/controllers/events/calendars_controller.rb
@@ -4,9 +4,12 @@ class Events::CalendarsController < ApplicationController
   skip_before_action :require_active_user_login, raise: false, only: :index
 
   def index
+    user_id = params[:user_id]
+    user = User.find_by(id: user_id)
+
     respond_to do |format|
       format.ics do
-        calendar = EventsToIcalExporter.export_events(set_export)
+        calendar = EventsToIcalExporter.export_events(set_export(user))
         calendar.publish
         render plain: calendar.to_ical
       end
@@ -15,7 +18,15 @@ class Events::CalendarsController < ApplicationController
 
   private
 
-  def set_export
-    Event.where('start_at > ?', Date.current)
+  def set_export(user)
+    participated_list = user.participations.pluck(:event_id)
+    upcoming_event_list = Event.where('start_at > ?', Date.current).pluck(:id)
+
+    joined_events = Event.where(id: participated_list & upcoming_event_list)
+    upcoming_events = Event.where(id: upcoming_event_list).where.not(id: participated_list)
+    {
+      joined_events: joined_events,
+      upcoming_events: upcoming_events
+    }
   end
 end
