7f6c4f1859ca433276fb25757b0dd84484854fba
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-01 13:42:15 +0900

    参加済みの特別イベントに「参加登録済」の表示が出るように修正

--- test/models/events_to_ical_exporter_test.rb
+++ test/models/events_to_ical_exporter_test.rb
@@ -3,14 +3,25 @@
 require 'test_helper'
 
 class EventsToIcalExporterTest < ActiveSupport::TestCase
-  test 'check to export only feature events' do
-    travel_to Time.zone.local(2023, 5, 19, 10, 0, 0) do
-      events = Event.where('start_at > ?', Time.zone.today)
-      calendar = EventsToIcalExporter.export_events(events)
+  test 'check to export special events' do
+    travel_to Time.zone.local(2024, 3, 25, 10, 0, 0) do
+      user = users(:kimura)
+
+      participated_list = user.participations.pluck(:event_id)
+      upcoming_event_list = Event.where('start_at > ?', Time.zone.today).pluck(:id)
+
+      joined_events = Event.where(id: participated_list & upcoming_event_list)
+      upcoming_events = Event.where(id: upcoming_event_list).where.not(id: participated_list)
+      events_set= {
+        joined_events: joined_events,
+        upcoming_events: upcoming_events
+      }
+      calendar = EventsToIcalExporter.export_events(events_set)
 
       calendar.publish
-      assert_match(/明日開催のイベント/, calendar.to_ical)
-      assert_no_match(/昨日開催のイベント/, calendar.to_ical)
+      assert_match(/【参加登録済】未来のイベント\(参加済\)/, calendar.to_ical)
+      assert_match(/未来のイベント\(未参加\)/, calendar.to_ical)
+      assert_no_match(/過去のイベント/, calendar.to_ical)
     end
   end
 end
