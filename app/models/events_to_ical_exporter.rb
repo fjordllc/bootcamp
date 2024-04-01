eefaf0b7f7bd14ee151170cf039835476b277dbd
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-01 13:42:15 +0900

    参加済みの特別イベントに「参加登録済」の表示が出るように修正

--- app/models/events_to_ical_exporter.rb
+++ app/models/events_to_ical_exporter.rb
@@ -1,21 +1,31 @@
 # frozen_string_literal: true
 
 class EventsToIcalExporter
-  def self.export_events(events)
+  def self.export_events(events_set)
     cal = Icalendar::Calendar.new
-    events.each do |event|
-      tzid = 'Asia/Tokyo'
 
-      cal.event do |e|
-        e.dtstart     = Icalendar::Values::DateTime.new event.start_at, { 'tzid' => tzid }
-        e.dtend       = Icalendar::Values::DateTime.new event.end_at, { 'tzid' => tzid }
-        e.summary     = event.title
-        e.description = event.description
-        e.location    = event.location
-        e.uid         = "event#{event.id}"
-        e.sequence    = Time.now.to_i
-      end
+    events_set[:joined_events].each do |event|
+      add_event_to_calendar(cal, event, true)
+    end
+  
+    events_set[:upcoming_events].each do |event|
+      add_event_to_calendar(cal, event, false)
     end
     cal
+  end
+
+  private
+
+  def self.add_event_to_calendar(cal, event, joined)
+    tzid = 'Asia/Tokyo'
+    cal.event do |e|
+      e.dtstart     = Icalendar::Values::DateTime.new event.start_at, { 'tzid' => tzid }
+      e.dtend       = Icalendar::Values::DateTime.new event.end_at, { 'tzid' => tzid }
+      e.summary     = joined ? "【参加登録済】#{event.title}" : event.title
+      e.description = event.description
+      e.location    = event.location
+      e.uid         = "event#{event.id}"
+      e.sequence    = Time.now.to_i
+    end
   end
 end
