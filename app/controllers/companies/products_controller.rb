2884ac6d584ea8937468417a36ab8fcbd0635507
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-04-02 11:53:17 +0900

    提出物のWIP表示を修正・提出日、更新日、コメントされた年月日の表示を追加

--- app/controllers/companies/products_controller.rb
+++ app/controllers/companies/products_controller.rb
@@ -3,11 +3,13 @@
 class Companies::ProductsController < ApplicationController
   def index
     @company = Company.find(params[:company_id])
-    @products = Kaminari.paginate_array([]).page(params[:page]).per(5)
+    @products = []
     @company.users.each do |user|
       user.products.each do |product|
         @products << product
       end
     end
+    @products = Kaminari.paginate_array(@products).page(params[:page]).per(5)
+    @weekdays = { 0 => '日', 1 => '月', 2 => '火', 3 => '水', 4 => '木', 5 => '金', 6 => '土' }
   end
 end
