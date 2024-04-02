5fdf6ea14813d3b4f0abd8c54bb39e67e790978c
hirano-vm4 <hirano.vm4@gmail.com>
2024-04-02 23:19:17 +0900

    【他の通知と統一】ブログが削除された場合、通知のリンクから詳細へ飛ぶとブログ一覧にリダイレクトする挙動から、サイト内通知自体を削除するコードに変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,6 +51,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
+    Newspaper.publish(:destroy_article, { article: @article })
     redirect_to articles_url, notice: '記事を削除しました'
   end
 
