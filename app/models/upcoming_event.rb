46cedaba062fe643a985ec8f7a07728304edc1b5
masyuko0222 <masyuko0222@gmail.com>
2024-04-03 23:41:04 +0900

    UpcomingEventモデルの作成

---
+++ app/models/upcoming_event.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class UpcomingEvent
+  attr_reader :original_event, :title, :scheduled_date
+
+  delegate :participants, to: :original_event
+
+  def initialize(event)
+    @original_event = event
+    @title = event.title
+    @scheduled_date = event.recent_scheduled_date
+    @event_type = event.class
+  end
+
+  class << self
+    def wrap(event)
+      new(event)
+    end
+  end
+
+  def held_on_national_holiday?
+    return true if event_type == Event
+
+    original_event.hold_national_holiday
+  end
+
+  def for_job_hunting?
+    return false if event_type == RegularEvent
+
+    original_event.job_hunting?
+  end
+
+  private
+
+  attr_reader :event_type
+end
