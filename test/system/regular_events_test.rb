b13a5715b1a585dc31821bcc2421a263c50ded80
masyuko0222 <masyuko0222@gmail.com>
2024-04-05 21:33:22 +0900

    近日開催イベントと単なる定期イベントのスタイルが同じでテストが通らなくなったので、直した

--- test/system/regular_events_test.rb
+++ test/system/regular_events_test.rb
@@ -173,14 +173,14 @@ class RegularEventsTest < ApplicationSystemTestCase
 
   test 'show listing not finished regular events' do
     visit_with_auth regular_events_path(target: 'not_finished'), 'kimura'
-    assert_selector '.card-list-item', count: 14
+    assert_selector '.card-list.a-card .card-list-item', count: 14
   end
 
   test 'show listing all regular events' do
     visit_with_auth regular_events_path, 'kimura'
-    assert_selector '.card-list-item', count: 25
+    assert_selector '.card-list.a-card .card-list-item', count: 25
     visit regular_events_path(page: 2)
-    assert_selector '.card-list-item', count: 8
+    assert_selector '.card-list.a-card .card-list-item', count: 8
   end
 
   test 'create a regular event for all students and trainees' do
