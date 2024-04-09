8e881ce04fa58eef5fec64570e885266215cbc36
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-09 16:02:51 +0900

    movieテーブルにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_21_092012) do
+ActiveRecord::Schema.define(version: 2024_04_09_060904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -408,6 +408,9 @@ ActiveRecord::Schema.define(version: 2024_03_21_092012) do
     t.bigint "practice_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "wip", default: false, null: false
+    t.datetime "published_at"
+    t.integer "last_updated_user_id"
     t.index ["practice_id"], name: "index_movies_on_practice_id"
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
