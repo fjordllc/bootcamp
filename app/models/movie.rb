a33452994a8cd96a57c4edb297bd6f63c554156c
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-09 19:48:26 +0900

    モデルとコントローラに必要なメソッド等を追加、合わせてslimファイルの修正とデータ修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,10 +11,13 @@ class Movie < ApplicationRecord
 
   belongs_to :user
   belongs_to :practice, optional: true
+  belongs_to :last_updated_user, class_name: 'User', optional: true
   has_one_attached :thumbnail
   has_one_attached :movie_data
 
   validates :user, presence: true
   validates :title, presence: true
   validates :description, presence: true
+
+  scope :wip, -> { where(wip: true) }
 end
