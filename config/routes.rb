9a5225bc60dafe51aad40771a5f4b1acaa72d72a
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-10 19:41:47 +0900

    不要なルーティングの制限とメソッドのprivate化

--- config/routes.rb
+++ config/routes.rb
@@ -101,7 +101,7 @@ Rails.application.routes.draw do
   get "thanks", to: "static_pages#thanks"
   get "portfolios" => "works#index"
   resource :buzz, only: %i(show edit update), controller: "buzz"
-  resources :movies
+  resources :movies, only: [:index, :show, :new, :create]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   mount GoodJob::Engine => 'good_job'
 end
