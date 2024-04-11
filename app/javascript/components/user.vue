3d67c60b77b4bf893f40abb5fd5d9aa1231b200c
machida <machida@fjord.jp>
2024-04-11 20:12:00 +0900

    ブレイクポイント追加

--- app/javascript/components/user.vue
+++ app/javascript/components/user.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.col-xxl-3.col-xl-4.col-lg-4.col-md-6.col-xs-12
+.col-xxxl-2.col-xxl-3.col-xl-4.col-lg-4.col-md-6.col-xs-12
   .users-item
     .users-item__inner.a-card
       .users-item__inactive-message-container.is-only-mentor(
