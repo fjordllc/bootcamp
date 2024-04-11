ccaa4e9355aa608f1a6cdb9216673cf064ea115e
masyuko0222 <masyuko0222@gmail.com>
2024-04-11 22:27:45 +0900

    アドバイザーのダッシュボードに、退会申請フォームのための導線を作った

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -46,6 +46,7 @@ class HomeController < ApplicationController
     collegue_trainees_reports = Report.with_avatar.where(wip: false).where(user: current_user.collegue_trainees&.with_attached_avatar)
     @collegue_trainees_recent_reports = collegue_trainees_reports.order(reported_on: :desc).limit(10)
     @recent_reports = Report.with_avatar.where(wip: false).order(reported_on: :desc, created_at: :desc).limit(10)
+    @collegue_members = current_user.collegues&.where&.not(id: current_user.id)
   end
 
   def display_events_on_dashboard
