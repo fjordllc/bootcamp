ec57116d156535b8913f5ea5126257d51ba2fdff
masyuko0222 <masyuko0222@gmail.com>
2024-04-11 21:27:14 +0900

    データ構造的に、コンストラクタにupcoming_eventsを渡すようにした。

--- app/models/upcoming_events_group.rb
+++ app/models/upcoming_events_group.rb
@@ -1,23 +1,30 @@
 # frozen_string_literal: true
 
 class UpcomingEventsGroup
+  EVENT_MODELS = [Event, RegularEvent].freeze
+
   attr_reader :date, :events
 
-  def initialize(date = :today)
+  def initialize(date, upcoming_events)
     @date = date
-    @original_events = fetch_date_events(date, Event, RegularEvent)
-    @events = original_events.map { |event| UpcomingEvent.wrap(event) }
+    @events = upcoming_events
   end
 
-  private
+  class << self
+    def create(date)
+      original_events = fetch_dated_original_events(date, EVENT_MODELS)
+      upcoming_events = original_events.map { |e| UpcomingEvent.wrap(e) }
+      new(date, upcoming_events)
+    end
 
-  attr_reader :original_events
+    private
 
-  def fetch_date_events(date, *event_models)
-    fetch_date_events_method = "#{date}_events"
+    def fetch_dated_original_events(date, event_models)
+      fetch_date_events_method = "#{date}_events"
 
-    event_models.map do |event_model|
-      event_model.public_send(fetch_date_events_method)
-    end.flatten
+      event_models.map do |event_model|
+        event_model.public_send(fetch_date_events_method)
+      end.flatten
+    end
   end
 end
