432f1180894cfc57c4d294b34ee4778a8e82b951
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-04-12 09:56:48 +0900

    アイコンの位置を下げる処理を変更した
    sassを変更すると、すべてのo-empty-message__iconに適用されるため変更した

--- app/javascript/components/Reports.jsx
+++ app/javascript/components/Reports.jsx
@@ -126,21 +126,29 @@ export default function Reports({
 
 const NoReports = ({ unchecked }) => {
   return (
-    <div className="o-empty-message">
-      <div className="o-empty-message__icon">
-        {unchecked ? (
-          <>
-            <i className="fa-regular fa-smile" />
-            <p className="o-empty-message__text">
-              未チェックの日報はありません
-            </p>
-          </>
-        ) : (
-          <>
-            <i className="fa-regular fa-sad-tear" />
-            <p className="o-empty-message__text">日報はまだありません。</p>
-          </>
-        )}
+    <div className="page-main">
+      <div className="page-body">
+        <div className="container is-md">
+          <div className="o-empty-message">
+            <div className="o-empty-message__icon">
+              {unchecked ? (
+                <>
+                  <i className="fa-regular fa-smile" />
+                  <p className="o-empty-message__text">
+                    未チェックの日報はありません
+                  </p>
+                </>
+              ) : (
+                <>
+                  <i className="fa-regular fa-sad-tear" />
+                  <p className="o-empty-message__text">
+                    日報はまだありません。
+                  </p>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
