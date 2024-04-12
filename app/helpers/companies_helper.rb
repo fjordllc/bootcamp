4017288846201f44f644d234b31e8fbf7dfa5f7e
appsu-mi <natubate870@gmail.com>
2024-04-12 13:10:58 +0900

    if文とlink_toが分かれていたが、link_to_ifでよりスマートに

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -8,9 +8,9 @@ module CompaniesHelper
 
   def link_to_company_users_path(company, target:)
     target_allowlist = %w[advisers students_and_trainees admins mentor]
-    return unless target_allowlist.include?(target)
-
-    link_to company.users.unretired.send(target).size, company_users_path(target: target.singularize, company_id: company.id),
-            class: 'card-counts__item-value-link'
+    link_to_if(target_allowlist.include?(target),
+               company.users.unretired.send(target).size,
+               company_users_path(target: target.singularize, company_id: company.id),
+               class: 'card-counts__item-value-link')
   end
 end
