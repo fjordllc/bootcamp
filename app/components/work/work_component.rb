acf75adc89924ede926ab54a30730bd068f103ef
reckyy <srreki98@gmail.com>
2024-04-14 09:05:27 +0900

    ロジックをメソッド化

--- app/components/work/work_component.rb
+++ app/components/work/work_component.rb
@@ -5,6 +5,18 @@ class Work::WorkComponent < ViewComponent::Base
     @work = work
   end
 
+  def work_thumbnail
+    if work.thumbnail.attached?
+      image_tag work.thumbnail_url, class: 'thumbnail-card__image'
+    else
+      image_tag('work-blank.svg', class: 'thumbnail-card__image')
+    end
+  end
+
+  def creator_avatar
+    image_tag work.user.avatar_url, title: work.user.icon_title, class: 'a-user-icons__item-icon a-user-icon'
+  end
+
   private
 
   attr_reader :work
