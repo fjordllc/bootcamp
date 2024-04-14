be5f170a305087e2500ded66d7c75d2876da59f0
reckyy <srreki98@gmail.com>
2024-04-14 09:05:27 +0900

    ロジックをメソッド化

--- test/components/work_component_test.rb
+++ test/components/work_component_test.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class WorkComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(WorkComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  setup do
+    @user = users(:kimura).extend(UserDecorator)
+    @work = works(:work1)
+    @work.user = @user
+    render_inline(Work::WorkComponent.new(work: @work))
+  end
+
+  def test_work_thumbnail
+    assert_selector 'img.thumbnail-card__image'
+  end
+
+  def test_creator_avatar
+    assert_selector 'img.a-user-icons__item-icon.a-user-icon[title="kimura (Kimura Tadasi)"][src="/images/users/avatars/default.png"]'
   end
 end
