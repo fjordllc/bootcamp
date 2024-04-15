0a5c2aa08ca5004b802e5a56be5db57484e70197
naokinaokiboo <naokinaokiboo@gmail.com>
2024-04-15 17:22:30 +0900

    products.vueの不要コードの削除・整理
    unconfirmed_links_open_button.vueを参照していたが、
    既にReactに置き換えられており使用されていないため。

--- app/javascript/products.js
+++ app/javascript/products.js
@@ -7,20 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const products = document.querySelector(selector)
   if (products) {
     const title = products.getAttribute('data-title')
-    const selectedTab = products.getAttribute('data-selected-tab')
     const isMentor = products.getAttribute('data-mentor-login')
     const currentUserId = Number(products.getAttribute('data-current-user-id'))
-    const checkerId = products.getAttribute('data-checker-id')
     new Vue({
       store,
       render: (h) =>
         h(Products, {
           props: {
             title: title,
-            selectedTab: selectedTab,
             isMentor: isMentor === 'true',
-            currentUserId: currentUserId,
-            checkerId: checkerId
+            currentUserId: currentUserId
           }
         })
     }).$mount(selector)
