a6ccf47fe2630ac842d600b812838fb93892d1b1
masyuko0222 <masyuko0222@gmail.com>
2024-04-15 22:39:53 +0900

    申請者の名前、メアド、退会をさせるアカウントの名前で検証を行い、検証が通ったらUserと紐づける

--- app/models/request_retirement.rb
+++ app/models/request_retirement.rb
@@ -10,4 +10,39 @@ class RequestRetirement < ApplicationRecord
   validates :target_user_name, presence: true
   validates :reason_of_request_retirement, presence: true
   validates :keep_data, inclusion: [true, false]
+
+  validate :user_existence
+
+  private
+
+  def user_existence
+    message = 'は登録されていません。'
+
+    if requester_registered? && target_user
+      self.requester = user_by_requester_email
+      self.target_user = target_user
+    elsif !user_by_requester_email
+      errors.add(:requester_email, message)
+    elsif !user_by_requester_name
+      errors.add(:requester_name, message)
+    elsif !target_user
+      errors.add(:target_user_name, message)
+    end
+  end
+
+  def target_user
+    User.find_by(login_name: target_user_name)
+  end
+
+  def requester_registered?
+    user_by_requester_email && user_by_requester_name
+  end
+
+  def user_by_requester_email
+    User.find_by(email: requester_email)
+  end
+
+  def user_by_requester_name
+    User.find_by(login_name: requester_name)
+  end
 end
