ad238c2ec9ede4ae6c6516166631415037602912
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-17 16:54:07 +0900

    選択した動画のプレビュー表示がされるように

--- app/javascript/movie-fileinput.js
+++ app/javascript/movie-fileinput.js
@@ -1,34 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const initializeFileInput = (target) => {
-    const inputs = target.querySelectorAll('.js-movie-file-input input');
-    if (!inputs) return null;
+  const FileInput = (target) => {
+    const input = target.querySelector('.js-movie-file-input input');
+    if (!input) return null;
 
-    inputs.forEach((input) => {
-      input.addEventListener('change', (e) => {
-        const file = e.target.files[0];
+    input.addEventListener('change', (e) => {
+      const file = e.target.files[0];
 
-        if (file) {
-          const fileReader = new FileReader();
-          fileReader.addEventListener('load', (event) => {
-            const dataUri = event.target.result;
-            const preview = input.parentElement.querySelector('.js-movie-file-input__preview'); // プレビュー要素を選択
-            const p = preview.querySelector('p');
-            let movieData = preview.querySelector('video');
+      if (file) {
+        const fileReader = new FileReader();
+        fileReader.addEventListener('load', (event) => {
+          const dataUri = event.target.result;
+          const preview = input.parentElement.parentElement.querySelector('.js-movie-file-input__preview');
+          const p = preview.querySelector('p');
+          let movieData = preview.querySelector('video');
 
-            if (!movieData) {
-              movieData = document.createElement('video');
-              movieData.setAttribute('controls', '');
-              preview.appendChild(movieData);
-            }
+          if (!movieData) {
+            movieData = document.createElement('video');
+            movieData.setAttribute('controls', '');
+            preview.appendChild(movieData);
+          }
 
-            movieData.src = dataUri;
-            p.innerHTML = '動画を変更';
-          });
-          fileReader.readAsDataURL(file);
-        }
-      });
+          movieData.src = dataUri;
+          p.innerHTML = '動画を変更';
+        });
+        fileReader.readAsDataURL(file);
+      }
     });
   };
 
-  initializeFileInput(document);
+  FileInput(document);
 });
