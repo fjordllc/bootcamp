34f93cf7131b41c1c79067d9ad98f440fd5d83f3
masyuko0222 <masyuko0222@gmail.com>
2024-04-17 22:30:15 +0900

    コントローラのnewとcreateアクションを作成

---
+++ app/controllers/request_retirement_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class RequestRetirementController < ApplicationController
+  skip_before_action :require_active_user_login, raise: false, only: %i[new show create]
+
+  def new
+    if logged_in? && current_user.belongs_company_and_adviser?
+      @request_retirement = RequestRetirement.new(requester_profile)
+      @target_users = current_user.collegues_other_than_self
+    else
+      @request_retirement = RequestRetirement.new
+    end
+  end
+
+  def show; end
+
+  def create
+    @request_retirement = RequestRetirement.new(request_retirement_params)
+    if @request_retirement.save
+      # resourceルーティングだと:idを渡せないので、sessionにidを保存しておく
+      temporarily_store_session(:request_retirement_id, @request_retirement.id)
+      redirect_to request_retirement_url
+    else
+      @target_users = current_user.collegues_other_than_self if logged_in? && current_user.belongs_company_and_adviser?
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def request_retirement_params
+    params.require(:request_retirement)
+          .permit(:requester_email,
+                  :requester_name,
+                  :requester_company_name,
+                  :target_user_name,
+                  :reason_of_request_retirement,
+                  :keep_data)
+  end
+
+  def requester_profile
+    {
+      requester_email: current_user.email,
+      requester_name: current_user.login_name,
+      requester_company_name: current_user.company.name
+    }
+  end
+end
