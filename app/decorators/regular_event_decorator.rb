d14781f1eaefaa0d6872834a5d6a9eb7a7984747
hirano-vm4 <hirano.vm4@gmail.com>
2024-04-17 23:33:12 +0900

    イベント詳細の休日を日付・理由を分けて表示する仕様に変更

--- app/decorators/regular_event_decorator.rb
+++ app/decorators/regular_event_decorator.rb
@@ -29,23 +29,25 @@ module RegularEventDecorator
 
   def holiday_schedule_for_year
     today = Time.zone.today
-
     national_holidays = HolidayJp.between(today, today + 1.year)
+
     holidays_on_event_dates = national_holidays.select { |holiday| match_event_rules?(holiday.date) }
-    formatted_event_holidays = holidays_on_event_dates.map { |date| format_national_holiday(date) }
+    event_holidays = holidays_on_event_dates.map { |date| format_holiday(date) }
+    custom_holidays = fetch_custom_holidays(today).map { |holiday| format_holiday(holiday) }
 
-    formatted_custom_holidays = fetch_custom_holidays(today).map { |holiday| format_custom_holiday(holiday) }
-
-    hold_national_holiday? ? formatted_custom_holidays : (formatted_custom_holidays + formatted_event_holidays).sort
+    holidays = hold_national_holiday? ? custom_holidays : (custom_holidays + event_holidays)
+    holidays.sort_by { |holiday| holiday[:date] }
   end
 
   private
 
-  def format_national_holiday(holiday)
-    I18n.l(holiday.date, format: :with_weekday_and_holiday)
-  end
+  def format_holiday(holiday)
+    date = holiday.respond_to?(:date) ? I18n.l(holiday.date, format: :with_weekday_and_holiday) : I18n.l(holiday.holiday_date, format: :long)
+    description = holiday.respond_to?(:name) ? holiday.name : holiday.description
 
-  def format_custom_holiday(holiday)
-    I18n.l(holiday.holiday_date, format: :long) + " #{holiday.description}"
+    {
+      date:,
+      description:
+    }
   end
 end
