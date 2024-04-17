4d6b6fc3b026dd15e9147ec0f35fae8d86c2929d
machida <machida@fjord.jp>
2024-04-17 15:19:38 +0900

    :cop:

--- app/javascript/components/User.jsx
+++ app/javascript/components/User.jsx
@@ -71,13 +71,13 @@ export default function User({ user, currentUser }) {
                     <div className="card-list-item-title">
                       <div className="card-list-item-title__end">
                         {user.company && user.company.logo_url && (
-                            <a href={user.company.url}>
-                              <img
-                                className="user-item__company-logo"
-                                src={user.company.logo_url}
-                              />
-                            </a>
-                          )}
+                          <a href={user.company.url}>
+                            <img
+                              className="user-item__company-logo"
+                              src={user.company.logo_url}
+                            />
+                          </a>
+                        )}
                         <a
                           className="card-list-item-title__title is-lg a-text-link"
                           href={user.url}>
