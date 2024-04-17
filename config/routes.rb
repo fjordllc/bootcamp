9f749331ac66e9731e115cab17f3a39b510ae1d5
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-04-17 16:52:50 +0900

    ルーティング設定を変更

--- config/routes.rb
+++ config/routes.rb
@@ -122,7 +122,7 @@ Rails.application.routes.draw do
   get '/', to: 'home#index', as: :niconico_calendar_date, constraints: niconico_calendar_constraints
   get '/users/:id', to: 'users#show', as: :niconico_calendar_date_in_profile, constraints: niconico_calendar_constraints
   resource :buzz, only: %i(show edit update), controller: "buzz"
-  resources :movies, only: %i(index show new create)
+  resources :movies
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   mount GoodJob::Engine => 'good_job'
 end
