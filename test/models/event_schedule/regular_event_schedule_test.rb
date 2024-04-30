d58cbf6b1085235ead6e91e474a246df14ab7876
masyuko0222 <masyuko0222@gmail.com>
2024-04-30 22:42:43 +0900

    setupを使ってDRY化

--- test/models/event_schedule/regular_event_schedule_test.rb
+++ test/models/event_schedule/regular_event_schedule_test.rb
@@ -7,29 +7,27 @@ module EventSchedule
     setup do
       @every_week_event = regular_events(:regular_event1)
       @specified_week_event = regular_events(:regular_event2)
+      @every_week_schedule = EventSchedule::RegularEventSchedule.new(@every_week_event)
+      @specified_week_schedule = EventSchedule::RegularEventSchedule.new(@specified_week_event)
     end
 
     test 'every week event #tentative_next_event_date' do
       travel_to Time.zone.local(2023, 12, 31, 0, 0, 0) do
-        schedule = EventSchedule::RegularEventSchedule.new(@every_week_event)
-        assert_equal Time.zone.local(2023, 12, 31, 15, 0, 0), schedule.tentative_next_event_date
+        assert_equal Time.zone.local(2023, 12, 31, 15, 0, 0), @every_week_schedule.tentative_next_event_date
       end
 
       travel_to Time.zone.local(2023, 12, 31, 16, 0, 0) do
-        schedule = EventSchedule::RegularEventSchedule.new(@every_week_event)
-        assert_equal Time.zone.local(2024, 1, 7, 15, 0, 0), schedule.tentative_next_event_date
+        assert_equal Time.zone.local(2024, 1, 7, 15, 0, 0), @every_week_schedule.tentative_next_event_date
       end
     end
 
     test 'specified week event #tentative_next_event_date' do
       travel_to Time.zone.local(2023, 12, 31, 0, 0, 0) do
-        schedule = EventSchedule::RegularEventSchedule.new(@specified_week_event)
-        assert_equal Time.zone.local(2024, 1, 1, 16, 0, 0), schedule.tentative_next_event_date
+        assert_equal Time.zone.local(2024, 1, 1, 16, 0, 0), @specified_week_schedule.tentative_next_event_date
       end
 
       travel_to Time.zone.local(2024, 1, 2, 0, 0, 0) do
-        schedule = EventSchedule::RegularEventSchedule.new(@specified_week_event)
-        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), schedule.tentative_next_event_date
+        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), @specified_week_schedule.tentative_next_event_date
       end
     end
 
@@ -48,8 +46,7 @@ module EventSchedule
 
     test 'specified week event #held_next_event_date' do
       travel_to Time.zone.local(2023, 12, 31, 0, 0, 0) do
-        schedule = EventSchedule::RegularEventSchedule.new(@specified_week_event)
-        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), schedule.held_next_event_date
+        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), @specified_week_schedule.held_next_event_date
       end
     end
   end
