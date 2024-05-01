d38650b9b5c8699c9973a3f906bec7525b694643
masyuko0222 <masyuko0222@gmail.com>
2024-05-01 17:08:11 +0900

    指定された期間のイベント予定日をすべて取得するように修正した
    ルールが複数あるイベントだとバグが生じるため、それを直すため

--- test/models/event_schedule/regular_event_schedule_test.rb
+++ test/models/event_schedule/regular_event_schedule_test.rb
@@ -11,42 +11,68 @@ module EventSchedule
       @specified_week_schedule = EventSchedule::RegularEventSchedule.new(@specified_week_event)
     end
 
-    test 'every week event #tentative_next_event_date' do
+    test 'every week event #gather_scheduled_dates' do
       travel_to Time.zone.local(2023, 12, 31, 0, 0, 0) do
-        assert_equal Time.zone.local(2023, 12, 31, 15, 0, 0), @every_week_schedule.tentative_next_event_date
+        start = Time.current
+        last = start.next_month
+        dates = @every_week_schedule.gather_scheduled_dates(from: start, to: last)
+
+        assert_equal Time.zone.local(2023, 12, 31, 15, 0, 0), dates.first
+        assert_equal 5, dates.count
       end
 
       travel_to Time.zone.local(2023, 12, 31, 16, 0, 0) do
-        assert_equal Time.zone.local(2024, 1, 7, 15, 0, 0), @every_week_schedule.tentative_next_event_date
+        start = Time.current
+        last = start.next_month
+        dates = @every_week_schedule.gather_scheduled_dates(from: start, to: last)
+
+        assert_equal Time.zone.local(2024, 1, 7, 15, 0, 0), dates.first
+        assert_equal 4, dates.count
       end
     end
 
-    test 'specified week event #tentative_next_event_date' do
+    test 'specified week event #gather_scheduled_dates' do
       travel_to Time.zone.local(2023, 12, 31, 0, 0, 0) do
-        assert_equal Time.zone.local(2024, 1, 1, 16, 0, 0), @specified_week_schedule.tentative_next_event_date
+        start = Time.current
+        last = start.next_month
+        dates = @specified_week_schedule.gather_scheduled_dates(from: start, to: last)
+
+        assert_equal Time.zone.local(2024, 1, 1, 16, 0, 0), dates.first
+        assert_equal 1, dates.count
       end
 
       travel_to Time.zone.local(2024, 1, 2, 0, 0, 0) do
-        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), @specified_week_schedule.tentative_next_event_date
+        start = Time.current
+        last = start.next_month.end_of_month
+        dates = @specified_week_schedule.gather_scheduled_dates(from: start, to: last)
+
+        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), dates.first
+        assert_equal 1, dates.count
       end
     end
 
     test 'every week event #held_next_event_date' do
       travel_to Time.zone.local(2024, 2, 10, 0, 0, 0) do
+        start = Time.current
+        last = start.next_year.end_of_month
+
         @every_week_event.hold_national_holiday = true
         schedule = EventSchedule::RegularEventSchedule.new(@every_week_event)
-        assert_equal Time.zone.local(2024, 2, 11, 15, 0, 0), schedule.held_next_event_date
+        assert_equal Time.zone.local(2024, 2, 11, 15, 0, 0), schedule.held_next_event_date(from: start, to: last)
 
         @every_week_event.hold_national_holiday = false
         schedule = EventSchedule::RegularEventSchedule.new(@every_week_event)
         # 2024/02/11が祝日のため見送られる
-        assert_equal Time.zone.local(2024, 2, 18, 15, 0, 0), schedule.held_next_event_date
+        assert_equal Time.zone.local(2024, 2, 18, 15, 0, 0), schedule.held_next_event_date(from: start, to: last)
       end
     end
 
     test 'specified week event #held_next_event_date' do
       travel_to Time.zone.local(2023, 12, 31, 0, 0, 0) do
-        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), @specified_week_schedule.held_next_event_date
+        start = Time.current
+        last = start.next_year.end_of_month
+
+        assert_equal Time.zone.local(2024, 2, 5, 16, 0, 0), @specified_week_schedule.held_next_event_date(from: start, to: last)
       end
     end
   end
