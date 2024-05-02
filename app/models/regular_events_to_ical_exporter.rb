6bcee9c1d3b6458df2ddc9863bc4db219415e927
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-05-02 19:41:50 +0900

    fetch_eventメソッドをcalendars_controllerから移動

--- app/models/regular_events_to_ical_exporter.rb
+++ app/models/regular_events_to_ical_exporter.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class RegularEventsToIcalExporter
-  def self.export_events(holding_events)
+  def self.export_events(user)
+    holding_events = fetch_events(user)
     cal = Icalendar::Calendar.new
 
     holding_events.each do |holding_event|
@@ -23,5 +24,29 @@ class RegularEventsToIcalExporter
       end
     end
     cal
+  end
+
+  def self.fetch_events(user)
+    participated_list = user.regular_event_participations.pluck(:regular_event_id)
+    holding_events = []
+    RegularEvent.where(id: participated_list).where(finished: false).find_each do |event|
+      event.regular_event_repeat_rules.each do |repeat_rule|
+        current_date = Time.zone.today
+
+        while current_date <= Time.zone.today + 1.year
+          if repeat_rule.frequency.zero?
+            day_of_the_week_symbol = DateAndTime::Calculations::DAYS_INTO_WEEK.key(repeat_rule.day_of_the_week)
+            event_date = current_date.next_occurring(day_of_the_week_symbol).to_date
+            event_date = event_date.next_occurring(day_of_the_week_symbol) while !event.hold_national_holiday && HolidayJp.holiday?(event_date)
+            current_date = event_date + 1
+          else
+            event_date = event.possible_next_event_date(current_date, repeat_rule)
+            current_date = current_date.next_month.beginning_of_month
+          end
+          holding_events << { event_id: event.id, event_date: }
+        end
+      end
+    end
+    holding_events
   end
 end
