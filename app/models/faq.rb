7e6f4da7f0f71932043d95212cfdc9100e408d5c
sochi419 <equipment2241@gmail.com>
2024-05-08 02:34:45 +0900

    enumを使った処理に変更 faqs_categoriesテーブルのデータを使った処理群とテーブルを削除

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -3,8 +3,18 @@
 class FAQ < ApplicationRecord
   validates :answer, presence: true, uniqueness: { scope: :question }
   validates :question, presence: true, uniqueness: true
+  validates :category, presence: true
 
   default_scope -> { order(:position) }
-  belongs_to :faqs_category
   acts_as_list
+
+  enum category: {
+    study_content: 0,
+    study_environment: 1,
+    fee: 2,
+    find_job: 3,
+    join: 4,
+    withdrawal_hibernation_graduation: 5,
+    corporate_use: 6
+  }, _prefix: true
 end
