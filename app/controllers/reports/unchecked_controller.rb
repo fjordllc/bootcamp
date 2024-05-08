27919ad6421736b830a3907050bfd365899c8718
naokinaokiboo <naokinaokiboo@gmail.com>
2024-05-08 10:39:24 +0900

    「未チェックの日報」を非React化

--- app/controllers/reports/unchecked_controller.rb
+++ app/controllers/reports/unchecked_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Reports::UncheckedController < ApplicationController
+  PAGER_NUMBER = 25
+
   before_action :require_staff_login
-  def index; end
+  def index
+    @reports = Report.unchecked.not_wip.list.page(params[:page]).per(PAGER_NUMBER)
+  end
 end
