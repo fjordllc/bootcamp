4230897f43a89e719759b431066a7eebbdead520
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-05-14 18:20:51 +0900

    unassigned_counts_test.rbに合わせるように変更した

--- test/integration/api/products/passed_test.rb
+++ test/integration/api/products/passed_test.rb
@@ -6,16 +6,21 @@ class API::Products::PassedTest < ActionDispatch::IntegrationTest
   fixtures :products
 
   test 'GET /api/products/passed.json' do
+    products(:product15).update_column(:checker_id, nil)
+
     get api_products_passed_path(format: :text)
     assert_response :unauthorized
 
     token = create_token('komagata', 'testtest')
     get api_products_passed_path(format: :text),
         headers: { 'Authorization' => "Bearer #{token}" }
-
     assert_response :ok
-    assert_match '4日経過：3件', response.body
-    assert_match '5日経過：2件', response.body
-    assert_match '6日以上経過：10件', response.body
+
+    expected = <<~BODY
+      - 4日経過：2件
+      - 5日経過：1件
+      - 6日以上経過：8件
+    BODY
+    assert_includes response.body, expected
   end
 end
