cc5b553b0ffe5fc443dbd5542a5e579b13cb4454
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-05-14 14:58:05 +0900

    inactivateButtons関数を定義・ドロップダウンリストの要素を取得する記述を修正
    ・フォローボタンのドロップダウンリストで選択されなかったものからチェックマークを外す処理にコードの重複が多かったため関数に切り出した
    
    ・これまではドロップダウンリストの各要素を取得する際、a-dropdown__itemsの直下にあるli要素を取得していたが、実際に操作するのは更に一つ下の階層の要素であるため、最初からそれらの要素を取得するように修正した

--- app/javascript/user-follow.js
+++ app/javascript/user-follow.js
@@ -1,12 +1,21 @@
 import CSRF from 'csrf'
 
+function inactivateButtons(notSelectedButtons) {
+  if (notSelectedButtons[0].classList.contains('is-active')) {
+    notSelectedButtons[0].classList.remove('is-active')
+  } else if (notSelectedButtons[1].classList.contains('is-active')) {
+    notSelectedButtons[1].classList.remove('is-active')
+  }
+}
+
 function changeButtonAppearance(userId) {
   const details = event.target.closest('#followingDetailsRef')
   const followingSummary = details.children[0]
   const dropdownItems = details.querySelector('.a-dropdown__items')
-  const firstDropdownItem = dropdownItems.children[0]
-  const secondDropdownItem = dropdownItems.children[1]
-  const thirdDropdownItem = dropdownItems.children[2]
+  const firstDropdownItemButton = dropdownItems.children[0].children[0]
+  const secondDropdownItemButton = dropdownItems.children[1].children[0]
+  const thirdDropdownItemButton = dropdownItems.children[2].children[0]
+
   details.removeAttribute('open')
 
   if (
@@ -24,24 +33,24 @@ function changeButtonAppearance(userId) {
     button.appendChild(checkMark)
     button.appendChild(text)
     followingSummary.appendChild(button)
-    firstDropdownItem.children[0].classList.add('is-active')
-    firstDropdownItem.children[0].setAttribute(
+    firstDropdownItemButton.classList.add('is-active')
+    firstDropdownItemButton.setAttribute(
       'onclick',
       'userFollow.closeDropDown()'
     )
-    secondDropdownItem.children[0].setAttribute(
+    secondDropdownItemButton.setAttribute(
       'onclick',
       `userFollow.followOrChangeFollow(${userId}, ${true}, ${false} )`
     )
-    thirdDropdownItem.children[0].setAttribute(
+    thirdDropdownItemButton.setAttribute(
       'onclick',
       `userFollow.unfollow(${userId},${true}, ${true})`
     )
-    if (secondDropdownItem.children[0].classList.contains('is-active')) {
-      secondDropdownItem.children[0].classList.remove('is-active')
-    } else if (thirdDropdownItem.children[0].classList.contains('is-active')) {
-      thirdDropdownItem.children[0].classList.remove('is-active')
-    }
+    const notSelectedButtons = [
+      secondDropdownItemButton,
+      thirdDropdownItemButton
+    ]
+    inactivateButtons(notSelectedButtons)
   } else if (
     event.currentTarget.children[0].children[0].textContent === 'コメントなし'
   ) {
@@ -57,48 +66,48 @@ function changeButtonAppearance(userId) {
     button.appendChild(checkMark)
     button.appendChild(text)
     followingSummary.appendChild(button)
-    secondDropdownItem.children[0].classList.add('is-active')
-    secondDropdownItem.children[0].setAttribute(
+    secondDropdownItemButton.classList.add('is-active')
+    secondDropdownItemButton.setAttribute(
       'onclick',
       'userFollow.closeDropDown()'
     )
-    firstDropdownItem.children[0].setAttribute(
+    firstDropdownItemButton.setAttribute(
       'onclick',
       `userFollow.followOrChangeFollow(${userId}, ${true}, ${true} )`
     )
-    thirdDropdownItem.children[0].setAttribute(
+    thirdDropdownItemButton.setAttribute(
       'onclick',
       `userFollow.unfollow(${userId}, ${true}, ${false})`
     )
-    if (firstDropdownItem.children[0].classList.contains('is-active')) {
-      firstDropdownItem.children[0].classList.remove('is-active')
-    } else if (thirdDropdownItem.children[0].classList.contains('is-active')) {
-      thirdDropdownItem.children[0].classList.remove('is-active')
-    }
+    const notSelectedButtons = [
+      firstDropdownItemButton,
+      thirdDropdownItemButton
+    ]
+    inactivateButtons(notSelectedButtons)
   } else if (
     event.currentTarget.children[0].children[0].textContent === 'フォローしない'
   ) {
     details.children[0].children[0].textContent = 'フォローする'
     details.children[0].children[0].className =
       'a-button is-secondary is-sm is-block'
-    thirdDropdownItem.children[0].classList.add('is-active')
-    thirdDropdownItem.children[0].setAttribute(
+    thirdDropdownItemButton.classList.add('is-active')
+    thirdDropdownItemButton.setAttribute(
       'onclick',
       'userFollow.closeDropDown()'
     )
-    firstDropdownItem.children[0].setAttribute(
+    firstDropdownItemButton.setAttribute(
       'onclick',
       `userFollow.followOrChangeFollow(${userId}, ${false}, ${true} )`
     )
-    secondDropdownItem.children[0].setAttribute(
+    secondDropdownItemButton.setAttribute(
       'onclick',
       `userFollow.followOrChangeFollow(${userId}, ${false}, ${false} )`
     )
-    if (firstDropdownItem.children[0].classList.contains('is-active')) {
-      firstDropdownItem.children[0].classList.remove('is-active')
-    } else if (secondDropdownItem.children[0].classList.contains('is-active')) {
-      secondDropdownItem.children[0].classList.remove('is-active')
-    }
+    const notSelectedButtons = [
+      firstDropdownItemButton,
+      secondDropdownItemButton
+    ]
+    inactivateButtons(notSelectedButtons)
   }
 }
 
