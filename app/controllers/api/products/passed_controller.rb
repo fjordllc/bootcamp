ea1ea183fa2b8d14f8c3e5eb845a73396ca3968c
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-05-14 18:20:24 +0900

    未アサインの提出物の件数が異なっていたため修正した

--- app/controllers/api/products/passed_controller.rb
+++ app/controllers/api/products/passed_controller.rb
@@ -3,8 +3,9 @@
 class API::Products::PassedController < API::BaseController
   def show
     products = Product
-               .list
-               .ascending_by_date_of_publishing_and_id
+               .unassigned
+               .unchecked
+               .not_wip
     @passed4 = products.count { |product| product.elapsed_days == 4 }
     @passed5 = products.count { |product| product.elapsed_days == 5 }
     @over6 = products.count { |product| product.elapsed_days >= 6 }
