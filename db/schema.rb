d38c7f1de5946701b77543632cdabe7444b23782
hirano-vm4 <hirano.vm4@gmail.com>
2024-05-15 11:52:25 +0900

    schemaの差分を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_14_051751) do
+ActiveRecord::Schema.define(version: 2024_05_02_051341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
