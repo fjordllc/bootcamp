10259009f6454a39f67cfbb7dfc505e59583bc34
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-05-17 19:16:32 +0900

    テストの修正とコード整形

--- test/models/events_to_ical_exporter_test.rb
+++ test/models/events_to_ical_exporter_test.rb
@@ -7,12 +7,13 @@ class EventsToIcalExporterTest < ActiveSupport::TestCase
     travel_to Time.zone.local(2024, 3, 25, 10, 0, 0) do
       user = users(:kimura)
 
-      calendar = EventsToIcalExporter.export_events(user)
+      calendar = EventsToIcalExporter.new
+      subscription_calendar = calendar.export_events(user)
 
-      calendar.publish
-      assert_match(/【参加登録済】未来のイベント\(参加済\)/, calendar.to_ical)
-      assert_match(/未来のイベント\(未参加\)/, calendar.to_ical)
-      assert_no_match(/過去のイベント/, calendar.to_ical)
+      subscription_calendar.publish
+      assert_match(/【参加登録済】未来のイベント\(参加済\)/, subscription_calendar.to_ical)
+      assert_match(/未来のイベント\(未参加\)/, subscription_calendar.to_ical)
+      assert_no_match(/過去のイベント/, subscription_calendar.to_ical)
     end
   end
 end
--- test/models/regular_events_to_ical_exporter_test.rb
+++ test/models/regular_events_to_ical_exporter_test.rb
@@ -7,11 +7,12 @@ class RegularEventsToIcalExporterTest < ActiveSupport::TestCase
     travel_to Time.zone.local(2024, 3, 12, 10, 0, 0) do
       user = users(:kimura)
 
-      calendar = RegularEventsToIcalExporter.export_events(user)
+      calendar = RegularEventsToIcalExporter.new
+      subscription_calendar = calendar.export_events(user)
 
-      calendar.publish
-      assert_match(/参加反映テスト用定期イベント\(祝日非開催\)/, calendar.to_ical)
-      assert_no_match(/未参加反映テスト用定期イベント\(祝日非開催\)/, calendar.to_ical)
+      subscription_calendar.publish
+      assert_match(/参加反映テスト用定期イベント\(祝日非開催\)/, subscription_calendar.to_ical)
+      assert_no_match(/未参加反映テスト用定期イベント\(祝日非開催\)/, subscription_calendar.to_ical)
     end
   end
 end
