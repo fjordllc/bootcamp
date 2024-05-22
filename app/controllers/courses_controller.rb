174a21fea0a2e0fb50eedfe76dbf431e9bdf2e15
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-05-22 18:23:42 +0900

    サインアップページにコースを選択できるようにする
    サインアップページにコース選択を配置した
    別ページでコース一覧ページを作成した

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class CoursesController < ApplicationController
-  before_action :require_mentor_login, except: %i[index]
+  before_action :require_mentor_login, except: %i[index list]
+  skip_before_action :require_active_user_login, only: %i[list]
 
   def index
     @courses = Course.order(created_at: :desc)
+  end
+
+  def list
+    @courses = Course.all
   end
 
   private
