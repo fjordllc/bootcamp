969593d54668c29d65f594f04fbf3addb2753e6c
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-05-23 16:40:56 +0900

    クラス名とメソッド名の変更

--- test/models/special_events_calendar_test.rb
+++ test/models/special_events_calendar_test.rb
@@ -2,13 +2,13 @@
 
 require 'test_helper'
 
-class EventsToIcalExporterTest < ActiveSupport::TestCase
-  test 'check to export special events' do
+class SpecialEventsCalendarTest < ActiveSupport::TestCase
+  test 'check to convert special events' do
     travel_to Time.zone.local(2024, 3, 25, 10, 0, 0) do
       user = users(:kimura)
 
-      calendar = EventsToIcalExporter.new
-      subscription_calendar = calendar.export_events(user)
+      calendar = SpecialEventsCalendar.new
+      subscription_calendar = calendar.convert_to_ical(user)
 
       subscription_calendar.publish
       assert_match(/【参加登録済】未来のイベント\(参加済\)/, subscription_calendar.to_ical)
