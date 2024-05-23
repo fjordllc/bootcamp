ad12e376d61cd18e88203cdebb09e1d6281d859c
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-05-23 15:22:22 +0900

    動画が選択されていないときは追加できないように、警告メッセージが機能していなかったので修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,4 +17,5 @@ class Movie < ApplicationRecord
   validates :user, presence: true
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, presence: true
+  validates :movie_data, presence: true
 end
