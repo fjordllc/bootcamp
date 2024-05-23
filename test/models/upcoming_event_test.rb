572068dea6a7bfb8892dac6329191e005fd6d9ba
masyuko0222 <masyuko0222@gmail.com>
2024-05-23 21:06:01 +0900

    UpcomingEventのinitializeでscheduled_dateを受け取るように
    Viewで使うとき使いやすいし、コンストラクタの引数にあっても違和感はないため

--- test/models/upcoming_event_test.rb
+++ test/models/upcoming_event_test.rb
@@ -10,41 +10,54 @@ class UpcomingEventTest < ActiveSupport::TestCase
   end
 
   test '#held?(date) Event always be true' do
-    upcoming = UpcomingEvent.new(@special_event)
-    assert upcoming.held?(@holiday)
+    upcoming_special_event = UpcomingEvent.new(@special_event, @holiday)
+
+    assert upcoming_special_event.held?(@holiday)
   end
 
   test '#held?(date) RegularEvent is dynamic by rules' do
-    held_holiday = @regular_event.dup
-    held_holiday.hold_national_holiday = true
-    upcoming = UpcomingEvent.new(held_holiday)
-    assert upcoming.held?(@holiday)
+    held_holiday_event = @regular_event.dup
+    held_holiday_event.hold_national_holiday = true
 
-    not_held_holiday = @regular_event.dup
-    not_held_holiday.hold_national_holiday = false
-    upcoming = UpcomingEvent.new(not_held_holiday)
-    assert_not upcoming.held?(@holiday)
+    upcoming_regular_event = UpcomingEvent.new(held_holiday_event, @holiday)
+
+    assert upcoming_regular_event.held?(@holiday)
+
+    not_held_holiday_event = @regular_event.dup
+    not_held_holiday_event.hold_national_holiday = false
+
+    upcoming_regular_event = UpcomingEvent.new(not_held_holiday_event, @holiday)
+
+    assert_not upcoming_regular_event.held?(@holiday)
   end
 
   test '#date_with_start_time(date) Event' do
-    date = Date.new(2019, 12, 20)
-    upcoming = UpcomingEvent.new(@special_event)
-    assert_equal Time.zone.local(2019, 12, 20, 10), upcoming.date_with_start_time(date)
+    scheduled_date = Date.new(2019, 12, 20)
+
+    upcoming_event = UpcomingEvent.new(@special_event, scheduled_date)
+
+    assert_equal Time.zone.local(2019, 12, 20, 10), upcoming_event.date_with_start_time(scheduled_date)
   end
 
   test '#date_with_start_time(date) RegularEvent' do
-    date = Date.new(2024, 5, 5) # Sunday
-    upcoming = UpcomingEvent.new(@regular_event)
-    assert_equal Time.zone.local(2024, 5, 5, 15), upcoming.date_with_start_time(date)
+    scheduled_date = Date.new(2024, 5, 5) # Sunday
+
+    upcoming_event = UpcomingEvent.new(@regular_event, scheduled_date)
+
+    assert_equal Time.zone.local(2024, 5, 5, 15), upcoming_event.date_with_start_time(scheduled_date)
   end
 
   test '#for_job_hunting?' do
     @special_event.update!(job_hunting: true)
-    upcoming_special = UpcomingEvent.new(@special_event)
-    assert upcoming_special.for_job_hunting?
+    scheduled_date = Time.zone.today
+
+    upcoming_special_event = UpcomingEvent.new(@special_event, scheduled_date)
+
+    assert upcoming_special_event.for_job_hunting?
 
     @special_event.update!(job_hunting: false)
-    upcoming_special = UpcomingEvent.new(@special_event)
-    assert_not upcoming_special.for_job_hunting?
+    upcoming_special_event = UpcomingEvent.new(@special_event, scheduled_date)
+
+    assert_not upcoming_special_event.for_job_hunting?
   end
 end
