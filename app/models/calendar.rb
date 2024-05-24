23faaeebb38c13471a4d14e874e7e48c64569c6c
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-05-24 19:43:09 +0900

    クラス構成とメソッドを見直し

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,24 +2,32 @@
 
 class Calendar
   def combine_special_regular_calendar(user)
-    special_calendar = special_calendar_str(user)
-    regular_calendar = regular_calendar_str(user)
+    calendar_to_ical_str = generate_calendar_str(user)
+    regular_calendar_to_ical_str = generate_regular_calendar_str(user)
 
-    calendar_str = "#{special_calendar}#{regular_calendar}"
-    Icalendar::Calendar.parse(calendar_str).first
+    calendar_to_ical_str = "#{calendar_to_ical_str}#{regular_calendar_to_ical_str}"
+    Icalendar::Calendar.parse(calendar_to_ical_str).first
   end
 
   private
 
-  def special_calendar_str(user)
-    special_calendar = SpecialEventsCalendar.new
-    special_calendar_to_ical = special_calendar.convert_to_ical(user).to_ical
-    special_calendar_to_ical.gsub(/END:VCALENDAR\r?\n?\z/, '')
+  def generate_calendar_str(user)
+    events_list = EventList.new
+    personal_events_list = events_list.fetch_events_list(user)
+    events_icalendar_str = IcalendarStr.new
+    calendar_str = events_icalendar_str.convert_events_to_ical(personal_events_list)
+
+    calendar_to_ical = calendar_str.to_ical
+    calendar_to_ical.gsub(/END:VCALENDAR\r?\n?\z/, '')
   end
 
-  def regular_calendar_str(user)
-    regular_calendar = RegularEventsCalendar.new
-    regular_calendar_to_ical = regular_calendar.convert_to_ical(user).to_ical
+  def generate_regular_calendar_str(user)
+    regular_events_list = EventList.new
+    personal_regular_events_list = regular_events_list.fetch_regular_events_list(user)
+    regular_events_icalendar_str = IcalendarStr.new
+    regular_calendar_str = regular_events_icalendar_str.convert_regular_events_to_ical(personal_regular_events_list)
+
+    regular_calendar_to_ical = regular_calendar_str.to_ical
     regular_calendar_to_ical.gsub(/\A(?:BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:icalendar-ruby\r\nCALSCALE:GREGORIAN\r\n)/, '')
   end
 end
--- test/models/events_calendar_test.rb
+++ test/models/events_calendar_test.rb
@@ -2,18 +2,20 @@
 
 require 'test_helper'
 
-class SpecialEventsCalendarTest < ActiveSupport::TestCase
-  test 'check to convert special events' do
+class EventsCalendarTest < ActiveSupport::TestCase
+  test 'check to generate icalendar' do
     travel_to Time.zone.local(2024, 3, 25, 10, 0, 0) do
       user = users(:kimura)
 
-      calendar = SpecialEventsCalendar.new
-      subscription_calendar = calendar.convert_to_ical(user)
+      calendar = Calendar.new
+      subscription_calendar = calendar.combine_special_regular_calendar(user).publish
 
       subscription_calendar.publish
       assert_match(/【参加登録済】未来のイベント\(参加済\)/, subscription_calendar.to_ical)
       assert_match(/未来のイベント\(未参加\)/, subscription_calendar.to_ical)
       assert_no_match(/過去のイベント/, subscription_calendar.to_ical)
+      assert_match(/参加反映テスト用定期イベント\(祝日非開催\)/, subscription_calendar.to_ical)
+      assert_no_match(/未参加反映テスト用定期イベント\(祝日非開催\)/, subscription_calendar.to_ical)
     end
   end
 end
