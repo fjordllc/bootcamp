ba479ffb61783354f0ac1b798cfdedd8df38a96a
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-05-27 12:58:12 +0900

    コース一覧ページを/welcomeの配下に変更した

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
 class CoursesController < ApplicationController
-  before_action :require_mentor_login, except: %i[index list]
-  skip_before_action :require_active_user_login, only: %i[list]
+  before_action :require_mentor_login, except: %i[index]
 
   def index
     @courses = Course.order(created_at: :desc)
-  end
-
-  def list
-    @courses = Course.all
   end
 
   private
