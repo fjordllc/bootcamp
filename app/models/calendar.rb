b69a4af95776063baebcb27e9ca1ece9b2fd6428
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-05-27 11:34:33 +0900

    クラス名・メソッド名・変数名を修正

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -12,20 +12,20 @@ class Calendar
   private
 
   def generate_calendar_str(user)
-    events_list = EventList.new
-    personal_events_list = events_list.fetch_events_list(user)
-    events_icalendar_str = IcalendarStr.new
-    calendar_str = events_icalendar_str.convert_events_to_ical(personal_events_list)
+    events = EventsFetcher.new
+    personal_events = events.fetch_events(user)
+    events_icalendar_str = EventsToIcalConverter.new
+    calendar_str = events_icalendar_str.convert_events_to_ical(personal_events)
 
     calendar_to_ical = calendar_str.to_ical
     calendar_to_ical.gsub(/END:VCALENDAR\r?\n?\z/, '')
   end
 
   def generate_regular_calendar_str(user)
-    regular_events_list = EventList.new
-    personal_regular_events_list = regular_events_list.fetch_regular_events_list(user)
-    regular_events_icalendar_str = IcalendarStr.new
-    regular_calendar_str = regular_events_icalendar_str.convert_regular_events_to_ical(personal_regular_events_list)
+    regular_events = EventsFetcher.new
+    personal_regular_events = regular_events.fetch_regular_events(user)
+    regular_events_icalendar_str = EventsToIcalConverter.new
+    regular_calendar_str = regular_events_icalendar_str.convert_regular_events_to_ical(personal_regular_events)
 
     regular_calendar_to_ical = regular_calendar_str.to_ical
     regular_calendar_to_ical.gsub(/\A(?:BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:icalendar-ruby\r\nCALSCALE:GREGORIAN\r\n)/, '')
