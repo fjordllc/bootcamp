d11b4d6c9baae511cae584df06caffe8ca195b3d
masyuko0222 <masyuko0222@gmail.com>
2024-05-28 23:13:51 +0900

    翻訳ファイル追加

--- app/models/request_retirement.rb
+++ app/models/request_retirement.rb
@@ -4,7 +4,7 @@ class RequestRetirement < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :target_user, class_name: 'User', optional: true
 
-  validates :target_user_id, presence: true, uniqueness: true
+  validates :target_user_id, presence: true, uniqueness: { message: 'は既に申請済みです' }
   validates :reason, presence: true
   validates :keep_data, inclusion: [true, false]
 end
