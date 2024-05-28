ea7a2ca80fb046780e12e1c89ac174101ed784ae
naokinaokiboo <naokinaokiboo@gmail.com>
2024-05-28 21:15:51 +0900

    招待URLの変更をHTTPリクエスト無しで行うように修正

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     namespace 'admin' do
       resource :count, controller: 'count', only: %i(show)
       resources :companies, only: %i(index destroy)
-      resources :invitation_url, only: %i(index)
     end
     namespace 'mentor' do
       resources :practices, only: %i(index)
