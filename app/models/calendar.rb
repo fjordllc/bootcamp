42d645a369a690324bd301643add00b77b329fa9
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-05-29 13:59:54 +0900

    strを使っていた箇所を修正

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -2,32 +2,32 @@
 
 class Calendar
   def combine_special_regular_calendar(user)
-    calendar_to_ical_str = generate_calendar_str(user)
-    regular_calendar_to_ical_str = generate_regular_calendar_str(user)
+    formatted_calendar_to_ical = generate_formatted_calendar(user)
+    formatted_regular_calendar_to_ical = generate_formatted_regular_calendar(user)
 
-    calendar_to_ical_str = "#{calendar_to_ical_str}#{regular_calendar_to_ical_str}"
-    Icalendar::Calendar.parse(calendar_to_ical_str).first
+    formatted_calendar_to_ical = "#{formatted_calendar_to_ical}#{formatted_regular_calendar_to_ical}"
+    Icalendar::Calendar.parse(formatted_calendar_to_ical).first
   end
 
   private
 
-  def generate_calendar_str(user)
+  def generate_formatted_calendar(user)
     events = EventsFetcher.new
     personal_events = events.fetch_events(user)
-    events_icalendar_str = EventsToIcalConverter.new
-    calendar_str = events_icalendar_str.convert_events_to_ical(personal_events)
+    events_to_icalendar = EventsToIcalConverter.new
+    converted_calendar = events_to_icalendar.convert_events_to_ical(personal_events)
 
-    calendar_to_ical = calendar_str.to_ical
+    calendar_to_ical = converted_calendar.to_ical
     calendar_to_ical.gsub(/END:VCALENDAR\r?\n?\z/, '')
   end
 
-  def generate_regular_calendar_str(user)
+  def generate_formatted_regular_calendar(user)
     regular_events = EventsFetcher.new
     personal_regular_events = regular_events.fetch_regular_events(user)
-    regular_events_icalendar_str = EventsToIcalConverter.new
-    regular_calendar_str = regular_events_icalendar_str.convert_regular_events_to_ical(personal_regular_events)
+    regular_events_to_icalendar = EventsToIcalConverter.new
+    converted_regular_calendar = regular_events_to_icalendar.convert_regular_events_to_ical(personal_regular_events)
 
-    regular_calendar_to_ical = regular_calendar_str.to_ical
+    regular_calendar_to_ical = converted_regular_calendar.to_ical
     regular_calendar_to_ical.gsub(/\A(?:BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:icalendar-ruby\r\nCALSCALE:GREGORIAN\r\n)/, '')
   end
 end
