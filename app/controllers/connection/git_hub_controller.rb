0b4a9106a229ab9d686f779fef51943e6e4bc1fb
Kuroneko <ken.aster8@gmail.com>
2024-05-31 17:45:21 +0900

    管理者以外は他のユーザーのGitHub連携を解除できないようにガードを追加

--- app/controllers/connection/git_hub_controller.rb
+++ app/controllers/connection/git_hub_controller.rb
@@ -5,6 +5,11 @@ class Connection::GitHubController < ApplicationController
 
   def destroy
     user = User.find(params[:user_id])
+    if !admin_login? && user != current_user
+      redirect_to root_path, alert: '管理者としてログインしてください'
+      return
+    end
+
     user.update(github_id: nil)
     redirect_to user_path(user), notice: 'GitHubとの連携を解除しました。'
   end
