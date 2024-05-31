bbaf218a92f5e9a757854f65c40543cb2909d99e
Kuroneko <ken.aster8@gmail.com>
2024-05-31 16:58:09 +0900

    他のユーザーのGitHub連携を解除できるように修正

--- app/controllers/connection/git_hub_controller.rb
+++ app/controllers/connection/git_hub_controller.rb
@@ -4,7 +4,8 @@ class Connection::GitHubController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
 
   def destroy
-    current_user.update(github_id: nil)
-    redirect_to root_path, notice: 'GitHubとの連携を解除しました。'
+    user = User.find(params[:user_id])
+    user.update(github_id: nil)
+    redirect_to user_path(user), notice: 'GitHubとの連携を解除しました。'
   end
 end
