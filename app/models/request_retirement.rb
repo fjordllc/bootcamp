be49330e29f8be14224fdff6e7acce66d0a0ca7c
masyuko0222 <masyuko0222@gmail.com>
2024-06-03 20:35:48 +0900

    仕様変更前につけてて、変更後に削除し忘れていたので消した

--- app/models/request_retirement.rb
+++ app/models/request_retirement.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class RequestRetirement < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :target_user, class_name: 'User', optional: true
+  belongs_to :user
+  belongs_to :target_user, class_name: 'User'
 
   validates :target_user_id, presence: true, uniqueness: { message: 'は既に申請済みです' }
   validates :reason, presence: true
