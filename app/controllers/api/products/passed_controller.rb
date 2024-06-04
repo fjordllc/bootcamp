db9239e38a1af25d454655c080a637e57265acb8
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-06-04 10:25:29 +0900

    文章の日数を表示できるようにした

--- app/controllers/api/products/passed_controller.rb
+++ app/controllers/api/products/passed_controller.rb
@@ -6,9 +6,9 @@ class API::Products::PassedController < API::BaseController
                .unassigned
                .unchecked
                .not_wip
-    product_deadline_day = ProductDeadline.first_or_create(alert_day: 4).alert_day
-    @first_alert = products.count { |product| product.elapsed_days == product_deadline_day }
-    @second_alert = products.count { |product| product.elapsed_days == product_deadline_day + 1 }
-    @last_alert = products.count { |product| product.elapsed_days >= product_deadline_day + 2 }
+    @product_deadline_day = ProductDeadline.first_or_create(alert_day: 4).alert_day
+    @first_alert = products.count { |product| product.elapsed_days == @product_deadline_day }
+    @second_alert = products.count { |product| product.elapsed_days == @product_deadline_day + 1 }
+    @last_alert = products.count { |product| product.elapsed_days >= @product_deadline_day + 2 }
   end
 end
