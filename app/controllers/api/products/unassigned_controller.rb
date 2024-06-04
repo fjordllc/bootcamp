7ede993a2bee1399e42a27997527a2b519c0e0fc
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-06-04 10:10:43 +0900

    Discordで送るアラートをダッシュボードで設定した期限で表示できるように変更した
    アラートの期限を変更できるようになったので、変数名を修正した

--- app/controllers/api/products/unassigned_controller.rb
+++ app/controllers/api/products/unassigned_controller.rb
@@ -17,8 +17,9 @@ class API::Products::UnassignedController < API::BaseController
                .unassigned
                .unchecked
                .not_wip
-    @passed4 = products.count { |product| product.elapsed_days == 4 }
-    @passed5 = products.count { |product| product.elapsed_days == 5 }
-    @over6 = products.count { |product| product.elapsed_days >= 6 }
+    product_deadline_day = ProductDeadline.first_or_create(alert_day: 4).alert_day
+    @first_alert = products.count { |product| product.elapsed_days == product_deadline_day }
+    @second_alert = products.count { |product| product.elapsed_days == product_deadline_day + 1 }
+    @last_alert = products.count { |product| product.elapsed_days >= product_deadline_day + 2 }
   end
 end
