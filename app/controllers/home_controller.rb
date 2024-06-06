75e729e24aa355af721ed705a0f8bdee0d26cba7
masyuko0222 <masyuko0222@gmail.com>
2024-06-06 23:41:38 +0900

    nilを返すとwhereとかでエラーが起こるので、空のActiveRecord_Relationを返すように

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -42,8 +42,8 @@ class HomeController < ApplicationController
     @completed_learnings = current_user.learnings.where(status: 3).includes(:practice).order(updated_at: :desc)
     @inactive_students = User.with_attached_avatar.inactive_students_and_trainees.order(last_activity_at: :desc)
     @job_seeking_users = User.with_attached_avatar.job_seeking.includes(:reports, :products, :works, :course, :company)
-    @collegue_trainees = current_user.collegue_trainees&.with_attached_avatar&.includes(:reports, :products, :comments)
-    collegue_trainees_reports = Report.with_avatar.where(wip: false).where(user: current_user.collegue_trainees&.with_attached_avatar)
+    @collegue_trainees = current_user.collegue_trainees.with_attached_avatar.includes(:reports, :products, :comments)
+    collegue_trainees_reports = Report.with_avatar.where(wip: false).where(user: current_user.collegue_trainees.with_attached_avatar)
     @collegue_trainees_recent_reports = collegue_trainees_reports.order(reported_on: :desc).limit(10)
     @recent_reports = Report.with_avatar.where(wip: false).order(reported_on: :desc, created_at: :desc).limit(10)
     @collegues = current_user.collegues_other_than_self
