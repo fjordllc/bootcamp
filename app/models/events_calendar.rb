fe0728860f12cc94dc652c46faff35768af2ace6
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-06-06 13:36:10 +0900

    クラス構成を再度見直し

---
+++ app/models/events_calendar.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class EventsCalendar
+  attr_reader :events
+
+  def initialize(user)
+    @user = user
+    @events = generate_event_calendar
+  end
+
+  private
+
+  def generate_event_calendar
+    special_event = Event.new
+    upcoming_speial_events = special_event.fetch_special_events(@user)
+
+    regular_event = RegularEvent.new
+    participated_regular_events = regular_event.fetch_participated_regular_events(@user)
+    format_participated_regular_events = regular_event.format_participated_regular_events(participated_regular_events)
+
+    upcoming_speial_events + format_participated_regular_events
+  end
+end
