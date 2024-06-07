6515108e802cb9ea0ad074b83a45662cf75ac55c
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-06-07 17:40:46 +0900

    コンストラクタでのメソッド呼び出しをしていた箇所の処理をなくした

--- app/controllers/events/calendars_controller.rb
+++ app/controllers/events/calendars_controller.rb
@@ -8,7 +8,7 @@ class Events::CalendarsController < ApplicationController
     user = User.find_by(id: user_id)
 
     events_calendar = EventsCalendar.new(user)
-    @events = events_calendar.events
+    @events = events_calendar.generate_event_calendar
 
     render :index, layout: false, content_type: 'text/calendar'
   end
