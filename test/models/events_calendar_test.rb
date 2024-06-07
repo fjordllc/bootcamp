dfb4a05b7fc0b76cd68c9cf0ec479174e6d814e1
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-06-07 17:40:46 +0900

    コンストラクタでのメソッド呼び出しをしていた箇所の処理をなくした

--- test/models/events_calendar_test.rb
+++ test/models/events_calendar_test.rb
@@ -9,7 +9,7 @@ class EventsCalendarTest < ActiveSupport::TestCase
       user = users(:kimura)
 
       events_calendar = EventsCalendar.new(user)
-      @events = events_calendar.events
+      @events = events_calendar.generate_event_calendar
 
       cal = Icalendar::Calendar.new
 
