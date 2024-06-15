fb04e30a7733e5b4307095a13e88872ec276878a
appsu-mi <natubate870@gmail.com>
2024-06-15 12:35:44 +0900

    ヘルパーメソッドを廃止、partialとして切り出す形に

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -5,12 +5,4 @@ module CompaniesHelper
     desc_ordered_companies = Company.order(created_at: :desc).to_a
     desc_ordered_companies.unshift(Company.new(name: '所属なし'))
   end
-
-  def link_to_company_users_path(company, target:)
-    target_allowlist = %w[advisers students_and_trainees admins mentor]
-    link_to_if(target_allowlist.include?(target),
-               company.users.unretired.send(target).size,
-               company_users_path(target: target.singularize, company_id: company.id),
-               class: 'card-counts__item-value-link')
-  end
 end
