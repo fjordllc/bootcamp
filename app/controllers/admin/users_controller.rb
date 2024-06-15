71e7b967984173c48aee363c4efbd2226134cf61
Kuroneko <ken.aster8@gmail.com>
2024-06-15 15:29:50 +0900

    管理者が他のメンターのプロフィール公開・非公開を変更できるように修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -66,7 +66,7 @@ class Admin::UsersController < AdminController
       :graduated_on, :retired_on, :free,
       :job_seeker, :github_collaborator,
       :officekey_permission, :tag_list, :training_ends_on,
-      :auto_retire, :invoice_payment,
+      :auto_retire, :invoice_payment, :hide_mentor_profile,
       :profile_image, :profile_name, :profile_job, :mentor,
       :profile_text, { authored_books_attributes: %i[id title url cover _destroy] },
       :country_code, :subdivision_code, discord_profile_attributes: %i[account_name times_url times_id]
