79cdbe028fb2dee8a1921a0ffd20b1df8d8825fc
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-06-19 23:12:00 +0900

    冗長だったコードをまとめた
    liの中身を関数でまとめ、コードを短縮した

--- app/javascript/components/ElapsedDays.jsx
+++ app/javascript/components/ElapsedDays.jsx
@@ -8,66 +8,50 @@ export default function ElapsedDays({
     return quantity ? 'is-active' : 'is-inactive'
   }
 
+  const renderElapsedDay = (elapsedDays, linkHref, linkText, alertText) => {
+    return (
+      <li
+        className={`page-nav__item is-reply-${linkText} border-b-0 ${activeClass(
+          countProductsGroupedBy(productDeadlineDay + elapsedDays)
+        )}`}>
+        <a className="page-nav__item-link" href={`#${linkHref}`}>
+          <span className="page-nav__item-link-inner">
+            {productDeadlineDay + elapsedDays}日{alertText}経過
+            {` (${countProductsGroupedBy(productDeadlineDay + elapsedDays)})`}
+          </span>
+        </a>
+      </li>
+    )
+  }
+
   return (
     <nav className="page-body__column is-sub">
       <div className="page-nav a-card">
         <ol className="page-nav__items elapsed-days">
-          <li
-            className={`page-nav__item is-reply-deadline border-b-0 ${activeClass(
-              countProductsGroupedBy(productDeadlineDay + 2)
-            )}`}>
-            <a className="page-nav__item-link" href="#deadline-alert">
-              <span className="page-nav__item-link-inner">
-                {productDeadlineDay + 2}日以上経過
-                {` (${countProductsGroupedBy(productDeadlineDay + 2)})`}
-              </span>
-            </a>
-          </li>
-          <li
-            className={`page-nav__item is-reply-alert border-b-0 ${activeClass(
-              countProductsGroupedBy(productDeadlineDay + 1)
-            )}`}>
-            <a className="page-nav__item-link" href="#second-alert">
-              <span className="page-nav__item-link-inner">
-                {productDeadlineDay + 1}日経過
-                {` (${countProductsGroupedBy(productDeadlineDay + 1)})`}
-              </span>
-            </a>
-          </li>
-          <li
-            className={`page-nav__item is-reply-warning border-b-0 ${activeClass(
-              countProductsGroupedBy(productDeadlineDay)
-            )}`}>
-            <a className="page-nav__item-link" href="#first-alert">
-              <span className="page-nav__item-link-inner">
-                {productDeadlineDay}日経過
-                {` (${countProductsGroupedBy(productDeadlineDay)})`}
-              </span>
-            </a>
-          </li>
+          {renderElapsedDay(+2, 'deadline-alert', 'deadline', '以上')}
+          {renderElapsedDay(+1, 'second-alert', 'alert')}
+          {renderElapsedDay(0, 'first-alert', 'warning')}
           {Array.from(
             { length: productDeadlineDay - 1 },
             (_, index) => index + 1
           )
             .reverse()
-            .map((passedDay) => {
-              return (
-                <li
-                  key={passedDay}
-                  className={`page-nav__item ${activeClass(
-                    countProductsGroupedBy(passedDay)
-                  )}`}>
-                  <a
-                    href={`#${passedDay}days-elapsed`}
-                    className="page-nav__item-link">
-                    <span className="page-nav__item-link-inner">
-                      {passedDay}日経過
-                      {` (${countProductsGroupedBy(passedDay)})`}
-                    </span>
-                  </a>
-                </li>
-              )
-            })}
+            .map((passedDay) => (
+              <li
+                key={passedDay}
+                className={`page-nav__item ${activeClass(
+                  countProductsGroupedBy(passedDay)
+                )}`}>
+                <a
+                  href={`#${passedDay}days-elapsed`}
+                  className="page-nav__item-link">
+                  <span className="page-nav__item-link-inner">
+                    {passedDay}日経過
+                    {` (${countProductsGroupedBy(passedDay)})`}
+                  </span>
+                </a>
+              </li>
+            ))}
           <li
             className={`page-nav__item ${activeClass(
               countProductsGroupedBy(0)
