d477c6eb9d9667f14783ae7510de15d8f9222a0a
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2024-06-19 13:05:46 +0900

    デフォルトのコースの定義場所を変更した
    find_default_courseは他でも使いそうなのでモデルに移動した

--- app/helpers/signup_helper.rb
+++ app/helpers/signup_helper.rb
@@ -4,10 +4,6 @@ module SignupHelper
   def find_course(course_id)
     Course.find(course_id)
   rescue ActiveRecord::RecordNotFound
-    find_default_course
-  end
-
-  def find_default_course
-    Course.find_by(title: 'Railsプログラマー')
+    Course.find_default_course
   end
 end
