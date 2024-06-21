59130ec9a654455ae7aeff96d45eb229b31ca462
machida <machida@fjord.jp>
2024-06-21 14:43:17 +0900

    学習内容ページ更新

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@
 class WelcomeController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
   layout 'welcome'
+  DEFAULT_COURSE = 'Railsプログラマー'
+  RAILS_COURSE = Course.find_by(title: DEFAULT_COURSE)
+  FRONTEND_COURSE = Course.find_by(title: 'フロントエンドエンジニア')
 
   def index
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
@@ -16,6 +19,8 @@ class WelcomeController < ApplicationController
 
   def practices
     @categories = Course.default_course.categories.preload(:practices).order(:position)
+    @rails_categories = RAILS_COURSE ? RAILS_COURSE.categories.preload(:practices).order(:position) : []
+    @frontend_categories = FRONTEND_COURSE ? FRONTEND_COURSE.categories.preload(:practices).order(:position) : []
   end
 
   def tos; end
