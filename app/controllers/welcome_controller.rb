33a4753c4069192e6290e0f7d256fe335129fc28
machida <machida@fjord.jp>
2024-06-21 14:46:46 +0900

    不要な定数を削除

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,6 @@ class WelcomeController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
   layout 'welcome'
   DEFAULT_COURSE = 'Railsプログラマー'
-  RAILS_COURSE = Course.find_by(title: DEFAULT_COURSE)
-  FRONTEND_COURSE = Course.find_by(title: 'フロントエンドエンジニア')
 
   def index
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
@@ -17,11 +15,7 @@ class WelcomeController < ApplicationController
 
   def training; end
 
-  def practices
-    @categories = Course.default_course.categories.preload(:practices).order(:position)
-    @rails_categories = RAILS_COURSE ? RAILS_COURSE.categories.preload(:practices).order(:position) : []
-    @frontend_categories = FRONTEND_COURSE ? FRONTEND_COURSE.categories.preload(:practices).order(:position) : []
-  end
+  def practices; end
 
   def tos; end
 
