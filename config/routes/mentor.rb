d2f6e1f704171e424676000fcfe3651e3d3ef950
masyuko0222 <masyuko0222@gmail.com>
2024-06-23 17:44:12 +0900

    ルーティング追加

--- config/routes/mentor.rb
+++ config/routes/mentor.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :categories do
       resources :practices, only: %i(index), controller: "categories/practices"
     end
-    resources :practices, only: %i(index new edit create update)
+    resources :practices, only: %i(index new edit create update) do
+      resource :model_answer, only: %i(new edit create update), controller: "practices/model_answer"
+    end
     resources :courses, only: %i(index new edit create update) do
       resources :categories, only: %i(index), controller: "courses/categories"
     end
