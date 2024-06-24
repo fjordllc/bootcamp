f6a8ac1db40178eeb23cd629d57ce0fa5a1f1e12
masyuko0222 <masyuko0222@gmail.com>
2024-06-24 22:49:57 +0900

    model_answer#showの閲覧権限チェック

--- app/controllers/practices/model_answer_controller.rb
+++ app/controllers/practices/model_answer_controller.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 class Practices::ModelAnswerController < ApplicationController
+  before_action :set_model_answer, only: %i[show]
+  before_action :check_permission!, only: %i[show]
+
   def show
-    @practice = Practice.find(params[:practice_id])
-    @model_answer = @practice.model_answer
+    @practice = @model_answer.practice
+  end
+
+  private
+
+  def set_model_answer
+    @model_answer = ModelAnswer.find_by(practice_id: params[:practice_id])
+  end
+
+  def check_permission!
+    return if policy(@model_answer).show?
+
+    redirect_to practice_path(@model_answer.practice), alert: 'プラクティスを修了するまで模範解答は見れません。'
   end
 end
