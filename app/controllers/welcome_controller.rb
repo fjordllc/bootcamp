8f13491177012bc178c6d18e03600101e6c809f6
machida <machida@fjord.jp>
2024-06-24 16:06:48 +0900

    プログラマーをエンジニアに変更

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@
 class WelcomeController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
   layout 'welcome'
-  DEFAULT_COURSE = 'Railsプログラマー'
+  DEFAULT_COURSE = 'Railsエンジニア'
 
   def index
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
