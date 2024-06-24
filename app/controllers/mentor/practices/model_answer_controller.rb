bbe0093d7db958f461f606b6014dc66cef007f79
masyuko0222 <masyuko0222@gmail.com>
2024-06-24 21:43:21 +0900

    model_answer#edit,update

--- app/controllers/mentor/practices/model_answer_controller.rb
+++ app/controllers/mentor/practices/model_answer_controller.rb
@@ -2,23 +2,45 @@
 
 class Mentor::Practices::ModelAnswerController < ApplicationController
   before_action :require_admin_or_mentor_login
+  before_action :set_model_answer, only: %i[edit update]
 
   def new
-    @practice = Practice.find(params[:practice_id])
+    @practice = find_practice
     @model_answer = @practice.build_model_answer
   end
 
+  def edit
+    @practice = @model_answer.practice
+  end
+
   def create
-    @practice = Practice.find(params[:practice_id])
+    @practice = find_practice
     @model_answer = @practice.build_model_answer(model_answer_params)
     if @model_answer.save
-      redirect_to practice_model_answer_url(@practice), notice: '模範解答を作成しました。'
+      redirect_to practice_model_answer_url, notice: '模範解答を作成しました。'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def update
+    @practice = @model_answer.practice
+    if @model_answer.update(model_answer_params)
+      redirect_to practice_model_answer_url, notice: '模範解答を更新しました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_model_answer
+    @model_answer = ModelAnswer.find_by(practice_id: params[:practice_id])
+  end
+
+  def find_practice
+    Practice.find(params[:practice_id])
+  end
 
   def model_answer_params
     params.require(:model_answer).permit(:description)
