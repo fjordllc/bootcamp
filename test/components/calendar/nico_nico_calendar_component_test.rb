88825b05f37256e63ab9068c949c896c9052c0c4
reckyy <srreki98@gmail.com>
2024-06-29 11:23:05 +0900

    niconicocalendarのパーシャルをviewcomponentに移行

---
+++ test/components/calendar/nico_nico_calendar_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Calendar::NicoNicoCalendarComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Calendar::NicoNicoCalendarComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
