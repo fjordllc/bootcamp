081227fc956c1b0c7d4955f7475b525cffd6ef98
reckyy <srreki98@gmail.com>
2024-06-29 21:16:49 +0900

    一部を新たにメソッド化
    また、それに伴いテストも一部同時に行うようにした。component_test内のコメント参照。

--- test/components/calendar/nico_nico_calendar_component_test.rb
+++ test/components/calendar/nico_nico_calendar_component_test.rb
@@ -5,8 +5,8 @@ require 'test_helper'
 class Calendar::NicoNicoCalendarComponentTest < ViewComponent::TestCase
   setup do
     @user = users(:hatsuno).extend(UserDecorator)
-    @current_date = "#{Date.current.year}-#{Date.current.month}"
-    calendar = NicoNicoCalendar.new(@user, @current_date)
+    params_date = "#{Date.current.year}-#{Date.current.month}"
+    calendar = NicoNicoCalendar.new(@user, params_date)
     @current_date = calendar.current_date
     @current_calendar = calendar.with_reports
     render_inline(Calendar::NicoNicoCalendarComponent.new(
@@ -17,21 +17,26 @@ class Calendar::NicoNicoCalendarComponentTest < ViewComponent::TestCase
                   ))
   end
 
-  def test_prev_month?
-    assert_selector '.niconico-calendar-nav__previous'
+  # test_prev_month_linkでprev_month?とprev_month_pathメソッド2つのテストを行なってる。nextも同様。
+  def test_prev_month_link
+    prev_month_str = @current_date.prev_month.strftime('%Y-%m')
+    expected_path = "/?niconico_calendar=#{prev_month_str}"
+    assert_selector "a[href='#{expected_path}'] .niconico-calendar-nav__previous i.fa-solid.fa-angle-left"
   end
 
   def test_frame_and_background
     assert_selector '.niconico-calendar__day.is-blank.is-today'
   end
 
-  def test_next_month?
+  def test_next_month_link
     render_inline(Calendar::NicoNicoCalendarComponent.new(
                     user: @user,
                     path: :niconico_calendar_date_path,
                     current_date: Date.current.prev_month,
                     current_calendar: @current_calendar
                   ))
-    assert_selector '.niconico-calendar-nav__next'
+    next_month_str = @current_date.strftime('%Y-%m')
+    expected_path = "/?niconico_calendar=#{next_month_str}"
+    assert_selector "a[href='#{expected_path}'] .niconico-calendar-nav__next i.fa-solid.fa-angle-right"
   end
 end
