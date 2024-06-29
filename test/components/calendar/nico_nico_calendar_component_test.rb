6f5deff09d6d176a3463aa524608802fc2697eca
reckyy <srreki98@gmail.com>
2024-06-29 16:20:58 +0900

    nico_nico_calendar_componentのテスト作成

--- test/components/calendar/nico_nico_calendar_component_test.rb
+++ test/components/calendar/nico_nico_calendar_component_test.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class Calendar::NicoNicoCalendarComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(Calendar::NicoNicoCalendarComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  setup do
+    @user = users(:hatsuno).extend(UserDecorator)
+    @current_date = "#{Date.current.year}-#{Date.current.month}"
+    calendar = NicoNicoCalendar.new(@user, @current_date)
+    @current_date = calendar.current_date
+    @current_calendar = calendar.with_reports
+    render_inline(Calendar::NicoNicoCalendarComponent.new(
+                    user: @user,
+                    path: :niconico_calendar_date_path,
+                    current_date: @current_date,
+                    current_calendar: @current_calendar
+                  ))
+  end
+
+  def test_prev_month?
+    assert_selector '.niconico-calendar-nav__previous'
+  end
+
+  def test_frame_and_background
+    assert_selector '.niconico-calendar__day.is-blank.is-today'
+  end
+
+  def test_next_month?
+    render_inline(Calendar::NicoNicoCalendarComponent.new(
+                    user: @user,
+                    path: :niconico_calendar_date_path,
+                    current_date: Date.current.prev_month,
+                    current_calendar: @current_calendar
+                  ))
+    assert_selector '.niconico-calendar-nav__next'
   end
 end
