b722e9bfe83fefc8926ab11aaa92aea5d183f5c1
goruchan <ppcfr481@yahoo.co.jp>
2024-07-04 23:51:00 +0900

    slim が表示されなかった不具合を修正

--- db/migrate/20240704140358_update_articles_with_secure_token.rb
+++ db/migrate/20240704140358_update_articles_with_secure_token.rb
@@ -1,12 +1,7 @@
 class UpdateArticlesWithSecureToken < ActiveRecord::Migration[6.1]
   def up
-    Article.reset_column_information
-    Article.find_each do |article|
-      article.update_columns(token: SecureRandom.urlsafe_base64)
+    Article.where(wip: true).find_each do |article|
+      article.update(token: SecureRandom.urlsafe_base64)
     end
-  end
-
-  def down
-    Article.update_all(token: nil)
   end
 end
