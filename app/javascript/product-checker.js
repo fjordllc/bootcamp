9211df8f688b2abf624163c938cd53aa56b0546a
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-07-05 12:02:23 +0900

    担当するorしないを切り替える際のcheckInChargeの呼び出し方法を修正
    ・従来はwindowにproductCheckerを格納し、View側のonclickで呼び出す方法をとっていたが、グローバル変数は誰がいつどこで変更しているかわかりづらくなるため修正した。

--- app/javascript/product-checker.js
+++ app/javascript/product-checker.js
@@ -13,79 +13,86 @@ function toast(title, status = 'success') {
   })
 }
 
-const productChecker = {
-  checkInCharge(productId, currentUserId) {
-    const token = CSRF.getToken()
-    const url = '/api/products/checker'
+function checkInCharge(event, productId, currentUserId) {
+  event.preventDefault()
+  const token = CSRF.getToken()
+  const url = '/api/products/checker'
 
-    const params = {
-      product_id: productId,
-      current_user_id: currentUserId
-    }
+  const params = {
+    product_id: productId,
+    current_user_id: currentUserId
+  }
 
-    const query = new URLSearchParams(params)
-    fetch(url + `?${query}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-Token': token
-      },
-      credentials: 'same-origin',
-      redirect: 'manual'
+  const query = new URLSearchParams(params)
+  fetch(url + `?${query}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRF-Token': token
+    },
+    credentials: 'same-origin',
+    redirect: 'manual'
+  })
+    .then((response) => {
+      return response.json()
     })
-      .then((response) => {
-        return response.json()
+    .then((json) => {
+      return json.checker_id ? 'DELETE' : 'PATCH'
+    })
+    .then((method) => {
+      fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': token
+        },
+        credentials: 'same-origin',
+        redirect: 'manual',
+        body: JSON.stringify(params)
       })
-      .then((json) => {
-        return json.checker_id ? 'DELETE' : 'PATCH'
-      })
-      .then((method) => {
-        fetch(url, {
-          method: method,
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'X-Requested-With': 'XMLHttpRequest',
-            'X-CSRF-Token': token
-          },
-          credentials: 'same-origin',
-          redirect: 'manual',
-          body: JSON.stringify(params)
+        .then((response) => {
+          return response.json()
         })
-          .then((response) => {
-            return response.json()
-          })
-          .then((json) => {
-            if (json.message) {
-              alert(json.message)
+        .then((json) => {
+          if (json.message) {
+            alert(json.message)
+          } else {
+            const id = json.checker_id
+            if (id !== null) {
+              toast('担当になりました。')
             } else {
-              const id = json.checker_id
-              if (id !== null) {
-                toast('担当になりました。')
-              } else {
-                toast('担当から外れました。')
-              }
+              toast('担当から外れました。')
             }
-          })
-          .catch((error) => {
-            console.warn(error)
-          })
-      })
+          }
+        })
+        .catch((error) => {
+          console.warn(error)
+        })
+    })
 
-    if (
-      event.currentTarget.className ===
-      'product-checker a-button is-block is-sm'
-    ) {
-      event.currentTarget.className =
-        'product-checker a-button is-block is-warning is-sm'
-      event.currentTarget.children[0].className = 'fas fa-times'
-      event.currentTarget.children[0].textContent = '担当から外れる'
-    } else {
-      event.currentTarget.className = 'product-checker a-button is-block is-sm'
-      event.currentTarget.children[0].className = 'fas fa-hand-paper'
-      event.currentTarget.children[0].textContent = '担当する'
-    }
+  if (
+    event.currentTarget.className === 'product-checker a-button is-block is-sm'
+  ) {
+    event.currentTarget.className =
+      'product-checker a-button is-block is-warning is-sm'
+    event.currentTarget.children[0].className = 'fas fa-times'
+    event.currentTarget.children[0].textContent = '担当から外れる'
+  } else {
+    event.currentTarget.className = 'product-checker a-button is-block is-sm'
+    event.currentTarget.children[0].className = 'fas fa-hand-paper'
+    event.currentTarget.children[0].textContent = '担当する'
   }
 }
 
-window.productChecker = productChecker
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.product-checker')
+  buttons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      const productId = this.dataset.productId
+      const currentUserId = this.dataset.currentUserId
+      checkInCharge(event, productId, currentUserId)
+    })
+  })
+})
