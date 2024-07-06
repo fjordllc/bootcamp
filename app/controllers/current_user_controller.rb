5460a66553810870f41313ae3ce5117134da511f
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-07-06 17:35:43 +0900

    コンフリクトの解消&無料フラグを使用していた箇所の削除

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -34,7 +34,7 @@ class CurrentUserController < ApplicationController
       :feed_url, :country_code, :subdivision_code, { discord_profile_attributes: %i[id account_name times_url] }
     ]
     if current_user.admin?
-      user_attribute.push(:retired_on, :graduated_on, :free, :github_collaborator, :auto_retire, :invoice_payment, :mentor, :subscription_id)
+      user_attribute.push(:retired_on, :graduated_on, :github_collaborator, :auto_retire, :invoice_payment, :mentor, :subscription_id)
     end
     params.require(:user).permit(user_attribute)
   end
