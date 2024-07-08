ed992786b08de49e3a8c19656e19e51ce85e82c3
machida <machida@fjord.jp>
2024-07-08 19:53:05 +0900

    分報フォームの動きを実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -58,6 +58,7 @@ import '../editor-selection-form.js'
 import '../user_mentor_memo.js'
 import '../invitation-url-updater.js'
 import '../micro-report-form-tabs.js'
+import '../micro-reports.js'
 
 import VueMounter from '../VueMounter.js'
 import Books from '../components/books.vue'
