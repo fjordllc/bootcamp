1fef89dbfbed701c17f8b9c8275a5313dbc7ea42
goruchan <ppcfr481@yahoo.co.jp>
2024-07-08 22:34:01 +0900

    schema.rb内の不要な変更を修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_04_140358) do
+ActiveRecord::Schema.define(version: 2024_05_02_051341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
