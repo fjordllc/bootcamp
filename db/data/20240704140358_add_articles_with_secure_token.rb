b5220ace248e9d7587f8471d327b4704c7c3a84f
goruchan <ppcfr481@yahoo.co.jp>
2024-07-08 23:13:35 +0900

    Rubocop 対応もれ修正

--- db/data/20240704140358_add_articles_with_secure_token.rb
+++ db/data/20240704140358_add_articles_with_secure_token.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class AddArticlesWithSecureToken < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
-  
   def up
     Article.where(wip: true).find_each do |article|
       article.update!(token: SecureRandom.urlsafe_base64)
