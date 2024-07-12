a82b7fd4b041a0ccf71d5c5b1475fe2395be24b7
machida <machida@fjord.jp>
2024-07-12 22:22:32 +0900

    法人利用ページの枠を作成
    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get "alumni_voices", to: "welcome#alumni_voices", as: "alumni_voices"
   get "training", to: "welcome#training", as: "training"
   get "faq", to: "welcome#faq", as: "faq"
+  get "job_support", to: "welcome#job_support", as: "job_support"
   get "tos", to: "welcome#tos", as: "tos"
   get "pp", to: "welcome#pp", as: "pp"
   get "law", to: "welcome#law", as: "law"
