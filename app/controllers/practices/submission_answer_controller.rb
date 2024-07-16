479035b2bde74d8e53c7e7aa2c27c4bbb38e34ae
masyuko0222 <masyuko0222@gmail.com>
2024-07-16 14:40:52 +0900

    find_practiceというメソッド定義をやめた
    ・２箇所の重複しかないため、DRYとしての意味が薄いため
    ・不要なメソッドを定義することで可読性が低くなってしまっていたため

--- app/controllers/practices/submission_answer_controller.rb
+++ app/controllers/practices/submission_answer_controller.rb
@@ -4,21 +4,17 @@ class Practices::SubmissionAnswerController < ApplicationController
   before_action :check_permission!, only: %i[show]
 
   def show
-    @practice = find_practice
+    @practice = Practice.find(params[:practice_id])
     @submission_answer = @practice.submission_answer
   end
 
   private
 
   def check_permission!
-    practice = find_practice
+    practice = Practice.find(params[:practice_id])
     submission_answer = practice.submission_answer || SubmissionAnswer.new(practice:)
     return if policy(submission_answer).show?
 
     redirect_to practice_path(practice), alert: 'プラクティスを修了するまで模範解答は見れません。'
-  end
-
-  def find_practice
-    Practice.find(params[:practice_id])
   end
 end
