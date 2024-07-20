e68a2804bda296ebcac95ba0ac9fad0ea3c91581
sochi419 <equipment2241@gmail.com>
2024-07-21 00:58:01 +0900

    enum削除、faq_categoryモデルを使った処理に変更

--- db/migrate/20240507123850_remove_foreign_key_from_faqs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignKeyFromFaqs < ActiveRecord::Migration[6.1]
-  def down
-    remove_foreign_key :faqs, column: :faqs_category_id
-  end
-end
--- db/migrate/20240507125931_change_faqs_category_id_to_category.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFaqsCategoryIdToCategory < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :faqs, :faqs_category_id, :category
-    change_column :faqs, :category, :integer, null: false
-  end
-end
