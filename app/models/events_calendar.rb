4fab41e32baf9162d80b2e08ebf1d7bbaa9c21b3
a-terumoto-gs <a.terumoto@genbasupport.com>
2024-07-23 12:02:00 +0900

    ical形式への変形処理を修正

--- app/models/events_calendar.rb
+++ app/models/events_calendar.rb
@@ -1,18 +1,34 @@
 # frozen_string_literal: true
 
 class EventsCalendar
-  attr_reader :events
-
   def initialize(user)
     @user = user
     @events = []
   end
 
   def fetch_events
-    upcoming_special_events = Event.fetch_events(@user)
+    upcoming_special_events = Event.fetch_upcoming_events(@user)
 
     participated_regular_events = RegularEvent.fetch_participated_regular_events(@user)
 
     @events = upcoming_special_events + participated_regular_events
+  end
+
+  def to_ical
+    cal = Icalendar::Calendar.new
+
+    @events.each do |event|
+      cal.event do |e|
+        e.dtstart     = Icalendar::Values::DateTime.new(event.start_at)
+        e.dtend       = Icalendar::Values::DateTime.new(event.end_at)
+        e.summary     = event.title
+        e.description = event.description
+        e.location    = event.respond_to?(:location) && event.location.present? ? event.location : nil
+        e.uid         = event.id.to_s
+      end
+    end
+
+    cal.publish
+    cal.to_ical
   end
 end
