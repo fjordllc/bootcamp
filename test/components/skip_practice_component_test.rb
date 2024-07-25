16aa9fc2d22c1e6056d3e250983f80fb53e94e38
ko <kyokucho1989@gmail.com>
2024-07-26 05:37:45 +0900

    componentのテスト方法変更
    フォームを含むcomponentのテスト方法がわかったのでcomponentテストを追加

---
+++ test/components/skip_practice_component_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SkipPracticeComponentTest < ViewComponent::TestCase
+  include ActionView::Helpers::FormHelper
+  include ActionView::Context
+
+  setup do
+    @user = users(:kensyu).extend(UserDecorator)
+    @form_builder = ActionView::Helpers::FormBuilder.new(:user, @user, ActionView::Base.empty, {})
+    @component = SkipPracticeComponent.new(form: @form_builder, user: @user)
+    render_inline(@component)
+  end
+
+  # user categories に所属するプラクティスが重複がなくなっていること
+  def test_user_categories
+    assert_selector 'label', text: '複数カテゴリに所属するプラクティス1', count: 1
+    assert_selector 'label', text: '複数カテゴリに所属するプラクティス2', count: 1
+  end
+end
