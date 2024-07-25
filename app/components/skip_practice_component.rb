c6ac5d59373268594b44dad670c77c04a6347bc0
ko <kyokucho1989@gmail.com>
2024-07-26 05:37:45 +0900

    componentのテスト方法変更
    フォームを含むcomponentのテスト方法がわかったのでcomponentテストを追加

--- app/components/skip_practice_component.rb
+++ app/components/skip_practice_component.rb
@@ -6,16 +6,21 @@ class SkipPracticeComponent < ViewComponent::Base
     @user = user
   end
 
-  def user_categories
-    display_practice_ids = []
+  def user_categories_with_uniq_practices
     user_categories = @user.course.categories
+    display_practice_ids = []
+    user_categories_with_uniq_practices = []
     user_categories.each do |category|
-      filtered_practices = category.practices.reject do |practice|
-        display_practice_ids.include?(practice.id)
+      copy_category = category.dup
+      category.practices.each do |practice|
+        if !display_practice_ids.include?(practice.id)
+          copy_category.practices << practice
+          display_practice_ids << practice.id
+        end
       end
-      category.practices = filtered_practices
-      display_practice_ids.concat(category.practice_ids)
+      user_categories_with_uniq_practices << copy_category
     end
-    user_categories
+
+    user_categories_with_uniq_practices
   end
 end
