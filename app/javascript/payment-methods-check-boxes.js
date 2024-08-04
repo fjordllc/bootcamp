f0a6b6692a0c30b28feefe16bf7f6a030a3631b4
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-08-04 17:09:55 +0900

    「研修生（支払い方法を選択）」が選択された場合のチェックボックスの挙動の制御を追加
    ・招待URL作成ページにて「研修生（支払い方法を選択）」が選択された場合、登録ページのチェックボックスの挙動が、
      (1)「請求書払い」と「クレジットカード払い」のいずれか一方しかチェックできない、
      (2)「クレジットカード払い」が選択されている時のみカード情報の入力フォームを表示する、
    となるように実装した。
    それに伴い、カード情報の入力フォームの表示/非表示の制御と2種類のチェックボックスの状態制御を同時に行う必要があったため、card-form-display-switcher.jsで行っていた処理をpayment-methods-check-boxes.jsに統合した。

--- app/javascript/payment-methods-check-boxes.js
+++ app/javascript/payment-methods-check-boxes.js
@@ -16,3 +16,39 @@ document.addEventListener('DOMContentLoaded', () => {
   const checkBoxes = [invoiceCheckBox, creditCardCheckBox]
   keepCheckBoxChecked(checkBoxes)
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const selectableInvoiceCheckBox = document.querySelector(
+    '.selectable-invoice-box'
+  )
+  const selectableCreditCardCheckBox = document.querySelector(
+    '.selectable-credit-card-box'
+  )
+  const checkBoxes = [selectableInvoiceCheckBox, selectableCreditCardCheckBox]
+  if (checkBoxes.includes(null)) {
+    return
+  }
+  const cardForm = document.getElementById('card')
+  checkBoxes.forEach((checkBox) => {
+    checkBox.addEventListener('click', (event) => {
+      if (
+        event.currentTarget === selectableCreditCardCheckBox &&
+        event.currentTarget.checked
+      ) {
+        cardForm.classList.remove('hidden')
+        selectableInvoiceCheckBox.checked = false
+      } else if (
+        event.currentTarget === selectableCreditCardCheckBox &&
+        !event.currentTarget.checked
+      ) {
+        cardForm.classList.add('hidden')
+      } else if (
+        event.currentTarget === selectableInvoiceCheckBox &&
+        event.currentTarget.checked
+      ) {
+        cardForm.classList.add('hidden')
+        selectableCreditCardCheckBox.checked = false
+      }
+    })
+  })
+})
