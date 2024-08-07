0461a8d6767d86426b4c31b0bd50aafb0fa003cf
motohiro-mm <polarbear.dx@gmail.com>
2024-08-07 21:14:57 +0900

    並列してるプロセスそれぞれに保存先を作成し設定

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -32,6 +32,11 @@ class ActiveSupport::TestCase
   teardown do
     ActiveStorage::Current.host = nil
   end
+
+  parallelize_setup do |i|
+    ActiveStorage::Blob.service.instance_variable_set(:@root, "#{ActiveStorage::Blob.service.root}/storage-#{i}")
+    ActiveStorage::Blob.services.fetch(:test_fixtures).instance_variable_set(:@root, "#{ActiveStorage::Blob.services.fetch(:test_fixtures).root}/fixtures-#{i}")
+  end
 end
 
 class ActionDispatch::IntegrationTest
