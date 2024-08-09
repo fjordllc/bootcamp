60c88dff0aabdd9b9a23b14c1ec695bdbf69f615
ko <kyokucho1989@gmail.com>
2024-08-10 08:14:55 +0900

    cateories_with_uniq_practicesメソッドをusermodelのメソッドへ移行
    このメソッドはComponent内だけでなく他のところでも使う可能性があるため
    不要なメソッド(practice_ids_in_category())の削除も行なった。

--- app/models/user.rb
+++ app/models/user.rb
@@ -768,8 +768,22 @@ class User < ApplicationRecord
     course.categories.pluck(:id)
   end
 
-  def practice_ids_in_category(category)
-    category.practices
+  def categories_with_uniq_practices
+    user_categories = course.categories
+    display_practice_ids = []
+    user_categories_with_uniq_practices = []
+    user_categories.each do |category|
+      copy_category = category.dup
+      category.practices.each do |practice|
+        if !display_practice_ids.include?(practice.id)
+          copy_category.practices << practice
+          display_practice_ids << practice.id
+        end
+      end
+      user_categories_with_uniq_practices << copy_category
+    end
+
+    user_categories_with_uniq_practices
   end
 
   def update_mentor_memo(new_memo)
