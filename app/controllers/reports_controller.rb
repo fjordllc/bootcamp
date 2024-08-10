be162b1d086b006ec84a99951090ff1d75b12507
su-su-su-su <suzukikoji0503@gmail.com>
2024-08-10 20:14:40 +0900

    ActiveRecord::RecordNotUniqueのエラーが出ないように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,9 +20,6 @@ class ReportsController < ApplicationController
 
   def show
     @products = @report.user.products.not_wip.order(published_at: :desc)
-    @recent_reports = Report.list.where(user_id: @report.user.id).limit(10)
-    Footprint.find_or_create_by(footprintable: @report, user: current_user) unless @report.user == current_user
-    @footprints = Footprint.fetch_for_resource(@report)
     respond_to do |format|
       format.html
       format.md
@@ -56,6 +53,7 @@ class ReportsController < ApplicationController
     @report.user = current_user
     set_wip
     canonicalize_learning_times(@report)
+
     if @report.save
       Newspaper.publish(:report_save, { report: @report })
       redirect_to redirect_url(@report), notice: notice_message(@report), flash: flash_contents(@report)
@@ -153,7 +151,7 @@ class ReportsController < ApplicationController
   def celebrating_count(report)
     return nil if report.wip
 
-    report_count = current_user.reports.not_wip.count
+    report_count = current_user.reports.count
     CELEBRATING_COUNTS.find { |count| count == report_count }
   end
 
