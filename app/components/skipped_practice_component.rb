dce9caf970f8714f20a0bfb55735230d31cc3fdd
ko <kyokucho1989@gmail.com>
2024-08-16 15:51:41 +0900

    スキップするプラクティスの数を表示する部分をメソッド化
    Viewファイル内のコメントをなくしても意味がわかるようにするため

--- app/components/skipped_practice_component.rb
+++ app/components/skipped_practice_component.rb
@@ -5,4 +5,13 @@ class SkippedPracticeComponent < ViewComponent::Base
     @f = form
     @user = user
   end
+
+  def skipped_practices_count(category)
+    skipped_practices_in_category = category.practice_ids & @user.practice_ids_skipped
+    if category.practices.empty?
+      '(0)'
+    else
+      "(#{skipped_practices_in_category.size}/#{category.practices.size})"
+    end
+  end
 end
