27831f28932ab20f650d7e2c29e89d0e40f489d0
Kuroneko <ken.aster8@gmail.com>
2024-08-18 23:02:07 +0900

    地域別ユーザーの一覧画面の非React版を作成
    - 既存コードの削除は未実施
    - ページネーションは未実施

--- app/controllers/users/areas_controller.rb
+++ app/controllers/users/areas_controller.rb
@@ -3,5 +3,6 @@
 class Users::AreasController < ApplicationController
   def index
     @number_of_users_by_region = Area.number_of_users_by_region
+    @users_group_by_areas = Area.sorted_users_group_by_areas
   end
 end
