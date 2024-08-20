6a8dbc3b6c540b4517f6789104c819530abb1f6c
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-08-21 01:20:21 +0900

    質問タイトルが長い場合、titleタグ内では省略されることを確認するテストを作成

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -30,7 +30,13 @@ class QuestionsTest < ApplicationSystemTestCase
   test 'show a question' do
     question = questions(:question8)
     visit_with_auth question_path(question), 'kimura'
-    assert_equal 'Q＆A: テストの質問 | FBC', title
+    assert_equal 'Q&A: テストの質問 | FBC', title
+  end
+
+  test 'title of the title tag is truncated' do
+    question = questions(:question16)
+    visit_with_auth question_path(question), 'kimura'
+    assert_selector 'title', text: 'Q&A: 長いタイトルの質問長いタイトルの質問長いタイトルの質問長いタイト... | FBC', visible: false
   end
 
   test 'create a question' do
