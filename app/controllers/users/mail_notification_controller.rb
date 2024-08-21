25abbb3013a87bec32558b7a9e06016793fa4533
motohiro-mm <polarbear.dx@gmail.com>
2024-08-21 12:42:40 +0900

    クラス名を修正、confirmをeditへ修正

--- app/controllers/users/mail_notification_controller.rb
+++ app/controllers/users/mail_notification_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class MailNotificationController < ApplicationController
+class Users::MailNotificationController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
   layout 'not_logged_in'
 
-  def confirm
+  def edit
     @user = User.find_by(unsubscribe_email_token: params[:token])
     redirect_to root_path, notice: 'メール配信停止にはTOKENが必要です。' unless @user
   end
