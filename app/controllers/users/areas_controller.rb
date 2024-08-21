2f03f529ba847301ee99d119b4646a710b6d23c6
Kuroneko <ken.aster8@gmail.com>
2024-08-21 21:14:42 +0900

    ページネーションを実装

--- app/controllers/users/areas_controller.rb
+++ app/controllers/users/areas_controller.rb
@@ -3,6 +3,6 @@
 class Users::AreasController < ApplicationController
   def index
     @number_of_users_by_region = Area.number_of_users_by_region
-    @users_group_by_areas = Area.sorted_users_group_by_areas
+    @users_group_by_areas = Kaminari.paginate_array(Area.sorted_users_group_by_areas).page(params[:page]).per(20)
   end
 end
