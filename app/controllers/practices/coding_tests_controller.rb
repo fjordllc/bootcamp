55791bb9674ec301fb2a929a54b701126367745b
komagata <komagata@gmail.com>
2024-08-22 10:14:47 +0900

    coding_test関連のpagerを確認するためのseedを追加

--- app/controllers/practices/coding_tests_controller.rb
+++ app/controllers/practices/coding_tests_controller.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 class Practices::CodingTestsController < ApplicationController
-  PAGER_NUMBER = 20
-
   def index
     @practice = Practice.find(params[:practice_id])
     @coding_tests = CodingTest.includes(:practice)
                               .where(practice_id: params[:practice_id])
                               .order(:position)
-                              .page(params[:page])
-                              .per(PAGER_NUMBER)
   end
 end
