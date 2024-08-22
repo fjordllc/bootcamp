6e51c56bd533c6e77152b37ece04d97bdc39d59b
komagata <komagata@gmail.com>
2024-08-22 10:14:47 +0900

    coding_test関連のpagerを確認するためのseedを追加

--- app/controllers/coding_tests/coding_test_submissions_controller.rb
+++ app/controllers/coding_tests/coding_test_submissions_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class CodingTests::CodingTestSubmissionsController < ApplicationController
+  PER_PAGE = 20
+
   before_action :set_coding_test
 
   def index
-    @coding_test_submissions = @coding_test.coding_test_submissions.page(params[:page])
+    @coding_test_submissions = @coding_test.coding_test_submissions
+                                           .page(params[:page])
+                                           .per(PER_PAGE)
   end
 
   def show
