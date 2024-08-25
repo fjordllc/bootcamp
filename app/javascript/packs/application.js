b8ee88fee78769fe07254d4ee348edebda1bac64
reckyy <srreki98@gmail.com>
2024-08-25 10:14:00 +0900

    不要なファイルを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -71,7 +71,6 @@ import User from '../components/user.vue'
 import Watches from '../components/watches.vue'
 import WatchToggle from '../components/watch-toggle.vue'
 import Footprints from '../components/footprints.vue'
-import QuestionAnswers from '../components/question-answers.vue'
 import SadReports from '../components/sad_reports.vue'
 import UserProducts from '../components/user-products.vue'
 import ActionCompletedButton from '../components/action-completed-button.vue'
@@ -86,7 +85,6 @@ mounter.addComponent(User)
 mounter.addComponent(Watches)
 mounter.addComponent(WatchToggle)
 mounter.addComponent(Footprints)
-mounter.addComponent(QuestionAnswers)
 mounter.addComponent(SadReports)
 mounter.addComponent(UserProducts)
 mounter.addComponent(ActionCompletedButton)
