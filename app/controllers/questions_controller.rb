40250be19c6994cfa714e4c23259be68a11c6467
reckyy <srreki98@gmail.com>
2024-08-30 18:40:25 +0900

    pager削除

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,7 +33,6 @@ class QuestionsController < ApplicationController
                           .includes(:correct_answer)
                           .latest_update_order
                           .limit(MAX_PRACTICE_QUESTIONS_DISPLAYED)
-    @answers = @question.answers.order(created_at: :asc).page(params[:page]).per(15)
     respond_to do |format|
       format.html
       format.md
