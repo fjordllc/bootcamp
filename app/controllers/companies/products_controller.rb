039c819a1735dc79c2f0c95b24b3c4218aa8c7b0
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-09-01 14:57:48 +0900

    Companies::ProductsControllerのindexアクションにおけるDBからのデータ取得の仕方を修正
    ・@productsを作成する際、Ruby側で加工することを前提としてDBからデータを取得する方法では余分なデータもDBから取ってくることになり、パフォーマンスの面で好ましくないため、なるべくSQLのみで必要最小限のデータを取ってくるよう修正した。

--- app/controllers/companies/products_controller.rb
+++ app/controllers/companies/products_controller.rb
@@ -3,8 +3,11 @@
 class Companies::ProductsController < ApplicationController
   def index
     @company = Company.find(params[:company_id])
-    products = @company.users.flat_map(&:products)
-    @users = products.flat_map { |p| p.comments.map(&:user) }
-    @products = Kaminari.paginate_array(products.sort).page(params[:page]).per(50)
+    @products = Product.joins(user: :company)
+                       .where('company_id = ?', @company.id)
+                       .order('id ASC')
+                       .page(params[:page])
+                       .per(50)
+    @users = @products.flat_map { |p| p.comments.map(&:user) }
   end
 end
