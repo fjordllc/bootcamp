9cb8c63d490424ff4be0015fe8c894ce99724452
Kuroneko <ken.aster8@gmail.com>
2024-09-08 17:49:09 +0900

    分報一覧ページに遷移時に、最新の投稿の場所に遷移するように修正

--- app/javascript/micro-reports.js
+++ app/javascript/micro-reports.js
@@ -1,13 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function () {
   const textarea = document.getElementById('js-micro-report-textarea')
   const microReports = document.getElementById('js-micro-reports')
   const form = document.getElementById('js-micro-report-form')
-  const scrollTarget = document.getElementById('js-scroll')
   let prevHeight = 0
-
-  // Initially set padding to 0 and add transition
-  microReports.style.paddingBottom = '0px'
-  microReports.style.transition = 'padding-bottom 0.3s ease'
 
   const adjustPadding = () => {
     const height = form.scrollHeight
@@ -18,25 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   if (form) {
-    // Call adjustPadding on page load to set initial padding
-    adjustPadding()
-
     // Event listener for textarea input
     textarea.addEventListener('input', debounce(adjustPadding, 100)) // Update padding on input with debounce
   }
-
-  // Function to handle the smooth scroll to the bottom
-  function scrollToBottom(element) {
-    setTimeout(() => {
-      element.scrollTo({
-        top: element.scrollHeight,
-        behavior: 'smooth'
-      })
-    }, 200) // Delaying the scroll for 100 milliseconds
-  }
-
-  // Scroll to the bottom of the scrollTarget div as soon as the page loads
-  scrollToBottom(scrollTarget)
 
   // Debounce function to limit the rate of execution of a function
   function debounce(func, wait) {
