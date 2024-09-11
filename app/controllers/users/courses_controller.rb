e25a47290e84b428bffe3e738cd08c564fd955ac
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-09-11 17:00:11 +0900

    ユーザーの一覧表示部分を実装
    ・既存のユーザー一覧の構造を流用するため、app/views/users/_user_list.html.slimにおいてpaginateメソッドの第二引数（params）を削除した。（元の記述のままでは、ページネートのリンクがUsersController#indexに固定されてしまい、今回追加したUser::CoursesControllerへのリンクにならないため。）
    なお、paginateメソッドは現在のページ情報を使用してページネートのリンクを生成するため、上記の第二引数を削除してコントローラーとアクションを明示しなくなっても適切なリンクを生成してくれる。
    参考：https://stackoverflow.com/questions/11681915/where-does-kaminari-calculate-its-page-url-links

--- app/controllers/users/courses_controller.rb
+++ app/controllers/users/courses_controller.rb
@@ -3,11 +3,17 @@
 class Users::CoursesController < ApplicationController
   ALLOWED_TARGETS = %w[rails_course front_end_course other_courses].freeze
 
+  PAGER_NUMBER = 24
+
   def index
     @target = ALLOWED_TARGETS.include?(params[:target]) ? params[:target] : ALLOWED_TARGETS.first
     course_names = { rails_course: 'Railsエンジニア',
                      front_end_course: 'フロントエンドエンジニア',
                      other_courses: %w[Unityゲームエンジニア iOSエンジニア] }
-    @users = User.by_course(course_names[@target.to_sym]).students_and_trainees
+    target_users = User.by_course(course_names[@target.to_sym]).students_and_trainees
+    @users = target_users
+             .page(params[:page]).per(PAGER_NUMBER)
+             .preload(:avatar_attachment, :course, :taggings)
+             .order(updated_at: :desc)
   end
 end
