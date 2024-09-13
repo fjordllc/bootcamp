5cb712a788eab99da54973300de02524d1791155
Kuroneko <ken.aster8@gmail.com>
2024-09-13 18:11:11 +0900

    マジックナンバーを定数化

--- app/controllers/users/areas_controller.rb
+++ app/controllers/users/areas_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 class Users::AreasController < ApplicationController
+  PAGER_NUMBER = 15
+
   def index
     @number_of_users_by_region = Area.number_of_users_by_region
-    @sorted_user_groups_by_area_user_num = Kaminari.paginate_array(Area.sorted_user_groups_by_area_user_num).page(params[:page]).per(15)
+    @sorted_user_groups_by_area_user_num = Kaminari.paginate_array(Area.sorted_user_groups_by_area_user_num).page(params[:page]).per(PAGER_NUMBER)
   end
 
   def show
     @area = params[:area]
-    @users = User.by_area(@area).page(params[:page]).per(15)
+    @users = User.by_area(@area).page(params[:page]).per(PAGER_NUMBER)
     @number_of_users_by_region = Area.number_of_users_by_region
   end
 end
