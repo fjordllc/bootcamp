8b5725c58cdee29c9484c9ba99487a7ff51cb882
Kuroneko <ken.aster8@gmail.com>
2024-09-15 00:16:16 +0900

    product.vueをViewComponent化

--- app/components/products/product_component.rb
+++ app/components/products/product_component.rb
@@ -1,7 +1,38 @@
 # frozen_string_literal: true
 
 class Products::ProductComponent < ViewComponent::Base
-  def initialize(product:)
+  def initialize(product:, is_mentor:, current_user_id:, display_user_icon: false)
     @product = product
+    @is_mentor = is_mentor
+    @current_user_id = current_user_id
+    @display_user_icon = display_user_icon
+  end
+
+  def role_class
+    "is-#{@product.user.primary_role}"
+  end
+
+  def practice_title
+    "#{@product.practice.title}の提出物"
+  end
+
+  def dashboard?
+    request.path == root_path
+  end
+
+  def not_responded_sign?
+    @product.comments.empty? ||
+      (@product.self_last_commented_at &&
+        (!@product.mentor_last_commented_at || @product.self_last_commented_at > @product.mentor_last_commented_at))
+  end
+
+  def until_next_elapsed_days
+    elapsed_times = calc_elapsed_times
+    ((elapsed_times.ceil - elapsed_times) * 24).floor
+  end
+
+  def calc_elapsed_times
+    time = @product.published_at || @product.created_at
+    ((Time.zone.now - time) / 1.day).to_f
   end
 end
