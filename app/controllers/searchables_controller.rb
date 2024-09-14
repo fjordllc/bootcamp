8344afd17948082ef46ac9ee430ad3eef47a1276
kurumadaisuke <kuruma0909@gmail.com>
2024-09-14 21:31:23 +0900

    エラー時における「failed to load」の表示を修正

--- app/controllers/searchables_controller.rb
+++ app/controllers/searchables_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class SearchablesController < ApplicationController
-  def index; end
+  def index
+    @per = 50
+    @word = params[:word]
+    @error = nil
+    # @data = fetch_searchables(params[:word], @page, @per)
+    # @error = @data.nil?
+  end
 end
