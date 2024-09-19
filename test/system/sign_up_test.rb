3e5cfb9ae44d174d58bd8d7a8d230237762fb116
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-09-19 18:21:26 +0900

    メンター登録画面から登録可能であることをテストするテストを作成

--- test/system/sign_up_test.rb
+++ test/system/sign_up_test.rb
@@ -135,6 +135,33 @@ class SignUpTest < ApplicationSystemTestCase
     assert User.find_by(email:).adviser?
   end
 
+  test 'sign up as mentor' do
+    visit '/users/new?role=mentor'
+
+    email = 'shunka@example.com'
+
+    within 'form[name=user]' do
+      fill_in 'user[login_name]', with: 'shunka'
+      fill_in 'user[email]', with: email
+      fill_in 'user[name]', with: 'テスト 春夏'
+      fill_in 'user[name_kana]', with: 'テスト シュンカ'
+      fill_in 'user[description]', with: 'テスト春夏です。'
+      fill_in 'user[password]', with: 'testtest'
+      fill_in 'user[password_confirmation]', with: 'testtest'
+      select '会社員', from: 'user[job]'
+      find('label', text: 'Mac（Intel チップ）').click
+      first('.choices__inner').click
+      find('.choices__list--dropdown').click
+      find('.choices__list').click
+      find('label', text: 'アンチハラスメントポリシーに同意').click
+      find('label', text: '利用規約に同意').click
+    end
+
+    click_button 'メンター登録'
+    assert_text 'サインアップメールをお送りしました。メールからサインアップを完了させてください。'
+    assert User.find_by(email:).mentor?
+  end
+
   test 'sign up as trainee' do
     visit '/users/new?role=trainee'
 
