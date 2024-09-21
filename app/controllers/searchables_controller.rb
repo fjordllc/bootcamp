451e78ce5216f14bb682ef368c1dc2fd4c3c4ac2
kurumadaisuke <kuruma0909@gmail.com>
2024-09-21 20:25:14 +0900

    一通りのviewを書いてみた

--- app/controllers/searchables_controller.rb
+++ app/controllers/searchables_controller.rb
@@ -4,9 +4,15 @@ class SearchablesController < ApplicationController
   def index
     @per = 50
     @word = params[:word]
+    @page_number = params[:page].present? ? params[:page].to_i : 1
     @error = nil
-    @data = true
-    # @data = fetch_searchables(params[:word], @page, @per)
-    # @error = @data.nil?
+
+    begin
+      response = RestClient.get('/api/searchables.json', { params: { word: @word, page: @page_number, per: @per } })
+      @data = JSON.parse(response.body)
+    rescue RestClient::ExceptionWithResponse => e
+      @error = e.response
+      @data = nil
+    end
   end
 end
