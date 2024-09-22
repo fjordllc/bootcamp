b761d8c2566db0f2c21a86ab41a466f88ac1040c
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-09-22 20:47:30 +0900

    コンフリクトを解消した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_30_042450) do
+ActiveRecord::Schema.define(version: 2024_08_21_190009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
