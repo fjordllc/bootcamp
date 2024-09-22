acc71b0840d186f507e871ddb3a60ef01559a5c3
Kuroneko <ken.aster8@gmail.com>
2024-09-22 20:26:54 +0900

    product_checker.vueをViewComponent化

--- app/components/products/product_checker_component.rb
+++ app/components/products/product_checker_component.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class Products::ProductCheckerComponent < ViewComponent::Base
-  def initialize(checker_id:, checker_name:, checker_avatar:, current_user_id:, product_id:)
+  def initialize(checker_id:, checker_name:, current_user_id:, product_id:, checker_avatar:)
     @checker_id = checker_id
     @checker_name = checker_name
-    @checker_avatar = checker_avatar
     @current_user_id = current_user_id
     @product_id = product_id
+    @checker_avatar = checker_avatar
+  end
+
+  def button_label
+    @checker_id ? '担当から外れる' : '担当する'
+  end
+
+  def url
+    '/api/products/checker'
   end
 end
