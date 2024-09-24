55094b9a482d4192568960e3e1dda8d9f1cd89eb
Kuroneko <ken.aster8@gmail.com>
2024-09-24 19:07:33 +0900

    使用していないフラグを削除

--- app/components/products/product_component.rb
+++ app/components/products/product_component.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Products::ProductComponent < ViewComponent::Base
-  def initialize(product:, is_mentor:, current_user_id:, display_user_icon: false)
+  def initialize(product:, is_mentor:, current_user_id:)
     @product = product
     @is_mentor = is_mentor
     @current_user_id = current_user_id
-    @display_user_icon = display_user_icon
   end
 
   def role_class
