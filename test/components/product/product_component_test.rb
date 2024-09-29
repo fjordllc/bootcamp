d44dc58faea7b52d7e0783af19db3e2dbdceec9b
Kuroneko <ken.aster8@gmail.com>
2024-09-29 15:53:59 +0900

    product_componentのテストを作成

---
+++ test/components/product/product_component_test.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Products::ProductComponentTest < ViewComponent::TestCase
+  def setup
+    @current_user = users(:komagata).extend(UserDecorator)
+    @is_mentor = true
+  end
+
+  def test_default
+    unassigned_product = decorate_product_user(products(:product6))
+    render_inline(Products::ProductComponent.new(
+                    product: unassigned_product,
+                    is_mentor: @is_mentor,
+                    current_user_id: @current_user_id
+                  ))
+
+    assert_selector '.is-graduate'
+    assert_selector '.card-list-item__user-icon'
+    assert_text unassigned_product.user.long_name
+    assert_text "#{unassigned_product.practice.title}の提出物"
+    assert_text I18n.l(unassigned_product.published_at)
+    assert_text I18n.l(unassigned_product.updated_at)
+  end
+
+  def test_render_wip_badge_when_product_is_wip
+    wip_product = decorate_product_user(products(:product5))
+    render_inline(Products::ProductComponent.new(
+                    product: wip_product,
+                    is_mentor: @is_mentor,
+                    current_user_id: @current_user_id
+                  ))
+
+    assert_selector '.a-list-item-badge.is-wip'
+    assert_text 'WIP'
+    assert_text '提出物作成中'
+  end
+
+  def test_render_published_at_when_product_is_published
+    unassigned_product = decorate_product_user(products(:product6))
+    render_inline(Products::ProductComponent.new(
+                    product: unassigned_product,
+                    is_mentor: @is_mentor,
+                    current_user_id: @current_user_id
+                  ))
+
+    assert_selector '.a-meta', text: '提出'
+    assert_text I18n.l(unassigned_product.published_at)
+  end
+
+  def test_render_created_at_when_product_is_not_published
+    unpublished_product = decorate_product_user(products(:product66))
+    render_inline(Products::ProductComponent.new(
+                    product: unpublished_product,
+                    is_mentor: @is_mentor,
+                    current_user_id: @current_user_id
+                  ))
+
+    assert_selector '.a-meta', text: '提出'
+    assert_text I18n.l(unpublished_product.created_at)
+  end
+
+  private
+
+  def decorate_product_user(product)
+    product.user = product.user.extend(UserDecorator)
+    product
+  end
+end
