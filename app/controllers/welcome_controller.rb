8a63b84b1bc1f1f843cc47e9a24d9206ff4728f2
machida <machida@fjord.jp>
2024-09-30 23:19:02 +0900

    :cop:

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@
 class WelcomeController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
   layout 'lp'
+  DEFAULT_COURSE = 'Railsエンジニア'
 
   def index
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
@@ -12,17 +13,9 @@ class WelcomeController < ApplicationController
 
   def faq; end
 
-  def alumni_voices; end
-
   def training; end
 
-  def practices
-    render layout: 'lp'
-  end
-
-  def job_support; end
-
-  def certified_reskill_courses; end
+  def practices; end
 
   def tos; end
 
@@ -31,8 +24,4 @@ class WelcomeController < ApplicationController
   def law; end
 
   def coc; end
-
-  def courses
-    @courses = Course.order(:created_at)
-  end
 end
