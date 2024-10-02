588a7d9c58a5b1d90bce03a832c6d92495eb9f1e
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-10-02 16:29:51 +0900

    @productsを取得する際の構文を変更
    ・whereメソッドで生のSQLの構文で条件を指定していたが、joinsで取得した関連テーブルをハッシュ構文で指定することが可能であり、可読性等の観点からそちらに修正した。
    ・各productにコメントしたユーザーを取得する際にN+1が発生していたため修正した。

--- app/controllers/companies/products_controller.rb
+++ app/controllers/companies/products_controller.rb
@@ -4,8 +4,9 @@ class Companies::ProductsController < ApplicationController
   def index
     @company = Company.find(params[:company_id])
     @products = Product.joins(user: :company)
-                       .where('company_id = ?', @company.id)
-                       .order('id ASC')
+                       .includes(comments: :user)
+                       .where({ users: { company_id: @company.id } })
+                       .order(id: :asc)
                        .page(params[:page])
                        .per(50)
     @commented_users = @products.map { |product| [product.id, product.comments.map(&:user)] }.to_h
