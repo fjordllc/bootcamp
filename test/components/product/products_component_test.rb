79fbbae257f784f16e5fbb1508db33883107af7e
Kuroneko <ken.aster8@gmail.com>
2024-10-06 23:46:06 +0900

    テストを修正

--- test/components/product/products_component_test.rb
+++ test/components/product/products_component_test.rb
@@ -8,18 +8,25 @@ class Products::ProductsComponentTest < ViewComponent::TestCase
     DecoratorHelper.auto_decorate(User)
     @current_user = users(:komagata)
     @is_mentor = true
+    @reply_warning_days = 4
   end
 
   def test_default
-    products = [products(:product6), products(:product7), products(:product8), products(:product9), products(:product10)]
+    products = [products(:product1), products(:product2), products(:product3), products(:product4), products(:product5), products(:product6),
+                products(:product7), products(:product8), products(:product9)]
     products_grouped_by_elapsed_days = group_by_elapsed_days(products)
     render_inline(Products::ProductsComponent.new(products:,
                                                   products_grouped_by_elapsed_days:,
                                                   is_mentor: @is_mentor,
-                                                  current_user_id: @current_user.id))
-    assert_selector '.card-header.a-elapsed-days.is-reply-deadline#7days-elapsed', text: '7日以上経過（3）'
-    assert_selector '.card-header.a-elapsed-days.is-reply-alert#6days-elapsed', text: '6日経過（1）'
-    assert_selector '.card-header.a-elapsed-days.is-reply-warning#5days-elapsed', text: '5日経過（1）'
+                                                  current_user_id: @current_user.id,
+                                                  reply_warning_days: @reply_warning_days))
+    assert_selector '.is-reply-deadline#6days-elapsed', text: '6日以上経過（3）'
+    assert_selector '.is-reply-alert#5days-elapsed', text: '5日経過（1）'
+    assert_selector '.is-reply-warning#4days-elapsed', text: '4日経過（1）'
+    assert_selector '#3days-elapsed', text: '3日経過（1）'
+    assert_selector '#2days-elapsed', text: '2日経過（1）'
+    assert_selector '#1days-elapsed', text: '1日経過（1）'
+    assert_selector '#0days-elapsed', text: '今日提出（1）'
   end
 
   def test_any_products
@@ -28,45 +35,50 @@ class Products::ProductsComponentTest < ViewComponent::TestCase
     render_inline(Products::ProductsComponent.new(products:,
                                                   products_grouped_by_elapsed_days:,
                                                   is_mentor: @is_mentor,
-                                                  current_user_id: @current_user.id))
+                                                  current_user_id: @current_user.id,
+                                                  reply_warning_days: @reply_warning_days))
     assert_text '提出物はありません'
   end
 
-  def test_any_products_5days_elapsed
-    products_within_5days = [products(:product2), products(:product3), products(:product4), products(:product5)]
-    products_grouped_by_elapsed_days = group_by_elapsed_days(products_within_5days)
-    render_inline(Products::ProductsComponent.new(products: products_within_5days,
+  def test_any_products_elapsed_reply_warning_days
+    products_within_reply_warning_days = [products(:product2), products(:product3), products(:product4)]
+    products_grouped_by_elapsed_days = group_by_elapsed_days(products_within_reply_warning_days)
+    render_inline(Products::ProductsComponent.new(products: products_within_reply_warning_days,
                                                   products_grouped_by_elapsed_days:,
                                                   is_mentor: @is_mentor,
-                                                  current_user_id: @current_user.id))
-    assert_text '5日経過した提出物はありません'
+                                                  current_user_id: @current_user.id,
+                                                  reply_warning_days: @reply_warning_days))
+    assert_text '4日経過した提出物はありません'
   end
 
-  def any_products_almost_passed_5days
-    products_passed_5days = [products(:product6), products(:product7), products(:product8)]
-    products_grouped_by_elapsed_days = group_by_elapsed_days(products_passed_5days)
-    render_inline(Products::ProductsComponent.new(products: products_passed_5days,
+  def any_products_almost_passed_reply_warning_days
+    products_passed_reply_warning_days = [products(:product5), products(:product6), products(:product7)]
+    products_grouped_by_elapsed_days = group_by_elapsed_days(products_passed_reply_warning_days)
+    render_inline(Products::ProductsComponent.new(products: products_passed_reply_warning_days,
                                                   products_grouped_by_elapsed_days:,
                                                   is_mentor: @is_mentor,
-                                                  current_user_id: @current_user.id))
-    assert_text 'しばらく5日経過に到達する提出物はありません。'
+                                                  current_user_id: @current_user.id,
+                                                  reply_warning_days: @reply_warning_days))
+    assert_text 'しばらく4日経過に到達する提出物はありません。'
   end
 
-  def test_products_almost_passed_5days
-    products_passed_5days = [products(:product6)]
-    products_almost_passed_5days = [products(:product70), products(:product71)]
-    products = products_passed_5days + products_almost_passed_5days
+  def test_products_almost_passed_reply_warning_days
+    products_passed_reply_warning_days = [products(:product5)]
+    products_almost_passed_reply_warning_days = [products(:product70), products(:product71)]
+    products = products_passed_reply_warning_days + products_almost_passed_reply_warning_days
     products_grouped_by_elapsed_days = group_by_elapsed_days(products)
     render_inline(Products::ProductsComponent.new(products:,
                                                   products_grouped_by_elapsed_days:,
                                                   is_mentor: @is_mentor,
-                                                  current_user_id: @current_user.id))
-    assert_text '2件の提出物が、8時間以内に5日経過に到達します。'
+                                                  current_user_id: @current_user.id,
+                                                  reply_warning_days: @reply_warning_days))
+    assert_text '2件の提出物が、8時間以内に4日経過に到達します。'
   end
 
   private
 
   def group_by_elapsed_days(products)
-    products.group_by { |product| product.elapsed_days >= 7 ? 7 : product.elapsed_days }
+    reply_deadline_days = @reply_warning_days + 2
+    products.group_by { |product| product.elapsed_days >= reply_deadline_days ? reply_deadline_days : product.elapsed_days }
   end
 end
