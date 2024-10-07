41a235bf46b683c122bca0ed15a0c79aadf4309b
kurumadaisuke <kuruma0909@gmail.com>
2024-10-07 10:07:11 +0900

    途中経過 Javascriptでfetch関数使用

--- app/controllers/searchables_controller.rb
+++ app/controllers/searchables_controller.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'uri'
+require 'json'
+
 class SearchablesController < ApplicationController
   def index
     @per = 50
     @word = params[:word]
     @page_number = params[:page].present? ? params[:page].to_i : 1
-    request_params = params.permit(:document_type, :word).to_h.to_query
+    request_params = params.permit(:document_type, :word, :page).to_h.to_query
     @url = "/api/searchables.json?#{request_params}"
-    @error = nil
-    # @data = []
   end
 end
