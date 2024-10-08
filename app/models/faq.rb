c31b63d51988dc006c0ffe8c1cf509892b527198
machida <machida@fjord.jp>
2024-10-09 00:41:39 +0900

    新デザインを適用し、とりあえず動くようにした
    動かなくなったFAQの順番移動を動くようにした

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -3,8 +3,9 @@
 class FAQ < ApplicationRecord
   validates :answer, presence: true, uniqueness: { scope: :question }
   validates :question, presence: true, uniqueness: true
+  validates :faq_category_id, presence: true
 
   default_scope -> { order(:position) }
-  belongs_to :faqs_category
+  belongs_to :faq_category
   acts_as_list
 end
