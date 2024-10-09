431f18383ee67a150157b8397c3748f381764fa7
machida <machida@fjord.jp>
2024-10-09 22:29:22 +0900

    FAQデザイン更新
    :cop:
    
    :cop:
    
    faqs categoryをfaq categoryに修正
    
    FaqをFAQに修正
    
    FaqをFAQに修正
    
    FaqをFAQに修正
    
    デザイン変更に伴うテスト変更’
    
    デザイン変更に伴うseed変更’
    
    並べ替えを動くようにした
    
    不要なカラムを削除
    
    :cop:
    
    ATCH リクエストで faq_category_id パラメータが送信されていなかったのを修正
    
    リファクタ
    
    testでのカテゴリーの指定方法を修正

--- test/system/faq_test.rb
+++ test/system/faq_test.rb
@@ -5,13 +5,13 @@ require 'application_system_test_case'
 class FAQTest < ApplicationSystemTestCase
   test 'show listing FAQs' do
     visit faq_path
-    assert_selector '.faqs-item', count: FAQ.all.size
+    assert_selector '.lp-faq', count: FAQ.all.size
   end
 
   test 'show listing FAQs by category' do
     visit '/faq?category=study_environment'
 
     study_environment_id = FAQCategory.find_by(name: 'study_environment').id
-    assert_selector '.faqs-item', count: FAQ.where(faq_category_id: study_environment_id).size
+    assert_selector '.lp-faq', count: FAQ.where(faq_category_id: study_environment_id).size
   end
 end
