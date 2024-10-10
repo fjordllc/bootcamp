572eb4e30943f70f0d902b9affe3108abfb890ba
machida <machida@fjord.jp>
2024-10-10 13:58:03 +0900

    FaqをFAQに修正

--- db/migrate/20240506064858_rename_faqs_categories_id_to_faqs_category_id.rb
+++ db/migrate/20240506064858_rename_faqs_categories_id_to_faqs_category_id.rb
@@ -1,4 +1,4 @@
-class RenameFAQCategoriesIdToFaqCategoryId < ActiveRecord::Migration[6.1]
+class RenameFAQCategoriesIdToFAQCategoryId < ActiveRecord::Migration[6.1]
   def change
     rename_column :faqs, :faq_categories_id, :faq_category_id
   end
