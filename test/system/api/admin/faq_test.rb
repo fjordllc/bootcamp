549e0a5bccf65f04dcb21720cd896c5f30276098
machida <machida@fjord.jp>
2024-10-10 17:27:11 +0900

    デザイン変更に伴うテスト変更’

--- test/system/api/admin/faq_test.rb
+++ test/system/api/admin/faq_test.rb
@@ -5,8 +5,8 @@ require 'application_system_test_case'
 class API::Admin::FaqTest < ApplicationSystemTestCase
   test 'sort FAQ' do
     visit faq_path
-    assert_equal all('h3.faqs-item__title').first.text, '学習を終えるのにどれくらいの時間がかかりますか？'
-    assert_equal all('h3.faqs-item__title').last.text, 'フィヨルドブートキャンプを利用していることを誰にも知られたくないのですが、匿名で参加はできますか？'
+    assert_equal all('h3.lp-faq__title').first.text, '学習を終えるのにどれくらいの時間がかかりますか？'
+    assert_equal all('h3.lp-faq__title').last.text, 'フィヨルドブートキャンプを利用していることを誰にも知られたくないのですが、匿名で参加はできますか？'
 
     visit_with_auth admin_faqs_path, 'komagata'
     source = all('.js-grab.a-grab').first
@@ -14,13 +14,13 @@ class API::Admin::FaqTest < ApplicationSystemTestCase
     source.drag_to(target)
 
     visit faq_path
-    assert_equal all('h3.faqs-item__title').last.text, '学習を終えるのにどれくらいの時間がかかりますか？'
+    assert_equal all('h3.lp-faq__title').last.text, '学習を終えるのにどれくらいの時間がかかりますか？'
   end
 
   test "doesn't affect the order of other FAQs" do
     visit faq_path
-    assert_equal all('h3.faqs-item__title')[0].text, '学習を終えるのにどれくらいの時間がかかりますか？'
-    assert_equal all('h3.faqs-item__title')[1].text, '途中で辞めることは可能ですか？'
+    assert_equal all('h3.lp-faq__title')[0].text, '学習を終えるのにどれくらいの時間がかかりますか？'
+    assert_equal all('h3.lp-faq__title')[1].text, '途中で辞めることは可能ですか？'
 
     visit_with_auth admin_faqs_path, 'komagata'
     source = all('.js-grab')[7] # faq8
@@ -28,7 +28,7 @@ class API::Admin::FaqTest < ApplicationSystemTestCase
     source.drag_to(target)
 
     visit faq_path
-    assert_equal all('h3.faqs-item__title')[0].text, '学習を終えるのにどれくらいの時間がかかりますか？'
-    assert_equal all('h3.faqs-item__title')[1].text, '途中で辞めることは可能ですか？'
+    assert_equal all('h3.lp-faq__title')[0].text, '学習を終えるのにどれくらいの時間がかかりますか？'
+    assert_equal all('h3.lp-faq__title')[1].text, '途中で辞めることは可能ですか？'
   end
 end
