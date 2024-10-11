14fc42e6dc38b7868608d2a58770d36ce7beebdb
ko <kyokucho1989@gmail.com>
2024-10-12 05:37:06 +0900

    カテゴリごとの修了を判定するメソッドを移行

--- test/models/user_course_practice_test.rb
+++ test/models/user_course_practice_test.rb
@@ -4,12 +4,13 @@ require 'test_helper'
 
 class UserCoursePracticeTest < ActiveSupport::TestCase
   setup do
-    @user_course_practice = UserCoursePractice.new(users(:kensyu))
+    @user_course_practice_kensyu = UserCoursePractice.new(users(:kensyu))
+    @user_course_practice_kimura = UserCoursePractice.new(users(:kimura))
   end
 
   test '#categories_for_skip_practices' do
     user = users(:kensyu)
-    categories = @user_course_practice.categories_for_skip_practice
+    categories = @user_course_practice_kensyu.categories_for_skip_practice
     category_for_skip_practice = categories.select { |category| category.name == 'Ruby on Rails(Rails 6.1版)' }.first
     assert_equal 0, category_for_skip_practice.practices.size
 
@@ -18,7 +19,7 @@ class UserCoursePracticeTest < ActiveSupport::TestCase
   end
 
   test '#uniq_practice_ids' do
-    uniq_practices_ids = @user_course_practice.uniq_practice_ids
+    uniq_practices_ids = @user_course_practice_kensyu.uniq_practice_ids
 
     assert_equal uniq_practices_ids.size, uniq_practices_ids.uniq.size
   end
@@ -26,7 +27,7 @@ class UserCoursePracticeTest < ActiveSupport::TestCase
   test '#filter_category_by_practice_ids' do
     category = categories(:category4)
     practice_ids = category.practice_ids[0..3] << 0
-    filterd_category, left_ids = @user_course_practice.filter_category_by_practice_ids(category, practice_ids)
+    filterd_category, left_ids = @user_course_practice_kensyu.filter_category_by_practice_ids(category, practice_ids)
 
     assert_equal filterd_category.practices.size, 4
     assert_equal left_ids, [0]
@@ -35,11 +36,16 @@ class UserCoursePracticeTest < ActiveSupport::TestCase
   test '#sorted_practices' do
     user = users(:kensyu)
     practices = user.course.practices.reverse
-    orderd_practices = @user_course_practice.sorted_practices
+    orderd_practices = @user_course_practice_kensyu.sorted_practices
     assert_equal orderd_practices.last.id, practices.first.id
   end
 
   test '#skipped_practice_ids' do
-    assert_includes(@user_course_practice.skipped_practice_ids, practices(:practice8).id)
+    assert_includes(@user_course_practice_kensyu.skipped_practice_ids, practices(:practice8).id)
+  end
+
+  test '#completed_practices_size_by_category' do
+    category2 = categories(:category2)
+    assert_equal 1, @user_course_practice_kimura.completed_practices_size_by_category[category2.id]
   end
 end
