fa42bc0a6ead4a524bc33c9e2c3ff84fd97faaf8
komagata <komagata@gmail.com>
2024-10-16 12:49:24 +0900

    コーディングテスト用のタブを追加

--- app/models/coding_test_submission.rb
+++ app/models/coding_test_submission.rb
@@ -4,5 +4,7 @@ class CodingTestSubmission < ApplicationRecord
   belongs_to :coding_test
   belongs_to :user
 
+  has_one :practice, through: :coding_test
+
   validates :coding_test_id, uniqueness: { scope: :user_id }
 end
