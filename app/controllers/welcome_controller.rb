5c5697f51aa21f65487d86a5209744bf0ba2b0e3
machida <machida@fjord.jp>
2024-10-18 19:33:59 +0900

    URLとファイルのパスを変更

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,12 @@ class WelcomeController < ApplicationController
   layout 'lp'
   DEFAULT_COURSE = 'Railsエンジニア'
 
+  # 管理者チェックを特定のアクションにだけ適用
+  before_action :require_admin, only: %i[
+    rails_developer_course
+    rails_developer_course_regulations
+  ]
+
   def index
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
   end
@@ -30,4 +36,12 @@ class WelcomeController < ApplicationController
   def law; end
 
   def coc; end
+
+  def rails_developer_course
+    render template: 'welcome/certified_reskill_courses/rails_developer_course/index'
+  end
+
+  def rails_developer_course_regulations
+    render template: 'welcome/certified_reskill_courses/rails_developer_course/regulations'
+  end
 end
