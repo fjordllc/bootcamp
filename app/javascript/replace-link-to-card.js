d0e6d6e702bb3af703c6f089b189488514349b8a
machida <machida@fjord.jp>
2024-10-22 03:21:29 +0900

    不要なコメントを削除

--- app/javascript/replace-link-to-card.js
+++ app/javascript/replace-link-to-card.js
@@ -2,7 +2,7 @@ import CSRF from 'csrf'
 import { debounce } from './debounce.js'
 
 export default (textareas) => {
-  replaceLinkToCard() // 初回実行
+  replaceLinkToCard()
   const debouncedReplace = debounce(replaceLinkToCard, 300)
 
   Array.from(textareas).forEach((textarea) => {
@@ -31,7 +31,7 @@ const replaceLinkToCard = () => {
       )
     }
 
-    targetLink.classList.add('processed') // 再処理を防ぐためにフラグを追加
+    targetLink.classList.add('processed')
   })
 }
 
