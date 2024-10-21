cdbea612bf8b8a51f2d1846e5c1234307315f2bf
machida <machida@fjord.jp>
2024-10-22 02:51:37 +0900

    エラーを出すようにし、エラー時の表示、取得出来ない項目があったときのデザイン、読み込み中が出っぱなしなのを修正

--- app/javascript/markdown-it-link-to-card.js
+++ app/javascript/markdown-it-link-to-card.js
@@ -1,26 +1,28 @@
 export default (md, _options) => {
-  md.inline.ruler.after('text', 'link-to-card', (state, silent) => {
-    const start = state.pos
-    const end = state.posMax
-    const content = state.src.slice(start, end)
-    const match = content.match(/.*?@\[card\]\((.+?)\)/)
-    if (!match) return false
+  md.block.ruler.before(
+    'paragraph',
+    'link-to-card',
+    (state, startLine, _endLine, silent) => {
+      const content = state.src
+        .slice(state.bMarks[startLine], state.eMarks[startLine])
+        .trim()
 
-    if (!silent) {
+      const match = content.match(/@\[card\]\((.+?)\)/)
+      if (!match) return false
+
+      if (silent) return true
+
       const linkCardUrl = md.utils.escapeHtml(match[1])
 
-      const tokenOpen = state.push('link_open', 'a', 1)
-      tokenOpen.attrs = [
-        ['href', linkCardUrl],
-        ['class', 'before-replacement-link-card']
-      ]
+      const token = state.push('html_block', '', 0)
+      token.content = `
+      <div class="a-link-card before-replacement-link-card" data-url="${linkCardUrl}">
+        <p>リンクカード適用中... <i class="fa-regular fa-loader fa-spin"></i></p>
+      </div>
+    `
 
-      const tokenText = state.push('text', '', 0)
-      tokenText.content = 'リンクカード適用前'
-
-      state.push('link_close', 'a', -1)
+      state.line = startLine + 1
+      return true
     }
-    state.pos += match[0].length
-    return true
-  })
+  )
 }
