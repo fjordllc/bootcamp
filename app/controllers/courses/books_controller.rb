a03bb5078739ef5c35081500385e2af6d68f6bca
ai-24 <ai.morikawa2410sj@gmail.com>
2024-10-27 18:47:28 +0900

    /courses/:course_id/booksを非Vue化した

--- app/controllers/courses/books_controller.rb
+++ app/controllers/courses/books_controller.rb
@@ -3,5 +3,6 @@
 class Courses::BooksController < ApplicationController
   def index
     @course = Course.find(params[:course_id])
+    @books = Book.filtered_books(params[:status], @course)
   end
 end
