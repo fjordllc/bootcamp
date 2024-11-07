b881702d61ae44f7c5e7829aa01cd51e002fcf49
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-11-08 00:12:46 +0900

    足跡をつけるロジックをモデルに移動した
    コントローラー同士でメソッドを呼び出すのは一般的ではないと判断した
    また、Productの足跡機能を修正した

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -6,4 +6,13 @@ class Footprint < ApplicationRecord
   belongs_to :user
   belongs_to :footprintable, polymorphic: true
   validates :user_id, presence: true
+
+  def self.create_or_find(footprintable_type, footprintable_id, user)
+    find_or_create_by(
+      footprintable_type:,
+      footprintable_id:,
+      user:
+    )
+    where(footprintable_type:, footprintable_id:).order(created_at: :desc)
+  end
 end
