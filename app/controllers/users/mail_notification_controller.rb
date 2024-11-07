ae1d4ce5cbaebcea761f970923ebdfb0841685b4
ryo <ta090972@icloud.com>
2024-11-07 15:05:31 +0900

    ログイン不要とし、tokenとuser_idでアクセス制御するようにした

--- app/controllers/users/mail_notification_controller.rb
+++ app/controllers/users/mail_notification_controller.rb
@@ -1,23 +1,29 @@
 # frozen_string_literal: true
 
 class Users::MailNotificationController < ApplicationController
+  skip_before_action :require_active_user_login, raise: false
+  before_action :set_user, only: %i[edit update]
+  before_action :validate_user_access, only: %i[edit update]
   layout 'not_logged_in'
 
-  def edit
-    @user = User.find_by(unsubscribe_email_token: params[:token])
-    redirect_to root_path, notice: 'メール配信停止にはTOKENが必要です。' unless @user
-  end
+  def edit; end
 
   def update
-    user_id = params[:user_id].to_i
-
-    if current_user.id == user_id
-      user = User.find(user_id)
-      user.mail_notification = false
-      user.save!
+    if @user.update(mail_notification: false)
       redirect_to root_path, notice: 'メール配信を停止しました。'
     else
-      redirect_to root_path, alert: '無効な操作です。'
+      render :edit
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(unsubscribe_email_token: params[:token])
+  end
+
+  def validate_user_access
+    is_unauthorized_user_access = !@user || @user.id != params[:user_id].to_i
+    redirect_to root_path, alert: 'ユーザーIDもしくはTOKENが違います。' if is_unauthorized_user_access
   end
 end
--- test/system/mail_notification_test.rb
+++ test/system/mail_notification_test.rb
@@ -3,24 +3,15 @@
 require 'application_system_test_case'
 
 class MailNotificationsTest < ApplicationSystemTestCase
-  test "update user's mail_notification settings while logged in" do
-    url = "/users/#{users(:kimura).id}/mail_notification/edit?token=#{users(:kimura).unsubscribe_email_token}"
-    visit_with_auth url, 'kimura'
+  test "update user's mail_notification settings" do
+    visit "/users/#{users(:kimura).id}/mail_notification/edit?token=#{users(:kimura).unsubscribe_email_token}"
     assert_text 'メール通知をオフにしますか？'
     click_on 'オフにする'
     assert_text 'メール配信を停止しました。'
   end
 
-  test "update user's mail_notification settings without being logged in" do
-    visit "/users/#{users(:kimura).id}/mail_notification/edit?token=#{users(:kimura).unsubscribe_email_token}"
-    assert_text 'ログインしてください'
-  end
-
-  test "update another user's mail_notification" do
-    url = "/users/#{users(:kimura).id}/mail_notification/edit?token=#{users(:kimura).unsubscribe_email_token}"
-    visit_with_auth url, 'komagata'
-    assert_text 'メール通知をオフにしますか？'
-    click_on 'オフにする'
-    assert_text '無効な操作です。'
+  test "can not update other user's mail_notification" do
+    visit "/users/#{users(:komagata).id}/mail_notification/edit?token=#{users(:kimura).unsubscribe_email_token}"
+    assert_text 'ユーザーIDもしくはTOKENが違います。'
   end
 end
