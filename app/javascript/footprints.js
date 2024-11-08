e0b3bc1fe209075f0a32bb20703c2e3233cd2c32
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-11-09 02:37:25 +0900

    lintを実行した

--- app/javascript/footprints.js
+++ app/javascript/footprints.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const moreLink = document.querySelector('.user-icons__more');
+  const moreLink = document.querySelector('.user-icons__more')
   if (moreLink) {
     moreLink.addEventListener('click', () => {
-      const remainingFootprints = document.getElementById('remaining-footprints');
-      const initialFootprintsContainer = document.getElementById('initial-footprints');
-      
+      const remainingFootprints = document.getElementById(
+        'remaining-footprints'
+      )
+      const initialFootprintsContainer =
+        document.getElementById('initial-footprints')
+
       while (remainingFootprints.firstChild) {
-        initialFootprintsContainer.appendChild(remainingFootprints.firstChild);
+        initialFootprintsContainer.appendChild(remainingFootprints.firstChild)
       }
 
-      moreLink.remove();
-    });
+      moreLink.remove()
+    })
   }
-});
+})
