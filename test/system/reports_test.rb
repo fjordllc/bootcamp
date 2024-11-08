058c13e433cc6a5e377e234c0f838191195adcc1
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-11-08 13:43:14 +0900

    システムテストの日報作成日付を過去の基準日前に変更、モデルバリデーションからHTMLバリデーションのチェックに変更

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -475,10 +475,12 @@ class ReportsTest < ApplicationSystemTestCase
     within('form[name=report]') do
       fill_in('report[title]', with: '学習日が2013年1月1日より前では日報を作成できない')
       fill_in('report[description]', with: 'エラーになる')
-      fill_in('report[reported_on]', with: Date.new(2013, 1, 1))
+      fill_in('report[reported_on]', with: Date.new(2012, 12, 31))
     end
     click_button '提出'
-    assert_text '学習日は2013年01月01日から今日以前の間の日付にしてください'
+    html_validataion_message = page.find('#report_reported_on').native.attribute('validationMessage')
+    assert_not_nil html_validataion_message
+    assert_not_empty html_validataion_message
   end
 
   test 'display recently reports' do
