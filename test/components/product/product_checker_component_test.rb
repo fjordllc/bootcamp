01059fb4e7a31f9b7e3cdff3f1a3f6850a5977ef
Kuroneko <ken.aster8@gmail.com>
2024-11-09 16:46:38 +0900

    他のユーザーが担当している場合のテストを追加

--- test/components/product/product_checker_component_test.rb
+++ test/components/product/product_checker_component_test.rb
@@ -8,16 +8,16 @@ class Products::ProductCheckerComponentTest < ViewComponent::TestCase
 
   def setup
     auto_decorate(User)
-    @current_user = users(:komagata)
   end
 
   def test_render_assign_button_when_product_is_unassigned
+    current_user = users(:komagata)
     unassigned__product = products(:product6)
 
     render_inline(Products::ProductCheckerComponent.new(
                     checker_id: nil,
                     checker_name: nil,
-                    current_user_id: @current_user.id,
+                    current_user_id: current_user.id,
                     product_id: unassigned__product.id,
                     checker_avatar: nil
                   ))
@@ -27,17 +27,34 @@ class Products::ProductCheckerComponentTest < ViewComponent::TestCase
   end
 
   def test_render_unassign_button_when_product_is_assigned_by_current_user
-    assigned_product = products(:product2)
+    current_user = users(:komagata)
+    self_assigned_product = products(:product2)
 
     render_inline(Products::ProductCheckerComponent.new(
-                    checker_id: assigned_product.checker_id,
-                    checker_name: assigned_product.checker_name,
-                    current_user_id: @current_user.id,
-                    product_id: assigned_product.id,
-                    checker_avatar: assigned_product.checker_avatar
+                    checker_id: self_assigned_product.checker_id,
+                    checker_name: self_assigned_product.checker_name,
+                    current_user_id: current_user.id,
+                    product_id: self_assigned_product.id,
+                    checker_avatar: self_assigned_product.checker_avatar
                   ))
 
     assert_selector 'button#check-product-button.is-warning'
     assert_text '担当から外れる'
+  end
+
+  def test_render_chekcer_name_and_avatar_when_product_is_assigned_by_other_user
+    current_user = users(:machida)
+    other_assigned_product = products(:product2)
+
+    render_inline(Products::ProductCheckerComponent.new(
+                    checker_id: other_assigned_product.checker_id,
+                    checker_name: other_assigned_product.checker_name,
+                    current_user_id: current_user.id,
+                    product_id: other_assigned_product.id,
+                    checker_avatar: other_assigned_product.checker_avatar
+                  ))
+
+    assert_selector "img.a-user-icon[src*='#{other_assigned_product.checker_avatar}']"
+    assert_text other_assigned_product.checker_name
   end
 end
