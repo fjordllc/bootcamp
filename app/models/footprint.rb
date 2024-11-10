cefa7038cc6cc4dcaae4e9ca21ecaa4895e9a7a3
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-11-11 05:07:30 +0900

    テストのエラーを修正した
    投稿者自身の足跡が表示されることなく、他のユーザーの足跡が表示されるようにした
    また、テスト内容を修正した

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -8,6 +8,8 @@ class Footprint < ApplicationRecord
   validates :user_id, presence: true
 
   def self.create_or_find(footprintable_type, footprintable_id, user)
+    return if user.nil? || user.id == footprintable_id
+
     footprint = find_or_initialize_by(
       footprintable_type:,
       footprintable_id:,
