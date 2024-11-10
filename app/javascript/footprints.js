2d26c5eead7bdba79fb1e7057452cc2c8becfa63
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-11-10 17:09:52 +0900

    lintを実行した

--- app/javascript/footprints.js
+++ app/javascript/footprints.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const userIconsContainer = document.querySelector('.user-icons');
-  const loadingMessage = document.querySelector('.user-icons__loading');
+  const userIconsContainer = document.querySelector('.user-icons')
+  const loadingMessage = document.querySelector('.user-icons__loading')
 
   if (userIconsContainer && loadingMessage) {
-    loadingMessage.remove();
+    loadingMessage.remove()
 
-    const moreLink = document.querySelector('.user-icons__more');
+    const moreLink = document.querySelector('.user-icons__more')
     if (moreLink) {
       moreLink.addEventListener('click', () => {
-        const remainingFootprints = document.getElementById('remaining-footprints');
-        const initialFootprintsContainer = document.getElementById('initial-footprints');
+        const remainingFootprints = document.getElementById(
+          'remaining-footprints'
+        )
+        const initialFootprintsContainer =
+          document.getElementById('initial-footprints')
 
         while (remainingFootprints.firstChild) {
-          initialFootprintsContainer.appendChild(remainingFootprints.firstChild);
+          initialFootprintsContainer.appendChild(remainingFootprints.firstChild)
         }
 
-        moreLink.remove();
-      });
+        moreLink.remove()
+      })
     }
   }
-});
+})
