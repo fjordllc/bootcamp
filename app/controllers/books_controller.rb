0d87637c8368482c687547879d3bb3e42899a36e
Toshiyasu Nonohara <hh0648075@gmail.com>
2024-11-11 11:37:53 +0900

    インデントを調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,9 @@ class BooksController < ApplicationController
   def index
     books = Book.all
     books = params[:practice_id].present? ? books.joins(:practices).where(practices: { id: params[:practice_id] }) : books
-    @books = books
-             .with_attached_cover
-             .includes(:practices)
-             .order(updated_at: :desc, id: :desc)
+    @books = books.with_attached_cover
+                  .includes(:practices)
+                  .order(updated_at: :desc, id: :desc)
   end
 
   def new
