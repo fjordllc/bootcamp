5fb84773b0d00c6a78c75486779d259f0145135f
appsu-mi <natubate870@gmail.com>
2024-11-12 10:09:32 +0900

    仕様に合わせるためプラグインのコードを変更

--- app/javascript/markdown-it-link-to-card.js
+++ app/javascript/markdown-it-link-to-card.js
@@ -1,35 +1,50 @@
 export default (md, _options) => {
-  md.block.ruler.before(
-    'paragraph',
-    'link-to-card',
-    (state, startLine, _endLine, silent) => {
-      const content = state.src
-        .slice(state.bMarks[startLine], state.eMarks[startLine])
-        .trim()
+  md.core.ruler.after('replacements', 'link-to-card', function (state) {
+    const tokens = state.tokens
+    let allowLevel = 0
+    tokens.forEach((token, i) => {
+      if (token.type === 'container_details_open') {
+        allowLevel++
+        return
+      }
+      if (token.type === 'container_details_close' && allowLevel > 0) {
+        allowLevel--
+        return
+      }
+      // 親のp要素がマークダウンでネストしていない場合のみリンクカードを生成する(details以外)
+      const parentToken = tokens[i - 1]
+      const isParentRootParagraph =
+        parentToken &&
+        parentToken.type === 'paragraph_open' &&
+        parentToken.level === allowLevel
+      if (!isParentRootParagraph) return
 
-      const match = content.match(/^@\[card\]\((.+?)\)/)
-      if (!match) return false
+      // 対象となるlinkはinlineTokenのchildrenにのみ存在する
+      if (token.type !== 'inline') return
 
-      if (silent) return true
+      const children = token.children
+      if (!children) return
+
+      // childrenにlinkが存在する場合のみリンクカードを生成する
+      const hasLink = children?.some((child) => child.type === 'link_open')
+      if (!hasLink) return
+
+      // childrenにbrが存在しない場合のみリンクカードを生成する
+      const hasBr = children?.some((child) => child.type === 'softbreak')
+      if (hasBr) return
+
+      const match = token.content.match(/^@\[card\]\((.+?)\)/)
+      if (!match) return
 
       const linkCardUrl = md.utils.escapeHtml(match[1])
-
-      const token = state.push('html_block', '', 0)
-      token.content = `
-        <div class="a-link-card before-replacement-link-card" data-url="${linkCardUrl}">
-          <p>リンクカード適用中... <i class="fa-regular fa-loader fa-spin"></i></p>
-        </div>
-      `
-
-      const remainingText = content.slice(match[0].length).trim()
-
-      if (remainingText) {
-        const textToken = state.push('inline', '', 0)
-        textToken.content = remainingText
-      }
-
-      state.line = startLine + 1
-      return true
-    }
-  )
+      const linkCardToken = new state.Token('html_inline', '', 0)
+      linkCardToken.content = `
+            <div class="a-link-card before-replacement-link-card" data-url="${linkCardUrl}">
+              <p>リンクカード適用中... <i class="fa-regular fa-loader fa-spin"></i></p>
+            </div>
+          `
+      tokens[i] = linkCardToken
+    })
+    return true
+  })
 }
