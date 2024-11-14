b39b613462002f9b247d2ec7affeac1e74c14179
appsu-mi <natubate870@gmail.com>
2024-11-14 10:51:02 +0900

    check-stamp.vueをRailsで実装(非Vue化)した

--- app/javascript/check-stamp.js
+++ app/javascript/check-stamp.js
@@ -1,21 +1,53 @@
 import Vue from 'vue'
 import CheckStamp from 'check-stamp.vue'
 import store from 'check-store.js'
+import CSRF from 'csrf'
+import 'whatwg-fetch'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const checkStamp = document.getElementById('js-check-stamp')
-  if (checkStamp) {
-    const checkableId = checkStamp.getAttribute('data-checkable-id')
-    const checkableType = checkStamp.getAttribute('data-checkable-type')
-    new Vue({
-      store,
-      render: (h) =>
-        h(CheckStamp, {
-          props: {
-            checkableId: checkableId,
-            checkableType: checkableType
-          }
-        })
-    }).$mount('#js-check-stamp')
-  }
+  setChecks()
 })
+
+const setChecks = () => {
+  const checkStamp = document.getElementById('js-check-stamp')
+  if (!checkStamp) return
+
+  const checkableType = checkStamp.getAttribute('data-checkable-type')
+  const checkableId = checkStamp.getAttribute('data-checkable-id')
+  fetch(
+    `/api/checks.json/?checkable_type=${checkableType}&checkable_id=${checkableId}`,
+    {
+      method: 'GET',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': CSRF.getToken()
+      },
+      credentials: 'same-origin'
+    }
+  )
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      const checkStampElement = document.querySelector('.stamp-approve')
+
+      if (!json[0]) return checkStampElement.classList.add('is-hidden')
+
+      checkStampElement.classList.remove('is-hidden')
+
+      const checkedUserName = document.querySelector('.is-user-name')
+      const checkedCreatedAt = document.querySelector('.is-created-at')
+      checkedUserName.textContent = json[0].user.login_name
+      checkedCreatedAt.textContent = json[0].created_at
+
+      store.dispatch('setCheckable', {
+        checkableId: checkableId,
+        checkableType: checkableType
+      })
+    })
+    .catch((error) => {
+      console.warn(error)
+    })
+}
+
+export default setChecks
