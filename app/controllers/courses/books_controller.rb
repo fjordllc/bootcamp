1d4c7dc36043745fe9dd3e35b72547be0f763450
ai-24 <ai.morikawa2410sj@gmail.com>
2024-11-17 22:00:06 +0900

    データの取得方法を変更

--- app/controllers/courses/books_controller.rb
+++ app/controllers/courses/books_controller.rb
@@ -3,6 +3,16 @@
 class Courses::BooksController < ApplicationController
   def index
     @course = Course.find(params[:course_id])
-    @books = Book.filtered_books(params[:status], @course)
+    @books = Book.with_attached_cover
+                 .eager_load(:practices_books, practices: { categories: :courses_categories })
+                 .where(courses_categories: { course_id: @course.id })
+                 .where(must_read_status)
+                 .order(updated_at: :desc, id: :desc)
+  end
+
+  private
+
+  def must_read_status
+    params[:status] == 'mustread' ? { practices_books: { must_read: true } } : {}
   end
 end
