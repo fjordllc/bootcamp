73f5aa4e1460b01fb643c24f8c0e727de66a62ba
hagiya0121 <koazu0121@gmail.com>
2024-11-28 17:39:37 +0900

    定期イベントの更新通知の送信者をイベント作成者からログインユーザーに変更

--- app/models/regular_event_update_notifier.rb
+++ app/models/regular_event_update_notifier.rb
@@ -3,10 +3,11 @@
 class RegularEventUpdateNotifier
   def call(payload)
     regular_event = payload[:regular_event]
+    sender = payload[:sender]
     participants = regular_event.participants
 
     participants.each do |participant|
-      ActivityDelivery.with(regular_event:, receiver: participant).notify(:update_regular_event) if regular_event.user != participant
+      ActivityDelivery.with(regular_event:, sender:, receiver: participant).notify(:update_regular_event) if regular_event.user != participant
     end
   end
 end
