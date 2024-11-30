72b7f906ce8f0ef8192e78a1af18ade36bc63369
Kuroneko <ken.aster8@gmail.com>
2024-11-30 13:46:17 +0900

    時間のフォーマットをstrftimeからi18nに修正

--- test/components/users/micro_reports/micro_report_component_test.rb
+++ test/components/users/micro_reports/micro_report_component_test.rb
@@ -13,7 +13,7 @@ class Users::MicroReports::MicroReportComponentTest < ViewComponent::TestCase
 
     assert_selector "img.page-content-header__user-icon[title='#{micro_report.user.icon_title}']"
     assert_selector '.thread-comment__description', text: micro_report.content
-    assert_selector 'time.thread-comment__created-at', text: micro_report.created_at.strftime('%Y/%m/%d %H:%M')
+    assert_selector 'time.thread-comment__created-at', text: I18n.l(micro_report.created_at, format: :date_and_time)
     assert_text '👍1'
     assert_text '❤️1'
   end
@@ -22,21 +22,21 @@ class Users::MicroReports::MicroReportComponentTest < ViewComponent::TestCase
     micro_report = @user.micro_reports.create(content: '今日の分報', created_at: Time.zone.now)
     render_component(micro_report)
 
-    assert_includes page.text, "今日 #{Time.zone.now.strftime('%H:%M')}"
+    assert_includes page.text, "今日 #{I18n.l(Time.zone.now, format: :time_only)}"
   end
 
   def test_posted_datetime_yesterday
     micro_report = @user.micro_reports.create(content: '昨日の分報', created_at: 1.day.ago)
     render_component(micro_report)
 
-    assert_includes page.text, "昨日 #{1.day.ago.strftime('%H:%M')}"
+    assert_includes page.text, "昨日 #{I18n.l(1.day.ago, format: :time_only)}"
   end
 
   def test_posted_datetime_older_than_two_days
     micro_report = @user.micro_reports.create(content: '2日前の分報', created_at: 2.days.ago)
     render_component(micro_report)
 
-    assert_includes page.text, 2.days.ago.strftime('%Y/%m/%d %H:%M')
+    assert_includes page.text, I18n.l(2.days.ago, format: :date_and_time)
   end
 
   private
