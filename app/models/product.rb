ba66dc38da10e01f9c61c2f9d090e08bf3a72f44
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-12-07 10:32:47 +0900

    リファクタリングをした
    find_footprintsを1つにまとめた
    find_footprintsのfootprintsの取得、作成をアソシエーションメソッドで実装した

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,6 +16,7 @@ class Product < ApplicationRecord
 
   belongs_to :practice
   belongs_to :user, touch: true
+  has_many :footprints, as: :footprintable, dependent: :destroy
   belongs_to :checker, class_name: 'User', optional: true
   alias sender user
 
