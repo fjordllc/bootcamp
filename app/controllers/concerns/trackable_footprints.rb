f2c7aff8fd940fc992608e2d225ae6cf5414672e
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-12-07 12:43:39 +0900

    checkで落ちていたので修正した
    bundle exec rake tracerouteで落ちていたため、privateにした

--- app/controllers/concerns/trackable_footprints.rb
+++ app/controllers/concerns/trackable_footprints.rb
@@ -3,6 +3,8 @@
 module TrackableFootprints
   extend ActiveSupport::Concern
 
+  private
+
   def find_footprints(resource)
     resource.footprints.find_or_create_by(user: current_user) if resource.user != current_user
     resource.footprints.where.not(user_id: resource.user.id).order(created_at: :desc)
