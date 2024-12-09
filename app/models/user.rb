0cd335f8f6d79d2549956c01a7e79f3ec3f08ad1
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-12-09 22:43:45 +0900

    Userモデルにaccess_grantsとaccess_tokensの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -172,6 +172,16 @@ class User < ApplicationRecord
            through: :regular_event_participations,
            source: :regular_event
 
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+
   has_one_attached :avatar
   has_one_attached :profile_image
 
