b231b863d3dc563c73cb37b35595a7804c09446e
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-12-09 22:57:44 +0900

    スコープの設定を追加

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -10,4 +10,9 @@ orm :active_record
       redirect_to login_path
     end
   end
+
+  # デフォルトのスコープを read に設定
+  # これにより承認されたアプリケーションは API から公開データの「読み取り」が可能
+  default_scopes :read
+  enforce_configured_scopes
 end
