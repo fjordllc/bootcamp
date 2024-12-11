e8a4b1ad4ae638b82e4e551a197c2199d35be190
hagiya0121 <koazu0121@gmail.com>
2024-12-11 13:22:58 +0900

    企業研修申し込み一覧表示機能

---
+++ app/controllers/admin/corporate_training_inquiries_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Admin::CorporateTrainingInquiriesController < AdminController
+  PAGER_NUMBER = 20
+  def index
+    per = params[:per] || PAGER_NUMBER
+    @corporate_training_inquiries = CorporateTrainingInquiry.order(created_at: :desc).page(params[:page]).per(per)
+  end
+end
