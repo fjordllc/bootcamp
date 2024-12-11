c782a23b85867837bf8e4466d1e9ec9de93713c9
hiromisugie <lozz.gggg@gmail.com>
2024-12-12 06:02:42 +0900

    定期イベント用のテストを作成

--- test/models/regular_event_test.rb
+++ test/models/regular_event_test.rb
@@ -157,14 +157,19 @@ class RegularEventTest < ActiveSupport::TestCase
     end
   end
 
-  test '.scheduled_on_without_ended' do
+  test 'Regular event in progress today should be displayed' do
     travel_to Time.zone.local(2024, 12, 1, 10, 0, 0) do
       today_date = Time.zone.today
-      regular_events = RegularEvent.scheduled_on_without_ended(today_date)
-      regular_event_in_progress = regular_events(:regular_event36)
-      regular_event_ended = regular_events(:regular_event37)
-      assert_includes regular_events, regular_event_in_progress
-      assert_not_includes regular_events, regular_event_ended
+      regular_event = RegularEvent.scheduled_on_without_ended(today_date)
+      assert_includes regular_event, regular_events(:regular_event36)
+    end
+  end
+
+  test 'Regular event that have already ended today should not be displayed' do
+    travel_to Time.zone.local(2024, 12, 1, 10, 0, 0) do
+      today_date = Time.zone.today
+      regular_event = RegularEvent.scheduled_on_without_ended(today_date)
+      assert_not_includes regular_event, regular_events(:regular_event37)
     end
   end
 end
