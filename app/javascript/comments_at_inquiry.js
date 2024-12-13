a5044881f0428659c31c21254ace251327ff3a2c
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-12-13 10:27:51 +0900

    多量コメント時の前コメント読み込みボタンの文言に対し、表示前コメント数が表示されるように変更

--- app/javascript/comments_at_inquiry.js
+++ app/javascript/comments_at_inquiry.js
@@ -8,4 +8,32 @@ document.addEventListener('DOMContentLoaded', () => {
     loadingContent.style.display = 'none'
     commentContent.style.display = 'block'
   }
+
+  displayMoreComments(comments)
 })
+
+function displayMoreComments(comments) {
+  const commentLimit = 8
+  let commentOffset = 0
+  const incrementCommentSize = 8
+  const commentTotalCount = comments.length
+  const moreCommentButton = document.querySelector(
+    '.a-button.is-lg.is-text.is-block'
+  )
+  const allCommentsIsLoaded = commentLimit + commentOffset >= commentTotalCount
+  if (!allCommentsIsLoaded) {
+    commentOffset += commentLimit
+  }
+  const commentRemaining = commentTotalCount - commentOffset
+
+  const nextCommentAmount =
+    commentRemaining > incrementCommentSize
+      ? `${incrementCommentSize} / ${commentRemaining}`
+      : commentRemaining
+  const commentText = `前のコメント（ ${nextCommentAmount} ）`
+  const moreComments = document.querySelector('.thread-comments-more')
+  if (!allCommentsIsLoaded) {
+    moreComments.style.display = 'block'
+    moreCommentButton.textContent = commentText
+  }
+}
