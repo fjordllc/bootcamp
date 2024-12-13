ec324d4044dfb44a7e16747527ea6fec98e7d34e
hagiya0121 <koazu0121@gmail.com>
2024-12-13 17:49:54 +0900

    企業研修申し込み一覧表示のテストを追加

--- test/models/corporate_training_inquiry_test.rb
+++ test/models/corporate_training_inquiry_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class CorporateTrainingInquiryTest < ActiveSupport::TestCase
   test '#unique_meeting_dates' do
-    corporate_training_inquiry = corporate_training_inquiry(:corporate_training_inquiry1)
+    corporate_training_inquiry = corporate_training_inquiries(:corporate_training_inquiry1)
     corporate_training_inquiry.meeting_date1 = Time.zone.parse('2030-01-01-10:00')
     corporate_training_inquiry.meeting_date2 = corporate_training_inquiry.meeting_date1
     assert_not corporate_training_inquiry.valid?
@@ -12,7 +12,7 @@ class CorporateTrainingInquiryTest < ActiveSupport::TestCase
   end
 
   test '#meeting_dates_not_in_past' do
-    corporate_training_inquiry = corporate_training_inquiry(:corporate_training_inquiry1)
+    corporate_training_inquiry = corporate_training_inquiries(:corporate_training_inquiry1)
     corporate_training_inquiry.meeting_date1 = Time.zone.today - 1.day
     assert_not corporate_training_inquiry.valid?
     assert_includes corporate_training_inquiry.errors[:base], '研修打ち合わせ希望日時に過去の日付は入力できません'
