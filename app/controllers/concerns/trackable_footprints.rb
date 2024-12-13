1437eb0c7fede83831ab6330ad9b85e7cd2e50fe
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-12-14 00:18:07 +0900

    N+1を解消した

--- app/controllers/concerns/trackable_footprints.rb
+++ app/controllers/concerns/trackable_footprints.rb
@@ -7,6 +7,6 @@ module TrackableFootprints
 
   def find_footprints(resource)
     resource.footprints.find_or_create_by(user: current_user) if resource.user != current_user
-    resource.footprints.where.not(user_id: resource.user.id).order(created_at: :desc)
+    resource.footprints.includes(:user).where.not(user_id: resource.user.id).order(created_at: :desc)
   end
 end
