5fbb75cc624d092fd2c13a1089b23187b5004d9f
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2024-12-14 00:24:52 +0900

    使用していないので削除した

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -6,19 +6,4 @@ class Footprint < ApplicationRecord
   belongs_to :user
   belongs_to :footprintable, polymorphic: true
   validates :user_id, presence: true
-
-  def self.create_or_find(footprintable_type, footprintable_id, user)
-    return if user.nil? || user.id == footprintable_id
-
-    footprint = find_or_initialize_by(
-      footprintable_type:,
-      footprintable_id:,
-      user:
-    )
-
-    footprint.save! if footprint.new_record?
-
-    where(footprintable_type:, footprintable_id:)
-      .order(created_at: :desc)
-  end
 end
