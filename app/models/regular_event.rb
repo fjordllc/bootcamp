d82e3d62b6daa4ade369c7f7789099b190f969df
hiromisugie <lozz.gggg@gmail.com>
2024-12-20 06:10:06 +0900

    RegularEventの方のnot_endedのロジックを修正

--- app/models/regular_event.rb
+++ app/models/regular_event.rb
@@ -58,8 +58,7 @@ class RegularEvent < ApplicationRecord # rubocop:disable Metrics/ClassLength
   scope :participated_by, ->(user) { where(id: all.filter { |e| e.participated_by?(user) }.map(&:id)) }
   scope :organizer_event, ->(user) { where(id: user.organizers.map(&:regular_event_id)) }
   scope :scheduled_on, ->(date) { holding.filter { |event| event.scheduled_on?(date) } }
-  scope :not_ended, -> { where('end_at > ?', Time.current) }
-  scope :scheduled_on_without_ended, ->(date) { holding.not_ended.filter { |event| event.scheduled_on?(date) } }
+  scope :scheduled_on_without_ended, ->(date) { holding.filter { |event| event.scheduled_on?(date) && event.not_ended? } }
 
   belongs_to :user
   has_many :organizers, dependent: :destroy
@@ -80,9 +79,8 @@ class RegularEvent < ApplicationRecord # rubocop:disable Metrics/ClassLength
     all_scheduled_dates.include?(date)
   end
 
-  def ended?
-    end_time_of_today_event = Time.current.change(hour: end_at.hour, min: end_at.min)
-    Time.current >= end_time_of_today_event
+  def not_ended?
+    Time.current.strftime('%H:%M') < end_at.strftime('%H:%M')
   end
 
   def next_event_date
