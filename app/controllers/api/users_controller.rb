28f0f7b279cf35ac8702e38c643b5f696e735c1e
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-12-20 02:02:03 +0900

    doorkeeper_tokenが存在する場合にのみ、show APIの保護が行われるようにした

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,8 +2,9 @@
 
 class API::UsersController < API::BaseController
   before_action :set_user, only: %i[show update]
+  before_action :require_login_for_api
   before_action :require_login_for_api, except: :show
-  before_action :doorkeeper_authorize!, only: :show
+  before_action :doorkeeper_authorize!, if: -> { doorkeeper_token.present? }, only: :show
   PAGER_NUMBER = 24
 
   def index
