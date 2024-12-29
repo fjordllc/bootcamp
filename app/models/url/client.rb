a96d722f7c01d0950ec48308b340942746409625
appsu-mi <natubate870@gmail.com>
2024-12-29 12:17:15 +0900

    ドメインのチェックをLinkCheckerクラスに移譲

--- app/models/url/client.rb
+++ app/models/url/client.rb
@@ -12,24 +12,9 @@ module Url
 
     def request
       uri = Addressable::URI.parse(@url).normalize
-      return unless valid_url?(uri)
-
-      Net::HTTP.get_response(uri)
-    end
-
-    private
-
-    def valid_url?(uri)
       return unless LinkChecker::Checker.valid_url?(uri)
 
-      valid_domein?(uri.host)
-    end
-
-    def valid_domein?(domein)
-      Resolv.getaddress(domein)
-      true
-    rescue Resolv::ResolvError
-      false
+      Net::HTTP.get_response(uri)
     end
   end
 end
