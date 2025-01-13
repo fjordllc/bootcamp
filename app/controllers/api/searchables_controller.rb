76976302bf5a7736e1e7cf44599e4daffdefeefd
Yuichiro Makita <kitarou888@YuichironoMacBook-Pro.local>
2025-01-13 10:40:49 +0900

    検索機能を拡充：自分のみのチェックボックスを追加

--- app/controllers/api/searchables_controller.rb
+++ app/controllers/api/searchables_controller.rb
@@ -5,6 +5,9 @@ class API::SearchablesController < API::BaseController
 
   def index
     result = Searcher.search(params[:word], document_type: document_type_param)
+
+    result = result.select { |record| record.user_id == current_user.id } if params[:only_logged_in_user]
+
     @searchables = Kaminari.paginate_array(result).page(params[:page]).per(PAGER_NUMBER)
   end
 
