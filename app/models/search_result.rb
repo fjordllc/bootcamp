71ffb478493aee41c9b78ec714360c1b0409e5e1
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-01-15 21:50:58 +0900

    search_helperのメソッドを元に戻す実装にした

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
 class SearchResult
+  include SearchHelper
+
   attr_accessor :url, :title, :summary, :formatted_summary, :user_id,
                 :login_name, :formatted_updated_at, :model_name, :label,
                 :wip, :commentable_user, :commentable_type, :primary_role
 
   def initialize(searchable, word)
-    @url = searchable.try(:url)
+    @url = searchable_url(searchable)
     @title = Searcher.fetch_title(searchable)
-    @summary = searchable.try(:summary)
-    @formatted_summary = searchable.formatted_summary(word)
+    @summary = filtered_message(searchable)
+    @formatted_summary = Searcher.highlight_word(@summary, word)
     @user_id = searchable.is_a?(User) ? searchable.id : searchable.try(:user_id)
     @login_name = Searcher.fetch_login_name(searchable)
-    @formatted_updated_at = searchable.formatted_updated_at
+    @formatted_updated_at = searchable.respond_to?(:formatted_updated_at) ? searchable.formatted_updated_at : searchable.updated_at.strftime('%Y年%m月%d日 %H:%M')
     @model_name = searchable.class.name.underscore
     @label = Searcher.fetch_label(searchable)
     @wip = searchable.try(:wip)
     @commentable_user = Searcher.fetch_commentable_user(searchable)
     @commentable_type = I18n.t("activerecord.models.#{searchable.try(:commentable)&.try(:model_name)&.name&.underscore}", default: '')
-    @primary_role = searchable.primary_role
+    @primary_role = created_user(searchable)&.primary_role
   end
 end
