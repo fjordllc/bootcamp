99cc901fe53da46eeb9683502639ea8037f25a51
machida <machida@fjord.jp>
2025-01-21 00:23:00 +0900

    注目の記事のデザインを整えた。要約がない場合、本文の最初の100文字をHTMLタグを省いて表示するようにした。

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module MarkdownHelper
+  require 'redcarpet'
+  require 'nokogiri'
+
+  def markdown_to_plain_text(markdown_content)
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer)
+    html_content = markdown.render(markdown_content)
+    Nokogiri::HTML(html_content).text.strip
+  end
+
   def md2html(text)
     return '' if text.nil?
 
