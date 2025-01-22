765622b24e3444a84c3ddb8178f4865b69e69d97
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-01-22 10:52:04 +0900

    通知ターゲットにて通知しない以外を選択した場合はダイアログが出るように変更

---
+++ app/javascript/article.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const radioButtons = document.querySelectorAll(
+    'input[name="article[target]"]'
+  )
+  if (radioButtons) {
+    radioButtons.forEach((radio) => {
+      radio.addEventListener('change', (event) => {
+        const target = event.target.value
+        const targetName = document
+          .querySelector(`label[for="${radio.id}"]`)
+          .textContent.trim()
+        if (target !== 'no_recipient') {
+          window.confirm(
+            `この記事を公開すると、${targetName}に通知が送られます。もし記事に誤った情報が含まれている場合、その通知を受け取った人を介してSNSなどで拡散される可能性があります。そのため、記事内の年月日などに間違いがないか、しっかり確認してください。公開しても問題ありませんか？`
+          )
+        }
+      })
+    })
+  }
+})
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -69,7 +69,7 @@ import '../coding-test.js'
 import '../copy-url.js'
 import '../survey_result_chart.js'
 import '../footprints.js'
-import '../article-target.js'
+import '../article.js'
 
 import VueMounter from '../VueMounter.js'
 import Questions from '../components/questions.vue'
