c39fd08127e809498c9b79651fc7959b54bd35d7
hiromisugie <lozz.gggg@gmail.com>
2025-01-24 06:23:40 +0900

    イベントリスナーを使った書き方に変更

--- app/javascript/copy-url.js
+++ app/javascript/copy-url.js
@@ -1,17 +1,20 @@
-function copyUrl() {
-  const url = window.location.href
-  navigator.clipboard
-    .writeText(url)
-    .then(() => {
-      const button = document.querySelector('.a-copy-button')
-      button.classList.add('is-active')
-      setTimeout(() => {
-        button.classList.remove('is-active')
-      }, 4000)
-    })
-    .catch((err) => {
-      console.error(err)
-    })
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.a-copy-button')
 
-window.copyUrl = copyUrl
+  if (button) {
+    button.addEventListener('click', () => {
+      const url = window.location.href
+      navigator.clipboard
+        .writeText(url)
+        .then(() => {
+          button.classList.add('is-active')
+          setTimeout(() => {
+            button.classList.remove('is-active')
+          }, 4000)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    })
+  }
+})
