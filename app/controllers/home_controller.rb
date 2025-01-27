201418edf50524bc7c270a5fe10249b611a1472a
su-su-su-su <suzukikoji0503@gmail.com>
2025-01-27 15:06:43 +0900

    ホーム画面表示用のタグを「feature」から「注目の記事」に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -68,6 +68,6 @@ class HomeController < ApplicationController
   end
 
   def list_articles_with_specific_tag
-    Article.tagged_with('feature').order(published_at: :desc).where(wip: false).first(6)
+    Article.tagged_with('注目の記事').order(published_at: :desc).where(wip: false).first(6)
   end
 end
