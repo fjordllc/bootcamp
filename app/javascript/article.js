cfd06fe947b34136f5a78ef392e1141710cc64bd
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-01-30 15:25:41 +0900

    article.jsをリファクタリングした（関数化してDRYにする、早期リターンの条件対象を変更）

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,8 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('article_form')
-  if (!form) {
+  const radioButtons = document.querySelectorAll(
+    'input[name="article[target]"]'
+  )
+  const defaultTarget = document.querySelector(
+    'input[name="article[target]"]:checked'
+  )
+  if (!defaultTarget) {
     return
   }
+  let target = defaultTarget.value
+  let targetName = document
+    .querySelector(`label[for="${defaultTarget.id}"]`)
+    .textContent.trim()
+  let text = setAlertText(target, targetName)
+
+  radioButtons.forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+      target = event.target.value
+      targetName = document
+        .querySelector(`label[for="${radio.id}"]`)
+        .textContent.trim()
+      text = setAlertText(target, targetName)
+    })
+  })
+
+  const form = document.getElementById('article_form')
   const submitButtons = form.querySelectorAll(
     'input[type="submit"',
     'button[type="submit"]'
@@ -13,40 +35,12 @@ document.addEventListener('DOMContentLoaded', () => {
     isPublish = true
   })
 
-  const radioButtons = document.querySelectorAll(
-    'input[name="article[target]"]'
-  )
-  const defaultTarget = document.querySelector(
-    'input[name="article[target]"]:checked'
-  )
-  let target = defaultTarget.value
-  let targetName = document
-    .querySelector(`label[for="${defaultTarget.id}"]`)
-    .textContent.trim()
-  let alertText =
-    target === 'none'
-      ? '「通知しない」が選択されています。よろしいですか？'
-      : `この記事を公開すると、${targetName}に通知が送られます。もし記事に誤った情報が含まれている場合、その通知を受け取った人を介してSNSなどで拡散される可能性があります。そのため、記事内の年月日などに間違いがないか、しっかり確認してください。公開しても問題ありませんか？`
-
-  radioButtons.forEach((radio) => {
-    radio.addEventListener('change', (event) => {
-      target = event.target.value
-      targetName = document
-        .querySelector(`label[for="${radio.id}"]`)
-        .textContent.trim()
-      alertText =
-        target === 'none'
-          ? '「通知しない」が選択されています。よろしいですか？'
-          : `この記事を公開すると、${targetName}に通知が送られます。もし記事に誤った情報が含まれている場合、その通知を受け取った人を介してSNSなどで拡散される可能性があります。そのため、記事内の年月日などに間違いがないか、しっかり確認してください。公開しても問題ありませんか？`
-    })
-  })
-
   form.addEventListener('submit', (event) => {
     if (isPublish) {
       submitButtons.forEach((button) => {
         button.removeAttribute('data-disable-with')
       })
-      if (!window.confirm(alertText)) {
+      if (!window.confirm(text)) {
         event.preventDefault()
         isPublish = false
       } else {
@@ -57,3 +51,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 })
+
+function setAlertText(target, targetName) {
+  if (target === 'none') {
+    return '「通知しない」が選択されています。よろしいですか？'
+  } else {
+    return `この記事を公開すると、${targetName}に通知が送られます。もし記事に誤った情報が含まれている場合、その通知を受け取った人を介してSNSなどで拡散される可能性があります。そのため、記事内の年月日などに間違いがないか、しっかり確認してください。公開しても問題ありませんか？`
+  }
+}
