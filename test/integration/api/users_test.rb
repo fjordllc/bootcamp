3d04d15890ff6e9dc7274f802fc7ca8aa7f98628
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2025-02-05 00:31:04 +0900

    show API が提供する情報が許可されたものであるかをテストするテスト作成

--- test/integration/api/users_test.rb
+++ test/integration/api/users_test.rb
@@ -5,6 +5,14 @@ require 'test_helper'
 class API::UsersTest < ActionDispatch::IntegrationTest
   fixtures :users
 
+  def setup
+    @application = Doorkeeper::Application.create!(
+      name: 'Sample Application',
+      redirect_uri: 'https://example.com/callback',
+      scopes: 'read'
+    )
+  end
+
   test 'GET /api/users.json' do
     get api_users_path(format: :json)
     assert_response :unauthorized
@@ -79,5 +87,50 @@ class API::UsersTest < ActionDispatch::IntegrationTest
         headers: { 'Authorization' => "Bearer #{token}" }
     assert_response :ok
     assert_nil(JSON.parse(@response.body)['mentor_memo'])
+  end
+
+  test 'returns only authorized user information for admin user with doorkeeper token' do
+    user = users(:komagata)
+    doorkeeper_token = Doorkeeper::AccessToken.create!(
+      application_id: @application.id,
+      resource_owner_id: user.id,
+      scopes: 'read'
+    )
+    get api_user_path(id: 'show'), headers: { Authorization: "Bearer #{doorkeeper_token.token}", Accept: 'application/json' }
+    assert_response :ok
+
+    response_body = JSON.parse(@response.body)
+    authorized_keys = %w[id login_name email long_name url roles primary_role icon_title adviser avatar_url company]
+    assert_equal authorized_keys.sort, response_body.keys.sort
+  end
+
+  test 'returns only authorized user information for authorized mentor with doorkeeper token' do
+    user = users(:mentormentaro)
+    doorkeeper_token = Doorkeeper::AccessToken.create!(
+      application_id: @application.id,
+      resource_owner_id: user.id,
+      scopes: 'read'
+    )
+    get api_user_path(id: 'show'), headers: { Authorization: "Bearer #{doorkeeper_token.token}", Accept: 'application/json' }
+    assert_response :ok
+
+    response_body = JSON.parse(@response.body)
+    authorized_keys = %w[id login_name email long_name url roles primary_role icon_title adviser avatar_url]
+    assert_equal authorized_keys.sort, response_body.keys.sort
+  end
+
+  test 'returns only authorized user information for student with doorkeeper token' do
+    user = users(:hatsuno)
+    doorkeeper_token = Doorkeeper::AccessToken.create!(
+      application_id: @application.id,
+      resource_owner_id: user.id,
+      scopes: 'read'
+    )
+    get api_user_path(id: 'show'), headers: { Authorization: "Bearer #{doorkeeper_token.token}", Accept: 'application/json' }
+    assert_response :ok
+
+    response_body = JSON.parse(@response.body)
+    authorized_keys = %w[id login_name email long_name url roles primary_role icon_title adviser avatar_url]
+    assert_equal authorized_keys.sort, response_body.keys.sort
   end
 end
