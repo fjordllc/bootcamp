f9f77447c3065575f079b2a27eced72b036abe7a
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-02-05 11:16:23 +0900

    ヘッダータブのコメント数にお問い合わせが含まれないように変更

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -14,8 +14,8 @@ class CommentTest < ActiveSupport::TestCase
     AbstractNotifier.delivery_mode = @delivery_mode
   end
 
-  test '.without_talk' do
-    non_talk_comment_count = Comment.without_talk.count
+  test '.without_private_comment' do
+    non_talk_comment_count = Comment.without_private_comment.count
     all_comment_count = Comment.count
     only_talk_comment_count = Comment.where(commentable_type: 'Talk').count
     assert_equal non_talk_comment_count, all_comment_count - only_talk_comment_count
