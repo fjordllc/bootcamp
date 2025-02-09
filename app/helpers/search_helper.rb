15fbb89d25a3dae60492d6cd6ae05f2e75fc52db
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-02-09 17:12:43 +0900

    大文字小文字どちらでも検索できるようにした

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -56,10 +56,8 @@ module SearchHelper
       else
         searchable.description
       end
-    when Product
-      searchable.body
     else
-      searchable.try(:description)
+      searchable.try(:description) || searchable.try(:body)
     end
   end
 
