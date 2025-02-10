9d0086783c0576d83c7391cf1a899d6f198d6114
ryo <ta090972@icloud.com>
2025-02-10 19:02:17 +0900

    Categoryに関するプリロード処理を変更

--- app/controllers/courses/practices_controller.rb
+++ app/controllers/courses/practices_controller.rb
@@ -3,7 +3,7 @@
 class Courses::PracticesController < ApplicationController
   def index
     @course = Course.find(params[:course_id])
-    @categories = @course.categories.includes(practices: %i[practices_books learning_minute_statistic started_students]).order(:created_at)
+    @categories = @course.categories.includes(practices: %i[practices_books learning_minute_statistic started_or_submitted_students]).order(:created_at)
     @learnings = current_user.learnings
   end
 end
