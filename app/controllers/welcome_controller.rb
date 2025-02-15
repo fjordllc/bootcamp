49f12e18caa42f91603cbf055d8aabbeefdbb68b
su-su-su-su <suzukikoji0503@gmail.com>
2025-02-16 00:25:05 +0900

    「注目の記事」取得処理をスコープに統一して重複コードを削除

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
 
   def index
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
-    @articles = list_articles_with_specific_tag
+    @featured_articles = Article.featured
   end
 
   def alumni_voices
@@ -69,9 +69,5 @@ class WelcomeController < ApplicationController
     return if current_user&.admin?
 
     redirect_to root_path, alert: 'ページのアクセス権限がありませんでした。'
-  end
-
-  def list_articles_with_specific_tag
-    Article.tagged_with('注目の記事').order(published_at: :desc).where(wip: false).first(6)
   end
 end
