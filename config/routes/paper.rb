c6ce27953c6193ed24448211b9809ed8b1e96ba2
komagata <komagata@gmail.com>
2025-02-19 03:21:43 +0900

    演習の具体的な実施手順等を示す資料を作るためのページを作成

--- config/routes/paper.rb
+++ config/routes/paper.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   namespace :paper do
     root to: "home#index", as: :root
+    resource :exercise_procedure_document, only: %i(show), controller: "exercise_procedure_document"
   end
 end
