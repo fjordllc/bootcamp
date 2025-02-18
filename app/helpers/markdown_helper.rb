e5f35c60c499eb05a1a852fa02f11aa661558967
su-su-su-su <suzukikoji0503@gmail.com>
2025-02-18 11:21:51 +0900

    rubocopに対する修正

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 module MarkdownHelper
-
   def markdown_to_plain_text(markdown_content)
-    return "" if markdown_content.blank?
+    return '' if markdown_content.blank?
 
-    parse_options = [:DEFAULT, :UNSAFE, :LIBERAL_HTML_TAG]
+    parse_options = %i[DEFAULT UNSAFE LIBERAL_HTML_TAG]
     extensions = []
     doc = CommonMarker.render_doc(markdown_content, parse_options, extensions)
     html = doc.to_html([:UNSAFE])
