2a63c5413ac84609ef1181f8c12e2061f0077997
su-su-su-su <suzukikoji0503@gmail.com>
2025-02-18 11:01:10 +0900

    MarkdownHelperのテストを追加

---
+++ test/helpers/markdown_helper_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class MarkdownHelperTest < ActionView::TestCase
+  include MarkdownHelper
+
+  def test_markdown_to_plain_text_converts_markdown_to_plain_text
+    markdown = "# Hello, world! \n This is a **test**."
+    expected_plain_text = "Hello, world!\nThis is a test."
+    assert_equal expected_plain_text, markdown_to_plain_text(markdown)
+
+    markdown = "<script>alert('xss');</script>"
+    expected_plain_text = ""
+    assert_equal expected_plain_text, markdown_to_plain_text(markdown)
+
+    markdown = "<div>test</div>"
+    expected_plain_text = "test"
+    assert_equal expected_plain_text, markdown_to_plain_text(markdown)
+  end
+
+  def test_markdown_to_plain_text_handles_empty_markdown
+    assert_equal "", markdown_to_plain_text("")
+  end
+end
