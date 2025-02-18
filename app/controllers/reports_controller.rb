551e78a0def346c9f0b6f7c3e76f22f070c92d8a
su-su-su-su <suzukikoji0503@gmail.com>
2025-02-18 11:51:47 +0900

    Newspaper.publish呼び出しを復元

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -57,6 +57,7 @@ class ReportsController < ApplicationController
     canonicalize_learning_times(@report)
 
     if @report.save_with_uniqueness_handling
+      Newspaper.publish(:report_save, { report: @report })
       redirect_to redirect_url(@report), notice: notice_message(@report), flash: flash_contents(@report)
     else
       render :new
