44a9f6954dbf4512b9aab256570938755c16f23c
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-02-19 16:02:51 +0900

    ファイル名を実態に沿うように変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -66,7 +66,7 @@ import '../new-answer.js'
 import '../coding-test.js'
 import '../copy-url.js'
 import '../footprints.js'
-import '../article.js'
+import '../article-target.js'
 
 import VueMounter from '../VueMounter.js'
 import Questions from '../components/questions.vue'
