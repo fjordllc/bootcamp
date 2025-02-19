f7a6a5a1c1c393d76043ea7492b0351bacd17419
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-02-19 14:22:40 +0900

    テストにおける使用メソッド名を変更に合わせて修正、ダイアログの影響でFlakyになっていた箇所を修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -213,14 +213,14 @@ class UserTest < ActiveSupport::TestCase
     assert user.invalid?
   end
 
-  test 'announcment for all' do
-    target = User.announcement_receiver('all')
+  test 'notification for all' do
+    target = User.notification_receiver('all')
     assert_includes(target, users(:kimura))
     assert_not_includes(target, users(:yameo))
   end
 
-  test 'announcment for students' do
-    target = User.announcement_receiver('students')
+  test 'notification for students' do
+    target = User.notification_receiver('students')
     assert_includes(target, users(:kimura))
     assert_includes(target, users(:komagata))
     assert_includes(target, users(:mentormentaro))
@@ -230,14 +230,26 @@ class UserTest < ActiveSupport::TestCase
     assert_not_includes(target, users(:kensyu))
   end
 
-  test 'announcment for job_seekers' do
-    target = User.announcement_receiver('job_seekers')
+  test 'notification for job_seekers' do
+    target = User.notification_receiver('job_seekers')
     assert_includes(target, users(:jobseeker))
     assert_includes(target, users(:komagata))
     assert_includes(target, users(:sotugyou))
     assert_includes(target, users(:mentormentaro))
     assert_not_includes(target, users(:sotugyou_with_job))
     assert_not_includes(target, users(:kimura))
+    assert_not_includes(target, users(:yameo))
+  end
+
+  test 'notification for none' do
+    target = User.notification_receiver('none')
+    assert_not_includes(target, users(:kimura))
+    assert_not_includes(target, users(:jobseeker))
+    assert_not_includes(target, users(:komagata))
+    assert_not_includes(target, users(:mentormentaro))
+    assert_not_includes(target, users(:sotugyou))
+    assert_not_includes(target, users(:advijirou))
+    assert_not_includes(target, users(:kensyu))
     assert_not_includes(target, users(:yameo))
   end
 
