152a15d137bc16b363d328d27cfe5b9120300182
appsu-mi <natubate870@gmail.com>
2025-02-20 10:32:48 +0900

    外から使われないメソッドをprivateに

--- app/models/link_card/ogp.rb
+++ app/models/link_card/ogp.rb
@@ -6,16 +6,16 @@ module LinkCard
       @html = html
     end
 
-    def ogp_nodes
-      @ogp_nodes ||= extract_ogp_nodes
-    end
-
     def find_by(name)
       target = ogp_nodes.find { |node| node['property'] == "og:#{name}" }
       target['content']
     end
 
     private
+
+    def ogp_nodes
+      @ogp_nodes ||= extract_ogp_nodes
+    end
 
     def extract_ogp_nodes
       doc = Nokogiri::HTML(@html)
