0fc437df6aa3a3bf41472508ca0e926119d9d1ef
SuzukiShuntarou <da8801593@gmail.com>
2025-02-21 14:26:00 +0900

    分報のコントローラーへのdestroyアクションの追加

--- app/controllers/users/micro_reports_controller.rb
+++ app/controllers/users/micro_reports_controller.rb
@@ -3,7 +3,8 @@
 class Users::MicroReportsController < ApplicationController
   PAGER_NUMBER = 25
 
-  before_action :set_user
+  before_action :set_user, only: %i[index create]
+  before_action :set_my_micro_report, only: %i[destroy]
 
   def index
     @micro_reports = @user.micro_reports.order(created_at: :asc).page(params[:page]).per(PAGER_NUMBER)
@@ -21,10 +22,21 @@ class Users::MicroReportsController < ApplicationController
     redirect_to user_micro_reports_path(@user, page: @user.latest_micro_report_page)
   end
 
+  def destroy
+    @micro_report.destroy!
+
+    redirect_to user_micro_reports_path(current_user, page: current_user.latest_micro_report_page)
+    flash[:notice] = '分報を削除しました。'
+  end
+
   private
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def set_my_micro_report
+    @micro_report = current_user.micro_reports.find(params[:id])
   end
 
   def micro_report_params
