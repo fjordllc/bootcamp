22a7362957706fb264f3d903160c76c1d59dfbc4
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-02-24 15:39:41 +0900

    Footprint.trackのように足跡をリソース上に作成するという意味合いにした

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -15,7 +15,7 @@ class AnnouncementsController < ApplicationController
   end
 
   def show
-    Footprint.record_footprint(@announcement, current_user) unless @announcement.user == current_user
+    Footprint.create_on_resource(@announcement, current_user) unless @announcement.user == current_user
     @footprints = Footprint.fetch_footprints(@announcement)
     @footprint_total_count = Footprint.footprint_count(@announcement)
   end
