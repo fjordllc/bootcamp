539c06f50b92932981e4a29be0e6b82948d77bd1
SuzukiShuntarou <da8801593@gmail.com>
2025-02-27 21:11:51 +0900

    sponsorshipsページにはsponsorship記事だけが表示され他の記事が表示されないことを確認するテストを追加

--- test/system/article/sponsorships_test.rb
+++ test/system/article/sponsorships_test.rb
@@ -3,10 +3,13 @@
 require 'application_system_test_case'
 
 class Article::SponsorshipsTest < ApplicationSystemTestCase
-  test 'show listing sponsorship articles' do
+  test 'show listing only sponsorship articles' do
     visit_with_auth sponsorships_path, 'komagata'
     assert_selector '.thumbnail-card__inner', count: 1
+    assert_no_text 'タイトル１'
+    assert_no_text 'タイトル２'
+    assert_text 'sponsorshipページに表示される記事のタイトルです。'
     click_link_or_button 'ブログ記事のブランクアイキャッチ画像'
-    assert_text 'sponsorshipページに表示される記事です。'
+    assert_text 'sponsorshipページに表示される記事の本文です。'
   end
 end
