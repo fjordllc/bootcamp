6bfaac4588c218c7763bac3e82dbe4647bb0f1b4
komagata <komagata@gmail.com>
2025-03-04 16:18:06 +0900

    アンケート結果のグラフ表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_02_152255) do
+ActiveRecord::Schema.define(version: 2025_03_04_062341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -729,6 +729,7 @@ ActiveRecord::Schema.define(version: 2025_03_02_152255) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["survey_id", "user_id"], name: "index_survey_answers_on_survey_id_and_user_id", unique: true
     t.index ["survey_id"], name: "index_survey_answers_on_survey_id"
     t.index ["user_id"], name: "index_survey_answers_on_user_id"
   end
