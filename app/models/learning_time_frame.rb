535f52e63a78e4062f779fe7249aa5a7edc1e298
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-03-04 17:31:22 +0900

    presenceバリデーションを追加
    https://github.com/fjordllc/bootcamp/pull/8263#discussion_r1968325062

--- app/models/learning_time_frame.rb
+++ app/models/learning_time_frame.rb
@@ -3,4 +3,6 @@
 class LearningTimeFrame < ApplicationRecord
   has_many :learning_time_frames_users, dependent: :destroy
   has_many :users, through: :learning_time_frames_users
+
+  validates :week_day, :activity_time, presence: true
 end
