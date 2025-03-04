f8aa235b3715e29548b9b276d158abd56d69b1e6
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-03-04 17:23:30 +0900

    NOT NULL制約を追加
    https://github.com/fjordllc/bootcamp/pull/8263#discussion_r1968323182

--- db/migrate/20250130052049_create_learning_time_frames.rb
+++ db/migrate/20250130052049_create_learning_time_frames.rb
@@ -1,8 +1,8 @@
 class CreateLearningTimeFrames < ActiveRecord::Migration[6.1]
   def change
     create_table :learning_time_frames do |t|
-      t.string :week_day
-      t.integer :activity_time
+      t.string :week_day, null: false
+      t.integer :activity_time, null: false
 
       t.timestamps
     end
