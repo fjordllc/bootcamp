855c88000771df9fcd2cefb85d623e1918e06115
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-03-05 17:00:29 +0900

    その他を選択した際にのみ記述フォームが出現するようにJSを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -70,6 +70,7 @@ import '../copy-url.js'
 import '../survey_result_chart.js'
 import '../footprints.js'
 import '../article-target.js'
+import '../referral-source-selection-form.js'
 
 import VueMounter from '../VueMounter.js'
 import Questions from '../components/questions.vue'
---
+++ app/javascript/referral-source-selection-form.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const referralSources = document.getElementsByName('user[referral_source]')
+  if (!referralSources) {
+    return null
+  }
+
+  const otherReferralSource = document.getElementById(
+    'user_referral_source_other'
+  )
+  const otherReferralSourceForm = document.getElementById(
+    'other_referral_source_form'
+  )
+  if (otherReferralSource.checked) {
+    otherReferralSourceForm.classList.remove('hidden')
+  }
+
+  referralSources.forEach((source) => {
+    source.addEventListener('change', () => {
+      if (otherReferralSource.checked) {
+        otherReferralSourceForm.classList.remove('hidden')
+      } else {
+        otherReferralSourceForm.classList.add('hidden')
+      }
+    })
+  })
+
+  const form = document.getElementById('payment-form')
+  if (form) {
+    form.addEventListener('submit', validateOtherReferralSource)
+  }
+
+  function validateOtherReferralSource() {
+    if (!otherReferralSource.checked) {
+      const otherReferralSourceText = document.getElementById(
+        'other_referral_source'
+      )
+      otherReferralSourceText.value = ''
+    }
+  }
+})
