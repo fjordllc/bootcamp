d17d39ff71062c3d05527dc5ef2013a32bb27e77
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2025-03-07 21:54:48 +0900

    markdown-it-sanitizerを導入した

--- app/javascript/markdown-initializer.js
+++ app/javascript/markdown-initializer.js
@@ -13,6 +13,7 @@ import MarkDownItContainerDetails from 'markdown-it-container-details'
 import MarkDownItLinkAttributes from 'markdown-it-link-attributes'
 import MarkDownItContainerSpeak from 'markdown-it-container-speak'
 import ReplaceLinkToCard from 'replace-link-to-card'
+import MarkdownItSanitizer from 'markdown-it-sanitizer'
 
 export default class {
   replace(selector) {
@@ -55,6 +56,7 @@ export default class {
       }
     })
     md.use(MarkDownItContainerSpeak)
+    md.use(MarkdownItSanitizer)
     return md.render(text)
   }
 }
