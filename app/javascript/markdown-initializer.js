06640c634dcff4154e4df149454b59fd2fe52aff
harada-webdev <141257112+harada-webdev@users.noreply.github.com>
2025-03-08 02:56:50 +0900

    markdown-it-videoを導入した

--- app/javascript/markdown-initializer.js
+++ app/javascript/markdown-initializer.js
@@ -14,6 +14,7 @@ import MarkDownItLinkAttributes from 'markdown-it-link-attributes'
 import MarkDownItContainerSpeak from 'markdown-it-container-speak'
 import ReplaceLinkToCard from 'replace-link-to-card'
 import MarkdownItSanitizer from 'markdown-it-sanitizer'
+import MarkdownItOnlineVideo from './markdown-it-online-video'
 
 export default class {
   replace(selector) {
@@ -57,6 +58,7 @@ export default class {
     })
     md.use(MarkDownItContainerSpeak)
     md.use(MarkdownItSanitizer)
+    md.use(MarkdownItOnlineVideo)
     return md.render(text)
   }
 }
