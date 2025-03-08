9cf5237d9bd1a61c5c5bdcee07e1a523ac8c798c
komagata <komagata@gmail.com>
2025-03-08 17:44:55 +0900

    落ちているテストを修正

--- test/supports/stripe_helper.rb
+++ test/supports/stripe_helper.rb
@@ -2,20 +2,33 @@
 
 module StripeHelper
   def fill_stripe_element(card, exp, cvc)
-    card_iframe = all('iframe')[0]
+    card_iframe = find('iframe[name^="__privateStripeFrame"]', wait: 10)
 
     within_frame card_iframe do
-      card.chars.each do |piece|
-        find_field('cardnumber').send_keys(piece)
-      end
+      card_field = find_field('cardnumber', wait: 10)
+      fill_field_carefully(card_field, card)
 
-      exp.chars.each do |piece|
-        find_field('exp-date').send_keys(piece)
-      end
+      exp_field = find_field('exp-date', wait: 5)
+      fill_field_carefully(exp_field, exp)
 
-      cvc.chars.each do |piece|
-        find_field('cvc').send_keys(piece)
-      end
+      cvc_field = find_field('cvc', wait: 5)
+      fill_field_carefully(cvc_field, cvc)
+    end
+
+    page.has_no_css?('iframe[name^="__privateStripeFrame"]', wait: 5)
+  end
+
+  private
+
+  def fill_field_carefully(field, value)
+    field.clear if field.value.present?
+    field.send_keys(value)
+
+    return if field.value.include?(value.gsub(/\s+/, ''))
+
+    field.clear
+    value.chars.each do |char|
+      field.send_keys(char)
     end
   end
 end
