1cea9ab361a07a6f0c2af131cd94e814b564ffd5
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-03-11 23:56:51 +0900

    変数が修正できていなかった

--- app/controllers/regular_events_controller.rb
+++ app/controllers/regular_events_controller.rb
@@ -9,7 +9,7 @@ class RegularEventsController < ApplicationController
 
   def show
     @regular_event = RegularEvent.find(params[:id])
-    Footprint.create_on_resource(footprintable: resource, user: current_user) unless @regular_event.user == current_user
+    Footprint.create_on_resource(footprintable: @regular_event, user: current_user) unless @regular_event.user == current_user
     @footprints = Footprint.fetch_for_resource(@regular_event)
   end
 
