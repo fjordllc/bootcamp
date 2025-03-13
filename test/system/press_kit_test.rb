201ac7dfdd1dfc5e1c214debaf9a73291d7ce1db
ryo <ta090972@icloud.com>
2025-03-13 18:32:43 +0900

    最新のプレスリリース記事を６つまで表示しているかのテストを修正

--- test/system/press_kit_test.rb
+++ test/system/press_kit_test.rb
@@ -3,7 +3,21 @@
 require 'application_system_test_case'
 
 class PressKitTest < ApplicationSystemTestCase
-  test 'show press releases' do
+  test 'show recent 6 press releases' do
+    Article.delete_all
+
+    8.times do |i|
+      article = Article.create(
+        title: "press releases #{i}",
+        body: 'プレスリリースのタグを持つブログ記事',
+        user: users(:komagata),
+        wip: false,
+        published_at: "2022-1-1 #{i}:00:00"
+      )
+      article.tag_list.add('プレスリリース')
+      article.save
+    end
+
     visit press_kit_path
     assert_selector '.thumbnail-card.a-card', count: 6
   end
