899dbf641e05af6afcf63d75bbac018089b50e17
ryo <ta090972@icloud.com>
2025-03-14 00:03:44 +0900

    公開順に並んでいることを確認するテストを追加

--- test/system/press_releases_test.rb
+++ test/system/press_releases_test.rb
@@ -32,4 +32,46 @@ class PressReleasesTest < ApplicationSystemTestCase
     assert_text 'プレスリリースのタグを持つ公開済みのブログ記事'
     assert_no_text 'プレスリリースのタグを持つwipブログ記事'
   end
+
+  test 'press releases are displayed from the most recent publication date' do
+    three_days_ago_press_release = Article.create(
+      title: '3日前に公開されたプレスリリース',
+      body: 'test',
+      user: users(:komagata),
+      wip: false,
+      created_at: Date.current - 4.days,
+      updated_at: Date.current - 4.days,
+      published_at: Date.current - 3.days
+    )
+    three_days_ago_press_release.tag_list.add('プレスリリース')
+    three_days_ago_press_release.save
+
+    two_days_ago_press_release = Article.create(
+      title: '2日前に公開されたプレスリリース',
+      body: 'test',
+      user: users(:komagata),
+      wip: false,
+      created_at: Date.current - 5.days,
+      updated_at: Date.current - 5.days,
+      published_at: Date.current - 2.days
+    )
+    two_days_ago_press_release.tag_list.add('プレスリリース')
+    two_days_ago_press_release.save
+
+    one_day_ago_press_release = Article.create(
+      title: '1日前に公開されたプレスリリース',
+      body: 'test',
+      user: users(:komagata),
+      wip: false,
+      created_at: Date.current - 6.days,
+      updated_at: Date.current - 6.days,
+      published_at: Date.current - 1.day
+    )
+    one_day_ago_press_release.tag_list.add('プレスリリース')
+    one_day_ago_press_release.save
+
+    visit press_releases_path
+    top_three_titles = all('h2.thumbnail-card__title').take(3).map(&:text)
+    assert_equal [one_day_ago_press_release.title, two_days_ago_press_release.title, three_days_ago_press_release.title], top_three_titles
+  end
 end
