be6b10435cb6b10d1a559ec8238cb70eb869cfa0
ryo <ta090972@icloud.com>
2025-03-13 23:56:32 +0900

    公開済みのプレスリリースのみ表示しているテストと認識がしやすい形に変更

--- test/system/press_releases_test.rb
+++ test/system/press_releases_test.rb
@@ -10,7 +10,27 @@ class PressReleasesTest < ApplicationSystemTestCase
   end
 
   test 'show only published press releases' do
+    press_release = Article.create(
+      title: 'プレスリリースのタグを持つ公開済みのブログ記事',
+      body: 'test',
+      user: users(:komagata),
+      wip: false,
+      published_at: '2022-1-1 00:00:00'
+    )
+    press_release.tag_list.add('プレスリリース')
+    press_release.save
+
+    wip_press_release = Article.create(
+      title: 'プレスリリースのタグを持つwipブログ記事',
+      body: 'test',
+      user: users(:komagata),
+      wip: true
+    )
+    wip_press_release.tag_list.add('プレスリリース')
+    wip_press_release.save
+
     visit press_releases_path
-    assert_selector '.thumbnail-card.a-card', count: 7
+    assert_text 'プレスリリースのタグを持つ公開済みのブログ記事'
+    assert_no_text 'プレスリリースのタグを持つwipブログ記事'
   end
 end
