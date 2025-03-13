325ebafbe4a2a4e001cb2ef43c1f58c472d86268
ryo <ta090972@icloud.com>
2025-03-13 23:39:47 +0900

    プレスキットが正しく表示されるかtitleの整合性を確認するテストを追加

--- test/system/press_kit_test.rb
+++ test/system/press_kit_test.rb
@@ -3,6 +3,11 @@
 require 'application_system_test_case'
 
 class PressKitTest < ApplicationSystemTestCase
+  test 'show listing press kit' do
+    visit press_kit_url
+    assert_equal 'プレスキット | FJORD BOOT CAMP（フィヨルドブートキャンプ）', title
+  end
+
   test 'show recent 6 press releases' do
     8.times do |i|
       article = Article.create(
