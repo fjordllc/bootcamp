99da01ec1463bc01e52d920c08f555380dec4d33
r.kawano <r.kawano@genbasupport.com>
2025-03-22 23:41:28 +0900

    テストコードを修正

--- test/system/article/sponsorships_test.rb
+++ test/system/article/sponsorships_test.rb
@@ -4,12 +4,12 @@ require 'application_system_test_case'
 
 class Article::SponsorshipsTest < ApplicationSystemTestCase
   test 'show listing only sponsorship articles' do
-    visit_with_auth sponsorships_path, 'komagata'
-    assert_selector '.thumbnail-card__inner', count: 1
-    assert_no_text 'タイトル１'
-    assert_no_text 'タイトル２'
+    visit_with_auth new_article_url, 'komagata'
+    fill_in 'タイトル', with: 'sponsorshipページに表示される記事のタイトルです。'
+    fill_in '本文', with: 'sponsorshipページに表示される記事の本文です。'
+    fill_in_tag 'スポンサーシップ'
+    click_on '公開する'
+    visit sponsorships_path
     assert_text 'sponsorshipページに表示される記事のタイトルです。'
-    click_link_or_button 'ブログ記事のブランクアイキャッチ画像'
-    assert_text 'sponsorshipページに表示される記事の本文です。'
   end
 end
