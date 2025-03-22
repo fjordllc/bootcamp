a73e3e182d595ae2fda371764fa6bb5adc2b3381
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2025-03-23 02:37:51 +0900

    不要な空白削除（Rubocop 指摘箇所修正）

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -7,7 +7,7 @@ class API::BaseController < ApplicationController
   def current_user
     super || current_resource_owner
   end
-  
+
   def current_resource_owner
     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
