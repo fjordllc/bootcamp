41113263fba8c0bcc68863ac1c89cae94a4df48e
su-su-su-su <suzukikoji0503@gmail.com>
2025-03-22 23:54:12 +0900

    テストの記法をtest 'xxxx' do~ end 形式に変更

--- test/helpers/markdown_helper_test.rb
+++ test/helpers/markdown_helper_test.rb
@@ -5,13 +5,13 @@ require 'test_helper'
 class MarkdownHelperTest < ActionView::TestCase
   include MarkdownHelper
 
-  def test_markdown_to_plain_text_converts_markdown_to_plain_text
+  test 'markdown_to_plain_text_converts_markdown_to_plain_text' do
     markdown = "# Hello, world! \n This is a **test**."
     expected_plain_text = "Hello, world!\nThis is a test."
     assert_equal expected_plain_text, markdown_to_plain_text(markdown)
   end
 
-  def test_markdown_to_plain_text_handles_empty_markdown
+  test 'markdown_to_plain_text_handles_empty_markdown' do
     assert_equal '', markdown_to_plain_text('')
   end
 end
