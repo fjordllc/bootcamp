474b9a3cedacbb11067e77a87e0df1a6e95de1e1
SuzukiShuntarou <da8801593@gmail.com>
2025-03-23 15:32:42 +0900

    プラクティスに関連する動画のタイトルリンクをクリックすると該当の動画の個別ページが表示されることを確認するテストを追加した

--- test/system/practice/movies_test.rb
+++ test/system/practice/movies_test.rb
@@ -17,4 +17,14 @@ class Practice::MoviesTest < ApplicationSystemTestCase
     assert_selector '.page-tabs__item-link.is-active', text: '動画 （0）'
     assert_text '動画はまだありません'
   end
+
+  test 'show movie related to practices when click title link' do
+    visit_with_auth "/practices/#{practices(:practice1).id}/movies", 'hatsuno'
+    assert_equal 'OS X Mountain Lionをクリーンインストールするに関する動画 | FBC', title
+    assert_selector '.thumbnail-card__title-link.a-text-link', text: 'OS X Mountain Lionをクリーンインストールするに関連する動画'
+    click_link_or_button 'OS X Mountain Lionをクリーンインストールするに関連する動画'
+
+    assert_current_path movie_path(movies(:movie5))
+    assert_selector 'h1.page-content-header__title', text: 'OS X Mountain Lionをクリーンインストールするに関連する動画'
+  end
 end
