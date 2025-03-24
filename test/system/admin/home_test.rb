35af93b2885420d77282db81e4954a090e2fc0e3
komagata <komagata@gmail.com>
2025-03-24 09:17:02 +0900

    管理ページのタブのテストを修正

--- test/system/admin/home_test.rb
+++ test/system/admin/home_test.rb
@@ -6,5 +6,10 @@ class Admin::HomeTest < ApplicationSystemTestCase
   test 'GET /admin' do
     visit_with_auth '/admin', 'komagata'
     assert_equal '管理ページ | FBC', title
+
+    tabs = %w[FAQ お問い合わせ お試し延長 ユーザー 企業 企業研修 管理ページ 給付金対応コース申し込み].sort
+    assert_equal tabs, all('a.page-tabs__item-link').map(&:text).sort
+    assert_no_selector '.page-tabs__item-link', text: 'プラクティス'
+    assert_no_selector '.page-tabs__item-link', text: 'コース'
   end
 end
