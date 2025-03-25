753f55a5a727f5aa2c82dd1b7ecb64bb59ffb9a5
SuzukiShuntarou <da8801593@gmail.com>
2025-03-25 14:56:23 +0900

    プラクティスに関連する動画が表示されタイトルリンクが正しいことを確認するテストを１つにまとめた

--- test/system/practice/movies_test.rb
+++ test/system/practice/movies_test.rb
@@ -7,7 +7,8 @@ class Practice::MoviesTest < ApplicationSystemTestCase
     visit_with_auth "/practices/#{practices(:practice1).id}/movies", 'hatsuno'
     assert_equal 'OS X Mountain Lionをクリーンインストールするに関する動画 | FBC', title
     assert_selector 'h2.page-header__title', text: 'OS X Mountain Lionをクリーンインストールする'
-    assert_selector '.page-tabs__item-link.is-active', text: '動画 （1）'
+    click_link_or_button 'OS X Mountain Lionをクリーンインストールするに関連する動画'
+    assert_current_path movie_path(movies(:movie5))
   end
 
   test 'show listing no movies' do
@@ -16,15 +17,5 @@ class Practice::MoviesTest < ApplicationSystemTestCase
     assert_selector 'h2.page-header__title', text: 'sshdをインストールする'
     assert_selector '.page-tabs__item-link.is-active', text: '動画 （0）'
     assert_text '動画はまだありません'
-  end
-
-  test 'show movie related to practices when click title link' do
-    visit_with_auth "/practices/#{practices(:practice1).id}/movies", 'hatsuno'
-    assert_equal 'OS X Mountain Lionをクリーンインストールするに関する動画 | FBC', title
-    assert_selector '.thumbnail-card__title-link.a-text-link', text: 'OS X Mountain Lionをクリーンインストールするに関連する動画'
-    click_link_or_button 'OS X Mountain Lionをクリーンインストールするに関連する動画'
-
-    assert_current_path movie_path(movies(:movie5))
-    assert_selector 'h1.page-content-header__title', text: 'OS X Mountain Lionをクリーンインストールするに関連する動画'
   end
 end
