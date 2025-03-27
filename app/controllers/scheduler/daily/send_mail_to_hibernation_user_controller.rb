bd40a8501adf0277007f0e8563c92e62ee7e743e
SuzukiShuntarou <da8801593@gmail.com>
2025-03-27 22:57:50 +0900

    タイプミスを修正した

--- app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
+++ app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
@@ -9,7 +9,7 @@ class Scheduler::Daily::SendMailToHibernationUserController < SchedulerControlle
   private
 
   def send_mail_one_week_before_auto_retire
-    User.unretired.hibernated_for(User::HIBERNATION_LIMIT_BEFORE_one_WEEK).each do |user|
+    User.unretired.hibernated_for(User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK).each do |user|
       UserMailer.one_week_before_auto_retire(user).deliver_now
     rescue Postmark::InactiveRecipientError => e
       Rails.logger.warn "[Postmark] 受信者由来のエラーのためメールを送信できませんでした。：#{e.message}"
