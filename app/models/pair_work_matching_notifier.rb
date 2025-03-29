fdf1e0247c314f16b30274ffbaac78e6d2dd3482
appsu-mi <natubate870@gmail.com>
2025-03-29 12:06:29 +0900

    ペアワークに通知機能を追加

---
+++ app/models/pair_work_matching_notifier.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class PairWorkMatchingNotifier
+  def call(payload)
+    pair_work = payload[:pair_work]
+    return if pair_work.wip?
+    return if !pair_work.saved_change_to_attribute?(:reserved_at, from: nil, to: pair_work.reserved_at)
+
+    notify_watchers(pair_work)
+    notify_to_chat(pair_work)
+  end
+
+  private
+
+  def notify_watchers(pair_work)
+    receiver_ids = pair_work.watches.pluck(:user_id)
+
+    receiver_ids.each do |receiver_id|
+      receiver = User.find(receiver_id)
+      ActivityDelivery.with(pair_work:, receiver:).notify(:matching_pair_work)
+    end
+  end
+
+  def notify_to_chat(pair_work)
+    ChatNotifier.message(<<~TEXT)
+      ペアワーク：「#{pair_work.title}」のマッチング相手が決定しました。
+      <#{Rails.application.routes.url_helpers.pair_work_url(pair_work)}>
+    TEXT
+  end
+end
