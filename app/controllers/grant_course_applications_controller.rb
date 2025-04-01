0f42510757c8957cb94f32621e14ef35dc2fc3d7
komagata <komagata@gmail.com>
2025-04-01 10:38:00 +0900

    給付金対応コース申し込み機能からReCAPTCHA削除

--- app/controllers/grant_course_applications_controller.rb
+++ app/controllers/grant_course_applications_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class GrantCourseApplicationsController < ApplicationController
-  include Recaptchable::V3
   skip_before_action :require_active_user_login, raise: false
 
   def new
@@ -10,13 +9,11 @@ class GrantCourseApplicationsController < ApplicationController
 
   def create
     @grant_course_application = GrantCourseApplication.new(grant_course_application_params)
-    result = valid_recaptcha?('grant_course_application')
 
-    if result && @grant_course_application.save
+    if @grant_course_application.save
       GrantCourseApplicationMailer.incoming(@grant_course_application).deliver_later
       redirect_to created_grant_course_applications_url
     else
-      flash.now[:alert] = 'Bot対策のため送信を拒否しました。しばらくしてからもう一度送信してください。' unless result
       render :new
     end
   end
