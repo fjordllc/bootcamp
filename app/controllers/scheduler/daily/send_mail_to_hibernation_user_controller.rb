b214b8e7ec531db7f24fb68943b6ee6ef55c0d6e
SuzukiShuntarou <da8801593@gmail.com>
2025-04-01 14:54:47 +0900

    自動退会期限の１週間前のユーザ以外にもメールが送信されていたバグを修正した

--- app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
+++ app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
@@ -9,7 +9,8 @@ class Scheduler::Daily::SendMailToHibernationUserController < SchedulerControlle
   private
 
   def send_mail_one_week_before_auto_retire
-    User.unretired.hibernated_for(User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK).each do |user|
+    sending_dates = (User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK.ago - 1.day)..User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK.ago
+    User.unretired.where(hibernated_at: sending_dates).find_each do |user|
       UserMailer.one_week_before_auto_retire(user).deliver_now
     rescue Postmark::InactiveRecipientError => e
       Rails.logger.warn "[Postmark] 受信者由来のエラーのためメールを送信できませんでした。：#{e.message}"
