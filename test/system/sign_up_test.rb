f390bfae65121b784dbb0bd7379b8659f5cd37aa
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-04-02 11:14:00 +0900

    テストを削除

--- test/system/sign_up_test.rb
+++ test/system/sign_up_test.rb
@@ -442,35 +442,4 @@ class SignUpTest < ApplicationSystemTestCase
     visit '/users/new'
     assert_no_selector ".form-item.a-form-label[for='user_learning_time_frames']", text: '主な活動予定時間'
   end
-
-  test 'other_referral_source_text_enabled_only_if_other_referral_source_checked' do
-    visit '/users/new'
-    within 'form[name=user]' do
-      fill_in 'user[login_name]', with: 'foo'
-      fill_in 'user[email]', with: 'test@example.com'
-      fill_in 'user[name]', with: 'テスト 太郎'
-      fill_in 'user[name_kana]', with: 'テスト タロウ'
-      fill_in 'user[description]', with: 'テスト太郎です。'
-      fill_in 'user[password]', with: 'testtest'
-      fill_in 'user[password_confirmation]', with: 'testtest'
-      fill_in 'user[after_graduation_hope]', with: '起業したいです'
-      select '学生', from: 'user[job]'
-      find('label', text: 'Mac（Intel チップ）').click
-      check 'Rubyの経験あり', allow_label_click: true
-      find('label', text: 'アンチハラスメントポリシーに同意').click
-      find('label', text: '利用規約に同意').click
-    end
-    find('label', text: 'その他').click
-    assert_text '「その他」を選んだ方は、フィヨルドブートキャンプを知った具体的な経路をご記入ください。'
-    fill_in 'user[other_referral_source]', with: 'mixi2'
-    find('label', text: '知人の紹介').click
-    assert_no_text '「その他」を選んだ方は、フィヨルドブートキャンプを知った具体的な経路をご記入ください。'
-
-    fill_stripe_element('4242 4242 4242 4242', '12 / 50', '111')
-
-    VCR.use_cassette 'sign_up/valid-card', record: :once, match_requests_on: %i[method uri] do
-      click_button '参加する'
-      assert_text 'サインアップメールをお送りしました。メールからサインアップを完了させてください。'
-    end
-  end
 end
