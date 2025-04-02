53af3ec6fa83e74331a3b9ced8da36e2cb6a3a03
SuzukiShuntarou <da8801593@gmail.com>
2025-04-02 19:01:58 +0900

    自動退会する設定のユーザで、かつ、送信されていないユーザに対して送信フラグの更新とメールを送信するように変更した

--- app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
+++ app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
@@ -9,9 +9,11 @@ class Scheduler::Daily::SendMailToHibernationUserController < SchedulerControlle
   private
 
   def send_mail_one_week_before_auto_retire
-    sending_dates = (User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK.ago - 1.day)..User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK.ago
-    User.unretired.where(hibernated_at: sending_dates).find_each do |user|
-      UserMailer.one_week_before_auto_retire(user).deliver_now
+    User.unretired.hibernated_for(User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK).each do |user|
+      if user.auto_retire && !user.sent_student_before_auto_retire_mail
+        user.mark_mail_as_sent_before_auto_retire
+        UserMailer.one_week_before_auto_retire(user).deliver_now
+      end
     rescue Postmark::InactiveRecipientError => e
       Rails.logger.warn "[Postmark] 受信者由来のエラーのためメールを送信できませんでした。：#{e.message}"
     end
