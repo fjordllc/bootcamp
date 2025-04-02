5a3ab2e21ab84911c38490a4098136736ad3cca9
r.kawano <r.kawano@genbasupport.com>
2025-04-02 16:33:44 +0900

    テストデータの追加を`create!`を使った方法に修正

--- test/system/article/sponsorships_test.rb
+++ test/system/article/sponsorships_test.rb
@@ -4,14 +4,18 @@ require 'application_system_test_case'
 
 class Article::SponsorshipsTest < ApplicationSystemTestCase
   test 'show listing only sponsorship articles' do
-    visit_with_auth new_article_url, 'komagata'
-    fill_in 'タイトル', with: 'sponsorshipページに表示される記事のタイトルです。'
-    fill_in '本文', with: 'sponsorshipページに表示される記事の本文です。'
-    fill_in_tag 'スポンサーシップ'
-    page.accept_confirm do
-      click_on '公開する'
-    end
-    visit sponsorships_path
+    sponsorship_article = Article.create!(
+      title: 'sponsorshipページに表示される記事のタイトルです。',
+      body: 'sponsorshipページに表示される記事の本文です。',
+      user: users(:komagata),
+      wip: false,
+      published_at: '2022-01-01 00:00:00',
+      thumbnail_type: :prepared_thumbnail
+    )
+    sponsorship_article.tag_list.add('スポンサーシップ')
+    sponsorship_article.save
+
+    visit_with_auth sponsorships_path, 'komagata'
     assert_selector '.thumbnail-card__inner', count: 1
     assert_text 'sponsorshipページに表示される記事のタイトルです。'
   end
