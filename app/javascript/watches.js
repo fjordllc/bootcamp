17f4e4e2577a4e25776a3dd690aeedf19c6fe168
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-04-02 16:03:16 +0900

    Watch一覧にてwatch削除後に次ページのwatchが１件読み込まれるように変更

--- app/javascript/watches.js
+++ app/javascript/watches.js
@@ -3,20 +3,14 @@ import { unWatch } from './watch-toggle'
 document.addEventListener('DOMContentLoaded', () => {
   const localStorage = window.localStorage
   const editToggle = document.getElementById('card-list-tools__action')
-
   const deleteButtons = document.querySelectorAll('.a-watch-button')
+
   if (!editToggle) {
     window.addEventListener('beforeunload', function () {
       localStorage.removeItem('watchs-delete-mode')
     })
     return
   }
-
-  deleteButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      unWatch(button)
-    })
-  })
 
   if (localStorage.getItem('watchs-delete-mode') === 'on') {
     editToggle.checked = true
@@ -43,4 +37,53 @@ document.addEventListener('DOMContentLoaded', () => {
       })
     }
   })
+
+  const hiddenData = document.getElementById('hidden-watch-data')
+  const perPage = hiddenData.dataset.default_per_page
+  const pageNum = hiddenData.dataset.page_num
+  const ids = hiddenData.dataset.all_ids.match(/\d+/g).map(Number)
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      unWatch(button)
+      fetchWatch(perPage, pageNum, ids)
+    })
+  })
 })
+
+async function fetchWatch(perPage, pageNum, ids) {
+  const nextWatchIndex = pageNum ? perPage * pageNum : perPage
+  if (ids.length < nextWatchIndex) {
+    return
+  }
+
+  const nextWatchId = ids[nextWatchIndex]
+  try {
+    const response = await fetch(`/api/watches/${nextWatchId}`, {
+      method: 'GET',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      },
+      credentials: 'same-origin',
+      redirect: 'manual'
+    })
+
+    if (response.ok) {
+      ids.splice(nextWatchIndex, 1)
+      const html = await response.text()
+      const watchsList = document.querySelector('.card-list', '.a-card')
+      watchsList.insertAdjacentHTML('beforeend', html)
+
+      const nextWatch = document.getElementById(nextWatchId)
+      const deleteButton = nextWatch.querySelector('.a-watch-button')
+      deleteButton.classList.remove('hidden')
+      deleteButton.addEventListener('click', () => {
+        unWatch(deleteButton)
+        fetchWatch(perPage, pageNum, ids)
+      })
+    }
+  } catch (error) {
+    console.warn(error)
+    return false
+  }
+}
