0fa78cd7760f9a7128d7b776e839ea0587161549
sekito <sekito1107gg@gmail.com>
2025-04-05 06:14:44 +0900

    リアクティブな更新は出来ないが、学習状況の変更に対応した

--- app/javascript/learning-status.js
+++ app/javascript/learning-status.js
@@ -23,3 +23,54 @@
 //     }
 //   }
 // })
+
+import 'whatwg-fetch'
+import CSRF from 'csrf'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.practice-status-buttons__button')
+
+  const pushStatus = (name, button) => {
+    const params = new FormData()
+    params.append('status', name)
+    const practice = document.querySelector('#practice')
+
+    fetch(`/api/practices/${practice.dataset.id}/learning.json`, {
+      method: 'PATCH',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': CSRF.getToken()
+      },
+      credentials: 'same-origin',
+      redirect: 'manual',
+      body: params
+    }).then((response) => {
+      if (response.ok) {
+        console.log('成功してます')
+      } else {
+        response.json().then((data) => {
+          alert(data.error)
+        })
+      }
+    })
+  }
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const clickedButton = event.target
+      if (clickedButton.classList.contains('js-not-complete')) {
+        console.log('未着手ボタンが押されました')
+        pushStatus('unstarted', clickedButton)
+      } else if (clickedButton.classList.contains('js-started')) {
+        console.log('着手ボタンが押されました')
+        pushStatus('started', clickedButton)
+      } else if (clickedButton.classList.contains('js-submitted')) {
+        console.log('提出済みボタンが押されました')
+        pushStatus('submitted', clickedButton)
+      } else if (clickedButton.classList.contains('js-complete')) {
+        console.log('修了ボタンが押されました')
+        pushStatus('complete', clickedButton)
+      }
+    })
+  })
+})
