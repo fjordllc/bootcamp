4f8283ab39831151cdf10d81fc80e76a42133a77
sekito <sekito1107gg@gmail.com>
2025-04-04 18:43:09 +0900

    vueを使わずにviewで静的な部分を代用できるようにした（動作はしない）

--- app/javascript/learning-status.js
+++ app/javascript/learning-status.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import LearningStatus from 'learning-status.vue'
+// import Vue from 'vue'
+// import LearningStatus from 'learning-status.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const learningStatuses = document.querySelectorAll('.js-learning-status')
-  if (learningStatuses) {
-    for (let i = 0; i < learningStatuses.length; i++) {
-      const learningStatus = learningStatuses[i]
+// document.addEventListener('DOMContentLoaded', () => {
+//   const learningStatuses = document.querySelectorAll('.js-learning-status')
+//   if (learningStatuses) {
+//     for (let i = 0; i < learningStatuses.length; i++) {
+//       const learningStatus = learningStatuses[i]
 
-      const practiceId = learningStatus.getAttribute('data-practice-id')
-      const status = learningStatus.getAttribute('data-status')
-      const submission = learningStatus.getAttribute('data-submission')
-      new Vue({
-        render: (h) =>
-          h(LearningStatus, {
-            props: {
-              practiceId: practiceId,
-              status: status,
-              submission: submission
-            }
-          })
-      }).$mount('.js-learning-status')
-    }
-  }
-})
+//       const practiceId = learningStatus.getAttribute('data-practice-id')
+//       const status = learningStatus.getAttribute('data-status')
+//       const submission = learningStatus.getAttribute('data-submission')
+//       new Vue({
+//         render: (h) =>
+//           h(LearningStatus, {
+//             props: {
+//               practiceId: practiceId,
+//               status: status,
+//               submission: submission
+//             }
+//           })
+//       }).$mount('.js-learning-status')
+//     }
+//   }
+// })
