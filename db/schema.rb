4bda7856762a74c9fd7dc02d383d186224eadae9
SuzukiShuntarou <da8801593@gmail.com>
2025-04-05 11:20:32 +0900

    コンフリクトを解消する過程で重複したカラムを修正した

--- db/schema.rb
+++ db/schema.rb
@@ -889,7 +889,6 @@ ActiveRecord::Schema.define(version: 2025_03_24_002043) do
     t.boolean "trainee", default: false, null: false
     t.string "customer_id"
     t.boolean "job_seeking", default: false, null: false
-    t.string "customer_id"
     t.string "subscription_id"
     t.boolean "mail_notification", default: true, null: false
     t.boolean "job_seeker", default: false, null: false
