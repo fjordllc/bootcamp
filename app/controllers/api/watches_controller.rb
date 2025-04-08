9a71e1c5fab07f6ce9d27ee48db868d61eb2ac0c
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-04-08 11:24:32 +0900

    コメントをすると自動的にwatchになる機能を追加

--- app/controllers/api/watches_controller.rb
+++ app/controllers/api/watches_controller.rb
@@ -7,4 +7,33 @@ class API::WatchesController < API::BaseController
     @watch = Watch.find(params[:id])
     render partial: 'watches/watch', locals: { watch: @watch }
   end
+
+  def create
+    watch_existence = Watch.exists?(
+      user_id: current_user.id,
+      watchable:
+    )
+    if watch_existence
+      message = "この#{watchable.class.model_name.human}はWatch済です。"
+      render json: { message: }, status: :unprocessable_entity
+    else
+      watch = Watch.create!(
+        user: current_user,
+        watchable:
+      )
+      render json: watch
+    end
+  end
+
+  def destroy
+    @watch = Watch.find(params[:id])
+    @watch.destroy
+    head :no_content
+  end
+
+  private
+
+  def watchable
+    params[:watchable_type].constantize.find_by(id: params[:watchable_id])
+  end
 end
