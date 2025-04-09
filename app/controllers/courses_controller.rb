9b273bca1312c3881d9b91faf5c702f4bd55eb5c
sekito <sekito1107gg@gmail.com>
2025-04-09 10:51:15 +0900

    コントローラーの修正

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 class CoursesController < ApplicationController
-  skip_before_action :require_active_user_login, raise: false
 
   def index
-    if logged_in?
-      @courses = Course.order(created_at: :desc)
-    else
-      @rails_course = Course.preload(categories: :practices).find_by(title: 'Railsエンジニア')
-      @frontend_course = Course.preload(categories: :practices).find_by(title: 'フロントエンドエンジニア')
-      render 'welcome/courses', layout: 'lp'
-    end
+    @courses = Course.order(created_at: :desc)
   end
 
   private
