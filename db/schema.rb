09454845d768d6db2cdfb397d12900473977c16b
komagata <komagata@gmail.com>
2025-04-09 14:27:43 +0900

    給付金対応コースへの登録フォームの対応
    - コースに給付金対応フラグを追加
    - 給付金対応コースへの登録の場合に請求書払いになるように修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_23_175738) do
+ActiveRecord::Schema.define(version: 2025_03_24_002043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -252,6 +252,7 @@ ActiveRecord::Schema.define(version: 2025_03_23_175738) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "published", default: false, null: false
+    t.boolean "grant", default: false, null: false
   end
 
   create_table "courses_categories", force: :cascade do |t|
