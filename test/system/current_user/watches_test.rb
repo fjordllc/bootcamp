04fc159cbb2fcccfeb2667e7010b777cfea60ae2
ayu-0505 <amehuri.boshi.sh@icloud.com>
2025-04-09 15:00:35 +0900

    テスト内容の修正（変更したrouteに合わせる）、テストに伴う修正（hidden位置を変更）

--- test/system/current_user/watches_test.rb
+++ test/system/current_user/watches_test.rb
@@ -8,43 +8,33 @@ class CurrentUser::WatchesTest < ApplicationSystemTestCase
     assert_equal 'Watch中 | FBC', title
   end
 
-  # 画面上では更新の完了がわからないため、やむを得ずsleepする
-  # 注意）安易に使用しないこと!! https://bootcamp.fjord.jp/pages/use-assert-text-instead-of-wait-for-vuejs
-  def wait_for_watch_change
-    sleep 1
-  end
-
-  # 下記3つのtestは/watches削除に伴い、WatchingTestより転記し修正した
   test 'show my watch list' do
     visit_with_auth '/current_user/watches', 'hajime'
     assert_no_text 'テストの質問1'
     question = questions(:question3)
     visit question_path(question)
     find('#watch-button').click
-    wait_for_watch_change
+    assert_text 'Watch中'
     visit '/current_user/watches'
     assert_text 'テストの質問1'
   end
 
   test 'watch index checkbox test' do
-    login_user 'kimura', 'testtest'
-    visit '/current_user/watches'
+    visit_with_auth '/current_user/watches', 'kimura'
     assert_no_selector '.card-list-item__option'
     find(:css, '#spec-edit-mode').set(true)
     assert_selector '.card-list-item__option'
   end
 
   test 'watch index page watchbutton test' do
-    login_user 'kimura', 'testtest'
     report = reports(:report1)
-    visit report_path(report)
+    visit_with_auth report_path(report), 'kimura'
     assert_text 'Watch中'
     visit '/current_user/watches'
     assert_text '作業週1日目'
     find(:css, '#spec-edit-mode').set(true)
     assert_selector '.card-list-item__option'
-    first('#watch-button').click
-    wait_for_watch_change
+    first('.a-watch-button').click
     assert_no_text '作業週1日目'
     visit report_path(report)
     assert_text 'Watch'
