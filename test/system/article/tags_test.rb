72899680188c7592e18805e0729bee92ef1f03a0
r.kawano <r.kawano@genbasupport.com>
2025-04-11 10:42:19 +0900

    システムテストを検証内容で分割するように修正

--- test/system/article/tags_test.rb
+++ test/system/article/tags_test.rb
@@ -3,6 +3,16 @@
 require 'application_system_test_case'
 
 class Article::TagsTest < ApplicationSystemTestCase
+  setup do
+    @article = Article.create!(
+      user: users(:mentormentaro),
+      title: 'タグ付きテスト記事',
+      body: '2つタグが付与された記事です',
+      tag_list: %w[FirstTag SecondTag],
+      wip: false
+    )
+  end
+
   test 'cat add tag to article' do
     visit_with_auth new_article_url, 'komagata'
     fill_in 'タイトル', with: 'タグ追加のテスト記事'
@@ -21,23 +31,21 @@ class Article::TagsTest < ApplicationSystemTestCase
     assert_equal tags, created_article.tag_list.sort
   end
 
-  test 'mentor can view and use tags' do
-    article = Article.create!(
-      user: users(:mentormentaro),
-      title: 'タグ付きテスト記事',
-      body: '2つタグが付与された記事です',
-      tag_list: %w[FirstTag SecondTag],
-      wip: false
-    )
-
-    visit_with_auth article_path(article), 'mentormentaro'
+  test 'mentor can view tags' do
+    visit article_path(@article)
+    assert_no_selector 'ul.a-tags__items li.a-tags__item'
+    visit_with_auth article_path(@article), 'mentormentaro'
     assert_selector 'ul.a-tags__items li.a-tags__item', text: 'FirstTag'
     assert_selector 'ul.a-tags__items li.a-tags__item', text: 'SecondTag'
+  end
+
+  test 'mentor can use tags' do
+    visit_with_auth article_path(@article), 'mentormentaro'
     click_link 'SecondTag'
     assert_text 'タグ付きテスト記事'
+  end
 
-    visit_with_auth article_path(article), 'hatsuno'
-    assert_no_selector 'ul.a-tags__items li.a-tags__item'
+  test 'non-mentor cannot access tag filter page' do
     visit articles_path(tag: 'SecretTag')
     assert_current_path articles_path
     assert_text '管理者・メンターとしてログインしてください'
