d7cbfcd71a75d0bd7c3d2a599bda93589712d81c
sekito <sekito1107gg@gmail.com>
2025-04-13 20:21:38 +0900

    複数の責務があったテストを分割して、適切なファイルに配置

--- test/system/mentor/courses_test.rb
+++ test/system/mentor/courses_test.rb
@@ -27,4 +27,20 @@ class Mentor::CoursesTest < ApplicationSystemTestCase
     end
     assert_text 'コースを更新しました。'
   end
+
+  test 'can published course' do
+    visit_with_auth "/mentor/courses/#{courses(:course2).id}/edit", 'komagata'
+    check 'course_published', allow_label_click: true, visible: false
+    click_button '内容を保存'
+    visit "/mentor/courses/#{courses(:course2).id}/edit"
+    assert has_checked_field?('course_published', visible: false)
+  end
+
+  test 'can hide course' do
+    visit_with_auth "/mentor/courses/#{courses(:course1).id}/edit", 'komagata'
+    uncheck 'course_published', allow_label_click: true, visible: false
+    click_button '内容を保存'
+    visit "/mentor/courses/#{courses(:course1).id}/edit"
+    assert_no_checked_field('course_published', visible: false)
+  end
 end
