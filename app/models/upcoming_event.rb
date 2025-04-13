5c4527dd5edc583b2d88a6c1f660710b00352b21
appsu-mi <natubate870@gmail.com>
2025-04-13 21:17:53 +0900

    関係者の近日開催イベント欄にペアワークを表示するように

--- app/models/upcoming_event.rb
+++ app/models/upcoming_event.rb
@@ -5,6 +5,12 @@ class UpcomingEvent
 
   delegate :participants, to: :original_event
 
+  TARGET_TO_DATE = {
+    today: Date.current,
+    tomorrow: Date.current + 1,
+    day_after_tomorrow: Date.current + 2
+  }.freeze
+
   def initialize(event, scheduled_date)
     @original_event = event
     @scheduled_date = scheduled_date
@@ -47,7 +53,7 @@ class UpcomingEvent
 
   class << self
     def build_group(date_key)
-      date = date_key_to_date_class(date_key)
+      date = TARGET_TO_DATE[date_key]
       Rails.logger.debug("build_group date: #{date}")
       upcoming_events = original_events_scheduled_on(date).map { |e| UpcomingEvent.new(e, date) }
 
@@ -74,16 +80,6 @@ class UpcomingEvent
     end
 
     private
-
-    def date_key_to_date_class(date_key)
-      table = {
-        today: Time.zone.today,
-        tomorrow: Time.zone.today + 1.day,
-        day_after_tomorrow: Time.zone.today + 2.days
-      }
-
-      table[date_key.to_sym]
-    end
 
     def original_events_scheduled_on(date)
       [Event, RegularEvent].map do |model|
