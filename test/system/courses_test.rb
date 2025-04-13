ac3d0b0a8dc80ce0fc12bcdea8b2d2819273d6ee
sekito <sekito1107gg@gmail.com>
2025-04-13 18:38:24 +0900

    重複していたテストケースを削除

--- test/system/courses_test.rb
+++ test/system/courses_test.rb
@@ -8,27 +8,6 @@ class CoursesTest < ApplicationSystemTestCase
     assert_equal 'コース一覧 | FBC', title
   end
 
-  test 'create course' do
-    visit_with_auth '/mentor/courses/new', 'komagata'
-    within 'form[name=course]' do
-      fill_in 'course[title]', with: 'テストコース'
-      fill_in 'course[description]', with: 'テストのコースです。'
-      click_button '内容を保存'
-    end
-    assert_text 'コースを作成しました。'
-  end
-
-  test 'update course' do
-    visit_with_auth "/mentor/courses/#{courses(:course1).id}/edit", 'komagata'
-    within 'form[name=course]' do
-      fill_in 'course[title]', with: 'テストコース'
-      find(:css, '#checkbox-published-course').set(true)
-      fill_in 'course[description]', with: 'テストのコースです。'
-      click_button '内容を保存'
-    end
-    assert_text 'コースを更新しました。'
-  end
-
   test 'show published courses' do
     visit_with_auth '/courses', 'hajime'
     assert_link courses(:course1).title
