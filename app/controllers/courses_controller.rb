c3829c235ceb0ca91f0fd632eade11e0ee5dfd39
sekito <sekito1107gg@gmail.com>
2025-04-14 15:56:52 +0900

    コンフリクトの解消

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,16 +4,4 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.order(created_at: :desc)
   end
-
-  private
-
-  def course_params
-    params.require(:course).permit(
-      :title,
-      :description,
-      :published,
-      :grant,
-      category_ids: []
-    )
-  end
 end
