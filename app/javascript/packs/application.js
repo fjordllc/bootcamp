d8644ee6f10b4b510a2b659db2598868036e79e6
r.kawano <r.kawano@genbasupport.com>
2025-04-14 19:30:09 +0900

    タグ入力のショートカット機能を作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -78,6 +78,7 @@ import '../coding_tests_sort.js'
 import '../watches.js'
 import '../watch-toggle.js'
 import '../diploma-upload.js'
+import '../tag-shortcut.js'
 
 import VueMounter from '../VueMounter.js'
 import Questions from '../components/questions.vue'
