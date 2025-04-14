31cbdebbffe9b05dc85ece3dcebc11bd8bee5e05
SuzukiShuntarou <da8801593@gmail.com>
2025-04-14 19:43:41 +0900

    コメントアウトにTODOを追記した

--- app/helpers/page_tabs/practices_helper.rb
+++ app/helpers/page_tabs/practices_helper.rb
@@ -8,7 +8,7 @@ module PageTabs
       tabs << { name: '日報', link: practice_reports_path(practice), count: practice.reports.length }
       tabs << { name: '質問', link: practice_questions_path(practice), count: practice.questions.length }
       tabs << { name: 'Docs', link: practice_pages_path(practice), count: practice.pages.length }
-      # 動画リンクを隠した状態でのリリース。
+      # TODO: 動画機能の完成時に公開。動画リンクを隠した状態でのリリース。
       # tabs << { name: '動画', link: practice_movies_path(practice), count: practice.movies.length }
       tabs << { name: '提出物', link: practice_products_path(practice) }
       tabs << { name: '模範解答', link: practice_submission_answer_path(practice) } if practice.submission_answer.present?
