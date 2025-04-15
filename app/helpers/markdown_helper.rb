2634d40b13a88e0ff98dd7992c3114c9c79463c5
su-su-su-su <suzukikoji0503@gmail.com>
2025-04-15 20:59:30 +0900

    markdown_to_plain_textのコードを元のコードに修正した。

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
+require 'redcarpet'
+require 'nokogiri'
+
 module MarkdownHelper
   def markdown_to_plain_text(markdown_content)
-    html = CommonMarker.render_html(markdown_content)
-    Nokogiri::HTML(html).text.strip
+    return '' if markdown_content.blank?
+    renderer = Redcarpet::Render::HTML.new(escape_html: true)
+    markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true, strikethrough: true, fenced_code_blocks: true, no_intra_emphasis: true, space_after_headers: true)
+    html_content = markdown.render(markdown_content.to_s)
+    Nokogiri::HTML::DocumentFragment.parse(html_content).text.strip
   end
 end
