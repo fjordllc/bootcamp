c2d13deceb2bbea9101e4bd35932c611d6a922c3
kousei-ya <k.yano@genbasupport.com>
2025-04-16 13:50:08 +0900

    querySelectorAll()をquerySelector()に変更し、forEach()を使わないように修正

--- app/javascript/action_completed_button.js
+++ app/javascript/action_completed_button.js
@@ -2,60 +2,55 @@ import CSRF from 'csrf'
 import { toast } from './vanillaToast'
 
 document.addEventListener('DOMContentLoaded', function () {
-  const buttons = document.querySelectorAll('.check-button')
+  const button = document.querySelector('.check-button')
+  if (!button) {
+    return
+  }
+  const commentableId = button.dataset.commentableId
+  button.addEventListener('click', function () {
+    const isActionCompleted = button.classList.contains('is-muted-borderd')
 
-  buttons.forEach((button) => {
-    const commentableId = button.dataset.commentableId
-
-    if (!commentableId) return
-
-    button.addEventListener('click', function () {
-      const isActionCompleted = button.classList.contains('is-muted-borderd')
-
-      fetch(`/api/talks/${commentableId}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8',
-          'X-Requested-With': 'XMLHttpRequest',
-          'X-CSRF-Token': CSRF.getToken()
-        },
-        body: JSON.stringify({
-          talk: { action_completed: !isActionCompleted }
-        })
+    fetch(`/api/talks/${commentableId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-Token': CSRF.getToken()
+      },
+      body: JSON.stringify({
+        talk: { action_completed: !isActionCompleted }
       })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok')
-          }
-        })
-        .then(() => {
-          const newButtonText = isActionCompleted
-            ? '対応済にする'
-            : '対応済です'
-          const iconClass = isActionCompleted ? 'fa-redo' : 'fa-check'
-          const newMessage = isActionCompleted
-            ? '返信が完了し次は相談者からのアクションの待ちの状態になったとき、もしくは、相談者とのやりとりが一通り完了した際は、このボタンをクリックして対応済のステータスに変更してください。'
-            : 'お疲れ様でした！相談者から次のアクションがあった際は、自動で未対応のステータスに変更されます。再度このボタンをクリックすると、未対応にステータスに戻ります。'
-
-          button.innerHTML = `<i class="fas ${iconClass}"></i> ${newButtonText}`
-          button.classList.toggle('is-warning', isActionCompleted)
-          button.classList.toggle('is-muted-borderd', !isActionCompleted)
-
-          const description = button
-            .closest('.thread-comment-form')
-            .querySelector('.action-completed__description p')
-          if (description) {
-            description.innerHTML = newMessage
-          }
-
-          const tostMessage = isActionCompleted
-            ? '未対応にしました'
-            : '対応済みにしました'
-          toast(tostMessage, 'success')
-        })
-        .catch((error) => {
-          console.warn(error)
-        })
     })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+      })
+      .then(() => {
+        const newButtonText = isActionCompleted ? '対応済にする' : '対応済です'
+        const iconClass = isActionCompleted ? 'fa-redo' : 'fa-check'
+        const newMessage = isActionCompleted
+          ? '返信が完了し次は相談者からのアクションの待ちの状態になったとき、もしくは、相談者とのやりとりが一通り完了した際は、このボタンをクリックして対応済のステータスに変更してください。'
+          : 'お疲れ様でした！相談者から次のアクションがあった際は、自動で未対応のステータスに変更されます。再度このボタンをクリックすると、未対応にステータスに戻ります。'
+
+        button.innerHTML = `<i class="fas ${iconClass}"></i> ${newButtonText}`
+        button.classList.toggle('is-warning', isActionCompleted)
+        button.classList.toggle('is-muted-borderd', !isActionCompleted)
+
+        const description = button
+          .closest('.thread-comment-form')
+          .querySelector('.action-completed__description p')
+        if (description) {
+          description.innerHTML = newMessage
+        }
+
+        const tostMessage = isActionCompleted
+          ? '未対応にしました'
+          : '対応済みにしました'
+        toast(tostMessage, 'success')
+      })
+      .catch((error) => {
+        console.warn(error)
+      })
   })
 })
