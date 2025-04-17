0a81c9420d3b3b3f1c80f684990bf25e4f5971c8
r.kawano <r.kawano@genbasupport.com>
2025-04-17 11:31:05 +0900

    `not_logged_in_test.rb`のFlakyな箇所を修正

--- test/system/page/not_logged_in_test.rb
+++ test/system/page/not_logged_in_test.rb
@@ -4,10 +4,11 @@ require 'application_system_test_case'
 
 class Page::NotLoggedInTest < ApplicationSystemTestCase
   test 'when not logged in user access docs, meta description is displayed' do
+    logout
     target_page = pages(:page1)
     visit page_path(target_page)
-    assert_selector 'head', visible: false do
-      assert_selector "meta[name='description'][content='オンラインプログラミングスクール「フィヨルドブートキャンプ」のドキュメント「#{target_page.title}」のページです。']", visible: false
-    end
+    meta = page.find("meta[name='description']", visible: false, match: :first)
+    expected_content = "オンラインプログラミングスクール「フィヨルドブートキャンプ」のドキュメント「#{target_page.title}」のページです。"
+    assert_equal expected_content, meta[:content]
   end
 end
