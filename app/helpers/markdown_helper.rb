a6f193bee29e0b6ba420e01d97f43d933f1e8719
su-su-su-su <suzukikoji0503@gmail.com>
2025-04-22 00:17:17 +0900

    md2htmlメソッドを復元した

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -4,10 +4,26 @@ require 'redcarpet'
 require 'nokogiri'
 
 module MarkdownHelper
+
+  def md2html(text)
+    return '' if text.nil?
+
+    html = Kramdown::Document.new(text, input: 'GFM', hard_wrap: true).to_html
+    doc = Nokogiri::HTML::DocumentFragment.parse(html)
+    doc.css('img').each do |img|
+      img.remove_attribute('width')
+      img.remove_attribute('height')
+      img['style'] = [img['style'], 'max-width: 100%;'].compact.join(' ')
+    end
+    raw(doc.to_html) # rubocop:disable Rails/OutputSafety
+  end
+
   def markdown_to_plain_text(markdown_content)
     return '' if markdown_content.blank?
+
     renderer = Redcarpet::Render::HTML.new(escape_html: true)
-    markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true, strikethrough: true, fenced_code_blocks: true, no_intra_emphasis: true, space_after_headers: true)
+    markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true, strikethrough: true, fenced_code_blocks: true, no_intra_emphasis: true,
+                                                 space_after_headers: true)
     html_content = markdown.render(markdown_content.to_s)
     Nokogiri::HTML::DocumentFragment.parse(html_content).text.strip
   end
