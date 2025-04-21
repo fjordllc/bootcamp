ff5f6047abb33865689d429c7e5e0261371a2b00
sekito <sekito1107gg@gmail.com>
2025-04-21 22:17:17 +0900

    自作プラグインの追加と呼び出しの変更

--- app/javascript/markdown-initializer.js
+++ app/javascript/markdown-initializer.js
@@ -13,9 +13,7 @@ import MarkDownItContainerDetails from 'markdown-it-container-details'
 import MarkDownItLinkAttributes from 'markdown-it-link-attributes'
 import MarkDownItContainerSpeak from 'markdown-it-container-speak'
 import ReplaceLinkToCard from 'replace-link-to-card'
-import MarkdownItSanitizer from 'markdown-it-sanitizer'
-import MarkdownItOnlineVideo from './markdown-it-online-video'
-import MarkdownItLocalVideo from './markdown-it-local-video'
+import MarkdownItSanitize from 'markdown-it-sanitize'
 
 export default class {
   replace(selector) {
@@ -58,9 +56,7 @@ export default class {
       }
     })
     md.use(MarkDownItContainerSpeak)
-    md.use(MarkdownItSanitizer)
-    md.use(MarkdownItOnlineVideo)
-    md.use(MarkdownItLocalVideo)
+    md.use(MarkdownItSanitize)
     return md.render(text)
   }
 }
---
+++ app/javascript/markdown-it-sanitize.js
@@ -0,0 +1,41 @@
+export default (md) => {
+  const escapeTags = ['style']
+  const escapeAttributes = ['onload']
+
+  md.core.ruler.push('sanitize', function (state) {
+    for (const token of state.tokens) {
+      if (['html_block', 'html_inline'].includes(token.type)) {
+        token.content = escapeHtmlContent(token.content, escapeTags, escapeAttributes)
+      } else if (token.type === 'inline' && token.children) {
+        for (const child of token.children) {
+            child.content = escapeHtmlContent(child.content, escapeTags, escapeAttributes)
+        }
+      }
+    }
+  })
+}
+
+function escapeHtmlContent(html, escapeTags = [], escapeAttributes = []) {
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(`<body>${html}</body>`, 'text/html')
+
+  doc.querySelectorAll('*').forEach(el => {
+    if (shouldEscape(el, escapeTags, escapeAttributes)) {
+      el.replaceWith(doc.createTextNode(el.outerHTML))
+    }
+  })
+
+  return doc.body.innerHTML
+}
+
+function shouldEscape(el, escapeTags, escapeAttributes) {
+  const tag = el.tagName.toLowerCase()
+  const isDangerousTag = escapeTags.includes(tag)
+
+  const hasDangerousAttribute = [...el.attributes].some(attr => {
+    const attrName = attr.name.toLowerCase()
+    return escapeAttributes.includes(attrName)
+  })
+
+  return isDangerousTag || hasDangerousAttribute
+}
