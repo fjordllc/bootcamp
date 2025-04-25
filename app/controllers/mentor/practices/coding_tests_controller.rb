c15e112cdbe5eca16b453cadb167a5be3dab22a4
sekito <sekito1107gg@gmail.com>
2025-04-25 21:50:00 +0900

    テストの作成と、lintツールの整形

--- app/controllers/mentor/practices/coding_tests_controller.rb
+++ app/controllers/mentor/practices/coding_tests_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class Mentor::Practices::CodingTestsController < ApplicationController
   before_action :require_mentor_login
 
   def index
     @practice = Practice.find(params[:practice_id])
     @practices_coding_tests = @practice
-                          .coding_tests
-                          .order(:position)
+                              .coding_tests
+                              .order(:position)
   end
 end
