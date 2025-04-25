8b0f849656840ff284fa93b779d4954fab91ce7a
sekito <sekito1107gg@gmail.com>
2025-04-25 20:54:25 +0900

    並び替え機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -70,6 +70,7 @@ import '../copy-url.js'
 import '../survey_result_chart.js'
 import '../footprints.js'
 import '../article-target.js'
+import '../coding_tests_sort.js'
 
 import VueMounter from '../VueMounter.js'
 import Questions from '../components/questions.vue'
