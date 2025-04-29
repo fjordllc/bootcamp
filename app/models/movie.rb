6763f66fd3f10044960ea8862947653991527e33
reckyy <srreki98@gmail.com>
2025-04-29 18:03:05 +0900

    動画一覧にタグ一覧と絞り込み機能を追加

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,6 +20,7 @@ class Movie < ApplicationRecord
   validates :movie_data, presence: true
 
   scope :wip, -> { where(wip: true) }
+  scope :by_tag, ->(tag) { tagged_with(tag) if tag }
 
   after_create_commit :start_transcode_job, on: :create
 
