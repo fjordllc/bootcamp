86e941239fb065fe00f3fd5507516dfdd5215ea5
sekito <sekito1107gg@gmail.com>
2025-05-06 10:03:31 +0900

    データの変更をdata-migrateを使うように変更

--- db/migrate/20250411103935_reverse_show_mentor_profile_for_users.rb
+++
@@ -1,9 +0,0 @@
-class ReverseShowMentorProfileForUsers < ActiveRecord::Migration[6.1]
-  def up
-    User.update_all("show_mentor_profile = NOT show_mentor_profile")
-  end
-
-  def down
-    User.update_all("show_mentor_profile = NOT show_mentor_profile")
-  end
-end
