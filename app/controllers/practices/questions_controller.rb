8a8d1f060c1d39f8ccf5426b865eb42f01d320ef
Kushizaki <kushimegu288@gmail.com>
2025-05-08 15:58:23 +0900

    静的なページの実装

--- app/controllers/practices/questions_controller.rb
+++ app/controllers/practices/questions_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Practices::QuestionsController < ApplicationController
+  QuestionsProperty = Struct.new(:title, :empty_message)
+
   def index
     @practice = Practice.find(params[:practice_id])
     @empty_message = empty_message
+    @questions = @practice.questions.includes(%i[correct_answer practice answers tag_taggings tags]).order(created_at: :desc).page(params[:page])
+    @questions_property = QuestionsProperty.new('全ての質問', '質問はありません。')
   end
 
   private
