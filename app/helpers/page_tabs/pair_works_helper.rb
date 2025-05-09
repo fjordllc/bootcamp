53ed00ed0337d0002e606edca091c1c95b9a9e96
machida <machida@fjord.jp>
2025-05-09 23:24:26 +0900

    ペアワークの相手をペアに名称変更、確定ブロックのベースを作成

--- app/helpers/page_tabs/pair_works_helper.rb
+++ app/helpers/page_tabs/pair_works_helper.rb
@@ -4,8 +4,8 @@ module PageTabs
   module PairWorksHelper
     def pair_works_page_tabs
       tabs = []
-      tabs << { name: '相手募集中', link: pair_works_path(target: 'not_solved'), badge: PairWork.not_solved.not_wip.size }
-      tabs << { name: '相手確定', link: pair_works_path(target: 'solved') }
+      tabs << { name: 'ペア募集中', link: pair_works_path(target: 'not_solved'), badge: PairWork.not_solved.not_wip.size }
+      tabs << { name: 'ペア確定', link: pair_works_path(target: 'solved') }
       tabs << { name: '全て', link: pair_works_path }
       render PageTabsComponent.new(tabs:, active_tab: pair_work_active_tab)
     end
@@ -15,9 +15,9 @@ module PageTabs
     def pair_work_active_tab
       case params[:target]
       when 'not_solved'
-        '相手募集中'
+        'ペア募集中'
       when 'solved'
-        '相手確定'
+        'ペア確定'
       else
         '全て'
       end
