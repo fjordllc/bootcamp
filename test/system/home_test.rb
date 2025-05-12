48ea2ee28907c74ec4791308104387111fc3c472
hiromisugie <lozz.gggg@gmail.com>
2025-05-12 09:33:47 +0900

    本PRの変更に伴って既存テストを修正

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -257,6 +257,7 @@ class HomeTest < ApplicationSystemTestCase
       today_events_texts = [
         { category: '特別', title: '直近イベントの表示テスト用(当日)', start_at: '2017年04月03日(月) 09:00' },
         { category: '特別', title: 'kimura専用イベント', start_at: '2017年04月03日(月) 09:00' },
+        { category: '輪読会', title: '月曜日開催で10:00に終了する定期イベント', start_at: '2017年04月03日(月) 09:00' },
         { category: '質問', title: '質問・雑談タイム', start_at: '2017年04月03日(月) 16:00' },
         { category: '輪読会', title: 'ダッシュボード表示確認用テスト定期イベント', start_at: '2017年04月03日(月) 21:00' },
         { category: '輪読会', title: 'ダッシュボード表示確認用テスト定期イベント', start_at: '2017年04月03日(月) 21:00' }
