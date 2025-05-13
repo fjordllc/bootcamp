e82e92d2800431b16a8016c190fd1e0ae1bad2d8
sekito <sekito1107gg@gmail.com>
2025-05-13 12:07:04 +0900

    rubocopの修正に伴う各種変更と、使用する値を本番用に変更

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-require "google/cloud/video/transcoder"
-require "open-uri"
 
 class MoviesController < ApplicationController
   before_action :set_movie, only: %i[show edit update destroy]
