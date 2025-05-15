243d8ea8d787d79a7c9fab71c6c5510397b55e1a
sekito <sekito1107gg@gmail.com>
2025-05-16 08:20:44 +0900

    命名を整理

--- test/models/transcoder/client_test.rb
+++ test/models/transcoder/client_test.rb
@@ -11,7 +11,7 @@ module Transcoder
       @mock_service = Object.new
     end
 
-    test '#start' do
+    test '#create_job' do
       @mock_service.define_singleton_method(:create_job) do |parent:, job:|
         _parent = parent
         _job = job
@@ -20,19 +20,19 @@ module Transcoder
 
       Google::Cloud::Video::Transcoder.stub :transcoder_service, @mock_service do
         client = Transcoder::Client.new(@movie)
-        response = client.start
+        response = client.create_job
         assert_equal DEFAULT_JOB_NAME, response.name
       end
     end
 
-    test '#fetch' do
+    test '#get_job' do
       @mock_service.define_singleton_method(:get_job) do |name:|
         OpenStruct.new(name:)
       end
 
       Google::Cloud::Video::Transcoder.stub :transcoder_service, @mock_service do
         client = Transcoder::Client.new(@movie)
-        response = client.fetch(DEFAULT_JOB_NAME)
+        response = client.get_job(DEFAULT_JOB_NAME)
         assert_equal DEFAULT_JOB_NAME, response.name
       end
     end
