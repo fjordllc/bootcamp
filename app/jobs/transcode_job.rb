b4fae14bdaf3bb96be9a1a84904db33ddc4e90db
sekito <sekito1107gg@gmail.com>
2025-05-18 22:19:34 +0900

    クラス名の変更に伴う各種修正

--- app/jobs/transcode_job.rb
+++ app/jobs/transcode_job.rb
@@ -4,6 +4,6 @@ class TranscodeJob < ApplicationJob
   queue_as :default
 
   def perform(movie, job_name = nil)
-    Transcoder::Driver.new(movie, job_name).call
+    Transcoder::Job.new(movie, job_name).call
   end
 end
