74994ac58c45d4d2245397a0b8e83244bb7bd16a
appsu-mi <natubate870@gmail.com>
2025-05-19 10:37:42 +0900

    ペアワークのdesctiptionにNotNullを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_18_144325) do
+ActiveRecord::Schema.define(version: 2025_05_19_012857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -613,7 +613,7 @@ ActiveRecord::Schema.define(version: 2025_06_18_144325) do
 
   create_table "pair_works", force: :cascade do |t|
     t.string "title", null: false
-    t.text "description"
+    t.text "description", null: false
     t.datetime "reserved_at"
     t.bigint "user_id", null: false
     t.bigint "practice_id"
