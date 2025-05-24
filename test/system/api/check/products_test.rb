5ff7f192af0778dd052e92ab1ef5dd26af622d7d
Rikuoh <mail@riq0h.jp>
2025-05-24 09:10:23 +0900

    提出物のOKボタンの文言を変更した

--- test/system/api/check/products_test.rb
+++ test/system/api/check/products_test.rb
@@ -5,21 +5,21 @@ require 'application_system_test_case'
 class Check::ProductsTest < ApplicationSystemTestCase
   test 'user can see stamp' do
     visit_with_auth "/products/#{products(:product3).id}", 'kimura'
-    assert_text '確認済'
+    assert_text '合格'
   end
 
   test 'success product checking' do
     visit_with_auth "/products/#{products(:product1).id}", 'komagata'
-    click_button '提出物を確認'
-    assert_text '確認済'
-    assert has_button? '提出物の確認を取り消す'
+    click_button '提出物を合格にする'
+    assert_text '合格'
+    assert has_button? '提出物の合格を取り消す'
   end
 
   test 'when product checked learning status to complete' do
     visit_with_auth "/products/#{products(:product1).id}", 'komagata'
-    click_button '提出物を確認'
-    assert_text '確認済'
-    assert has_button? '提出物の確認を取り消す'
+    click_button '提出物を合格にする'
+    assert_text '合格'
+    assert has_button? '提出物の合格を取り消す'
 
     visit_with_auth "/practices/#{products(:product1).practice.id}", 'mentormentaro'
     assert_text '修了しています'
@@ -27,30 +27,29 @@ class Check::ProductsTest < ApplicationSystemTestCase
 
   test 'success product checking cancel' do
     visit_with_auth "/products/#{products(:product1).id}", 'komagata'
-    click_button '提出物を確認'
-    click_button '提出物の確認を取り消す'
-    assert_no_text '確認済'
-    assert has_button? '提出物を確認'
+    click_button '提出物を合格にする'
+    click_button '提出物の合格を取り消す'
+    assert has_button? '提出物を合格にする'
   end
 
   test 'comment and check product' do
     visit_with_auth "/products/#{products(:product1).id}", 'komagata'
-    fill_in 'new_comment[description]', with: '提出物でcomment+確認OKにするtest'
+    fill_in 'new_comment[description]', with: '提出物でcomment+合格にするtest'
     page.accept_confirm do
-      click_on '確認OKにする'
+      click_on '合格にする'
     end
-    assert_text '確認済'
-    assert_text '提出物でcomment+確認OKにするtest'
+    assert_text '合格'
+    assert_text '提出物でcomment+合格にするtest'
   end
 
   test 'comment and check product by mentor' do
     visit_with_auth "/products/#{products(:product1).id}", 'mentormentaro'
-    fill_in 'new_comment[description]', with: '提出物でcomment+確認OKにするtest'
+    fill_in 'new_comment[description]', with: '提出物でcomment+合格にするtest'
     page.accept_confirm do
-      click_on '確認OKにする'
+      click_on '合格にする'
     end
-    assert_text '確認済'
-    assert_text '提出物でcomment+確認OKにするtest'
+    assert_text '合格'
+    assert_text '提出物でcomment+合格にするtest'
   end
 
   test 'display error message when checking confirmed product' do
@@ -63,12 +62,13 @@ class Check::ProductsTest < ApplicationSystemTestCase
     end
 
     using_session :mentormentaro do
-      click_button '提出物を確認'
-      assert_text '確認済'
+      click_button '提出物を合格にする'
+      assert_text '合格'
+      sleep 1
     end
 
     using_session :komagata do
-      click_button '提出物を確認'
+      click_button '提出物を合格にする'
       assert_text 'この提出物は確認済です'
     end
   end
