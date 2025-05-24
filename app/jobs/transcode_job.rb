82c299d0ddf494ff3445b64b0dd202c414b53353
sekito <sekito1107gg@gmail.com>
2025-05-24 09:33:21 +0900

    activejobがインスタンス変数を持たないように修正

--- app/jobs/transcode_job.rb
+++ app/jobs/transcode_job.rb
@@ -8,44 +8,43 @@ class TranscodeJob < ApplicationJob
   def perform(movie, job_name = nil)
     return unless Rails.application.config.transcoder['enable']
 
-    @movie = movie
-    api_client = Transcoder::APIClient.new(@movie)
-    @job_name = job_name || @api_client.create_transcoding_job
+    api_client = Transcoder::APIClient.new(movie)
+    job_name ||= api_client.create_transcoding_job
 
-    job_state = @api_client.job_state(@job_name)
+    job_state = api_client.job_state(job_name)
     case job_state
-    when :succeeded then attach_transcoded_movie
-    when :failed    then log_state(:failed)
-    when :cancelled then log_state(:cancelled)
-    when :active    then schedule_polling
+    when :succeeded then attach_transcoded_movie(movie)
+    when :failed    then log_state(:failed, movie)
+    when :cancelled then log_state(:cancelled, movie)
+    when :active    then schedule_polling(movie, job_name)
     else
-      log_state(:unknown)
+      log_state(:unknown, movie)
     end
   end
 
   private
 
-  def attach_transcoded_movie
-    file = Transcoder::TranscodedMovieFile.new(@movie)
-    @movie.movie_data.attach(io: file.data, filename: "#{@movie.id}.mp4")
+  def attach_transcoded_movie(movie)
+    file = Transcoder::TranscodedMovieFile.new(movie)
+    movie.movie_data.attach(io: file.data, filename: "#{movie.id}.mp4")
     file.cleanup
   end
 
-  def log_state(state)
+  def log_state(state, movie)
     level = %i[failed cancelled].include?(state) ? :error : :warn
     message = case state
               when :failed
-                "Transcoding failed for Movie #{@movie.id}"
+                "Transcoding failed for Movie #{movie.id}"
               when :cancelled
-                "Transcoding job for Movie #{@movie.id} was cancelled."
+                "Transcoding job for Movie #{movie.id} was cancelled."
               else
-                "Unknown transcoder job state for Movie #{@movie.id}. No further action taken."
+                "Unknown transcoder job state for Movie #{movie.id}. No further action taken."
               end
 
     Rails.logger.send(level, message)
   end
 
-  def schedule_polling
-    TranscodeJob.set(wait: POLLING_INTERVAL).perform_later(@movie, @job_name)
+  def schedule_polling(movie, job_name)
+    TranscodeJob.set(wait: POLLING_INTERVAL).perform_later(movie, job_name)
   end
 end
