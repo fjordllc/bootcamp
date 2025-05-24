a58aeec7b99fd4fdfd43f8e631328d67e2673103
SuzukiShuntarou <da8801593@gmail.com>
2025-05-24 17:04:55 +0900

    メール送信後にフラグをtrueにするように変更

--- app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
+++ app/controllers/scheduler/daily/send_mail_to_hibernation_user_controller.rb
@@ -11,8 +11,8 @@ class Scheduler::Daily::SendMailToHibernationUserController < SchedulerControlle
   def send_mail_one_week_before_auto_retire
     User.unretired.hibernated_for(User::HIBERNATION_LIMIT_BEFORE_ONE_WEEK).each do |user|
       if user.auto_retire && !user.sent_student_before_auto_retire_mail
-        user.mark_mail_as_sent_before_auto_retire
         UserMailer.one_week_before_auto_retire(user).deliver_now
+        user.mark_mail_as_sent_before_auto_retire
       end
     rescue Postmark::InactiveRecipientError => e
       Rails.logger.warn "[Postmark] 受信者由来のエラーのためメールを送信できませんでした。：#{e.message}"
