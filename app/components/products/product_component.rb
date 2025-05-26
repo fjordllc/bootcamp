1de940a64bbf4b29e069e26ff044fdaecf45db1e
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-05-26 13:56:31 +0900

    動的にnew-userかstandard-userクラスを作成するjoining_status_classを追加

--- app/components/products/product_component.rb
+++ app/components/products/product_component.rb
@@ -15,6 +15,10 @@ class Products::ProductComponent < ViewComponent::Base
     "is-#{@product.user.primary_role}"
   end
 
+  def joining_status_class
+    "is-#{@product.user.joining_status}"
+  end
+
   def practice_title
     "#{@product.practice.title}の提出物"
   end
