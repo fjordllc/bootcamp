cffce5de36da221859826b4607fe18f95d645f1e
Rikuoh <mail@riq0h.jp>
2025-05-26 23:01:17 +0900

    テストの追加およびリファクタリング

--- app/controllers/users/activity_times_controller.rb
+++ app/controllers/users/activity_times_controller.rb
@@ -8,18 +8,12 @@ class Users::ActivityTimesController < ApplicationController
     @target_day_of_week = params[:day_of_week] || @current_day_of_week
     @target_hour = params[:hour] || @current_hour
 
-    Rails.logger.debug "ActivityTimes: day_of_week=#{@target_day_of_week}, hour=#{@target_hour}"
-
     target_users = fetch_users_by_activity_time(@target_day_of_week, @target_hour)
-    Rails.logger.debug "ActivityTimes: target_users.count=#{target_users.count}"
 
     @users = target_users
              .page(params[:page]).per(PAGER_NUMBER)
              .preload(:avatar_attachment, :course, :taggings)
              .order(updated_at: :desc)
-
-    Rails.logger.debug "ActivityTimes: @users.count=#{@users.count}"
-    Rails.logger.debug "ActivityTimes: @users.total_count=#{@users.total_count}"
   end
 
   private
@@ -31,28 +25,13 @@ class Users::ActivityTimesController < ApplicationController
   end
 
   def fetch_users_by_activity_time(day_of_week, hour)
-    return User.none unless valid_day_of_week?(day_of_week) && valid_hour?(hour)
+    return User.none unless day_of_week.to_s.match?(/\A[0-6]\z/) && hour.to_s.match?(/\A(?:[0-9]|1[0-9]|2[0-3])\z/)
 
-    # 曜日名を数値から文字列に変換
-    week_day_name = convert_day_of_week_to_name(day_of_week)
+    week_day_name = helpers.day_of_the_week[day_of_week.to_i]
 
-    # 指定された曜日と時間に活動時間を設定しているユーザーを取得
-    User.joins(:learning_time_frames)
+    User.joins(learning_time_frames_users: :learning_time_frame)
         .where(learning_time_frames: { week_day: week_day_name, activity_time: hour.to_i })
-        .students_and_trainees
+        .students_trainees_mentors_and_admins
         .distinct
-  end
-
-  def convert_day_of_week_to_name(day_of_week)
-    day_names = %w[日 月 火 水 木 金 土]
-    day_names[day_of_week.to_i]
-  end
-
-  def valid_day_of_week?(day_of_week)
-    day_of_week.to_s.match?(/\A[0-6]\z/)
-  end
-
-  def valid_hour?(hour)
-    hour.to_s.match?(/\A(?:[0-9]|1[0-9]|2[0-3])\z/)
   end
 end
