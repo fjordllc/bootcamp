e0ea6e60d11336c5a502d7d36207c5ddaa2641ff
sharoa119 <sau.rin1109@gmail.com>
2025-05-27 11:54:06 +0900

    user_icon コンポーネントを作成

---
+++ test/components/user_icon_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class UserIconComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(UserIconComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
