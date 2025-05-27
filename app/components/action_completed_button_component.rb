cea621ca281e474ac9af7d24de31c3cff63f2a86
kousei-ya <k.yano@genbasupport.com>
2025-05-27 17:55:33 +0900

    不要なキーワード引数を削除

--- app/components/action_completed_button_component.rb
+++ app/components/action_completed_button_component.rb
@@ -3,7 +3,7 @@
 class ActionCompletedButtonComponent < ViewComponent::Base
   attr_reader :is_action_completed, :commentable_id
 
-  def initialize(is_initial_action_completed:, commentable_id:)
+  def initialize(is_initial_action_completed, commentable_id)
     @is_action_completed = is_initial_action_completed
     @commentable_id = commentable_id
   end
