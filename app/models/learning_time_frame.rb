8ac9c665a242b9c42acc76d27320d99f9e1428b1
sharoa119 <sau.rin1109@gmail.com>
2025-05-28 16:10:05 +0900

    ダッシュボードに学習時間が該当するユーザーのアイコンを表示する

--- app/models/learning_time_frame.rb
+++ app/models/learning_time_frame.rb
@@ -5,4 +5,12 @@ class LearningTimeFrame < ApplicationRecord
   has_many :users, through: :learning_time_frames_users
 
   validates :week_day, :activity_time, presence: true
+
+  scope :active_now, -> {
+    now = Time.zone.now
+    weekday = now.strftime('%A').downcase  # => "monday", "tuesday", ...
+    hour = now.hour                        # => 10（現在の時間 0〜23）
+
+    where(week_day: weekday, activity_time: hour)
+  }
 end
