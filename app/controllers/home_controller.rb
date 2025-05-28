39c76c44f09871c45c96308ef1df989c48434004
sharoa119 <sau.rin1109@gmail.com>
2025-05-28 16:10:05 +0900

    ダッシュボードに学習時間が該当するユーザーのアイコンを表示する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -40,7 +40,6 @@ class HomeController < ApplicationController
   end
 
   def display_dashboard
-    @users_for_time_slot = User.with_attached_avatar.limit(5)
     @announcements = Announcement.with_avatar.where(wip: false).order(published_at: :desc).limit(5)
     @bookmarks = current_user.bookmarks.order(created_at: :desc).limit(5)
     @completed_learnings = current_user.learnings.where(status: 3).includes(:practice).order(updated_at: :desc)
@@ -55,6 +54,7 @@ class HomeController < ApplicationController
     @calendar = NicoNicoCalendar.new(current_user, params[:niconico_calendar])
     @target_end_date = GrassDateParameter.new(params[:end_date]).target_end_date
     @times = Grass.times(current_user, @target_end_date)
+    @users_for_time_slot = User.students_and_trainees.joins(:learning_time_frames).merge(LearningTimeFrame.active_now)
   end
 
   def display_events_on_dashboard
