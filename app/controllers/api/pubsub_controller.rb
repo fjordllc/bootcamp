0ab3e57d0e976a64e39c75d66da3fae4ed6d4282
sekito <sekito1107gg@gmail.com>
2025-05-29 19:46:49 +0900

    rubocopによる修正

--- app/controllers/api/pubsub_controller.rb
+++ app/controllers/api/pubsub_controller.rb
@@ -4,32 +4,32 @@ class API::PubsubController < API::BaseController
   skip_before_action :verify_authenticity_token
   skip_before_action :require_login_for_api
 
-def create
-  message = parse_pubsub_message(request.body.read)
-  job_name = message[:job_name]
-  job_state = message[:job_state]
+  def create
+    message = parse_pubsub_message(request.body.read)
+    job_name = message[:job_name]
+    job_state = message[:job_state]
 
-  movie = find_movie(job_name)
+    movie = find_movie(job_name)
 
-  if movie
-    handle_job_state(movie, job_name, job_state)
-  else
-    Rails.logger.error("Movie not found for job_name: #{job_name}")
+    if movie
+      handle_job_state(movie, job_name, job_state)
+    else
+      Rails.logger.error("Movie not found for job_name: #{job_name}")
+    end
+
+    head :ok
   end
-
-  head :ok
-end
 
   private
 
   def parse_pubsub_message(body)
-  message = JSON.parse(body)
-  data = JSON.parse(Base64.decode64(message['message']['data']))
+    message = JSON.parse(body)
+    data = JSON.parse(Base64.decode64(message['message']['data']))
 
-  {
-    job_name: data.dig('job', 'name'),
-    job_state: data.dig('job', 'state')
-  }
+    {
+      job_name: data.dig('job', 'name'),
+      job_state: data.dig('job', 'state')
+    }
   end
 
   def find_movie(job_name)
