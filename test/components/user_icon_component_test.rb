392549e02d1433698b244e0b302d485e3684d404
sharoa119 <sau.rin1109@gmail.com>
2025-05-30 14:47:48 +0900

    テストを作成

--- test/components/user_icon_component_test.rb
+++ test/components/user_icon_component_test.rb
@@ -3,10 +3,19 @@
 require "test_helper"
 
 class UserIconComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(UserIconComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  fixtures :users, :learning_time_frames, :learning_time_frames_users
+
+  test 'shows user icon if user has active learning time now' do
+    travel_to Time.zone.local(2025, 5, 28, 12, 0, 0) do
+      users = User.students_and_trainees.joins(:learning_time_frames).merge(LearningTimeFrame.active_now)
+      assert_includes users, users(:kimura)
+    end
+  end
+
+  test 'does not show user icon if user does not have active learning time now' do
+    travel_to Time.zone.local(2025, 5, 28, 14, 0, 0) do
+      users = User.students_and_trainees.joins(:learning_time_frames).merge(LearningTimeFrame.active_now)
+      refute_includes users, users(:kimura)
+    end
   end
 end
