70547d73cf33ea7516de54b539f2bfb668823a9b
sharoa119 <sau.rin1109@gmail.com>
2025-05-30 14:47:48 +0900

    テストを作成

--- test/components/user_icon_list_component_test.rb
+++ test/components/user_icon_list_component_test.rb
@@ -3,10 +3,28 @@
 require "test_helper"
 
 class UserIconListComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(UserIconListComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  fixtures :users, :learning_time_frames, :learning_time_frames_users
+
+  test 'shows user icons if multiple users have active learning time now' do
+    travel_to Time.zone.local(2025, 5, 28, 12, 0, 0) do
+      users = User.students_and_trainees.joins(:learning_time_frames).merge(LearningTimeFrame.active_now)
+      assert_includes users, users(:kimura)
+      assert_includes users, users(:kensyu)
+    end
+  end
+
+  # ログインユーザー自身は除外されることを確認するテスト
+  test 'does not show login user icon even if login user have active learning time now' do
+    travel_to Time.zone.local(2025, 5, 28, 12, 0, 0) do
+      current_user = users(:hatsuno)
+      users_for_time_slot = User.students_and_trainees
+                                .joins(:learning_time_frames)
+                                .merge(LearningTimeFrame.active_now)
+                                .where.not(id: current_user.id)
+
+      assert_includes users_for_time_slot, users(:kimura)
+      assert_includes users_for_time_slot, users(:kensyu)
+      refute_includes users_for_time_slot, current_user
+    end
   end
 end
