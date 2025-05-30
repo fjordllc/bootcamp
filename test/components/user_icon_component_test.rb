b8d517c40dc9a1ee59a29233f2dfef2cdd7bbabe
sharoa119 <sau.rin1109@gmail.com>
2025-05-30 17:24:21 +0900

    rubocopに指摘された箇所を直す

--- test/components/user_icon_component_test.rb
+++ test/components/user_icon_component_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class UserIconComponentTest < ViewComponent::TestCase
   fixtures :users, :learning_time_frames, :learning_time_frames_users
@@ -15,7 +15,7 @@ class UserIconComponentTest < ViewComponent::TestCase
   test 'does not show user icon if user does not have active learning time now' do
     travel_to Time.zone.local(2025, 5, 28, 14, 0, 0) do
       users = User.students_and_trainees.joins(:learning_time_frames).merge(LearningTimeFrame.active_now)
-      refute_includes users, users(:kimura)
+      assert_not_includes users, users(:kimura)
     end
   end
 end
