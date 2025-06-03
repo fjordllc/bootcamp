e039f3c029d7c9eaff9be832c08af00f7c9c6a7f
sharoa119 <sau.rin1109@gmail.com>
2025-06-03 14:29:33 +0900

    画像のファイル名も変更。不要なマイグレーションファイルを削除

--- db/migrate/20250603030422_replace_soso_emotion_with_neutral.rb
+++
@@ -1,9 +0,0 @@
-class ReplaceSosoEmotionWithNeutral < ActiveRecord::Migration[6.1]
-  def up
-    Report.where(emotion: 0).update_all(emotion: Report.emotions[:neutral])
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
