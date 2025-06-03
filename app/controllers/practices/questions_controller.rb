1eff8e772c99e98b9859ee2afe3f8f7de7885dad
Kushizaki <kushimegu288@gmail.com>
2025-06-03 13:58:49 +0900

    質問なし表示のメソッドの統一

--- app/controllers/practices/questions_controller.rb
+++ app/controllers/practices/questions_controller.rb
@@ -5,21 +5,7 @@ class Practices::QuestionsController < ApplicationController
 
   def index
     @practice = Practice.find(params[:practice_id])
-    @empty_message = empty_message
     @questions = @practice.questions.includes(%i[correct_answer practice answers tag_taggings tags]).order(created_at: :desc).page(params[:page])
     @questions_property = QuestionsProperty.new('全ての質問', '質問はありません。')
-  end
-
-  private
-
-  def empty_message
-    case params[:target]
-    when 'solved'
-      '解決済みのQ&Aはありません。'
-    when 'not_solved'
-      '未解決のQ&Aはありません。'
-    else
-      '質問はありません。'
-    end
   end
 end
