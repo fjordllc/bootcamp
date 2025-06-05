35def1b21f01a1b71455f50169305bc486a6621d
e-yanagita-gs <e.kuroe@genbasupport.com>
2025-06-05 13:56:25 +0900

    ダッシュボードのタブの順番の変更

--- app/helpers/page_tabs/dashboard_helper.rb
+++ app/helpers/page_tabs/dashboard_helper.rb
@@ -7,8 +7,8 @@ module PageTabs
       tabs << { name: 'ダッシュボード', link: '/' }
       tabs << { name: '自分の日報', link: current_user_reports_path, count: current_user.reports.length }
       tabs << { name: '自分の提出物', link: current_user_products_path, count: current_user.products.length }
-      tabs << { name: 'Watch中', link: current_user_watches_path }
       tabs << { name: 'ブックマーク', link: current_user_bookmarks_path, count: current_user.bookmarks.length }
+      tabs << { name: 'Watch中', link: current_user_watches_path }
       render PageTabsComponent.new(tabs:, active_tab:)
     end
   end
