59f1fbf15ce865d407cfea144f8b3c2a411910b8
Kushizaki <kushimegu288@gmail.com>
2025-06-05 15:42:53 +0900

    質問がない時のメッセージ表示の修正

--- app/controllers/practices/questions_controller.rb
+++ app/controllers/practices/questions_controller.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 class Practices::QuestionsController < ApplicationController
-  QuestionsProperty = Struct.new(:title, :empty_message)
-
   def index
     @practice = Practice.find(params[:practice_id])
-    @questions = @practice.questions.includes(%i[correct_answer practice answers tag_taggings tags]).by_target(params[:target]).order(created_at: :desc).page(params[:page])
-    @questions_property = QuestionsProperty.new('全ての質問', '質問はありません。')
+    @questions = @practice.questions.includes(%i[correct_answer practice answers tag_taggings
+                                                 tags]).by_target(params[:target]).order(created_at: :desc).page(params[:page])
+    @empty_message = empty_message
+  end
+
+  private
+
+  def empty_message
+    case params[:target]
+    when 'solved'
+      '解決済みのQ&Aはありません。'
+    when 'not_solved'
+      '未解決のQ&Aはありません。'
+    else
+      '質問はありません。'
+    end
   end
 end
