d7e277b15646879ffd16c4a40d01da2099accff6
Kushizaki <kushimegu288@gmail.com>
2025-06-09 18:26:26 +0900

    質問データ取得のクエリに関する修正

--- app/controllers/practices/questions_controller.rb
+++ app/controllers/practices/questions_controller.rb
@@ -3,8 +3,7 @@
 class Practices::QuestionsController < ApplicationController
   def index
     @practice = Practice.find(params[:practice_id])
-    @questions = @practice.questions.includes(%i[correct_answer practice answers tag_taggings
-                                                 tags]).by_target(params[:target]).order(created_at: :desc).page(params[:page])
+    @questions = @practice.questions.includes(%i[correct_answer answers]).by_target(params[:target]).order(created_at: :desc).page(params[:page])
     @empty_message = empty_message
   end
 
