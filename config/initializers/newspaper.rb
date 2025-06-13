e84430a95f17e72da9616c8774c8df5534743380
appsu-mi <natubate870@gmail.com>
2025-06-13 17:18:09 +0900

    pair_workのsave時に関連付けを利用してschedulesもsaveするように

--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -83,7 +83,5 @@ Rails.configuration.after_initialize do
   Newspaper.subscribe(:pair_work_create, pair_work_notifier)
   Newspaper.subscribe(:pair_work_update, pair_work_notifier)
 
-  Newspaper.subscribe(:pair_work_create, PairWorkScheduleCreator.new)
-
   Newspaper.subscribe(:pair_work_update, PairWorkMatchingNotifier.new)
 end
