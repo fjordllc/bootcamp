a6db63b8c54bfc9093e4e4af45c44451cbb7b224
tyrrell-IH <gotothertm@gmail.com>
2025-06-13 11:00:02 +0900

    faq_categoriesテーブルのnameカラム内で使用されている法人利用の文言を企業研修代行に修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
   skip_before_action :require_active_user_login, raise: false
   layout 'lp'
   DEFAULT_COURSE = 'Railsエンジニア'
-  FAQ_CATEGORY_NAME = '法人利用について'
+  FAQ_CATEGORY_NAME = '企業研修代行について'
 
   def index
     @mentors = current_user ? User.mentors_sorted_by_created_at : User.visible_sorted_mentors
