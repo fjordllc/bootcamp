2f82e9d1c6e93a0fda43ffe4b92b49a441242700
komagata <komagata@gmail.com>
2025-06-19 19:22:03 +0900

    重複マイグレーションファイルを削除：users.emailとlogin_nameのユニークインデックス

--- db/migrate/20250618143741_add_unique_index_to_users_email_and_login_name.rb
+++
@@ -1,6 +0,0 @@
-class AddUniqueIndexToUsersEmailAndLoginName < ActiveRecord::Migration[6.1]
-  def change
-    add_index :users, :email, unique: true
-    add_index :users, :login_name, unique: true
-  end
-end
