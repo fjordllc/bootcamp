f4642a595a68b481f7d951ee6787d11562af2e00
e-yanagita-gs <e.kuroe@genbasupport.com>
2025-06-20 14:37:37 +0900

    日付解析ロジックにバリデーションを追加

--- app/controllers/concerns/date_parsable.rb
+++ app/controllers/concerns/date_parsable.rb
@@ -8,6 +8,10 @@ module DateParsable
   def parse_target_end_date
     return Date.current unless params[:end_date].present?
 
+    unless params[:end_date].match?(/\A\d{4}-\d{2}-\d{2}\z/)
+      return Date.current
+    end
+
     Date.parse(params[:end_date])
   rescue Date::Error
     Date.current
