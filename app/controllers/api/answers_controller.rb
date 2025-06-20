6f113b8ad20de2ad10756dce96eacb319a1f67af
komagata <komagata@gmail.com>
2025-06-20 22:29:11 +0900

    AnswererWatcherをnewspaperからActiveSupport::Notificationsに移行
    - config/initializers/active_support_notifications.rbを追加
    - AnswererWatcherクラスをActiveSupport::Notificationsのインターフェースに対応
    - AnswersControllerでイベント発火をActiveSupport::Notifications.instrumentに変更
    - newspaper.rbからAnswererWatcherの登録を削除

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -29,7 +29,7 @@ class API::AnswersController < API::BaseController
     @answer = question.answers.new(answer_params)
     @answer.user = current_user
     if @answer.save
-      Newspaper.publish(:answer_create, { answer: @answer })
+      ActiveSupport::Notifications.instrument('answer.create', answer: @answer)
       Newspaper.publish(:answer_save, { answer: @answer })
       render partial: 'questions/answer', locals: { question:, answer: @answer, user: current_user }, status: :created
     else
