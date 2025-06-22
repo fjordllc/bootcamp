41f0a5eea7f790832b3da0f837891e982327ca41
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-06-22 11:16:37 +0900

    UserIconFrameClassをクラス名のみ返すだけの関数に変更

--- app/javascript/components/UserIconFrameClass.jsx
+++ app/javascript/components/UserIconFrameClass.jsx
@@ -1,24 +1,5 @@
-import React from 'react'
-
-export default function UserIconFrameClass({
-  user,
-  spanClassName,
-  className,
-  src,
-  title,
-  alt
-}) {
-  const roleClass = `is-${user.primary_role}`
-  const joiningStatusClass =
+export function UserIconFrameClass(user) {
+  return `a-user-role is-${user.primary_role} ${
     user.joining_status === 'new-user' ? 'is-new-user' : ''
-  return (
-    <span
-      className={`a-user-role ${spanClassName} ${roleClass} ${joiningStatusClass}`}>
-      <img className={className} src={src} title={title} alt={alt} />
-    </span>
-  )
-}
-
-export function getIconFrameClass(user) {
-  return `is-${user.primary_role} is-${user.joining_status}`
+  }`
 }
