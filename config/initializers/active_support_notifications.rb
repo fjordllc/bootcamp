834c3979e6fb87e2fd659791c94d2c59fa65af6f
e-yanagita-gs <e.kuroe@genbasupport.com>
2025-06-23 16:53:53 +0900

    EventOrganizerWatcherクラスの実装を置き換え

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -2,4 +2,5 @@
 
 Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('answer.create', AnswererWatcher.new)
+  ActiveSupport::Notifications.subscribe('event.create', EventOrganizerWatcher.new)
 end
