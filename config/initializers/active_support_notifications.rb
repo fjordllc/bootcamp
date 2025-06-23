14e2fe4285953b9ffe0854caf3c6563e078874d7
e-yanagita-gs <e.kuroe@genbasupport.com>
2025-06-23 17:17:22 +0900

    AnswerNotifierクラスの置き換え

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -2,5 +2,6 @@
 
 Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('answer.create', AnswererWatcher.new)
+  ActiveSupport::Notifications.subscribe('answer.create', AnswerNotifier.new)
   ActiveSupport::Notifications.subscribe('event.create', EventOrganizerWatcher.new)
 end
