44872fc878a3638174f67b52035f9708bb37c8ce
e-yanagita-gs <e.kuroe@genbasupport.com>
2025-06-23 17:30:42 +0900

    NotifierToWatchingUserクラスの実装を置き換え

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -3,5 +3,6 @@
 Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('answer.create', AnswererWatcher.new)
   ActiveSupport::Notifications.subscribe('answer.create', AnswerNotifier.new)
+  ActiveSupport::Notifications.subscribe('answer.create', NotifierToWatchingUser.new)
   ActiveSupport::Notifications.subscribe('event.create', EventOrganizerWatcher.new)
 end
