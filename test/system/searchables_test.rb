677c3cacf61995728b4d274dcbee5ea6f1cedaf0
ryo <ta090972@icloud.com>
2025-06-24 06:34:16 +0900

    テストの修正

--- test/system/searchables_test.rb
+++ test/system/searchables_test.rb
@@ -193,11 +193,16 @@ class SearchablesTest < ApplicationSystemTestCase
       select 'すべて'
       fill_in 'word', with: '提出物のコメントです。'
     end
+<<<<<<< HEAD
     find('#test-search-modal').click
     assert_includes find('img.card-list-item-meta__icon.a-user-icon')['src'], 'komagata.webp'
+=======
+    find('#test-search').click
+    assert_includes find('img.card-list-item-meta__icon.a-user-icon')['src'], 'mentormentaro.webp'
+>>>>>>> 8bdf34510 (テストの修正)
 
     find('img.card-list-item-meta__icon.a-user-icon').click
-    assert_selector 'h1.page-content-header__title', text: 'komagata'
+    assert_selector 'h1.page-content-header__title', text: 'mentormentaro'
   end
 
   test 'disappear icon when search user' do
