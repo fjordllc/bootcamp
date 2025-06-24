81e9d215aebbe30249b4799dea7d7fd01a22d915
原田大暉 <th@mac-1216.local>
2025-06-24 21:46:35 +0900

    退会済みの研修生を表示しないようにしたことに伴うテストの修正

--- test/models/generation_test.rb
+++ test/models/generation_test.rb
@@ -32,7 +32,7 @@ class GenerationTest < ActiveSupport::TestCase
 
   test '#count_classmates_by_target' do
     assert_equal 18, Generation.new(5).count_classmates_by_target(:students)
-    assert_equal 4, Generation.new(5).count_classmates_by_target(:trainees)
+    assert_equal 3, Generation.new(5).count_classmates_by_target(:trainees)
     assert_equal 1, Generation.new(5).count_classmates_by_target(:hibernated)
     assert_equal 2, Generation.new(5).count_classmates_by_target(:graduated)
     assert_equal 3, Generation.new(5).count_classmates_by_target(:advisers)
