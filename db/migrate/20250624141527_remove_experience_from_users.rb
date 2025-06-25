440f81271832921647a29523cd655962bb9de869
原田大暉 <th@mac-1216.local>
2025-06-25 23:32:59 +0900

    coderabbitの指摘に従い、削除したカラムのロールバックをできるように変更

--- db/migrate/20250624141527_remove_experience_from_users.rb
+++ db/migrate/20250624141527_remove_experience_from_users.rb
@@ -1,5 +1,9 @@
 class RemoveExperienceFromUsers < ActiveRecord::Migration[6.1]
-  def change
+  def up
     remove_column :users, :experience, :integer
+  end
+
+  def down
+    add_column :users, :experience, :integer
   end
 end
