66b307c37b56c1de8c2ce8ed3e82317fd445aea6
appsu-mi <natubate870@gmail.com>
2025-06-25 12:36:36 +0900

    テストが通らなくなっていたため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_05_19_012857) do
+ActiveRecord::Schema.define(version: 2025_06_18_144325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
