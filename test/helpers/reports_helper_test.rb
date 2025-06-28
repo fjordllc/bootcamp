e9e3aa9da8281407dab1388a8b83b47fd3fafcef
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-06-28 23:18:53 +0900

    unchecked_report_messageのテストを追加

--- test/helpers/reports_helper_test.rb
+++ test/helpers/reports_helper_test.rb
@@ -28,4 +28,11 @@ class ReportsHelperTest < ActionView::TestCase
     assert_equal 'is-danger', user_report_count_class(10)
     assert_equal 'is-danger', user_report_count_class(100)
   end
+
+  test 'unchecked_report_message' do
+    user = users(:hajime)
+    assert_equal "#{user.login_name}さんの日報へ", unchecked_report_message(0, user)
+    assert_equal "#{user.login_name}さんの未チェックの日報はこれで最後です。", unchecked_report_message(1, user)
+    assert_equal "#{user.login_name}さんの未チェックの日報が2件あります。", unchecked_report_message(2, user)
+  end
 end
