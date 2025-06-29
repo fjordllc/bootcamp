2986d0833d9a9d35c28e0d75880598ca77cd909a
hiromisugie <lozz.gggg@gmail.com>
2025-06-30 05:35:14 +0900

    コメントを追加

--- test/supports/login_assert_helper.rb
+++ test/supports/login_assert_helper.rb
@@ -3,14 +3,20 @@
 module LoginAssertHelper
   def assert_login_required(path)
     visit path
+
+    # まずbodyがロードされるのを待つ
     assert_selector 'body'
+
     assert_text 'ログインしてください'
     assert_selector 'h1', text: 'プラス戦力のスキルを身につける'
   end
 
   def assert_no_login_required(path, text)
     visit path
+
+    # まずbodyがロードされるのを待つ
     assert_selector 'body'
+
     assert_text text
     assert_no_text 'ログインしてください'
   end
