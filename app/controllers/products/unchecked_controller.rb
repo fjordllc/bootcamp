9ce9838ddf4ec713cd4121d10cd406380c9346f5
su-su-su-su <suzukikoji0503@gmail.com>
2025-06-30 15:30:00 +0900

    未チェック提出物一覧ページでkaminariによるページネーションを追加

--- app/controllers/products/unchecked_controller.rb
+++ app/controllers/products/unchecked_controller.rb
@@ -6,6 +6,13 @@ class Products::UncheckedController < ApplicationController
     @target = params[:target]
     @target = 'unchecked_all' unless target_allowlist.include?(@target)
     @checker_id = params[:checker_id]
+
+    products_scope = Product.unchecked.list
+    products_scope = products_scope.where(checker_id: @checker_id) if @checker_id.present?
+
+    @products = products_scope.order_for_all_list
+                              .page(params[:page])
+                              .per(50)
   end
 
   private
