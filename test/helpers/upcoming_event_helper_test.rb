2a343d1802bda93a7bc418facce5b761d3e308de
appsu-mi <natubate870@gmail.com>
2025-06-30 14:09:37 +0900

    近日開催されるペアワークのテストをわかりやすく、且つ順序に依存しないように修正

--- test/helpers/upcoming_event_helper_test.rb
+++ test/helpers/upcoming_event_helper_test.rb
@@ -5,29 +5,33 @@ require 'test_helper'
 class UpcomingEventHelperTest < ActionView::TestCase
   include Sorcery::Controller::InstanceMethods
   include Sorcery::TestHelpers::Rails::Controller
-
   test 'scheduled_on_pair_works' do
-    login_user(users(:kimura))
-    upcoming_pair_work = PairWork.create!(
-      title: 'ペアが確定していて、今日行われるペアワーク',
-      description: 'ペアが確定していて、今日行われるペアワーク',
-      user: users(:kimura),
-      reserved_at: Time.current.beginning_of_day,
+    user = users(:kimura)
+    login_user(user)
+
+    pair_work_attributes = {
+      user:,
+      title: 'ペアが確定していて、近日開催されるペアワーク',
+      description: 'ペアが確定していて、近日開催されるペアワーク',
       buddy_id: users(:komagata),
       channel: 'ペアワーク・モブワーク1',
       wip: false
-    )
-    upcoming_pair_work_second = PairWork.create!(
-      title: 'ペアが確定していて、今日行われるペアワーク2',
-      description: 'ペアが確定していて、今日行われるペアワーク2',
-      user: users(:kimura),
-      reserved_at: Time.current.beginning_of_day,
-      buddy_id: users(:komagata),
-      channel: 'ペアワーク・モブワーク1',
-      wip: false
-    )
-    upcoming_pair_works = []
-    upcoming_pair_works << upcoming_pair_work << upcoming_pair_work_second
-    assert_equal upcoming_pair_works, scheduled_on_pair_works(:today)
+    }
+    upcoming_pair_work_today = PairWork.create!(pair_work_attributes.merge(
+                                                  reserved_at: Time.current.beginning_of_day,
+                                                  schedules_attributes: [{ proposed_at: Time.current.beginning_of_day }]
+                                                ))
+    upcoming_pair_work_tomorrow = PairWork.create!(pair_work_attributes.merge(
+                                                     reserved_at: Time.current.beginning_of_day + 1.day,
+                                                     schedules_attributes: [{ proposed_at: Time.current.beginning_of_day + 1.day }]
+                                                   ))
+    upcoming_pair_work_day_after_tomorrow = PairWork.create!(pair_work_attributes.merge(
+                                                               reserved_at: Time.current.beginning_of_day + 2.days,
+                                                               schedules_attributes: [{ proposed_at: Time.current.beginning_of_day + 2.days }]
+                                                             ))
+
+    assert_includes scheduled_on_pair_works(:today), upcoming_pair_work_today
+    assert_includes scheduled_on_pair_works(:tomorrow), upcoming_pair_work_tomorrow
+    assert_includes scheduled_on_pair_works(:day_after_tomorrow), upcoming_pair_work_day_after_tomorrow
   end
 end
