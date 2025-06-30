a4d5bafa0544adbafed6c73fe23387b19f700c3c
su-su-su-su <suzukikoji0503@gmail.com>
2025-06-30 15:30:30 +0900

    未担当提出物一覧ページでkaminariによるページネーションを追加

--- app/controllers/products/unassigned_controller.rb
+++ app/controllers/products/unassigned_controller.rb
@@ -4,5 +4,10 @@ class Products::UnassignedController < ApplicationController
   before_action :require_staff_login
   def index
     @product_deadline_day = Product::PRODUCT_DEADLINE
+    @products = Product.unassigned
+                       .list
+                       .order_for_all_list
+                       .page(params[:page])
+                       .per(50)
   end
 end
