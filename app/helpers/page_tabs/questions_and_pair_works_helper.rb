5c5e2c2ea252d605cb9dd76991bd52c18250d4d7
appsu-mi <natubate870@gmail.com>
2025-07-01 12:29:18 +0900

    既存の実装に合わせてunsolved_badgeを使用

--- app/helpers/page_tabs/questions_and_pair_works_helper.rb
+++ app/helpers/page_tabs/questions_and_pair_works_helper.rb
@@ -4,8 +4,8 @@ module PageTabs
   module QuestionsAndPairWorksHelper
     def questions_and_pair_works_page_tabs
       tabs = []
-      tabs << { name: 'Q&A', link: questions_path(target: 'not_solved'), badge: Question.not_solved.not_wip.size }
-      tabs << { name: 'ペアワーク', link: pair_works_path(target: 'not_solved'), badge: PairWork.not_solved.not_wip.size }
+      tabs << { name: 'Q&A', link: questions_path(target: 'not_solved'), badge: Question.unsolved_badge(current_user:, practice_id: params[:practice_id]) }
+      tabs << { name: 'ペアワーク', link: pair_works_path(target: 'not_solved'), badge: PairWork.unsolved_badge(current_user:) }
       render PageTabsComponent.new(tabs:, active_tab: question_and_pair_work_active_tab)
     end
 
