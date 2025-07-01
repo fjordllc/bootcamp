c45816601d241997d8679d206c600047cefff916
tyrrell-IH <gotothertm@gmail.com>
2025-07-01 11:42:35 +0900

    AnnouncementNotifierをNewspaperからActiveSupport::Notificationsへ移行

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -4,4 +4,6 @@ Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('answer.create', AnswererWatcher.new)
   ActiveSupport::Notifications.subscribe('report.create', FirstReportNotifier.new)
   ActiveSupport::Notifications.subscribe('report.update', FirstReportNotifier.new)
+  ActiveSupport::Notifications.subscribe('announcement.create', AnnouncementNotifier.new)
+  ActiveSupport::Notifications.subscribe('announcement.update', AnnouncementNotifier.new)
 end
