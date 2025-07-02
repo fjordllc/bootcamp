4bd000fc945b4300d7a3b7e49dbe3577df241b98
su-su-su-su <suzukikoji0503@gmail.com>
2025-07-02 10:01:53 +0900

    テストの名前のアンダーバーをスペースに修正

--- test/helpers/markdown_helper_test.rb
+++ test/helpers/markdown_helper_test.rb
@@ -5,13 +5,13 @@ require 'test_helper'
 class MarkdownHelperTest < ActionView::TestCase
   include MarkdownHelper
 
-  test 'markdown_to_plain_text_converts_markdown_to_plain_text' do
+  test 'markdown to plain text converts markdown to plain text' do
     markdown = "# Hello, world! \n This is a **test**."
     expected_plain_text = "Hello, world!\nThis is a test."
     assert_equal expected_plain_text, markdown_to_plain_text(markdown)
   end
 
-  test 'markdown_to_plain_text_handles_empty_markdown' do
+  test 'markdown to plain text handles empty markdown' do
     assert_equal '', markdown_to_plain_text('')
   end
 end
