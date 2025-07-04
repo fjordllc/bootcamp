216402f4bfd10437aba4a5460594fb3afda7d274
e-yanagita-gs <e.kuroe@genbasupport.com>
2025-07-04 11:35:40 +0900

    引数が多すぎるrubocopエラーの解消

--- app/components/tag/tag_component.rb
+++ app/components/tag/tag_component.rb
@@ -1,16 +1,30 @@
 # frozen_string_literal: true
 
 class Tag::TagComponent < ViewComponent::Base
-  def initialize(initial_tags:, param_name:, input_id:, taggable_type:, taggable_id:, editable: true)
-    @initial_tags = initial_tags
+  def initialize(taggable:, param_name:, input_id:, editable: true)
+    @taggable = taggable
     @param_name = param_name
     @input_id = input_id
-    @taggable_type = taggable_type
-    @taggable_id = taggable_id
     @editable = editable
+  end
+
+  def initial_tags
+    @taggable.tag_list.join(',')
+  end
+
+  def taggable_type
+    @taggable.class.to_s
+  end
+
+  def taggable_id
+    @taggable.id.to_s
   end
 
   def editable?
     @editable
   end
+
+  private
+
+  attr_reader :taggable, :param_name, :input_id, :editable
 end
