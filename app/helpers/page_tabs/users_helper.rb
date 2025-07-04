1db5cbb64a551ead8ab7ba071f33bdfe543bee37
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-07-04 18:05:20 +0900

    コンフリクトを解消した

--- app/helpers/page_tabs/users_helper.rb
+++ app/helpers/page_tabs/users_helper.rb
@@ -13,10 +13,12 @@ module PageTabs
       tabs << { name: '質問', link: user_questions_path(user), count: user.questions.length }
       tabs << { name: '回答', link: user_answers_path(user), count: user.answers.length }
       tabs << { name: 'イベント', link: user_events_path(user), count: user.participate_events.length }
-      tabs << { name: '分報',
-                link: "#{user_micro_reports_path(user, page: user.latest_micro_report_page)}#latest-micro-report",
-                count: user.micro_reports.length }
-      tabs << { name: '相談部屋', link: talk_path(user.talk) } if current_user.admin? && !user.admin?
+      if Rails.env.development? || Rails.env.test?
+        tabs << { name: '分報',
+                  link: "#{user_micro_reports_path(user, page: user.latest_micro_report_page)}#latest-micro-report",
+                  count: user.micro_reports.length }
+      end
+      tabs << { name: '相談部屋', link: talk_path(user.talk) } if current_user.admin? && !user.admin? && user.talk.present?
       render PageTabsComponent.new(tabs:, active_tab:)
     end
 
