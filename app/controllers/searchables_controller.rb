33af515da367158a71b42e9a0d53d38a54c480cc
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-07-06 01:00:57 +0900

    リファクタリングした
    初期化メソッド内の処理をメソッド化した
    N+1 クエリを修正した
    HTML エスケープされていないコンテンツに対してHTMLタグを挿入しているため、XSS攻撃の可能性があったので修正

--- app/controllers/searchables_controller.rb
+++ app/controllers/searchables_controller.rb
@@ -9,5 +9,9 @@ class SearchablesController < ApplicationController
 
     searchables = Searcher.search(@word, document_type: @document_type, current_user:)
     @searchables = Kaminari.paginate_array(searchables.uniq).page(params[:page]).per(PER_PAGE)
+
+    user_ids = @searchables.map(&:user_id).compact.uniq
+    @users_by_id = User.where(id: user_ids).index_by(&:id)
+    @talks_by_user_id = Talk.where(user_id: user_ids).index_by(&:user_id)
   end
 end
