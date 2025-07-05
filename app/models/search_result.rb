b23839241b0be496bb3b8b6112f943e6a81eae0b
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-07-06 01:00:57 +0900

    リファクタリングした
    初期化メソッド内の処理をメソッド化した
    N+1 クエリを修正した
    HTML エスケープされていないコンテンツに対してHTMLタグを挿入しているため、XSS攻撃の可能性があったので修正

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -9,22 +9,38 @@ class SearchResult
 
   def initialize(searchable, word, current_user)
     @current_user = current_user
-    @url = searchable_url(searchable)
-    @title = fetch_title(searchable)
-    @summary = searchable_summary(filtered_message(searchable), word)
-    @formatted_summary = highlight_word(@summary, word)
-    @user_id = searchable.is_a?(User) ? searchable.id : searchable.try(:user_id)
-    @login_name = fetch_login_name(searchable)
-    @formatted_updated_at = searchable.respond_to?(:formatted_updated_at) ? searchable.formatted_updated_at : searchable.updated_at.strftime('%Y年%m月%d日 %H:%M')
-    @model_name = searchable.class.name.underscore
-    @label = fetch_label(searchable)
-    @wip = searchable.try(:wip)
-    @commentable_user = fetch_commentable_user(searchable)
-    @commentable_type = I18n.t("activerecord.models.#{searchable.try(:commentable)&.try(:model_name)&.name&.underscore}", default: '')
-    @primary_role = created_user(searchable)&.primary_role
+    initialize_basic_attributes(searchable)
+    initialize_content_attributes(searchable, word)
+    initialize_user_attributes(searchable)
+    initialize_metadata_attributes(searchable)
   end
 
   private
+
+  def initialize_basic_attributes(searchable)
+    @url = searchable_url(searchable)
+    @title = fetch_title(searchable)
+    @model_name = searchable.class.name.underscore
+    @wip = searchable.try(:wip)
+  end
+
+  def initialize_content_attributes(searchable, word)
+    @summary = searchable_summary(filtered_message(searchable), word)
+    @formatted_summary = highlight_word(@summary, word)
+  end
+
+  def initialize_user_attributes(searchable)
+    @user_id = searchable.is_a?(User) ? searchable.id : searchable.try(:user_id)
+    @login_name = fetch_login_name(searchable)
+    @commentable_user = fetch_commentable_user(searchable)
+    @primary_role = created_user(searchable)&.primary_role
+  end
+
+  def initialize_metadata_attributes(searchable)
+    @formatted_updated_at = searchable.respond_to?(:formatted_updated_at) ? searchable.formatted_updated_at : searchable.updated_at.strftime('%Y年%m月%d日 %H:%M')
+    @label = fetch_label(searchable)
+    @commentable_type = fetch_commentable_type(searchable)
+  end
 
   def fetch_title(searchable)
     return searchable.question&.title if searchable.is_a?(Answer)
@@ -49,12 +65,23 @@ class SearchResult
 
   def highlight_word(text, word)
     return unless text
+    return text if word.blank?
 
+    escaped_text = ERB::Util.html_escape(text)
     words = word.split(/[[:blank:]]+/).reject(&:blank?)
     words.each do |w|
-      text = text.gsub(/(#{Regexp.escape(w)})/i, '<strong class="matched_word">\1</strong>')
+      escaped_text = escaped_text.gsub(/(#{Regexp.escape(w)})/i, '<strong class="matched_word">\1</strong>')
     end
 
-    text
+    escaped_text.html_safe
+  end
+
+  def fetch_commentable_type(searchable)
+    return '' unless searchable.try(:commentable)
+    model_name = searchable.commentable.model_name.name.underscore
+    I18n.t("activerecord.models.#{model_name}", default: '')
+  rescue => e
+    Rails.logger.warn "Failed to fetch commentable type: #{e.message}"
+    ''
   end
 end
