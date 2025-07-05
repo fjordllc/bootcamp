39a1f1da29ca4fbf47c227a356240f9f88b1bdaa
kitarou888 <yuichiro.makita24@gmail.com>
2025-07-06 08:32:25 +0900

    検索カテゴリーが「すべて」かつ「自分のもののみ」にチェックがあるときにプラクティスとユーザーは検索結果に出ないように修正

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -18,10 +18,10 @@ class Searcher
       searchables = fetch_results(words, document_type) || []
       searchables = filter_results!(searchables, current_user)
 
-      # only_me=true の場合、user_id カラムを持たないPracticeおよびUserはすべて対象とする
+      # only_me=true の場合、user_id カラムを持たないプラクティスとユーザーは対象外とする
       if only_me
         searchables = searchables.select do |searchable|
-          %w[User Practice].include?(searchable.class.name) ? true : searchable.user_id == current_user.id
+          %w[User Practice].include?(searchable.class.name) ? false : searchable.user_id == current_user.id
         end
       end
 
