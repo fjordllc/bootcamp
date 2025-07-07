e9847e3dbc384528fef6cf808ffb89cda246561b
e-yanagita-gs <e.kuroe@genbasupport.com>
2025-07-07 14:36:22 +0900

    CSSクラス名の統一とテストメソッド内でコンポーネント描画するよう修正

--- test/components/grass/grass_component_test.rb
+++ test/components/grass/grass_component_test.rb
@@ -7,31 +7,29 @@ class Grass::GrassComponentTest < ViewComponent::TestCase
     @user = users(:sotugyou).extend(UserDecorator)
     params_date = "#{Date.current.year}-#{Date.current.month}-#{Date.current.day}"
     @target_end_date = GrassDateParameter.new(params_date).target_end_date
-    @times = Grass.times(@user, @target_end_date)
-    render_inline(Grass::GrassComponent.new(
-                    user: @user,
-                    times: @times,
-                    target_end_date: @target_end_date,
-                    path: :root_path
-                  ))
+    @prev_year = @target_end_date.prev_year
   end
 
   def test_prev_year_link
-    prev_year_str = @target_end_date.prev_year.to_s
-    expected_path = "/?end_date=#{prev_year_str}"
-    assert_selector "a[href='#{expected_path}'] .user-grass.nav__previous i.fa-solid.fa-angle-left"
+    render_inline(Grass::GrassComponent.new(
+                    user: @user,
+                    times: Grass.times(@user, @target_end_date),
+                    target_end_date: @target_end_date,
+                    path: :root_path
+                  ))
+    expected_path = "/?end_date=#{@prev_year.to_s}"
+    assert_selector "a[href='#{expected_path}'] .user-grass-nav__previous i.fa-solid.fa-angle-left"
   end
 
   def test_next_year_link
-    target_end_date = Date.current.prev_year
     render_inline(Grass::GrassComponent.new(
                     user: @user,
-                    times: Grass.times(@user, target_end_date),
-                    target_end_date:,
+                    times: Grass.times(@user, @target_end_date),
+                    target_end_date: @prev_year,
                     path: :root_path
                   ))
-    next_year_str = target_end_date.next_year.to_s
-    expected_path = "/?end_date=#{next_year_str}"
+    next_year = @prev_year.next_year
+    expected_path = "/?end_date=#{next_year.to_s}"
     assert_selector "a[href='#{expected_path}'] .user-grass-nav__next i.fa-solid.fa-angle-right"
   end
 
