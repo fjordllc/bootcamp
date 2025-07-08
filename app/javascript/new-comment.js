286706089c1983a89643e218ae39929151cd5c29
machida <machida@fjord.jp>
2025-07-08 17:45:55 +0900

    JSのセレクタのclass名とデザインのためのclass名を切り離した。コメントのRaw周りにデザインを入れた。

--- app/javascript/new-comment.js
+++ app/javascript/new-comment.js
@@ -59,9 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
   )
 
   const saveButton = commentEditor.querySelector('.js-comment-save-button')
-  const saveAndCheckButton = commentEditor.querySelector(
-    '.js-comment-check-button'
-  )
+  const saveAndCheckButton = commentEditor.querySelector('.js-comment-check-button')
   const saveAndCheckWrapper = saveAndCheckButton?.parentElement
 
   const disableButtons = () => {
