c3399f24ddbe110c2547a4517567742349f9cd98
原田大暉 <th@mac-1216.local>
2025-07-09 23:32:51 +0900

    プラティクスごとのタブを、production環境以外で表示するように変更

--- app/helpers/page_tabs/practices_helper.rb
+++ app/helpers/page_tabs/practices_helper.rb
@@ -8,8 +8,8 @@ module PageTabs
       tabs << { name: '日報', link: practice_reports_path(practice), count: practice.reports.length }
       tabs << { name: '質問', link: practice_questions_path(practice), count: practice.questions.length }
       tabs << { name: 'Docs', link: practice_pages_path(practice), count: practice.pages.length }
-      # TODO: 動画機能の完成時に公開。動画リンクを隠した状態でのリリース。
-      # tabs << { name: '動画', link: practice_movies_path(practice), count: practice.movies.length }
+      # TODO: 動画機能の完成時に本番環境で公開。動画リンクを隠した状態でのリリース。
+      tabs << { name: '動画', link: practice_movies_path(practice), count: practice.movies.length } unless Rails.env.production?
       tabs << { name: '提出物', link: practice_products_path(practice) }
       tabs << { name: '模範解答', link: practice_submission_answer_path(practice) } if practice.submission_answer.present?
       tabs << { name: 'コーディングテスト', link: practice_coding_tests_path(practice) } if practice.coding_tests.present?
