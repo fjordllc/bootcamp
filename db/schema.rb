4a38be66a0762b81d68e0e6ee9ac728aa8090d74
su-su-su-su <suzukikoji0503@gmail.com>
2025-07-11 22:11:20 +0900

    重複したコードを削除

--- db/schema.rb
+++ db/schema.rb
@@ -945,7 +945,6 @@ ActiveRecord::Schema.define(version: 2025_06_24_141527) do
     t.boolean "trainee", default: false, null: false
     t.integer "experience"
     t.text "retire_reason"
-    t.boolean "trainee", default: false, null: false
     t.string "customer_id"
     t.string "subscription_id"
     t.boolean "mail_notification", default: true, null: false
