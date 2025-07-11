7016d3b1150e9e8ed1b03e1646972014c7fc927c
sharoa119 <sau.rin1109@gmail.com>
2025-07-11 14:45:52 +0900

    Revert schema.rb to match sad_ columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_08_061233) do
+ActiveRecord::Schema.define(version: 2025_06_24_141527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -964,8 +964,8 @@ ActiveRecord::Schema.define(version: 2025_07_08_061233) do
     t.text "mentor_memo"
     t.text "after_graduation_hope"
     t.date "training_ends_on"
-    t.boolean "negative_streak", default: false, null: false
-    t.integer "last_negative_report_id"
+    t.boolean "sad_streak", default: false, null: false
+    t.integer "last_sad_report_id"
     t.datetime "last_activity_at"
     t.datetime "hibernated_at"
     t.string "profile_name"
