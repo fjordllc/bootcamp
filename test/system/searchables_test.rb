242b4a2fd831e4be26e7cc0a66ce4a20105880af
ryo <ta090972@icloud.com>
2025-07-12 06:18:32 +0900

    avatar_urlメソッド内で画像変換処理を行うようにした

--- test/system/searchables_test.rb
+++ test/system/searchables_test.rb
@@ -197,7 +197,7 @@ class SearchablesTest < ApplicationSystemTestCase
     assert_includes find('img.card-list-item-meta__icon.a-user-icon')['src'], 'komagata.webp'
 
     find('img.card-list-item-meta__icon.a-user-icon').click
-    assert_selector 'h1.page-content-header__title', text: 'mentormentaro'
+    assert_selector 'h1.page-content-header__title', text: 'komagata'
   end
 
   test 'disappear icon when search user' do
