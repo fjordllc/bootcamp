013ff0f6e47ead2052bd2549d1961290d7428975
原田大暉 <th@mac-1216.local>
2025-07-15 23:40:03 +0900

    定期イベント更新時の通知をNewsPaperからActiveSupport::Notificationに変更

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -6,6 +6,7 @@ Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('answer.create', NotifierToWatchingUser.new)
   ActiveSupport::Notifications.subscribe('event.create', EventOrganizerWatcher.new)
   ActiveSupport::Notifications.subscribe('regular_event.create', RegularEventOrganizerWatcher.new)
+  ActiveSupport::Notifications.subscribe('regular_event.update', RegularEventUpdateNotifier.new)
   ActiveSupport::Notifications.subscribe('student.or.trainee.create', TimesChannelCreator.new)
   ActiveSupport::Notifications.subscribe('user.create', SignUpNotifier.new)
 end
