d7d5fdca7f3729397e34a7630aede1636d39dc52
machida <machida@fjord.jp>
2025-07-17 00:43:28 +0900

    scssから画像を使えるようにし、メインビジュアルの画像を更新した

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,6 +6,21 @@ const pug = require('./loaders/pug')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('pug', pug)
+
+// Fix sass-loader to handle Rails asset helpers
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(use => use.loader === 'sass-loader')
+if (sassLoaderConfig) {
+  sassLoaderConfig.options = sassLoaderConfig.options || {}
+  sassLoaderConfig.options.sassOptions = sassLoaderConfig.options.sassOptions || {}
+  sassLoaderConfig.options.sassOptions.functions = {
+    'image-url($path)': function(path) {
+      const sass = require('sass')
+      const pathValue = path.getValue().replace(/['"]/g, '')
+      return new sass.types.String(`url("~${pathValue}")`)
+    }
+  }
+}
 module.exports = environment
 
 function hotfixPostcssLoaderConfig (subloader) {
