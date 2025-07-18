826229e3a8d076317649115656f4f597a3ac8590
sharoa119 <sau.rin1109@gmail.com>
2025-07-18 11:19:36 +0900

    未チェックバッジはメンターと管理者のみ

--- app/helpers/page_tabs/reports_helper.rb
+++ app/helpers/page_tabs/reports_helper.rb
@@ -4,7 +4,7 @@ module PageTabs
   module ReportsHelper
     def report_page_tabs(active_tab:)
       tabs = []
-      tabs << { name: '日報', link: reports_path, badge: Cache.unchecked_report_count.presence }
+      tabs << { name: '日報', link: reports_path, badge: admin_or_mentor_login? ? Cache.unchecked_report_count.presence : nil }
       tabs << { name: 'みんなのブログ', link: external_entries_path }
       render PageTabsComponent.new(tabs:, active_tab:)
     end
