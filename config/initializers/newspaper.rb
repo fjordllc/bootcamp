4ce164ecb55d74dfa30dc3c4aaff7f8f4a35140f
matsuyama <aya.k.matsuyama@gmail.com>
2025-07-20 14:27:17 +0900

    ProductUpdateNotifierForCheckerをnewspaperからActiveSupport::Notificationsに移行

--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -31,6 +31,5 @@ Rails.configuration.after_initialize do
   Newspaper.subscribe(:question_update, question_notifier)
 
   Newspaper.subscribe(:product_update, ProductUpdateNotifierForWatcher.new)
-  Newspaper.subscribe(:product_update, ProductUpdateNotifierForChecker.new)
   Newspaper.subscribe(:came_comment_in_talk, CommentNotifierForAdmin.new)
 end
