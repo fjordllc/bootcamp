394863cb4a202c5ac7087c7135b994d4f868024d
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-07-20 21:13:10 +0900

    提出物作成時に投稿者のWatchが重複しないよう修正

--- app/models/product_author_watcher.rb
+++ app/models/product_author_watcher.rb
@@ -3,6 +3,6 @@
 class ProductAuthorWatcher
   def call(_name, _started, _finished, _unique_id, payload)
     product = payload[:product]
-    Watch.create!(user: product.user, watchable: product)
+    Watch.find_or_create_by!(user: product.user, watchable: product)
   end
 end
