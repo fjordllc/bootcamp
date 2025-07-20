f5432e922ed58669a27c293ce28c185e7dd12857
matsuyama <aya.k.matsuyama@gmail.com>
2025-07-20 14:27:17 +0900

    ProductUpdateNotifierForCheckerをnewspaperからActiveSupport::Notificationsに移行

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -28,6 +28,7 @@ Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('regular_event.update', RegularEventUpdateNotifier.new)
   ActiveSupport::Notifications.subscribe('student_or_trainee.create', TimesChannelCreator.new)
 
+  ActiveSupport::Notifications.subscribe('product.update', ProductUpdateNotifierForChecker.new)
   ActiveSupport::Notifications.subscribe('came.comment', CommentNotifier.new)
   
   learning_status_updater = LearningStatusUpdater.new
