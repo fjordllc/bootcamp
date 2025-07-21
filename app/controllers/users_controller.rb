695539030ccce9b6be62a5df4be7617501e00f7c
Miya096jp <miyawaki.yske@gmail.com>
2025-07-21 13:12:15 +0900

    TimesChannelsCreatorのフックをnewspaperからActiveSupport::notificationsに置換
    \#8938とのコンフリクトを解消
    \#8921, #8922とのコンフリクトを解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -102,7 +102,7 @@ class UsersController < ApplicationController # rubocop:todo Metrics/ClassLength
       UserMailer.welcome(@user).deliver_now
       notify_to_mentors(@user)
       notify_to_chat(@user)
-      Newspaper.publish(:student_or_trainee_create, { user: @user }) if @user.trainee?
+      ActiveSupport::Notifications.instrument('student_or_trainee.create', user: @user) if @user.trainee?
       logger.info "[Signup] 4. after create times channel for free user. #{@user.email}"
       redirect_to root_url, notice: 'サインアップメールをお送りしました。メールからサインアップを完了させてください。'
     else
@@ -148,7 +148,7 @@ class UsersController < ApplicationController # rubocop:todo Metrics/ClassLength
         UserMailer.welcome(@user).deliver_now
         notify_to_mentors(@user)
         notify_to_chat(@user)
-        Newspaper.publish(:student_or_trainee_create, { user: @user }) if @user.student?
+        ActiveSupport::Notifications.instrument('student_or_trainee.create', user: @user) if @user.student?
         flash[:x_conversion] = 'signup'
         logger.info "[Signup] 8. after create times channel. #{@user.email}"
         redirect_to root_url, notice: 'サインアップメールをお送りしました。メールからサインアップを完了させてください。'
