4b05006398667e81c73bf6fbc24cd5c13cba2e7e
sharoa119 <sau.rin1109@gmail.com>
2025-07-22 16:04:59 +0900

    不要になったコントローラーに関連するテストと、routesを削除

--- config/routes/reports.rb
+++ config/routes/reports.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :reports do
-    resources :unchecked, only: %i(index)
-  end
-
-  resources :reports do
-    resources :checks, only: %i[create destroy]
-  end
+  resources :reports
 end
