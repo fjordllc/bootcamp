cb4216b0139d33499c2a97c7cc174d6fe4e994f2
tyrrell-IH <gotothertm@gmail.com>
2025-07-22 11:22:34 +0900

    reactionsAPIのindexアクションより絵文字のJSONデータを返すよう実装

--- app/controllers/api/reactions_controller.rb
+++ app/controllers/api/reactions_controller.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class API::ReactionsController < API::BaseController
+  before_action :set_reactionable, only: %i[create index]
+
   def create
-    reactionables = params[:reactionable_id].split('_')
-    reactionable_id = reactionables.pop
-    reactionable_type = reactionables.join('_')
-    reactionable = reactionable_type.camelcase.constantize.find(reactionable_id)
-    reaction = reactionable.reactions.build(user: current_user, kind: params[:kind])
+    reaction = @reactionable.reactions.build(user: current_user, kind: params[:kind])
 
     if reaction.save
       render json: { id: reaction.id }, status: :created
@@ -19,5 +17,28 @@ class API::ReactionsController < API::BaseController
     reaction = current_user.reactions.find(params[:id])
     reaction.destroy
     render json: {}, status: :ok
+  end
+
+  def index
+    return render json: {} unless @reactionable
+
+    reactions = @reactionable.reactions.includes(:user)
+    result = Reaction.emojis.each_with_object({}) do |(kind, emoji), hash|
+      users = reactions
+              .select { |r| r.kind == kind.to_s }
+              .map { |r| r.user.login_name }
+      hash[kind] = { emoji:, users: } unless users.empty?
+    end
+
+    render json: result
+  end
+
+  private
+
+  def set_reactionable
+    type_and_id = params[:reactionable_id].to_s.split('_')
+    id = type_and_id.pop
+    type = type_and_id.join('_')
+    @reactionable = type.camelcase.constantize&.find_by(id:)
   end
 end
