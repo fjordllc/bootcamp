094d9e4c19a8649b2bd36d64b118445b3d256dda
jun-kondo <ranmaru.oo.q@gmail.com>
2025-07-23 12:18:43 +0900

    LearningCacheDestroyerをnewspaperからActiveSupport::Notificationsに移行

--- app/models/learning_cache_destroyer.rb
+++ app/models/learning_cache_destroyer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LearningCacheDestroyer
-  def call(payload)
+  def call(_name, _started, _finished, _unique_id, payload)
     user = payload[:user]
     Rails.cache.delete "/model/user/#{user.id}/completed_percentage"
     Rails.logger.info "[LearningCacheDestroyer] Cache destroyed for user #{user.id}"
