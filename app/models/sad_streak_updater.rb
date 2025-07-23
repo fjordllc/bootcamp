8b7e2a394431d4436f1ad34341c9d2d788b503e6
jun-kondo <ranmaru.oo.q@gmail.com>
2025-07-23 11:39:20 +0900

    SadStreakUpdaterをnewspaperからActiveSupport::Notificationsに移行

--- app/models/sad_streak_updater.rb
+++ app/models/sad_streak_updater.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SadStreakUpdater
-  def call(payload)
+  def call(_name, _started, _finished, _unique_id, payload)
     report = payload[:report]
     report.user.update_sad_streak
   end
--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.configuration.after_initialize do
-  sad_streak_updater = SadStreakUpdater.new
-  Newspaper.subscribe(:report_save, sad_streak_updater)
-  Newspaper.subscribe(:report_destroy, sad_streak_updater)
-
   learning_cache_destroyer = LearningCacheDestroyer.new
   Newspaper.subscribe(:learning_create, learning_cache_destroyer)
   Newspaper.subscribe(:learning_destroy, learning_cache_destroyer)
