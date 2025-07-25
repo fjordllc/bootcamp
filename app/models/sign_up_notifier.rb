7f90a16c3f6b59da1e608245707c514b95d54d63
thmz337 <th994209@gmail.com>
2025-07-26 00:13:45 +0900

    user作成時の通知をNewsPaperからActiveSupport::Notificationに変更

--- app/models/sign_up_notifier.rb
+++ app/models/sign_up_notifier.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SignUpNotifier
-  def call(_name, _started_, _finished, _unique_id, payload)
+  def call(_name, _started, _finished, _unique_id, payload)
     user = payload[:user]
     user.unsubscribe_email_token = SecureRandom.urlsafe_base64
   end
