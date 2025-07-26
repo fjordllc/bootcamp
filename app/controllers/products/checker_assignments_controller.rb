e970e7d2d6e6d77087e464c765db4323780b27fd
su-su-su-su <suzukikoji0503@gmail.com>
2025-07-26 21:50:53 +0900

    担当者ボタンに権限チェックとバリデーションを追加

--- app/controllers/products/checker_assignments_controller.rb
+++ app/controllers/products/checker_assignments_controller.rb
@@ -5,11 +5,17 @@ class Products::CheckerAssignmentsController < ApplicationController
   before_action :require_staff_login
 
   def create
+    return redirect_back(fallback_location: @product, alert: '既に担当者がいます。') if @product.checker.present?
+    return redirect_back(fallback_location: @product, alert: '担当者になる権限がありません。') unless can_be_checker
+
     @product.update!(checker: current_user)
     redirect_back(fallback_location: @product, notice: '担当になりました。')
   end
 
   def destroy
+    return redirect_back(fallback_location: @product, alert: '担当者が設定されていません。') if @product.checker.blank?
+    return redirect_back(fallback_location: @product, alert: '担当者を削除する権限がありません。') unless can_remove_checker?
+
     @product.update!(checker: nil)
     redirect_back(fallback_location: @product, notice: '担当から外れました。')
   end
@@ -19,5 +25,13 @@ class Products::CheckerAssignmentsController < ApplicationController
   def find_product
     product_id = params[:product_id] || params[:id]
     @product = Product.find(product_id)
+  end
+
+  def can_be_checker?
+    admin_or_mentor_login?
+  end
+
+  def can_remove_checker?
+    @product.checker == current_user || admin_login?
   end
 end
