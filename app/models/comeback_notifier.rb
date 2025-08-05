7b20a3f6e8930d6c0b035e0a954817f515d9011a
ryufuta <rfutago@gmail.com>
2025-08-05 15:42:30 +0900

    ComebackNotifierをnewspaperからActiveSupport::Notificationsへ移行

--- app/models/comeback_notifier.rb
+++ app/models/comeback_notifier.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ComebackNotifier
-  def call(payload)
+  def call(_name, _started, _finished, _unique_id, payload)
     user = payload[:user]
     User.admins_and_mentors.each do |admin_or_mentor|
       ActivityDelivery.with(sender: user, receiver: admin_or_mentor).notify(:comebacked)
