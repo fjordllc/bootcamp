28995cc28e79afaed8fdafc3f9979f244abe6bb9
jun-kondo <ranmaru.oo.q@gmail.com>
2025-08-08 22:02:06 +0900

    Rubocop修正

--- test/supports/avatar_helper.rb
+++ test/supports/avatar_helper.rb
@@ -3,9 +3,9 @@
 module AvatarHelper
   def reset_avatar(user)
     filename = "#{user.login_name}.jpg"
-    path = Rails.root.join("test/fixtures/files/users/avatars", filename)
+    path = Rails.root.join('test/fixtures/files/users/avatars', filename)
     user.avatar.attach(
-      io: File.open(path, "rb"),
+      io: File.open(path, 'rb'),
       filename:,
       content_type: 'image/jpeg'
     )
