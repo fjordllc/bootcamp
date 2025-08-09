412ccfc203b15c33d27cc0e2cc49320247e68b07
ryo <ta090972@icloud.com>
2025-08-09 13:02:12 +0900

    reset_avatarの説明コメントをメソッドを定義した箇所に移動

--- test/supports/avatar_helper.rb
+++ test/supports/avatar_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module AvatarHelper
+  # ユーザーアイコンがwebpに変換されていることを確認するテストは、対象となるavatarをresetする。
+  # （テスト環境では、複数のテストでavatarを共有する影響で、avatarに不具合が生じ画像変換処理が出来ない可能性があるため。）
   def reset_avatar(user)
     filename = "#{user.login_name}.jpg"
     path = Rails.root.join('test/fixtures/files/users/avatars', filename)
