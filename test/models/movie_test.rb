8b7b93b1b206057de53953203e0522662997c266
sekito <sekito1107gg@gmail.com>
2025-08-10 17:01:39 +0900

    テストを追加

---
+++ test/models/movie_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class MovieTest < ActiveSupport::TestCase
+  test "has_audio? returns false when no audio stream" do
+    movie = movies(:movie1)
+    movie.movie_data.attach(
+      io: File.open(Rails.root.join("test/fixtures/files/movies/movie.mp4")),
+      filename: "movie.mp4",
+      content_type: "video/mp4"
+    )
+
+    assert_equal false, movie.has_audio?
+  end
+
+  test "has_audio? returns true when audio stream present" do
+    movie = movies(:movie1)
+    movie.movie_data.attach(
+      io: File.open(Rails.root.join("test/fixtures/files/movies/movie_with_silent_audio.mp4")),
+      filename: "movie_with_silent_audio.mp4",
+      content_type: "video/mp4"
+    )
+
+    assert_equal true, movie.has_audio?
+  end
+end
