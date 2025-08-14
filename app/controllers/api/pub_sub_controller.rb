f84fb79a7f11ce9edfb4113d95ad8a6c60d3ba84
sekito <sekito1107gg@gmail.com>
2025-08-14 17:57:48 +0900

    JWT認証を実装

--- app/controllers/api/pub_sub_controller.rb
+++ app/controllers/api/pub_sub_controller.rb
@@ -3,6 +3,7 @@
 class API::PubSubController < API::BaseController
   skip_before_action :verify_authenticity_token
   skip_before_action :require_login_for_api
+  before_action :authenticate_pubsub_token
 
   def create
     result = ProcessTranscodingNotification.call(body: request.body.read)
@@ -10,5 +11,24 @@ class API::PubSubController < API::BaseController
     Rails.logger.error("Failed to process transcoding notification: #{result.error}") unless result.success?
 
     head :ok
+  end
+
+  private
+
+  def authenticate_pubsub_token
+    token = request.headers['Authorization']&.delete_prefix('Bearer ')
+    return if token && valid_pubsub_token?(token)
+
+    Rails.logger.warn('Unauthorized Pub/Sub request')
+    head :unauthorized
+  end
+
+  def valid_pubsub_token?(token)
+    validator = GoogleIDToken::Validator.new
+    current_audience = request.original_url
+    validator.check(token, current_audience)
+  rescue GoogleIDToken::ValidationError => e
+    Rails.logger.warn("Invalid JWT: #{e.message}")
+    false
   end
 end
