fde3b0373b36c82f7221c59557d4f191813de2fb
sekito <sekito1107gg@gmail.com>
2025-08-22 20:25:32 +0900

    integrationテストを追加、伴いpub_sub_controllerのJWT認証をテスト時は無効に

---
+++ test/integration/api/pub_sub_test.rb
@@ -0,0 +1,17 @@
+# test/integration/api/pub_sub_test.rb
+# frozen_string_literal: true
+require "test_helper"
+
+class API::PubSubTest < ActionDispatch::IntegrationTest
+  test "POST /api/pubsub returns ok" do
+    payload = { message: { data: "dummy" } }
+
+    ProcessTranscodingNotification.stub(:call, OpenStruct.new(success?: true)) do
+      post api_pubsub_path(format: :json),
+           params: payload.to_json,
+           headers: { "CONTENT_TYPE" => "application/json" }
+
+      assert_response :ok
+    end
+  end
+end
