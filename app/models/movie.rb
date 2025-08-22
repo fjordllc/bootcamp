7cebf65fd516eb6a9ecf21621389ae4cbf3573cc
thmz337 <th994209@gmail.com>
2025-08-22 21:46:16 +0900

    coderabbitの指摘事項を修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,5 +20,5 @@ class Movie < ApplicationRecord
   validates :movie_data, presence: true
 
   scope :wip, -> { where(wip: true) }
-  scope :by_tag, ->(tag) { tagged_with(tag) if tag }
+  scope :by_tag, ->(tag) { tag.present? ? tagged_with(tag) : all }
 end
