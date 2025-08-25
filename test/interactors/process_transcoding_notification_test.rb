05a84ea98223a68279c4f9937ae15d58728e6e09
sekito <sekito1107gg@gmail.com>
2025-08-26 08:26:48 +0900

    rubocopによる修正

--- test/interactors/process_transcoding_notification_test.rb
+++ test/interactors/process_transcoding_notification_test.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
-require "test_helper"
+
+require 'test_helper'
 
 class ProcessTranscodingNotificationTest < ActiveSupport::TestCase
   def build_message(data)
     {
-      "message" => {
-        "data" => Base64.encode64(data.to_json)
+      'message' => {
+        'data' => Base64.encode64(data.to_json)
       }
     }.to_json
   end
@@ -14,9 +15,9 @@ class ProcessTranscodingNotificationTest < ActiveSupport::TestCase
     @movie = movies(:movie1)
   end
 
-  test "SUCCEEDED job calls attach_transcoded_file" do
-    body = build_message({ "job" => { "name" => "job-1", "state" => "SUCCEEDED" } })
-    interactor = ProcessTranscodingNotification.new(body: body)
+  test 'SUCCEEDED job calls attach_transcoded_file' do
+    body = build_message({ 'job' => { 'name' => 'job-1', 'state' => 'SUCCEEDED' } })
+    interactor = ProcessTranscodingNotification.new(body:)
 
     attach_called = false
     interactor.stub(:find_movie, @movie) do
@@ -25,23 +26,23 @@ class ProcessTranscodingNotificationTest < ActiveSupport::TestCase
       end
     end
 
-    assert attach_called, "attach_transcoded_file should be called for SUCCEEDED jobs"
+    assert attach_called, 'attach_transcoded_file should be called for SUCCEEDED jobs'
   end
 
-  test "job with missing name fails with retryable=false" do
-    body = build_message({ "job" => { "state" => "SUCCEEDED" } })
-    interactor = ProcessTranscodingNotification.new(body: body)
+  test 'job with missing name fails with retryable=false' do
+    body = build_message({ 'job' => { 'state' => 'SUCCEEDED' } })
+    interactor = ProcessTranscodingNotification.new(body:)
 
     error = assert_raises(Interactor::Failure) { interactor.call }
     failure_context = error.context
 
-    assert_equal false, failure_context.retryable
-    assert_equal "Pub/Sub message missing required job fields: name=, state=SUCCEEDED", failure_context.error
+    assert_not(failure_context.retryable)
+    assert_equal 'Pub/Sub message missing required job fields: name=, state=SUCCEEDED', failure_context.error
   end
 
-  test "job with unknown movie fails with retryable=false" do
-    body = build_message({ "job" => { "name" => "unknown", "state" => "SUCCEEDED" } })
-    interactor = ProcessTranscodingNotification.new(body: body)
+  test 'job with unknown movie fails with retryable=false' do
+    body = build_message({ 'job' => { 'name' => 'unknown', 'state' => 'SUCCEEDED' } })
+    interactor = ProcessTranscodingNotification.new(body:)
 
     error = assert_raises(Interactor::Failure) do
       interactor.stub(:find_movie, nil) { interactor.call }
@@ -49,45 +50,45 @@ class ProcessTranscodingNotificationTest < ActiveSupport::TestCase
     failure_context = error.context
 
     assert failure_context.failure?
-    refute failure_context.retryable
+    assert_not failure_context.retryable
   end
 
-  test "FAILED job with unexpected error is retryable" do
+  test 'FAILED job with unexpected error is retryable' do
     body = build_message({
-      "job" => {
-        "name" => "job-1",
-        "state" => "FAILED",
-        "error" => {
-          "details" => [{ "fieldViolations" => [{ "description" => "Unexpected error" }] }]
-        }
-      }
-    })
-    interactor = ProcessTranscodingNotification.new(body: body)
+                           'job' => {
+                             'name' => 'job-1',
+                             'state' => 'FAILED',
+                             'error' => {
+                               'details' => [{ 'fieldViolations' => [{ 'description' => 'Unexpected error' }] }]
+                             }
+                           }
+                         })
+    interactor = ProcessTranscodingNotification.new(body:)
 
     error = assert_raises(Interactor::Failure) { interactor.call }
     failure_context = error.context
 
     assert failure_context.failure?
-    assert failure_context.retryable, "Unexpected errors should be retryable"
+    assert failure_context.retryable, 'Unexpected errors should be retryable'
   end
 
-  test "invalid JSON fails with retryable=false" do
-    body = "{ invalid json"
-    interactor = ProcessTranscodingNotification.new(body: body)
+  test 'invalid JSON fails with retryable=false' do
+    body = '{ invalid json'
+    interactor = ProcessTranscodingNotification.new(body:)
 
     error = assert_raises(Interactor::Failure) { interactor.call }
     failure_context = error.context
 
     assert failure_context.failure?
-    refute failure_context.retryable
+    assert_not failure_context.retryable
   end
 
-  test "get_movie_id raises Google::Cloud::Error is retryable" do
-    body = build_message({ "job" => { "name" => "job-1", "state" => "SUCCEEDED" } })
-    interactor = ProcessTranscodingNotification.new(body: body)
+  test 'get_movie_id raises Google::Cloud::Error is retryable' do
+    body = build_message({ 'job' => { 'name' => 'job-1', 'state' => 'SUCCEEDED' } })
+    interactor = ProcessTranscodingNotification.new(body:)
 
     error = assert_raises(Interactor::Failure) do
-      interactor.stub(:find_movie, ->(*) { raise Google::Cloud::Error, "boom" }) do
+      interactor.stub(:find_movie, ->(*) { raise Google::Cloud::Error, 'boom' }) do
         interactor.call
       end
     end
@@ -97,25 +98,27 @@ class ProcessTranscodingNotificationTest < ActiveSupport::TestCase
     assert failure_context.retryable
   end
 
-  test "FAILED job with AudioMissing error triggers TranscodeJob.perform_later" do
+  test 'FAILED job with AudioMissing error triggers TranscodeJob.perform_later' do
     body = build_message({
-      "job" => {
-        "name" => "job-1",
-        "state" => "FAILED",
-        "error" => {
-          "details" => [{ "fieldViolations" => [{ "description" => "AudioMissing" }] }]
-        }
-      }
-    })
-    interactor = ProcessTranscodingNotification.new(body: body)
+                           'job' => {
+                             'name' => 'job-1',
+                             'state' => 'FAILED',
+                             'error' => {
+                               'details' => [{ 'fieldViolations' => [{ 'description' => 'AudioMissing' }] }]
+                             }
+                           }
+                         })
+    interactor = ProcessTranscodingNotification.new(body:)
 
     perform_later_called = false
     interactor.stub(:find_movie, @movie) do
       TranscodeJob.stub(:perform_later, ->(*) { perform_later_called = true }) do
-        interactor.call rescue nil
+        interactor.call
+      rescue StandardError
+        nil
       end
     end
 
-    assert perform_later_called, "AudioMissing error should trigger TranscodeJob.perform_later"
+    assert perform_later_called, 'AudioMissing error should trigger TranscodeJob.perform_later'
   end
 end
