24d3041ac76e3ba1244e71b8ef6f135be5025276
smallmonkeykey <sarinitakuoo@gmail.com>
2025-08-26 12:40:06 +0900

    不要なNewspaper.publishを削除 NewspaperとNotificationsの併用に関する懸念は解消済みのため

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -58,7 +58,6 @@ class QuestionsController < ApplicationController
     set_wip
     if @question.save
       ActiveSupport::Notifications.instrument('question.create', question: @question)
-      Newspaper.publish(:question_create, { question: @question })
       redirect_to Redirection.determin_url(self, @question), notice: @question.generate_notice_message(:create)
     else
       render :new
@@ -68,10 +67,7 @@ class QuestionsController < ApplicationController
   def update
     set_wip
     if @question.update(question_params)
-      if @question.saved_change_to_wip?
-        ActiveSupport::Notifications.instrument('question.update', question: @question)
-        Newspaper.publish(:question_update, { question: @question })
-      end
+      ActiveSupport::Notifications.instrument('question.update', question: @question) if @question.saved_change_to_wip?
       redirect_to Redirection.determin_url(self, @question), notice: @question.generate_notice_message(:update)
     else
       render :edit
