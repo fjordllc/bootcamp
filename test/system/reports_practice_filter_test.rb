a3a6886bcdf99ea62fd836a3734dfc22b3d9fdf2
sharoa119 <sau.rin1109@gmail.com>
2025-08-26 14:41:51 +0900

    Node を20 に更新し、テストを安定化

--- test/system/reports_practice_filter_test.rb
+++ test/system/reports_practice_filter_test.rb
@@ -3,13 +3,10 @@
 require 'application_system_test_case'
 
 class ReportsPracticeFilterTest < ApplicationSystemTestCase
-  driven_by :rack_test
-
   setup do
     @user = users(:kimura)
     @practice1 = practices(:practice1)
     @practice2 = practices(:practice2)
-
     @report1 = Report.create!(
       user: @user,
       title: 'レポート1',
@@ -34,12 +31,12 @@ class ReportsPracticeFilterTest < ApplicationSystemTestCase
 
   test 'GET filter by practice works' do
     visit reports_path(practice_id: @practice1.id)
-    assert_text 'レポート1'
-    assert_no_text 'レポート2'
+    assert_text 'レポート1', wait: 5
+    assert_no_text 'レポート2', wait: 5
   end
 
   test 'practice filter is hidden when unchecked parameter is present' do
     visit reports_path(unchecked: true)
-    assert_no_selector 'select#js-choices-single-select'
+    assert_no_selector 'select#js-choices-single-select', wait: 5
   end
 end
