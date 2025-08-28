d35574bd7137775ec115281157dc7c9e17d25999
komagata <komagata@gmail.com>
2025-08-28 11:47:00 +0900

    アプリケーション固有設定を復元
    - タイムゾーン設定（Tokyo）
    - 国際化設定（日本語）
    - eager_loadパスの設定（lib、presenters）
    - フィールドエラー表示設定
    - Active Storage設定
    - ViewComponent設定
    - Doorkeeper設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,23 @@ module Bootcamp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
+
+    config.paths.add "lib", eager_load: true
+    config.paths.add "app/presenters", eager_load: true
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      html_tag.html_safe
+    end
+
+    config.active_storage.resolve_model_to_route = :rails_storage_proxy
+    config.active_storage.variant_processor = :vips
+
+    config.view_component.capture_compatibility_patch_enabled = true
+
+    config.to_prepare do
+      Doorkeeper::AuthorizationsController.layout "authorization"
+    end
   end
 end
