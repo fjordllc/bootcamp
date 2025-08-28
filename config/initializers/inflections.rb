7857f35e78644b39accda89206a392be60acfbb6
komagata <komagata@gmail.com>
2025-08-28 11:49:35 +0900

    rails app:updateで失われた設定を復元
    - inflections.rb: API/AI/FAQ略語設定、buzzes複数形、progress不可算名詞設定を復元
    - filter_parameter_logging.rb: reCAPTCHA (g-recaptcha) パラメーターフィルタリング設定を復元
    - puma.rb: 開発環境用worker_timeout設定やスレッド数設定を復元

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,6 +11,10 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
-# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym "API"
+  inflect.acronym "AI"
+  inflect.acronym "FAQ"
+  inflect.irregular "buzz", "buzzes"
+  inflect.uncountable "progress"
+end
