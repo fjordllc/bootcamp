d2482d06f8b5832dd4d99b54a8c81b86efcb5385
Miya096jp <miyawaki.yske@gmail.com>
2025-08-29 09:23:02 +0900

    routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -132,6 +132,7 @@ Rails.application.routes.draw do
   get '/', to: 'home#index', as: :niconico_calendar_date, constraints: niconico_calendar_constraints
   get '/users/:id', to: 'users#show', as: :niconico_calendar_date_in_profile, constraints: niconico_calendar_constraints
   resource :buzz, only: %i(show edit update), controller: "buzz"
+  resources :buzzes, only: %i[index]
   resources :movies
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   mount MissionControl::Jobs::Engine, at: "/jobs"
