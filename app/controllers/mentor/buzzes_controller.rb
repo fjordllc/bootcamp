d6740f00e2f6751facda196fc372f1f9ef3a8fb0
Miya096jp <miyawaki.yske@gmail.com>
2025-08-29 09:26:28 +0900

    buzzes_controllerとmento/buzzes_controllerを生成

---
+++ app/controllers/mentor/buzzes_controller.rb
@@ -0,0 +1,19 @@
+class Mentor::BuzzesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
