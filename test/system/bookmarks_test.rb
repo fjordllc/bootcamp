d5f4577516132f726788f893c7fe5f78917a736b
jun-kondo <ranmaru.oo.q@gmail.com>
2025-08-30 17:18:24 +0900

    Bookmarkボタンのテストでボタン読み込み状態の待機処理を追加

--- test/system/bookmarks_test.rb
+++ test/system/bookmarks_test.rb
@@ -11,21 +11,29 @@ class BookmarksTest < ApplicationSystemTestCase
 
   test 'show my bookmark report' do
     visit_with_auth "/reports/#{@report.id}", 'komagata'
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-active'
     assert_no_selector '#bookmark-button.is-inactive'
+    assert_selector '#bookmark-button', text: 'Bookmark中'
   end
 
   test 'show not bookmark report' do
     visit_with_auth "/reports/#{@report.id}", 'machida'
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-inactive'
     assert_no_selector '#bookmark-button.is-active'
+    assert_selector '#bookmark-button', text: 'Bookmark'
   end
 
   test 'bookmark' do
     visit_with_auth "/reports/#{@report.id}", 'machida'
+    wait_for_javascript_components
+    assert_selector '#bookmark-button.is-inactive', text: 'Bookmark'
     find('#bookmark-button').click
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-active'
     assert_no_selector '#bookmark-button.is-inactive'
+    assert_selector '#bookmark-button', text: 'Bookmark中'
 
     visit '/current_user/bookmarks'
     assert_text @report.title
@@ -33,10 +41,13 @@ class BookmarksTest < ApplicationSystemTestCase
 
   test 'unbookmark' do
     visit_with_auth "/reports/#{@report.id}", 'komagata'
-    assert_selector '#bookmark-button.is-active'
+    wait_for_javascript_components
+    assert_selector '#bookmark-button.is-active', text: 'Bookmark中'
     find('#bookmark-button').click
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-inactive'
     assert_no_selector '#bookmark-button.is-active'
+    assert_selector '#bookmark-button', text: 'Bookmark'
 
     visit '/current_user/bookmarks'
     assert_no_text @report.title
@@ -49,21 +60,29 @@ class BookmarksTest < ApplicationSystemTestCase
 
   test 'show active button when bookmarked question' do
     visit_with_auth "/questions/#{@question.id}", 'kimura'
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-active'
     assert_no_selector '#bookmark-button.is-inactive'
+    assert_selector '#bookmark-button', text: 'Bookmark中'
   end
 
   test 'show inactive button when not bookmarked question' do
     visit_with_auth "/questions/#{@question.id}", 'hajime'
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-inactive'
     assert_no_selector '#bookmark-button.is-active'
+    assert_selector '#bookmark-button', text: 'Bookmark'
   end
 
   test 'bookmark question' do
     visit_with_auth "/questions/#{@question.id}", 'hatsuno'
+    wait_for_javascript_components
+    assert_selector '#bookmark-button.is-inactive', text: 'Bookmark'
     find('#bookmark-button').click
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-active'
     assert_no_selector '#bookmark-button.is-inactive'
+    assert_selector '#bookmark-button', text: 'Bookmark中'
 
     visit '/current_user/bookmarks'
     assert_text @question.title
@@ -71,10 +90,13 @@ class BookmarksTest < ApplicationSystemTestCase
 
   test 'unbookmark question' do
     visit_with_auth "/questions/#{@question.id}", 'kimura'
-    assert_selector '#bookmark-button.is-active'
+    wait_for_javascript_components
+    assert_selector '#bookmark-button.is-active', text: 'Bookmark中'
     find('#bookmark-button').click
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-inactive'
     assert_no_selector '#bookmark-button.is-active'
+    assert_selector '#bookmark-button', text: 'Bookmark'
 
     visit '/current_user/bookmarks'
     assert_no_text @question.title
@@ -82,9 +104,13 @@ class BookmarksTest < ApplicationSystemTestCase
 
   test 'bookmark announcement' do
     visit_with_auth "/announcements/#{@announcement.id}", 'hatsuno'
+    wait_for_javascript_components
+    assert_selector '#bookmark-button.is-inactive', text: 'Bookmark'
     find('#bookmark-button').click
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-active'
     assert_no_selector '#bookmark-button.is-inactive'
+    assert_selector '#bookmark-button', text: 'Bookmark中'
 
     visit '/current_user/bookmarks'
     assert_text @announcement.title
@@ -95,10 +121,13 @@ class BookmarksTest < ApplicationSystemTestCase
     user.bookmarks.create!(bookmarkable: @announcement)
 
     visit_with_auth "/announcements/#{@announcement.id}", user.login_name
-    assert_selector '#bookmark-button.is-active'
+    wait_for_javascript_components
+    assert_selector '#bookmark-button.is-active', text: 'Bookmark中'
     find('#bookmark-button').click
+    wait_for_javascript_components
     assert_selector '#bookmark-button.is-inactive'
     assert_no_selector '#bookmark-button.is-active'
+    assert_selector '#bookmark-button', text: 'Bookmark'
 
     visit '/current_user/bookmarks'
     assert_no_text @announcement.title
