b06bede2f788d9476430339c84429cc417c4951b
thmz337 <th994209@gmail.com>
2025-08-30 09:18:20 +0900

    prettierによる指摘事項の修正

--- app/javascript/loading.js
+++ app/javascript/loading.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
   const loading = document.getElementById('loading-screen')
   const body = document.getElementById('loading-body')
-  
+
   if (loading) {
     loading.style.display = 'none'
     body.style.removeProperty('display')
