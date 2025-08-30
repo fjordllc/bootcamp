2b6d4f50e9ebb9f74e5eef6560d437ef37f994c0
Miya096jp <miyawaki.yske@gmail.com>
2025-08-30 20:02:06 +0900

    Buzzesテーブルからdefaultオプションを削除

--- db/schema.rb
+++ db/schema.rb
@@ -113,10 +113,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_11_162031) do
   create_table "buzzes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "url", default: "", null: false
-    t.string "title", default: "", null: false
-    t.date "published_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.text "memo", default: ""
+    t.string "url", null: false
+    t.string "title", null: false
+    t.date "published_at", null: false
+    t.text "memo"
   end
 
   create_table "campaigns", force: :cascade do |t|
