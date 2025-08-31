9fa3263fc2650243ab8f557015a40fdbeb70e5d3
Miya096jp <miyawaki.yske@gmail.com>
2025-08-31 09:26:46 +0900

    Mentor::BuzzesController#createを作成

--- app/controllers/mentor/buzzes_controller.rb
+++ app/controllers/mentor/buzzes_controller.rb
@@ -7,13 +7,44 @@ class Mentor::BuzzesController < ApplicationController
     @buzzes = Buzz.order(published_at: :desc, id: :desc).page(params[:page]).per(PER_PAGE)
   end
 
-  def new; end
+  def new
+    @buzz = Buzz.new
+  end
 
   def edit; end
 
-  def create; end
+  def create
+    @buzz = Buzz.new(buzz_params)
+
+    doc = Buzz.doc_from_url(buzz_params[:url])
+    date = Buzz.date_from_doc(doc)
+
+    @buzz.title = Buzz.title_from_doc(doc) if buzz_params[:title].empty?
+
+    if buzz_params[:published_at].empty?
+      if date.nil?
+        flash.now[:alert] = '日付を入力してください'
+      else
+        @buzz.published_at = date
+      end
+    end
+
+    if @buzz.save
+      redirect_to mentor_buzzes_path, notice: 'Buzz was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def buzz_params
+    keys = %i[title memo published_at]
+    keys << :url if action_name == 'create'
+    params.require(:buzz).permit(*keys)
+  end
 end
