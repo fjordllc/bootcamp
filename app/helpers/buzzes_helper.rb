c8af75883cb0d66c69339357b70374a055e1d148
Miya096jp <miyawaki.yske@gmail.com>
2025-09-01 20:35:06 +0900

    helpers/buzzes_helper.rbを作成しヘルパーメソッドを定義

---
+++ app/helpers/buzzes_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module BuzzesHelper
+  def buzzes_year(buzzes)
+    buzzes[0].published_at.year
+  end
+
+  def group_buzzes_by_month(buzzes)
+    buzzes.group_by { |b| b.published_at.month }
+  end
+end
