2afd2e0a88856ff41aff919bbf7dd01fe22ead7f
Miya096jp <miyawaki.yske@gmail.com>
2025-09-01 19:28:47 +0900

    LPのviewのレイアウトの大枠を整えた
    年・月ごとに記事が降順の表示されるように変更

--- app/controllers/buzzes_controller.rb
+++ app/controllers/buzzes_controller.rb
@@ -5,7 +5,7 @@ class BuzzesController < ApplicationController
     @years = Buzz.years
     year = params[:year] || Buzz.latest_year
     @year = year
-    @buzzes = Buzz.for_year(year)
+    @buzzes = Buzz.for_year(year).order(published_at: :desc, id: :desc)
     render layout: 'lp'
   end
 end
