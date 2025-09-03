5d20af4167cfb48be56f5b6dd7d696906fc63763
smallmonkeykey <sarinitakuoo@gmail.com>
2025-09-03 16:33:37 +0900

    Queryオブジェクトにリファクタリング Product.self_assigned_no_replied_product_idsメソッドをリファクタリング

--- app/models/product.rb
+++ app/models/product.rb
@@ -64,41 +64,8 @@ class Product < ApplicationRecord # rubocop:todo Metrics/ClassLength
     end
   end
 
-  # rubocop:disable Metrics/MethodLength
-  def self.self_assigned_no_replied_product_ids(user_id)
-    sql = <<~SQL
-      WITH last_comments AS (
-        SELECT *
-        FROM comments AS parent
-        WHERE commentable_type = 'Product' AND id = (
-          SELECT id
-          FROM comments AS child
-          WHERE parent.commentable_id = child.commentable_id
-            AND commentable_type = 'Product'
-          ORDER BY created_at DESC LIMIT 1
-        )
-      ),
-      self_assigned_products AS (
-        SELECT products.*
-        FROM products
-        WHERE checker_id = ?
-        AND wip = false
-      )
-      SELECT self_assigned_products.id
-      FROM self_assigned_products
-      LEFT JOIN last_comments ON self_assigned_products.id = last_comments.commentable_id
-      WHERE last_comments.id IS NULL
-      OR self_assigned_products.checker_id != last_comments.user_id
-      ORDER BY self_assigned_products.created_at DESC
-    SQL
-    Product.find_by_sql([sql, user_id]).map(&:id)
-  end
-  # rubocop:enable Metrics/MethodLength
-
   def self.self_assigned_no_replied_products(user_id)
-    no_replied_product_ids = self_assigned_no_replied_product_ids(user_id)
-    Product.where(id: no_replied_product_ids)
-           .order(published_at: :asc, id: :asc)
+    ProductSelfAssignedNoRepliedQuery.new(user_id:).call
   end
 
   def self.require_assignment_products
