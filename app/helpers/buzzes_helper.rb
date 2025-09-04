526eb834150f8ffe66c4247e28580f8e8bc128c4
Miya096jp <miyawaki.yske@gmail.com>
2025-09-04 11:20:03 +0900

    BuzzesHelper#buzzes_yearを削除しyearをコントローラーから直接渡すように修正

--- app/helpers/buzzes_helper.rb
+++ app/helpers/buzzes_helper.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module BuzzesHelper
-  def buzzes_year(buzzes)
-    buzzes.first&.published_at&.year
-  end
-
   def group_buzzes_by_month(buzzes)
     buzzes.group_by { |b| b.published_at.month }
   end
