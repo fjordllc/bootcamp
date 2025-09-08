35f583b17f3b08a90b368435bdfe6358bb9b14e0
sharoa119 <sau.rin1109@gmail.com>
2025-09-08 10:47:58 +0900

    TimeRangeHelperで曜日をI18nから取得するように変更

--- app/helpers/time_range_helper.rb
+++ app/helpers/time_range_helper.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-JAPANESE_WEEKDAYS = %w[日 月 火 水 木 金 土].freeze
-
 module TimeRangeHelper
   def time_range(time)
     start_hour = time.change(min: 0, sec: 0)
     end_hour = start_hour + 1.hour
 
-    "(#{JAPANESE_WEEKDAYS[time.wday]}) #{start_hour.strftime('%H:%M')} 〜 #{end_hour.strftime('%H:%M')}"
+    "(#{I18n.t('date.abbr_day_names')[time.wday]}) #{start_hour.strftime('%H:%M')} 〜 #{end_hour.strftime('%H:%M')}"
   end
 end
