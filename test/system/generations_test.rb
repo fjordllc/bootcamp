fbd51080fbf49c59890f0f58fb9e0c4883256593
thmz337 <th994209@gmail.com>
2025-09-08 22:33:58 +0900

    リンククリック時のテストを追加

--- test/system/generations_test.rb
+++ test/system/generations_test.rb
@@ -292,20 +292,53 @@ class GenerationsTest < ApplicationSystemTestCase
     end
   end
 
-  test 'a link will appear if there is activity' do
+  test 'navigates to user activity when clicking count link' do
     user = users(:kimura)
 
     visit_with_auth generation_path(user.generation), 'kimura'
 
     within('.users-item', text: user.name) do
       assert_selector "a[href='/users/#{user.id}/reports']", text: '1'
+      within('.card-counts__item-inner', text: '日報') do
+        click_link user.reports.count.to_s
+      end
+      assert_current_path("/users/#{user.id}/reports")
+    end
+
+    visit generation_path(user.generation)
+
+    within('.users-item', text: user.name) do
       assert_selector "a[href='/users/#{user.id}/products']", text: '11'
+      within('.card-counts__item-inner', text: '提出物') do
+        click_link user.products.count.to_s
+      end
+      assert_current_path("/users/#{user.id}/products")
+    end
+
+    visit generation_path(user.generation)
+
+    within('.users-item', text: user.name) do
+      assert_selector "a[href='/users/#{user.id}/questions']", text: '4'
+      within('.card-counts__item-inner', text: '質問') do
+        click_link user.questions.count.to_s
+      end
+      assert_current_path("/users/#{user.id}/questions")
+    end
+  end
+
+  test 'shows 0 without link when user has no activity' do
+    user = users(:kimura)
+    visit_with_auth generation_path(user.generation), 'kimura'
+
+    within('.users-item', text: user.name) do
       within('.card-counts__item-inner', text: 'コメント') do
         assert_selector '.is-empty'
+        assert_text '0'
       end
-      assert_selector "a[href='/users/#{user.id}/questions']", text: '4'
+
       within('.card-counts__item-inner', text: '回答') do
         assert_selector '.is-empty'
+        assert_text '0'
       end
     end
   end
