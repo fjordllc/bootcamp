27f9f331807558963a9ae485f9fe1ce1655bd743
sharoa119 <sau.rin1109@gmail.com>
2025-09-09 11:27:22 +0900

    unconfirmed_links_openのテストを作成（通知と日報）

---
+++ test/system/report/unconfirmed_links_open_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class Report::UnconfirmedLinksOpenTest < ApplicationSystemTestCase
+  setup do
+    @mentor = users(:komagata)
+    @student = users(:kimura)
+  end
+
+  test 'mentor sees bulk open button when unchecked reports exist' do
+    visit_with_auth reports_path(unchecked: true), 'komagata'
+    assert_selector 'button', text: '未チェックの日報を一括で開く'
+  end
+
+  test 'mentor does not see bulk open button when no unchecked reports exist' do
+    Report.unchecked.find_each do |report|
+      report.checks.create!(user: @mentor)
+    end
+
+    visit_with_auth reports_path(unchecked: true), 'komagata'
+    assert_no_selector 'button', text: '未チェックの日報を一括で開く'
+  end
+
+  test 'non-mentor never sees bulk open button' do
+    visit_with_auth reports_path(unchecked: true), 'kimura'
+    assert_no_selector 'button', text: '未チェックの日報を一括で開く'
+  end
+end
