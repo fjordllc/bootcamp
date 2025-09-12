7b0753b3c1c04d3c5340acfc8ad074492d843490
jun-kondo <ranmaru.oo.q@gmail.com>
2025-09-12 23:41:48 +0900

    学習連続記録を表示するUserStudyStreakTrackerComponentを新規作成

---
+++ app/components/study_streak/user_study_streak_tracker_component.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class StudyStreak::UserStudyStreakTrackerComponent < ViewComponent::Base
+  def initialize(study_streak:)
+    @study_streak = study_streak
+  end
+
+  def current_streak?
+    @study_streak.current_days.to_i.positive?
+  end
+
+  def longest_streak?
+    @study_streak.longest_days.to_i.positive?
+  end
+
+  def current_streak_days
+    @study_streak.current_days.to_i
+  end
+
+  def longest_streak_days
+    @study_streak.longest_days.to_i
+  end
+
+  def current_streak_period
+    format_period(
+      days: @study_streak.current_days,
+      start_on: @study_streak.current_start_on,
+      end_on: @study_streak.current_end_on
+    )
+  end
+
+  def longest_streak_period
+    format_period(
+      days: @study_streak.longest_days,
+      start_on: @study_streak.longest_start_on,
+      end_on: @study_streak.longest_end_on
+    )
+  end
+
+  private
+
+  def format_period(days:, start_on:, end_on:)
+    return '' if days.to_i.zero? || start_on.blank? || end_on.blank?
+
+    if start_on.year == end_on.year
+      "#{start_on.strftime('%b %-d')} - #{end_on.strftime('%b %-d')}"
+    else
+      "#{start_on.strftime('%b %-d, %Y')} - #{end_on.strftime('%b %-d, %Y')}"
+    end
+  end
+end
