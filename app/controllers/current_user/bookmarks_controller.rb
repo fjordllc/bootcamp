bb0430d7cb23aba5dfdb821032ef83da15df4eea
smallmonkeykey <sarinitakuoo@gmail.com>
2025-09-15 22:48:39 +0900

    非Reactに書き換える

--- app/controllers/current_user/bookmarks_controller.rb
+++ app/controllers/current_user/bookmarks_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class CurrentUser::BookmarksController < ApplicationController
+  PAGER_NUMBER = 20
+
   def index
     @user = current_user
+    @bookmarks = current_user.bookmarks.includes(:bookmarkable).order(created_at: :desc, id: :desc).page(params[:page]).per(PAGER_NUMBER)
   end
 end
