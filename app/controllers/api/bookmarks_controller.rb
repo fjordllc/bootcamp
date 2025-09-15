a4fdda987a353c600d97ec0491607846e9ec89bd
smallmonkeykey <sarinitakuoo@gmail.com>
2025-09-15 22:48:39 +0900

    非Reactに書き換える

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class API::BookmarksController < API::BaseController
-  PAGER_NUMBER = 25
+  PAGER_NUMBER = 20
 
   def index
     per = params[:per] || PAGER_NUMBER
@@ -30,7 +30,15 @@ class API::BookmarksController < API::BaseController
 
   def destroy
     Bookmark.find(params[:id]).destroy
-    head :no_content
+    @bookmarks = current_user.bookmarks.includes(:bookmarkable).order(created_at: :desc, id: :desc).page(params[:page]).per(PAGER_NUMBER)
+
+    respond_to do |format|
+      format.json do
+        render partial: 'current_user/bookmarks/list',
+               formats: [:html],
+               locals: { bookmarks: @bookmarks }
+      end
+    end
   end
 
   private
