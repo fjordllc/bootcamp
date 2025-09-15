36c4f6e659fb068a503d777a43469ef7ec4c4155
appsu-mi <natubate870@gmail.com>
2025-09-15 11:49:11 +0900

    ペアワーク作成者の場合の表示を変更、partialを導入し整理した

--- app/helpers/pair_work_helper.rb
+++ app/helpers/pair_work_helper.rb
@@ -27,4 +27,8 @@ module PairWorkHelper
   def checked?(target_date, id)
     !disabled?(target_date) && current_user.learning_time_frame_ids.include?(id)
   end
+
+  def pair_work_user?(pair_work, current_user)
+    pair_work.user == current_user
+  end
 end
