3f1a3c3eb10a8fe7fe7458373b96c9540eddd7a7
smallmonkeykey <sarinitakuoo@gmail.com>
2025-09-17 12:03:42 +0900

    削除後に編集ボタンが効いてなかったので修正した

--- app/javascript/bookmarks-delete-button.js
+++ app/javascript/bookmarks-delete-button.js
@@ -16,11 +16,28 @@ document.addEventListener('DOMContentLoaded', () => {
         Accept: 'text/html'
       }
     })
-      .then((response) => response.text())
-      .then((html) => {
-        const pageMain = document.querySelector('.page-main')
-        if (pageMain) pageMain.outerHTML = html
+      .then(() => {
+        const cardListItems = document.querySelectorAll('.card-list-item')
+
+        if (cardListItems.length === 1) {
+          deleteButton.closest('.card-list-item').remove()
+
+          const pageBody = document.querySelector('.page-body')
+          if (pageBody) {
+            pageBody.innerHTML = `
+        <div class="o-empty-message">
+          <div class="o-empty-message__icon">
+            <i class="fa-regular fa-face-sad-tear"></i>
+            <p class="o-empty-message__text">ブックマークはまだありません。</p>
+          </div>
+        </div>
+      `
+          }
+        } else {
+          deleteButton.closest('.card-list-item').remove()
+        }
       })
+
       .catch((error) => console.error('削除に失敗しました', error))
   })
 })
