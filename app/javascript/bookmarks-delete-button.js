6cc6efeee2784aea8bc299cdcbc8e323a6d5859a
smallmonkeykey <sarinitakuoo@gmail.com>
2025-09-17 18:17:16 +0900

    request.jsを使って書き換えた

--- app/javascript/bookmarks-delete-button.js
+++ app/javascript/bookmarks-delete-button.js
@@ -1,42 +1,47 @@
+import { destroy } from '@rails/request.js'
+
 document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('click', (event) => {
     const deleteButton = event.target.closest('.bookmark-delete-button')
     if (!deleteButton) return
 
     event.preventDefault()
-    
-    const url = deleteButton.dataset.url
 
-    fetch(url, {
-      method: 'DELETE',
-      headers: {
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]')
-          .content,
-        Accept: 'text/html'
-      }
-    })
-      .then(() => {
-        const cardListItems = document.querySelectorAll('.card-list-item')
-
-        if (cardListItems.length === 1) {
-          deleteButton.closest('.card-list-item').remove()
-
-          const pageBody = document.querySelector('.page-body')
-          if (pageBody) {
-            pageBody.innerHTML = `
-        <div class="o-empty-message">
-          <div class="o-empty-message__icon">
-            <i class="fa-regular fa-face-sad-tear"></i>
-            <p class="o-empty-message__text">ブックマークはまだありません。</p>
-          </div>
-        </div>
-      `
-          }
-        } else {
-          deleteButton.closest('.card-list-item').remove()
-        }
-      })
-
-      .catch((error) => console.error('削除に失敗しました', error))
+    deleteBookmark(deleteButton)
   })
 })
+
+async function deleteBookmark(deleteButton) {
+  const url = deleteButton.dataset.url
+  try {
+    const response = await destroy(url, {
+      headers: { Accept: 'text/html' }
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const cardListItems = document.querySelectorAll('.card-list-item')
+
+    if (cardListItems.length === 1) {
+      deleteButton.closest('.card-list-item').remove()
+
+      const pageBody = document.querySelector('.page-body')
+      if (pageBody) {
+        pageBody.innerHTML = `
+          <div class="o-empty-message">
+            <div class="o-empty-message__icon">
+              <i class="fa-regular fa-face-sad-tear"></i>
+              <p class="o-empty-message__text">ブックマークはまだありません。</p>
+            </div>
+          </div>
+        `
+      }
+    } else {
+      deleteButton.closest('.card-list-item').remove()
+    }
+  } catch (error) {
+    console.warn(error)
+  }
+}
