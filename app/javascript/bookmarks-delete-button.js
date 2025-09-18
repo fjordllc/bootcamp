552e59bd1cd42891d5fae3405192a7e0374fdd38
smallmonkeykey <sarinitakuoo@gmail.com>
2025-09-18 15:52:20 +0900

    ブックマーク削除時に部分更新ではなく全体を再描画するよう変更

--- app/javascript/bookmarks-delete-button.js
+++ app/javascript/bookmarks-delete-button.js
@@ -1,48 +1,30 @@
 import { destroy } from '@rails/request.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.addEventListener('click', (event) => {
+  document.addEventListener('click', async (event) => {
     const deleteButton = event.target.closest('.bookmark-delete-button')
     if (!deleteButton) return
 
     event.preventDefault()
 
-    deleteBookmark(deleteButton)
+    const url = deleteButton.dataset.url
+
+    try {
+      const response = await destroy(url, {
+        responseKind: 'html',
+        headers: { Accept: 'text/html' }
+      })
+
+      console.log(response)
+
+      if (!response.ok) {
+        throw new Error(`削除に失敗しました。`)
+      }
+
+      const html = await response.text
+      document.querySelector('.page-main').outerHTML = html
+    } catch (error) {
+      console.warn(error)
+    }
   })
 })
-
-async function deleteBookmark(deleteButton) {
-  const url = deleteButton.dataset.url
-  try {
-    const response = await destroy(url, {
-      responseKind: 'html',
-      headers: { Accept: 'text/html' }
-    })
-
-    if (!response.ok) {
-      throw new Error(`削除に失敗しました。`)
-    }
-
-    const cardListItems = document.querySelectorAll('.card-list-item')
-    const cardListItem = deleteButton.closest('.card-list-item')
-
-    if (cardListItems.length === 1) {
-      cardListItem.remove()
-      const pageBody = document.querySelector('.page-body')
-      if (pageBody) {
-        pageBody.innerHTML = `
-          <div class="o-empty-message">
-            <div class="o-empty-message__icon">
-              <i class="fa-regular fa-face-sad-tear"></i>
-              <p class="o-empty-message__text">ブックマークはまだありません。</p>
-            </div>
-          </div>
-        `
-      }
-    } else {
-      cardListItem.remove()
-    }
-  } catch (error) {
-    console.warn(error)
-  }
-}
