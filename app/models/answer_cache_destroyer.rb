66cd818cdf1511087f33570b83e612815646f4a4
jun-kondo <ranmaru.oo.q@gmail.com>
2025-09-20 07:56:59 +0900

    ログ出力を追加
    - トリガーになったアクション(save, destroy)をログに含める
    - 操作対象になったQ&Aの回答のidをログに含める

--- app/models/answer_cache_destroyer.rb
+++ app/models/answer_cache_destroyer.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class AnswerCacheDestroyer
-  def call(_name, _started, _finished, _unique_id, payload)
-    _answer = payload[:answer]
+  def call(name, _started, _finished, _unique_id, payload)
+    action = name.split('.').last
+    answer = payload[:answer]
     Cache.delete_not_solved_question_count
+    Rails.logger.info "[AnswerCacheDestroyer] Cache destroyed for unsolved question count. action: #{action}, answer: #{answer.id}"
   end
 end
