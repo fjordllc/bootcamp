dd129cd282cbab6391a5626149b7c331187ef8c0
appsu-mi <natubate870@gmail.com>
2025-09-22 08:42:48 +0900

    メソッド修正　buddy_idさえあればペアワークの内容修正が出来てしまうのを修正

--- app/controllers/pair_works_controller.rb
+++ app/controllers/pair_works_controller.rb
@@ -69,7 +69,7 @@ class PairWorksController < ApplicationController
   end
 
   def set_updatable_pair_work
-    @pair_work = if PairWork.update_permission?(current_user:, matching: !!pair_work_params[:buddy_id])
+    @pair_work = if current_user.admin? || PairWork.matching_permission?(current_user, params[:pair_work])
                    PairWork.find(params[:id])
                  else
                    current_user.pair_works.find(params[:id])
