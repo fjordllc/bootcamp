62a3a04f47ebe4297b09631830a80e5f5ceea023
sharoa119 <sau.rin1109@gmail.com>
2025-09-22 10:22:23 +0900

    不要な reports インデックス関連マイグレーションを削除

--- db/migrate/20250905025850_remove_redundant_index_from_reports.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveRedundantIndexFromReports < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :reports, name: 'idx_reports_user_date'
-  end
-end
