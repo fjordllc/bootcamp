623f0b45a16399426133ec424efc564c0147725b
smallmonkeykey <sarinitakuoo@gmail.com>
2025-09-22 23:00:30 +0900

    ダッシュボードに分報タブを追加した

--- app/helpers/page_tabs/dashboard_helper.rb
+++ app/helpers/page_tabs/dashboard_helper.rb
@@ -9,6 +9,11 @@ module PageTabs
       tabs << { name: '自分の提出物', link: current_user_products_path, count: current_user.products.length }
       tabs << { name: 'ブックマーク', link: current_user_bookmarks_path, count: current_user.bookmarks.length }
       tabs << { name: 'Watch中', link: current_user_watches_path }
+      if Rails.env.in? %w[development test]
+        tabs << { name: '分報',
+                  link: "#{user_micro_reports_path(current_user, page: current_user.latest_micro_report_page)}#latest-micro-report",
+                  count: current_user.micro_reports.length }
+      end
       render PageTabsComponent.new(tabs:, active_tab:)
     end
   end
