52b9c7ba3dc41e32ed21ae799549ae83dd680804
matsuyama <aya.k.matsuyama@gmail.com>
2025-09-29 05:48:12 +0900

    retirement_createの処理をNewspaperからActiveSupport::Notificationsに移行

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -35,7 +35,7 @@ class Admin::UsersController < AdminController
     @user.diploma_file = nil if params[:user][:remove_diploma] == '1'
     if @user.update(user_params)
       destroy_subscription(@user)
-      Newspaper.publish(:retirement_create, { user: @user }) if @user.saved_change_to_retired_on?
+      ActiveSupport::Notifications.instrument('retirement.create', user: @user) if @user.saved_change_to_retired_on?
       redirect_to user_url(@user), notice: 'ユーザー情報を更新しました。'
     else
       render :edit
