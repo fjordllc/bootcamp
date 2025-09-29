8643fe4e3e8a8b63066bae94721db3d6d1c27139
sharoa119 <sau.rin1109@gmail.com>
2025-09-29 17:46:48 +0900

    self_assigned 系のラベルとボタン描画を調整
    - unconfirmed_links_label ヘルパーを更新して self_assigned_all / self_assigned_no_replied に対応
    - self_assigned タブでボタンが正しく表示されるよう修正し、他のタブと整合性を揃えた

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -7,7 +7,8 @@ module ProductsHelper
     when 'unchecked', 'unchecked_all' then '未完了の提出物を一括で開く'
     when 'unchecked_no_replied' then '未返信の提出物を一括で開く'
     when 'unassigned' then '未アサインの提出物を一括で開く'
-    when 'self_assigned' then '自分の担当の提出物を一括で開く'
+    when 'self_assigned', 'self_assigned_all' then '自分の担当の提出物を一括で開く'
+    when 'self_assigned_no_replied' then '未返信の担当提出物を一括で開く'
     else ''
     end
   end
