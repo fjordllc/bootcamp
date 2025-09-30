3d3caa504e29142a57b8a6743ac1aa013c67634c
matsuyama <aya.k.matsuyama@gmail.com>
2025-09-30 22:27:07 +0900

    training_completion_createの処理をNewspaperからActiveSupport::Notificationsに移行

--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -10,11 +10,5 @@ Rails.configuration.after_initialize do
 
   Newspaper.subscribe(:comeback_update, ComebackNotifier.new)
 
-  unfinished_data_destroyer = UnfinishedDataDestroyer.new
-  Newspaper.subscribe(:training_completion_create, unfinished_data_destroyer)
-
-  times_channel_destroyer = TimesChannelDestroyer.new
-  Newspaper.subscribe(:training_completion_create, times_channel_destroyer)
-
   Newspaper.subscribe(:came_comment_in_talk, CommentNotifierForAdmin.new)
 end
