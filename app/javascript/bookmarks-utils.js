64623c6a1c273d969b193f0a4fd70ecfcb9383b5
smallmonkeykey <sarinitakuoo@gmail.com>
2025-10-01 11:47:29 +0900

    Prettierによるbookmarks関連ファイルの整形

--- app/javascript/bookmarks-utils.js
+++ app/javascript/bookmarks-utils.js
@@ -2,7 +2,7 @@ export function toggleDeleteButton(bookMarksEditButton, bookmarkDeleteButton) {
   bookMarksEditButton.addEventListener('click', () => {
     const displayStyle = bookMarksEditButton.checked ? 'block' : 'none'
     for (const button of bookmarkDeleteButton) {
-    button.style.display = displayStyle
-  }
+      button.style.display = displayStyle
+    }
   })
 }
