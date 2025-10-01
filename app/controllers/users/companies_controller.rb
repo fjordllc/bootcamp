a72a42cb4fcc911b3ab5d73e85c2e2d33e99db89
karlley <karlley.jp@gmail.com>
2025-10-02 08:56:08 +0900

    N+1対策を追加

--- app/controllers/users/companies_controller.rb
+++ app/controllers/users/companies_controller.rb
@@ -6,6 +6,9 @@ class Users::CompaniesController < ApplicationController
   def index
     @target = ALLOWED_TARGETS.include?(params[:target]) ? params[:target] : ALLOWED_TARGETS.first
     @allowed_targets = ALLOWED_TARGETS
-    @companies = Company.with_attached_logo.order(:created_at).reverse_order
+    @companies = Company.with_attached_logo.order(created_at: :desc)
+    company_ids = @companies.map(&:id)
+    filtered_users = User.with_attached_avatar.where(company_id: company_ids).users_role(@target, allowed_targets: @allowed_targets).order(:id)
+    @company_users = filtered_users.group_by(&:company_id)
   end
 end
