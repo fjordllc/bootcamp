a470dcb588a32e94c9bb3f4d4069d2c96bafa983
Miya096jp <miyawaki.yske@gmail.com>
2025-10-02 13:18:54 +0900

    キャッシュクリアのデバッグのため、Rails.loggerを追加

--- app/models/answer_cache_destroyer.rb
+++ app/models/answer_cache_destroyer.rb
@@ -4,5 +4,6 @@ class AnswerCacheDestroyer
   def call(payload)
     _answer = payload[:answer]
     Cache.delete_not_solved_question_count
+    Rails.logger.info '[AnswerCacheDestroyer] Cache destroyed for unsolved question count.'
   end
 end
