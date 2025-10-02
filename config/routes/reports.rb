d9e852bb4da80a7069a0476d2780f6d6a09aa6b5
Masaki Komagata <komagata@gmail.com>
2025-10-02 15:25:29 +0900

    未チェックの日報を一括で開くボタンを復活させた

--- config/routes/reports.rb
+++ config/routes/reports.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :reports do
-    resources :checks, only: [:create, :destroy]
-  end
   namespace :reports do
     resources :unchecked, only: %i[index]
+  end
+  resources :reports do
+    resources :checks, only: [:create, :destroy]
   end
 end
