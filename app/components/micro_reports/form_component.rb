ab7ccd581e164dd3c9572e02b4c9f9b1e2941ec9
smallmonkeykey <sarinitakuoo@gmail.com>
2025-10-07 12:07:32 +0900

    呼び出し元によってフォームの送信先を正しく変えるための変更とテストを追加

--- app/components/micro_reports/form_component.rb
+++ app/components/micro_reports/form_component.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 class MicroReports::FormComponent < ViewComponent::Base
-  def initialize(user:)
+  def initialize(user:, controller_name: nil)
     @user = user
+    @controller_name = controller_name
     @micro_report = user.micro_reports.build
   end
 
   def form_action_path
-    case controller.class.name
+    case @controller_name
     when 'Users::MicroReportsController'
       helpers.user_micro_reports_path(@user, anchor: 'latest-micro-report')
     when 'CurrentUser::MicroReportsController'
       helpers.current_user_micro_reports_path(anchor: 'latest-micro-report')
     else
-      raise "Unsupported controller: #{controller.class.name}"
+      raise "Unsupported controller: #{@controller_name}"
     end
   end
 end
