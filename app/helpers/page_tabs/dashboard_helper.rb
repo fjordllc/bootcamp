d08b914761051878fe1d617444156d506f0a78f3
smallmonkeykey <sarinitakuoo@gmail.com>
2025-10-12 17:26:07 +0900

    ダッシュボードの分報の位置を移動し名前を変更

--- app/helpers/page_tabs/dashboard_helper.rb
+++ app/helpers/page_tabs/dashboard_helper.rb
@@ -5,15 +5,15 @@ module PageTabs
     def dashboard_page_tabs(active_tab:)
       tabs = []
       tabs << { name: 'ダッシュボード', link: '/' }
+      if Rails.env.in? %w[development test]
+        tabs << { name: '自分の分報',
+                  link: "#{current_user_micro_reports_path(page: current_user.latest_micro_report_page)}#latest-micro-report",
+                  count: current_user.micro_reports.length }
+      end
       tabs << { name: '自分の日報', link: current_user_reports_path, count: current_user.reports.length }
       tabs << { name: '自分の提出物', link: current_user_products_path, count: current_user.products.length }
       tabs << { name: 'ブックマーク', link: current_user_bookmarks_path, count: current_user.bookmarks.length }
       tabs << { name: 'Watch中', link: current_user_watches_path }
-      if Rails.env.in? %w[development test]
-        tabs << { name: '分報',
-                  link: "#{current_user_micro_reports_path(page: current_user.latest_micro_report_page)}#latest-micro-report",
-                  count: current_user.micro_reports.length }
-      end
       render PageTabsComponent.new(tabs:, active_tab:)
     end
   end
