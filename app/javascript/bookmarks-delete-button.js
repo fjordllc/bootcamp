b281facb7d75c76b5fc871cf0dff70c292af0308
smallmonkeykey <sarinitakuoo@gmail.com>
2025-10-14 19:47:31 +0900

    削除後の動作を修正 - ページネーションが正しいURLになるように修正 - 2ページ目以降、そのページの最後のブックマークだったときは前のページに移動するように修正

--- app/javascript/bookmarks-delete-button.js
+++ app/javascript/bookmarks-delete-button.js
@@ -2,31 +2,36 @@ import { destroy } from '@rails/request.js'
 import { toggleDeleteButton } from './bookmarks-utils'
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.addEventListener('click', async (event) => {
+
+  document.body.addEventListener('click', async (event) => {
     const deleteButton = event.target.closest('.bookmark-delete-button')
     if (!deleteButton) return
 
-    event.preventDefault()
-
-    const url = deleteButton.dataset.url
+    deleteButton.disabled = true
 
     try {
-      const response = await destroy(url, {
-        responseKind: 'html',
-        headers: { Accept: 'text/html' }
-      })
+      const url = deleteButton.dataset.url
+      const response = await destroy(url)
 
       if (!response.ok) {
         throw new Error(`削除に失敗しました。(ステータス: ${response.status})`)
       }
 
-      const html = await response.text
-      document.querySelector('.page-main').outerHTML = html
+      const params = new URLSearchParams(location.search)
+      const currentPage = parseInt(params.get('page') || '1', 10)
+      const newPageMain = await fetchPageMain(currentPage)
+
+      let pageToShow = newPageMain
+      if (currentPage > 1 && newPageMain.querySelector('.o-empty-message')) {
+          pageToShow = await fetchPageMain(currentPage - 1)
+        }
+      document.querySelector('.page-main').replaceWith(pageToShow)
 
       const bookMarksEditButton = document.getElementById('bookmark_edit')
       const bookmarkDeleteButton = document.getElementsByClassName(
         'js-bookmark-delete-button'
       )
+
       if (bookMarksEditButton && bookmarkDeleteButton.length > 0) {
         bookMarksEditButton.checked = true
         toggleDeleteButton(bookMarksEditButton, bookmarkDeleteButton)
@@ -36,3 +41,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 })
+
+const fetchPageMain = async (page) => {
+  const bookmarkUrl = `/current_user/bookmarks?page=${page}`
+  const html = await fetch(bookmarkUrl).then((res) => res.text())
+  const parser = new DOMParser()
+  const parsedDocument = parser.parseFromString(html, 'text/html')
+  return parsedDocument.querySelector('.page-main')
+}
