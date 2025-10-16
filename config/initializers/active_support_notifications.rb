f8acc8ebe36ee2fa47381f5aaa2ca37c7e6962a6
appsu-mi <natubate870@gmail.com>
2025-10-16 10:02:51 +0900

    重複を避けるため変数を定義

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -71,9 +71,12 @@ Rails.application.reloader.to_prepare do
   ActiveSupport::Notifications.subscribe('student_or_trainee.create', TimesChannelCreator.new)
   ActiveSupport::Notifications.subscribe('user.create', SignUpNotifier.new)
   ActiveSupport::Notifications.subscribe('regular_event.update', RegularEventUpdateNotifier.new)
-  ActiveSupport::Notifications.subscribe('pair_work.create', WatchForPairWorkCreator.new)
-  ActiveSupport::Notifications.subscribe('pair_work.update', WatchForPairWorkCreator.new)
-  ActiveSupport::Notifications.subscribe('pair_work.create', PairWorkNotifier.new)
-  ActiveSupport::Notifications.subscribe('pair_work.update', PairWorkNotifier.new)
+
+  watch_for_pair_work_creator = WatchForPairWorkCreator.new
+  ActiveSupport::Notifications.subscribe('pair_work.create', watch_for_pair_work_creator)
+  ActiveSupport::Notifications.subscribe('pair_work.update', watch_for_pair_work_creator)
+  pair_work_notifier = PairWorkNotifier.new
+  ActiveSupport::Notifications.subscribe('pair_work.create', pair_work_notifier)
+  ActiveSupport::Notifications.subscribe('pair_work.update', pair_work_notifier)
   ActiveSupport::Notifications.subscribe('pair_work.update', PairWorkMatchingNotifier.new)
 end
