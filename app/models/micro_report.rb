42cdbf2e7b0d8fc4f16084136c9eacdac72e3c60
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-10-17 22:45:14 +0900

    テストが落ちていたので修正した
    テストデータにcomment_userを追加
    comment_userをデフォルトにセットした

--- app/models/micro_report.rb
+++ app/models/micro_report.rb
@@ -6,4 +6,12 @@ class MicroReport < ApplicationRecord
   belongs_to :user
   belongs_to :comment_user, class_name: 'User'
   validates :content, presence: true
+
+  before_validation :set_default_comment_user, on: :create
+
+  private
+
+  def set_default_comment_user
+    self.comment_user ||= user
+  end
 end
