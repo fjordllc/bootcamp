798c9f310039ab8f26908ae50b125a38b2f5aae2
matsuyama <aya.k.matsuyama@gmail.com>
2025-10-17 19:25:39 +0900

    他のユーザーのデータに影響なくユーザーの日報と提出物が正しく削除されるテストを追加

--- test/models/unfinished_data_destroyer_test.rb
+++ test/models/unfinished_data_destroyer_test.rb
@@ -3,17 +3,52 @@
 require 'test_helper'
 
 class UnfinishedDataDestroyerTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:kimura)
+    @reports = 3.times.map do |i|
+      Report.create!(
+        user: @user,
+        title: "wipの日報#{i}",
+        description: 'テスト日報',
+        wip: true, reported_on: Date.current - i
+      )
+    end
+    @payload = { user: @user }
+  end
   test '#call' do
-    user = users(:sotugyou)
-    user.update(job: 'office_worker', career_path: 1)
+    @user.update(career_path: 1)
+    product = products(:product5)
+    report = @reports.first
 
-    wip_report = reports(:report9)
-    unchecked_product = products(:product6)
-    payload = { user: }
+    UnfinishedDataDestroyer.new.call(nil, nil, nil, nil, @payload)
+    assert_not Report.exists?(report.id)
+    assert_not Product.exists?(product.id)
+    assert_equal 'unset', @user.reload.career_path
+  end
 
-    UnfinishedDataDestroyer.new.call(nil, nil, nil, nil, payload)
-    assert_not Report.exists?(wip_report.id)
-    assert_not Product.exists?(unchecked_product.id)
-    assert_equal 'unset', user.reload.career_path
+  test 'deletes all wip reports and unchecked products for user but other users remain unaffected' do
+    user_unchecked_products = [products(:product5), products(:product8), products(:product10)]
+    user_wip_reports = @reports
+
+    other_unchecked_product = products(:product1)
+    other_wip_report = reports(:report9)
+
+    UnfinishedDataDestroyer.new.call(nil, nil, nil, nil, @payload)
+
+    assert_not Product.where(id: user_unchecked_products).exists?
+    assert_not Report.where(id: user_wip_reports).exists?
+
+    assert Product.exists?(other_unchecked_product.id)
+    assert Report.exists?(other_wip_report.id)
+  end
+
+  test 'does not delete checked product or unwip report' do
+    product = products(:product2)
+    report = reports(:report24)
+
+    UnfinishedDataDestroyer.new.call(nil, nil, nil, nil, @payload)
+
+    assert Product.exists?(product.id)
+    assert Report.exists?(report.id)
   end
 end
