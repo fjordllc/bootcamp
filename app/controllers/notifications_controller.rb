8ec339cfe15327f19eb9c8db7c5d513c1f4e6b53
smallmonkeykey <sarinitakuoo@gmail.com>
2025-10-18 19:27:11 +0900

    railsviewとJSで書き換えた
    未読通知ボタンの動作をStimulusからJS実装にした
    
    stimulusの実装の部分は不要になったので削除
    
    一括で開く時は、元のタブの通知を削除
    
    インデントの修正とフィルターボタンの重複レンダリングを改善
    
    フィルターボタンが消えていたので反映されるようにした
    
    コードスタイルを修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,6 +5,19 @@ class NotificationsController < ApplicationController
 
   def index
     @target = params[:target]
+    target = params[:target].presence&.to_sym
+    status = params[:status]
+
+    latest_notifications = current_user.notifications
+                                       .by_target(target)
+                                       .by_read_status(status)
+                                       .latest_of_each_link
+
+    @notifications = Notification.with_avatar
+                                 .from(latest_notifications, :notifications)
+                                 .order(created_at: :desc)
+                                 .page(params[:page])
+                                 .per(20)
   end
 
   def show
