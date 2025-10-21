b60d992be34928a9dd8ce5068f73043ce89a6553
smallmonkeykey <sarinitakuoo@gmail.com>
2025-10-21 21:08:22 +0900

    `textarea.js`の読み込みを削除し、画像が2重挿入される不具合を修正 `textarea.js`は他で使ってなかったため削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 import 'controllers'
-import '../textarea.js'
 import '../markdown.js'
 import '../shortcut.js'
 import '../learning.js'
