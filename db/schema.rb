5541036f2c106d20fdf3e8f54a21a0474a0d0fae
Miya096jp <miyawaki.yske@gmail.com>
2025-10-22 17:39:06 +0900

    urlカラムにuniqueインデックスを追加
    fixtures/buzzes.ymlからurl重複を除去
    BuzzHelperで生成するtest用のfixtureのurlが重複しないように修正
    BuzzHelperをBuzzTest(models/buzz_test.rb)にinclude
    coderabbitの指摘に対応し、test/system/buzzes_test.rbにテストを追加
    (buzzes published on same day use id as tie breaker in descending order)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_11_162031) do
+ActiveRecord::Schema.define(version: 2025_10_22_075147) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -117,6 +118,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_11_162031) do
     t.string "title", null: false
     t.date "published_at", null: false
     t.text "memo"
+    t.index ["url"], name: "index_buzzes_on_url", unique: true
   end
 
   create_table "campaigns", force: :cascade do |t|
