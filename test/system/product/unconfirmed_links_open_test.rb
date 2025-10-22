7465544f78e686e9edfc6a74b62450b2a194406e
sharoa119 <sau.rin1109@gmail.com>
2025-10-22 20:54:23 +0900

    UnconfirmedLinksOpenTest の整理 - 重複していたテストを削除 - それに伴いsetupなど修正

--- test/system/product/unconfirmed_links_open_test.rb
+++ test/system/product/unconfirmed_links_open_test.rb
@@ -5,44 +5,17 @@ require 'application_system_test_case'
 class Product::UnconfirmedLinksOpenTest < ApplicationSystemTestCase
   setup do
     @mentor = users(:komagata)
-    @student1 = users(:hajime)
-    @student2 = users(:hatsuno)
-
+    @student1 = users(:hatsuno)
     @practice1 = practices(:practice9)
-    @practice2 = practices(:practice10)
 
-    Notification.stub(:create!, nil) do
-      Watch.stub(:create!, nil) do
-        # 自分が担当かつ未返信提出物
-        @unchecked_no_replied_product = Product.create!(
-          user: @student2,
-          practice: @practice1,
-          body: '未返信提出物',
-          checker_id: @mentor.id,
-          published_at: 1.day.ago
-        )
-
-        # 未アサイン提出物
-        @unassigned_product = Product.create!(
-          user: @student1,
-          practice: @practice1,
-          body: '未アサイン提出物',
-          checker_id: nil,
-          published_at: 1.day.ago
-        )
-
-        # 自分が担当の提出物（返信済み）
-        @self_assigned_product = Product.create!(
-          user: @student1,
-          practice: @practice2,
-          body: '自分が担当している提出物',
-          checker_id: @mentor.id,
-          published_at: 1.day.ago,
-          commented_at: 2.days.ago,
-          mentor_last_commented_at: 2.days.ago
-        )
-      end
-    end
+    # 自分が担当かつ未返信提出物
+    @self_assigned_no_replied_product = Product.create!(
+      user: @student1,
+      practice: @practice1,
+      body: '未返信提出物',
+      checker_id: @mentor.id,
+      published_at: 1.day.ago
+    )
   end
 
   # 「全ての提出物を一括で開く」ボタンの表示確認（それ以外のボタンはそれぞれのコントローラテストで確認済み）
@@ -55,19 +28,6 @@ class Product::UnconfirmedLinksOpenTest < ApplicationSystemTestCase
   test 'mentor sees unchecked_no_replied products links' do
     visit_with_auth '/products/unchecked?target=unchecked_no_replied', 'komagata'
 
-    assert_selector "a.js-unconfirmed-link[href$='#{@unchecked_no_replied_product.id}']", count: 1, wait: 5
-  end
-
-  test 'mentor sees unassigned products links' do
-    visit_with_auth '/products/unassigned', 'komagata'
-
-    assert_selector "a.js-unconfirmed-link[href$='#{@unassigned_product.id}']", count: 1, wait: 5
-  end
-
-  test 'mentor sees self_assigned products links' do
-    visit_with_auth '/products/self_assigned', 'komagata'
-
-    assert_selector "a.js-unconfirmed-link[href$='#{@self_assigned_product.id}']", count: 1
-    assert_selector "a.js-unconfirmed-link[href$='#{@unchecked_no_replied_product.id}']", count: 1
+    assert_selector "a.js-unconfirmed-link[href$='#{@self_assigned_no_replied_product.id}']", count: 1, wait: 5
   end
 end
