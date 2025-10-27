9aa36704b7fdbcce6dc614be0c9f62c8e8023e52
Masaki Komagata <komagata@gmail.com>
2025-10-27 15:22:24 +0900

    update

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,7 +28,7 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
   # Setup for each parallel process
-  parallelize_setup do |worker|
+  parallelize_setup do |_worker|
     ActiveStorage::Current.url_options = {
       protocol: 'http', host: 'localhost', port: '3000'
     }
