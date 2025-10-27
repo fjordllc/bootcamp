385ed38b55a7a922930064925f351aef05a4ff1c
tyrrell-IH <gotothertm@gmail.com>
2025-10-27 10:48:22 +0900

    メンションをつけた分報を保存、通知が届くところまで実装

--- app/models/micro_report.rb
+++ app/models/micro_report.rb
@@ -2,7 +2,15 @@
 
 class MicroReport < ApplicationRecord
   include Reactionable
+  include Mentioner
 
   belongs_to :user
   validates :content, presence: true
+
+  mentionable_as :content
+  alias sender user
+
+  def path
+    Rails.application.routes.url_helpers.user_micro_reports_path(user)
+  end
 end
