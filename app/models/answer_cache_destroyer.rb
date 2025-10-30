1d91b8c50afeccad7738a7cb32b24d95401609fe
jun-kondo <ranmaru.oo.q@gmail.com>
2025-10-30 14:32:42 +0900

    操作対象になったQ&Aの回答のidをログのメッセージに追加

--- app/models/answer_cache_destroyer.rb
+++ app/models/answer_cache_destroyer.rb
@@ -2,9 +2,9 @@
 
 class AnswerCacheDestroyer
   def call(_name, _started, _finished, _unique_id, payload)
-    _answer = payload[:answer]
+    answer = payload[:answer]
     action = payload[:action]
     Cache.delete_not_solved_question_count
-    Rails.logger.info "[AnswerCacheDestroyer] #{action} Cache destroyed for unsolved question count."
+    Rails.logger.info "[AnswerCacheDestroyer] #{action} Cache destroyed for unsolved question count. modified_answer_id: #{answer.id}"
   end
 end
