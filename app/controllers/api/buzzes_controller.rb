400c31ce22e57ad838af99a0a770f76547539b23
Miya096jp <miyawaki.yske@gmail.com>
2025-11-01 14:50:11 +0900

    coderabbitの指摘に対応
    クローズした#9283の指摘を受けてエラーハンドリングを追加
    StrongParameterにurlを含めるように変更

--- app/controllers/api/buzzes_controller.rb
+++ app/controllers/api/buzzes_controller.rb
@@ -5,6 +5,8 @@ class API::BuzzesController < API::BaseController
   skip_before_action :verify_authenticity_token
 
   def check
+    return render json: { error: 'URLパラメータが必要です' }, status: :bad_request if params[:url].blank?
+
     if Buzz.find_by(url: params[:url])
       render json: { exists: true }
     else
@@ -13,14 +15,20 @@ class API::BuzzesController < API::BaseController
   end
 
   def create
-    @buzz = Buzz.find_or_initialize_by(url: params[:buzz][:url])
+    return render json: { error: 'パラメータが不正です' }, status: :bad_request unless params[:buzz].present? && params[:buzz][:url].present?
+
+    @buzz = Buzz.find_or_initialize_by(url: buzz_params[:url])
     if @buzz.new_record?
       @buzz.assign_attributes(buzz_params)
-      @buzz.save
-      render json: @buzz, status: :created
-    else
-      @buzz.update(buzz_params)
+      if @buzz.save
+        render json: @buzz, status: :created
+      else
+        render json: { errors: @buzz.errors.full_messages }, status: :unprocessable_entity
+      end
+    elsif @buzz.update(buzz_params)
       render json: @buzz, status: :ok
+    else
+      render json: { errors: @buzz.errors.full_message }, status: :unprocessable_entity
     end
   end
 
@@ -33,6 +41,6 @@ class API::BuzzesController < API::BaseController
   end
 
   def buzz_params
-    params.require(:buzz).permit(:title, :published_at, :memo)
+    params.require(:buzz).permit(:title, :url, :published_at, :memo)
   end
 end
