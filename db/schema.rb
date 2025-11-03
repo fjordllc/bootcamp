3a49195b40ff143a8562c213919ae8ef89861065
nakamu-kazu222 <nakamiraiwan1922@gmail.com>
2025-11-03 14:55:47 +0900

    マイグレーションを最新まで適用した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_02_152627) do
+ActiveRecord::Schema.define(version: 2025_11_03_053813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
