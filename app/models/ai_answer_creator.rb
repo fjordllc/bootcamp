3268c3d80fd06a7a60ca67bb12d627b588419ad2
machida <machida@fjord.jp>
2025-11-10 20:31:05 +0900

    Rails 7.2対応: AiAnswerCreatorクラス名をZeitwerkルールに準拠
    ファイル名ai_answer_creator.rbに対して、クラス名を
    AIAnswerCreator → AiAnswerCreator に変更してZeitwerkの
    命名規則に準拠させる

--- app/models/ai_answer_creator.rb
+++ app/models/ai_answer_creator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AIAnswerCreator
+class AiAnswerCreator
   def call(_name, _started, _finished, _unique_id, payload)
     question = payload[:question]
     question.update(ai_answer: '')
