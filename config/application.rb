3835e64a3f1770006771a1bd0449afa61c7fd0ad
machida <machida@fjord.jp>
2025-11-10 20:30:11 +0900

    Rails 7.2対応: secretsメソッドを環境変数ベースに移行
    Rails 7.2でRails.application.secretsが削除されたため、
    後方互換性のためにsecretsメソッドを追加し、環境変数から
    設定を読み込むように変更。
    
    変更内容:
    - application.rbにsecretsメソッドを追加
    - 環境変数からstripe/webhook/open_aiの設定を読み込む
    - stripe.rbを環境変数使用に更新

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Bootcamp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,23 +23,27 @@ module Bootcamp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.time_zone = "Tokyo"
-    config.i18n.default_locale = :ja
 
-    config.paths.add "lib", eager_load: true
-    config.paths.add "app/presenters", eager_load: true
-
-    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
-      html_tag.html_safe
-    end
-
-    config.active_storage.resolve_model_to_route = :rails_storage_proxy
-    config.active_storage.variant_processor = :vips
-
-    config.view_component.capture_compatibility_patch_enabled = true
-
-    config.to_prepare do
-      Doorkeeper::AuthorizationsController.layout "authorization"
+    # Rails 7.2対応: secrets メソッドの後方互換性を提供
+    def secrets
+      @secrets ||= {
+        secret_key_base: Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE'],
+        stripe: {
+          secret_key: ENV['STRIPE_SECRET_KEY'],
+          public_key: ENV['STRIPE_PUBLIC_KEY'],
+          endpoint_secret: ENV['STRIPE_ENDPOINT_SECRET'],
+          tax_rate_id: ENV['STRIPE_TAX_RATE_ID']
+        },
+        webhook: {
+          admin: ENV['WEBHOOK_ADMIN_URL'],
+          all: ENV['WEBHOOK_ALL_URL'],
+          mentor: ENV['WEBHOOK_MENTOR_URL'],
+          introduction: ENV['WEBHOOK_INTRODUCTION_URL']
+        },
+        open_ai: {
+          access_token: ENV['OPENAI_ACCESS_TOKEN']
+        }
+      }.with_indifferent_access
     end
   end
 end
