ae259815c3bc2a97df3ee2ac84c8d2e8b87aa2c4
machida <machida@fjord.jp>
2025-11-10 20:31:28 +0900

    Rails 7.2対応: rails app:updateで生成されたファイルを追加
    - 新しいデフォルト設定ファイル (new_framework_defaults_7_2.rb)
    - 更新されたbin/スクリプト
    - 更新された設定ファイル (application, boot, environments, initializers)
    - 更新されたpublic/エラーページとアイコン
    - 新しい406エラーページ

--- config/application.rb
+++ config/application.rb
@@ -23,27 +23,5 @@ module Bootcamp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Rails 7.2対応: secrets メソッドの後方互換性を提供
-    def secrets
-      @secrets ||= {
-        secret_key_base: Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE'],
-        stripe: {
-          secret_key: ENV['STRIPE_SECRET_KEY'],
-          public_key: ENV['STRIPE_PUBLIC_KEY'],
-          endpoint_secret: ENV['STRIPE_ENDPOINT_SECRET'],
-          tax_rate_id: ENV['STRIPE_TAX_RATE_ID']
-        },
-        webhook: {
-          admin: ENV['WEBHOOK_ADMIN_URL'],
-          all: ENV['WEBHOOK_ALL_URL'],
-          mentor: ENV['WEBHOOK_MENTOR_URL'],
-          introduction: ENV['WEBHOOK_INTRODUCTION_URL']
-        },
-        open_ai: {
-          access_token: ENV['OPENAI_ACCESS_TOKEN']
-        }
-      }.with_indifferent_access
-    end
   end
 end
