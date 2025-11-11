0b91970369cbfa6e42121b3db175e1b54fda86b6
machida <machida@fjord.jp>
2025-11-11 21:07:50 +0900

    AIAnswerCreateJobからAiAnswerCreateJobへの定数名変更に対応
    ジョブクラス名がAiAnswerCreateJobに変更されたため、
    呼び出し元の定数参照を更新:
    - app/models/ai_answer_creator.rb
    - test/jobs/ai_answer_create_job_test.rb

--- app/models/ai_answer_creator.rb
+++ app/models/ai_answer_creator.rb
@@ -4,6 +4,6 @@ class AiAnswerCreator
   def call(_name, _started, _finished, _unique_id, payload)
     question = payload[:question]
     question.update(ai_answer: '')
-    AIAnswerCreateJob.perform_later(question_id: question.id)
+    AiAnswerCreateJob.perform_later(question_id: question.id)
   end
 end
--- test/jobs/ai_answer_create_job_test.rb
+++ test/jobs/ai_answer_create_job_test.rb
@@ -2,12 +2,12 @@
 
 require 'test_helper'
 
-class AIAnswerCreateJobTest < ActiveJob::TestCase
+class AiAnswerCreateJobTest < ActiveJob::TestCase
   test '#perform' do
     question = questions(:question1)
 
     VCR.use_cassette 'question/ai_answer_create_job' do
-      AIAnswerCreateJob.perform_now(question_id: question.id)
+      AiAnswerCreateJob.perform_now(question_id: question.id)
 
       assert_equal 'テストの解答です。', question.reload.ai_answer
     end
