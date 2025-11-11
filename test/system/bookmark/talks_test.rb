be428764de8275b92547a975676aac4cac4629bb
smallmonkeykey <sarinitakuoo@gmail.com>
2025-11-12 00:39:56 +0900

    1ページめ2ページめにあってもどちらでも対応できるように変更

--- test/system/bookmark/talks_test.rb
+++ test/system/bookmark/talks_test.rb
@@ -11,7 +11,8 @@ class Bookmark::TalkTest < ApplicationSystemTestCase
 
   test 'show talk bookmark on lists' do
     visit_with_auth '/current_user/bookmarks', 'komagata'
-    click_link '2'
+    find_all('a.pagination__item-link', text: '2').first.click if !page.has_text?("#{@decorated_user.long_name} さんの相談部屋")
+
     assert_text "#{@decorated_user.long_name} さんの相談部屋"
   end
 
