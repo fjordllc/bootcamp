5f54de95ccd74e8931bb45c8efc9741f3c11b496
smallmonkeykey <sarinitakuoo@gmail.com>
2025-11-11 20:40:28 +0900

    条件付きを削除し、元のコードに戻した

--- test/system/bookmark/talks_test.rb
+++ test/system/bookmark/talks_test.rb
@@ -11,7 +11,7 @@ class Bookmark::TalkTest < ApplicationSystemTestCase
 
   test 'show talk bookmark on lists' do
     visit_with_auth '/current_user/bookmarks', 'komagata'
-    assert_text "#{@decorated_user.long_name} さんの相談部屋" if page.has_text?(@decorated_user.long_name)
+    assert_text "#{@decorated_user.long_name} さんの相談部屋"
 
     assert_selector 'a.pagination__item-link', text: '2'
     first('a.pagination__item-link', text: '2').click
