591b3af73dac3ba898e7f9365d2f0ae1c3b4d636
machida <machida@fjord.jp>
2025-11-15 14:36:57 +0900

    Rails 7.2対応: システムテストでのリダイレクト修正
    Capybara.server_hostを127.0.0.1に設定し、リダイレクト時のERR_CONNECTION_REFUSEDエラーを修正。

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -29,10 +29,15 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include ProductHelper
   include AvatarHelper
 
+  # Rails 7.2: Configure Capybara to use 127.0.0.1 for system tests
+  # This ensures redirects work properly
+  Capybara.server_host = '127.0.0.1'
+  Capybara.server_port = 3000
+
   if ENV['HEADFUL']
-    driven_by :selenium, using: :chrome
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   else
-    driven_by(:selenium, using: :headless_chrome) do |driver_option|
+    driven_by(:selenium, using: :headless_chrome, screen_size: [1400, 1400]) do |driver_option|
       driver_option.add_argument('--no-sandbox')
       driver_option.add_argument('--disable-dev-shm-usage')
       driver_option.add_argument('enable-blink-features=Clipboard')
