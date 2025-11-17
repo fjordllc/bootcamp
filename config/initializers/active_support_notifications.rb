61ae4a7afa9e169a75e43b64542830e5c84bbcff
appsu-mi <natubate870@gmail.com>
2025-11-17 09:46:54 +0900

    既存の実装と合わせ1行開ける

--- config/initializers/active_support_notifications.rb
+++ config/initializers/active_support_notifications.rb
@@ -75,8 +75,10 @@ Rails.application.reloader.to_prepare do
   watch_for_pair_work_creator = WatchForPairWorkCreator.new
   ActiveSupport::Notifications.subscribe('pair_work.create', watch_for_pair_work_creator)
   ActiveSupport::Notifications.subscribe('pair_work.update', watch_for_pair_work_creator)
+
   pair_work_notifier = PairWorkNotifier.new
   ActiveSupport::Notifications.subscribe('pair_work.create', pair_work_notifier)
   ActiveSupport::Notifications.subscribe('pair_work.update', pair_work_notifier)
+
   ActiveSupport::Notifications.subscribe('pair_work.update', PairWorkMatchingNotifier.new)
 end
