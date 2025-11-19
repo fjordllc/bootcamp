64f973f4facf81895da57079d4664b6d379984de
Miya096jp <miyawaki.yske@gmail.com>
2025-11-19 16:46:28 +0900

    不要なマイグレーションファイルを削除

--- db/migrate/20250831044340_add_unique_index_to_buzzes_url_and_title.rb
+++
@@ -1,6 +0,0 @@
-class AddUniqueIndexToBuzzesUrlAndTitle < ActiveRecord::Migration[6.1]
-  def change
-    add_index :buzzes, :url, unique: true
-    add_index :buzzes, :title, unique: true
-  end
-end
--- db/migrate/20250831235134_remove_unique_index_from_buzzes_url.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUniqueIndexFromBuzzesUrl < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :buzzes, :url
-  end
-end
