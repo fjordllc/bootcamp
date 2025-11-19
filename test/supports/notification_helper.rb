84597ba91b31ebaacd08a1e5540080449d384ec4
Masaki Komagata <komagata@gmail.com>
2025-11-19 09:05:22 +0900

    テストの信頼性向上のためテストヘルパーを追加・改善

--- test/supports/notification_helper.rb
+++ test/supports/notification_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module NotificationHelper
+  def notifications(user:, target:, status:)
+    UserNotificationsQuery.new(user:, target:, status:).call
+  end
+
   def notification_message
     first('.test-notification-message').text
   end
@@ -9,19 +13,13 @@ module NotificationHelper
     all('.test-notification-message').map(&:text)
   end
 
-  # TODO: このモジュール以外では使用禁止。いつかなくしたい
-  # 本来であればsleepは使いたくないが、テストコードがロジカルすぎてすぐに修正できないため、やむを得ず残したままにする
-  def wait_for_vuejs_再利用禁止 # rubocop:disable Naming/MethodName, Naming/AsciiIdentifiers
-    sleep 2
-  end
-
   # notification_messages.include?(text)
   # でも可能だが、notification_messagesは
   # open_notificationを実行した(右上のベルボタンを押した)かで
   # 戻り値が変更されるため、これを作成
   def exists_unread_notification?(message)
     visit notifications_path(status: 'unread')
-    wait_for_vuejs_再利用禁止 # 通知一覧はVueでREST APIを利用して表示しているため # rubocop:disable Naming/AsciiIdentifiers
+    wait_for_notifications_loaded
     exists = page.has_selector?('span.card-list-item-title__link-label',
                                 text: message)
     go_back
@@ -30,8 +28,35 @@ module NotificationHelper
 
   def link_to_page_by_unread_notification(message)
     visit notifications_path(status: 'unread')
-    wait_for_vuejs_再利用禁止 # 通知一覧はVueでREST APIを利用して表示しているため # rubocop:disable Naming/AsciiIdentifiers
+    wait_for_notifications_loaded
     click_link message, class: 'card-list-item-title__link'
+  end
+
+  def user_has_notification?(user:, kind:, text: nil, unread: false, sender: nil)
+    notifications = Notification.where(user:, kind:)
+    notifications = notifications.unreads if unread
+    notifications = notifications.where(sender:) if sender
+    return notifications.any? unless text
+
+    notifications.any? { |n| n.message.include?(text) }
+  end
+
+  def assert_user_has_notification(user:, kind:, text: nil, unread: false, sender: nil)
+    assert user_has_notification?(user:, kind:, text:, unread:, sender:),
+           "#{user.login_name} should have notification with text: #{text}"
+  end
+
+  def assert_user_has_no_notification(user:, kind:, text: nil, unread: false, sender: nil)
+    assert_not user_has_notification?(user:, kind:, text:, unread:, sender:),
+               "#{user.login_name} should not have notification with text: #{text}"
+  end
+
+  private
+
+  def wait_for_notifications_loaded
+    using_wait_time 20 do
+      assert_no_selector '#notifications.loading'
+    end
   end
 
   def make_write_report_notification_message(user_login_name, report_title)
