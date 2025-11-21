9300b4f8df61e0b404afe418b589947117111df2
Masaki Komagata <komagata@gmail.com>
2025-11-21 16:37:38 +0900

    開発環境のasset_host設定を削除

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_controller.asset_host = "http://localhost:3035"
-  config.action_mailer.asset_host = "http://localhost:3035"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
