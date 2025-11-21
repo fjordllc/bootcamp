b3e1a79e4d79a65f8a24a5c15dc9856ecf20f766
ryufuta <rfutago@gmail.com>
2025-11-21 17:47:14 +0900

    WIPのQ&Aを自動クローズ機能の対象外にした

--- test/models/question_auto_closer_test.rb
+++ test/models/question_auto_closer_test.rb
@@ -60,4 +60,48 @@ class QuestionAutoCloserTest < ActiveSupport::TestCase
     assert_equal system_user, correct_answer.user
     assert_includes correct_answer.description, '自動的にクローズしました'
   end
+
+  test 'does not post warning for WIP questions' do
+    created_at = Time.zone.local(2025, 10, 1, 0, 0, 0)
+    question = Question.create!(
+      title: 'WIPの質問',
+      description: 'テスト',
+      user: users(:kimura),
+      wip: true,
+      created_at:,
+      updated_at: created_at
+    )
+
+    travel_to created_at.advance(months: 1) do
+      assert_no_difference -> { question.answers.count } do
+        QuestionAutoCloser.post_warning
+      end
+    end
+  end
+
+  test 'does not close WIP questions' do
+    created_at = Time.zone.local(2025, 10, 1, 0, 0, 0)
+    question = Question.create!(
+      title: '公開から1ヶ月後WIPにする質問',
+      description: 'テスト',
+      user: users(:kimura),
+      created_at:,
+      updated_at: created_at
+    )
+
+    warned_at = created_at.advance(months: 1)
+    question.answers.create!(
+      user: users(:pjord),
+      description: 'このQ&Aは1ヶ月間コメントがありませんでした。1週間後に自動的にクローズされます。',
+      created_at: warned_at,
+      updated_at: warned_at
+    )
+    question.update!(wip: true, updated_at: warned_at)
+
+    travel_to warned_at.advance(weeks: 1) do
+      assert_no_difference -> { question.answers.count } do
+        QuestionAutoCloser.close_and_select_best_answer
+      end
+    end
+  end
 end
