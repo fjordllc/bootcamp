31ce91cafc92a8b20bbcfb134678e787a9a395c5
ryufuta <rfutago@gmail.com>
2025-11-23 23:42:34 +0900

    テストコード内のシステムメッセージを定数化

--- test/models/question_auto_closer_test.rb
+++ test/models/question_auto_closer_test.rb
@@ -3,6 +3,9 @@
 require 'test_helper'
 
 class QuestionAutoCloserTest < ActiveSupport::TestCase
+  AUTO_CLOSE_WARNING_MESSAGE = 'このQ&Aは1ヶ月間コメントがありませんでした。1週間後に自動的にクローズされます。'
+  AUTO_CLOSE_MESSAGE = '自動的にクローズしました。'
+
   test '.post_warning' do
     created_at = Time.zone.local(2025, 10, 1, 0, 0, 0)
     question = Question.create!(
@@ -25,7 +28,7 @@ class QuestionAutoCloserTest < ActiveSupport::TestCase
       end
       answer = question.answers.last
       assert_equal users(:pjord), answer.user
-      assert_equal 'このQ&Aは1ヶ月間コメントがありませんでした。1週間後に自動的にクローズされます。', answer.description
+      assert_equal AUTO_CLOSE_WARNING_MESSAGE, answer.description
     end
   end
 
@@ -43,7 +46,7 @@ class QuestionAutoCloserTest < ActiveSupport::TestCase
     system_user = users(:pjord)
     question.answers.create!(
       user: system_user,
-      description: 'このQ&Aは1ヶ月間コメントがありませんでした。1週間後に自動的にクローズされます。',
+      description: AUTO_CLOSE_WARNING_MESSAGE,
       created_at: warned_at,
       updated_at: warned_at
     )
@@ -60,7 +63,7 @@ class QuestionAutoCloserTest < ActiveSupport::TestCase
       end
       answer = question.answers.last
       assert_equal system_user, answer.user
-      assert_equal '自動的にクローズしました。', answer.description
+      assert_equal AUTO_CLOSE_MESSAGE, answer.description
       assert answer.is_a?(CorrectAnswer)
     end
   end
@@ -96,7 +99,7 @@ class QuestionAutoCloserTest < ActiveSupport::TestCase
     warned_at = created_at.advance(months: 1)
     question.answers.create!(
       user: users(:pjord),
-      description: 'このQ&Aは1ヶ月間コメントがありませんでした。1週間後に自動的にクローズされます。',
+      description: AUTO_CLOSE_WARNING_MESSAGE,
       created_at: warned_at,
       updated_at: warned_at
     )
