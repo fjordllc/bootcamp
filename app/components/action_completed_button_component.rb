d934834f1858ed582a1e2b6d6331a416289bc3fe
ryufuta <rfutago@gmail.com>
2025-11-30 18:56:07 +0900

    ActionCompletedButtonComponentのメソッドをprivateにする
    `app/components/action_completed_button_component.html.slim`からはprivateでも参照可能

--- app/components/action_completed_button_component.rb
+++ app/components/action_completed_button_component.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class ActionCompletedButtonComponent < ViewComponent::Base
-  attr_reader :is_action_completed, :commentable_id
-
   def initialize(is_initial_action_completed:, commentable_id:)
     @is_action_completed = is_initial_action_completed
     @commentable_id = commentable_id
   end
+
+  private
+
+  attr_reader :is_action_completed, :commentable_id
 end
