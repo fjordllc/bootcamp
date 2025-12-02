e8165b262dbbf814a5fe805f86fe6ae6d8f32383
tyrrell-IH <gotothertm@gmail.com>
2025-12-02 12:20:00 +0900

    wait_for_javascript_componentsを使用してブックマークボタンのloading状態が消えるのを待機するよう修正

--- test/system/bookmark/regular_events_test.rb
+++ test/system/bookmark/regular_events_test.rb
@@ -9,8 +9,10 @@ class Bookmark::RegularEventTest < ApplicationSystemTestCase
 
   test 'shows regular event link on list when clicking bookmark button' do
     visit_with_auth regular_event_path(@regular_event), 'machida'
+    wait_for_javascript_components
     assert_selector '#bookmark-button', text: 'Bookmark'
     click_button 'Bookmark'
+    wait_for_javascript_components
     assert_selector '#bookmark-button', text: 'Bookmark中'
 
     visit current_user_bookmarks_path
@@ -22,16 +24,20 @@ class Bookmark::RegularEventTest < ApplicationSystemTestCase
 
   test 'updates bookmarks list when toggling bookmark' do
     visit_with_auth regular_event_path(@regular_event), 'machida'
+    wait_for_javascript_components
     assert_selector '#bookmark-button', text: 'Bookmark'
     click_button 'Bookmark'
+    wait_for_javascript_components
     assert_selector '#bookmark-button', text: 'Bookmark中'
 
     visit current_user_bookmarks_path
     assert_selector 'a.card-list-item-title__link', text: @regular_event.title
 
     visit regular_event_path(@regular_event)
+    wait_for_javascript_components
     assert_selector '#bookmark-button', text: 'Bookmark中'
     click_button 'Bookmark中'
+    wait_for_javascript_components
     assert_selector '#bookmark-button', text: 'Bookmark'
 
     visit current_user_bookmarks_path
