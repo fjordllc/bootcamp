4411be6bf44f62e4792a4991524fd66eeb41fc77
smallmonkeykey <sarinitakuoo@gmail.com>
2025-12-11 21:31:36 +0900

    NotificationsControllerのロジックを整理し、絞り込み処理をQueryObjectへ移行

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,22 +1,10 @@
 # frozen_string_literal: true
 
 class NotificationsController < ApplicationController
-  ALLOWED_TARGETS = %i[announcement mention comment check watching following_report].freeze
-  PER_PAGE = 20
-
   before_action :set_my_notification, only: %i[show]
 
   def index
     @target = params[:target]
-    target = params[:target].presence&.to_sym
-    target = nil unless ALLOWED_TARGETS.include?(target)
-    status = params[:status]
-
-    latest_notifications = current_user.notifications
-                                       .by_target(target)
-                                       .by_read_status(status)
-                                       .latest_of_each_link
-
     @notifications = UserNotificationsQuery.new(
       user: current_user,
       target: params[:target],
