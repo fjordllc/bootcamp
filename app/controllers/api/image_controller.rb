7e302045a0d001bda1d482088fc642fa720c6088
matsuyama <aya.k.matsuyama@gmail.com>
2025-12-22 06:09:25 +0900

    EXIF削除処理をコントローラーからモデルへ移動

--- app/controllers/api/image_controller.rb
+++ app/controllers/api/image_controller.rb
@@ -3,33 +3,11 @@
 class API::ImageController < API::BaseController
   def create
     @image = Image.new(user: current_user, image: params[:file])
+
     if @image.save
-      begin
-        strip_exif
-        render :create, status: :created
-      rescue StandardError => e
-        Rails.logger.error("Failed to strip EXIF: #{e.message}")
-        @image.destroy
-        render json: { error: '画像処理に失敗しました' }, status: :unprocessable_entity
-      end
+      render :create, status: :created
     else
       render json: @image.errors, status: :unprocessable_entity
     end
-  end
-
-  private
-
-  def strip_exif
-    original_image = @image.image
-    copied_image = MiniMagick::Image.read(original_image.download)
-    copied_image.strip
-
-    ext = File.extname(original_image.filename.to_s)
-    timestamp = Time.current.strftime('%Y%m%d%H%M%S%L')
-    File.open(copied_image.path) do |file|
-      original_image.attach(io: file, filename: "#{current_user.id}_#{timestamp}#{ext}")
-    end
-  ensure
-    copied_image&.destroy!
   end
 end
