9ac5f04da96ed3d69e603692b689b9fd09b15a46
yokomaru <cabochetoganlan@gmail.com>
2025-12-24 13:55:56 +0900

    定期イベントの曜日リスト定数を削除しI18n.tの曜日配列から参照するように修正した - ドロップダウンはRails7から追加されたweekday_options_for_select helperを使用した

--- app/models/regular_event.rb
+++ app/models/regular_event.rb
@@ -15,16 +15,6 @@ class RegularEvent < ApplicationRecord # rubocop:disable Metrics/ClassLength
     ['偶数週', 6]
   ].freeze
 
-  DAY_OF_THE_WEEK_LIST = [
-    ['日曜日', 0],
-    ['月曜日', 1],
-    ['火曜日', 2],
-    ['水曜日', 3],
-    ['木曜日', 4],
-    ['金曜日', 5],
-    ['土曜日', 6]
-  ].freeze
-
   include WithAvatar
   include Commentable
   include Footprintable
