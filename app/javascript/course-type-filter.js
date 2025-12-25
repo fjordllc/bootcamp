7e347c5699d016e50cc4d97b940bb6ca89949055
karlley <karlley.jp@gmail.com>
2025-12-26 06:41:31 +0900

    prettierでの修正

--- app/javascript/course-type-filter.js
+++ app/javascript/course-type-filter.js
@@ -1,12 +1,12 @@
 export default class {
   constructor(courseType, setCourseType) {
-    this.courseType = courseType;
-    this.setCourseType = setCourseType;
-    this.tabs = null;
-    this.handleSelect = null;
+    this.courseType = courseType
+    this.setCourseType = setCourseType
+    this.tabs = null
+    this.handleSelect = null
   }
 
-  render(element){
+  render(element) {
     element.innerHTML = `
     <nav class="tab-nav">
       <ul class="tab-nav__items">
@@ -20,37 +20,37 @@ export default class {
     </nav>
     `
 
-    this.tabs = element.querySelectorAll('.tab-nav__item-link');
+    this.tabs = element.querySelectorAll('.tab-nav__item-link')
 
-    this.tabs.forEach(tab => {
+    this.tabs.forEach((tab) => {
       if (tab.dataset.courseType === this.courseType) {
-        tab.classList.add('is-active');
+        tab.classList.add('is-active')
       }
     })
 
     this.handleSelect = (event) => {
-      event.preventDefault();
-      const selectedTab = event.currentTarget;
-      this.tabs.forEach(tab => {
-        tab.classList.remove('is-active');
+      event.preventDefault()
+      const selectedTab = event.currentTarget
+      this.tabs.forEach((tab) => {
+        tab.classList.remove('is-active')
       })
-      selectedTab.classList.add('is-active');
-      this.setCourseType(selectedTab.dataset.courseType);
+      selectedTab.classList.add('is-active')
+      this.setCourseType(selectedTab.dataset.courseType)
     }
 
-    this.tabs.forEach(tab => {
+    this.tabs.forEach((tab) => {
       tab.addEventListener('click', this.handleSelect)
     })
   }
 
   destroy() {
-    if (!this.tabs || !this.handleSelect) return;
+    if (!this.tabs || !this.handleSelect) return
 
-    this.tabs.forEach(tab => {
-      tab.removeEventListener('click', this.handleSelect);
+    this.tabs.forEach((tab) => {
+      tab.removeEventListener('click', this.handleSelect)
     })
 
-    this.tabs = null;
-    this.handleSelect = null;
+    this.tabs = null
+    this.handleSelect = null
   }
 }
