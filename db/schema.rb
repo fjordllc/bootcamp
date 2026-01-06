050561f35fe6db93f1393c0643e9e8a2e42db831
ryufuta <rfutago@gmail.com>
2026-01-06 15:41:24 +0900

    usersテーブルからjob_seekingカラムを削除したことをスキーマファイルに反映
    https://github.com/fjordllc/bootcamp/pull/8468/commits/fcb9370271d75aa8273a4eddb399ab00d5a5691b のコミットで反映し忘れた

--- db/schema.rb
+++ db/schema.rb
@@ -1015,7 +1015,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_17_205428) do
     t.boolean "invoice_payment", default: false, null: false
     t.integer "job"
     t.boolean "job_seeker", default: false, null: false
-    t.boolean "job_seeking", default: false, null: false
     t.datetime "last_activity_at"
     t.integer "last_negative_report_id"
     t.string "login_name", limit: 255, null: false
