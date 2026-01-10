122f110e264c4e0eff968122e6ded7f6cfa4c1f1
appsu-mi <natubate870@gmail.com>
2026-01-10 12:31:28 +0900

    preloadする関連を増やした

--- app/controllers/pair_works_controller.rb
+++ app/controllers/pair_works_controller.rb
@@ -9,7 +9,7 @@ class PairWorksController < ApplicationController
   def index
     @pair_works = PairWork.by_target(params[:target])
                           .with_avatar
-                          .includes(:practice, :comments)
+                          .includes(:practice, :comments, :user)
                           .order(:published_at)
                           .page(params[:page])
                           .per(PAGER_NUMBER)
