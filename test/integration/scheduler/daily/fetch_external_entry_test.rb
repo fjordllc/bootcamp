6e04da1a731e851d80be89374b6dd33074137fc7
tyrrell-IH <gotothertm@gmail.com>
2026-01-13 19:51:24 +0900

    Rubocop指摘事項: ステータスコードの指定を数字ではなくシンボルに変更

--- test/integration/scheduler/daily/fetch_external_entry_test.rb
+++ test/integration/scheduler/daily/fetch_external_entry_test.rb
@@ -9,16 +9,16 @@ class Scheduler::Daily::FetchExternalEntryTest < ActionDispatch::IntegrationTest
   test 'token evaluation' do
     mock_env('TOKEN' => '') do
       get scheduler_daily_fetch_external_entry_path(token: '')
-      assert_response 401
+      assert_response :unauthorized
     end
 
     mock_env('TOKEN' => 'token') do
       get scheduler_daily_fetch_external_entry_path(token: 'invalid')
-      assert_response 401
+      assert_response :unauthorized
 
       ExternalEntry.stub(:fetch_and_save_rss_feeds, nil) do
         get scheduler_daily_fetch_external_entry_path(token: 'token')
-        assert_response 200
+        assert_response :ok
       end
     end
   end
