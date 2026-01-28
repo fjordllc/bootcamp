d27d6e634cbffca678d9e2ad2cdc6c98edf681d5
machida <machida@fjord.jp>
2026-01-28 21:51:22 +0900

    フレイキーなシステムテストを修正
    - users_test: 具体的なセレクタを使用してページロード完了を確実に待機
    - learning_time_frame_test: テーブルのレンダリング完了を待機、不要なwithinとscroll_toを削除

--- test/system/current_user/learning_time_frame_test.rb
+++ test/system/current_user/learning_time_frame_test.rb
@@ -30,20 +30,11 @@ module CurrentUser
       visit_with_auth '/current_user/edit', 'kimura'
       assert_selector 'h1.auth-form__title', text: '登録情報変更'
       assert_selector 'label.a-form-label', text: '主な活動予定時間'
+      assert_selector '#learning_time_frame'
 
-      within '#learning_time_frame' do
-        label1 = find('label[for="user_learning_time_frame_ids_1"]')
-        scroll_to(label1)
-        label1.click
-
-        label25 = find('label[for="user_learning_time_frame_ids_25"]')
-        scroll_to(label25)
-        label25.click
-
-        label49 = find('label[for="user_learning_time_frame_ids_49"]')
-        scroll_to(label49)
-        label49.click
-      end
+      find('label[for="user_learning_time_frame_ids_1"]').click
+      find('label[for="user_learning_time_frame_ids_25"]').click
+      find('label[for="user_learning_time_frame_ids_49"]').click
 
       click_on '更新する'
       assert_text 'ユーザー情報を更新しました。'
