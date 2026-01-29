87f51cbbb2e7ec26b61a1c6c0808182a747f55e1
kutimiti1234 <tarbit.offensive@gmail.com>
2026-01-29 20:10:45 +0900

    PR#9306のラベル内表記に合わせる

--- app/helpers/label_helper.rb
+++ app/helpers/label_helper.rb
@@ -3,6 +3,8 @@
 module LabelHelper
   def bookmarkable_label(bookmarkable)
     case bookmarkable.model_name.name
+    when 'Event'
+      safe_join(%w[特別 イベント], tag.br)
     when 'RegularEvent'
       safe_join(%w[定期 イベント], tag.br)
     else
