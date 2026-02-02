3ba385f04b42701314d808a1940ffd103cfc4e79
yokomaru <cabochetoganlan@gmail.com>
2026-02-03 00:30:53 +0900

    主催者追加時の通知イベントの作成してテストを追加

---
+++ test/models/organizer_added_notifier_test.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class OrganizerAddedNotifierTest < ActiveSupport::TestCase
+  include ActiveJob::TestHelper
+
+  setup do
+    @regular_event = regular_events(:regular_event4)
+    @sender = users(:kimura)
+  end
+
+  test 'sends a notification when a new organizer is added' do
+    new_organizer_users = [users(:hatsuno)]
+
+    assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 1 do
+      OrganizerAddedNotifier.new.call(nil, nil, nil, nil, { regular_event: @regular_event, new_organizer_users:, sender: @sender })
+    end
+  end
+
+  test 'does not send a notification when no new organizers are added' do
+    new_organizer_users = []
+
+    assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 0 do
+      OrganizerAddedNotifier.new.call(nil, nil, nil, nil, { regular_event: @regular_event, new_organizer_users:, sender: @sender })
+    end
+  end
+
+  test 'sends notifications when multiple organizers are added' do
+    new_organizer_users = [users(:hatsuno), users(:hajime)]
+
+    assert_difference -> { AbstractNotifier::Testing::Driver.enqueued_deliveries.count }, 2 do
+      OrganizerAddedNotifier.new.call(nil, nil, nil, nil, { regular_event: @regular_event, new_organizer_users:, sender: @sender })
+    end
+  end
+end
