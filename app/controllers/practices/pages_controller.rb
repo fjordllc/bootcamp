600618b15ce0dd351939a3d4e7cf47ad2e8e85e7
kutimiti1234 <tarbit.offensive@gmail.com>
2026-02-15 22:04:44 +0900

    給付金コースのフィルターを実装
    給付金コースのフィルターはパーシャルとして再利用できるように実装

--- app/controllers/practices/pages_controller.rb
+++ app/controllers/practices/pages_controller.rb
@@ -5,11 +5,17 @@ class Practices::PagesController < ApplicationController
 
   def index
     @practice = Practice.find(params[:practice_id])
-    @pages = Page.with_avatar
-                 .includes(:comments, :practice, { last_updated_user: { avatar_attachment: :blob } })
-                 .where(practice_id: params[:practice_id])
-                 .order(updated_at: :desc, id: :desc)
-                 .page(params[:page])
-                 .per(PAGER_NUMBER)
+
+    @pages = @practice.pages
+                      .includes(:comments, :practice, { last_updated_user: { avatar_attachment: :blob } })
+
+    if @practice.grant_course? && params[:target] != 'only_grant_course'
+      @pages = @pages.or(@practice.source_practice.pages).includes(:comments, :practice,
+                                                                   { last_updated_user: { avatar_attachment: :blob } })
+    end
+
+    @pages = @pages.order(updated_at: :desc, id: :desc)
+                   .page(params[:page])
+                   .per(PAGER_NUMBER)
   end
 end
