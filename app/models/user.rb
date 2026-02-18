fc3333c4d7ecd876794b29d7fcdd1b86cb32b354
shoma <s.tone@genbasupport.com>
2026-02-18 10:35:11 +0900

    休会した回数を管理者・メンター・本人が確認できるようにした
    管理者とメンターはプロフィールページから
    本人はダッシュボードに表示されるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -678,6 +678,14 @@ class User < ApplicationRecord # rubocop:todo Metrics/ClassLength
     hibernated_at?
   end
 
+  def hasEverHibernated?
+    !hibernations.empty?
+  end
+
+  def hibernationCount
+    hibernations.count
+  end
+
   def after_twenty_nine_days_registration?
     twenty_nine_days = Time.current.ago(29.days).to_date
     created_at.to_date.before? twenty_nine_days
