e1dc86e1e3f541fe3e1cdc674109e923e912ea6a
y-kawahara-gs <y.kawahara@genbasupport.com>
2026-02-25 09:02:02 +0900

    研修生が提出物合格になった際のアドバイザーへのアプリ内通知を実装

--- app/models/check_callbacks.rb
+++ app/models/check_callbacks.rb
@@ -8,6 +8,8 @@ class CheckCallbacks
         receiver: check.receiver,
         check:
       ).notify(:checked)
+
+      notify_advisers check if check.checkable.user.trainee? && check.checkable.user.company
     end
 
     delete_report_cache(check)
@@ -33,5 +35,18 @@ class CheckCallbacks
     return unless check.checkable_type == 'Product'
 
     Cache.delete_unchecked_product_count
+  end
+
+  def notify_advisers(check)
+    send_notification(
+      check:,
+      receivers: check.checkable.user.company.advisers
+    )
+  end
+
+  def send_notification(check:, receivers:)
+    receivers.each do |receiver|
+      ActivityDelivery.with(check:, receiver:).notify(:checked)
+    end
   end
 end
