ea6fdfcb683b09751b2dd92a74b15307581533c6
komagata <komagata@gmail.com>
2026-02-27 15:31:37 +0900

    AI検索のFeatureフラグを削除して全ユーザーに公開
    - ApplicationHelperのsmart_search_available?メソッドを削除
    - 検索フォームのFeatureフラグによる条件分岐を削除
    - 検索方法（キーワード/意味検索/ハイブリッド）の選択が全ユーザーに表示されるようになる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,6 @@ module ApplicationHelper
     [:everyone, current_user.job].include?(practice.target)
   end
 
-  # development/test環境では常に有効、それ以外はフィーチャーフラグで制御
-  def smart_search_available?
-    Rails.env.local? || Switchlet.enabled?(:smart_search)
-  end
-
   def movie_available?
     Rails.env.local? || Switchlet.enabled?(:movie)
   end
