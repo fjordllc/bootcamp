25a715f453cec4a8552bf0fd30d8f8531fbf725a
Miya096jp <miyawaki.yske@gmail.com>
2026-02-27 16:26:03 +0900

    google拡張のcors設定許可についてコメントを追記

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    # 紹介・言及記事を収集するためのChrome拡張です。
     origins "chrome-extension://ddobgcfepfdpccmgnpfgjcomccpkcdjj"
     resource "/api/*",
       headers: :any,
