0309dc3a857be2a163b965d85f36f3c3a8441cd2
komagata <komagata@gmail.com>
2026-02-28 12:56:48 +0900

    ActiveStorageエラー時にデフォルトアイコンにフォールバックするrescue追加

--- app/controllers/user_icons_controller.rb
+++ app/controllers/user_icons_controller.rb
@@ -9,7 +9,13 @@ class UserIconsController < ApplicationController
     user = User.find_by(login_name: params[:login_name])
 
     if user&.avatar&.attached?
-      avatar_url = url_for(user.avatar.variant(resize_to_limit: [56, 56]))
+      begin
+        avatar_url = url_for(user.avatar.variant(resize_to_limit: [56, 56]))
+      rescue ActiveStorage::FileNotFoundError, ActiveStorage::Error => e
+        logger.error "[UserIcons] Avatar error for user #{user.login_name} (id=#{user.id}): #{e.class} - #{e.message}"
+        expires_in 1.hour, public: true
+        return redirect_to DEFAULT_ICON_PATH
+      end
       expires_in 1.hour, public: true
       redirect_to avatar_url, allow_other_host: true
     else
