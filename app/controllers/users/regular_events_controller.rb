4ffef9a034b1cbd8c9e20016da8901a17847bc5c
y-kawahara-gs <y.kawahara@genbasupport.com>
2026-03-04 15:34:18 +0900

    N+1問題対策の実装、整合性を保った処理に修正

--- app/controllers/users/regular_events_controller.rb
+++ app/controllers/users/regular_events_controller.rb
@@ -4,7 +4,8 @@ class Users::RegularEventsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @regular_events = @user.participate_regular_events
-                           .includes(:comments, :users)
+                           .includes(:comments, :user,
+                                     { users: { avatar_attachment: :blob } })
                            .order(start_at: :desc)
                            .page(params[:page])
   end
