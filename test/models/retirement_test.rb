5978c4bb4ced5aae0fe93daa0c9097b47ca36200
yokomaru <cabochetoganlan@gmail.com>
2026-03-05 18:15:58 +0900

    退会/休会/研修終了の定期イベント離脱処理を修正した
    - 参加・主催ともに離脱するのは開催中(holding)のイベントのみとした
    - 参加・主催の離脱処理が2つに分かれていたが、現状どの動線からも必ず2つ一緒に呼ばれているため、`clean_up_regular_events`としてまとめて呼び出し元も修正した
        - 以下メソッドは不要になったのでテストも含めて削除そた
            - cancel_active_regular_event_participations
            - leave_or_close_active_organized_regular_events
            - delete_and_assign_new
            - assign_admin_as_organizer_if_none
    - RegularEventに主催者の数により処理を分岐するメソッドを追加した
        - close_or_destroy_organizer
            - 離脱時に主催が1人の場合はkomagataに付け替えるではなくイベントを終了させる
            - 仕様の変更に伴い影響するシステムテストの修正を行った

--- test/models/retirement_test.rb
+++ test/models/retirement_test.rb
@@ -16,8 +16,7 @@ class RetirementTest < ActiveSupport::TestCase
       clear_hibernation_state
       save_user
       destroy_subscription
-      cancel_event_subscription
-      remove_as_event_organizer
+      clean_up_regular_events
       clear_github_info
       destroy_cards
       publish
