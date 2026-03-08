e1d6069876fd71ff26af08ff3a7e69e44a46b676
karlley <karlley.jp@gmail.com>
2026-03-09 05:51:21 +0900

    日報一覧を給付金コースのスコープで絞り込むように修正

--- app/controllers/practices/reports_controller.rb
+++ app/controllers/practices/reports_controller.rb
@@ -3,6 +3,8 @@
 class Practices::ReportsController < ApplicationController
   def index
     @practice = Practice.find(params[:practice_id])
-    @reports = @practice.reports.list.page(params[:page])
+    @reports = Report.list.page(params[:page])
+    @reports = @reports.with_practice_and_source(params[:practice_id]) if params[:practice_id].present?
+    @reports = @reports.without_original_practice if params[:practice_id].present? && params[:grant_only] == 'true'
   end
 end
