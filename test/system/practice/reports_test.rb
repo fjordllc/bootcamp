bd60d99d7fa7011c3029acb67ba2cce5a12420c7
karlley <karlley.jp@gmail.com>
2026-03-10 07:04:33 +0900

    日報一覧の給付金コースフィルターのsystemテストを追加

--- test/system/practice/reports_test.rb
+++ test/system/practice/reports_test.rb
@@ -9,4 +9,48 @@ class Practice::ReportsTest < ApplicationSystemTestCase
     assert_selector 'img[alt="positive"]', count: 2
     assert_selector '.card-list-item-title__link', text: '1時間だけ学習'
   end
+
+  test 'grant filter is not present on rails course practice' do
+    visit_with_auth "/practices/#{practices(:practice1).id}/reports", 'komagata'
+
+    assert_no_selector 'a.pill-nav__item-link', text: '全て'
+    assert_no_selector 'a.pill-nav__item-link', text: '給付金コース'
+  end
+
+  test 'grant filter is present on grant course practice' do
+    visit_with_auth "/practices/#{practices(:practice64).id}/reports", 'komagata'
+
+    assert_selector 'a.pill-nav__item-link', text: '全て'
+    assert_selector 'a.pill-nav__item-link', text: '給付金コース'
+  end
+
+  test 'grant filter shows both copied and grant course reports when "全て" is selected' do
+    visit_with_auth "/practices/#{practices(:practice64).id}/reports", 'komagata'
+
+    assert_selector 'a.pill-nav__item-link.is-active', text: '全て'
+    assert_selector '.card-list-item-title__link', text: 'コピー元のプラクティスの日報'
+    assert_selector '.card-list-item-title__link', text: '給付金コースのプラクティスの日報'
+  end
+
+  test 'grant filter shows only grant course reports when "給付金コース" is selected' do
+    visit_with_auth "/practices/#{practices(:practice64).id}/reports", 'komagata'
+    within '.pill-nav' do
+      click_on '給付金コース'
+    end
+
+    assert_selector 'a.pill-nav__item-link.is-active', text: '給付金コース'
+    assert_no_selector '.card-list-item-title__link', text: 'コピー元のプラクティスの日報'
+    assert_selector '.card-list-item-title__link', text: '給付金コースのプラクティスの日報'
+  end
+
+  test 'grant filter shows empty message when no reports exist' do
+    visit_with_auth "/practices/#{practices(:practice65).id}/reports", 'komagata'
+    within '.pill-nav' do
+      click_on '給付金コース'
+    end
+
+    assert_selector 'a.pill-nav__item-link.is-active', text: '給付金コース'
+    assert_no_selector '.card-list-item-title__link'
+    assert_selector '.o-empty-message__text', text: '日報はまだありません。'
+  end
 end
