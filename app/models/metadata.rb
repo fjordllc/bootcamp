118d94f6f30cda8af61adbc9d4eea6367b7f7571
appsu-mi <natubate870@gmail.com>
2026-03-10 12:55:47 +0900

    OGP, 各プロパティが見つからなくてもエラーにならないよう修正

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -15,14 +15,16 @@ class Metadata
 
   def parse(html)
     object = OpenGraphReader.parse(html)
+    return nil unless object
+
     metadata_keys = %i[site_name site_url favicon url title description images]
     metadata_keys.map do |metadata_key|
       content = case metadata_key
                 when :site_url then site_url
                 when :favicon then favicon(site_url, html)
-                when :images then object.og.image.url
+                when :images then object.og.image&.url || ''
                 when :site_name then object.og.site_name || @uri.host
-                else object.og.public_send(metadata_key)
+                else object.og.public_send(metadata_key) || ''
                 end
       [metadata_key, content]
     end.to_h
