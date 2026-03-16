7536793d9a2264076b818f984c05c5c12f272623
y-kawahara-gs <y.kawahara@genbasupport.com>
2026-03-16 14:04:53 +0900

    N+1問題対策としてuser/regular_eventのincludeにregular_event_repeat_rulesを追加

--- app/controllers/users/regular_events_controller.rb
+++ app/controllers/users/regular_events_controller.rb
@@ -4,7 +4,7 @@ class Users::RegularEventsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @regular_events = @user.participate_regular_events
-                           .includes(:comments, :user,
+                           .includes(:comments, :regular_event_repeat_rules, :user,
                                      { users: { avatar_attachment: :blob } })
                            .order(start_at: :desc)
                            .page(params[:page])
