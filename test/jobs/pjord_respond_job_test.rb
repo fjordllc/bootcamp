b02c19e23a3e579d7978df900826a878bafab971
Masaki Komagata <komagata@gmail.com>
2026-03-29 16:03:21 +0900

    Fix RuboCop offenses: add rubocop:disable for update_column and fix string quotes

--- test/jobs/pjord_respond_job_test.rb
+++ test/jobs/pjord_respond_job_test.rb
@@ -10,7 +10,9 @@ class PjordRespondJobTest < ActiveJob::TestCase
   test 'creates a comment reply when mentioned in a report comment' do
     comment = comments(:comment1)
     # after_commitコールバックの副作用を避けるため、DBを直接更新
-    comment.update_column(:description, "@pjord CSSについて教えて")
+    # rubocop:disable Rails/SkipsModelValidations
+    comment.update_column(:description, '@pjord CSSについて教えて')
+    # rubocop:enable Rails/SkipsModelValidations
 
     Pjord.stub(:respond, 'テストの回答です。') do
       assert_difference 'Comment.count', 1 do
@@ -30,7 +32,9 @@ class PjordRespondJobTest < ActiveJob::TestCase
   test 'creates an answer reply when mentioned in a question' do
     question = questions(:question1)
     # after_commitコールバックの副作用を避けるため、DBを直接更新
-    question.update_column(:description, "@pjord エディターについて教えて")
+    # rubocop:disable Rails/SkipsModelValidations
+    question.update_column(:description, '@pjord エディターについて教えて')
+    # rubocop:enable Rails/SkipsModelValidations
 
     Pjord.stub(:respond, 'ヒントです。') do
       assert_difference 'Answer.count', 1 do
@@ -48,7 +52,9 @@ class PjordRespondJobTest < ActiveJob::TestCase
 
   test 'does nothing when response is blank' do
     comment = comments(:comment1)
+    # rubocop:disable Rails/SkipsModelValidations
     comment.update_column(:description, '@pjord テスト')
+    # rubocop:enable Rails/SkipsModelValidations
 
     Pjord.stub(:respond, nil) do
       assert_no_difference 'Comment.count' do
@@ -75,7 +81,9 @@ class PjordRespondJobTest < ActiveJob::TestCase
   test 'does nothing when mention is removed by edit' do
     comment = comments(:comment1)
     # メンションが含まれていないdescriptionをDBに直接セット
+    # rubocop:disable Rails/SkipsModelValidations
     comment.update_column(:description, 'メンション削除済み')
+    # rubocop:enable Rails/SkipsModelValidations
 
     Pjord.stub(:respond, '回答') do
       assert_no_difference 'Comment.count' do
