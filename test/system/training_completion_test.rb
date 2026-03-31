d436fe36bfcff300e5764ab597e5f16703230fe7
zamami <s1413115@gmail.com>
2026-03-31 18:54:36 +0900

    ・テストをassert_tesxtからwhithinに変更 ・テスト名を日本語に修正 ・rebase mainで不要に追加してしまったテストを削除

--- test/system/training_completion_test.rb
+++ test/system/training_completion_test.rb
@@ -81,55 +81,16 @@ class TrainingCompletionTest < ApplicationSystemTestCase
     assert_text '研修終了情報（非公開）'
   end
 
-  test 'removes user from regular event upon training completion' do
-    regular_event = regular_events(:regular_event1)
-    visit_with_auth regular_event_path(regular_event), 'kensyu'
-    accept_confirm do
-      click_link '参加申込'
-    end
-    assert_text '参加登録しました。'
-
-    visit new_training_completion_path
-    find('label', text: 'とても良い').click
-    page.accept_confirm '本当によろしいですか？' do
-      click_on '研修を終了する'
-    end
-    assert_text '研修終了手続きが完了しました'
-    assert_equal Time.current, @user.reload.training_completed_at
-
-    visit_with_auth "regular_events/#{regular_event.id}", 'komagata'
-    within('.card-body') do
-      assert_no_selector '.is-kensyu'
-    end
-  end
-
-  test 'clears github data when user completes training' do
-    @user.github_id = '12345'
-    @user.github_account = 'github_kensyu'
-    @user.github_collaborator = true
-    @user.save!(validate: false)
-
-    visit_with_auth new_training_completion_path, 'kensyu'
-    find('label', text: 'とても良い').click
-    page.accept_confirm '本当によろしいですか？' do
-      click_on '研修を終了する'
-    end
-    assert_text '研修終了手続きが完了しました'
-
-    @user.reload
-    assert_equal Time.current, @user.training_completed_at
-
-    assert_nil @user.github_id
-    assert_nil @user.github_account
-    assert_not @user.github_collaborator
-  end
-
-  test 'shows not entered when satisfaction is nil' do
+  test 'shows 未入力 when satisfaction is nil' do
     user = users(:kensyuowata)
     user.update!(satisfaction: nil)
 
     visit_with_auth "/users/#{user.id}", 'komagata'
-    assert_text '研修終了情報（非公開）'
-    assert_text '未入力'
+
+    within('.user-metas.is-only-mentor', text: '研修終了情報（非公開）') do
+      within('.user-metas__item', text: '選択入力の研修感想') do
+        assert_text '未入力'
+      end
+    end
   end
 end
