9e97e7dd7511838908a5930d725bbef37dd65903
Miya096jp <miyawaki.yske@gmail.com>
2026-04-11 09:14:37 +0900

    親クラスと重複するコールバックを削除

--- app/controllers/api/buzzes_controller.rb
+++ app/controllers/api/buzzes_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class API::BuzzesController < API::BaseController
-  before_action :require_login_for_api
   before_action :require_admin_or_mentor_login_for_api
   skip_before_action :verify_authenticity_token
 
