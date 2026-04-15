538bfbc225a9d502ae231c2c40975bf2907c6759
shoma <s.tone@genbasupport.com>
2026-04-15 18:48:39 +0900

    通知メソッドの命名を修正
    変更前のバディに通知を送ることが伝わらない命名になっていたため修正

--- app/models/pair_work_rematching_notifier.rb
+++ app/models/pair_work_rematching_notifier.rb
@@ -6,12 +6,12 @@ class PairWorkRematchingNotifier
     past_buddy = payload[:past_buddy]
     return if pair_work.wip?
 
-    notify_pair_work_creator(pair_work, past_buddy)
+    notify_pair_work_creator_and_past_buddy(pair_work, past_buddy)
   end
 
   private
 
-  def notify_pair_work_creator(pair_work, past_buddy)
+  def notify_pair_work_creator_and_past_buddy(pair_work, past_buddy)
     [pair_work.user, past_buddy].compact.each do |receiver|
       ActivityDelivery.with(pair_work:, receiver:).notify(:rematching_pair_work)
     end
