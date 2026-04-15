972aa504cd12df7a5252ebf133702c6f5cc60480
yokomaru <cabochetoganlan@gmail.com>
2026-04-15 18:25:28 +0900

    マイグレーションファイルから不要なオプションや条件分岐を削除

--- db/migrate/20260415062046_remove_index_user_id_from_regular_event_organizers.rb
+++ db/migrate/20260415062046_remove_index_user_id_from_regular_event_organizers.rb
@@ -1,5 +1,5 @@
 class RemoveIndexUserIdFromRegularEventOrganizers < ActiveRecord::Migration[8.1]
   def change
-    remove_index :regular_event_organizers, column: :user_id, name: "index_regular_event_organizers_on_user_id", if_exists: true
+    remove_index :regular_event_organizers, :user_id, name: "index_regular_event_organizers_on_user_id"
   end
 end
