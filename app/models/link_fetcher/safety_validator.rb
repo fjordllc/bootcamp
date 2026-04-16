f69fe3a75ac9c281e6a927c1fa5ad21fd21e1f2b
appsu-mi <natubate870@gmail.com>
2026-04-16 09:34:29 +0900

    URLのセキュリティチェックを追加

---
+++ app/models/link_fetcher/safety_validator.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module LinkFetcher
+  module SafetyValidator
+    BLOCKED_IP_RANGES = [
+      # ループバック（SSRFで内部サービスに到達可能なためブロック）
+      IPAddr.new('127.0.0.0/8'),
+      IPAddr.new('::1/128'),
+
+      # プライベートネットワーク（SSRFで社内/VPCリソースに到達可能なためブロック）
+      IPAddr.new('10.0.0.0/8'),
+      IPAddr.new('172.16.0.0/12'),
+      IPAddr.new('192.168.0.0/16'),
+
+      # リンクローカル（メタデータAPI等に到達可能なためブロック）
+      IPAddr.new('169.254.0.0/16'),
+      IPAddr.new('fe80::/10'),
+
+      # IPv6 ユニークローカルアドレス（内部ネットワークへのアクセスを防ぐためブロック）
+      IPAddr.new('fc00::/7'),
+
+      # 旧site-local（廃止済みだが内部ネットワーク扱いのため念の為ブロック）
+      IPAddr.new('fec0::/10'),
+
+      # 無効・未指定アドレス（接続先として無効、且つ挙動が不安定のためブロック）
+      IPAddr.new('0.0.0.0/32'),
+      IPAddr.new('::/128'),
+
+      # CGNAT（外部から到達すべきでない領域のためブロック）
+      IPAddr.new('100.64.0.0/10'),
+
+      # IPv4-mapped IPv6（IPv4アドレスをIPv6で表現してのフィルタ回避を防ぐためブロック）
+      IPAddr.new('::ffff:0:0/96')
+    ].freeze
+
+    module_function
+
+    def valid?(uri)
+      return false unless valid_http_uri?(uri)
+
+      ips = Resolv.getaddresses(uri.host)
+      ips.all? { |ip| safe_ip?(IPAddr.new(ip)) }
+    end
+
+    def valid_http_uri?(uri)
+      port = uri.port || uri.inferred_port
+
+      port.in?([80, 443]) && uri.scheme.downcase.in?(%w[http https])
+    end
+
+    def safe_ip?(ip_addr)
+      BLOCKED_IP_RANGES.none? { |ip_range| ip_range.include?(ip_addr) }
+    end
+  end
+end
