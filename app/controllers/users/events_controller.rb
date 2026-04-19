97bb8f6395443739becdddf4aa35410e78feb82e
koguchi.e <koguchi.e915@gmail.com>
2026-04-19 16:48:46 +0900

    ユーザーのイベント一覧に主催イベントも表示されるよう修正

--- app/controllers/users/events_controller.rb
+++ app/controllers/users/events_controller.rb
@@ -3,7 +3,8 @@
 class Users::EventsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @events = @user.participate_events
+    @events = Event.where(id: @user.participate_events.select(:id))
+                   .or(Event.where(user_id: current_user.id))
                    .includes(:comments, :users)
                    .order(start_at: :desc)
                    .page(params[:page])
