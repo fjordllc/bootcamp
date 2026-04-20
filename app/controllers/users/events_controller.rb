7d7a1894a0c1f3d6b1680f03ecfb1facda38cf7a
koguchi-e <koguchi.e915@gmail.com>
2026-04-20 11:07:36 +0900

    ユーザーのイベント一覧ページのため、current_userではなくuserに変更

--- app/controllers/users/events_controller.rb
+++ app/controllers/users/events_controller.rb
@@ -4,7 +4,7 @@ class Users::EventsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @events = Event.where(id: @user.participate_events.select(:id))
-                   .or(Event.where(user_id: current_user.id))
+                   .or(Event.where(user_id: @user.id))
                    .includes(:comments, :users)
                    .order(start_at: :desc)
                    .page(params[:page])
