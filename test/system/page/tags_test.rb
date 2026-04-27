4aeec2ca2e2b1d496606024786f589b3a7fa23e9
Masaki Komagata <komagata@gmail.com>
2026-04-27 18:08:05 +0900

    Replace slow system tests with lower-level coverage

--- test/system/page/tags_test.rb
+++ test/system/page/tags_test.rb
@@ -13,15 +13,6 @@ class Page::TagsTest < ApplicationSystemTestCase
     end
   end
 
-  test 'update tags without page transitions' do
-    visit_with_auth "/pages/#{pages(:page1).id}", 'kimura'
-    find('.tag-links__item-edit').click
-    fill_in_tag '追加タグ'
-    click_on '保存'
-    assert_equal Page.tagged_with('追加タグ').pluck(:title),
-                 all('.card-list-item-title__link').map(&:text)
-  end
-
   test 'admin can edit tag' do
     tag = acts_as_taggable_on_tags('game')
     assert_admin_can_edit_tag(pages_tag_path(tag.name, all: 'true'))
@@ -30,32 +21,6 @@ class Page::TagsTest < ApplicationSystemTestCase
   test 'users except admin cannot edit tag' do
     tag = acts_as_taggable_on_tags('game')
     assert_non_admin_cannot_edit_tag(pages_tag_path(tag.name, all: 'true'))
-  end
-
-  test 'update tag with not existing tag' do
-    tag = acts_as_taggable_on_tags('beginner')
-    update_tag_text = '上級者'
-
-    visit_with_auth pages_tag_path(tag.name, all: 'true'), 'komagata'
-    click_button 'タグ名変更'
-    fill_in('tag[name]', with: update_tag_text)
-    click_button '変更'
-    assert_text 'タグ 「上級者」'
-
-    visit_with_auth questions_tag_path(tag.name, all: 'true'), 'komagata'
-    assert_text 'Q&Aはありません。'
-    visit_with_auth questions_tag_path(update_tag_text, all: 'true'), 'komagata'
-    assert_text "タグ「#{update_tag_text}」のQ&A（1）"
-
-    visit_with_auth users_tag_path(tag.name), 'komagata'
-    assert_text "#{tag.name}のユーザーはいません"
-    visit_with_auth users_tag_path(update_tag_text), 'komagata'
-    assert_text "タグ「#{update_tag_text}」のユーザー（1）"
-
-    visit_with_auth pages_tag_path(tag.name), 'komagata'
-    has_selector?('card-list-item')
-    visit_with_auth pages_tag_path(update_tag_text), 'komagata'
-    has_selector?('card-list-item')
   end
 
   test 'update tag with existing tag' do
