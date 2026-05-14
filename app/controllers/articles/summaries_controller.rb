d9f69a0a1bda571aec85f1d32e9d0e6821b184f7
zecky1120 <takahide1120@gmail.com>
2026-05-14 22:02:43 +0900

    メンターでしか開けないようにbefore_actionで認証実装

--- app/controllers/articles/summaries_controller.rb
+++ app/controllers/articles/summaries_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Articles::SummariesController < ApplicationController
+  before_action :require_admin_or_mentor_login, only: %i[create]
+
   def create
     render json: Article.agent_summary(params[:body])
   end
