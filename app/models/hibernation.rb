65ea891328acdf3cb9c355d358a44b3eeced3a5b
koguchi-e <koguchi.e915@gmail.com>
2026-05-15 11:21:58 +0900

    休会の処理をHibernationモデルにまとめる

--- app/models/hibernation.rb
+++ app/models/hibernation.rb
@@ -4,4 +4,35 @@ class Hibernation < ApplicationRecord
   belongs_to :user
   validates :reason, presence: true
   validates :scheduled_return_on, presence: true
+
+  def excute
+    update_hibernated_at!
+    destroy_subscription!
+    notify_to_chat
+    notify_to_mentors_and_admins
+    user.clean_up_regular_events
+  end
+
+  private
+
+  def update_hibernated_at!
+    user.hibernated_at = created_at
+    user.save!(validate: false)
+  end
+
+  def destroy_subscription!
+    return nil if !Rails.env.production? || staging?
+
+    Subscription.new.destroy(user.subscription_id) if user.subscription_id
+  end
+
+  def notify_to_mentors_and_admins
+    User.admins_and_mentors.each do |admin_or_mentor|
+      ActivityDelivery.with(sender: user, receiver: admin_or_mentor).notify(:hibernated)
+    end
+  end
+
+  def notify_to_chat
+    DiscordNotifier.with(sender: user).hibernated.notify_now
+  end
 end
