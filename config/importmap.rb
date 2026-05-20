844628b6c56fcf00dc480d85bb1b50d6674e36bc
Masaki Komagata <komagata@gmail.com>
2026-05-20 23:36:05 +0900

    importmapに小さなJS依存をpinする

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,6 @@ pin "sortablejs", to: "importmap-sortablejs.js" # @1.15.0
 pin "sweetalert2", to: "importmap-sweetalert2.js" # @11.1.5
 pin "choices.js", to: "importmap-choices.js" # @10.1.0
 pin "dayjs", to: "importmap-dayjs.js" # @1.10.4
+pin "escape-html", to: "importmap-escape-html.js" # @1.0.3
+pin "escape-string-regexp", to: "importmap-escape-string-regexp.js" # @2.0.0
+pin "hotkeys-js", to: "importmap-hotkeys-js.js" # @3.3.5
---
+++ vendor/javascript/importmap-escape-string-regexp.js
@@ -0,0 +1,4 @@
+// escape-string-regexp@2.0.0 downloaded from https://ga.jspm.io/npm:escape-string-regexp@2.0.0/index.js
+
+var r={};const e=/[|\\{}()[\]^$+*?.-]/g;r=r=>{if("string"!==typeof r)throw new TypeError("Expected a string");return r.replace(e,"\\$&")};var t=r;export default t;
+
