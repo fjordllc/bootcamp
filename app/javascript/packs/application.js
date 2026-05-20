e7602bc2a06187ce402268b6ce59217aa0390d90
Masaki Komagata <komagata@gmail.com>
2026-05-20 22:01:43 +0900

    Turboをimportmapで読み込む

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-import '@hotwired/turbo-rails'
 import '../controllers/index.js'
 import '../textarea.js'
 import '../markdown.js'
