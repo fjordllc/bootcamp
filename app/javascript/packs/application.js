4aecec27db249a9424c76cdc49d6192e9f01bff6
Masaki Komagata <komagata@gmail.com>
2026-05-27 16:46:44 +0900

    review appでFont Awesomeを表示できるようにする

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 /* eslint no-console:0 */
+import '@fortawesome/fontawesome-free/js/all'
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
