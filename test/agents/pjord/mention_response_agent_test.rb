73dd19801138a7a5707d256b06b5206c4659b1fc
Masaki Komagata <komagata@gmail.com>
2026-06-01 00:21:11 +0900

    ピヨルドの日報コメントとメンション返信で外部リンク確認を促す

--- test/agents/pjord/mention_response_agent_test.rb
+++ test/agents/pjord/mention_response_agent_test.rb
@@ -15,22 +15,32 @@ class Pjord::MentionResponseAgentTest < ActiveSupport::TestCase
     assert_includes chat.instructions, 'あなたはFJORD BOOT CAMP'
     assert_includes chat.instructions, 'メンション返信の指示'
     assert_includes chat.instructions, comment.sender.login_name
+    assert_includes chat.instructions, 'external_content_toolを使って内容を確認してから返信してください。'
+    assert_includes chat.instructions, 'GitHubのPR、ファイル、ディレクトリ、rawファイルへのURLが含まれる場合'
     assert_includes chat.asked_message, comment.description
+    assert_equal [BootcampSearchTool, UserInfoTool, ExternalContentTool], chat.tools
+    assert_equal PjordResponse, chat.schema
   end
 
   class AgentChatFake
-    attr_reader :asked_message, :instructions
+    attr_reader :asked_message, :instructions, :schema, :tools
+
+    def initialize
+      @tools = []
+    end
 
     def with_instructions(instructions)
       @instructions = instructions
       self
     end
 
-    def with_tools(*)
+    def with_tools(*tools)
+      @tools = tools
       self
     end
 
-    def with_schema(*)
+    def with_schema(schema)
+      @schema = schema
       self
     end
 
