48c394395364950b7ca96728abf517cc5c471a90
matsuyama <aya.k.matsuyama@gmail.com>
2026-06-03 05:05:12 +0900

    メンターメモにメモの編集機能を実装

--- app/javascript/initializeMentorMemo.js
+++ app/javascript/initializeMentorMemo.js
@@ -1,16 +1,108 @@
+import CSRF from 'csrf'
 import TextareaInitializer from 'textarea-initializer'
 import MarkdownInitializer from 'markdown-initializer'
 
-export default function initializeMemo(memo) {
+export default function initializeMemo(memo, userId) {
   const markdownInitializer = new MarkdownInitializer()
 
   const memoId = memo.dataset.memo_id
   const memoBody = memo.dataset.memo_body
-  TextareaInitializer.initialize(`#js-mentor-memo-${memoId}`)
+  let savedMemo = memoBody || ''
+  TextareaInitializer.initialize(`#js-memo-${memoId}`)
 
+  const memoDisplay = memo.querySelector('.memo-display')
   const memoDisplayContent = memo.querySelector('.memo-text')
 
+  const memoEditor = memo.querySelector('.memo-editor')
+  const editorTextarea = memo.querySelector('.a-markdown-input__textarea')
+  const editorPreview = memo.querySelector('.a-markdown-input__preview')
+
   if (memoBody) {
-    memoDisplayContent.innerHTML = markdownInitializer.render(memoBody)
+    memoDisplayContent.innerHTML = markdownInitializer.render(savedMemo)
+    editorPreview.innerHTML = markdownInitializer.render(savedMemo)
+  }
+
+  const editButton = memo.querySelector('.js-edit-memo')
+  if (editButton) {
+    editButton.addEventListener('click', () => {
+      toggleEditor()
+    })
+  }
+
+  const saveButton = memo.querySelector('.js-save-memo')
+  editorTextarea.addEventListener('input', () => {
+    saveButton.disabled = editorTextarea.value.length === 0
+  })
+
+  saveButton.addEventListener('click', async () => {
+    try {
+      const editedMemo = editorTextarea.value
+      await updateMemo(memoId, editedMemo, userId)
+      savedMemo = editedMemo
+      toggleEditor()
+      memoDisplayContent.innerHTML = markdownInitializer.render(savedMemo)
+    } catch (error) {
+      console.warn(error)
+    }
+  })
+
+  const cancelButton = memo.querySelector('.js-cancel-memo')
+  cancelButton.addEventListener('click', () => {
+    toggleEditor()
+    editorTextarea.value = savedMemo
+    editorPreview.innerHTML = markdownInitializer.render(savedMemo)
+  })
+
+  const editorTab = memoEditor.querySelector('.editor-tab')
+  const editorTabContent = memoEditor.querySelector('.is-editor')
+  const previewTab = memoEditor.querySelector('.preview-tab')
+  const previewTabContent = memoEditor.querySelector('.is-preview')
+
+  const tabElements = [
+    editorTab,
+    editorTabContent,
+    previewTab,
+    previewTabContent
+  ]
+  editorTab.addEventListener('click', () =>
+    toggleClass(tabElements, 'is-active')
+  )
+  previewTab.addEventListener('click', () => {
+    editorPreview.innerHTML = markdownInitializer.render(editorTextarea.value)
+    toggleClass(tabElements, 'is-active')
+  })
+
+  async function updateMemo(memoId, memoContent, userId) {
+    const params = {
+      mentor_memo: {
+        body: memoContent
+      }
+    }
+    const response = await fetch(
+      `/api/users/${userId}/mentor_memos/${memoId}`,
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': CSRF.getToken()
+        },
+        credentials: 'same-origin',
+        redirect: 'manual',
+        body: JSON.stringify(params)
+      }
+    )
+
+    if (!response.ok) throw new Error('Failed to update')
+  }
+
+  function toggleEditor() {
+    toggleClass([memoDisplay, memoEditor], 'is-hidden')
+  }
+
+  function toggleClass(elements, className) {
+    elements.forEach((element) => {
+      element.classList.toggle(className)
+    })
   }
 }
