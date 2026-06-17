64921e3b967f0b3d347d524bbd2a97cd29aa946f
Masaki Komagata <komagata@gmail.com>
2026-06-17 13:55:27 +0900

    未使用のPrism言語依存を削除

--- vendor/javascript/prismjs--components--prism-tsx.js
+++
@@ -1,4 +0,0 @@
-// prismjs/components/prism-tsx@1.30.0 downloaded from https://ga.jspm.io/npm:prismjs@1.30.0/components/prism-tsx.js
-
-(function(e){var a=e.util.clone(e.languages.typescript);e.languages.tsx=e.languages.extend("jsx",a);delete e.languages.tsx.parameter;delete e.languages.tsx["literal-property"];var t=e.languages.tsx.tag;t.pattern=RegExp(/(^|[^\w$]|(?=<\/))/.source+"(?:"+t.pattern.source+")",t.pattern.flags);t.lookbehind=true})(Prism);var e={};export{e as default};
-
