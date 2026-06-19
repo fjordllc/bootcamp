63a3593ab25d990fe5804ad7ada9ad9b7a16b937
shibainurou <shibainurou@gmail.com>
2026-06-19 18:28:24 +0900

    元プラクティスのDocを含めるorNOTを判定するメソッドを追加

--- app/controllers/practices/pages_controller.rb
+++ app/controllers/practices/pages_controller.rb
@@ -12,4 +12,13 @@ class Practices::PagesController < ApplicationController
                  .page(params[:page])
                  .per(PAGER_NUMBER)
   end
+
+  private
+
+  def include_source?
+    # 給付金コースの場合、include_sourceはデフォルトでtrue
+    return false unless @practice.grant_course?
+
+    params.fetch(:include_source, 'true') == 'true'
+  end
 end
