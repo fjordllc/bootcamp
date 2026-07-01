799772fdc6c5b211dd982df977b10a1d33c8e6b9
Masaki Komagata <komagata@gmail.com>
2026-07-01 15:04:56 +0900

    理解度テストのレビュー指摘を追加反映

--- app/models/practice_quiz_choice.rb
+++ app/models/practice_quiz_choice.rb
@@ -2,7 +2,7 @@
 
 class PracticeQuizChoice < ApplicationRecord
   belongs_to :practice_quiz_question
-  has_many :practice_quiz_answers, dependent: :destroy
+  has_many :practice_quiz_answers, dependent: :restrict_with_error
 
   validates :body, presence: true
   validates :position, presence: true
