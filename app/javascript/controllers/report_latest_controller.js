00ddcf5820121f00a4a28fa1e2af57ce4a6e5918
shibainurou <shibainurou@gmail.com>
2026-07-16 07:31:26 +0900

    最新の日報を複製を複製ボタンを追加

---
+++ app/javascript/controllers/report_latest_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from '@hotwired/stimulus'
+import TextareaInitializer from 'textarea-initializer'
+import { toast } from 'vanillaToast'
+
+export default class extends Controller {
+  replaceReport(e) {
+  }
+}
