d15cb8b611a9bf563bb8e2539a515f1f7a0d9d01
oyamairishitomi <h.tezuka0707@gmail.com>
2026-07-15 17:23:41 +0900

    mentor_memosのcreateアクションを実装

--- app/controllers/api/mentor_memos_controller.rb
+++ app/controllers/api/mentor_memos_controller.rb
@@ -2,12 +2,13 @@
 
 class API::MentorMemosController < API::BaseController
   before_action :require_mentor_login_for_api
-  before_action -> { doorkeeper_authorize! :write }, only: %i[update], if: -> { doorkeeper_token.present? }
-  before_action -> { doorkeeper_authorize! :mentor }, only: %i[update], if: -> { doorkeeper_token.present? }
-  before_action :set_user, only: %i[update]
+  before_action -> { doorkeeper_authorize! :write }, only: %i[create], if: -> { doorkeeper_token.present? }
+  before_action -> { doorkeeper_authorize! :mentor }, only: %i[create], if: -> { doorkeeper_token.present? }
+  before_action :set_user, only: %i[create]
 
-  def update
-    if @user.update_mentor_memo(user_params[:mentor_memo])
+  def create
+    mentor_memo = @user.mentor_memos.new(content: user_params[:content], author: current_user)
+    if mentor_memo.save
       head :ok
     else
       head :bad_request
@@ -17,10 +18,10 @@ class API::MentorMemosController < API::BaseController
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:user][:user_id])
   end
 
   def user_params
-    params.require(:user).permit(:mentor_memo)
+    params.require(:user).permit(:content)
   end
 end
