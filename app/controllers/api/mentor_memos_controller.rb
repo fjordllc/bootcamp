194e8a35a246b3964f95b7825d0d7f342c2d2f0d
oyamairishitomi <h.tezuka0707@gmail.com>
2026-07-15 20:07:18 +0900

    mentor_memosにupdateアクションを追加(本人のみ編集可能)

--- app/controllers/api/mentor_memos_controller.rb
+++ app/controllers/api/mentor_memos_controller.rb
@@ -2,13 +2,23 @@
 
 class API::MentorMemosController < API::BaseController
   before_action :require_mentor_login_for_api
-  before_action -> { doorkeeper_authorize! :write }, only: %i[create], if: -> { doorkeeper_token.present? }
-  before_action -> { doorkeeper_authorize! :mentor }, only: %i[create], if: -> { doorkeeper_token.present? }
+  before_action -> { doorkeeper_authorize! :write }, only: %i[create update], if: -> { doorkeeper_token.present? }
+  before_action -> { doorkeeper_authorize! :mentor }, only: %i[create update], if: -> { doorkeeper_token.present? }
   before_action :set_user, only: %i[create]
+  before_action :set_mentor_memo, only: %i[update]
+  before_action :authorize_author, only: %i[update]
 
   def create
     mentor_memo = @user.mentor_memos.new(content: user_params[:content], author: current_user)
     if mentor_memo.save
+      head :ok
+    else
+      head :bad_request
+    end
+  end
+
+  def update
+    if @mentor_memo.update(content: user_params[:content])
       head :ok
     else
       head :bad_request
@@ -21,7 +31,15 @@ class API::MentorMemosController < API::BaseController
     @user = User.find(params[:user][:user_id])
   end
 
+  def set_mentor_memo
+    @mentor_memo = MentorMemo.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:content)
+  end
+
+  def authorize_author
+    head :forbidden unless @mentor_memo.author == current_user
   end
 end
