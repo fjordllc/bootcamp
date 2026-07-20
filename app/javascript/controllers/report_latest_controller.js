4d4b8317245742c357d408d30552d2d1887fb8c4
shibainurou <shibainurou@gmail.com>
2026-07-20 10:53:18 +0900

    最新の日報をJSONで受け取る側を実装

--- app/javascript/controllers/report_latest_controller.js
+++ app/javascript/controllers/report_latest_controller.js
@@ -3,6 +3,21 @@ import TextareaInitializer from 'textarea-initializer'
 import { toast } from 'vanillaToast'
 
 export default class extends Controller {
-  replaceReport(e) {
+  async replaceReport(e) {
+    e.preventDefault()
+
+    const response = await fetch('/api/reports/latest.json', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-Requested-With': 'XMLHttpRequest'
+      },
+      credentials: 'same-origin'
+    })
+
+    if (!response.ok) {
+      toast('最新の日報が見つかりませんでした。')
+      return
+    }
   }
 }
