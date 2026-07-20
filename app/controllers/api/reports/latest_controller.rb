eefff291586f9064e1d8c914339e9b6a867553d4
shibainurou <shibainurou@gmail.com>
2026-07-20 10:06:01 +0900

    最新の日報をJSONで返すAPIを実装

--- app/controllers/api/reports/latest_controller.rb
+++ app/controllers/api/reports/latest_controller.rb
@@ -2,9 +2,12 @@
 
 class API::Reports::LatestController < API::BaseController
   def show
-    @report = current_user
-              .reports
-              .order(reported_on: :desc)
-              .first
+    report = current_user
+             .reports
+             .order(reported_on: :desc)
+             .first
+    return render_not_found('最新の日報が見つかりません。') unless report
+
+    render json: { title: report.title, practice_ids: report.practice_ids, description: report.description }
   end
 end
