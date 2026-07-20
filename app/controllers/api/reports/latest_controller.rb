47bd4f39886b92ab32803b626dae9074954e8a33
shibainurou <shibainurou@gmail.com>
2026-07-20 17:05:36 +0900

    最新の日報を取得するAPIのレスポンスjsonに日報idを追加
    テストを書くときにidで突合させたいのでidを追加した。

--- app/controllers/api/reports/latest_controller.rb
+++ app/controllers/api/reports/latest_controller.rb
@@ -8,6 +8,6 @@ class API::Reports::LatestController < API::BaseController
              .first
     return render_not_found('最新の日報が見つかりません。') unless report
 
-    render json: { title: report.title, practice_ids: report.practice_ids, description: report.description }
+    render json: { id: report.id, title: report.title, practice_ids: report.practice_ids, description: report.description }
   end
 end
