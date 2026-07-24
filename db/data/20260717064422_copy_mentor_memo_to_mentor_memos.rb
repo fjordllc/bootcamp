dbc7fd8bbf795e71cbe266f1a993bd2e0ab905d4
oyamairishitomi <h.tezuka0707@gmail.com>
2026-07-24 14:35:49 +0900

    移行データの空白メモ除外とcreated_at欠落を修正

--- db/data/20260717064422_copy_mentor_memo_to_mentor_memos.rb
+++ db/data/20260717064422_copy_mentor_memo_to_mentor_memos.rb
@@ -2,9 +2,10 @@
 
 class CopyMentorMemoToMentorMemos < ActiveRecord::Migration[8.1]
   def up
-    User.where.not(mentor_memo: [nil, '']).find_each do |user|
+    User.find_each do |user|
+      next if user.mentor_memo.blank?
+
       mentor_memo = user.mentor_memos.build(content: user.mentor_memo)
-      mentor_memo.record_timestamps = false
       mentor_memo.save!
     end
   end
