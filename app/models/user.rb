3b1afc78f3308ee65ad5c8f9f7d4c846d89da6b9
oyamairishitomi <h.tezuka0707@gmail.com>
2026-07-29 17:38:32 +0900

    在籍ステータスに関するscope群をUserStatus concernに切り出す

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord # rubocop:todo Metrics/ClassLength
+class User < ApplicationRecord
   include ActionView::Helpers::AssetUrlHelper
   include Taggable
   include Searchable
   include StagingEnvironment
+  include UserStatus
 
   attr_accessor :credit_card_payment, :role, :uploaded_avatar
 
@@ -323,12 +324,6 @@ class User < ApplicationRecord # rubocop:todo Metrics/ClassLength
     languages_other_than_ruby_and_javascript
   ]
 
-  scope :in_school, -> { where(graduated_on: nil) }
-  scope :graduated, -> { where.not(graduated_on: nil) }
-  scope :hibernated, -> { where.not(hibernated_at: nil) }
-  scope :unhibernated, -> { where(hibernated_at: nil) }
-  scope :retired, -> { where.not(retired_on: nil) }
-  scope :unretired, -> { where(retired_on: nil) }
   scope :hibernated_for, ->(period) { where(hibernated_at: nil..period.ago) }
   scope :auto_retire, -> { where(auto_retire: true) }
   scope :advisers, -> { where(adviser: true) }
