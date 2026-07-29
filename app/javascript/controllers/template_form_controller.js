22048745e778021730b8c5894ec5172eebf1d5f0
koguchi-e <koguchi.e915@gmail.com>
2026-07-29 17:32:07 +0900

    削除ボタンを押した際、提出物テンプレートのプレビューも削除されるよう修正

--- app/javascript/controllers/template_form_controller.js
+++ app/javascript/controllers/template_form_controller.js
@@ -1,12 +1,14 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ "description", "destroy", "message" ]
+  static targets = ['description', 'destroy', 'message', 'preview']
 
   clear(event) {
     event.preventDefault()
-    this.descriptionTarget.value = ""
-    this.destroyTarget.value = "1"
-    this.messageTarget.classList.remove("hidden")
+
+    this.descriptionTarget.value = ''
+    this.previewTarget.textContent = ''
+    this.destroyTarget.value = '1'
+    this.messageTarget.classList.remove('hidden')
   }
 }
