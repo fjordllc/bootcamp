10c96efc87722a66523894e3d3621b5e9940e6e2
oyamairishitomi <h.tezuka0707@gmail.com>
2026-07-29 19:00:59 +0900

    students_and_traineesとstudents_trainees_mentors_and_adminsの重複条件をDRYする

--- app/models/concerns/user_student_group_scopes.rb
+++ app/models/concerns/user_student_group_scopes.rb
@@ -4,25 +4,8 @@ module UserStudentGroupScopes
   extend ActiveSupport::Concern
 
   included do
-    scope :students_and_trainees, lambda {
-      where(
-        admin: false,
-        mentor: false,
-        adviser: false,
-        graduated_on: nil,
-        hibernated_at: nil,
-        retired_on: nil,
-        training_completed_at: nil
-      )
-    }
-    scope :students_trainees_mentors_and_admins, lambda {
-      where(
-        adviser: false,
-        graduated_on: nil,
-        hibernated_at: nil,
-        retired_on: nil
-      )
-    }
+    scope :students_and_trainees, -> { students_trainees_mentors_and_admins.where(admin: false, mentor: false, training_completed_at: nil) }
+    scope :students_trainees_mentors_and_admins, -> { where(adviser: false, graduated_on: nil, hibernated_at: nil).unretired }
     scope :students_base, lambda {
       where(
         admin: false,
