431204934ac87bcf2364cf095174f64f630bc1d8
oyamairishitomi <h.tezuka0707@gmail.com>
2026-07-30 22:37:41 +0900

    notification_receiver/users_role/users_job/tagsクラスメソッドをUserStatusScopesに切り出す・行数削減

--- app/models/concerns/user_status_scopes.rb
+++ app/models/concerns/user_status_scopes.rb
@@ -19,4 +19,38 @@ module UserStatusScopes
       )
     }
   end
+
+  class_methods do
+    def notification_receiver(target)
+      case target
+      when 'all' then User.unretired
+      when 'students' then User.admins_and_mentors.or(User.students)
+      when 'job_seekers' then User.admins_and_mentors.or(User.job_seekers)
+      when 'none' then User.none
+      else User.none
+      end
+    end
+
+    # このメソッドはユーザから送信された値をsendに渡すので、悪意のあるコードが実行される危険性がある
+    # そのため、このメソッドを使用する際には安全性の確保のために以下の引数を指定すること
+    # allowed_targets:　呼び出したいscope名に対応するtargetを過不足なく指定した配列。
+    # default_target: targetに不正な値が渡された際、users_roleが返すスコープ名に対応するtargetを指定する。デフォルトでは:noneを指定しているため何も返さない。
+    def users_role(target, allowed_targets: [], default_target: :none)
+      key = (User::ALL_ALLOWED_TARGETS & allowed_targets).include?(target) ? target : default_target
+      scope_name = User::TARGET_TO_SCOPE.fetch(key, key)
+      send(scope_name)
+    end
+
+    # User::users_roleと同じく安全性確保のため、以下の条件を指定している。
+    # allowed_job.include?(job): 存在する職業を過不足なく指定した配列の中に、jobが存在するかどうかチェック。
+    def users_job(job)
+      allowed_jobs = User.jobs.keys.freeze
+      scope_name = allowed_jobs.include?(job) ? "job_#{job}" : 'all'
+      send(scope_name)
+    end
+
+    def tags
+      unretired.unhibernated.all_tag_counts(order: 'count desc, name asc')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -339,45 +339,6 @@ class User < ApplicationRecord
     languages_other_than_ruby_and_javascript
   ]
 
-  class << self
-    def notification_receiver(target)
-      case target
-      when 'all'
-        User.unretired
-      when 'students'
-        User.admins_and_mentors.or(User.students)
-      when 'job_seekers'
-        User.admins_and_mentors.or(User.job_seekers)
-      when 'none'
-        User.none
-      else
-        User.none
-      end
-    end
-
-    # このメソッドはユーザから送信された値をsendに渡すので、悪意のあるコードが実行される危険性がある
-    # そのため、このメソッドを使用する際には安全性の確保のために以下の引数を指定すること
-    # allowed_targets:　呼び出したいscope名に対応するtargetを過不足なく指定した配列。
-    # default_target: targetに不正な値が渡された際、users_roleが返すスコープ名に対応するtargetを指定する。デフォルトでは:noneを指定しているため何も返さない。
-    def users_role(target, allowed_targets: [], default_target: :none)
-      key = (ALL_ALLOWED_TARGETS & allowed_targets).include?(target) ? target : default_target
-      scope_name = TARGET_TO_SCOPE.fetch(key, key)
-      send(scope_name)
-    end
-
-    # User::users_roleと同じく安全性確保のため、以下の条件を指定している。
-    # allowed_job.include?(job): 存在する職業を過不足なく指定した配列の中に、jobが存在するかどうかチェック。
-    def users_job(job)
-      allowed_jobs = User.jobs.keys.freeze
-      scope_name = allowed_jobs.include?(job) ? "job_#{job}" : 'all'
-      send(scope_name)
-    end
-
-    def tags
-      unretired.unhibernated.all_tag_counts(order: 'count desc, name asc')
-    end
-  end
-
   def generation
     (created_at.year - 2013) * 4 + (created_at.month + 2) / 3
   end
