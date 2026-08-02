927e626b65be68658123655a3ffb8297fbf99d7a
oyamairishitomi <h.tezuka0707@gmail.com>
2026-08-02 20:20:27 +0900

    メール送信時にstopped_mail_by_adminを考慮するようにする

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'フィヨルドブートキャンプ <noreply@bootcamp.fjord.jp>'
   layout 'mailer'
+
+  after_action :stopped_mail_by_admin
+
+  private
+
+  def stopped_mail_by_admin
+    if @user&.stopped_mail_by_admin?
+      mail.perform_deliveries = false
+    end
+  end
 end
