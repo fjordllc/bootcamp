cd2f5f447666bbeed596937687c6ac25dc956a29
machida <machida@fjord.jp>
2026-08-13 03:44:00 +0900

    一括サムネイル生成テストをCI環境で安定させる

--- test/jobs/bulk_generate_movie_thumbnail_job_test.rb
+++ test/jobs/bulk_generate_movie_thumbnail_job_test.rb
@@ -5,6 +5,12 @@ require 'test_helper'
 class BulkGenerateMovieThumbnailJobTest < ActiveJob::TestCase
   test 'generates thumbnails synchronously for movies without one' do
     movie = movies(:movie1)
+    thumbnail_path = Rails.root.join('test/fixtures/files/articles/ogp_images/test.jpg')
+    Movie.where.not(id: movie.id).find_each do |other_movie|
+      File.open(thumbnail_path) do |thumbnail|
+        other_movie.thumbnail.attach(io: thumbnail, filename: 'test.jpg', content_type: 'image/jpeg')
+      end
+    end
     movie.thumbnail.purge
     movie.movie_data.attach(
       io: File.open(Rails.root.join('test/fixtures/files/movies/movie.mp4')),
