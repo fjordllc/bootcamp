e1305093f84406f56f69b8e12476892827c6cdfc
mikan-v7y <mikan.v7y@gmail.com>
2026-08-16 15:59:55 +0900

    検索モーダルの開閉処理をStimulus化

--- test/system/header_test.rb
+++ test/system/header_test.rb
@@ -18,4 +18,11 @@ class HeaderTest < ApplicationSystemTestCase
     find('#notifications-bell-button').click
     assert_selector '#notifications-bell-container.is-opened-dropdown'
   end
+
+  test 'toggle search modal' do
+    visit_with_auth root_path, 'komagata'
+
+    find('button.header-links__link', text: '検索').click
+    assert_selector '#js-modal-search.is-shown'
+  end
 end
