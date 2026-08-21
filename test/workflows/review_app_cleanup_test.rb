c202f014175c246661550d132f5c11b904543894
Masaki Komagata <komagata@gmail.com>
2026-08-21 15:04:06 +0900

    cleanupコマンド単位で設定を検証する

--- test/workflows/review_app_cleanup_test.rb
+++ test/workflows/review_app_cleanup_test.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'yaml'
 
 class ReviewAppCleanupTest < ActiveSupport::TestCase
   WORKFLOW = Rails.root.join('.github/workflows/review-app-cleanup.yml')
 
   test 'waits for Cloud Build without streaming logs' do
-    workflow = WORKFLOW.read
+    cleanup_step = YAML.load_file(WORKFLOW).dig('jobs', 'cleanup', 'steps').find do |step|
+      step['name'] == 'Cleanup Review App via Cloud Build'
+    end
+    command = cleanup_step.fetch('run')
 
-    assert_includes workflow, '--suppress-logs'
-    assert_not_includes workflow, '--async'
+    assert_includes command, '--suppress-logs'
+    assert_not_includes command, '--async'
   end
 end
