cc5c736b388182e63f735b699992e2cfd977c011
Masaki Komagata <komagata@gmail.com>
2026-08-26 21:11:13 +0900

    polyfillをCDNから読み込む #10479

--- test/lib/importmap_application_test.rb
+++ test/lib/importmap_application_test.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'open3'
-
 class ImportmapApplicationTest < ActiveSupport::TestCase
   test 'loads the DisposableStack polyfill only when Cocooned needs it' do
     application = Rails.root.join('app/javascript/importmap_application.js').read
     polyfill = Rails.application.importmap.packages['core-js/actual/disposable-stack']
 
-    assert_equal 'disposable-stack-polyfill.js', polyfill.path
+    assert_equal 'https://esm.sh/core-js@3.50.0/es2022/actual/disposable-stack.bundle.mjs', polyfill.path
     assert_not polyfill.preload
     assert_includes application, "document.querySelector('.cocooned-container')"
     assert_match(
@@ -21,24 +19,5 @@ class ImportmapApplicationTest < ActiveSupport::TestCase
     assert_not_nil cocooned_index
     assert_operator polyfill_index, :<, cocooned_index
     assert_includes application, "console.error('Failed to initialize Cocooned', error)"
-  end
-
-  test 'the polyfill implements DisposableStack' do
-    polyfill = Rails.root.join('vendor/javascript/disposable-stack-polyfill.js')
-    script = <<~'JAVASCRIPT'
-      Object.defineProperty(globalThis, 'DisposableStack', { value: undefined, configurable: true })
-      require(process.argv[1])
-      const calls = []
-      const stack = new DisposableStack()
-      stack.use({ [Symbol.dispose]: () => calls.push('use') })
-      stack.defer(() => calls.push('defer'))
-      stack.dispose()
-      if (calls.join(',') !== 'defer,use') process.exit(1)
-    JAVASCRIPT
-
-    _, error, status = Open3.capture3('node', '-e', script, polyfill.to_s)
-
-    assert status.success?, error
-    assert_includes polyfill.read, "defineWellKnownSymbol('dispose')"
   end
 end
