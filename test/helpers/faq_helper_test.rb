c9e506e4ddb0ee5910ea199e630a3d2f216668af
samovar <samovar1214@gmail.com>
2026-09-22 15:26:36 +0900

    重複するテストケースを削除

--- test/helpers/faq_helper_test.rb
+++ test/helpers/faq_helper_test.rb
@@ -3,11 +3,6 @@
 require 'test_helper'
 
 class FAQHelperTest < ActionView::TestCase
-  test '#format_question' do
-    faq = faqs(:faq1)
-    assert_equal format_question(faq.question), "#{faq.question}？"
-  end
-
   test '#format_question returns a ？ mark' do
     faq = faqs(:faq1)
     faq.question += '？'
